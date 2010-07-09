09e505c5e3f086d4bb761cf29592fe8b87bcb5d0
John E. Bailey <baileyje@gmail.com>
2010-07-09 14:22:01 -0500

    Updated service deployment item to handle mbean attribute configuration

--- domain/src/main/java/org/jboss/as/deployment/item/JBossServiceDeploymentItem.java
+++ domain/src/main/java/org/jboss/as/deployment/item/JBossServiceDeploymentItem.java
@@ -22,6 +22,7 @@
 
 package org.jboss.as.deployment.item;
 
+import org.jboss.as.deployment.descriptor.JBossServiceAttributeConfig;
 import org.jboss.as.deployment.descriptor.JBossServiceConfig;
 import org.jboss.as.deployment.descriptor.JBossServiceConstructorConfig;
 import org.jboss.as.deployment.descriptor.JBossServiceDependencyConfig;
@@ -29,7 +30,6 @@ import org.jboss.modules.Module;
 import org.jboss.msc.service.BatchBuilder;
 import org.jboss.msc.service.BatchInjectionBuilder;
 import org.jboss.msc.service.BatchServiceBuilder;
-import org.jboss.msc.service.DuplicateServiceException;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.value.ConstructedValue;
 import org.jboss.msc.value.LookupClassValue;
@@ -41,6 +41,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ *  Deployment item responsible for taking a JBossServiceConfig object and creating the necessary services.
+ * 
  * @author John E. Bailey
  */
 public class JBossServiceDeploymentItem implements DeploymentItem {
@@ -54,10 +56,11 @@ public class JBossServiceDeploymentItem implements DeploymentItem {
     public void install(DeploymentItemContext context) {
         final Module module = context.getModule();
         final ClassLoader classLoader = module.getClassLoader();
+        final Value<ClassLoader> classLoaderValue = Values.immediateValue(classLoader);
         final BatchBuilder batchBuilder = context.getBatchBuilder();
 
         final String codeName = serviceConfig.getCode();
-        final LookupClassValue classValue = new LookupClassValue(codeName, Values.immediateValue(classLoader));
+        final LookupClassValue classValue = new LookupClassValue(codeName, classLoaderValue);
 
         final List<Value<?>> constructorArguments = new ArrayList<Value<?>>();
         final List<Value<Class<?>>> constructorSignature = new ArrayList<Value<Class<?>>>();
@@ -66,9 +69,9 @@ public class JBossServiceDeploymentItem implements DeploymentItem {
         if(constructorConfig != null) {
             final JBossServiceConstructorConfig.Argument[] arguments = constructorConfig.getArguments();
             for(JBossServiceConstructorConfig.Argument argument : arguments) {
-                final Object value = null; // TODO: Do something with argument.getValue()
-                constructorArguments.add(Values.immediateValue(value));
-                constructorSignature.add(new LookupClassValue(argument.getType(), Values.immediateValue(classLoader)));
+                final Value<?> value = null; // TODO: Do something with argument.getValue()
+                constructorArguments.add(value);
+                constructorSignature.add(new LookupClassValue(argument.getType(), classLoaderValue));
             }
         }
 
@@ -79,13 +82,44 @@ public class JBossServiceDeploymentItem implements DeploymentItem {
         final String serviceName = serviceConfig.getName();
         final BatchServiceBuilder<?> serviceBuilder = batchBuilder.addService(ServiceName.of(serviceName), jBossService);
         final JBossServiceDependencyConfig[] dependencyConfigs = serviceConfig.getDependencyConfigs();
-        for(JBossServiceDependencyConfig dependencyConfig : dependencyConfigs) {
-            final BatchInjectionBuilder injectionBuilder = serviceBuilder.addDependency(ServiceName.of(dependencyConfig.getDependencyName()));
-            final String optionalAttributeName = dependencyConfig.getOptionalAttributeName();
-            if(optionalAttributeName != null) {
-                injectionBuilder.toProperty(optionalAttributeName);
+        if(dependencyConfigs != null) {
+            for(JBossServiceDependencyConfig dependencyConfig : dependencyConfigs) {
+                final BatchInjectionBuilder injectionBuilder = serviceBuilder.addDependency(ServiceName.of(dependencyConfig.getDependencyName()));
+                final String optionalAttributeName = dependencyConfig.getOptionalAttributeName();
+                if(optionalAttributeName != null) {
+                    injectionBuilder.toProperty(optionalAttributeName);
+                }
             }
         }
-        // TODO: What should we do with the MBEAN attributes
+
+        final JBossServiceAttributeConfig[] attributeConfigs = serviceConfig.getAttributeConfigs();
+        if(attributeConfigs != null) {
+            for(JBossServiceAttributeConfig attributeConfig : attributeConfigs) {
+                final String attributeName = attributeConfig.getName();
+                final JBossServiceAttributeConfig.Inject inject = attributeConfig.getInject();
+                final JBossServiceAttributeConfig.ValueFactory valueFactory = attributeConfig.getValueFactory();
+                if(inject != null) {
+                    final BatchInjectionBuilder injectionBuilder = serviceBuilder.addDependency(ServiceName.of(inject.getBeanName()))
+                        .toProperty(attributeName);
+                    final String propertyName = inject.getPropertyName();
+                    if(propertyName != null) {
+                        injectionBuilder.fromProperty(inject.getPropertyName());
+                    }
+                } else if(valueFactory != null) {
+                    final String methodName = valueFactory.getMethodName();
+                    final JBossServiceAttributeConfig.ValueFactoryParameter[] parameters = valueFactory.getParameters();
+                    final List<Value<Class<?>>> paramTypes = new ArrayList<Value<Class<?>>>(parameters.length);
+                    final List<Value<?>> paramValues = new ArrayList<Value<?>>(parameters.length);
+                    for(JBossServiceAttributeConfig.ValueFactoryParameter parameter : parameters) {
+                        paramTypes.add(new LookupClassValue(parameter.getClassName(), classLoaderValue));
+                        final Value<?> value = null; // TODO: Use parameter.getValue() to get the value
+                        paramValues.add(value);
+                    }
+                    final BatchInjectionBuilder injectionBuilder = serviceBuilder.addDependency(ServiceName.of(valueFactory.getBeanName()))
+                        .toProperty(attributeName)
+                        .fromMethod(methodName, paramTypes, paramValues);
+                }
+            }
+        }
     }
 }
