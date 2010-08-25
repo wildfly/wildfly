3f9af0c244986f508f8660d505f471dca18b9244
Brian Stansberry <brian.stansberry@jboss.com>
2010-08-25 16:42:04 -0500

    [JBAS-8340]

--- build.bat
+++ build.bat
@@ -27,7 +27,7 @@ SETLOCAL
 
 set CLASSPATH=
 set M2_HOME=
-set MAVEN+HOME=
+set MAVEN_HOME=
 
 REM ******************************************************
 REM - "for" loops have been unrolled for compatibility
