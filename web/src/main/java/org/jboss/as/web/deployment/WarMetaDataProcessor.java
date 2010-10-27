9655a766e708272689942816fafa8d61ff5bca3f
Jean-Frederic Clere <jfclere@jfcpc.(none)>
2010-10-27 09:56:36 +0200

    Don't process annotiation when there is none...

--- web/src/main/java/org/jboss/as/web/deployment/WarMetaDataProcessor.java
+++ web/src/main/java/org/jboss/as/web/deployment/WarMetaDataProcessor.java
@@ -259,7 +259,9 @@ public class WarMetaDataProcessor implements DeploymentUnitProcessor {
                 // Add non overriding default distributable flag
                 webFragmentMetaData.setDistributable(new EmptyMetaData());
             }
-            WebMetaData jarAnnotatedMetaData = annotationsMetaData.get(jar);
+            WebMetaData jarAnnotatedMetaData = null;
+            if (annotationsMetaData != null)
+                jarAnnotatedMetaData = annotationsMetaData.get(jar);
             if ((isComplete || webFragmentMetaData.isMetadataComplete()) && jarAnnotatedMetaData != null) {
                 // Discard @WebFilter, @WebListener and @WebServlet
                 jarAnnotatedMetaData.setFilters(null);
