9b0b9c3b8d97a71c8f70427a78a0ee92e51e6052
Jason T. Greene <jason.greene@redhat.com>
2010-11-01 18:53:40 -0500

    Add domain.bat for windows. Also change default domain.xml to use IPv4 (IPv6 is broken on older Windows)

---
+++ build/src/main/resources/bin/domain.bat
@@ -0,0 +1,109 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem JBoss Bootstrap Script for Windows
+rem -------------------------------------------------------------------------
+
+rem $Id$
+
+@if not "%ECHO%" == ""  echo %ECHO%
+@if "%OS%" == "Windows_NT" setlocal
+
+if "%OS%" == "Windows_NT" (
+  set "DIRNAME=%~dp0%"
+) else (
+  set DIRNAME=.\
+)
+
+rem Read an optional configuration file.
+if "x%DOMAIN_CONF%" == "x" (   
+   set "DOMAIN_CONF=%DIRNAME%domain.conf.bat"
+)
+if exist "%DOMAIN_CONF%" (
+   echo Calling %DOMAIN_CONF%
+   call "%DOMAIN_CONF%" %*
+) else (
+   echo Config file not found %DOMAIN_CONF%
+)
+
+pushd %DIRNAME%..
+if "x%JBOSS_HOME%" == "x" (
+  set "JBOSS_HOME=%CD%"
+)
+popd
+
+set DIRNAME=
+
+if "%OS%" == "Windows_NT" (
+  set "PROGNAME=%~nx0%"
+) else (
+  set "PROGNAME=domain.bat"
+)
+
+rem Setup JBoss specific properties
+set JAVA_OPTS=-Dprogram.name=%PROGNAME% %JAVA_OPTS%
+
+if "x%JAVA_HOME%" == "x" (
+  set  JAVA=java
+  echo JAVA_HOME is not set. Unexpected results may occur.
+  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  set "JAVA=%JAVA_HOME%\bin\java"
+)
+
+rem Add -server to the JVM options, if supported
+"%JAVA%" -server -version 2>&1 | findstr /I hotspot > nul
+if not errorlevel == 1 (
+  set "JAVA_OPTS=%JAVA_OPTS% -server"
+)
+
+rem Find run.jar, or we can't continue
+if exist "%JBOSS_HOME%\jboss-modules.jar" (
+    set "RUNJAR=%JBOSS_HOME%\jboss-modules.jar"
+) else (
+  echo Could not locate "%JBOSS_HOME%\jboss-modules.jar".
+  echo Please check that you are in the bin directory when running this script.
+  goto END
+)
+
+rem Setup JBoss specific properties
+
+rem Setup the java endorsed dirs
+set JBOSS_ENDORSED_DIRS=%JBOSS_HOME%\lib\endorsed
+
+echo ===============================================================================
+echo.
+echo   JBoss Bootstrap Environment
+echo.
+echo   JBOSS_HOME: %JBOSS_HOME%
+echo.
+echo   JAVA: %JAVA%
+echo.
+echo   JAVA_OPTS: %JAVA_OPTS%
+echo.
+echo ===============================================================================
+echo.
+
+:RESTART
+"%JAVA%" %JAVA_OPTS% ^
+ -Dorg.jboss.boot.log.file=%JBOSS_HOME%\process-manager\log\boot.log ^
+ -Dlogging.configuration=file:%JBOSS_HOME%/domain/configuration/logging.properties ^
+    -jar "%JBOSS_HOME%\jboss-modules.jar" ^
+    -mp "%JBOSS_HOME%\modules" ^
+    -logmodule "org.jboss.logmanager" ^
+     org.jboss.as.process-manager ^
+    -jboss-home "%JBOSS_HOME%" ^
+    -jvm "%JAVA%" ^
+    -- ^
+    -Dorg.jboss.boot.log.file=%JBOSS_HOME%\domain\log\server-manager\boot.log ^
+    -Dlogging.configuration=file:%JBOSS_HOME%/domain/configuration/logging.properties ^
+    %JAVA_OPTS% ^
+    -- ^
+    -default-jvm "%JAVA%" ^
+    %*
+
+if ERRORLEVEL 10 goto RESTART
+
+:END
+if "x%NOPAUSE%" == "x" pause
+
+:END_NO_PAUSE
