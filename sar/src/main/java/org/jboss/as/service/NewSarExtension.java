d4e7930a99930ca99bafdeb68e5a42abe44b7a28
Emanuel Muckenhuber <emuckenh@redhat.com>
2011-01-13 19:13:53 +0100

    detyped sar subsystem

---
+++ sar/src/main/java/org/jboss/as/service/NewSarExtension.java
@@ -0,0 +1,87 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.service;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
+import java.util.List;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+
+import org.jboss.as.controller.NewExtension;
+import org.jboss.as.controller.NewExtensionContext;
+import org.jboss.as.controller.SubsystemRegistration;
+import org.jboss.as.controller.parsing.ExtensionParsingContext;
+import org.jboss.as.controller.registry.ModelNodeRegistration;
+import org.jboss.as.model.ParseUtils;
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementReader;
+import org.jboss.staxmapper.XMLElementWriter;
+import org.jboss.staxmapper.XMLExtendedStreamReader;
+import org.jboss.staxmapper.XMLExtendedStreamWriter;
+
+/**
+ * @author Emanuel Muckenhuber
+ */
+public class NewSarExtension implements NewExtension {
+
+    public static final String NAMESPACE = "urn:jboss:domain:sar:1.0";
+
+    static final SarSubsystemParser parser = new SarSubsystemParser();
+
+    /** {@inheritDoc} */
+    public void initialize(NewExtensionContext context) {
+        final SubsystemRegistration subsystem = context.registerSubsystem("sar");
+        final ModelNodeRegistration registration = subsystem.registerSubsystemModel(NewSarSubsystemProviders.SUBSYSTEM);
+        registration.registerOperationHandler(ADD, NewSarSubsystemAdd.INSTANCE, NewSarSubsystemProviders.SUBSYSTEM_ADD, false);
+    }
+
+    /** {@inheritDoc} */
+    public void initializeParsers(ExtensionParsingContext context) {
+        context.setSubsystemXmlMapping(NAMESPACE, parser, parser);
+    }
+
+    static class SarSubsystemParser implements XMLStreamConstants, XMLElementReader<List<ModelNode>>, XMLElementWriter<ModelNode> {
+
+        /** {@inheritDoc} */
+        public void writeContent(XMLExtendedStreamWriter writer, ModelNode node) throws XMLStreamException {
+            //
+        }
+
+        /** {@inheritDoc} */
+        public void readElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
+            // Require no content
+            ParseUtils.requireNoContent(reader);
+
+            final ModelNode subsystem = new ModelNode();
+            subsystem.get(OP).set(ADD);
+            subsystem.get(OP_ADDR).setEmptyObject();
+            list.add(subsystem);
+        }
+
+    }
+
+}
