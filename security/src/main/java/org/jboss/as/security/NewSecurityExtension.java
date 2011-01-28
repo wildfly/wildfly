650d9aabdcd477b8e53bca88039356ad73f328ad
Darran Lofthouse <darran.lofthouse@jboss.com>
2011-01-28 16:34:55 +0000

    Converted security subsystem to detyped API.

---
+++ security/src/main/java/org/jboss/as/security/NewSecurityExtension.java
@@ -0,0 +1,148 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.security;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.security.CommonAttributes.AUTHENTICATION_MANAGER_CLASS_NAME;
+import static org.jboss.as.security.CommonAttributes.DEEP_COPY_SUBJECT_MODE;
+import static org.jboss.as.security.CommonAttributes.DEFAULT_CALLBACK_HANDLER_CLASS_NAME;
+
+import java.util.List;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+
+import org.jboss.as.controller.NewExtension;
+import org.jboss.as.controller.NewExtensionContext;
+import org.jboss.as.controller.SubsystemRegistration;
+import org.jboss.as.controller.parsing.ExtensionParsingContext;
+import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
+import org.jboss.as.controller.registry.ModelNodeRegistration;
+import org.jboss.as.model.ParseUtils;
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementReader;
+import org.jboss.staxmapper.XMLElementWriter;
+import org.jboss.staxmapper.XMLExtendedStreamReader;
+import org.jboss.staxmapper.XMLExtendedStreamWriter;
+
+/**
+ * The security extension
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+public class NewSecurityExtension implements NewExtension {
+
+    public static final String SUBSYSTEM_NAME = "security";
+
+    private static final SecuritySubsystemParser PARSER = new SecuritySubsystemParser();
+
+    @Override
+    public void initialize(NewExtensionContext context) {
+        final SubsystemRegistration subsystem = context.registerSubsystem(SUBSYSTEM_NAME);
+        final ModelNodeRegistration registration = subsystem.registerSubsystemModel(SecuritySubsystemProviders.SUBSYSTEM);
+        registration.registerOperationHandler(ADD, NewSecuritySubsystemAdd.INSTANCE, SecuritySubsystemProviders.SUBSYSTEM_ADD,
+                false);
+        subsystem.registerXMLElementWriter(PARSER);
+    }
+
+    @Override
+    public void initializeParsers(ExtensionParsingContext context) {
+        context.setSubsystemXmlMapping(Namespace.CURRENT.getUriString(), PARSER);
+    }
+
+    static class SecuritySubsystemParser implements XMLStreamConstants, XMLElementReader<List<ModelNode>>,
+            XMLElementWriter<SubsystemMarshallingContext> {
+
+        @Override
+        public void readElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
+            final ModelNode subsystem = new ModelNode();
+            subsystem.get(OP).set(ADD);
+            subsystem.get(OP_ADDR).add(SUBSYSTEM, SUBSYSTEM_NAME);
+
+            // read attributes
+            final int count = reader.getAttributeCount();
+            for (int i = 0; i < count; i++) {
+                final String value = reader.getAttributeValue(i);
+                if (reader.getAttributeNamespace(i) != null) {
+                    throw ParseUtils.unexpectedAttribute(reader, i);
+                } else {
+                    final Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
+                    switch (attribute) {
+                        case AUTHENTICATION_MANAGER_CLASS_NAME: {
+                            subsystem.get(AUTHENTICATION_MANAGER_CLASS_NAME).set(value);
+                            break;
+                        }
+                        case DEEP_COPY_SUBJECT_MODE: {
+                            subsystem.get(DEEP_COPY_SUBJECT_MODE).set(value);
+                            break;
+                        }
+                        case DEFAULT_CALLBACK_HANDLER_CLASS_NAME: {
+                            subsystem.get(DEFAULT_CALLBACK_HANDLER_CLASS_NAME).set(value);
+                            break;
+                        }
+                        default:
+                            throw ParseUtils.unexpectedAttribute(reader, i);
+                    }
+                }
+            }
+
+            // no sub elements yet
+            ParseUtils.requireNoContent(reader);
+
+            list.add(subsystem);
+        }
+
+        @Override
+        public void writeContent(XMLExtendedStreamWriter writer, SubsystemMarshallingContext context) throws XMLStreamException {
+            context.startSubsystemElement(Namespace.CURRENT.getUriString(), false);
+
+            ModelNode node = context.getModelNode();
+            if (has(node, AUTHENTICATION_MANAGER_CLASS_NAME)) {
+                writeAttribute(writer, Attribute.AUTHENTICATION_MANAGER_CLASS_NAME, node.get(AUTHENTICATION_MANAGER_CLASS_NAME));
+            }
+            if (has(node, DEEP_COPY_SUBJECT_MODE)) {
+                writeAttribute(writer, Attribute.DEEP_COPY_SUBJECT_MODE, node.get(DEEP_COPY_SUBJECT_MODE));
+            }
+            if (has(node, DEFAULT_CALLBACK_HANDLER_CLASS_NAME)) {
+                writeAttribute(writer, Attribute.DEFAULT_CALLBACK_HANDLER_CLASS_NAME,
+                        node.get(DEFAULT_CALLBACK_HANDLER_CLASS_NAME));
+            }
+
+            writer.writeEndElement();
+        }
+
+        private boolean has(ModelNode node, String name) {
+            return node.has(name) && node.get(name) != null;
+        }
+
+        private void writeAttribute(final XMLExtendedStreamWriter writer, final Attribute attr, final ModelNode value)
+                throws XMLStreamException {
+            writer.writeAttribute(attr.getLocalName(), value.asString());
+        }
+
+    }
+
+}
---
+++ security/src/main/java/org/jboss/as/security/NewSecuritySubsystemAdd.java
@@ -0,0 +1,123 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.security;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REMOVE;
+import static org.jboss.as.security.CommonAttributes.AUTHENTICATION_MANAGER_CLASS_NAME;
+import static org.jboss.as.security.CommonAttributes.DEEP_COPY_SUBJECT_MODE;
+import static org.jboss.as.security.CommonAttributes.DEFAULT_CALLBACK_HANDLER_CLASS_NAME;
+
+import javax.naming.Context;
+import javax.naming.Reference;
+
+import org.jboss.as.controller.Cancellable;
+import org.jboss.as.controller.ModelAddOperationHandler;
+import org.jboss.as.controller.NewOperationContext;
+import org.jboss.as.controller.ResultHandler;
+import org.jboss.as.naming.service.JavaContextService;
+import org.jboss.as.security.context.SecurityDomainObjectFactory;
+import org.jboss.as.security.service.JaasBinderService;
+import org.jboss.as.security.service.SecurityBootstrapService;
+import org.jboss.as.security.service.SecurityManagementService;
+import org.jboss.as.security.service.SubjectFactoryService;
+import org.jboss.as.server.NewRuntimeOperationContext;
+import org.jboss.as.server.RuntimeOperationHandler;
+import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.ServiceController;
+import org.jboss.msc.service.ServiceTarget;
+import org.jboss.msc.value.Values;
+import org.jboss.security.ISecurityManagement;
+
+/**
+ * Add Security Subsystem Operation.
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+class NewSecuritySubsystemAdd implements ModelAddOperationHandler, RuntimeOperationHandler {
+
+    private static final String DEFAULT_AUTHENTICATION_MANAGER = "org.jboss.security.plugins.auth.JaasSecurityManagerBase";
+    private static final boolean DEFAULT_DEEP_COPY_OPERATION_MODE = false;
+    private static final String DEFAULT_CALLBACK_HANDLER = "org.jboss.security.auth.callback.JBossCallbackHandler";
+
+    static final NewSecuritySubsystemAdd INSTANCE = new NewSecuritySubsystemAdd();
+
+    private NewSecuritySubsystemAdd() {
+        // Private to ensure a singleton.
+    }
+
+    @Override
+    public Cancellable execute(NewOperationContext context, ModelNode operation, ResultHandler resultHandler) {
+        // Create the compensating operation
+        final ModelNode compensatingOperation = new ModelNode();
+        compensatingOperation.get(OP).set(REMOVE);
+        compensatingOperation.get(OP_ADDR).set(operation.require(OP_ADDR));
+
+        String authenticationManagerClassName = operation.has(AUTHENTICATION_MANAGER_CLASS_NAME) ? operation.get(
+                AUTHENTICATION_MANAGER_CLASS_NAME).asString() : DEFAULT_AUTHENTICATION_MANAGER;
+        boolean deepCopySubjectMode = operation.has(DEEP_COPY_SUBJECT_MODE) ? operation.get(DEEP_COPY_SUBJECT_MODE).asBoolean()
+                : DEFAULT_DEEP_COPY_OPERATION_MODE;
+        String defaultCallbackHandlerClassName = operation.has(DEFAULT_CALLBACK_HANDLER_CLASS_NAME) ? operation.get(
+                DEFAULT_CALLBACK_HANDLER_CLASS_NAME).asString() : DEFAULT_CALLBACK_HANDLER;
+
+        if (context instanceof NewRuntimeOperationContext) {
+            final NewRuntimeOperationContext updateContext = (NewRuntimeOperationContext) context;
+            final ServiceTarget target = updateContext.getServiceTarget();
+
+            // add bootstrap service
+            final SecurityBootstrapService bootstrapService = new SecurityBootstrapService();
+            target.addService(SecurityBootstrapService.SERVICE_NAME, bootstrapService)
+                    .setInitialMode(ServiceController.Mode.ACTIVE).install();
+
+            // add security management service
+            final SecurityManagementService securityManagementService = new SecurityManagementService(
+                    authenticationManagerClassName, deepCopySubjectMode, defaultCallbackHandlerClassName);
+            target.addService(SecurityManagementService.SERVICE_NAME, securityManagementService)
+                    .setInitialMode(ServiceController.Mode.ACTIVE).install();
+
+            // add service to bind SecurityDomainObjectFactory to JNDI
+            final Reference reference = SecurityDomainObjectFactory.createReference("JSM");
+            final JaasBinderService binderService = new JaasBinderService(Values.immediateValue(reference));
+            target.addService(JaasBinderService.SERVICE_NAME, binderService)
+                    .addDependency(JavaContextService.SERVICE_NAME, Context.class, binderService.getContextInjector())
+                    .setInitialMode(ServiceController.Mode.ACTIVE).install();
+
+            // add subject factory service
+            final SubjectFactoryService subjectFactoryService = new SubjectFactoryService();
+            target.addService(SubjectFactoryService.SERVICE_NAME, subjectFactoryService)
+                    .addDependency(SecurityManagementService.SERVICE_NAME, ISecurityManagement.class,
+                            subjectFactoryService.getSecurityManagementInjector())
+                    .setInitialMode(ServiceController.Mode.ACTIVE).install();
+        }
+
+        final ModelNode subModel = context.getSubModel();
+        subModel.get(AUTHENTICATION_MANAGER_CLASS_NAME).set(operation.get(AUTHENTICATION_MANAGER_CLASS_NAME));
+        subModel.get(DEEP_COPY_SUBJECT_MODE).set(operation.get(DEEP_COPY_SUBJECT_MODE));
+        subModel.get(DEFAULT_CALLBACK_HANDLER_CLASS_NAME).set(operation.get(DEFAULT_CALLBACK_HANDLER_CLASS_NAME));
+
+        resultHandler.handleResultComplete(compensatingOperation);
+
+        return Cancellable.NULL;
+    }
+
+}
