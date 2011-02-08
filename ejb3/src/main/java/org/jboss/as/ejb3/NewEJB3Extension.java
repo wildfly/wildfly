b4ce60c20d09089dac01e9737fedb4c619c6bb59
Emanuel Muckenhuber <emuckenh@redhat.com>
2011-02-08 12:49:24 +0100

    add detyped ejb3 subsystem

---
+++ ejb3/src/main/java/org/jboss/as/ejb3/NewEJB3Extension.java
@@ -0,0 +1,96 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.ejb3;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+
+import java.util.List;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.jboss.as.controller.NewExtension;
+import org.jboss.as.controller.NewExtensionContext;
+import org.jboss.as.controller.SubsystemRegistration;
+import org.jboss.as.controller.parsing.ExtensionParsingContext;
+import org.jboss.as.controller.parsing.ParseUtils;
+import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
+import org.jboss.as.controller.registry.ModelNodeRegistration;
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementReader;
+import org.jboss.staxmapper.XMLElementWriter;
+import org.jboss.staxmapper.XMLExtendedStreamReader;
+import org.jboss.staxmapper.XMLExtendedStreamWriter;
+
+/**
+ * @author Emanuel Muckenhuber
+ */
+public class NewEJB3Extension implements NewExtension {
+
+    public static final String SUBSYSTEM_NAME = "ejb3";
+    public static final String NAMESPACE = "urn:jboss:domain:ejb3:1.0";
+
+    private static final EJB3SubsystemParser parser = new EJB3SubsystemParser();
+
+    /** {@inheritDoc} */
+    @Override
+    public void initialize(NewExtensionContext context) {
+        final SubsystemRegistration subsystem = context.registerSubsystem(SUBSYSTEM_NAME);
+        final ModelNodeRegistration registration = subsystem.registerSubsystemModel(NewEJB3SubsystemProviders.SUBSYSTEM);
+        registration.registerOperationHandler(ADD, NewEjb3SubsystemAdd.INSTANCE, NewEJB3SubsystemProviders.SUBSYSTEM_ADD, false);
+
+        subsystem.registerXMLElementWriter(parser);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void initializeParsers(ExtensionParsingContext context) {
+        context.setSubsystemXmlMapping(NAMESPACE, parser);
+    }
+
+    static class EJB3SubsystemParser implements XMLElementReader<List<ModelNode>>, XMLElementWriter<SubsystemMarshallingContext> {
+
+        /** {@inheritDoc} */
+        @Override
+        public void writeContent(final XMLExtendedStreamWriter writer, final SubsystemMarshallingContext context) throws XMLStreamException {
+            // //TODO seems to be a problem with empty elements cleaning up the queue in FormattingXMLStreamWriter.runAttrQueue
+            //context.startSubsystemElement(NewManagedBeansExtension.NAMESPACE, true);
+            context.startSubsystemElement(NAMESPACE, false);
+            writer.writeEndElement();
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void readElement(final XMLExtendedStreamReader reader, final List<ModelNode> list) throws XMLStreamException {
+            ParseUtils.requireNoAttributes(reader);
+            ParseUtils.requireNoContent(reader);
+            final ModelNode update = new ModelNode();
+            update.get(OP).set(ADD);
+            update.get(OP_ADDR).add(SUBSYSTEM, SUBSYSTEM_NAME);
+            list.add(update);
+        }
+    }
+
+}
