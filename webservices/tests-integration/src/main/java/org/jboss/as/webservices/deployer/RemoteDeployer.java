03097a982f7ef7fe043c25327086b289b9521f73
Richard Opalka <ropalka@redhat.com>
2011-02-08 11:01:04 +0100

    [JBWS-3207] Implemented AS7 remote deployment support. There's also test provided that is switched off by default. It have to be activated using -Dexecute.deployment.test=true JVM property when building webservices/tests-integration module. AS7 have to be running in the background to pass that test.
    Another problem found is in AS7 deployment process & JBoss Web integration.
    AS7 deployer client returns successful deployment event although
    installed web bundle isn't installed yet. This results in
    HTTP 500 codes when trying to access installed bundle.
    I implemented ugly hack to workaround it for now :(

---
+++ webservices/tests-integration/src/main/java/org/jboss/as/webservices/deployer/RemoteDeployer.java
@@ -0,0 +1,96 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.webservices.deployer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+import org.jboss.as.server.client.api.StandaloneClient;
+import org.jboss.as.server.client.api.deployment.DeploymentAction;
+import org.jboss.as.server.client.api.deployment.DeploymentPlan;
+import org.jboss.as.server.client.api.deployment.DeploymentPlanBuilder;
+import org.jboss.as.server.client.api.deployment.ServerDeploymentActionResult;
+import org.jboss.as.server.client.api.deployment.ServerDeploymentManager;
+import org.jboss.as.server.client.api.deployment.ServerDeploymentPlanResult;
+import org.jboss.logging.Logger;
+import org.jboss.wsf.spi.deployer.Deployer;
+
+/**
+ * Remote deployer that uses AS7 client deployment API.
+ *
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
+ */
+public final class RemoteDeployer implements Deployer {
+
+    private static final Logger LOGGER = Logger.getLogger(RemoteDeployer.class);
+    private final Map<URL, String> url2Id = new HashMap<URL, String>();
+
+    private ServerDeploymentManager deploymentManager;
+
+    public RemoteDeployer() throws IOException {
+        final InetAddress address = InetAddress.getByName("127.0.0.1");
+        final StandaloneClient client = StandaloneClient.Factory.create(address, 9999);
+        deploymentManager = client.getDeploymentManager();
+    }
+
+    @Override
+    public void deploy(final URL url) throws Exception {
+        final DeploymentPlanBuilder builder = deploymentManager.newDeploymentPlan().add(url).andDeploy();
+        final DeploymentPlan plan = builder.build();
+        final DeploymentAction deployAction = builder.getLastAction();
+        final String uniqueId = executeDeploymentPlan(plan, deployAction);
+        url2Id.put(url, uniqueId);
+        // For some reason AS7 server deployer isn't notified when web application is started.
+        // Thus we're getting successful deployment notification even if the web application isn't started yet :(
+        // Then when trying to access e.g. WSDL file, we're getting HTTP 500 (not found)
+        LOGGER.fatal("TODO: remove this ugly hack (Thread.sleep(3000)");
+        Thread.sleep(3000);
+    }
+
+    @Override
+    public void undeploy(final URL archive) throws Exception {
+        final DeploymentPlanBuilder builder = deploymentManager.newDeploymentPlan();
+        final String uniqueName = url2Id.get(archive);
+        final DeploymentPlan plan = builder.undeploy(uniqueName).remove(uniqueName).build();
+        final Future<ServerDeploymentPlanResult> future = deploymentManager.execute(plan);
+        url2Id.remove(archive);
+        future.get();
+    }
+
+    private String executeDeploymentPlan(final DeploymentPlan plan, final DeploymentAction deployAction) throws Exception {
+        final ServerDeploymentPlanResult planResult = deploymentManager.execute(plan).get();
+
+        final ServerDeploymentActionResult actionResult = planResult.getDeploymentActionResult(deployAction.getId());
+        if (actionResult != null) {
+            final Exception deploymentException = (Exception) actionResult.getDeploymentException();
+            if (deploymentException != null)
+                throw deploymentException;
+        }
+
+        return deployAction.getDeploymentUnitUniqueName();
+    }
+
+}
---
+++ webservices/tests-integration/src/test/java/org/jboss/test/ws/tools/jbws3207/JBWS3207TestCase.java
@@ -0,0 +1,122 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.tools.jbws3207;
+
+import static org.jboss.as.protocol.StreamUtils.safeClose;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.jboss.wsf.spi.SPIProvider;
+import org.jboss.wsf.spi.SPIProviderResolver;
+import org.jboss.wsf.spi.deployer.Deployer;
+import org.junit.Test;
+
+/**
+ * 
+ * @author <a href="alessio.soldano@jboss.com">Alessio Soldano</a>
+ * @author <a href="ropalka@redhat.com">Richard Opalka</a>
+ */
+public class JBWS3207TestCase {
+
+    /**
+     * This JVM property needs to be activated from commandline.
+     * Note that AS7 server have to be running.
+     * Default configured value is false to allow AS7 builds.
+     */
+    private static final boolean EXECUTE_TEST = Boolean.getBoolean("execute.deployment.test");
+
+    @Test
+    public void testRemoteDeployer() throws Exception {
+        if (EXECUTE_TEST) {
+            SPIProvider spiProvider = SPIProviderResolver.getInstance().getProvider();
+            Deployer deployer = spiProvider.getSPI(Deployer.class);
+            File archiveFile = new File(System.getProperty("test.archive.directory"), "ws-example.war");
+            URL archiveURL = archiveFile.toURI().toURL();
+            try {
+                deployer.deploy(archiveURL);
+                testWSDL();
+                testSOAPCall();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                deployer.undeploy(archiveURL);
+            }
+        }
+    }
+
+    private static void testWSDL() throws Exception {
+        URLConnection conn = null;
+        InputStream in = null;
+        try {
+            URL url = new URL("http://localhost:8080/ws-example/?wsdl");
+            System.out.println("Reading response from " + url + ":");
+            conn = url.openConnection();
+            conn.setDoInput(true);
+            in = new BufferedInputStream(conn.getInputStream());
+            int i = in.read();
+            while (i != -1) {
+                System.out.print((char) i);
+                i = in.read();
+            }
+            System.out.println("");
+        } finally {
+            safeClose(in);
+        }
+    }
+
+    private static void testSOAPCall() throws Exception {
+        URLConnection conn = null;
+        InputStream in = null;
+        OutputStreamWriter osw = null;
+        final String message = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:arc=\"http://service.jbws3207.tools.ws.test.jboss.org/\">"
+                + "  <soapenv:Header/>"
+                + "  <soapenv:Body>"
+                + "    <arc:echoString>"
+                + "      <arg0>Foo</arg0>"
+                + "    </arc:echoString>" + "  </soapenv:Body>" + "</soapenv:Envelope>";
+        try {
+            URL url = new URL("http://localhost:8080/ws-example");
+            System.out.println("Reading response from " + url + ":");
+            conn = url.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            osw = new OutputStreamWriter(conn.getOutputStream());
+            osw.write(message);
+            osw.flush();
+            in = new BufferedInputStream(conn.getInputStream());
+            int i = in.read();
+            while (i != -1) {
+                System.out.print((char) i);
+                i = in.read();
+            }
+            System.out.println("");
+        } finally {
+            safeClose(osw);
+            safeClose(in);
+        }
+    }
+}
