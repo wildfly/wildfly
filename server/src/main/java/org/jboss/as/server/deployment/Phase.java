wolfc/jboss-as

Add DEPENDENCIES_EJB phase

server/src/main/java/org/jboss/as/server/deployment/Phase.java
@@ -234,6 +234,9 @@ public enum Phase {
     public static final int DEPENDENCIES_WELD                           = 0xA00;
     public static final int DEPENDENCIES_NAMING                         = 0xB00;
     public static final int DEPENDENCIES_WS                             = 0xC00;
+    // Sets up appropriate module dependencies for EJB deployments
+    public static final int DEPENDENCIES_EJB                            = 0xD00;
+
 
     // CONFIGURE_MODULE
     public static final int CONFIGURE_MODULE_WAR                        = 0x100;

wildfly/wildfly

server/src/main/java/org/jboss/as/server/deployment/Phase.java
@@ -239,6 +239,8 @@ public enum Phase {
     public static final int DEPENDENCIES_WS                             = 0xC00;
     public static final int DEPENDENCIES_JAXRS                          = 0xD00;
     public static final int DEPENDENCIES_SUB_DEPLOYMENTS                = 0xE00;
+    // Sets up appropriate module dependencies for EJB deployments
+    public static final int DEPENDENCIES_EJB                            = 0xF00;
 
     // CONFIGURE_MODULE
     public static final int CONFIGURE_MODULE_SPEC                       = 0x100;