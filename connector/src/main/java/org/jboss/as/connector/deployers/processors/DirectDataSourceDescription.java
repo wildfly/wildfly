946536bb144d522358c00ed166e4175bbb7bd3bc
Jason T. Greene <jason.greene@redhat.com>
2011-03-09 13:38:58 -0600

    Add missing comments / lic header

--- connector/src/main/java/org/jboss/as/connector/deployers/processors/DirectDataSourceDescription.java
+++ connector/src/main/java/org/jboss/as/connector/deployers/processors/DirectDataSourceDescription.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.as.connector.deployers.processors;
 
 import org.jboss.as.ee.component.AbstractComponentDescription;
@@ -13,7 +34,14 @@ import org.jboss.msc.inject.Injector;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.value.Values;
 
-
+/**
+ * A binding description for DataSourceDefinition annotations.
+ *
+ * The referenced datasource must be directly visible to the
+ * component declaring the annotation.
+ *
+ * @author Jason T. Greene
+ */
 public class DirectDataSourceDescription extends BindingSourceDescription {
     public static final String USER_PROP = "user";
     public static final String URL_PROP = "url";
@@ -117,7 +145,6 @@ public class DirectDataSourceDescription extends BindingSourceDescription {
         } catch (Exception ignore) {
         }
     }
-
 
     public String getClassName() {
         return className;
