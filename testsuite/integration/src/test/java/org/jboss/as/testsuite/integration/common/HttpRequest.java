fe5c0e36f7000476d39cc7b7d851960e43c4b22f
Carlo de Wolf <cdewolf@redhat.com>
2011-03-22 14:43:41 +0100

    Add HttpRequest for easier testing

---
+++ testsuite/integration/src/test/java/org/jboss/as/testsuite/integration/common/HttpRequest.java
@@ -0,0 +1,87 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.testsuite.integration.common;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
+ */
+public class HttpRequest implements Callable<String> {
+    private URL url;
+
+    public HttpRequest(String spec) throws MalformedURLException {
+        this.url = new URL(spec);
+    }
+
+    @Override
+    public String call() throws Exception {
+        URLConnection conn = url.openConnection();
+        InputStream in = conn.getInputStream();
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            int b;
+            while((b = in.read()) != -1) {
+                out.write(b);
+            }
+            return out.toString();
+        }
+        finally {
+            in.close();
+        }
+    }
+
+    public static String get(String spec, long timeout, TimeUnit unit) throws MalformedURLException, ExecutionException, TimeoutException {
+        Callable<String> task = new HttpRequest(spec);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<String> result = executor.submit(task);
+        try {
+            return result.get(timeout, unit);
+        } catch (TimeoutException e) {
+            result.cancel(true);
+            throw e;
+        } catch (InterruptedException e) {
+            // should not happen
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw e;
+        } finally {
+            executor.shutdownNow();
+            try {
+                executor.awaitTermination(timeout, unit);
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
+    }
+}
