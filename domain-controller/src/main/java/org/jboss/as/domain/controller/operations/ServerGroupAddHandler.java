4ca9e1513f2b0d1f04120e705ae58d600dda240b
Emanuel Muckenhuber <emuckenh@redhat.com>
2011-04-11 14:26:16 +0200

    JBAS-9287

--- domain-controller/src/main/java/org/jboss/as/domain/controller/operations/ServerGroupAddHandler.java
+++ domain-controller/src/main/java/org/jboss/as/domain/controller/operations/ServerGroupAddHandler.java
@@ -22,11 +22,6 @@
 
 package org.jboss.as.domain.controller.operations;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DEPLOYMENT;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.JVM;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.PROFILE;
-
 import java.util.Locale;
 
 import org.jboss.as.controller.BasicOperationResult;
@@ -38,6 +33,10 @@ import org.jboss.as.controller.descriptions.DescriptionProvider;
 import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.domain.controller.descriptions.ServerGroupDescription;
 import org.jboss.dmr.ModelNode;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SOCKET_BINDING_GROUP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SOCKET_BINDING_PORT_OFFSET;
 
 /**
  * @author Emanuel Muckenhuber
@@ -53,6 +52,13 @@ public class ServerGroupAddHandler implements ModelAddOperationHandler, Descript
         subModel.get(PROFILE).set(operation.require(PROFILE));
         subModel.get(JVM).setEmptyObject();
         subModel.get(DEPLOYMENT).setEmptyObject();
+
+        if(operation.hasDefined(SOCKET_BINDING_GROUP)) {
+            subModel.get(SOCKET_BINDING_GROUP).set(operation.get(SOCKET_BINDING_GROUP));
+        }
+        if(operation.hasDefined(SOCKET_BINDING_PORT_OFFSET)) {
+            subModel.get(SOCKET_BINDING_PORT_OFFSET).set(operation.get(SOCKET_BINDING_PORT_OFFSET));
+        }
 
         final ModelNode compensatingOperation = Util.getResourceRemoveOperation(operation.get(OP_ADDR));
 
