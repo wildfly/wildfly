398e9aeb1968bf0dcd12865458520eb2bb83cf2b
Richard Opalka <ropalka@redhat.com>
2011-04-14 21:10:03 +0200

    [JBWS-3222] providing JBossWS command line tools + minor EJB integration fixes

---
+++ build/src/main/resources/bin/wsconsume.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# Extract the directory and the program name
+# takes care of symlinks
+PRG="$0"
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG="`dirname "$PRG"`/$link"
+  fi
+done
+DIRNAME=`dirname "$PRG"`
+PROGNAME=`basename "$PRG"`
+
+# OS specific support (must be 'true' or 'false').
+cygwin=false;
+case "`uname`" in
+    CYGWIN*)
+        cygwin=true
+        ;;
+esac
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+    [ -n "$JBOSS_HOME" ] &&
+        JBOSS_HOME=`cygpath --unix "$JBOSS_HOME"`
+    [ -n "$JAVA_HOME" ] &&
+        JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+fi
+
+# Setup JBOSS_HOME
+if [ "x$JBOSS_HOME" = "x" ]; then
+    # get the full path (without any relative bits)
+    JBOSS_HOME=`cd $DIRNAME/..; pwd`
+fi
+export JBOSS_HOME
+
+# Setup the JVM
+if [ "x$JAVA" = "x" ]; then
+    if [ "x$JAVA_HOME" != "x" ]; then
+        JAVA="$JAVA_HOME/bin/java"
+    else
+        JAVA="java"
+    fi
+fi
+
+#JPDA options. Uncomment and modify as appropriate to enable remote debugging .
+#JAVA_OPTS="-classic -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y $JAVA_OPTS"
+
+# Setup JBoss sepecific properties
+JAVA_OPTS="$JAVA_OPTS"
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+    JBOSS_HOME=`cygpath --path --windows "$JBOSS_HOME"`
+    JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+fi
+
+# Execute the command
+eval \"$JAVA\" $JAVA_OPTS \
+    \"-Djava.endorsed.dirs=$JBOSS_HOME/modules/com/sun/xml/bin/main:$JBOSS_HOME/modules/javax/xml/ws/api/main\" \
+    -classpath \"$JAVA_HOME/lib/tools.jar:$JBOSS_HOME/jboss-modules.jar\" \
+    org.jboss.modules.Main \
+    -mp \"$JBOSS_HOME/modules\" \
+    -logmodule "org.jboss.logmanager" \
+    org.jboss.ws.tools.wsconsume \
+    "$@"
---
+++ build/src/main/resources/bin/wsprovide.bat
@@ -0,0 +1,27 @@
+@echo off
+
+@if not "%ECHO%" == ""  echo %ECHO%
+@if "%OS%" == "Windows_NT"  setlocal
+
+set DIRNAME=.\
+if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
+set PROGNAME=run.bat
+if "%OS%" == "Windows_NT" set PROGNAME=%~nx0%
+
+if "x%JAVA_HOME%" == "x" (
+  set  JAVA=java
+  echo JAVA_HOME is not set. Unexpected results may occur.
+  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  set "JAVA=%JAVA_HOME%\bin\java"
+)
+set JBOSS_HOME=%DIRNAME%\..
+
+rem Execute the command
+"%JAVA%" %JAVA_OPTS% ^
+    "-Djava.endorsed.dirs=file:%JBOSS_HOME%/modules/com/sun/xml/bin/main;file:%JBOSS_HOME%/modules/javax/xml/ws/api/main\" ^
+    -jar "%JBOSS_HOME%\jboss-modules.jar" ^
+    -mp "%JBOSS_HOME%\modules" ^
+    -logmodule "org.jboss.logmanager" ^
+    org.jboss.ws.tools.wsprovide ^
+    %*
