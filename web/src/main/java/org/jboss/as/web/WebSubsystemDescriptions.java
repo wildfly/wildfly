339afe7b6fc56651e6a6b39109e2d8ed391db384
Rémy Maucherat <rmaucher@redhat.com>
2011-05-11 21:37:27 +0200

    Improve descriptions.

--- web/src/main/java/org/jboss/as/web/WebSubsystemDescriptions.java
+++ web/src/main/java/org/jboss/as/web/WebSubsystemDescriptions.java
@@ -52,16 +52,18 @@ class WebSubsystemDescriptions {
         node.get(ATTRIBUTES, Constants.DEFAULT_VIRTUAL_SERVER, TYPE).set(ModelType.STRING);
         node.get(ATTRIBUTES, Constants.DEFAULT_VIRTUAL_SERVER, DESCRIPTION).set(bundle.getString("web.default-virtual-server"));
         node.get(ATTRIBUTES, Constants.DEFAULT_VIRTUAL_SERVER, REQUIRED).set(false);
+        node.get(ATTRIBUTES, Constants.DEFAULT_VIRTUAL_SERVER, DEFAULT).set("localhost");
 
         node.get(ATTRIBUTES, Constants.NATIVE, TYPE).set(ModelType.BOOLEAN);
         node.get(ATTRIBUTES, Constants.NATIVE, DESCRIPTION).set(bundle.getString("web.native"));
         node.get(ATTRIBUTES, Constants.NATIVE, REQUIRED).set(false);
+        node.get(ATTRIBUTES, Constants.NATIVE, DEFAULT).set(true);
 
         node.get(ATTRIBUTES, Constants.CONNECTOR, TYPE).set(ModelType.LIST);
         node.get(ATTRIBUTES, Constants.CONNECTOR, DESCRIPTION).set(bundle.getString("web.connector"));
         node.get(ATTRIBUTES, Constants.CONNECTOR, MAX_OCCURS).set(Integer.MAX_VALUE);
 
-        node.get(ATTRIBUTES, Constants.CONNECTOR, Constants.NAME, TYPE).set(ModelType.LIST);
+        node.get(ATTRIBUTES, Constants.CONNECTOR, Constants.NAME, TYPE).set(ModelType.STRING);
         node.get(ATTRIBUTES, Constants.CONNECTOR, Constants.NAME, DESCRIPTION).set(bundle.getString("web.connector.name"));
         node.get(ATTRIBUTES, Constants.CONNECTOR, Constants.NAME, REQUIRED).set(true);
         node.get(ATTRIBUTES, Constants.CONNECTOR, Constants.NAME, NILLABLE).set(false);
@@ -92,10 +94,12 @@ class WebSubsystemDescriptions {
         node.get(REQUEST_PROPERTIES, Constants.DEFAULT_VIRTUAL_SERVER, TYPE).set(ModelType.STRING);
         node.get(REQUEST_PROPERTIES, Constants.DEFAULT_VIRTUAL_SERVER, DESCRIPTION).set(bundle.getString("web.default-virtual-server"));
         node.get(REQUEST_PROPERTIES, Constants.DEFAULT_VIRTUAL_SERVER, REQUIRED).set(false);
+        node.get(REQUEST_PROPERTIES, Constants.DEFAULT_VIRTUAL_SERVER, DEFAULT).set("localhost");
 
         node.get(REQUEST_PROPERTIES, Constants.NATIVE, TYPE).set(ModelType.BOOLEAN);
         node.get(REQUEST_PROPERTIES, Constants.NATIVE, DESCRIPTION).set(bundle.getString("web.native"));
         node.get(REQUEST_PROPERTIES, Constants.NATIVE, REQUIRED).set(false);
+        node.get(REQUEST_PROPERTIES, Constants.NATIVE, DEFAULT).set(true);
 
         getConfigurationCommonDescription(node.get(REQUEST_PROPERTIES, Constants.CONTAINER_CONFIG), REQUEST_PROPERTIES, bundle);
 
@@ -159,37 +163,43 @@ class WebSubsystemDescriptions {
         node.get(DESCRIPTION).set(bundle.getString("web.configuration.static"));
         node.get(REQUIRED).set(false);
 
-        node.get(type, Constants.LISTINGS, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.LISTINGS, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.LISTINGS, DESCRIPTION).set(bundle.getString("web.configuration.static.listings"));
         node.get(type, Constants.LISTINGS, REQUIRED).set(false);
+        node.get(type, Constants.LISTINGS, DEFAULT).set(false);
 
-        node.get(type, Constants.SENDFILE, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.SENDFILE, TYPE).set(ModelType.INT);
         node.get(type, Constants.SENDFILE, DESCRIPTION).set(bundle.getString("web.configuration.static.sendfile"));
         node.get(type, Constants.SENDFILE, REQUIRED).set(false);
+        node.get(type, Constants.SENDFILE, DEFAULT).set(49152);
 
         node.get(type, Constants.FILE_ENCONDING, TYPE).set(ModelType.STRING);
         node.get(type, Constants.FILE_ENCONDING, DESCRIPTION).set(bundle.getString("web.configuration.static.file-encoding"));
         node.get(type, Constants.FILE_ENCONDING, REQUIRED).set(false);
 
-        node.get(type, Constants.READ_ONLY, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.READ_ONLY, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.READ_ONLY, DESCRIPTION).set(bundle.getString("web.configuration.static.read-only"));
         node.get(type, Constants.READ_ONLY, REQUIRED).set(false);
+        node.get(type, Constants.READ_ONLY, DEFAULT).set(true);
 
-        node.get(type, Constants.WEBDAV, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.WEBDAV, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.WEBDAV, DESCRIPTION).set(bundle.getString("web.configuration.static.webdav"));
         node.get(type, Constants.WEBDAV, REQUIRED).set(false);
+        node.get(type, Constants.WEBDAV, DEFAULT).set(false);
 
         node.get(type, Constants.SECRET, TYPE).set(ModelType.STRING);
         node.get(type, Constants.SECRET, DESCRIPTION).set(bundle.getString("web.configuration.static.secret"));
         node.get(type, Constants.SECRET, REQUIRED).set(false);
 
-        node.get(type, Constants.MAX_DEPTH, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.MAX_DEPTH, TYPE).set(ModelType.INT);
         node.get(type, Constants.MAX_DEPTH, DESCRIPTION).set(bundle.getString("web.configuration.static.max-depth"));
         node.get(type, Constants.MAX_DEPTH, REQUIRED).set(false);
+        node.get(type, Constants.MAX_DEPTH, DEFAULT).set(3);
 
-        node.get(type, Constants.DISABLED, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.DISABLED, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.DISABLED, DESCRIPTION).set(bundle.getString("web.configuration.static.disabled"));
         node.get(type, Constants.DISABLED, REQUIRED).set(false);
+        node.get(type, Constants.DISABLED, DEFAULT).set(false);
 
         return node;
     }
@@ -200,57 +210,70 @@ class WebSubsystemDescriptions {
         node.get(DESCRIPTION).set(bundle.getString("web.configuration.jsp"));
         node.get(REQUIRED).set(false);
 
-        node.get(type, Constants.DEVELOPMENT, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.DEVELOPMENT, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.DEVELOPMENT, DESCRIPTION).set(bundle.getString("web.configuration.jsp.development"));
         node.get(type, Constants.DEVELOPMENT, REQUIRED).set(false);
+        node.get(type, Constants.DEVELOPMENT, DEFAULT).set(false);
 
-        node.get(type, Constants.DISABLED, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.DISABLED, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.DISABLED, DESCRIPTION).set(bundle.getString("web.configuration.jsp.disabled"));
         node.get(type, Constants.DISABLED, REQUIRED).set(false);
+        node.get(type, Constants.DISABLED, DEFAULT).set(false);
 
-        node.get(type, Constants.KEEP_GENERATED, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.KEEP_GENERATED, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.KEEP_GENERATED, DESCRIPTION).set(bundle.getString("web.configuration.jsp.keep-generated"));
         node.get(type, Constants.KEEP_GENERATED, REQUIRED).set(false);
+        node.get(type, Constants.KEEP_GENERATED, DEFAULT).set(true);
 
-        node.get(type, Constants.TRIM_SPACES, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.TRIM_SPACES, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.TRIM_SPACES, DESCRIPTION).set(bundle.getString("web.configuration.jsp.trim-spaces"));
         node.get(type, Constants.TRIM_SPACES, REQUIRED).set(false);
+        node.get(type, Constants.TRIM_SPACES, DEFAULT).set(true);
 
-        node.get(type, Constants.TAG_POOLING, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.TAG_POOLING, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.TAG_POOLING, DESCRIPTION).set(bundle.getString("web.configuration.jsp.tag-pooling"));
         node.get(type, Constants.TAG_POOLING, REQUIRED).set(false);
+        node.get(type, Constants.TAG_POOLING, DEFAULT).set(true);
 
-        node.get(type, Constants.MAPPED_FILE, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.MAPPED_FILE, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.MAPPED_FILE, DESCRIPTION).set(bundle.getString("web.configuration.jsp.mapped-file"));
         node.get(type, Constants.MAPPED_FILE, REQUIRED).set(false);
+        node.get(type, Constants.MAPPED_FILE, DEFAULT).set(true);
 
-        node.get(type, Constants.CHECK_INTERVAL, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.CHECK_INTERVAL, TYPE).set(ModelType.INT);
         node.get(type, Constants.CHECK_INTERVAL, DESCRIPTION).set(bundle.getString("web.configuration.jsp.check-interval"));
         node.get(type, Constants.CHECK_INTERVAL, REQUIRED).set(false);
+        node.get(type, Constants.CHECK_INTERVAL, DEFAULT).set(0);
 
-        node.get(type, Constants.MODIFIFICATION_TEST_INTERVAL, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.MODIFIFICATION_TEST_INTERVAL, TYPE).set(ModelType.INT);
         node.get(type, Constants.MODIFIFICATION_TEST_INTERVAL, DESCRIPTION).set(bundle.getString("web.configuration.jsp.modification-test-interval"));
         node.get(type, Constants.MODIFIFICATION_TEST_INTERVAL, REQUIRED).set(false);
+        node.get(type, Constants.MODIFIFICATION_TEST_INTERVAL, DEFAULT).set(4);
 
-        node.get(type, Constants.RECOMPILE_ON_FAIL, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.RECOMPILE_ON_FAIL, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.RECOMPILE_ON_FAIL, DESCRIPTION).set(bundle.getString("web.configuration.jsp.recompile-on-fail"));
         node.get(type, Constants.RECOMPILE_ON_FAIL, REQUIRED).set(false);
+        node.get(type, Constants.RECOMPILE_ON_FAIL, DEFAULT).set(false);
 
-        node.get(type, Constants.SMAP, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.SMAP, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.SMAP, DESCRIPTION).set(bundle.getString("web.configuration.jsp.smap"));
         node.get(type, Constants.SMAP, REQUIRED).set(false);
+        node.get(type, Constants.SMAP, DEFAULT).set(false);
 
-        node.get(type, Constants.DUMP_SMAP, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.DUMP_SMAP, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.DUMP_SMAP, DESCRIPTION).set(bundle.getString("web.configuration.jsp.dump-smap"));
         node.get(type, Constants.DUMP_SMAP, REQUIRED).set(false);
+        node.get(type, Constants.DUMP_SMAP, DEFAULT).set(false);
 
-        node.get(type, Constants.GENERATE_STRINGS_AS_CHAR_ARRAYS, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.GENERATE_STRINGS_AS_CHAR_ARRAYS, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.GENERATE_STRINGS_AS_CHAR_ARRAYS, DESCRIPTION).set(bundle.getString("web.configuration.jsp.generate-strings-as-char-arrays"));
         node.get(type, Constants.GENERATE_STRINGS_AS_CHAR_ARRAYS, REQUIRED).set(false);
+        node.get(type, Constants.GENERATE_STRINGS_AS_CHAR_ARRAYS, DEFAULT).set(false);
 
-        node.get(type, Constants.ERROR_ON_USE_BEAN_INVALID_CLASS_ATTRIBUTE, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.ERROR_ON_USE_BEAN_INVALID_CLASS_ATTRIBUTE, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.ERROR_ON_USE_BEAN_INVALID_CLASS_ATTRIBUTE, DESCRIPTION).set(bundle.getString("web.configuration.jsp.error-on-use-bean-invalid-class-attribute"));
         node.get(type, Constants.ERROR_ON_USE_BEAN_INVALID_CLASS_ATTRIBUTE, REQUIRED).set(false);
+        node.get(type, Constants.ERROR_ON_USE_BEAN_INVALID_CLASS_ATTRIBUTE, DEFAULT).set(false);
 
         node.get(type, Constants.SCRATCH_DIR, TYPE).set(ModelType.STRING);
         node.get(type, Constants.SCRATCH_DIR, DESCRIPTION).set(bundle.getString("web.configuration.jsp.scratch-dir"));
@@ -259,22 +282,27 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.SOURCE_VM, TYPE).set(ModelType.STRING);
         node.get(type, Constants.SOURCE_VM, DESCRIPTION).set(bundle.getString("web.configuration.jsp.source-vm"));
         node.get(type, Constants.SOURCE_VM, REQUIRED).set(false);
+        node.get(type, Constants.SOURCE_VM, DEFAULT).set("1.5");
 
         node.get(type, Constants.TARGET_VM, TYPE).set(ModelType.STRING);
         node.get(type, Constants.TARGET_VM, DESCRIPTION).set(bundle.getString("web.configuration.jsp.target-vm"));
         node.get(type, Constants.TARGET_VM, REQUIRED).set(false);
+        node.get(type, Constants.TARGET_VM, DEFAULT).set("1.5");
 
         node.get(type, Constants.JAVA_ENCODING, TYPE).set(ModelType.STRING);
         node.get(type, Constants.JAVA_ENCODING, DESCRIPTION).set(bundle.getString("web.configuration.jsp.java-encoding"));
         node.get(type, Constants.JAVA_ENCODING, REQUIRED).set(false);
+        node.get(type, Constants.JAVA_ENCODING, DEFAULT).set("UTF-8");
 
-        node.get(type, Constants.X_POWERED_BY, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.X_POWERED_BY, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.X_POWERED_BY, DESCRIPTION).set(bundle.getString("web.configuration.jsp.x-powered-by"));
         node.get(type, Constants.X_POWERED_BY, REQUIRED).set(false);
+        node.get(type, Constants.X_POWERED_BY, DEFAULT).set(true);
 
-        node.get(type, Constants.DISPLAY_SOURCE_FRAGMENT, TYPE).set(ModelType.STRING);
+        node.get(type, Constants.DISPLAY_SOURCE_FRAGMENT, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.DISPLAY_SOURCE_FRAGMENT, DESCRIPTION).set(bundle.getString("web.configuration.jsp.display-source-fragment"));
         node.get(type, Constants.DISPLAY_SOURCE_FRAGMENT, REQUIRED).set(false);
+        node.get(type, Constants.DISPLAY_SOURCE_FRAGMENT, DEFAULT).set(true);
 
         return node;
     }
@@ -293,6 +321,7 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.SCHEME, TYPE).set(ModelType.STRING);
         node.get(type, Constants.SCHEME, DESCRIPTION).set(bundle.getString("web.connector.scheme"));
         node.get(type, Constants.SCHEME, REQUIRED).set(false);
+        node.get(type, Constants.SCHEME, DEFAULT).set("http");
 
         node.get(type, Constants.EXECUTOR, TYPE).set(ModelType.STRING);
         node.get(type, Constants.EXECUTOR, DESCRIPTION).set(bundle.getString("web.connector.executor"));
@@ -301,10 +330,12 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.ENABLED, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.ENABLED, DESCRIPTION).set(bundle.getString("web.connector.enabled"));
         node.get(type, Constants.ENABLED, REQUIRED).set(false);
+        node.get(type, Constants.ENABLED, DEFAULT).set(true);
 
         node.get(type, Constants.ENABLE_LOOKUPS, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.ENABLE_LOOKUPS, DESCRIPTION).set(bundle.getString("web.connector.enable-lookups"));
         node.get(type, Constants.ENABLE_LOOKUPS, REQUIRED).set(false);
+        node.get(type, Constants.ENABLE_LOOKUPS, DEFAULT).set(false);
 
         node.get(type, Constants.PROXY_NAME, TYPE).set(ModelType.STRING);
         node.get(type, Constants.PROXY_NAME, DESCRIPTION).set(bundle.getString("web.connector.proxy-name"));
@@ -317,18 +348,22 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.MAX_POST_SIZE, TYPE).set(ModelType.INT);
         node.get(type, Constants.MAX_POST_SIZE, DESCRIPTION).set(bundle.getString("web.connector.max-post-size"));
         node.get(type, Constants.MAX_POST_SIZE, REQUIRED).set(false);
+        node.get(type, Constants.MAX_POST_SIZE, DEFAULT).set(2097152);
 
         node.get(type, Constants.MAX_SAVE_POST_SIZE, TYPE).set(ModelType.INT);
         node.get(type, Constants.MAX_SAVE_POST_SIZE, DESCRIPTION).set(bundle.getString("web.connector.max-save-post-size"));
         node.get(type, Constants.MAX_SAVE_POST_SIZE, REQUIRED).set(false);
+        node.get(type, Constants.MAX_SAVE_POST_SIZE, DEFAULT).set(4096);
 
         node.get(type, Constants.SECURE, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.SECURE, DESCRIPTION).set(bundle.getString("web.connector.secure"));
         node.get(type, Constants.SECURE, REQUIRED).set(false);
+        node.get(type, Constants.SECURE, DEFAULT).set(false);
 
         node.get(type, Constants.REDIRECT_PORT, TYPE).set(ModelType.INT);
         node.get(type, Constants.REDIRECT_PORT, DESCRIPTION).set(bundle.getString("web.connector.redirect-port"));
         node.get(type, Constants.REDIRECT_PORT, REQUIRED).set(false);
+        node.get(type, Constants.REDIRECT_PORT, DEFAULT).set(8443);
 
         node.get(type, Constants.MAX_CONNECTIONS, TYPE).set(ModelType.INT);
         node.get(type, Constants.MAX_CONNECTIONS, DESCRIPTION).set(bundle.getString("web.connector.max-connections"));
@@ -454,9 +489,10 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.ALIAS, REQUIRED).set(false);
         node.get(type, Constants.ALIAS, NILLABLE).set(true);
 
-        node.get(type, Constants.DEFAULT_WEB_MODULE, TYPE).set(ModelType.LIST);
+        node.get(type, Constants.DEFAULT_WEB_MODULE, TYPE).set(ModelType.STRING);
         node.get(type, Constants.DEFAULT_WEB_MODULE, DESCRIPTION).set(bundle.getString("web.virtual-server.default-web-module"));
         node.get(type, Constants.DEFAULT_WEB_MODULE, REQUIRED).set(false);
+        node.get(type, Constants.DEFAULT_WEB_MODULE, DEFAULT).set("ROOT.war");
 
         getAccessLogCommonDescription(node.get(type, Constants.ACCESS_LOG), type, bundle);
         getRewriteCommonDescription(node.get(type, Constants.REWRITE), type, bundle);
@@ -472,14 +508,17 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.PATTERN, TYPE).set(ModelType.STRING);
         node.get(type, Constants.PATTERN, DESCRIPTION).set(bundle.getString("web.virtual-server.access-log.pattern"));
         node.get(type, Constants.PATTERN, REQUIRED).set(false);
+        node.get(type, Constants.PATTERN, DEFAULT).set("common");
 
         node.get(type, Constants.RESOLVE_HOSTS, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.RESOLVE_HOSTS, DESCRIPTION).set(bundle.getString("web.virtual-server.access-log.resolve-hosts"));
         node.get(type, Constants.RESOLVE_HOSTS, REQUIRED).set(false);
+        node.get(type, Constants.RESOLVE_HOSTS, DEFAULT).set(false);
 
         node.get(type, Constants.EXTENDED, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.EXTENDED, DESCRIPTION).set(bundle.getString("web.virtual-server.access-log.extended"));
         node.get(type, Constants.EXTENDED, REQUIRED).set(false);
+        node.get(type, Constants.EXTENDED, DEFAULT).set(false);
 
         node.get(type, Constants.PREFIX, TYPE).set(ModelType.STRING);
         node.get(type, Constants.PREFIX, DESCRIPTION).set(bundle.getString("web.virtual-server.access-log.prefix"));
@@ -488,6 +527,7 @@ class WebSubsystemDescriptions {
         node.get(type, Constants.ROTATE, TYPE).set(ModelType.BOOLEAN);
         node.get(type, Constants.ROTATE, DESCRIPTION).set(bundle.getString("web.virtual-server.access-log.rotate"));
         node.get(type, Constants.ROTATE, REQUIRED).set(false);
+        node.get(type, Constants.ROTATE, DEFAULT).set(true);
 
         node.get(type, Constants.DIRECTORY, TYPE).set(ModelType.OBJECT);
         node.get(type, Constants.DIRECTORY, DESCRIPTION).set(bundle.getString("web.virtual-server.access-log.directory"));
