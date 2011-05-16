luksa/jboss-as

More changes to migrate EJB3 component to new EE framework (WIP)

ejb3/src/main/java/org/jboss/as/ejb3/component/messagedriven/MessageDrivenComponentDescription.java
@@ -21,14 +21,11 @@
  */
 package org.jboss.as.ejb3.component.messagedriven;
 
-import org.jboss.as.ee.component.ComponentConfiguration;
 import org.jboss.as.ee.component.EEModuleDescription;
+import org.jboss.as.ee.component.ViewDescription;
 import org.jboss.as.ejb3.component.EJBComponentDescription;
 import org.jboss.as.ejb3.component.MethodIntf;
-import org.jboss.as.server.deployment.DeploymentPhaseContext;
-import org.jboss.as.server.deployment.DeploymentUnit;
-import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
-import org.jboss.modules.Module;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
@@ -42,10 +39,11 @@ public class MessageDrivenComponentDescription extends EJBComponentDescription {
      *
      * @param componentName      the component name
      * @param componentClassName the component instance class name
-     * @param moduleDescription the module description
+     * @param moduleDescription  the module description
      */
-    public MessageDrivenComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription) {
-        super(componentName, componentClassName, moduleDescription);
+    public MessageDrivenComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription,
+                                             final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, moduleDescription, deploymentUnitServiceName);
     }
 
     @Override
@@ -78,10 +76,15 @@ public class MessageDrivenComponentDescription extends EJBComponentDescription {
 //        }
 //    }
 
-//    public void setMessageListenerInterfaceName(String messageListenerInterfaceName) {
-//        getViewClassNames().add(messageListenerInterfaceName);
-//        this.messageListenerInterfaceName = messageListenerInterfaceName;
-//    }
+    public void setMessageListenerInterfaceName(String messageListenerInterfaceName) {
+        if (messageListenerInterfaceName == null || messageListenerInterfaceName.isEmpty()) {
+            throw new IllegalArgumentException("Cannot set null or empty string as message listener interface");
+        }
+        this.messageListenerInterfaceName = messageListenerInterfaceName;
+        // add it to the view description
+        ViewDescription viewDescription = new ViewDescription(this, messageListenerInterfaceName);
+        this.getViews().add(viewDescription);
+    }
 
     public void setResourceAdapterName(String resourceAdapterName) {
         this.resourceAdapterName = resourceAdapterName;

ejb3/src/main/java/org/jboss/as/ejb3/component/singleton/SingletonComponentDescription.java
@@ -22,9 +22,9 @@
 
 package org.jboss.as.ejb3.component.singleton;
 
-import org.jboss.as.ee.component.ComponentConfiguration;
 import org.jboss.as.ee.component.EEModuleDescription;
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * Component description for a singleton bean
@@ -45,13 +45,9 @@ public class SingletonComponentDescription extends SessionBeanComponentDescripti
      * @param componentClassName the component instance class name
      * @param moduleDescription  the module description
      */
-    public SingletonComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription) {
-        super(componentName, componentClassName, moduleDescription);
-    }
-
-    @Override
-    protected ComponentConfiguration constructComponentConfiguration() {
-        return new SingletonComponentConfiguration(this);
+    public SingletonComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription,
+                                         final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, moduleDescription, deploymentUnitServiceName);
     }
 
     /**

ejb3/src/main/java/org/jboss/as/ejb3/component/stateful/StatefulComponentDescription.java
@@ -22,11 +22,9 @@
 
 package org.jboss.as.ejb3.component.stateful;
 
-import org.jboss.as.ee.component.ComponentConfiguration;
-
 import org.jboss.as.ee.component.EEModuleDescription;
-
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * User: jpai
@@ -40,8 +38,9 @@ public class StatefulComponentDescription extends SessionBeanComponentDescriptio
      * @param componentClassName the component instance class name
      * @param moduleDescription  the module description
      */
-    public StatefulComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription) {
-        super(componentName, componentClassName, moduleDescription);
+    public StatefulComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription,
+                                        final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, moduleDescription, deploymentUnitServiceName);
     }
 
     @Override

ejb3/src/main/java/org/jboss/as/ejb3/component/stateless/StatelessComponentDescription.java
@@ -22,10 +22,9 @@
 
 package org.jboss.as.ejb3.component.stateless;
 
-import org.jboss.as.ee.component.ComponentConfiguration;
-
 import org.jboss.as.ee.component.EEModuleDescription;
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * User: jpai
@@ -39,13 +38,9 @@ public class StatelessComponentDescription extends SessionBeanComponentDescripti
      * @param componentClassName the component instance class name
      * @param moduleDescription  the module description
      */
-    public StatelessComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription) {
-        super(componentName, componentClassName, moduleDescription);
-    }
-
-    @Override
-    protected ComponentConfiguration constructComponentConfiguration() {
-        return new StatelessSessionComponentConfiguration(this);
+    public StatelessComponentDescription(final String componentName, final String componentClassName, final EEModuleDescription moduleDescription,
+                                         final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, moduleDescription, deploymentUnitServiceName);
     }
 
     @Override

ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/EjbAnnotationProcessor.java
@@ -42,6 +42,7 @@ import org.jboss.jandex.AnnotationValue;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceName;
 
 import javax.ejb.Singleton;
 import javax.ejb.Stateful;
@@ -112,7 +113,7 @@ public class EjbAnnotationProcessor implements DeploymentUnitProcessor {
         // get the module description
         final EEModuleDescription moduleDescription = deploymentUnit.getAttachment(org.jboss.as.ee.component.Attachments.EE_MODULE_DESCRIPTION);
         final String applicationName = moduleDescription.getApplicationName();
-
+        final ServiceName deploymentUnitServiceName = deploymentUnit.getServiceName();
         // process these session bean annotations and create component descriptions out of it
         for (AnnotationInstance sessionBeanAnnotation : sessionBeanAnnotations) {
             AnnotationTarget target = sessionBeanAnnotation.target();
@@ -130,13 +131,13 @@ public class EjbAnnotationProcessor implements DeploymentUnitProcessor {
             SessionBeanComponentDescription sessionBeanDescription = null;
             switch (sessionBeanType) {
                 case STATELESS:
-                    sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, moduleDescription);
+                    sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, moduleDescription, deploymentUnitServiceName);
                     break;
                 case STATEFUL:
-                    sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, moduleDescription);
+                    sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, moduleDescription, deploymentUnitServiceName);
                     break;
                 case SINGLETON:
-                    sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, moduleDescription);
+                    sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, moduleDescription, deploymentUnitServiceName);
                     break;
                 default:
                     throw new IllegalArgumentException("Unknown session bean type: " + sessionBeanType);

ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/dd/SessionBeanXmlDescriptorProcessor.java
@@ -97,13 +97,13 @@ public class SessionBeanXmlDescriptorProcessor extends AbstractEjbXmlDescriptorP
         SessionBeanComponentDescription sessionBeanDescription = null;
         switch (sessionType) {
             case Stateless:
-                sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, moduleDescription);
+                sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, moduleDescription, deploymentUnit.getServiceName());
                 break;
             case Stateful:
-                sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, moduleDescription);
+                sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, moduleDescription, deploymentUnit.getServiceName());
                 break;
             case Singleton:
-                sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, moduleDescription);
+                sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, moduleDescription, deploymentUnit.getServiceName());
                 break;
             default:
                 throw new IllegalArgumentException("Unknown session bean type: " + sessionType);

wildfly/wildfly

ejb3/src/main/java/org/jboss/as/ejb3/component/messagedriven/MessageDrivenComponentDescription.java
@@ -23,8 +23,10 @@ package org.jboss.as.ejb3.component.messagedriven;
 
 
 
+
 import org.jboss.as.ee.component.ComponentConfiguration;
 import org.jboss.as.ee.component.EEModuleDescription;
+import org.jboss.as.ee.component.ViewDescription;
 import org.jboss.as.ejb3.component.EJBComponentDescription;
 import org.jboss.as.ejb3.component.MethodIntf;
 import org.jboss.as.ejb3.deployment.EjbJarDescription;
@@ -32,6 +34,7 @@ import org.jboss.as.server.deployment.DeploymentPhaseContext;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.modules.Module;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
@@ -45,10 +48,11 @@ public class MessageDrivenComponentDescription extends EJBComponentDescription {
      *
      * @param componentName      the component name
      * @param componentClassName the component instance class name
-     * @param ejbModuleDescription the module description
+     * @param moduleDescription  the module description
      */
-    public MessageDrivenComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbModuleDescription) {
-        super(componentName, componentClassName, ejbModuleDescription);
+    public MessageDrivenComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbJarDescription,
+                                             final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, ejbJarDescription, deploymentUnitServiceName);
     }
 
     @Override
@@ -81,10 +85,15 @@ public class MessageDrivenComponentDescription extends EJBComponentDescription {
 //        }
 //    }
 
-//    public void setMessageListenerInterfaceName(String messageListenerInterfaceName) {
-//        getViewClassNames().add(messageListenerInterfaceName);
-//        this.messageListenerInterfaceName = messageListenerInterfaceName;
-//    }
+    public void setMessageListenerInterfaceName(String messageListenerInterfaceName) {
+        if (messageListenerInterfaceName == null || messageListenerInterfaceName.isEmpty()) {
+            throw new IllegalArgumentException("Cannot set null or empty string as message listener interface");
+        }
+        this.messageListenerInterfaceName = messageListenerInterfaceName;
+        // add it to the view description
+        ViewDescription viewDescription = new ViewDescription(this, messageListenerInterfaceName);
+        this.getViews().add(viewDescription);
+    }
 
     public void setResourceAdapterName(String resourceAdapterName) {
         this.resourceAdapterName = resourceAdapterName;

ejb3/src/main/java/org/jboss/as/ejb3/component/singleton/SingletonComponentDescription.java
@@ -22,13 +22,10 @@
 
 package org.jboss.as.ejb3.component.singleton;
 
-
-import org.jboss.as.ee.component.AbstractComponentConfiguration;
-
-import org.jboss.as.ee.component.ComponentConfiguration;
 import org.jboss.as.ee.component.EEModuleDescription;
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
 import org.jboss.as.ejb3.deployment.EjbJarDescription;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * Component description for a singleton bean
@@ -49,13 +46,9 @@ public class SingletonComponentDescription extends SessionBeanComponentDescripti
      * @param componentClassName the component instance class name
      * @param ejbModuleDescription  the module description
      */
-    public SingletonComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbModuleDescription) {
-        super(componentName, componentClassName, ejbModuleDescription);
-    }
-
-    @Override
-    protected ComponentConfiguration constructComponentConfiguration() {
-        return new SingletonComponentConfiguration(this);
+    public SingletonComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbJarDescription,
+                                         final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, ejbJarDescription, deploymentUnitServiceName);
     }
 
     /**

ejb3/src/main/java/org/jboss/as/ejb3/component/stateful/StatefulComponentDescription.java
@@ -22,10 +22,10 @@
 
 package org.jboss.as.ejb3.component.stateful;
 
-import org.jboss.as.ee.component.ComponentConfiguration;
 
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
 import org.jboss.as.ejb3.deployment.EjbJarDescription;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * User: jpai
@@ -39,8 +39,9 @@ public class StatefulComponentDescription extends SessionBeanComponentDescriptio
      * @param componentClassName the component instance class name
      * @param ejbModuleDescription  the module description
      */
-    public StatefulComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbModuleDescription) {
-        super(componentName, componentClassName, ejbModuleDescription);
+    public StatefulComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbJarDescription,
+                                        final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, ejbJarDescription, deploymentUnitServiceName);
     }
 
     @Override

ejb3/src/main/java/org/jboss/as/ejb3/component/stateless/StatelessComponentDescription.java
@@ -22,10 +22,15 @@
 
 package org.jboss.as.ejb3.component.stateless;
 
+
 import org.jboss.as.ee.component.ComponentConfiguration;
 
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
 import org.jboss.as.ejb3.deployment.EjbJarDescription;
+
+import org.jboss.as.ee.component.EEModuleDescription;
+import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
+import org.jboss.msc.service.ServiceName;
 
 /**
  * User: jpai
@@ -39,13 +44,9 @@ public class StatelessComponentDescription extends SessionBeanComponentDescripti
      * @param componentClassName the component instance class name
      * @param ejbModuleDescription  the module description
      */
-    public StatelessComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbModuleDescription) {
-        super(componentName, componentClassName, ejbModuleDescription);
-    }
-
-    @Override
-    protected ComponentConfiguration constructComponentConfiguration() {
-        return new StatelessSessionComponentConfiguration(this);
+    public StatelessComponentDescription(final String componentName, final String componentClassName, final EjbJarDescription ejbModuleDescription,
+                                         final ServiceName deploymentUnitServiceName) {
+        super(componentName, componentClassName, ejbModuleDescription, deploymentUnitServiceName);
     }
 
     @Override

ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/EjbAnnotationProcessor.java
@@ -44,6 +44,7 @@ import org.jboss.jandex.AnnotationValue;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceName;
 
 import javax.ejb.Singleton;
 import javax.ejb.Stateful;
@@ -121,7 +122,7 @@ public final class EjbAnnotationProcessor implements DeploymentUnitProcessor {
         }
 
         final String applicationName = moduleDescription.getApplicationName();
-
+        final ServiceName deploymentUnitServiceName = deploymentUnit.getServiceName();
         // process these session bean annotations and create component descriptions out of it
         for (final AnnotationInstance sessionBeanAnnotation : sessionBeanAnnotations) {
             final AnnotationTarget target = sessionBeanAnnotation.target();
@@ -139,13 +140,13 @@ public final class EjbAnnotationProcessor implements DeploymentUnitProcessor {
             SessionBeanComponentDescription sessionBeanDescription = null;
             switch (sessionBeanType) {
                 case STATELESS:
-                    sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, ejbJarDescription);
+                    sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, ejbJarDescription, deploymentUnitServiceName);
                     break;
                 case STATEFUL:
-                    sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, ejbJarDescription);
+                    sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, ejbJarDescription, deploymentUnitServiceName);
                     break;
                 case SINGLETON:
-                    sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, ejbJarDescription);
+                    sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, ejbJarDescription, deploymentUnitServiceName);
                     break;
                 default:
                     throw new IllegalArgumentException("Unknown session bean type: " + sessionBeanType);

ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/dd/SessionBeanXmlDescriptorProcessor.java
@@ -100,13 +100,13 @@ public class SessionBeanXmlDescriptorProcessor extends AbstractEjbXmlDescriptorP
         SessionBeanComponentDescription sessionBeanDescription = null;
         switch (sessionType) {
             case Stateless:
-                sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, ejbJarDescription);
+                sessionBeanDescription = new StatelessComponentDescription(beanName, beanClassName, ejbJarDescription, deploymentUnit.getServiceName());
                 break;
             case Stateful:
-                sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, ejbJarDescription);
+                sessionBeanDescription = new StatefulComponentDescription(beanName, beanClassName, ejbJarDescription, deploymentUnit.getServiceName());
                 break;
             case Singleton:
-                sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, ejbJarDescription);
+                sessionBeanDescription = new SingletonComponentDescription(beanName, beanClassName, ejbJarDescription, deploymentUnit.getServiceName());
                 break;
             default:
                 throw new IllegalArgumentException("Unknown session bean type: " + sessionType);