52c2a76818b80d4d138c78f3eaac6c135d365db3
jaikiran <jaikiran.pai@gmail.com>
2011-05-28 16:33:11 +0530

    Fix ejb-local-ref processing regression, introduced with WS integration

--- ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/EjbRefProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/EjbRefProcessor.java
@@ -29,6 +29,7 @@ import org.jboss.as.ee.component.DeploymentDescriptorEnvironment;
 import org.jboss.as.ee.component.EEModuleDescription;
 import org.jboss.as.ee.component.LookupInjectionSource;
 import org.jboss.as.ee.component.ViewBindingInjectionSource;
+import org.jboss.as.ejb3.component.MethodIntf;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.as.server.deployment.reflect.DeploymentReflectionIndex;
@@ -94,7 +95,7 @@ public class EjbRefProcessor extends AbstractDeploymentDescriptorBindingsProcess
                 } else if (!isEmpty(ejbName)) {
                     //TODO: implement cross deployment references
                     final ServiceName beanServiceName = deploymentUnit.getServiceName()
-                            .append("component").append(ejbName).append("VIEW").append(localInterfaceType.getName());
+                            .append("component").append(ejbName).append("VIEW").append(localInterfaceType.getName()).append(MethodIntf.LOCAL.toString());
                     bindingConfiguration = new BindingConfiguration(name, new ViewBindingInjectionSource(beanServiceName));
                 } else {
                     throw new RuntimeException("Support for ejb-local-ref without lookup or ejb-link isn't yet implemented");
