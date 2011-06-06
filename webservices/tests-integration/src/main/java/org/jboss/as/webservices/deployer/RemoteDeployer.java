3f9a8259352d2b8bc86511b005a5d1ed6492ceb1
asoldano <alessio.soldano@jboss.com>
2011-06-06 18:13:00 +0200

    [JBWS-3257] Allow starting a security domain through management API in RemoteDeployer and better handling of jbossws default test security domain creation and removal

--- webservices/tests-integration/src/main/java/org/jboss/as/webservices/deployer/RemoteDeployer.java
+++ webservices/tests-integration/src/main/java/org/jboss/as/webservices/deployer/RemoteDeployer.java
@@ -54,26 +54,24 @@ import org.jboss.wsf.spi.deployer.Deployer;
  * Remote deployer that uses AS7 client deployment API.
  *
  * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
+ * @author <a href="mailto:alessio.soldano@jboss.com">Alessio Soldano</a>
  */
 public final class RemoteDeployer implements Deployer {
 
     private static final Logger LOGGER = Logger.getLogger(RemoteDeployer.class);
-    private static final String JBOSSWS_SEC_DOMAIN = "JBossWS";
+    private static final int PORT = 9999;
     private final Map<URL, String> url2Id = new HashMap<URL, String>();
+    private final InetAddress address = InetAddress.getByName("127.0.0.1");
 
     private ServerDeploymentManager deploymentManager;
 
     public RemoteDeployer() throws IOException {
-        final InetAddress address = InetAddress.getByName("127.0.0.1");
-        deploymentManager = ServerDeploymentManager.Factory.create(address, 9999);
-        ModelControllerClient client = ModelControllerClient.Factory.create(address, 9999);
-        removeJBossWSSecurityDomain(client);
-        createJBossWSSecurityDomain(client);
+        deploymentManager = ServerDeploymentManager.Factory.create(address, PORT);
     }
 
     @Override
     public void deploy(final URL archiveURL) throws Exception {
-        final DeploymentPlanBuilder builder = deploymentManager.newDeploymentPlan().withRollback().add(archiveURL).andDeploy();
+        final DeploymentPlanBuilder builder = deploymentManager.newDeploymentPlan().add(archiveURL).andDeploy();
         final DeploymentPlan plan = builder.build();
         final DeploymentAction deployAction = builder.getLastAction();
         final String uniqueId = deployAction.getDeploymentUnitUniqueName();
@@ -96,52 +94,6 @@ public final class RemoteDeployer implements Deployer {
         }
     }
 
-    private void createJBossWSSecurityDomain(ModelControllerClient client) throws IOException {
-        String usersPropFile = System.getProperty("org.jboss.ws.testsuite.securityDomain.users.propfile");
-        String rolesPropFile = System.getProperty("org.jboss.ws.testsuite.securityDomain.roles.propfile");
-        ModelNode op = new ModelNode();
-        op.get(OP).set(ADD);
-        op.get(OP_ADDR).add(SUBSYSTEM, "security");
-        op.get(OP_ADDR).add(SECURITY_DOMAIN, JBOSSWS_SEC_DOMAIN);
-        ModelNode loginModule = op.get(AUTHENTICATION).add();
-        loginModule.get(CODE).set("UsersRoles");
-        loginModule.get(FLAG).set("required");
-        ModelNode moduleOptions = loginModule.get(MODULE_OPTIONS);
-        if (usersPropFile != null) {
-            moduleOptions.add("usersProperties", usersPropFile);
-        }
-        if (rolesPropFile != null) {
-            moduleOptions.add("rolesProperties", rolesPropFile);
-        }
-        moduleOptions.add("unauthenticatedIdentity", "anonymous");
-        applyUpdate(op, client);
-    }
-
-    private void removeJBossWSSecurityDomain(ModelControllerClient client) {
-        try {
-            ModelNode op = new ModelNode();
-            op.get(OP).set(REMOVE);
-            op.get(OP_ADDR).add(SUBSYSTEM, "security");
-            op.get(OP_ADDR).add(SECURITY_DOMAIN, JBOSSWS_SEC_DOMAIN);
-            applyUpdate(op, client);
-        } catch (Throwable e) {
-            LOGGER.debug("Could not remove '" + JBOSSWS_SEC_DOMAIN + "' security domain from target container", e);
-        }
-    }
-
-    private static void applyUpdate(ModelNode update, final ModelControllerClient client) throws IOException {
-        ModelNode result = client.execute(OperationBuilder.Factory.create(update).build());
-        if (result.hasDefined("outcome") && "success".equals(result.get("outcome").asString())) {
-            if (result.hasDefined("result")) {
-                System.out.println(result.get("result"));
-            }
-        } else if (result.hasDefined("failure-description")) {
-            throw new RuntimeException(result.get("failure-description").toString());
-        } else {
-            throw new RuntimeException("Operation not successful; outcome = " + result.get("outcome"));
-        }
-    }
-
     private void executeDeploymentPlan(final DeploymentPlan plan, final DeploymentAction deployAction) throws Exception {
         try {
             final ServerDeploymentPlanResult planResult = deploymentManager.execute(plan).get();
@@ -161,4 +113,54 @@ public final class RemoteDeployer implements Deployer {
         }
     }
 
+    @Override
+    public void addSecurityDomain(String name, Map<String, String> authenticationOptions) throws Exception {
+        ModelControllerClient client = ModelControllerClient.Factory.create(address, PORT);
+        ModelNode result = createSecurityDomain(client, name, authenticationOptions);
+        checkResult(result);
+    }
+
+    @Override
+    public void removeSecurityDomain(String name) throws Exception {
+        ModelControllerClient client = ModelControllerClient.Factory.create(address, PORT);
+        ModelNode result = removeSecurityDomain(client, name);
+        checkResult(result);
+    }
+
+    private static ModelNode createSecurityDomain(ModelControllerClient client, String name, Map<String, String> authenticationOptions) throws IOException {
+        ModelNode op = new ModelNode();
+        op.get(OP).set(ADD);
+        op.get(OP_ADDR).add(SUBSYSTEM, "security");
+        op.get(OP_ADDR).add(SECURITY_DOMAIN, name);
+        ModelNode loginModule = op.get(AUTHENTICATION).add();
+        loginModule.get(CODE).set("UsersRoles");
+        loginModule.get(FLAG).set("required");
+        ModelNode moduleOptions = loginModule.get(MODULE_OPTIONS);
+        if (authenticationOptions != null) {
+            for (String k : authenticationOptions.keySet()) {
+                moduleOptions.add(k, authenticationOptions.get(k));
+            }
+        }
+        return client.execute(OperationBuilder.Factory.create(op).build());
+    }
+
+    private static ModelNode removeSecurityDomain(ModelControllerClient client, String name) throws IOException {
+        ModelNode op = new ModelNode();
+        op.get(OP).set(REMOVE);
+        op.get(OP_ADDR).add(SUBSYSTEM, "security");
+        op.get(OP_ADDR).add(SECURITY_DOMAIN, name);
+        return client.execute(OperationBuilder.Factory.create(op).build());
+    }
+
+    private static void checkResult(ModelNode result) throws Exception {
+        if (result.hasDefined("outcome") && "success".equals(result.get("outcome").asString())) {
+            if (result.hasDefined("result")) {
+                LOGGER.info(result.get("result"));
+            }
+        } else if (result.hasDefined("failure-description")) {
+            throw new Exception(result.get("failure-description").toString());
+        } else {
+            throw new Exception("Operation not successful; outcome = " + result.get("outcome"));
+        }
+    }
 }
