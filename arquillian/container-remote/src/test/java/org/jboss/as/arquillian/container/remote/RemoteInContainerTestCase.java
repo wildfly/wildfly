dce436bd8af923159503dc9841748ee3ed088478
Aslak Knutsen <aslak@redhat.com>
2011-06-08 10:53:05 +0200

    AS7-1065 Add AS7 Remote Deployable Container support. Does not start or stop the container, but will connect to a running instance.

--- arquillian/container-remote/src/test/java/org/jboss/as/arquillian/container/remote/RemoteInContainerTestCase.java
+++ arquillian/container-remote/src/test/java/org/jboss/as/arquillian/container/remote/RemoteInContainerTestCase.java
@@ -20,66 +20,37 @@ import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.net.URL;
 
-import javax.management.Attribute;
-import javax.management.MBeanInfo;
 import javax.management.MBeanServerConnection;
-import javax.management.ObjectName;
 
-import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.container.remote.archive.ConfigService;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * JBossASRemoteIntegrationTestCase
+ * ManagedInContainerTestCase
  *
  * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ * @author Thomas.Diesler@jboss.com
  */
 @RunWith(Arquillian.class)
-public class JBossASRemoteInContainerTestCase {
+public class RemoteInContainerTestCase extends AbstractContainerTestCase {
+
     @Deployment
     public static JavaArchive createDeployment() throws Exception {
         JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "sar-example.sar");
         archive.addPackage(ConfigService.class.getPackage());
-        archive.addClass(JBossASRemoteInContainerTestCase.class);
+        archive.addClasses(AbstractContainerTestCase.class);
         String path = "META-INF/jboss-service.xml";
-        URL resourceURL = JBossASRemoteInContainerTestCase.class.getResource("/sar-example.sar/" + path);
-        archive.addResource(new File(resourceURL.getFile()), path);
+        URL resourceURL = RemoteInContainerTestCase.class.getResource("/sar-example.sar/" + path);
+        archive.addAsResource(new File(resourceURL.getFile()), path);
         return archive;
     }
 
-    @Test
-    public void testDeployedService() throws Exception {
-        MBeanServerConnection mbeanServer = getMBeanServer();
-        ObjectName objectName = new ObjectName("jboss:name=test,type=config");
-
-        //FIXME should have some notification happening when the deployment has been installed for client
-        waitForMbean(mbeanServer, objectName);
-
-        Object o = mbeanServer.getAttribute(objectName, "IntervalSeconds");
-        mbeanServer.setAttribute(objectName, new Attribute("IntervalSeconds", 2));
-    }
-
-    protected MBeanServerConnection getMBeanServer() throws Exception {
+    @Override
+    MBeanServerConnection getMBeanServer() throws Exception {
         return ManagementFactory.getPlatformMBeanServer();
     }
-
-    protected void waitForMbean(MBeanServerConnection mbeanServer, ObjectName name) throws Exception {
-        //FIXME remove this
-        long end = System.currentTimeMillis() + 3000;
-        do {
-            try {
-                MBeanInfo info = mbeanServer.getMBeanInfo(name);
-                if (info != null) {
-                    return;
-                }
-            } catch (Exception e) {
-            }
-            Thread.sleep(100);
-        } while (System.currentTimeMillis() < end);
-    }
-
 }
