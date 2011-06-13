787863ce0601f8ad4a9dc09f217572bfe65858de
bstansberry@jboss.com <brian.stansberry@redhat.com>
2011-06-13 14:21:36 -0500

    Initial rollout coordinator work

---
+++ domain-controller/src/main/java/org/jboss/as/domain/controller/plan/NewServerOperationExecutor.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package org.jboss.as.domain.controller.plan;
+
+import org.jboss.as.controller.client.Operation;
+import org.jboss.as.domain.controller.ServerIdentity;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * Callback from a task when it wants to execute an operation.
+ *
+ * @author Brian Stansberry (c) 2011 Red Hat Inc.
+ */
+public interface NewServerOperationExecutor {
+
+    ModelNode executeServerOperation(ServerIdentity server, ModelNode operation);
+}
