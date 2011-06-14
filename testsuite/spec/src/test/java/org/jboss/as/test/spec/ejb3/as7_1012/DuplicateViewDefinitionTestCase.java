1e3402a2151f78f81879fb9916189da2d1b26de8
Carlo de Wolf <cdewolf@redhat.com>
2011-06-14 14:48:38 +0200

    AS7-1012: test case

---
+++ testsuite/spec/src/test/java/org/jboss/as/test/spec/ejb3/as7_1012/DuplicateViewDefinitionTestCase.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.spec.ejb3.as7_1012;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.test.spec.ejb3.common.DoNothing;
+import org.jboss.as.test.spec.ejb3.common.DoNothingBean;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJB;
+import java.util.logging.Logger;
+
+/**
+ * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
+ */
+@RunWith(Arquillian.class)
+public class DuplicateViewDefinitionTestCase {
+    private static final Logger log = Logger.getLogger(DuplicateViewDefinitionTestCase.class.getName());
+
+    @Deployment
+    public static Archive<?> deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "ejb3as7-1012.jar")
+                .addPackage(AnnotatedDoNothingBean.class.getPackage())
+                .addPackage(DoNothingBean.class.getPackage())
+                .addAsManifestResource("ejb3/as7_1012/ejb-jar.xml", "ejb-jar.xml");
+        log.info(archive.toString(true));
+        return archive;
+    }
+
+    @EJB
+    private DoNothing bean;
+
+    @Test
+    public void testDoNothing() {
+        // if it deploys, we are good to go
+        bean.doNothing();
+    }
+}
