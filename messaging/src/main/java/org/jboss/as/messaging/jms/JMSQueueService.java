adb34a0480ee5a6a0762c8549fd59132007ddd03
Jason T. Greene <jason.greene@redhat.com>
2011-06-15 22:19:42 -0500

    Use service based JNDI registration

--- messaging/src/main/java/org/jboss/as/messaging/jms/JMSQueueService.java
+++ messaging/src/main/java/org/jboss/as/messaging/jms/JMSQueueService.java
@@ -65,21 +65,7 @@ public class JMSQueueService implements Service<Void> {
     public synchronized void start(StartContext context) throws StartException {
         final JMSServerManager jmsManager = jmsServer.getValue();
         try {
-            MockContext.pushBindingTrap();
-            try {
-                jmsManager.createQueue(false, queueName, selectorString, durable, jndi);
-            } finally {
-                final ServiceTarget target = context.getChildTarget();
-                final Map<String, Object> bindings = MockContext.popTrappedBindings();
-                for(Map.Entry<String, Object> binding : bindings.entrySet()) {
-                    final BinderService binderService = new BinderService(binding.getKey());
-                    target.addService(ContextNames.JAVA_CONTEXT_SERVICE_NAME.append(binding.getKey()), binderService)
-                        .addDependency(ContextNames.JAVA_CONTEXT_SERVICE_NAME, NamingStore.class, binderService.getNamingStoreInjector())
-                        .addInjection(binderService.getManagedObjectInjector(), new ValueManagedReferenceFactory(Values.immediateValue(binding.getValue())))
-                        .setInitialMode(ServiceController.Mode.ACTIVE)
-                        .install();
-                }
-            }
+            jmsManager.createQueue(false, queueName, selectorString, durable, jndi);
         } catch (Exception e) {
             throw new StartException("failed to create queue", e);
         }
