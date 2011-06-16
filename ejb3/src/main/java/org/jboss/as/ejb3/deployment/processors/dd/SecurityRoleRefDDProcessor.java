c660f3d4c3cc84c21fe5ae4551001136f9d46144
jaikiran <jaikiran.pai@gmail.com>
2011-06-16 16:20:26 +0530

    security role linking

--- ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/dd/SecurityRoleRefDDProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/dd/SecurityRoleRefDDProcessor.java
@@ -33,6 +33,7 @@ import org.jboss.logging.Logger;
 import org.jboss.metadata.ejb.spec.EnterpriseBeanMetaData;
 import org.jboss.metadata.ejb.spec.MessageDrivenBeanMetaData;
 import org.jboss.metadata.ejb.spec.SessionBeanMetaData;
+import org.jboss.metadata.javaee.spec.SecurityRoleRefMetaData;
 import org.jboss.metadata.javaee.spec.SecurityRoleRefsMetaData;
 
 /**
@@ -51,20 +52,33 @@ public class SecurityRoleRefDDProcessor extends AbstractEjbXmlDescriptorProcesso
 
     @Override
     protected void processBeanMetaData(final EnterpriseBeanMetaData beanMetaData, final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
-        SecurityRoleRefsMetaData securityRoles = null;
+        SecurityRoleRefsMetaData securityRoleRefs = null;
         if (beanMetaData instanceof SessionBeanMetaData) {
-            securityRoles = ((SessionBeanMetaData) beanMetaData).getSecurityRoleRefs();
+            securityRoleRefs = ((SessionBeanMetaData) beanMetaData).getSecurityRoleRefs();
         } else if (beanMetaData instanceof MessageDrivenBeanMetaData) {
             // TODO: Why doesn't MessageDrivenBeanMetaData have security role refs metadata
             logger.warn("security-role-ref for message driven beans isn't yet implemented");
         }
-        if (securityRoles == null) {
+        if (securityRoleRefs == null) {
             return;
         }
         final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
         final EEModuleDescription moduleDescription = deploymentUnit.getAttachment(Attachments.EE_MODULE_DESCRIPTION);
         final EJBComponentDescription ejbComponentDescription = (EJBComponentDescription) moduleDescription.getComponentByName(beanMetaData.getEjbName());
-        ejbComponentDescription.setDeclaredRoles(securityRoles.keySet());
+        for (final SecurityRoleRefMetaData securityRoleRef : securityRoleRefs) {
+            final String fromRole = securityRoleRef.getRoleName();
+            String toRole = securityRoleRef.getRoleLink();
+            if (fromRole == null || fromRole.trim().isEmpty()) {
+                throw new DeploymentUnitProcessingException("<role-name> cannot be null or empty in <security-role-ref> " +
+                        "for bean: " + ejbComponentDescription.getEJBName());
+            }
+            // if role-link hasn't been specified, then it links to the same role name as the one specified
+            // in the role-name
+            if (toRole == null) {
+                toRole = fromRole;
+            }
+            ejbComponentDescription.linkSecurityRoles(fromRole, toRole);
+        }
 
     }
 }
