3e4561bcb782208ef0de3950e7b2c361f688c340
jaikiran <jaikiran.pai@gmail.com>
2011-06-16 21:00:25 +0530

    AS7-965 Process @PermitAll

--- ejb3/src/main/java/org/jboss/as/ejb3/component/security/EJBSecurityMetaData.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/security/EJBSecurityMetaData.java
@@ -56,6 +56,8 @@ public class EJBSecurityMetaData {
 
     private final Map<String, Map<Method, Set<String>>> rolesAllowedForMethodsPerView = new HashMap<String, Map<Method, Set<String>>>();
 
+    private final Map<String, Collection<Method>> permitAllMethodsOnView = new HashMap<String, Collection<Method>>();
+
     public EJBSecurityMetaData(final ComponentConfiguration componentConfiguration) {
         if (componentConfiguration.getComponentDescription() instanceof EJBComponentDescription == false) {
             throw new IllegalArgumentException(componentConfiguration.getComponentName() + " is not an EJB component");
@@ -65,12 +67,19 @@ public class EJBSecurityMetaData {
         this.ejbName = ejbComponentDescription.getEJBName();
         this.runAsRole = ejbComponentDescription.getRunAs();
         this.securityDomain = ejbComponentDescription.getSecurityDomain();
+        // @DeclareRoles
         final Set<String> roles = ejbComponentDescription.getDeclaredRoles();
         this.declaredRoles = roles == null ? Collections.<String>emptySet() : Collections.unmodifiableSet(roles);
         // process @DenyAll/exclude-list
         this.processDenyAll(componentConfiguration);
+        // process @PermitAll list
+        this.processPermitAll(componentConfiguration);
         // process @RolesAllowed/method-permission
         this.processRolesAllowed(componentConfiguration);
+
+        // check role-link correctness (this should be done last, after we have assembled @DeclareRoles, @RolesAllowed
+        // and application level <security-role>
+        this.processSecurityRoleLinks(ejbComponentDescription);
 
     }
 
@@ -151,12 +160,70 @@ public class EJBSecurityMetaData {
     }
 
     private void applyDenyAll(final String viewClassName, final Method componentMethod) {
+        // make sure this method doesn't appear in @PermitAll
+        final Collection<Method> permitAllMethods = this.permitAllMethodsOnView.get(viewClassName);
+        if (permitAllMethods != null && permitAllMethods.contains(componentMethod)) {
+            throw new IllegalStateException("Method " + componentMethod + " for view: " + viewClassName + " is already " +
+                    "marked for @PermitAll and hence cannot be marked as @DenyAll/exclude-list");
+        }
         Collection<Method> accessDeniedMethods = this.accessDeniedMethodsOnView.get(viewClassName);
         if (accessDeniedMethods == null) {
             accessDeniedMethods = new HashSet<Method>();
             this.accessDeniedMethodsOnView.put(viewClassName, accessDeniedMethods);
         }
         accessDeniedMethods.add(componentMethod);
+    }
+
+    private void processPermitAll(final ComponentConfiguration componentConfiguration) {
+        List<ViewConfiguration> views = componentConfiguration.getViews();
+        if (views == null || views.isEmpty()) {
+            return;
+        }
+        final EJBComponentDescription ejbComponentDescription = (EJBComponentDescription) componentConfiguration.getComponentDescription();
+        for (ViewConfiguration view : views) {
+            final String viewClassName = view.getViewClass().getName();
+            Collection<EJBMethodIdentifier> permitAllMethodsForView = ejbComponentDescription.getPermitAllMethodsForView(viewClassName);
+            if (permitAllMethodsForView == null) {
+                permitAllMethodsForView = Collections.emptySet();
+            }
+            final Method[] viewMethods = view.getProxyFactory().getCachedMethods();
+            for (final Method viewMethod : viewMethods) {
+                // TODO: proxy factory exposes non-public methods, is this a bug in the no-interface view?
+                if (!Modifier.isPublic(viewMethod.getModifiers())) {
+                    continue;
+                }
+                // find the component method corresponding to this view method
+                final Method componentMethod = this.findComponentMethod(componentConfiguration, viewMethod);
+                final EJBMethodIdentifier ejbMethodIdentifier = EJBMethodIdentifier.fromMethod(componentMethod);
+                if (permitAllMethodsForView.contains(ejbMethodIdentifier)) {
+                    this.applyPermitAll(viewClassName, componentMethod);
+                    continue;
+                }
+                // check on class level
+
+                final Class<?> declaringClass = componentMethod.getDeclaringClass();
+                if (ejbComponentDescription.isPermitAllApplicableToClass(viewClassName, declaringClass.getName())) {
+                    this.applyPermitAll(viewClassName, componentMethod);
+                    continue;
+                }
+            }
+        }
+
+    }
+
+    private void applyPermitAll(final String viewClassName, final Method componentMethod) {
+        // make sure this method doesn't appear in @DenyAll
+        final Collection<Method> accessDeniedMethods = this.accessDeniedMethodsOnView.get(viewClassName);
+        if (accessDeniedMethods != null && accessDeniedMethods.contains(componentMethod)) {
+            throw new IllegalStateException("Method " + componentMethod + " for view: " + viewClassName + " is already " +
+                    "marked for @DenyAll/exclude-list and hence cannot be marked as @PermitAll");
+        }
+        Collection<Method> permitAllMethods = this.permitAllMethodsOnView.get(viewClassName);
+        if (permitAllMethods == null) {
+            permitAllMethods = new HashSet<Method>();
+            this.permitAllMethodsOnView.put(viewClassName, permitAllMethods);
+        }
+        permitAllMethods.add(componentMethod);
     }
 
     private void processRolesAllowed(final ComponentConfiguration componentConfiguration) {
@@ -206,6 +273,26 @@ public class EJBSecurityMetaData {
             perViewRoles.put(componentMethod, rolesAllowedForMethod);
         }
         rolesAllowedForMethod.addAll(roles);
+    }
+
+    private void processSecurityRoleLinks(final EJBComponentDescription ejbComponentDescription) {
+        final Map<String, Collection<String>> roleLinks = ejbComponentDescription.getSecurityRoleLinks();
+        if (roleLinks.isEmpty()) {
+            return;
+        }
+        // TODO: Enable this only if we want to do the security role-link check
+//        for (final Map.Entry<String, Collection<String>> entry : roleLinks.entrySet()) {
+//            final String roleName = entry.getKey();
+//            for (final String link : entry.getValue()) {
+//                // TODO: Make sure that the link belongs to either @DeclareRoles, @RolesAllowed or <security-role>
+//                // collection. if not, then it's an error as per EJB3 spec:
+//                // EJB3.1 spec, section 17.3.3
+//                // The value of the role-link element must be the name of one of the security roles defined in
+//                // a security-role element or by means of the DeclareRoles annotations or RolesAllowed
+//                // annotations
+//
+//            }
+//        }
     }
 
     private Method findComponentMethod(final ComponentConfiguration componentConfiguration, final Method viewMethod) {
