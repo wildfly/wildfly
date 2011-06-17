41e3b48f51a4f0bc8387eb966d4076a9e1125f19
Alexey Loubyansky <olubyans@tstudio.(none)>
2011-06-17 18:08:19 +0200

    hide the general data-source command for the moment

--- cli/src/main/java/org/jboss/as/cli/CommandLineMain.java
+++ cli/src/main/java/org/jboss/as/cli/CommandLineMain.java
@@ -134,7 +134,7 @@ public class CommandLineMain {
         cmdRegistry.registerHandler(new DataSourceAddHandler(), "add-data-source");
         cmdRegistry.registerHandler(new DataSourceModifyHandler(), "modify-data-source");
         cmdRegistry.registerHandler(new DataSourceRemoveHandler(), "remove-data-source");
-        cmdRegistry.registerHandler(new SimpleDataSourceOperationHandler("data-source"), "data-source");
+        //cmdRegistry.registerHandler(new SimpleDataSourceOperationHandler("data-source"), "data-source");
         cmdRegistry.registerHandler(new XADataSourceAddHandler(), "add-xa-data-source");
         cmdRegistry.registerHandler(new XADataSourceRemoveHandler(), "remove-xa-data-source");
         cmdRegistry.registerHandler(new XADataSourceModifyHandler(), "modify-xa-data-source");
