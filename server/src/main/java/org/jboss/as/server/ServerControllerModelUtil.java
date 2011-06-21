67f46e80db80ae93f285fb4883adb5883018f235
bstansberry@jboss.com <brian.stansberry@redhat.com>
2011-06-21 09:36:40 -0500

    Register reload op in the correct launch type

--- server/src/main/java/org/jboss/as/server/ServerControllerModelUtil.java
+++ server/src/main/java/org/jboss/as/server/ServerControllerModelUtil.java
@@ -207,9 +207,11 @@ public class
 
         // Runtime operations
         if (serverEnvironment != null) {
-            if (serverEnvironment.getLaunchType() == ServerEnvironment.LaunchType.DOMAIN)
+            // Reload op -- does not work on a domain mode server
+            if (serverEnvironment.getLaunchType() != ServerEnvironment.LaunchType.DOMAIN)
                 root.registerOperationHandler(ServerReloadHandler.OPERATION_NAME, ServerReloadHandler.INSTANCE, ServerReloadHandler.INSTANCE, false);
 
+            // The System.exit() based shutdown command is only valid for a server process directly launched from the command line
             if (serverEnvironment.getLaunchType() == ServerEnvironment.LaunchType.STANADALONE)
                 root.registerOperationHandler(ServerShutdownHandler.OPERATION_NAME, ServerShutdownHandler.INSTANCE, ServerShutdownHandler.INSTANCE, false);
 
