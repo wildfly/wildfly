thanhvc/jboss-as

Inital JSF 1.2 support

web/src/main/java/org/jboss/as/web/WebSubsystemAdd.java
@@ -50,6 +50,7 @@ import org.jboss.as.web.deployment.WebParsingDeploymentProcessor;
 import org.jboss.as.web.deployment.component.WebComponentProcessor;
 import org.jboss.as.web.deployment.jsf.JsfAnnotationProcessor;
 import org.jboss.as.web.deployment.jsf.JsfManagedBeanProcessor;
+import org.jboss.as.web.deployment.jsf.JsfVersionProcessor;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceBuilder.DependencyType;
 import org.jboss.msc.service.ServiceController.Mode;
@@ -119,6 +120,7 @@ class WebSubsystemAdd implements ModelAddOperationHandler, BootOperationHandler,
                     ctx.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_WEB_COMPONENTS, new WebComponentProcessor());
                     ctx.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_EAR_CONTEXT_ROOT, new EarContextRootProcessor());
                     ctx.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_WEB_MERGE_METADATA, new WarMetaDataProcessor());
+                    ctx.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_JSF_VERSION, new JsfVersionProcessor());
                     ctx.addDeploymentProcessor(Phase.DEPENDENCIES, Phase.DEPENDENCIES_WAR_MODULE, new WarClassloadingDependencyProcessor());
                     ctx.addDeploymentProcessor(Phase.POST_MODULE, Phase.POST_MODULE_JSF_MANAGED_BEANS, new JsfManagedBeanProcessor());
                     ctx.addDeploymentProcessor(Phase.INSTALL, Phase.INSTALL_SERVLET_INIT_DEPLOYMENT, new ServletContainerInitializerDeploymentProcessor());

web/src/main/java/org/jboss/as/web/deployment/WarClassloadingDependencyProcessor.java
@@ -30,6 +30,7 @@ import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.as.server.deployment.DeploymentUnitProcessor;
 import org.jboss.as.server.deployment.module.ModuleDependency;
 import org.jboss.as.server.deployment.module.ModuleSpecification;
+import org.jboss.logging.Logger;
 import org.jboss.modules.Module;
 import org.jboss.modules.ModuleIdentifier;
 import org.jboss.modules.ModuleLoader;
@@ -46,6 +47,9 @@ public class WarClassloadingDependencyProcessor implements DeploymentUnitProcess
     private static final ModuleIdentifier APACHE_XERCES = ModuleIdentifier.create("org.apache.xerces");
 
     private static final ModuleIdentifier JSF_IMPL = ModuleIdentifier.create("com.sun.jsf-impl");
+    private static final ModuleIdentifier JSF_API = ModuleIdentifier.create("javax.faces.api");
+    private static final ModuleIdentifier JSF_1_2_IMPL = ModuleIdentifier.create("com.sun.jsf-impl", "1.2");
+    private static final ModuleIdentifier JSF_1_2_API = ModuleIdentifier.create("javax.faces.api", "1.2");
     private static final ModuleIdentifier BEAN_VALIDATION = ModuleIdentifier.create("org.hibernate.validator");
 
     private static final ModuleIdentifier JBOSS_WEB = ModuleIdentifier.create("org.jboss.as.web");
@@ -55,22 +59,44 @@ public class WarClassloadingDependencyProcessor implements DeploymentUnitProcess
         Module.registerURLStreamHandlerFactoryModule(Module.forClass(WarClassloadingDependencyProcessor.class));
     }
 
+    private static final Logger logger = Logger.getLogger(WarClassloadingDependencyProcessor.class);
+
     public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
         final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
         final ModuleSpecification moduleSpecification = deploymentUnit.getAttachment(Attachments.MODULE_SPECIFICATION);
+        final ModuleLoader moduleLoader = Module.getBootModuleLoader();
+
+        //we always make the JSF api available
+        final String jsfVersion = JsfVersionMarker.getVersion(deploymentUnit);
+
+        if(jsfVersion.equals(JsfVersionMarker.JSF_1_2)) {
+            moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JSF_1_2_API, false, false, false));
+        } else {
+            moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JSF_API, false, false, false));
+        }
+
         if (!DeploymentTypeMarker.isType(DeploymentType.WAR, deploymentUnit)) {
             return; // Skip non web deployments
         }
-        final ModuleLoader moduleLoader = Module.getBootModuleLoader();
         // Add module dependencies on Java EE apis
 
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JAVAX_EE_API, false, false, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, APACHE_XERCES, false, false, true));
 
         // Add modules for JSF
-        ModuleDependency jsf = new ModuleDependency(moduleLoader, JSF_IMPL, false, false, false);
-        jsf.addImportFilter(PathFilters.getMetaInfFilter(), true);
-        moduleSpecification.addSystemDependency(jsf);
+
+        if(jsfVersion.equals(JsfVersionMarker.JSF_1_2)) {
+            ModuleDependency jsf = new ModuleDependency(moduleLoader, JSF_1_2_IMPL, false, false, false);
+            jsf.addImportFilter(PathFilters.getMetaInfFilter(), true);
+            moduleSpecification.addSystemDependency(jsf);
+        } else {
+            if(!jsfVersion.equals(JsfVersionMarker.JSF_2_0)) {
+                logger.warn("Ukown JSF version " + jsfVersion + " " + JsfVersionMarker.JSF_2_0 + " will be used instead");
+            }
+            ModuleDependency jsf = new ModuleDependency(moduleLoader, JSF_IMPL, false, false, false);
+            jsf.addImportFilter(PathFilters.getMetaInfFilter(), true);
+            moduleSpecification.addSystemDependency(jsf);
+        }
 
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, BEAN_VALIDATION, false, false, true));
 

web/src/main/java/org/jboss/as/web/deployment/jsf/JsfVersionProcessor.java
@@ -1,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.web.deployment.jsf;
+
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.jboss.as.web.deployment.JsfVersionMarker;
+import org.jboss.as.web.deployment.WarMetaData;
+import org.jboss.metadata.javaee.spec.ParamValueMetaData;
+
+import java.util.List;
+
+/**
+ * @author Stuart Douglas
+ */
+public class JsfVersionProcessor implements DeploymentUnitProcessor {
+
+    public static final String CONTEXT_PARAM = "org.jboss.jbossfaces.JSF_CONFIG_NAME";
+
+    @Override
+    public void deploy(final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+        final WarMetaData metaData = deploymentUnit.getAttachment(WarMetaData.ATTACHMENT_KEY);
+
+        if (metaData == null) {
+            return;
+        }
+
+        if (metaData.getMergedJBossWebMetaData() == null) {
+            return;
+        }
+
+        List<ParamValueMetaData> contextParams = metaData.getMergedJBossWebMetaData().getContextParams();
+        if(contextParams == null) {
+            return;
+        }
+
+        for(final ParamValueMetaData param : contextParams) {
+            if(param.getParamName().equals(CONTEXT_PARAM)) {
+                JsfVersionMarker.setVersion(deploymentUnit, param.getParamValue());
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void undeploy(final DeploymentUnit context) {
+    }
+}

wildfly/wildfly

web/src/main/java/org/jboss/as/web/WebSubsystemAdd.java
@@ -50,6 +50,7 @@ import org.jboss.as.web.deployment.WebParsingDeploymentProcessor;
 import org.jboss.as.web.deployment.component.WebComponentProcessor;
 import org.jboss.as.web.deployment.jsf.JsfAnnotationProcessor;
 import org.jboss.as.web.deployment.jsf.JsfManagedBeanProcessor;
+import org.jboss.as.web.deployment.jsf.JsfVersionProcessor;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.Property;
 import org.jboss.msc.service.ServiceBuilder.DependencyType;
@@ -132,6 +133,7 @@ class WebSubsystemAdd extends AbstractBoottimeAddStepHandler implements Descript
                 processorTarget.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_WEB_COMPONENTS, new WebComponentProcessor());
                 processorTarget.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_EAR_CONTEXT_ROOT, new EarContextRootProcessor());
                 processorTarget.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_WEB_MERGE_METADATA, new WarMetaDataProcessor());
+                processorTarget.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_JSF_VERSION, new JsfVersionProcessor());
                 processorTarget.addDeploymentProcessor(Phase.DEPENDENCIES, Phase.DEPENDENCIES_WAR_MODULE, new WarClassloadingDependencyProcessor());
                 processorTarget.addDeploymentProcessor(Phase.POST_MODULE, Phase.POST_MODULE_JSF_MANAGED_BEANS, new JsfManagedBeanProcessor());
                 processorTarget.addDeploymentProcessor(Phase.PARSE, Phase.POST_MODULE_JSF_MANAGED_BEANS, new JsfManagedBeanProcessor());

web/src/main/java/org/jboss/as/web/deployment/WarClassloadingDependencyProcessor.java
@@ -30,6 +30,7 @@ import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.as.server.deployment.DeploymentUnitProcessor;
 import org.jboss.as.server.deployment.module.ModuleDependency;
 import org.jboss.as.server.deployment.module.ModuleSpecification;
+import org.jboss.logging.Logger;
 import org.jboss.modules.Module;
 import org.jboss.modules.ModuleIdentifier;
 import org.jboss.modules.ModuleLoader;
@@ -46,6 +47,9 @@ public class WarClassloadingDependencyProcessor implements DeploymentUnitProcess
     private static final ModuleIdentifier APACHE_XERCES = ModuleIdentifier.create("org.apache.xerces");
 
     private static final ModuleIdentifier JSF_IMPL = ModuleIdentifier.create("com.sun.jsf-impl");
+    private static final ModuleIdentifier JSF_API = ModuleIdentifier.create("javax.faces.api");
+    private static final ModuleIdentifier JSF_1_2_IMPL = ModuleIdentifier.create("com.sun.jsf-impl", "1.2");
+    private static final ModuleIdentifier JSF_1_2_API = ModuleIdentifier.create("javax.faces.api", "1.2");
     private static final ModuleIdentifier BEAN_VALIDATION = ModuleIdentifier.create("org.hibernate.validator");
 
     private static final ModuleIdentifier JBOSS_WEB = ModuleIdentifier.create("org.jboss.as.web");
@@ -54,22 +58,45 @@ public class WarClassloadingDependencyProcessor implements DeploymentUnitProcess
         Module.registerURLStreamHandlerFactoryModule(Module.forClass(WarClassloadingDependencyProcessor.class));
     }
 
+    private static final Logger logger = Logger.getLogger(WarClassloadingDependencyProcessor.class);
+
     public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
         final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+        final DeploymentUnit topLevelDeployment = deploymentUnit.getParent() == null ? deploymentUnit : deploymentUnit.getParent();
         final ModuleSpecification moduleSpecification = deploymentUnit.getAttachment(Attachments.MODULE_SPECIFICATION);
+        final ModuleLoader moduleLoader = Module.getBootModuleLoader();
+
+        //we always make the JSF api available
+        final String jsfVersion = JsfVersionMarker.getVersion(topLevelDeployment);
+
+        if(jsfVersion.equals(JsfVersionMarker.JSF_1_2)) {
+            moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JSF_1_2_API, false, false, false));
+        } else {
+            moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JSF_API, false, false, false));
+        }
+
         if (!DeploymentTypeMarker.isType(DeploymentType.WAR, deploymentUnit)) {
             return; // Skip non web deployments
         }
-        final ModuleLoader moduleLoader = Module.getBootModuleLoader();
         // Add module dependencies on Java EE apis
 
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JAVAX_EE_API, false, false, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, APACHE_XERCES, false, false, true));
 
         // Add modules for JSF
-        ModuleDependency jsf = new ModuleDependency(moduleLoader, JSF_IMPL, false, false, false);
-        jsf.addImportFilter(PathFilters.getMetaInfFilter(), true);
-        moduleSpecification.addSystemDependency(jsf);
+
+        if(jsfVersion.equals(JsfVersionMarker.JSF_1_2)) {
+            ModuleDependency jsf = new ModuleDependency(moduleLoader, JSF_1_2_IMPL, false, false, false);
+            jsf.addImportFilter(PathFilters.getMetaInfFilter(), true);
+            moduleSpecification.addSystemDependency(jsf);
+        } else {
+            if(!jsfVersion.equals(JsfVersionMarker.JSF_2_0)) {
+                logger.warn("Ukown JSF version " + jsfVersion + " " + JsfVersionMarker.JSF_2_0 + " will be used instead");
+            }
+            ModuleDependency jsf = new ModuleDependency(moduleLoader, JSF_IMPL, false, false, false);
+            jsf.addImportFilter(PathFilters.getMetaInfFilter(), true);
+            moduleSpecification.addSystemDependency(jsf);
+        }
 
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, BEAN_VALIDATION, false, false, true));
 

web/src/main/java/org/jboss/as/web/deployment/jsf/JsfVersionProcessor.java
@@ -1,0 +1,76 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.web.deployment.jsf;
+
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.jboss.as.web.deployment.JsfVersionMarker;
+import org.jboss.as.web.deployment.WarMetaData;
+import org.jboss.metadata.javaee.spec.ParamValueMetaData;
+
+import java.util.List;
+
+/**
+ * @author Stuart Douglas
+ */
+public class JsfVersionProcessor implements DeploymentUnitProcessor {
+
+    public static final String CONTEXT_PARAM = "org.jboss.jbossfaces.JSF_CONFIG_NAME";
+
+    @Override
+    public void deploy(final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+        final DeploymentUnit topLevelDeployment = deploymentUnit.getParent() == null ? deploymentUnit : deploymentUnit.getParent();
+        final WarMetaData metaData = deploymentUnit.getAttachment(WarMetaData.ATTACHMENT_KEY);
+
+        if (metaData == null) {
+            return;
+        }
+
+        if (metaData.getMergedJBossWebMetaData() == null) {
+            return;
+        }
+
+        List<ParamValueMetaData> contextParams = metaData.getMergedJBossWebMetaData().getContextParams();
+        if(contextParams == null) {
+            return;
+        }
+
+        //we need to set the JSF version for the whole deployment
+        //as otherwise linkage errors can occur
+        //if the user does have an ear with two wars with two different
+        //JSF versions they are going to need to use deployment descriptors
+        //to manually sort out the dependencies
+        for(final ParamValueMetaData param : contextParams) {
+            if(param.getParamName().equals(CONTEXT_PARAM)) {
+                JsfVersionMarker.setVersion(topLevelDeployment, param.getParamValue());
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void undeploy(final DeploymentUnit context) {
+    }
+}