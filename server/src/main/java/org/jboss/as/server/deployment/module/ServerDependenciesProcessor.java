3ac3d25812c594ecbf20023a31f48f4bdd697559
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-06-24 10:24:00 -0400

    Make org.jboss.vfs module visible by default

--- server/src/main/java/org/jboss/as/server/deployment/module/ServerDependenciesProcessor.java
+++ server/src/main/java/org/jboss/as/server/deployment/module/ServerDependenciesProcessor.java
@@ -40,6 +40,7 @@ public class ServerDependenciesProcessor implements DeploymentUnitProcessor {
     private static ModuleIdentifier SUN_JDK = ModuleIdentifier.create("sun.jdk");
     private static ModuleIdentifier JAVAX_API = ModuleIdentifier.create("javax.api");
     private static ModuleIdentifier JBOSS_LOGGING = ModuleIdentifier.create("org.jboss.logging");
+    private static ModuleIdentifier JBOSS_VFS = ModuleIdentifier.create("org.jboss.vfs");
 
     @Override
     public void deploy(final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
@@ -49,6 +50,7 @@ public class ServerDependenciesProcessor implements DeploymentUnitProcessor {
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, SUN_JDK, false, false, true));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JAVAX_API, false, false, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JBOSS_LOGGING, false, false, false));
+        moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JBOSS_VFS, false, false, false));
     }
 
     @Override
