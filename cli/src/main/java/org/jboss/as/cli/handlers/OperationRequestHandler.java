e9f29b2e7d3738355682e36cdb677120c99c06ea
Darran Lofthouse <darran.lofthouse@jboss.com>
2011-06-27 16:26:02 +0100

    Ensure last is re-assigned during unwrapping to prevent an infinite loop.

--- cli/src/main/java/org/jboss/as/cli/handlers/OperationRequestHandler.java
+++ cli/src/main/java/org/jboss/as/cli/handlers/OperationRequestHandler.java
@@ -93,6 +93,7 @@ public class OperationRequestHandler implements CommandHandler, OperationCommand
                     ctx.disconnectController();
                     return;
                 }
+                last = cur;
             }
             throw e;
         }
