199a4cb9bc992819ea3a6b0aaa9fd990f2b14696
Stefano Maestri <stefano.maestri@javalinux.it>
2011-07-07 14:07:09 +0200

    AS7-1206 test for jndi binding isn't unbound during remove if jndi name and data-source name are different

--- testsuite/smoke/src/test/java/org/jboss/as/test/embedded/mgmt/datasource/DataSourceOperationsUnitTestCase.java
+++ testsuite/smoke/src/test/java/org/jboss/as/test/embedded/mgmt/datasource/DataSourceOperationsUnitTestCase.java
@@ -148,6 +148,10 @@ public class DataSourceOperationsUnitTestCase {
         getModelControllerClient().execute(compensatingOperation);
     }
 
+    /**
+     * AS7-1202 test for enable datasource
+     * @throws Exception
+     */
     @Test
     public void testAddDisabledDsEnableItAndTestConnection() throws Exception {
 
@@ -294,6 +298,46 @@ public class DataSourceOperationsUnitTestCase {
 
             getModelControllerClient().execute(compensatingOperation);
         }
+    }
+
+    /**
+     * AS7-1206 test for jndi binding isn't unbound during remove if jndi name
+     * and data-source name are different
+     * @throws Exception
+     */
+    @Test
+    public void testAddAndRemoveNameAndJndiNameDifferent() throws Exception {
+        final String dsName = "DsName";
+        final String jndiDsName = "JndiDsName";
+
+        final ModelNode address = new ModelNode();
+        address.add("subsystem", "datasources");
+        address.add("data-source", dsName);
+        address.protect();
+
+        final ModelNode operation = new ModelNode();
+        operation.get(OP).set("add");
+        operation.get(OP_ADDR).set(address);
+
+        operation.get("name").set(dsName);
+        operation.get("jndi-name").set("java:jboss/datasources/" + jndiDsName);
+        operation.get("enabled").set(true);
+
+        operation.get("driver-name").set("h2");
+        operation.get("pool-name").set(dsName + "_Pool");
+
+        operation.get("connection-url").set("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        operation.get("user-name").set("sa");
+        operation.get("password").set("sa");
+
+        final ModelNode compensatingOperation = new ModelNode();
+        compensatingOperation.get(OP).set("remove");
+        compensatingOperation.get(OP_ADDR).set(address);
+        final ModelNode result = getModelControllerClient().execute(operation);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        final ModelNode compensatinResult = getModelControllerClient().execute(compensatingOperation);
+        Assert.assertEquals(SUCCESS, compensatinResult.get(OUTCOME).asString());
+
     }
 
     @Test
