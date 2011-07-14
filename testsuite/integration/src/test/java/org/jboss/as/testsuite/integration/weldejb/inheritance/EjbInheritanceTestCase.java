0c62e50f94dfa33ba413743a762c2328d4e1f5c8
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-07-14 13:13:35 +1000

    Work around WELD-921

--- testsuite/integration/src/test/java/org/jboss/as/testsuite/integration/weldejb/inheritance/EjbInheritanceTestCase.java
+++ testsuite/integration/src/test/java/org/jboss/as/testsuite/integration/weldejb/inheritance/EjbInheritanceTestCase.java
@@ -23,50 +23,48 @@ package org.jboss.as.testsuite.integration.weldejb.inheritance;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.testsuite.integration.common.HttpRequest;
-import org.jboss.as.testsuite.integration.weldejb.SimpleServlet;
-import org.jboss.as.testsuite.integration.weldejb.SimpleStatefulSessionBean;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.stdio.WriterOutputStream;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.ejb.ConcurrentAccessException;
 import javax.inject.Inject;
-import java.beans.XMLDecoder;
-import java.io.StringBufferInputStream;
-import java.net.MalformedURLException;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Tests that ejb's can be injected into an injection point of a type that does not exactly correlate
  * to one of their views.
  *
+ * WELD-921
+ * Also tests that EJB invocations on base class methods work
+ *
  */
 @RunWith(Arquillian.class)
-public class EjbInterfaceInheritanceTestCase {
+public class EjbInheritanceTestCase {
     @Deployment
     public static WebArchive deployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class, "inheritance-test.war")
-            .addPackage(EjbInterfaceInheritanceTestCase.class.getPackage())
+            .addPackage(EjbInheritanceTestCase.class.getPackage())
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
         return war;
     }
 
     @Inject
-    public Synchronizations sync;
+    private Synchronizations sync;
+
+    @Inject
+    private InjectingCDIBean injectingCDIBean;
 
     @Test
     public void testEjbCanBeInjectedViaNonViewInjectionPoint() {
         sync.register();
+    }
+
+    @Test
+    public void testCdiInjectedEjb() {
+        Assert.assertEquals("Hello", injectingCDIBean.sayHello());
+        Assert.assertEquals("Goodbye", injectingCDIBean.sayGoodbye());
     }
 
 
