07fbf5436db8f110876de215dac9d6ee626c04fc
kabir <kkhan@redhat.com>
2011-07-14 11:04:26 +0100

    Subsystem test harness module

---
+++ subsystem-test/src/main/java/org/jboss/as/subsystem/test/AdditionalInitialization.java
@@ -0,0 +1,40 @@
+package org.jboss.as.subsystem.test;
+
+import org.jboss.as.controller.ExtensionContext;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.as.controller.registry.Resource;
+import org.jboss.msc.service.ServiceTarget;
+
+/**
+ * Allows you to additionally initialize the service container and the model controller
+ *
+ * beyond the subsystem being tested
+ *
+ * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ */
+public interface AdditionalInitialization extends AdditionalParsers {
+
+    /**
+     * Allows easy initialization of commonly used parts of the model and invocation of associated boottime operations
+     *
+     * @param controllerInitializer the controller initializer
+     */
+    void setupController(ControllerInitializer controllerInitializer);
+
+    /**
+     * Adds extra services to the service controller
+     *
+     * @param target the service controller target
+     */
+    void addExtraServices(ServiceTarget target);
+
+    /**
+     * Allows extra initialization of the model and addition of extra subsystems
+     *
+     * @param extensionContext allows installation of extra subsystem extensions, call {@code Extension.initialize(extensionContext)} for each extra extension you have
+     * @param rootResource the root model resource which allows you to for example add child elements to the model
+     * @param rootRegistration the root resource registration which allows you to for example add additional operations to the model
+     */
+    void initializeExtraSubystemsAndModel(ExtensionContext context, Resource rootResource, ManagementResourceRegistration rootRegistration);
+
+}
---
+++ subsystem-test/src/test/java/org/jboss/as/subsystem/test/extrasubsystem/subsystem/dependency/DependencySubsystemAdd.java
@@ -0,0 +1,42 @@
+package org.jboss.as.subsystem.test.extrasubsystem.subsystem.dependency;
+
+import java.util.List;
+
+import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceController;
+
+/**
+ * Handler responsible for adding the subsystem resource to the model
+ *
+ * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ */
+class DependencySubsystemAdd extends AbstractBoottimeAddStepHandler {
+
+    static final DependencySubsystemAdd INSTANCE = new DependencySubsystemAdd();
+
+    private final Logger log = Logger.getLogger(DependencySubsystemAdd.class);
+
+    private DependencySubsystemAdd() {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
+        log.info("Populating the model");
+        model.setEmptyObject();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void performBoottime(OperationContext context, ModelNode operation, ModelNode model,
+            ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers)
+            throws OperationFailedException {
+        Dependency dependency = new Dependency();
+        context.getServiceTarget().addService(Dependency.NAME, dependency).install();
+    }
+}
