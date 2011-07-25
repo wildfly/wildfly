aa0c4d12d616ec937683b4dd6c31ee3451bfa2f0
Tomaz Cerar <tomaz.cerar@gmail.com>
2011-07-25 10:16:19 +0200

    initial work for java mail support AS7-1177

---
+++ mail/src/main/java/org/jboss/as/mail/extension/MailSubsystemAdd.java
@@ -0,0 +1,55 @@
+package org.jboss.as.mail.extension;
+
+import java.util.List;
+
+import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.as.server.AbstractDeploymentChainStep;
+import org.jboss.as.server.DeploymentProcessorTarget;
+import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+import org.jboss.msc.service.ServiceController;
+
+import org.jboss.as.mail.deployment.SubsystemDeploymentProcessor;
+
+/**
+ * Handler responsible for adding the subsystem resource to the model
+ *
+ * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ */
+class MailSubsystemAdd extends AbstractBoottimeAddStepHandler {
+
+    static final MailSubsystemAdd INSTANCE = new MailSubsystemAdd();
+
+    private final Logger log = Logger.getLogger(MailSubsystemAdd.class);
+
+    private MailSubsystemAdd() {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
+        log.info("Populating the model");
+        model.setEmptyObject();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void performBoottime(OperationContext context, ModelNode operation, ModelNode model,
+            ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers)
+            throws OperationFailedException {
+
+        //Add deployment processors here
+        //Remove this if you don't need to hook into the deployers, or you can add as many as you like
+        //see SubDeploymentProcessor for explanation of the phases
+        /*context.addStep(new AbstractDeploymentChainStep() {
+            public void execute(DeploymentProcessorTarget processorTarget) {
+                processorTarget.addDeploymentProcessor(SubsystemDeploymentProcessor.PHASE, SubsystemDeploymentProcessor.PRIORITY, new SubsystemDeploymentProcessor());
+
+            }
+        }, OperationContext.Stage.RUNTIME);*/
+
+    }
+}
