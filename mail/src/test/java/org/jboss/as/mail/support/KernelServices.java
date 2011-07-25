d7ceddd3da90b7733e24f649c8bc2cfac33f2a5c
Tomaz Cerar <tomaz.cerar@gmail.com>
2011-07-25 10:16:19 +0200

    initial work for java mail support AS7-1177

---
+++ mail/src/test/java/org/jboss/as/mail/support/KernelServices.java
@@ -0,0 +1,84 @@
+
+package org.jboss.as.mail.support;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_RESOURCE_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RECURSIVE;
+
+import java.util.concurrent.TimeUnit;
+
+import org.jboss.as.controller.ModelController;
+import org.jboss.as.controller.ModelController.OperationTransactionControl;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.ServiceContainer;
+
+import org.jboss.as.mail.support.AbstractParsingTest.StringConfigurationPersister;
+
+/**
+ * Allows access to the service container and the model controller
+ *
+ * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ */
+public class KernelServices {
+
+    private final ServiceContainer container;
+    private final ModelController controller;
+    private final StringConfigurationPersister persister;
+
+    KernelServices(ServiceContainer container, ModelController controller, StringConfigurationPersister persister) {
+        this.container = container;
+        this.controller = controller;
+        this.persister = persister;
+    }
+
+    /**
+     * Gets the service container
+     * @return the service container
+     */
+    public ServiceContainer getContainer() {
+        return container;
+    }
+
+    /**
+     * Execute an operation in the model controller
+     * @param operation the operation to execute
+     * @return the result of the operation
+     */
+    public ModelNode executeOperation(ModelNode operation) {
+        return controller.execute(operation, null, OperationTransactionControl.COMMIT, null);
+    }
+
+    /**
+     * Reads the persisted subsystem xml
+     * @return the xml
+     */
+    public String getPersistedSubsystemXml() {
+        return persister.marshalled;
+    }
+
+    /**
+     * Reads the whole model from the model controller
+     * @return the whole model
+     */
+    public ModelNode readWholeModel() {
+        ModelNode op = new ModelNode();
+        op.get(OP).set(READ_RESOURCE_OPERATION);
+        op.get(OP_ADDR).set(PathAddress.EMPTY_ADDRESS.toModelNode());
+        op.get(RECURSIVE).set(true);
+        ModelNode result = executeOperation(op);
+        return AbstractParsingTest.checkResultAndGetContents(result);
+    }
+
+    void shutdown() {
+        if (container != null) {
+            container.shutdown();
+            try {
+                container.awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
