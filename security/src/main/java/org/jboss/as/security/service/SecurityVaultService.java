4002d8b8959b64f4a139b5878f37c690787969d7
Anil Saldhana <anil.saldhana@jboss.com>
2011-08-24 21:40:31 -0500

    AS7-1622: vault for attribs

---
+++ security/src/main/java/org/jboss/as/security/service/SecurityVaultService.java
@@ -0,0 +1,54 @@
+package org.jboss.as.security.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jboss.as.security.SecurityExtension;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
+import org.jboss.security.vault.SecurityVault;
+import org.jboss.security.vault.SecurityVaultException;
+import org.jboss.security.vault.SecurityVaultFactory;
+
+/**
+ * A {@link SecurityVault} service
+ *
+ * @author Anil Saldhana
+ */
+public class SecurityVaultService implements Service<SecurityVault> {
+    public static final ServiceName SERVICE_NAME = SecurityExtension.JBOSS_SECURITY.append("vault");
+    protected volatile SecurityVault vault;
+    private String vaultClass;
+    private Map<String, Object> options = new HashMap<String, Object>();
+
+    public SecurityVaultService(String fqn, Map<String, Object> options) {
+        this.vaultClass = fqn;
+        this.options.putAll(options);
+    }
+
+    @Override
+    public SecurityVault getValue() throws IllegalStateException, IllegalArgumentException {
+        return vault;
+    }
+
+    @Override
+    public void start(StartContext context) throws StartException {
+        try {
+            if (vaultClass == null || vaultClass.isEmpty()) {
+                vault = SecurityVaultFactory.get();
+            } else {
+                vault = SecurityVaultFactory.get(vaultClass);
+            }
+            vault.init(options);
+        } catch (SecurityVaultException e) {
+            throw new StartException(e);
+        }
+    }
+
+    @Override
+    public void stop(StopContext context) {
+    }
+}
