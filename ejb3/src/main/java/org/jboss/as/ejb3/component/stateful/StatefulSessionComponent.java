3f07064f875f0bd0ce5ce4d762b7a3a513c82827
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-08-26 11:05:08 +1000

    Clean up, rmeove some unused classes

--- ejb3/src/main/java/org/jboss/as/ejb3/component/stateful/StatefulSessionComponent.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/stateful/StatefulSessionComponent.java
@@ -24,6 +24,7 @@ package org.jboss.as.ejb3.component.stateful;
 import org.jboss.as.ee.component.BasicComponentInstance;
 import org.jboss.as.ee.component.Component;
 import org.jboss.as.ejb3.cache.Cache;
+import org.jboss.as.ejb3.cache.ExpiringCache;
 import org.jboss.as.ejb3.cache.StatefulObjectFactory;
 import org.jboss.as.ejb3.component.EJBBusinessMethod;
 import org.jboss.as.ejb3.component.session.SessionBeanComponent;
