5f015a7006e3e7810d39faf276350ba4928af480
Alexey Loubyansky <olubyans@tstudio.(none)>
2011-09-22 19:22:49 +0200

    Revert "pass the correct address to the candidates provider, use an instance variable in the anonymous command argument to cache the arg name"
    This reverts commit 7fafacd0ccc1775fe16ea6d52359d81f029e470d.

--- cli/src/main/java/org/jboss/as/cli/operation/impl/DefaultOperationCandidatesProvider.java
+++ cli/src/main/java/org/jboss/as/cli/operation/impl/DefaultOperationCandidatesProvider.java
@@ -164,10 +164,9 @@ public class DefaultOperationCandidatesProvider implements OperationCandidatesPr
                 result = new ArrayList<CommandArgument>(names.size());
                 for(final String name : names) {
                     result.add(new CommandArgument(){
-                        final String argName = name;
                         @Override
                         public String getFullName() {
-                            return argName;
+                            return name;
                         }
 
                         @Override
@@ -181,8 +180,9 @@ public class DefaultOperationCandidatesProvider implements OperationCandidatesPr
                         }
 
                         @Override
-                        public boolean isPresent(ParsedCommandLine args) throws CommandFormatException {
-                            return args.hasProperty(argName);
+                        public boolean isPresent(ParsedCommandLine args)
+                                throws CommandFormatException {
+                            return args.hasProperty(name);
                         }
 
                         @Override
@@ -192,15 +192,15 @@ public class DefaultOperationCandidatesProvider implements OperationCandidatesPr
 
                         @Override
                         public String getValue(ParsedCommandLine args) throws CommandFormatException {
-                            return args.getPropertyValue(argName);
+                            return args.getPropertyValue(name);
                         }
 
                         @Override
                         public String getValue(ParsedCommandLine args, boolean required) throws CommandFormatException {
                             if(!isPresent(args)) {
-                                throw new CommandFormatException("Property '" + argName + "' is missing required value.");
+                                throw new CommandFormatException("Property '" + name + "' is missing required value.");
                             }
-                            return args.getPropertyValue(argName);
+                            return args.getPropertyValue(name);
                         }
 
                         @Override
@@ -208,7 +208,7 @@ public class DefaultOperationCandidatesProvider implements OperationCandidatesPr
                             if(!isPresent(args)) {
                                 return false;
                             }
-                            if(argName.equals(args.getLastParsedPropertyName())) {
+                            if(name.equals(args.getLastParsedPropertyName())) {
                                 return false;
                             }
                             return true;
