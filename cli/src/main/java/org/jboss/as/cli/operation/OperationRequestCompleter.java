cfd87e8a2dfac62f085e2c419ef234fc1f20f06a
Alexey Loubyansky <olubyans@tstudio.(none)>
2011-09-22 19:22:49 +0200

    Revert "pass the correct address to the candidates provider, use an instance variable in the anonymous command argument to cache the arg name"
    This reverts commit 7fafacd0ccc1775fe16ea6d52359d81f029e470d.

--- cli/src/main/java/org/jboss/as/cli/operation/OperationRequestCompleter.java
+++ cli/src/main/java/org/jboss/as/cli/operation/OperationRequestCompleter.java
@@ -78,7 +78,7 @@ public class OperationRequestCompleter implements CommandLineCompleter {
 
         if (parsedCmd.hasProperties() || parsedCmd.endsOnPropertyListStart()) {
 
-            final List<CommandArgument> allArgs = candidatesProvider.getProperties(ctx, parsedCmd.getOperationName(), parsedCmd.getAddress());
+            final List<CommandArgument> allArgs = candidatesProvider.getProperties(ctx, parsedCmd.getOperationName(), ctx.getPrefix());
             if (allArgs.isEmpty()) {
                 final CommandLineFormat format = parsedCmd.getFormat();
                 if(format != null && format.getPropertyListEnd() != null) {
