8b9cd1d2f239e6ef7ae51778bdcd83d0bc792c5b
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-10-13 15:51:22 +1100

    Re-align with new testsuite structure after rebase

---
+++ testsuite/integration/src/test/java/org/jboss/as/test/integration/ejb/home/remotehome/injection/RemoteHomeInjectionTestCase.java
@@ -0,0 +1,65 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.home.remotehome.injection;
+
+import javax.naming.InitialContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.test.integration.ejb.home.remotehome.SimpleInterface;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests the
+ */
+@RunWith(Arquillian.class)
+public class RemoteHomeInjectionTestCase {
+
+    private static final String ARCHIVE_NAME = "SimpleLocalHomeTest.war";
+
+
+    @ArquillianResource
+    private InitialContext iniCtx;
+
+    @Deployment
+    public static Archive<?> deploy() {
+
+        WebArchive war = ShrinkWrap.create(WebArchive.class, ARCHIVE_NAME);
+        war.addPackage(RemoteHomeInjectionTestCase.class.getPackage());
+        war.addPackage(SimpleInterface.class.getPackage());
+        return war;
+    }
+
+    @Test
+    public void testRemoteHomeInjection() throws Exception {
+        final InjectingEjb ejb = (InjectingEjb) iniCtx.lookup("java:module/" + InjectingEjb.class.getSimpleName());
+        Assert.assertEquals("OtherEjb", ejb.getMessage());
+    }
+
+}
