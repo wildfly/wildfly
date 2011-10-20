c9ecaa1572523e46373555c51a267e5a07a99412
bstansberry@jboss.com <brian.stansberry@redhat.com>
2011-10-19 21:05:53 -0500

    Add JIRA to @Ignore message

--- testsuite/integration/src/test/java/org/jboss/as/test/integration/ejb/timerservice/persistence/TimerServicePersistenceSecondTestCase.java
+++ testsuite/integration/src/test/java/org/jboss/as/test/integration/ejb/timerservice/persistence/TimerServicePersistenceSecondTestCase.java
@@ -54,7 +54,7 @@ public class TimerServicePersistenceSecondTestCase {
      * The timer should be restored and the method should timeout, even without setting up the timer in this deployment
      */
     @Test
-    @Ignore("Failing")
+    @Ignore("AS7-2168 -- Disabled because failing")
     public void testTimerServiceCalled() throws NamingException {
         InitialContext ctx = new InitialContext();
         Assert.assertTrue(SimpleTimerServiceBean.awaitTimerCall());
