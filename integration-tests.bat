647277c38c420e50004e9a038beb1983c1ab1d56
Ondrej Zizka <zizka@seznam.cz>
2011-10-24 01:06:09 +0200

    Update integration-tests.bat

--- integration-tests.bat
+++ integration-tests.bat
@@ -60,7 +60,7 @@ goto :EOF
 
 
 REM ******************************************************
-REM ************ Test if Maven Batch file exists ***********
+REM ***  Test if Maven batch file exists.             ***
 REM ******************************************************
 
 :testIfExists
@@ -70,7 +70,7 @@ goto :EOF
 
 
 REM ******************************************************
-REM ************** Batch file has been found *************
+REM ***  Batch file has been found.                    ***
 REM ******************************************************
 
 :BatchFound
@@ -80,32 +80,32 @@ set EXECUTED=TRUE
 goto :EOF
 
 REM ******************************************************
-REM ************* Execute Batch file only once ***********
+REM ***  Execute batch file only once.                 ***
 REM ******************************************************
 
 :ExecuteBatch
 
-REM Support for testsuite profile processing
+REM  Support for testsuite profile processing
 set CMD_LINE_PARAMS=
 set TESTS_SPECIFIED=N
 
-REM each test module executes a different type of test
-set API_TESTS=-Dapi.module
+REM  Each test module executes a different type of test
 set BENCHMARK_TESTS=-Dbenchmark.module
 set INTEGRATION_TESTS=-Dintegration.module -Dbasic.integration.tests -Dcompat.integration.tests -Dclustering.integration.tests -Dtimerservice.integration.tests
 set SMOKE_TESTS=-Dintegration.module -Dsmoke.integration.tests
-set SPEC_TESTS=-Dspec.module
 set STRESS_TESTS=-Dstress.module
 set DOMAIN_TESTS=-Ddomain.module
+set COMPAT_TESTS=-Dcompat.module
+
 
 set MVN=%1%
 set GOAL=%2
 if "%GOAL%"=="" set GOAL=install
 
-REM process test directives before calling maven
+REM  Process test directives before calling maven
 call :processTestDirectives %GOAL% %3 %4 %5 %6 %7 %8
 
-REM change to testsuite directory before executing mvn
+REM  Change to testsuite directory before executing mvn.
 cd %DIRNAME%\testsuite
 
 echo Calling ..\%MVN% %CMD_LINE_PARAMS%
@@ -113,50 +113,62 @@ call ..\%MVN% %CMD_LINE_PARAMS%
 
 cd %DIRNAME%
 
-REM Pause the batch script when maven terminates
+REM  Pause the batch script when maven terminates.
 if "%NOPAUSE%" == "" pause
 
 goto :EOF
 
 REM ******************************************************
-REM **** Function to process testsuite directives ********
+REM ***  Function to process testsuite directives.     ***
 REM ******************************************************
 :processTestDirectives
 
 REM echo "Calling processTestDirectives %*"
 :loop
 
-REM check if we have no more parameters to process
+REM  Check if we have no more parameters to process.
 if "%1" == "" (
   if "%TESTS_SPECIFIED%" == "N" set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %SMOKE_TESTS%"
   goto :eof
 )
-REM Replace occurrences of directives with corresponding maven profiles
-REM -DallTests
+REM  Replace occurrences of directives with corresponding maven profiles
+REM  -DallTests
 if "%1" == "-DallTests" (
   set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %INTEGRATION_TESTS% %SPEC_TESTS% %API_TESTS% %DOMAIN_TESTS%"
   set "TESTS_SPECIFIED=Y"
   goto processed
 )
-REM -Dbenchmark-tests
+REM  -Dbenchmark-tests
 if "%1" == "-Dbenchmark-tests" (
   set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %BENCHMARK_TESTS%"
   set "TESTS_SPECIFIED=Y"
   goto processed
 )
-REM -Dsmoke-tests
+REM  -Ddomain-tests
+if "%1" == "-Ddomain-tests" (
+  set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %DOMAIN_TESTS%"
+  set "TESTS_SPECIFIED=Y"
+  goto processed
+)
+REM  -Dcompat-tests
+if "%1" == "-Dcompat-tests" (
+  set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %COMPAT_TESTS%"
+  set "TESTS_SPECIFIED=Y"
+  goto processed
+)
+REM  -Dsmoke-tests
 if "%1" == "-Dsmoke-tests" (
   set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %SMOKE_TESTS%"
   set "TESTS_SPECIFIED=Y"
   goto processed
 )
-REM -Dstress-tests
+REM  -Dstress-tests
 if "%1" == "-Dstress-tests" (
   set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %STRESS_TESTS%"
   set "TESTS_SPECIFIED=Y"
   goto processed
 )
-REM pass through other params
+REM  Pass through other params.
 set "CMD_LINE_PARAMS=%CMD_LINE_PARAMS% %1"
 
 :processed
