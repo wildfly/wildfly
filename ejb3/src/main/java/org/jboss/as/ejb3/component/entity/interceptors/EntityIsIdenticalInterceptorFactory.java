518caa0edd8dae9bfee14d9c9ccab475cbb95588
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-10-25 09:56:40 +1100

    Fix isIdentical bug

--- ejb3/src/main/java/org/jboss/as/ejb3/component/entity/interceptors/EntityIsIdenticalInterceptorFactory.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/entity/interceptors/EntityIsIdenticalInterceptorFactory.java
@@ -1,8 +1,8 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat Inc., and individual contributors as indicated
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -21,15 +21,8 @@
  */
 package org.jboss.as.ejb3.component.entity.interceptors;
 
-import java.rmi.RemoteException;
-
-import javax.ejb.EJBException;
-import javax.ejb.EJBHome;
-import javax.ejb.EJBLocalHome;
 import javax.ejb.EJBLocalObject;
 import javax.ejb.EJBObject;
-import javax.ejb.Handle;
-import javax.ejb.RemoveException;
 
 import org.jboss.as.ee.component.ComponentView;
 import org.jboss.as.ejb3.component.entity.EntityBeanComponent;
@@ -70,69 +63,11 @@ public class EntityIsIdenticalInterceptorFactory implements InterceptorFactory {
         public Object processInvocation(final InterceptorContext context) throws Exception {
             final Object primaryKey = context.getPrivateData(EntityBeanComponent.PRIMARY_KEY_CONTEXT_KEY);
             final Object other = context.getParameters()[0];
-            final Class<?> proxyType = componentView.getProxyClass();
-            if (proxyType.isAssignableFrom(other.getClass())) {
-                //now we know that this is an ejb for the correct component view
-                //as digging out the session id from the proxy object is not really
-                //a viable option, we invoke equals() for the other instance with a
-                //SessionIdHolder as the other side
-                if (other instanceof EJBLocalObject) {
-                    return ((EJBLocalObject) other).isIdentical(new SessionIdHolder(primaryKey));
-                } else if (other instanceof EJBObject) {
-                    return ((EJBObject) other).isIdentical(new SessionIdHolder(primaryKey));
-                } else {
-                    throw new RuntimeException(getClass() + " was attached to a view that is not an EJBObject or a EJBLocalObject");
-                }
-            } else if (other instanceof SessionIdHolder) {
-                return primaryKey.equals(((SessionIdHolder) other).primaryKey);
+            if (context.getMethod().getParameterTypes()[0].equals(EJBLocalObject.class)) {
+                return ((EJBLocalObject) other).getPrimaryKey().equals(primaryKey);
             } else {
-                return false;
+                return ((EJBObject) other).getPrimaryKey().equals(primaryKey);
             }
         }
     }
-
-
-    private static class SessionIdHolder implements EJBLocalObject, EJBObject {
-        private final Object primaryKey;
-
-        public SessionIdHolder(final Object primaryKey) {
-            this.primaryKey = primaryKey;
-        }
-
-        @Override
-        public EJBLocalHome getEJBLocalHome() throws EJBException {
-            return null;
-        }
-
-        @Override
-        public EJBHome getEJBHome() throws RemoteException {
-            return null;
-        }
-
-        @Override
-        public Object getPrimaryKey() throws EJBException {
-            return null;
-        }
-
-        @Override
-        public void remove() throws RemoveException, EJBException {
-
-        }
-
-        @Override
-        public Handle getHandle() throws RemoteException {
-            return null;
-        }
-
-        @Override
-        public boolean isIdentical(final EJBObject ejbo) throws RemoteException {
-            return false;
-        }
-
-        @Override
-        public boolean isIdentical(final EJBLocalObject obj) throws EJBException {
-            return false;
-        }
-    }
-
 }
