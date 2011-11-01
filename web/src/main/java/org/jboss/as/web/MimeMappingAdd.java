a1feb704a58a60349e9b436061e3ca0a38689ef1
jfclere <jfclere@gmail.com>
2011-11-01 09:39:11 +0100

    Simplify the Add and Remove mime.

--- web/src/main/java/org/jboss/as/web/MimeMappingAdd.java
+++ web/src/main/java/org/jboss/as/web/MimeMappingAdd.java
@@ -32,12 +32,9 @@ import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.descriptions.DescriptionProvider;
 import org.jboss.dmr.ModelNode;
-import org.jboss.logging.Logger;
 
 // implements ModelQueryOperationHandler, DescriptionProvider
 public class MimeMappingAdd implements OperationStepHandler, DescriptionProvider{
-
-    private static final Logger log = Logger.getLogger("org.jboss.as.web");
 
     static final MimeMappingAdd INSTANCE = new MimeMappingAdd();
 
@@ -49,18 +46,12 @@ public class MimeMappingAdd implements OperationStepHandler, DescriptionProvider
     @Override
     public void execute(OperationContext context, ModelNode operation)
             throws OperationFailedException {
-        if (context.getType() == OperationContext.Type.SERVER) {
-            context.addStep(new OperationStepHandler() {
-                @Override
-                public void execute(OperationContext context, ModelNode operation) throws OperationFailedException {
-                    final ModelNode mimetypes = context.readResourceForUpdate(PathAddress.EMPTY_ADDRESS).getModel().get(MIME_MAPPING);
-                    if (operation.hasDefined("name") && operation.hasDefined("value")) {
-                        mimetypes.get(operation.get("name").asString()).set(operation.get("value").asString());
-                    } else
-                        throw new OperationFailedException(new ModelNode().set("name and value are needed for add-mime"));
-                    context.completeStep();
-                }
-            }, OperationContext.Stage.MODEL);
+        if (context.getType() == OperationContext.Type.SERVER || context.getType() == OperationContext.Type.HOST) {
+            final ModelNode mimetypes = context.readResourceForUpdate(PathAddress.EMPTY_ADDRESS).getModel().get(MIME_MAPPING);
+            if (operation.hasDefined("name") && operation.hasDefined("value")) {
+                mimetypes.get(operation.get("name").asString()).set(operation.get("value").asString());
+            } else
+                throw new OperationFailedException(new ModelNode().set("name and value are needed for add-mime"));
         }
 
         context.completeStep();
