4ba0b9ea285ac31e563ff37a08a7f8ff43944021
Ondrej Chaloupka <ochaloup@redhat.com>
2011-11-07 18:08:51 +0100

    [JBQA-5451] EJBTHREE->AS7: ejbthree1945 (RunasPrincipal annotation)

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/runasprincipal/Caller.java
@@ -0,0 +1,45 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright (c) 2010, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.ejb.security.runasprincipal;
+
+import org.jboss.ejb3.annotation.SecurityDomain;
+
+import javax.annotation.security.RunAs;
+import javax.ejb.EJB;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+/**
+ * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
+ */
+@Stateless
+@Remote(WhoAmI.class)
+@RunAs("Admin")
+@SecurityDomain("other")
+public class Caller implements WhoAmI {
+    @EJB(beanName = "StatelessBBean")
+    private WhoAmI beanB;
+
+    public String getCallerPrincipal() {
+        return beanB.getCallerPrincipal();
+    }
+}
