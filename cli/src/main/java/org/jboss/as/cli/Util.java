05e58cda1fb25f26b6eda9253c54a68fa64bf0c6
Alexey Loubyansky <olubyans@tstudio.(none)>
2011-11-17 19:59:30 +0100

    don't exclude --profile argument from request properties for generic commands whose node path doesn't depend on profile, fixed header names for ls -l tables, use constants instead of String values

--- cli/src/main/java/org/jboss/as/cli/Util.java
+++ cli/src/main/java/org/jboss/as/cli/Util.java
@@ -88,6 +88,7 @@ public class Util {
     public static final String TAIL_COMMENT_ALLOWED = "tail-comment-allowed";
     public static final String TRUE = "true";
     public static final String TYPE = "type";
+    public static final String VALIDATE_ADDRESS = "validate-address";
     public static final String VALUE = "value";
     public static final String WRITE_ATTRIBUTE = "write-attribute";
 
