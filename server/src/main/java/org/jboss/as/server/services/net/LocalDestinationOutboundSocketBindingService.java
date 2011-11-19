1f3d0be227cd839e79538cd990b08d2b27c46d15
jaikiran <jaikiran.pai@gmail.com>
2011-11-19 12:54:04 +0530

    AS7-2709 Do not try to resolve destination address of outbound socket binding(s) until the dependent resource asks for

--- server/src/main/java/org/jboss/as/server/services/net/LocalDestinationOutboundSocketBindingService.java
+++ server/src/main/java/org/jboss/as/server/services/net/LocalDestinationOutboundSocketBindingService.java
@@ -22,12 +22,13 @@
 
 package org.jboss.as.server.services.net;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import org.jboss.as.network.OutboundSocketBinding;
 import org.jboss.as.network.SocketBinding;
 import org.jboss.msc.inject.Injector;
 import org.jboss.msc.value.InjectedValue;
-
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 
 /**
  * Service that represents a local-destination outbound socket binding
@@ -43,24 +44,35 @@ public class LocalDestinationOutboundSocketBindingService extends OutboundSocket
         super(name, sourcePort, fixedSourcePort);
     }
 
+    synchronized Injector<SocketBinding> getLocalDestinationSocketBindingInjector() {
+        return this.localDestinationSocketBindingInjectedValue;
+    }
+
     @Override
-    protected synchronized InetAddress getDestinationAddress() {
+    protected OutboundSocketBinding createOutboundSocketBinding() {
+        // unlike the RemoteDestinationOutboundSocketBindingService, we resolve the destination address
+        // here itself (instead of doing it lazily in the OutboundSocketBinding) is because we already
+        // inject a SocketBinding reference which is local to the server instance and is expected to have
+        // already resolved the (local) address (via the NetworkInterfaceBinding).
+        final InetAddress destinationAddress = this.getDestinationAddress();
+        final int destinationPort = this.getDestinationPort();
+        return new OutboundSocketBinding(this.outboundSocketName, this.socketBindingManagerInjectedValue.getValue(),
+                destinationAddress, destinationPort, this.sourceInterfaceInjectedValue.getOptionalValue(), this.sourcePort,
+                this.fixedSourcePort);
+    }
+
+    private InetAddress getDestinationAddress() {
         final SocketBinding localDestinationSocketBinding = this.localDestinationSocketBindingInjectedValue.getValue();
         return localDestinationSocketBinding.getSocketAddress().getAddress();
     }
 
-    @Override
-    protected synchronized int getDestinationPort() {
+    private int getDestinationPort() {
         final SocketBinding localDestinationSocketBinding = this.localDestinationSocketBindingInjectedValue.getValue();
         // instead of directly using SocketBinding.getPort(), we go via the SocketBinding.getSocketAddress()
         // since the getPort() method doesn't take into account whether the port is a fixed port or whether a offset
         // needs to be added. Alternatively, we could introduce a getAbsolutePort() in the SocketBinding class.
         final InetSocketAddress socketAddress = localDestinationSocketBinding.getSocketAddress();
         return socketAddress.getPort();
-    }
-
-    synchronized Injector<SocketBinding> getLocalDestinationSocketBindingInjector() {
-        return this.localDestinationSocketBindingInjectedValue;
     }
 
 }
