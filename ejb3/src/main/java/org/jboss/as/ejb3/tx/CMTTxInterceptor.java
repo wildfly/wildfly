d37083f2f9e29bcd18f3bfd6b7600f55899c2797
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-11-22 15:46:43 +1100

    Pass NoSuchEjbException back through the interceptor

--- ejb3/src/main/java/org/jboss/as/ejb3/tx/CMTTxInterceptor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/tx/CMTTxInterceptor.java
@@ -27,6 +27,7 @@ import java.util.Random;
 import javax.ejb.EJBException;
 import javax.ejb.EJBTransactionRequiredException;
 import javax.ejb.EJBTransactionRolledbackException;
+import javax.ejb.NoSuchEJBException;
 import javax.ejb.TransactionAttributeType;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
@@ -129,9 +130,10 @@ public class CMTTxInterceptor implements Interceptor {
                 Throwable cause = t;
                 t = new EJBTransactionRolledbackException("Unexpected Error");
                 t.initCause(cause);
-            }
-            // If this is an EJBException, pass through to the caller
-            else if (t instanceof RuntimeException) {
+            } else if (t instanceof NoSuchEJBException ) {
+                // If this is an NoSuchEJBException, pass through to the caller
+
+            } else if (t instanceof RuntimeException) {
                 t = new EJBTransactionRolledbackException(t.getMessage(), (Exception) t);
             } else {// application exception
                 throw (Exception) t;
