a040c4d87e7c09aa45d8644a19ae12875e40ea97
Stuart Douglas <stuart.w.douglas@gmail.com>
2011-11-22 11:01:54 +1100

    Fix async merging

--- ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/merging/AsynchronousMergingProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/merging/AsynchronousMergingProcessor.java
@@ -22,6 +22,7 @@
 package org.jboss.as.ejb3.deployment.processors.merging;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -99,8 +100,10 @@ public class AsynchronousMergingProcessor extends AbstractMergingProcessor<Sessi
                 final AsyncMethodsMetaData asyn = sessionBeanData.getAsyncMethods();
                 if (asyn != null) {
                     for (AsyncMethodMetaData method : asyn) {
-                        final Method m = MethodResolutionUtils.resolveMethod(method.getMethodName(), method.getMethodParams(), componentClass, deploymentReflectionIndex);
-                        description.addAsynchronousMethod(MethodIdentifier.getIdentifierForMethod(m));
+                        final Collection<Method> methods = MethodResolutionUtils.resolveMethods(method.getMethodName(), method.getMethodParams(), componentClass, deploymentReflectionIndex);
+                        for(final Method m : methods ) {
+                            description.addAsynchronousMethod(MethodIdentifier.getIdentifierForMethod(m));
+                        }
                     }
                 }
             }
