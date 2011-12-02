bffe499da073dab90b00c75fcae011799de50b46
Ondrej Zizka <zizka@seznam.cz>
2011-12-02 03:44:54 +0100

    Added -fae to -DallTests in integration-tests.sh

--- integration-tests.sh
+++ integration-tests.sh
@@ -22,30 +22,25 @@ MAVEN_HOME=""
 MAVEN_OPTS="$MAVEN_OPTS -Xmx512M"
 export MAVEN_OPTS
 
-# the default search path for maven
+# The default search path for Maven.
 MAVEN_SEARCH_PATH="\
     tools
     tools/maven \
     tools/apache/maven \
     maven"
 
-# the default arguments
+# The default arguments.
 MVN_OPTIONS="-s ../tools/maven/conf/settings.xml"
 
-# Use the maximum available, or set MAX_FD != -1 to use that
+# Use the maximum available, or set MAX_FD != -1 to use that.
 MAX_FD="maximum"
 
 # OS specific support (must be 'true' or 'false').
 cygwin=false;
 darwin=false;
 case "`uname`" in
-    CYGWIN*)
-        cygwin=true
-        ;;
-
-    Darwin*)
-        darwin=true
-        ;;
+    CYGWIN*)  cygwin=true;;
+    Darwin*)  darwin=true;;
 esac
 
 
@@ -68,14 +63,13 @@ TESTS_SPECIFIED="N"
 #
 process_test_directives() {
 
-  MVN_GOALS="";
+    MVN_GOALS="";
 
-  # For each parameter, check for testsuite directives.
-  for param in $@
-  do
+    # For each parameter, check for testsuite directives.
+    for param in $@ ; do
     case $param in
       ## -DallTests runs all tests except benchmark and stress.
-      -DallTests)        TESTS_SPECIFIED="Y";  CMD_LINE_PARAMS="$CMD_LINE_PARAMS -DallTests";;
+      -DallTests)        TESTS_SPECIFIED="Y";  CMD_LINE_PARAMS="$CMD_LINE_PARAMS -DallTests -fae";;
 
       -Dinteg-tests)     TESTS_SPECIFIED="Y";  CMD_LINE_PARAMS="$CMD_LINE_PARAMS $INTEGRATION_TESTS";;
       -Dcluster-tests)   TESTS_SPECIFIED="Y";  CMD_LINE_PARAMS="$CMD_LINE_PARAMS $CLUSTER_TESTS";;
@@ -97,16 +91,16 @@ process_test_directives() {
       ## Pass through all other params.
       *)      CMD_LINE_PARAMS="$CMD_LINE_PARAMS $param";;
     esac
-  done
+    done
 
-  #  Default goal if none specified.
-  if [ -z "$MVN_GOALS" ]; then MVN_GOALS="install"; fi
-  CMD_LINE_PARAMS="$MVN_GOALS $CMD_LINE_PARAMS";
+    #  Default goal if none specified.
+    if [ -z "$MVN_GOALS" ]; then MVN_GOALS="install"; fi
+    CMD_LINE_PARAMS="$MVN_GOALS $CMD_LINE_PARAMS";
 
-  # If no tests specified, run smoke tests.
-  if [[ $TESTS_SPECIFIED == "N" ]]; then
-    CMD_LINE_PARAMS="$CMD_LINE_PARAMS $SMOKE_TESTS"
-  fi
+    # If no tests specified, run smoke tests.
+    if [[ $TESTS_SPECIFIED == "N" ]]; then
+        CMD_LINE_PARAMS="$CMD_LINE_PARAMS $SMOKE_TESTS"
+    fi
 }
 
 #
@@ -129,22 +123,22 @@ warn() {
 #
 maybe_source() {
     for file in $*; do
-	if [ -f "$file" ]; then
-	    . $file
-	fi
+        if [ -f "$file" ]; then
+            . $file
+        fi
     done
 }
 
-search() {
+find_maven() {
     search="$*"
     for d in $search; do
-	MAVEN_HOME="`pwd`/$d"
-	MVN="$MAVEN_HOME/bin/mvn"
-	if [ -x "$MVN" ]; then
-	    # found one
-	    echo $MAVEN_HOME
-	    break
-	fi
+        MAVEN_HOME="`pwd`/$d"
+        MVN="$MAVEN_HOME/bin/mvn"
+        if [ -x "$MVN" ]; then
+            # found one
+            echo $MAVEN_HOME
+            break
+        fi
     done
 }
 
@@ -152,89 +146,89 @@ search() {
 # Main function.
 #
 main() {
-    # if there is a build config file. then source it
+    #  If there is a build config file. then source it.
     maybe_source "$DIRNAME/build.conf" "$HOME/.build.conf"
 
-    # Increase the maximum file descriptors if we can
+    #  Increase the maximum file descriptors if we can.
     if [ $cygwin = "false" ]; then
-	MAX_FD_LIMIT=`ulimit -H -n`
-	if [ $? -eq 0 ]; then
-	    if [ "$MAX_FD" = "maximum" -o "$MAX_FD" = "max" ]; then
-		# use the system max
-		MAX_FD="$MAX_FD_LIMIT"
-	    fi
+        MAX_FD_LIMIT=`ulimit -H -n`
+        if [ $? -eq 0 ]; then
+            if [ "$MAX_FD" = "maximum" -o "$MAX_FD" = "max" ]; then
+                #  Use the system max.
+                MAX_FD="$MAX_FD_LIMIT"
+            fi
 
-	    ulimit -n $MAX_FD
-	    if [ $? -ne 0 ]; then
-		warn "Could not set maximum file descriptor limit: $MAX_FD"
-	    fi
-	else
-	    warn "Could not query system maximum file descriptor limit: $MAX_FD_LIMIT"
-	fi
+            ulimit -n $MAX_FD
+            if [ $? -ne 0 ]; then
+                warn "Could not set maximum file descriptor limit: $MAX_FD"
+            fi
+        else
+            warn "Could not query system maximum file descriptor limit: $MAX_FD_LIMIT"
+        fi
     fi
 
-    # try the search path
-    MAVEN_HOME=`search $MAVEN_SEARCH_PATH`
+    #  Try the search path.
+    MAVEN_HOME=`find_maven $MAVEN_SEARCH_PATH`
 
-    # try looking up to root
+    #  Try looking up to root.
     if [ "x$MAVEN_HOME" = "x" ]; then
-	target="build"
-	_cwd=`pwd`
+        target="build"
+        _cwd=`pwd`
 
-	while [ "x$MAVEN_HOME" = "x" ] && [ "$cwd" != "$ROOT" ]; do
-	    cd ..
-	    cwd=`pwd`
-	    MAVEN_HOME=`search $MAVEN_SEARCH_PATH`
-	done
+        while [ "x$MAVEN_HOME" = "x" ] && [ "$cwd" != "$ROOT" ]; do
+            cd ..
+            cwd=`pwd`
+            MAVEN_HOME=`search $MAVEN_SEARCH_PATH`
+        done
 
-	# make sure we get back
-	cd $_cwd
+        #  Make sure we get back.
+        cd $_cwd
 
-	if [ "$cwd" != "$ROOT" ]; then
-	    found="true"
-	fi
+        if [ "$cwd" != "$ROOT" ]; then
+            found="true"
+        fi
 
-	# complain if we did not find anything
-	if [ "$found" != "true" ]; then
-	    die "Could not locate Maven; check \$MVN or \$MAVEN_HOME."
-	fi
+        #  Complain if we did not find anything.
+        if [ "$found" != "true" ]; then
+            die "Could not locate Maven; check \$MVN or \$MAVEN_HOME."
+        fi
     fi
 
-    # make sure we have one
+    #  Make sure we have one.
     MVN=$MAVEN_HOME/bin/mvn
     if [ ! -x "$MVN" ]; then
-	die "Maven file is not executable: $MVN"
+        die "Maven file is not executable: $MVN"
     fi
 
-    # need to specify planet57/buildmagic protocol handler package
+    #  Need to specify planet57/buildmagic protocol handler package.
     MVN_OPTS="-Djava.protocol.handler.pkgs=org.jboss.net.protocol"
 
-    # setup some build properties
+    #  Setup some build properties.
     MVN_OPTS="$MVN_OPTS -Dbuild.script=$0"
 
-    # change to the directory where the script lives so users are not forced
-    # to be in the same directory as build.xml
+    #  Change to the directory where the script lives 
+    #  so users are not forced to be in the same directory as build.xml.
     cd $DIRNAME/testsuite
 
     MVN_GOAL=$@
     if [ -z "$MVN_GOAL" ]; then
-      MVN_GOAL="install"
+        MVN_GOAL="install"
     fi
 
-    # process test directives before calling maven
+    #  Process test directives before calling maven.
     process_test_directives $MVN_GOAL
     MVN_GOAL=$CMD_LINE_PARAMS
 
-    # export some stuff for maven
+    # Export some stuff for maven.
     export MVN MAVEN_HOME MVN_OPTS MVN_GOAL
 
     echo "$MVN $MVN_OPTIONS $MVN_GOAL"
 
-    # execute in debug mode, or simply execute
+    #  Execute in debug mode, or simply execute.
     if [ "x$MVN_DEBUG" != "x" ]; then
-	  /bin/sh -x $MVN $MVN_OPTIONS $MVN_GOAL
+        /bin/sh -x $MVN $MVN_OPTIONS $MVN_GOAL
     else
-	  exec $MVN $MVN_OPTIONS $MVN_GOAL
+        exec $MVN $MVN_OPTIONS $MVN_GOAL
     fi
 
     cd $DIRNAME
