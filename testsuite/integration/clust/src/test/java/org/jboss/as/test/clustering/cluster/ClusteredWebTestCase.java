b9c0d31c3c2d6d6e91791308db92fd6a1cb5db43
Radoslav Husar <radosoft@gmail.com>
2011-12-02 19:23:27 +0100

    fix typo in a clusered test expecting a session to be not serialized

--- testsuite/integration/clust/src/test/java/org/jboss/as/test/clustering/cluster/ClusteredWebTestCase.java
+++ testsuite/integration/clust/src/test/java/org/jboss/as/test/clustering/cluster/ClusteredWebTestCase.java
@@ -107,7 +107,7 @@ public class ClusteredWebTestCase {
             Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
             Assert.assertEquals(2, Integer.parseInt(response.getFirstHeader("value").getValue()));
             // This won't be true unless we have somewhere to which to replicate
-            Assert.assertFalse(Boolean.valueOf(response.getFirstHeader("serialized").getValue()));
+            Assert.assertTrue(Boolean.valueOf(response.getFirstHeader("serialized").getValue()));
             response.getEntity().getContent().close();
         } finally {
             client.getConnectionManager().shutdown();
