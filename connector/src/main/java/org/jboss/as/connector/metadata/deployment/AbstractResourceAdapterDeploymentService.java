096089848eb8ec45fdf77ce59036ca98efc424aa
Jesper Pedersen <jesper.pedersen@redhat.com>
2011-12-06 11:57:29 -0500

    [AS7-2222] Fake javax.resource.Referenceable value

--- connector/src/main/java/org/jboss/as/connector/metadata/deployment/AbstractResourceAdapterDeploymentService.java
+++ connector/src/main/java/org/jboss/as/connector/metadata/deployment/AbstractResourceAdapterDeploymentService.java
@@ -58,6 +58,7 @@ import org.jboss.msc.service.StopContext;
 import org.jboss.msc.value.InjectedValue;
 import org.jboss.security.SubjectFactory;
 
+import javax.naming.Reference;
 import javax.resource.spi.IllegalStateException;
 import javax.resource.spi.ResourceAdapter;
 import javax.transaction.TransactionManager;
@@ -295,6 +296,11 @@ public abstract class AbstractResourceAdapterDeploymentService {
                         }
                     }).setInitialMode(ServiceController.Mode.ACTIVE).install();
 
+            // AS7-2222: Just hack it
+            if (cf instanceof javax.resource.Referenceable) {
+                ((javax.resource.Referenceable)cf).setReference(new Reference(jndi));
+            }
+
             return new String[] { jndi };
         }
 
@@ -348,6 +354,11 @@ public abstract class AbstractResourceAdapterDeploymentService {
                             }
                         }
                     }).setInitialMode(ServiceController.Mode.ACTIVE).install();
+
+            // AS7-2222: Just hack it
+            if (ao instanceof javax.resource.Referenceable) {
+                ((javax.resource.Referenceable)ao).setReference(new Reference(jndi));
+            }
 
             return new String[] { jndi };
         }
