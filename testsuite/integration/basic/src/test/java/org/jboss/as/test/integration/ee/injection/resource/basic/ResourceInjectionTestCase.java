949d34d90fe737b7cceee5256f8e33c7f2e6bf9c
Ondrej Chaloupka <ochaloup@redhat.com>
2011-12-06 14:07:12 +0100

    [JBQA-5483] EJBTHREE->AS7: orb resource injection

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/resource/basic/ResourceInjectionTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/resource/basic/ResourceInjectionTestCase.java
@@ -36,7 +36,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-
 /**
  * Tests that the Resource injection as specified by Java EE spec works as expected
  * <p/>
@@ -52,7 +51,8 @@ public class ResourceInjectionTestCase {
     @Before
     public void beforeTest() throws Exception {
         Context ctx = new InitialContext();
-        this.slsb = (SimpleSLSB) ctx.lookup("java:module/" + SimpleSLSB.class.getSimpleName() + "!" + SimpleSLSB.class.getName());
+        this.slsb = (SimpleSLSB) ctx.lookup("java:module/" + SimpleSLSB.class.getSimpleName() + "!"
+                + SimpleSLSB.class.getName());
     }
 
     @Deployment
@@ -74,7 +74,8 @@ public class ResourceInjectionTestCase {
         Assert.assertEquals("Unepxected greeting received from bean", CommonBean.HELLO_GREETING_PREFIX + user, greeting);
 
         Class<?> invokedBusinessInterface = this.slsb.getInvokedBusinessInterface();
-        Assert.assertEquals("Unexpected invoked business interface returned by bean", SimpleSLSB.class, invokedBusinessInterface);
+        Assert.assertEquals("Unexpected invoked business interface returned by bean", SimpleSLSB.class,
+                invokedBusinessInterface);
     }
 
     /**
@@ -95,34 +96,66 @@ public class ResourceInjectionTestCase {
     @Test
     public void testOptionalEnvEntryInjection() {
         int defaultInt = this.slsb.getUnInjectedInt();
-        Assert.assertEquals("env-entry of type int without an value was *not* expected to be injected", defaultInt, SimpleSLSB.DEFAULT_UNINJECTED_INT_VAL);
+        Assert.assertEquals("env-entry of type int without an value was *not* expected to be injected", defaultInt,
+                SimpleSLSB.DEFAULT_UNINJECTED_INT_VAL);
 
         String defaultString = this.slsb.getUnInjectedString();
-        Assert.assertEquals("env-entry of type String without an value was *not* expected to be injected", defaultString, SimpleSLSB.DEFAULT_UNINJECTED_STRING_VAL);
+        Assert.assertEquals("env-entry of type String without an value was *not* expected to be injected", defaultString,
+                SimpleSLSB.DEFAULT_UNINJECTED_STRING_VAL);
     }
 
     /**
      * Test scenario:
      * <p/>
-     * A @Resource backed by a env-entry should be made available in ENC only if the corresponding env-entry has a env-entry-value specified.
-     * If the env-entry-value is missing, then there should be no corresponding ENC entry for that env-entry
+     * A @Resource backed by a env-entry should be made available in ENC only if the corresponding env-entry has a
+     * env-entry-value specified. If the env-entry-value is missing, then there should be no corresponding ENC entry for that
+     * env-entry
      */
     @Test
     public void testOptionalEnvEntryEncAvailability() {
         boolean intEnvEntryAvailableInEnc = this.slsb.isUnInjectedIntEnvEntryPresentInEnc();
-        Assert.assertFalse("env-entry of type int, without a env-entry-value was *not* expected to be available in ENC", intEnvEntryAvailableInEnc);
+        Assert.assertFalse("env-entry of type int, without a env-entry-value was *not* expected to be available in ENC",
+                intEnvEntryAvailableInEnc);
 
         boolean stringEnvEntryAvailableInEnc = this.slsb.isUnInjectedStringEnvEntryPresentInEnc();
-        Assert.assertFalse("env-entry of type String, without a env-entry-value was *not* expected to be available in ENC", stringEnvEntryAvailableInEnc);
+        Assert.assertFalse("env-entry of type String, without a env-entry-value was *not* expected to be available in ENC",
+                stringEnvEntryAvailableInEnc);
 
     }
 
     /**
-     * Tests that a EJB with a @Resource of type {@link javax.ejb.TimerService} deploys fine and the {@link javax.ejb.TimerService}
-     * is injected in the bean
+     * Tests that a EJB with a @Resource of type {@link javax.ejb.TimerService} deploys fine and the
+     * {@link javax.ejb.TimerService} is injected in the bean
      */
     @Test
     public void testTimerServiceInjection() {
         Assert.assertTrue("Timerservice was not injected in bean", this.slsb.isTimerServiceInjected());
+    }
+
+    /**
+     * Test if an ORB can be properly located (EJB3 16.13). Part migration of tests from EJB testsuite [JIRA JBQA-5483].
+     * Ondrej Chaloupka
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testOrbEnvironment() throws Exception {
+        Context ctx = new InitialContext();
+        CheckORBRemote bean = (CheckORBRemote) ctx.lookup("java:module/" + CheckORBBean.class.getSimpleName() + "!"
+                + CheckORBRemote.class.getName());
+        bean.checkForORBInEnvironment();
+    }
+
+    /**
+     * Test if an ORB can be properly located (EJB3 16.13). Part migration of tests from EJB testsuite [JIRA JBQA-5483].
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testInjection() throws Exception {
+        Context ctx = new InitialContext();
+        CheckORBRemote bean = (CheckORBRemote) ctx.lookup("java:module/" + CheckORBBean.class.getSimpleName() + "!"
+                + CheckORBRemote.class.getName());
+        bean.checkForInjectedORB();
     }
 }
