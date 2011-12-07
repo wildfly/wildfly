d55c19592b548da5b2fe9d4db6bf6199ebbcc0da
Peter Skopek <pskopek@redhat.com>
2011-12-07 14:28:42 +0100

    Client to remote EJB security tests for JEE related authorization annotations. Resolving JIRAs JBQA-5620 and JBQA-5621.

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/SingleMethodsAnnSLSBTestCase.java
@@ -0,0 +1,309 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.security;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.test.integration.ejb.security.authorization.SimpleAuthorizationRemote;
+import org.jboss.as.test.integration.ejb.security.authorization.SingleMethodsAnnOnlyCheckSLSB;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJBAccessException;
+import javax.naming.Context;
+import javax.security.auth.login.LoginContext;
+
+/**
+ * This test case check whether basic EJB authorization works from EJB client to stateless remote EJB.
+ *
+ * @author <a href="mailto:pskopek@redhat.com">Peter Skopek</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class SingleMethodsAnnSLSBTestCase extends SecurityTest {
+
+    private static final Logger log = Logger.getLogger(SingleMethodsAnnSLSBTestCase.class);
+    private static final String MODULE = "singleMethodsAnnOnlySLSB";
+
+    @Deployment(name = MODULE + ".jar", order = 1, testable = false)
+    public static Archive<JavaArchive> testAppDeployment() {
+
+        // FIXME: change when there will be an option to deploy/call something before the first deployment
+        try {
+            // create required security domain
+            createSecurityDomain();
+        } catch (Exception e) {
+            log.warn("Problems during creation of security domain", e);
+        }
+        log.info("Security domain: ejb3-tests created");
+
+
+        // using JavaArchive doesn't work, because of a bug in Arquillian, it only deploys wars properly
+        final JavaArchive jar = ShrinkWrap.create(JavaArchive.class, MODULE + ".jar")
+                .addClass(SingleMethodsAnnOnlyCheckSLSB.class)
+                .addClass(SimpleAuthorizationRemote.class)
+                //.addClass(Util.class)
+                //.addClass(SecurityTest.class)
+                .addAsResource("ejb3/security/users.properties", "users.properties")
+                .addAsResource("ejb3/security/roles.properties", "roles.properties")
+                .addAsManifestResource("ejb3/security/EMPTY_MANIFEST.MF", "MANIFEST.MF");
+        log.info(jar.toString(true));
+        return jar;
+    }
+
+    /**
+     * Test objective:
+     *   Check if default, @RolesAllowed, @PermitAll, @DenyAll and @RolesAllowed with multiple roles
+     *   works on method level without user logged in as described in EJB 3.1 spec.
+     *   The target EJB is remote stateless bean.
+     * Expected results:
+     *   Test has to finish without any exception or error.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSingleMethodAnnotationsNoUser() throws Exception {
+
+        String myContext = Util.createRemoteEjbJndiContext(
+                "",
+                MODULE,
+                "",
+                SingleMethodsAnnOnlyCheckSLSB.class.getSimpleName(),
+                SimpleAuthorizationRemote.class.getName(),
+                false);
+
+        log.info("JNDI name=" + myContext);
+
+        final Context ctx = Util.createNamingContext();
+        final SimpleAuthorizationRemote singleMethodsAnnOnlyBean = (SimpleAuthorizationRemote)
+                ctx.lookup(myContext);
+
+        try {
+            String echoValue = singleMethodsAnnOnlyBean.defaultAccess("alohomora");
+            Assert.assertEquals(echoValue, "alohomora");
+        } catch (EJBAccessException e) {
+            Assert.fail("Exception not expected");
+        }
+
+
+        try {
+            String echoValue = singleMethodsAnnOnlyBean.roleBasedAccessOne("alohomora");
+            Assert.fail("Method cannot be successfully called without logged in user");
+        } catch (Exception e) {
+            // expected
+            Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+        }
+
+        try {
+            String echoValue = singleMethodsAnnOnlyBean.roleBasedAccessMore("alohomora");
+            Assert.fail("Method cannot be successfully called without logged in user");
+        } catch (Exception e) {
+            // expected
+            Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+        }
+
+        try {
+            String echoValue = singleMethodsAnnOnlyBean.permitAll("alohomora");
+            Assert.assertEquals(echoValue, "alohomora");
+        } catch (Exception e) {
+            Assert.fail("@PermitAll annotation must allow all users and no users to call the method");
+        }
+
+        try {
+            String echoValue = singleMethodsAnnOnlyBean.denyAll("alohomora");
+            Assert.fail("@DenyAll annotation must allow all users and no users to call the method");
+        } catch (Exception e) {
+            // expected
+            Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+        }
+
+
+    }
+
+    /**
+     * Test objective:
+     *   Check if default, @RolesAllowed, @PermitAll, @DenyAll and @RolesAllowed with multiple roles
+     *   works on method level with user1 logged in as described in EJB 3.1 spec.
+     *   user1 has "Users,Role1" roles.
+     *   The target EJB is remote stateless bean.
+     * Expected results:
+     *   Test has to finish without any exception or error.
+     *
+     * TODO: remove @Ignore after the JIRA is fixed
+     *
+     * @throws Exception
+     */
+    @Test
+    @Ignore("AS7-2942")
+    public void testSingleMethodAnnotationsUser1() throws Exception {
+
+        LoginContext lc = Util.getCLMLoginContext("user1", "password1");
+        lc.login();
+
+        try {
+
+            String myContext = Util.createRemoteEjbJndiContext(
+                    "",
+                    MODULE,
+                    "",
+                    SingleMethodsAnnOnlyCheckSLSB.class.getSimpleName(),
+                    SimpleAuthorizationRemote.class.getName(),
+                    false);
+            log.info("JNDI name=" + myContext);
+
+            final Context ctx = Util.createNamingContext();
+            final SimpleAuthorizationRemote singleMethodsAnnOnlyBean = (SimpleAuthorizationRemote)
+                    ctx.lookup(myContext);
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.defaultAccess("alohomora");
+                Assert.assertEquals(echoValue, "alohomora");
+            } catch (EJBAccessException e) {
+                Assert.fail("EJBAccessException not expected");
+            }
+
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.roleBasedAccessOne("alohomora");
+                Assert.assertEquals(echoValue, "alohomora");
+            } catch (EJBAccessException e) {
+                Assert.fail("EJBAccessException not expected");
+            }
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.roleBasedAccessMore("alohomora");
+                Assert.fail("Method cannot be successfully called with logged in principal:" + lc.getSubject());
+            } catch (Exception e) {
+                // expected
+                Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+            }
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.permitAll("alohomora");
+                Assert.assertEquals(echoValue, "alohomora");
+            } catch (Exception e) {
+                Assert.fail("@PermitAll annotation must allow all users and no users to call the method - principal:" + lc.getSubject());
+            }
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.denyAll("alohomora");
+                Assert.fail("@DenyAll annotation must allow all users and no users to call the method");
+            } catch (Exception e) {
+                // expected
+                Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+            }
+
+        } finally {
+            lc.logout();
+        }
+
+
+    }
+
+    /**
+     * Test objective:
+     *   Check if default, @RolesAllowed, @PermitAll, @DenyAll and @RolesAllowed with multiple roles
+     *   works on method level with user1 logged in as described in EJB 3.1 spec.
+     *   user2 has "Users,Role2" roles.
+     *   The target EJB is remote stateless bean.
+     * Expected results:
+     *   Test has to finish without any exception or error.
+     *
+     * TODO: remove @Ignore after the JIRA is fixed
+     *
+     * @throws Exception
+     */
+    @Test
+    @Ignore("AS7-2942")
+    public void testSingleMethodAnnotationsUser2() throws Exception {
+        LoginContext lc = Util.getCLMLoginContext("user2", "password2");
+        lc.login();
+
+        try {
+
+            String myContext = Util.createRemoteEjbJndiContext(
+                    "",
+                    MODULE,
+                    "",
+                    SingleMethodsAnnOnlyCheckSLSB.class.getSimpleName(),
+                    SimpleAuthorizationRemote.class.getName(),
+                    false);
+            log.info("JNDI name=" + myContext);
+
+            final Context ctx = Util.createNamingContext();
+            final SimpleAuthorizationRemote singleMethodsAnnOnlyBean = (SimpleAuthorizationRemote)
+                    ctx.lookup(myContext);
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.defaultAccess("alohomora");
+                Assert.assertEquals(echoValue, "alohomora");
+            } catch (EJBAccessException e) {
+                Assert.fail("EJBAccessException not expected");
+            }
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.roleBasedAccessOne("alohomora");
+                Assert.fail("Method cannot be successfully called with logged in user2");
+            } catch (Exception e) {
+                // expected
+                Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+            }
+
+
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.roleBasedAccessMore("alohomora");
+                Assert.assertEquals(echoValue, "alohomora");
+            } catch (EJBAccessException e) {
+                Assert.fail("EJBAccessException not expected");
+            }
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.permitAll("alohomora");
+                Assert.assertEquals(echoValue, "alohomora");
+            } catch (Exception e) {
+                Assert.fail("@PermitAll annotation must allow all users and no users to call the method - principal:" + lc.getSubject());
+            }
+
+            try {
+                String echoValue = singleMethodsAnnOnlyBean.denyAll("alohomora");
+                Assert.fail("@DenyAll annotation must allow all users and no users to call the method");
+            } catch (Exception e) {
+                // expected
+                Assert.assertTrue("Thrown exception must be EJBAccessException, but was different", e instanceof EJBAccessException);
+            }
+
+        } finally {
+            lc.logout();
+        }
+
+    }
+
+}
--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/Util.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/Util.java
@@ -21,6 +21,9 @@
  */
 package org.jboss.as.test.integration.ejb.security;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
@@ -34,6 +37,7 @@ import javax.security.auth.login.LoginException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.jboss.security.ClientLoginModule;
 
@@ -83,6 +87,48 @@ public class Util {
         };
 
         return new LoginContext(configurationName, new Subject(), cbh, config);
+    }
+
+    /**
+     * Creates JNDI context string based on given parameters.
+     * See details at https://docs.jboss.org/author/display/AS71/EJB+invocations+from+a+remote+client+using+JNDI
+     *
+     * @param appName - typically the ear name without the .ear
+     *                - could be empty string when deploying just jar with EJBs
+     * @param moduleName - jar file name without trailing .jar
+     * @param distinctName - AS7 allows each deployment to have an (optional) distinct name
+     *                     - could be empty string when not specified
+     * @param beanName - The EJB name which by default is the simple class name of the bean implementation class
+     * @param viewClassName - the remote view is fully qualified class name of @Remote EJB interface
+     * @param isStateful - if the bean is stateful set to true
+     *
+     * @return - JNDI context string to use in your client JNDI lookup
+     */
+    public static String createRemoteEjbJndiContext(
+            String appName,
+            String moduleName,
+            String distinctName,
+            String beanName,
+            String viewClassName,
+            boolean isStateful) {
+
+        return "ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName
+                + (isStateful ? "?stateful" : "");
+    }
+
+    /**
+     * Helper to create InitialContext with necessary properties.
+     *
+     * @return new InitialContext.
+     * @throws NamingException
+     */
+    public static Context createNamingContext() throws NamingException {
+
+        final Properties jndiProps = new Properties();
+        jndiProps.setProperty(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+        return new InitialContext(jndiProps);
+
     }
 
 }
