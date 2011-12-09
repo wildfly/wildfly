2b052c1f4d48f51f5a588b0f8154859f343980b4
Thomas Diesler <thomas.diesler@jboss.com>
2011-12-09 10:59:30 +0100

    Move JAXR client back to AS7 code base. Add JAXRDependencyProcessor to get rid of static dependencies in application-client.xml

---
+++ jaxr/src/main/java/org/jboss/as/jaxr/extension/JAXRSubsystemParser.java
@@ -0,0 +1,127 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.jaxr.extension;
+
+import org.jboss.as.jaxr.service.JAXRConstants.Attribute;
+import org.jboss.as.jaxr.service.JAXRConstants.Element;
+import org.jboss.as.jaxr.service.JAXRConstants.Namespace;
+import org.jboss.as.jaxr.service.ModelConstants;
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementReader;
+import org.jboss.staxmapper.XMLExtendedStreamReader;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.jboss.as.controller.parsing.ParseUtils.*;
+
+/**
+ * The subsystem parser.
+ */
+public class JAXRSubsystemParser implements XMLStreamConstants, XMLElementReader<List<ModelNode>> {
+
+    @Override
+    public void readElement(XMLExtendedStreamReader reader, List<ModelNode> operations) throws XMLStreamException {
+        final ModelNode addop = JAXRSubsystemAdd.createAddSubsystemOperation();
+        while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+            switch (Namespace.forUri(reader.getNamespaceURI())) {
+                case JAXR_1_0: {
+                    final Element element = Element.forName(reader.getLocalName());
+                    switch (element) {
+                        case CONNECTIONFACTORY: {
+                            parseBinding(reader, addop, ModelConstants.CONNECTIONFACTORY);
+                            break;
+                        }
+                        case DATASOURCE: {
+                            parseBinding(reader, addop, ModelConstants.DATASOURCE);
+                            break;
+                        }
+                        case FLAGS: {
+                            parseFlags(reader, addop);
+                            break;
+                        }
+                        default:
+                            throw unexpectedElement(reader);
+                    }
+                }
+            }
+        }
+        operations.add(addop);
+    }
+
+    private void parseBinding(XMLExtendedStreamReader reader, ModelNode addop, String modelAttribute) throws XMLStreamException {
+
+        // Handle attributes
+        String jndiName = null;
+        int count = reader.getAttributeCount();
+        for (int i = 0; i < count; i++) {
+            requireNoNamespaceAttribute(reader, i);
+            final String attrValue = reader.getAttributeValue(i);
+            final Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
+            switch (attribute) {
+                case JNDI_NAME: {
+                    jndiName = attrValue;
+                    break;
+                }
+                default:
+                    throw unexpectedAttribute(reader, i);
+            }
+        }
+
+        if (jndiName == null)
+            throw missingRequired(reader, Collections.singleton(Attribute.JNDI_NAME));
+
+        requireNoContent(reader);
+
+        addop.get(modelAttribute).set(jndiName);
+    }
+
+    private void parseFlags(XMLExtendedStreamReader reader, ModelNode addop) throws XMLStreamException {
+
+        // Handle attributes
+        int count = reader.getAttributeCount();
+        for (int i = 0; i < count; i++) {
+            requireNoNamespaceAttribute(reader, i);
+            final String attrValue = reader.getAttributeValue(i);
+            final Attribute attribute = Attribute.forName(reader.getAttributeLocalName(i));
+            switch (attribute) {
+                case DROPONSTART: {
+                    addop.get(ModelConstants.DROPONSTART).set(attrValue);
+                    break;
+                }
+                case CREATEONSTART: {
+                    addop.get(ModelConstants.CREATEONSTART).set(attrValue);
+                    break;
+                }
+                case DROPONSTOP: {
+                    addop.get(ModelConstants.DROPONSTOP).set(attrValue);
+                    break;
+                }
+                default:
+                    throw unexpectedAttribute(reader, i);
+            }
+        }
+        requireNoContent(reader);
+    }
+}
---
+++ jaxr/src/main/java/org/jboss/as/jaxr/extension/JAXRSubsystemWriter.java
@@ -0,0 +1,96 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.jaxr.extension;
+
+import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
+import org.jboss.as.jaxr.service.ModelConstants;
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementWriter;
+import org.jboss.staxmapper.XMLExtendedStreamWriter;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+
+import static org.jboss.as.jaxr.service.JAXRConstants.Attribute;
+import static org.jboss.as.jaxr.service.JAXRConstants.Element;
+import static org.jboss.as.jaxr.service.JAXRConstants.Namespace;
+
+/**
+ * The subsystem writer
+ */
+public class JAXRSubsystemWriter implements XMLStreamConstants, XMLElementWriter<SubsystemMarshallingContext> {
+
+    static final JAXRSubsystemWriter INSTANCE = new JAXRSubsystemWriter();
+
+    // Hide ctor
+    private JAXRSubsystemWriter() {
+    }
+
+    @Override
+    public void writeContent(XMLExtendedStreamWriter writer, SubsystemMarshallingContext context) throws XMLStreamException {
+        context.startSubsystemElement(Namespace.CURRENT.getUriString(), false);
+        ModelNode node = context.getModelNode();
+        if (has(node, ModelConstants.CONNECTIONFACTORY)) {
+            writer.writeStartElement(Element.CONNECTIONFACTORY.getLocalName());
+            writeAttribute(writer, Attribute.JNDI_NAME, node.get(ModelConstants.CONNECTIONFACTORY));
+            writer.writeEndElement();
+        }
+        if (has(node, ModelConstants.DATASOURCE)) {
+            writer.writeStartElement(Element.DATASOURCE.getLocalName());
+            writeAttribute(writer, Attribute.JNDI_NAME, node.get(ModelConstants.DATASOURCE));
+            writer.writeEndElement();
+        }
+        boolean flagsPresent =  false;
+        if (has(node, ModelConstants.DROPONSTART, ModelConstants.CREATEONSTART, ModelConstants.DROPONSTOP)) {
+            writer.writeStartElement(Element.FLAGS.getLocalName());
+            flagsPresent = true;
+        }
+        if (has(node, ModelConstants.DROPONSTART)) {
+            writeAttribute(writer, Attribute.DROPONSTART, node.get(ModelConstants.DROPONSTART));
+        }
+        if (has(node, ModelConstants.CREATEONSTART)) {
+            writeAttribute(writer, Attribute.CREATEONSTART, node.get(ModelConstants.CREATEONSTART));
+        }
+        if (has(node, ModelConstants.DROPONSTOP)) {
+            writeAttribute(writer, Attribute.DROPONSTOP, node.get(ModelConstants.DROPONSTOP));
+        }
+        if (flagsPresent) {
+            writer.writeEndElement();
+        }
+        writer.writeEndElement();
+    }
+
+    private boolean has(ModelNode node, String... names) {
+        boolean found = false;
+        for (String name : names) {
+            if (node.has(name) && node.get(name).isDefined()) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    private void writeAttribute(final XMLExtendedStreamWriter writer, final Attribute attr, final ModelNode value) throws XMLStreamException {
+        writer.writeAttribute(attr.getLocalName(), value.asString());
+    }
+}
