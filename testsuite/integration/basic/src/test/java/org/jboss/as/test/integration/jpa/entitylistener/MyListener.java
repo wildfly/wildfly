5d15c2f6a95aae3ee7b57caea5cdff5ea16c9484
Scott Marlow <smarlow@redhat.com>
2011-12-10 01:13:32 -0500

    AS7-2968 test case added

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/entitylistener/MyListener.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.jpa.entitylistener;
+
+import javax.ejb.EJBContext;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+/**
+ * test case from AS7-2968
+ *
+ */
+public class MyListener {
+
+    public static int getInvocationCount() {
+        return invocationCount;
+    }
+
+    public static void setInvocationCount(int invocationCount) {
+        MyListener.invocationCount = invocationCount;
+    }
+
+    private static volatile int invocationCount = 0;
+
+    @PrePersist
+    @PreUpdate
+    public void onEntityCallback(Object entity) {
+        try {
+            invocationCount++;
+            // Thread.dumpStack();
+            InitialContext jndiContext = new InitialContext();
+            EJBContext ctx = (EJBContext)jndiContext.lookup("java:comp/EJBContext");
+              System.out.println(ctx.getCallerPrincipal().getName() + ", entity=" + entity);
+        } catch (NamingException e) {
+            throw new RuntimeException("initial context error", e);
+        }
+
+    }
+}
