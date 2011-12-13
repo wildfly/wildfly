6acac2ccbcbe01e69d8f7498c8bae1e3d3294eaa
Jason T. Greene <jason.greene@redhat.com>
2011-12-13 11:39:57 -0600

    More debugging for this test

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/appclient/basic/AppClientMain.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/appclient/basic/AppClientMain.java
@@ -2,16 +2,26 @@ package org.jboss.as.test.integration.ee.appclient.basic;
 
 import javax.ejb.EJB;
 
+import org.jboss.logging.Logger;
+
 /**
  * @author Stuart Douglas
  */
 public class AppClientMain {
+    private static final Logger logger = Logger.getLogger("org.jboss.as.test.appclient");
 
     @EJB
     private static AppClientSingletonRemote appClientSingletonRemote;
 
     public static void main(final String[] params) {
-        appClientSingletonRemote.makeAppClientCall(params[0]);
+        logger.info("Main method invoked");
+
+        try {
+            appClientSingletonRemote.makeAppClientCall(params[0]);
+            logger.info("Main method invocation completed with success");
+        } catch (Exception e) {
+            logger.error("Main method failed", e);
+        }
     }
 
 }
