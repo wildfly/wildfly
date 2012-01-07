a6fbc5db54478b742b4f4d04732a7e107efb7024
jaikiran <jaikiran.pai@gmail.com>
2012-01-07 23:32:59 +0530

    Add support for marking a EJB as clustered via deployment descriptor (jboss-ejb3.xml) and also fix an issue with adding dependency on the remote connector service

--- ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/merging/ClusteredMergingProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/merging/ClusteredMergingProcessor.java
@@ -26,16 +26,22 @@ import org.jboss.as.ee.component.EEApplicationClasses;
 import org.jboss.as.ee.component.EEModuleClassDescription;
 import org.jboss.as.ee.metadata.ClassAnnotationInformation;
 import org.jboss.as.ejb3.EjbMessages;
+import org.jboss.as.ejb3.clustering.EJBBoundClusteringMetaData;
 import org.jboss.as.ejb3.component.EJBComponentDescription;
 import org.jboss.as.ejb3.component.entity.EntityBeanComponentDescription;
 import org.jboss.as.ejb3.component.messagedriven.MessageDrivenComponentDescription;
 import org.jboss.as.ejb3.component.session.ClusteringInfo;
 import org.jboss.as.ejb3.component.session.SessionBeanComponentDescription;
 import org.jboss.as.ejb3.component.singleton.SingletonComponentDescription;
+import org.jboss.as.ejb3.deployment.EjbDeploymentAttachmentKeys;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.as.server.deployment.reflect.DeploymentReflectionIndex;
 import org.jboss.ejb3.annotation.Clustered;
+import org.jboss.metadata.ejb.spec.AssemblyDescriptorMetaData;
+import org.jboss.metadata.ejb.spec.EjbJarMetaData;
+
+import java.util.List;
 
 /**
  * @author paul
@@ -60,6 +66,58 @@ public class ClusteredMergingProcessor extends AbstractMergingProcessor<EJBCompo
         if (clustering == null || clustering.getClassLevelAnnotations().isEmpty()) {
             return;
         }
+        // get the annotation information
+        final ClusteringInfo clusteringInfo = clustering.getClassLevelAnnotations().get(0);
+        // validate that the bean is eligible for clustering, before marking it as clustered
+        this.validateAndMarkBeanAsClustered(ejbComponentDescription, clusteringInfo, deploymentUnit);
+    }
+
+    @Override
+    protected void handleDeploymentDescriptor(DeploymentUnit deploymentUnit,
+                                              DeploymentReflectionIndex deploymentReflectionIndex, Class<?> componentClass,
+                                              EJBComponentDescription ejbComponentDescription) throws DeploymentUnitProcessingException {
+
+        final EjbJarMetaData ejbJarMetaData = deploymentUnit.getAttachment(EjbDeploymentAttachmentKeys.EJB_JAR_METADATA);
+        Boolean beanClustered = null;
+        Boolean allBeansClustered = null;
+        if (ejbJarMetaData != null) {
+            final AssemblyDescriptorMetaData assemblyMetadata = ejbJarMetaData.getAssemblyDescriptor();
+            if (assemblyMetadata != null) {
+                final List<EJBBoundClusteringMetaData> clusteringMetaDatas = assemblyMetadata.getAny(EJBBoundClusteringMetaData.class);
+                if (clusteringMetaDatas != null) {
+                    for (final EJBBoundClusteringMetaData clusteringMetaData : clusteringMetaDatas) {
+                        if ("*".equals(clusteringMetaData.getEjbName())) {
+                            allBeansClustered = clusteringMetaData.isClustered();
+                        } else if (ejbComponentDescription.getComponentName().equals(clusteringMetaData.getEjbName())) {
+                            beanClustered = clusteringMetaData.isClustered();
+                        }
+                    }
+                }
+            }
+        }
+        if (beanClustered != null) {
+            if (beanClustered) {
+                this.validateAndMarkBeanAsClustered(ejbComponentDescription, new ClusteringInfo(), deploymentUnit);
+            } else {
+                // if the bean is marked as not-clustered, then we have to set the ClusteringInfo to null
+                // in the EJB component description, so that we override any annotation information that's
+                // already applied for the bean
+                this.validateAndMarkBeanAsClustered(ejbComponentDescription, null, deploymentUnit);
+            }
+        } else if (allBeansClustered != null) {
+            if (allBeansClustered) {
+                this.validateAndMarkBeanAsClustered(ejbComponentDescription, new ClusteringInfo(), deploymentUnit);
+            } else {
+                // if the bean is marked as not-clustered, then we have to set the ClusteringInfo to null
+                // in the EJB component description, so that we override any annotation information that's
+                // already applied for the bean
+                this.validateAndMarkBeanAsClustered(ejbComponentDescription, null, deploymentUnit);
+            }
+        }
+    }
+
+    private void validateAndMarkBeanAsClustered(final EJBComponentDescription ejbComponentDescription, final ClusteringInfo clusteringInfo,
+                                                final DeploymentUnit deploymentUnit) throws DeploymentUnitProcessingException {
         // https://issues.jboss.org/browse/AS7-2887 Disallow @Clustered on certain EJB types
         if (ejbComponentDescription instanceof MessageDrivenComponentDescription) {
             throw EjbMessages.MESSAGES.clusteredAnnotationIsNotApplicableForMDB(deploymentUnit, ejbComponentDescription.getComponentName(), ejbComponentDescription.getComponentClassName());
@@ -74,15 +132,7 @@ public class ClusteredMergingProcessor extends AbstractMergingProcessor<EJBCompo
         if (!(ejbComponentDescription instanceof SessionBeanComponentDescription)) {
             throw EjbMessages.MESSAGES.clusteredAnnotationIsNotApplicableForBean(deploymentUnit, ejbComponentDescription.getComponentName(), ejbComponentDescription.getComponentClassName());
         }
-
-        // set the annotation information on the session bean description
-        final ClusteringInfo clusteringInfo = clustering.getClassLevelAnnotations().get(0);
+        // mark the bean as clustered
         ((SessionBeanComponentDescription) ejbComponentDescription).setClustering(clusteringInfo);
-    }
-
-    @Override
-    protected void handleDeploymentDescriptor(DeploymentUnit deploymentUnit,
-                                              DeploymentReflectionIndex deploymentReflectionIndex, Class<?> componentClass,
-                                              EJBComponentDescription ejbComponentDescription) throws DeploymentUnitProcessingException {
     }
 }
