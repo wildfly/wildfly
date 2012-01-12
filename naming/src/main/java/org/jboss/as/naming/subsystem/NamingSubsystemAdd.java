1afef3f408bd96e9611d1e7b37233d5dc1c4563c
David Bosschaert <david.bosschaert@gmail.com>
2012-01-12 15:30:29 +0000

    AS7-3286 Register InitialContextFactoryBuilder service with the OSGi layer
    Similar code was already in place for the InitialContext. I generalized
    this into a single generic service registration class.
    
    Also found a bug in the JBoss InitialContextFactoryBuilder in that it
    didn't accept a null argument, which the spec allows. I fixed this.
    
    The above is tested in the JNDITestCase integration test.

--- naming/src/main/java/org/jboss/as/naming/subsystem/NamingSubsystemAdd.java
+++ naming/src/main/java/org/jboss/as/naming/subsystem/NamingSubsystemAdd.java
@@ -28,12 +28,13 @@ import java.util.List;
 
 import javax.naming.CompositeName;
 import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.ServiceVerificationHandler;
-import org.jboss.as.naming.InitialContextFactoryService;
+import org.jboss.as.naming.InitialContextFactoryBuilder;
 import org.jboss.as.naming.NamingContext;
 import org.jboss.as.naming.NamingStore;
 import org.jboss.as.naming.ServiceBasedNamingStore;
@@ -118,8 +119,11 @@ public class NamingSubsystemAdd extends AbstractBoottimeAddStepHandler {
             }
         });
 
-        // Provide the {@link InitialContext} as OSGi service
-        newControllers.add(InitialContextFactoryService.addService(target, verificationHandler));
+        // Register InitialContext and InitialContextFactoryBuilder as OSGi services
+        newControllers.add(NamingSubsystemOSGiService.addService(target,
+                InitialContext.class, InitialContext.class, verificationHandler));
+        newControllers.add(NamingSubsystemOSGiService.addService(target,
+                javax.naming.spi.InitialContextFactoryBuilder.class, InitialContextFactoryBuilder.class, verificationHandler));
 
         newControllers.add(target.addService(JndiViewExtensionRegistry.SERVICE_NAME, new JndiViewExtensionRegistry()).install());
 
