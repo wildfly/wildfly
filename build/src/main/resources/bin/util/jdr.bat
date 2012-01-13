e52419dd8b97ae1eec1331135eda681e24b7b08f
Darran Lofthouse <darran.lofthouse@jboss.com>
2012-01-13 17:37:24 +0000

    [AS7-3139] Update the scripts to warn if JBOSS_HOME is set to a different location.

--- build/src/main/resources/bin/util/jdr.bat
+++ build/src/main/resources/bin/util/jdr.bat
@@ -19,11 +19,21 @@ if "%OS%" == "Windows_NT" (
   set DIRNAME=.\
 )
 
-pushd %DIRNAME%..\..
-if "x%JBOSS_HOME%" == "x" (
-  set "JBOSS_HOME=%CD%"
-)
+pushd %DIRNAME%..
+set "RESOLVED_JBOSS_HOME=%CD%"
 popd
+
+if "x%JBOSS_HOME%" == "x" (
+  set "JBOSS_HOME=%RESOLVED_JBOSS_HOME%" 
+)
+
+pushd "%JBOSS_HOME%"
+set "SANITIZED_JBOSS_HOME=%CD%"
+popd
+
+if "%RESOLVED_JBOSS_HOME%" NEQ "%SANITIZED_JBOSS_HOME%" (
+    echo WARNING JBOSS_HOME may be pointing to a different installation - unpredictable results may occur.
+)
 
 set DIRNAME=
 
