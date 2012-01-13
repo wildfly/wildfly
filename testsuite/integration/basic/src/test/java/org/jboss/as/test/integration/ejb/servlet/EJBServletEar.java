984244e9402e80974a1c167c74373471cda5f05a
Ondrej Chaloupka <ochaloup@redhat.com>
2012-01-13 15:12:35 +0100

    [JBQA-5275] AS5->AS7: servlet ejbs

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/servlet/EJBServletEar.java
@@ -0,0 +1,91 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.logging.Logger;
+import org.jboss.security.client.SecurityClient;
+import org.jboss.security.client.SecurityClientFactory;
+
+/**
+ * A servlet that accesses an EJB and tests whether the call argument is serialized.
+ * 
+ * @author Scott.Stark@jboss.org
+ */
+public class EJBServletEar extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger(EJBServletEar.class);
+
+    @EJB(lookup = "java:app/ejb3-ear-servlet-ejbs/Session30!org.jboss.as.test.integration.ejb.servlet.Session30BusinessRemote")
+    Session30BusinessRemote injectedSession;
+
+    @EJB(lookup = "java:app/ejb3-ear-servlet-ejbs/StatelessBean!org.jboss.as.test.integration.ejb.servlet.StatelessLocal")
+    StatelessLocal injectedStateless;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SecurityClient client = null;
+        try {           
+            InitialContext ctx = new InitialContext();
+            
+            client = SecurityClientFactory.getSecurityClient();
+            client.setSimple("somebody", "password");
+            client.login();
+
+            injectedSession.hello();
+            injectedSession.goodbye();
+
+            injectedStateless.hello();
+            injectedStateless.goodbye();
+
+            String lookupString = "java:app/ejb3-ear-servlet-ejbs/Session30!";
+            EJBServletHelper test = new EJBServletHelper();
+            test.processRequest(lookupString, ctx);
+        } catch (Exception e) {
+            log.error(e);
+            throw new ServletException("Failed to call EJBs/Session30 through remote and local interfaces", e);
+        } finally {
+            client.logout();
+        }
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.print("EJBServlet OK");
+        out.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/servlet/Session30Bean.java
@@ -0,0 +1,76 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.servlet;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.LocalHome;
+import javax.ejb.Remote;
+import javax.ejb.RemoteHome;
+import javax.ejb.Stateless;
+
+import org.jboss.ejb3.annotation.SecurityDomain;
+
+/**
+ * @author <a href="mailto:bdecoste@jboss.com">William DeCoste</a>
+ */
+@Stateless(name = "Session30")
+@Remote(Session30BusinessRemote.class)
+@Local(Session30BusinessLocal.class)
+@RemoteHome(Session30Home.class)
+@LocalHome(Session30LocalHome.class)
+@SecurityDomain("other")
+public class Session30Bean implements Session30 {
+
+    @EJB
+    private StatefulRemote stateful;
+
+    private TestObject testObject;
+
+    @RolesAllowed({ "allowed" })
+    public void hello() {
+    }
+
+    @RolesAllowed({ "allowed" })
+    public void goodbye() {
+    }
+
+    public String access(TestObject o) {
+        return stateful.access(o);
+    }
+
+    public TestObject createTestObject() {
+        testObject = new TestObject();
+        return testObject;
+    }
+
+    public boolean checkEqPointer(TestObject to) {
+        return to == testObject;
+    }
+
+    public WarTestObject getWarTestObject() {
+        return new WarTestObject();
+    }
+
+}
