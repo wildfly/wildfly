45630a6aec55742df4a7eb5e2d35f90f9121441e
Darran Lofthouse <darran.lofthouse@jboss.com>
2012-01-13 17:37:24 +0000

    [AS7-3139] Update the scripts to warn if JBOSS_HOME is set to a different location.

--- build/src/main/resources/bin/wsconsume.sh
+++ build/src/main/resources/bin/wsconsume.sh
@@ -32,9 +32,16 @@ if $cygwin ; then
 fi
 
 # Setup JBOSS_HOME
+RESOLVED_JBOSS_HOME=`cd "$DIRNAME/.."; pwd`  
 if [ "x$JBOSS_HOME" = "x" ]; then
     # get the full path (without any relative bits)
-    JBOSS_HOME=`cd "$DIRNAME/.."; pwd`
+    JBOSS_HOME=$RESOLVED_JBOSS_HOME
+else
+ SANITIZED_JBOSS_HOME=`cd "$JBOSS_HOME/.."; pwd`
+ if [ "$RESOLVED_JBOSS" != "$SANITIZED_JBOSS_HOME" ]; then
+   echo "WARNING JBOSS_HOME may be pointing to a different installation - unpredictable results may occur."
+   echo ""
+ fi
 fi
 export JBOSS_HOME
 
