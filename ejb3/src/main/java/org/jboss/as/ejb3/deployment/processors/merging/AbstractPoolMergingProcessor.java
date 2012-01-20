09dda0f2e8159161b2d4b4255e61765b5d70ce59
jaikiran <jaikiran.pai@gmail.com>
2012-01-20 18:02:57 +0530

    Add support for configuring EJBs to use configured pools via annotation and deployment descriptor

---
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/merging/AbstractPoolMergingProcessor.java
@@ -0,0 +1,112 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.ejb3.deployment.processors.merging;
+
+import java.util.List;
+
+import org.jboss.as.ee.component.EEApplicationClasses;
+import org.jboss.as.ee.component.EEModuleClassDescription;
+import org.jboss.as.ee.metadata.ClassAnnotationInformation;
+import org.jboss.as.ejb3.EjbMessages;
+import org.jboss.as.ejb3.component.EJBComponentDescription;
+import org.jboss.as.ejb3.deployment.EjbDeploymentAttachmentKeys;
+import org.jboss.as.ejb3.pool.EJBBoundPoolMetaData;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.reflect.DeploymentReflectionIndex;
+import org.jboss.ejb3.annotation.ResourceAdapter;
+import org.jboss.metadata.ejb.spec.AssemblyDescriptorMetaData;
+import org.jboss.metadata.ejb.spec.EjbJarMetaData;
+
+/**
+ * Sets up the EJB component description with the pool name configured via the {@link org.jboss.ejb3.annotation.Pool}
+ * annotation and/or the deployment descriptor, for a EJB
+ *
+ * @author Jaikiran Pai
+ */
+public abstract class AbstractPoolMergingProcessor<T extends EJBComponentDescription> extends AbstractMergingProcessor<T> {
+
+    public AbstractPoolMergingProcessor(Class<T> descriptionType) {
+        super(descriptionType);
+    }
+
+    @Override
+    protected void handleAnnotations(DeploymentUnit deploymentUnit, EEApplicationClasses applicationClasses, DeploymentReflectionIndex deploymentReflectionIndex, Class<?> componentClass, T description) throws DeploymentUnitProcessingException {
+        final EEModuleClassDescription clazz = applicationClasses.getClassByName(componentClass.getName());
+        //we only care about annotations on the bean class itself
+        if (clazz == null) {
+            return;
+        }
+        final ClassAnnotationInformation<ResourceAdapter, String> resourceAdaptor = clazz.getAnnotationInformation(ResourceAdapter.class);
+        if (resourceAdaptor == null) {
+            return;
+        }
+        if (!resourceAdaptor.getClassLevelAnnotations().isEmpty()) {
+            final String poolName = resourceAdaptor.getClassLevelAnnotations().get(0);
+            if (poolName == null || poolName.trim().isEmpty()) {
+                throw EjbMessages.MESSAGES.poolNameCannotBeEmptyString(description.getEJBName());
+            }
+            this.setPoolName(description, poolName);
+        }
+    }
+
+    @Override
+    protected void handleDeploymentDescriptor(DeploymentUnit deploymentUnit, DeploymentReflectionIndex deploymentReflectionIndex, Class<?> componentClass, T description) throws DeploymentUnitProcessingException {
+        final String ejbName = description.getEJBName();
+        final EjbJarMetaData metaData = deploymentUnit.getAttachment(EjbDeploymentAttachmentKeys.EJB_JAR_METADATA);
+        if (metaData == null) {
+            return;
+        }
+        final AssemblyDescriptorMetaData assemblyDescriptor = metaData.getAssemblyDescriptor();
+        if (assemblyDescriptor == null) {
+            return;
+        }
+        // get the pool metadata
+        final List<EJBBoundPoolMetaData> pools = assemblyDescriptor.getAny(EJBBoundPoolMetaData.class);
+
+        String poolName = null;
+        if (pools != null) {
+            for (final EJBBoundPoolMetaData poolMetaData : pools) {
+                // if this applies for all EJBs and if there isn't a pool name already explicitly specified
+                // for the specific bean (i.e. via a ejb-name match)
+                if ("*".equals(poolMetaData.getEjbName()) && poolName == null) {
+                    poolName = poolMetaData.getPoolName();
+                } else if (ejbName.equals(poolMetaData.getEjbName())) {
+                    poolName = poolMetaData.getPoolName();
+                }
+            }
+        }
+        if (poolName != null) {
+            this.setPoolName(description, poolName);
+        }
+    }
+
+    /**
+     * Set the pool name for the component
+     *
+     * @param componentDescription The component description
+     * @param poolName             The pool name
+     */
+    protected abstract void setPoolName(final T componentDescription, final String poolName);
+
+}
---
+++ ejb3/src/main/java/org/jboss/as/ejb3/pool/EJBBoundPoolParser.java
@@ -0,0 +1,66 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.ejb3.pool;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.jboss.metadata.ejb.parser.jboss.ejb3.AbstractEJBBoundMetaDataParser;
+
+/**
+ * Parser for <code>urn:ejb-pool</code> namespace. The <code>urn:ejb-pool</code> namespace elements
+ * can be used to configure pool names for EJBs.
+ *
+ * @author Jaikiran Pai
+ */
+public class EJBBoundPoolParser extends AbstractEJBBoundMetaDataParser<EJBBoundPoolMetaData> {
+
+    public static final String NAMESPACE_URI = "urn:ejb-pool:1.0";
+
+    private static final String ELEMENT_BEAN_INSTANCE_POOL_REF = "bean-instance-pool-ref";
+
+    @Override
+    public EJBBoundPoolMetaData parse(final XMLStreamReader reader) throws XMLStreamException {
+        final EJBBoundPoolMetaData ejbBoundPoolMetaData = new EJBBoundPoolMetaData();
+        this.processElements(ejbBoundPoolMetaData, reader);
+        return ejbBoundPoolMetaData;
+    }
+
+    @Override
+    protected void processElement(final EJBBoundPoolMetaData poolMetaData, final XMLStreamReader reader) throws XMLStreamException {
+        final String namespaceURI = reader.getNamespaceURI();
+        final String elementName = reader.getLocalName();
+        // if it doesn't belong to our namespace then let the super handle this
+        if (!NAMESPACE_URI.equals(namespaceURI)) {
+            super.processElement(poolMetaData, reader);
+            return;
+        }
+        if (ELEMENT_BEAN_INSTANCE_POOL_REF.equals(elementName)) {
+            final String poolName = getElementText(reader);
+            // set the pool name in the metadata
+            poolMetaData.setPoolName(poolName);
+        } else {
+            throw unexpectedElement(reader);
+        }
+    }
+}
