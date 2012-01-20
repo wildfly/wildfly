f7ad4254ee6ff74fbdde2d6f9be0aee070a67a24
Vladimir Rastseluev <vrastsel@redhat.com>
2012-01-20 09:55:53 +0100

    AS7-2124

--- testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/embedded/deployment/rar/configproperty/ConfigPropertyTestCase.java
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/embedded/deployment/rar/configproperty/ConfigPropertyTestCase.java
@@ -19,14 +19,12 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.smoke.embedded.deployment.rar.AS7_1452;
+package org.jboss.as.test.smoke.embedded.deployment.rar.configproperty;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.*;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -35,11 +33,14 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.ResourceAdapterArchive;
 import org.junit.*;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.jboss.dmr.*;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
 import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.as.test.integration.management.base.AbstractMgmtTestBase;
+import org.jboss.as.test.integration.management.util.MgmtOperationException;
+import org.jboss.as.test.smoke.embedded.deployment.rar.examples.BasicDeploymentTestCase;
+
 import java.net.*;
 import org.jboss.arquillian.container.test.api.*;
 
@@ -49,18 +50,17 @@ import org.jboss.shrinkwrap.api.asset.StringAsset;
 /**
  * @author <a href="vrastsel@redhat.com">Vladimir Rastseluev</a>
  * @author <a href="stefano.maestri@redhat.com">Stefano Maestri</a>
- *         Test casae for AS7-1452: Resource Adapter config-property value passed incorrectly
+ *         Test case for AS7-1452: Resource Adapter config-property value passed incorrectly
  */
 @RunWith(Arquillian.class)
-@Ignore("AS7-1415")
-public class AS7_1452TestCase {
+
+public class ConfigPropertyTestCase extends AbstractMgmtTestBase{
 	
-	private static ModelControllerClient client;
 	
 		
 	//@BeforeClass - called from @Deployment
 	public static void setUp() throws Exception{
-		
+		initModelControllerClient("localhost",9999);
 		
        final ModelNode address = new ModelNode();
         address.add("subsystem", "resource-adapters");
@@ -72,8 +72,7 @@ public class AS7_1452TestCase {
         operation.get(OP_ADDR).set(address);
         operation.get("archive").set("as7_1452.rar");
         operation.get("transaction-support").set("NoTransaction");
-        final ModelNode result = getModelControllerClient().execute(operation);
-        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        executeOperation(operation);
         
        final ModelNode addressConfigRes = address.clone();
         addressConfigRes.add("config-properties","Property");
@@ -83,8 +82,7 @@ public class AS7_1452TestCase {
         operationConfigRes.get(OP).set("add");
         operationConfigRes.get(OP_ADDR).set(addressConfigRes);
         operationConfigRes.get("value").set("A");
-        final ModelNode result1 = getModelControllerClient().execute(operationConfigRes);
-        Assert.assertEquals(SUCCESS, result1.get(OUTCOME).asString());
+        executeOperation(operationConfigRes);
           
         final ModelNode addressAdmin = address.clone();
         addressAdmin.add("admin-objects","java:jboss/ConfigPropertyAdminObjectInterface1");
@@ -93,10 +91,9 @@ public class AS7_1452TestCase {
         final ModelNode operationAdmin = new ModelNode();
         operationAdmin.get(OP).set("add");
         operationAdmin.get(OP_ADDR).set(addressAdmin);
-        operationAdmin.get("class-name").set("org.jboss.as.test.smoke.embedded.deployment.rar.AS7_1452.ConfigPropertyAdminObjectImpl");
+        operationAdmin.get("class-name").set("org.jboss.as.test.smoke.embedded.deployment.rar.configproperty.ConfigPropertyAdminObjectImpl");
         operationAdmin.get("jndi-name").set(AO_JNDI_NAME);
-        final ModelNode result2 = getModelControllerClient().execute(operationAdmin);
-        Assert.assertEquals(SUCCESS, result2.get(OUTCOME).asString());
+        executeOperation(operationAdmin);
         
         final ModelNode addressConfigAdm = addressAdmin.clone();
         addressConfigAdm.add("config-properties","Property");
@@ -106,8 +103,7 @@ public class AS7_1452TestCase {
         operationConfigAdm.get(OP).set("add");
         operationConfigAdm.get(OP_ADDR).set(addressConfigAdm);
         operationConfigAdm.get("value").set("C");
-        final ModelNode result3 = getModelControllerClient().execute(operationConfigAdm);
-        Assert.assertEquals(SUCCESS, result3.get(OUTCOME).asString());
+        executeOperation(operationConfigAdm);
         
         final ModelNode addressConn = address.clone();
         addressConn.add("connection-definitions","java:jboss/ConfigPropertyConnectionFactory1");
@@ -116,11 +112,10 @@ public class AS7_1452TestCase {
         final ModelNode operationConn = new ModelNode();
         operationConn.get(OP).set("add");
         operationConn.get(OP_ADDR).set(addressConn);
-        operationConn.get("class-name").set("org.jboss.as.test.smoke.embedded.deployment.rar.AS7_1452.ConfigPropertyManagedConnectionFactory");
+        operationConn.get("class-name").set("org.jboss.as.test.smoke.embedded.deployment.rar.configproperty.ConfigPropertyManagedConnectionFactory");
         operationConn.get("jndi-name").set(CF_JNDI_NAME);
         operationConn.get("pool-name").set("ConfigPropertyConnectionFactory");
-        final ModelNode result4 = getModelControllerClient().execute(operationConn);
-        Assert.assertEquals(SUCCESS, result4.get(OUTCOME).asString());
+        executeOperation(operationConn);
         
         final ModelNode addressConfigConn = addressConn.clone();
         addressConfigConn.add("config-properties","Property");
@@ -130,18 +125,7 @@ public class AS7_1452TestCase {
         operationConfigConn.get(OP).set("add");
         operationConfigConn.get(OP_ADDR).set(addressConfigConn);
         operationConfigConn.get("value").set("B");
-        final ModelNode result5 = getModelControllerClient().execute(operationConfigConn);
-        Assert.assertEquals(SUCCESS, result5.get(OUTCOME).asString());
-        
-        final ModelNode operationReload = new ModelNode();
-        operationReload.get(OP).set("reload");
-        
-        final ModelNode result6 = getModelControllerClient().execute(operationReload);
-        Assert.assertEquals(SUCCESS, result6.get(OUTCOME).asString());
-        
-        // add delay to let server restart
-        // temporary, until JIRA AS7-1415 is not resolved
-        Thread.sleep(10000);
+        executeOperation(operationConfigConn);
         
 	}
 	@AfterClass
@@ -152,22 +136,11 @@ public class AS7_1452TestCase {
         address.add("resource-adapter","as7_1452.rar");
         address.protect();
         
-        final ModelNode operation = new ModelNode();
-        operation.get(OP).set("remove");
-        operation.get(OP_ADDR).set(address);
-        
-        final ModelNode result = getModelControllerClient().execute(operation);
-        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
-        
-       // StreamUtils.safeClose(client);
+        remove(address);
+        closeModelControllerClient();
         
 	}
-	private static ModelControllerClient getModelControllerClient() throws UnknownHostException {
-		if (client == null) {
-            client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9999);
-        }
-        return client;
-    }
+	
 
     /**
      * Define the deployment
@@ -183,16 +156,12 @@ public class AS7_1452TestCase {
         ResourceAdapterArchive raa =
                 ShrinkWrap.create(ResourceAdapterArchive.class, deploymentName);
          JavaArchive ja = ShrinkWrap.create(JavaArchive.class,  "as7_1452.jar");
-        ja.addClasses(ConfigPropertyResourceAdapter.class, ConfigPropertyManagedConnectionFactory.class,
-                ConfigPropertyManagedConnection.class, ConfigPropertyConnectionFactory.class,
-                ConfigPropertyManagedConnectionMetaData.class,
-                ConfigPropertyConnectionFactoryImpl.class, ConfigPropertyConnection.class,
-                ConfigPropertyConnectionImpl.class, ConfigPropertyAdminObjectImpl.class,
-                ConfigPropertyAdminObjectInterface.class, AS7_1452TestCase.class);
+        ja.addPackage("org.jboss.as.test.smoke.embedded.deployment.rar.configproperty")
+        .addClasses(AbstractMgmtTestBase.class,MgmtOperationException.class);
         raa.addAsLibrary(ja);
 
         raa.addAsManifestResource("rar/" + deploymentName + "/META-INF/ra.xml", "ra.xml")
-        .addAsManifestResource(new StringAsset("Dependencies: org.apache.httpcomponents,org.jboss.as.controller-client,org.jboss.dmr\n"),"MANIFEST.MF");;
+        .addAsManifestResource(new StringAsset("Dependencies: org.jboss.as.controller-client,org.jboss.dmr,org.jboss.as.cli\n"),"MANIFEST.MF");;
         
         return raa; 
     }
