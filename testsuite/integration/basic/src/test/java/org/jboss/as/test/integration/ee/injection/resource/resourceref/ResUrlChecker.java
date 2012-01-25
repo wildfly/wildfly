48f745f3ae4902c602a65cd417e7760e263a5a33
Ondrej Chaloupka <ochaloup@redhat.com>
2012-01-25 17:36:11 +0100

    After rejecting the JIRA AS7-2744 cleaning tests

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/resource/resourceref/ResUrlChecker.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/resource/resourceref/ResUrlChecker.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.integration.ee.injection.url;
+package org.jboss.as.test.integration.ee.injection.resource.resourceref;
 
 import java.net.URL;
 
--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/resource/resourceref/ResUrlCheckerBean.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/resource/resourceref/ResUrlCheckerBean.java
@@ -19,16 +19,13 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.integration.ee.injection.url;
+package org.jboss.as.test.integration.ee.injection.resource.resourceref;
 
 import java.net.URL;
 
 import javax.annotation.Resource;
-import javax.annotation.Resources;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 /**
  * Let's see what we can do with resources of the URL breed.
@@ -37,17 +34,13 @@ import javax.naming.NamingException;
  */
 @Stateless
 @Remote(ResUrlChecker.class)
-// @Resources({ @Resource(name = "url3", type = java.net.URL.class, mappedName = "http://localhost/url3") })
 public class ResUrlCheckerBean implements ResUrlChecker {
-    // @Resource(mappedName = "http://localhost")
-    private URL url1;
-
     // coming in via res-url
     @Resource(name = "url2")
     private URL url2;
 
     public URL getURL1() {
-        return url1;
+        return null;
     }
 
     public URL getURL2() {
@@ -55,10 +48,6 @@ public class ResUrlCheckerBean implements ResUrlChecker {
     }
 
     public URL getURL3() {
-        try {
-            return (URL) new InitialContext().lookup("java:comp/env/url3");
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
+        return null;
     }
 }
