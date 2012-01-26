e2d0a70fe428b9b2484b425e21a6aa3241837b34
Peter Skopek <pskopek@redhat.com>
2012-01-26 14:58:04 +0100

    [JBQA-5632] - adding negative testcase

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/web/security/WebSecurityJBossWebXmlSecurityRolesTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/web/security/WebSecurityJBossWebXmlSecurityRolesTestCase.java
@@ -24,8 +24,10 @@ package org.jboss.as.test.integration.web.security;
 import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,7 +41,19 @@ import org.junit.runner.RunWith;
 @RunAsClient
 public class WebSecurityJBossWebXmlSecurityRolesTestCase extends WebSecurityFORMTestCase {
 
-    @Deployment
+    public static final String deploymentName = "web-secure.war";
+    
+    @ArquillianResource @OperateOnDeployment(deploymentName)
+    URL deploymentUrl;
+
+    public WebSecurityJBossWebXmlSecurityRolesTestCase() {
+        // make URL params flexible in case we are running tests against something different than localhost:8080
+        setHostname(deploymentUrl.getHost());
+        setPort(deploymentUrl.getPort());
+    }
+
+
+    @Deployment(name = deploymentName, order = 1, testable = false)
     public static WebArchive deployment() {
         // FIXME hack to get things prepared before the deployment happens
         try {
@@ -61,6 +75,7 @@ public class WebSecurityJBossWebXmlSecurityRolesTestCase extends WebSecurityFORM
         return war;
     }
 
+
     /**
      * Override test behavior to check if role mapping in jboss-web.xml works.
      */
@@ -68,6 +83,15 @@ public class WebSecurityJBossWebXmlSecurityRolesTestCase extends WebSecurityFORM
     @Test
     public void testPasswordBasedUnsuccessfulAuth() throws Exception {
         makeCall("marcus", "marcus", 200);
+    }
+
+    /**
+     * Negative test to see if mapping is not performed on username instead of role.
+     * @throws Exception
+     */
+    @Test
+    public void testPrincipalMappingOnRole() throws Exception {
+        makeCall("peter", "peter", 403);
     }
 
 }
