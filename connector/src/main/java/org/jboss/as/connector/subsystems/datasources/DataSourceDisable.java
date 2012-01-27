113bc45aa9eb5c59c1699c9d18a96b7ab492a802
Stefano Maestri <stefano.maestri@redhat.com>
2012-01-27 17:11:36 +0100

    [AS7-3477] If DataSourceDisable fails the stopped/removed services need bringing back to their original state

--- connector/src/main/java/org/jboss/as/connector/subsystems/datasources/DataSourceDisable.java
+++ connector/src/main/java/org/jboss/as/connector/subsystems/datasources/DataSourceDisable.java
@@ -22,10 +22,14 @@
 
 package org.jboss.as.connector.subsystems.datasources;
 
+import static org.jboss.as.connector.ConnectorLogger.SUBSYSTEM_DATASOURCES_LOGGER;
 import static org.jboss.as.connector.ConnectorMessages.MESSAGES;
 import static org.jboss.as.connector.subsystems.datasources.Constants.JNDINAME;
+import static org.jboss.as.connector.subsystems.datasources.DataSourceModelNodeUtil.from;
+import static org.jboss.as.connector.subsystems.datasources.DataSourceModelNodeUtil.xaFrom;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ENABLED;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.PERSISTENT;
 
 import java.util.List;
 
@@ -33,11 +37,19 @@ import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.as.naming.ManagedReferenceFactory;
+import org.jboss.as.naming.ServiceBasedNamingStore;
 import org.jboss.as.naming.deployment.ContextNames;
+import org.jboss.as.naming.service.BinderService;
 import org.jboss.dmr.ModelNode;
+import org.jboss.jca.common.api.validator.ValidateException;
+import org.jboss.msc.service.AbstractServiceListener;
+import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
 import org.jboss.msc.service.ServiceRegistry;
+import org.jboss.msc.service.ServiceTarget;
 
 /**
  * Operation handler responsible for disabling an existing data-source.
@@ -45,7 +57,15 @@ import org.jboss.msc.service.ServiceRegistry;
  * @author John Bailey
  */
 public class DataSourceDisable implements OperationStepHandler {
-    static final DataSourceDisable INSTANCE = new DataSourceDisable();
+    static final DataSourceDisable LOCAL_INSTANCE = new DataSourceDisable(false);
+    static final DataSourceDisable XA_INSTANCE = new DataSourceDisable(true);
+
+    private final boolean xa;
+
+    public DataSourceDisable(boolean xa) {
+        super();
+        this.xa = xa;
+    }
 
     public void execute(OperationContext context, ModelNode operation) {
 
@@ -53,7 +73,8 @@ public class DataSourceDisable implements OperationStepHandler {
         model.get(ENABLED).set(false);
 
         if (context.getType() == OperationContext.Type.SERVER) {
-            if (context.isResourceServiceRestartAllowed()) {
+            //if (context.isResourceServiceRestartAllowed()) {
+            if (true) {
                 context.addStep(new OperationStepHandler() {
                     public void execute(final OperationContext context, ModelNode operation) throws OperationFailedException {
 
@@ -140,7 +161,11 @@ public class DataSourceDisable implements OperationStepHandler {
                         context.completeStep(new OperationContext.RollbackHandler() {
                             @Override
                             public void handleRollback(OperationContext context, ModelNode operation) {
-                                //TODO AS7-3477 Readd services
+                                try {
+                                    reEnable(context,operation, model);
+                                } catch (OperationFailedException e) {
+
+                                }
                             }
                         });
                     }
@@ -150,5 +175,163 @@ public class DataSourceDisable implements OperationStepHandler {
             }
         }
         context.completeStep();
+    }
+
+    public void reEnable(final OperationContext context, final ModelNode operation, final ModelNode model) throws OperationFailedException {
+        // On boot we invoke this op but we may not want to store a default value the model
+        boolean persist = operation.get(PERSISTENT).asBoolean(true);
+        if (persist) {
+            model.get(ENABLED).set(true);
+        } else if (model.hasDefined(ENABLED) && !model.get(ENABLED).asBoolean()) {
+            // Just clear the "false" value that gets stored by default
+            model.get(ENABLED).set(new ModelNode());
+        }
+
+        if (context.getType() == OperationContext.Type.SERVER) {
+
+
+                    final ServiceTarget serviceTarget = context.getServiceTarget();
+                    final ServiceVerificationHandler verificationHandler = new ServiceVerificationHandler();
+
+                    final ModelNode address = operation.require(OP_ADDR);
+                    final String dsName = PathAddress.pathAddress(address).getLastElement().getValue();
+                    final String jndiName = model.get(JNDINAME.getName()).asString();
+                    final ServiceRegistry registry = context.getServiceRegistry(true);
+
+                    final List<ServiceName> serviceNames = registry.getServiceNames();
+
+
+                    if (isXa()) {
+                        final ModifiableXaDataSource dataSourceConfig;
+                        try {
+                            dataSourceConfig = xaFrom(context, model, dsName);
+                        } catch (ValidateException e) {
+                            throw new OperationFailedException(e, new ModelNode().set(MESSAGES.failedToCreate("XaDataSource", operation, e.getLocalizedMessage())));
+                        }
+                        final ServiceName xaDataSourceConfigServiceName = XADataSourceConfigService.SERVICE_NAME_BASE.append(dsName);
+                        final XADataSourceConfigService xaDataSourceConfigService = new XADataSourceConfigService(dataSourceConfig);
+
+                        final ServiceBuilder<?> builder = serviceTarget.addService(xaDataSourceConfigServiceName, xaDataSourceConfigService);
+                        builder.addListener(verificationHandler);
+
+                        for (ServiceName name : serviceNames) {
+                            if (xaDataSourceConfigServiceName.append("xa-datasource-properties").isParentOf(name)) {
+                                final ServiceController<?> xaConfigProperyController = registry.getService(name);
+                                XaDataSourcePropertiesService xaPropService = (XaDataSourcePropertiesService) xaConfigProperyController.getService();
+
+                                if (xaConfigProperyController != null) {
+                                    if (!ServiceController.State.UP.equals(xaConfigProperyController.getState())) {
+                                        xaConfigProperyController.setMode(ServiceController.Mode.ACTIVE);
+                                        builder.addDependency(name, String.class, xaDataSourceConfigService.getXaDataSourcePropertyInjector(xaPropService.getName()));
+
+                                    } else {
+                                        throw new OperationFailedException(new ModelNode().set(MESSAGES.serviceAlreadyStarted("Data-source.xa-config-property", name)));
+                                    }
+                                } else {
+                                    throw new OperationFailedException(new ModelNode().set(MESSAGES.serviceNotAvailable("Data-source.xa-config-property", name)));
+                                }
+                            }
+                        }
+                        builder.install();
+
+                    } else {
+
+                        final ModifiableDataSource dataSourceConfig;
+                        try {
+                            dataSourceConfig = from(context, model, dsName);
+                        } catch (ValidateException e) {
+                            throw new OperationFailedException(e, new ModelNode().set(MESSAGES.failedToCreate("DataSource", operation, e.getLocalizedMessage())));
+                        }
+                        final ServiceName dataSourceCongServiceName = DataSourceConfigService.SERVICE_NAME_BASE.append(dsName);
+                        final DataSourceConfigService configService = new DataSourceConfigService(dataSourceConfig);
+
+                        final ServiceBuilder<?> builder = serviceTarget.addService(dataSourceCongServiceName, configService);
+                        builder.addListener(verificationHandler);
+
+
+                        for (ServiceName name : serviceNames) {
+                            if (dataSourceCongServiceName.append("connetion-properties").isParentOf(name)) {
+                                final ServiceController<?> dataSourceController = registry.getService(name);
+                                ConnectionPropertiesService connPropService = (ConnectionPropertiesService) dataSourceController.getService();
+
+                                if (dataSourceController != null) {
+                                    if (!ServiceController.State.UP.equals(dataSourceController.getState())) {
+                                        dataSourceController.setMode(ServiceController.Mode.ACTIVE);
+                                        builder.addDependency(name, String.class, configService.getConnectionPropertyInjector(connPropService.getName()));
+
+                                    } else {
+                                        throw new OperationFailedException(new ModelNode().set(MESSAGES.serviceAlreadyStarted("Data-source.connectionProperty", name)));
+                                    }
+                                } else {
+                                    throw new OperationFailedException(new ModelNode().set(MESSAGES.serviceNotAvailable("Data-source.connectionProperty", name)));
+                                }
+                            }
+                        }
+                        builder.install();
+
+
+                    }
+                    context.completeStep();
+
+
+                    final ServiceName dataSourceServiceName = AbstractDataSourceService.SERVICE_NAME_BASE.append(jndiName);
+
+                    final ServiceController<?> dataSourceController = registry.getService(dataSourceServiceName);
+
+                    if (dataSourceController != null) {
+                        if (!ServiceController.State.UP.equals(dataSourceController.getState())) {
+                            dataSourceController.setMode(ServiceController.Mode.ACTIVE);
+                        } else {
+                            throw new OperationFailedException(new ModelNode().set(MESSAGES.serviceAlreadyStarted("Data-source", dsName)));
+                        }
+                    } else {
+                        throw new OperationFailedException(new ModelNode().set(MESSAGES.serviceNotAvailable("Data-source", dsName)));
+                    }
+
+                    final DataSourceReferenceFactoryService referenceFactoryService = new DataSourceReferenceFactoryService();
+                    final ServiceName referenceFactoryServiceName = DataSourceReferenceFactoryService.SERVICE_NAME_BASE
+                            .append(dsName);
+                    final ServiceBuilder<?> referenceBuilder = serviceTarget.addService(referenceFactoryServiceName,
+                            referenceFactoryService).addDependency(dataSourceServiceName, javax.sql.DataSource.class,
+                            referenceFactoryService.getDataSourceInjector());
+
+                    referenceBuilder.install();
+
+                    final ContextNames.BindInfo bindInfo = ContextNames.bindInfoFor(jndiName);
+                    final BinderService binderService = new BinderService(bindInfo.getBindName());
+                    final ServiceBuilder<?> binderBuilder = serviceTarget
+                            .addService(bindInfo.getBinderServiceName(), binderService)
+                            .addDependency(referenceFactoryServiceName, ManagedReferenceFactory.class, binderService.getManagedObjectInjector())
+                            .addDependency(bindInfo.getParentContextServiceName(), ServiceBasedNamingStore.class, binderService.getNamingStoreInjector()).addListener(new AbstractServiceListener<Object>() {
+                                public void transition(final ServiceController<? extends Object> controller, final ServiceController.Transition transition) {
+                                    switch (transition) {
+                                        case STARTING_to_UP: {
+                                            SUBSYSTEM_DATASOURCES_LOGGER.boundDataSource(jndiName);
+                                            break;
+                                        }
+                                        case START_REQUESTED_to_DOWN: {
+                                            SUBSYSTEM_DATASOURCES_LOGGER.unboundDataSource(jndiName);
+                                            break;
+                                        }
+                                        case REMOVING_to_REMOVED: {
+                                            SUBSYSTEM_DATASOURCES_LOGGER.debugf("Removed JDBC Data-source [%s]", jndiName);
+                                            break;
+                                        }
+                                    }
+                                }
+                            });
+                    binderBuilder.setInitialMode(ServiceController.Mode.ACTIVE)
+                            .addListener(verificationHandler);
+                    binderBuilder.install();
+
+                    context.completeStep();
+
+
+        }
+        context.completeStep();
+    }
+
+    public boolean isXa() {
+        return xa;
     }
 }
