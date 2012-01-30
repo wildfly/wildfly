406c5908ae0c407bc22d6f47a11ffa3ff77c5f6c
Ondrej Chaloupka <ochaloup@redhat.com>
2012-01-30 16:49:04 +0100

    Passivation test #2

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/passivation/PassivationSucceedsUnitTestCase.java
@@ -0,0 +1,151 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.stateful.passivation;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.naming.InitialContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.arquillian.container.Authentication;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
+
+/**
+ * Tests that passivation succeeds, and invocation is possible upon reactivation
+ *
+ * @author <a href="mailto:andrew.rubinger@jboss.org">ALR</a>
+ */
+@RunWith(Arquillian.class)
+public class PassivationSucceedsUnitTestCase {
+    private static final Logger log = Logger.getLogger(PassivationSucceedsUnitTestCase.class);
+
+    @ArquillianResource
+    InitialContext ctx;
+
+    private static ModelControllerClient client;
+
+    @Deployment
+    public static Archive<?> deploy() throws Exception {
+        setPassivationAttributes();
+
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "passivation-test.jar");
+        jar.addPackage(PassivationSucceedsUnitTestCase.class.getPackage());
+        jar.addAsManifestResource(new StringAsset("Dependencies: org.jboss.as.controller-client, org.jboss.dmr \n"), "MANIFEST.MF");
+        log.info(jar.toString(true));
+        return jar;
+    }
+
+    private static ModelControllerClient getModelControllerClient() throws UnknownHostException {
+        if (client == null) {
+            client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9999, Authentication.getCallbackHandler());
+        }
+        return client;
+    }
+
+    private static ModelNode getAddress() {
+        ModelNode address = new ModelNode();
+        address.add("subsystem", "ejb3");
+        address.add("file-passivation-store", "file");
+        address.protect();
+        return address;
+    }
+
+    private static void setPassivationAttributes() throws Exception {
+        ModelNode address = getAddress();
+
+        ModelNode operation = new ModelNode();
+        operation.get(OP).set("write-attribute");
+        operation.get(OP_ADDR).set(address);
+        operation.get("name").set("max-size");
+        operation.get("value").set(1);
+        ModelNode result = getModelControllerClient().execute(operation);
+        log.info("modelnode operation write attribute max-size=1: " + result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+
+        operation = new ModelNode();
+        operation.get(OP).set("write-attribute");
+        operation.get(OP_ADDR).set(address);
+        operation.get("name").set("idle-timeout");
+        operation.get("value").set(1);
+        result = getModelControllerClient().execute(operation);
+        log.info("modelnode operation write-attribute idle-timeout=1: " + result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+    }
+
+    @AfterClass
+    public static void unsetPassivationAttributes() throws Exception {
+        ModelNode address = getAddress();
+
+        ModelNode operation = new ModelNode();
+        operation.get(OP).set("undefine-attribute");
+        operation.get(OP_ADDR).set(address);
+        operation.get("name").set("max-size");
+        ModelNode result = getModelControllerClient().execute(operation);
+
+        operation = new ModelNode();
+        operation.get(OP).set("undefine-attribute");
+        operation.get(OP_ADDR).set(address);
+        operation.get("name").set("idle-timeout");
+        result = getModelControllerClient().execute(operation);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+    }
+
+    @Test
+    public void testPassivationMaxSize() throws Exception {
+        TestPassivationRemote remote1 = (TestPassivationRemote) ctx.lookup("java:module/" + TestPassivationBean.class.getSimpleName());
+        Assert.assertEquals("Returned remote1 result was not expected", TestPassivationRemote.EXPECTED_RESULT,
+                remote1.returnTrueString());
+
+        TestPassivationRemote remote2 = (TestPassivationRemote) ctx.lookup("java:module/" + TestPassivationBean.class.getSimpleName());
+        Assert.assertEquals("Returned remote2 result was not expected", TestPassivationRemote.EXPECTED_RESULT,
+                remote2.returnTrueString());
+
+        //create another bean. This should force the other bean to passivate, as only one bean is allowed in the pool at a time
+        ctx.lookup("java:module/" + TestPassivationBean.class.getSimpleName());
+
+        Assert.assertTrue("@PrePassivate not called, check cache configuration and client sleep time", remote1.hasBeenPassivated());
+        Assert.assertTrue("@PrePassivate not called, check cache configuration and client sleep time", remote2.hasBeenPassivated());
+
+        remote1.remove();
+        remote2.remove();
+    }
+}
+
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/passivation/TestPassivationBean.java
@@ -0,0 +1,100 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.stateful.passivation;
+
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
+import javax.ejb.Remote;
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+
+import org.jboss.ejb3.annotation.Cache;
+import org.jboss.logging.Logger;
+
+/**
+ * @author <a href="mailto:andrew.rubinger@jboss.org">ALR</a>
+ */
+@Stateful
+@Remote(TestPassivationRemote.class)
+@Cache("passivating")
+public class TestPassivationBean implements TestPassivationRemote {
+    private static final Logger log = Logger.getLogger(TestPassivationBean.class);
+   
+    private String identificator;
+    private boolean beenPassivated = false;
+    private boolean beenActivated = false;
+
+    /**
+     * Returns the expected result
+     */
+    public String returnTrueString() {
+        return TestPassivationRemote.EXPECTED_RESULT;
+    }
+
+    /**
+     * Returns whether or not this instance has been passivated
+     */
+    public boolean hasBeenPassivated() {
+        return this.beenPassivated;
+    }
+
+    /**
+     * Returns whether or not this instance has been activated
+     */
+    public boolean hasBeenActivated() {
+        return this.beenActivated;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        Random r = new Random();
+        this.identificator = new Integer(r.nextInt(999)).toString();
+        log.info("Bean [" +  this.identificator + "] created");
+    }
+    
+    @PreDestroy
+    public void preDestroy() {
+        log.info("Bean [" +  this.identificator + "] destroyed");
+    }
+    
+    @PrePassivate
+    public void setPassivateFlag() {
+        log.info(this.toString() + " PrePassivation [" +  this.identificator + "]");
+        this.beenPassivated = true;
+    }
+
+    @PostActivate
+    public void setActivateFlag() {
+        log.info(this.toString() + " PostActivation [" + this.identificator + "]");
+        this.beenActivated = true;
+    }
+    
+    @Remove
+    public void remove() {
+        log.info("Bean [" +  this.identificator + "] removing");
+    }
+}
