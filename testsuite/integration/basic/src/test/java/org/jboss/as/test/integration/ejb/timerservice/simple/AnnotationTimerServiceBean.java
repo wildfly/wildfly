7536d3608150b093e032531a49b90dc66543c1a8
Ondrej Chaloupka <ochaloup@redhat.com>
2012-01-31 12:13:33 +0100

    [JBQA-5269] Enhancing timer tests - checking ability of getting TimerService via JNDI lookup + timer creation method checks

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/timerservice/simple/AnnotationTimerServiceBean.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/timerservice/simple/AnnotationTimerServiceBean.java
@@ -22,9 +22,13 @@
 package org.jboss.as.test.integration.ejb.timerservice.simple;
 
 import javax.annotation.Resource;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.ejb.Timeout;
+import javax.ejb.Timer;
 import javax.ejb.TimerService;
+import com.kenai.jaffl.annotations.Synchronized;
+
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -33,20 +37,47 @@ import java.util.concurrent.TimeUnit;
  */
 @Stateless
 public class AnnotationTimerServiceBean {
-
-    private static final CountDownLatch latch = new CountDownLatch(1);
+    private static CountDownLatch latch = new CountDownLatch(1);
 
     private static boolean timerServiceCalled = false;
+    
+    private String timerInfo;
+    private boolean isPersistent;
+    private boolean isCalendar;
 
     @Resource
+    private SessionContext sessionContext;
+    
     private TimerService timerService;
 
-    public void createTimer() {
-        timerService.createTimer(100, null);
+    public synchronized TimerService getTimerService() {
+        if(timerService == null) {
+            timerService = (TimerService) sessionContext.lookup("java:comp/TimerService"); 
+        }
+        return timerService;
+    }
+    
+    public void resetTimerServiceCalled() {
+        timerServiceCalled = false;
+        latch = new CountDownLatch(1);
+    }
+    
+    public String getTimerInfo() {
+        return timerInfo;
+    }
+    public boolean isPersistent() {
+        return isPersistent;
+    }
+    public boolean isCalendar() {
+        return isCalendar;
     }
 
     @Timeout
-    public void timeout() {
+    public void timeout(Timer timer) {
+        timerInfo = (String) timer.getInfo();
+        isPersistent = timer.isPersistent();
+        isCalendar = timer.isCalendarTimer();
+        
         timerServiceCalled = true;
         latch.countDown();
     }
