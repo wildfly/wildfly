d9c3e588d50cb7a984bc26e93ad4231f38647ade
Jason T. Greene <jason.greene@redhat.com>
2012-01-31 12:52:55 -0600

    Fix comment

--- build/src/main/resources/bin/standalone.bat
+++ build/src/main/resources/bin/standalone.bat
@@ -79,7 +79,7 @@ if not "%PRESERVE_JAVA_OPTS%" == "true" (
     )
   )
 
-  rem Add compressed oops, if supported (64 bit VM), and not overriden
+  rem Add tiered compilation, if supported (64 bit VM), and not overriden
   echo "%JAVA_OPTS%" | findstr /I "\-XX:\-TieredCompilation \-client" > nul
   if errorlevel == 1 (
     "%JAVA%" -XX:+TieredCompilation -version > nul 2>&1
