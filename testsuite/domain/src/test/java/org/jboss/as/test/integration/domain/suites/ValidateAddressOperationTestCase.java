a286e13280dcd67a81493672b6ca602a08b40ea6
Alexey Loubyansky <olubyans@tstudio.(none)>
2012-02-02 15:15:53 +0100

    AS7-3561 tests for the new validate-address impl

---
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/suites/ValidateAddressOperationTestCase.java
@@ -0,0 +1,102 @@
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright 2011, Red Hat Middleware LLC, and individual contributors
+* as indicated by the @author tags. See the copyright.txt file in the
+* distribution for a full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jboss.as.test.integration.domain.suites;
+
+import java.io.IOException;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.controller.operations.common.ValidateAddressOperationHandler;
+import org.jboss.as.test.integration.management.base.AbstractMgmtTestBase;
+import org.jboss.as.test.integration.management.util.MgmtOperationException;
+import org.jboss.as.test.integration.management.util.ModelUtil;
+import org.jboss.dmr.ModelNode;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.PROBLEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALID;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests that the validate-address operation works as it should
+ *
+ * @author <a href="alex@jboss.org">Alexey Loubyansky</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class ValidateAddressOperationTestCase extends AbstractMgmtTestBase {
+
+
+    @BeforeClass
+    public static void initClient() throws Exception {
+        initModelControllerClient("127.0.0.1", 9999);
+    }
+
+    @AfterClass
+    public static void closeClient() throws Exception {
+        closeModelControllerClient();
+    }
+
+    @Test
+    public void testValidRootAddress() throws IOException, MgmtOperationException {
+        ModelNode op = ModelUtil.createOpNode(null, ValidateAddressOperationHandler.OPERATION_NAME);
+        op.get(VALUE).setEmptyList();
+        final ModelNode result = executeOperation(op);
+        assertTrue(result.hasDefined(VALID));
+        final ModelNode value = result.get(VALID);
+        assertTrue(value.asBoolean());
+        assertFalse(result.hasDefined(PROBLEM));
+    }
+
+    @Test
+    public void testValidPath() throws IOException, MgmtOperationException {
+        ModelNode op = ModelUtil.createOpNode(null, ValidateAddressOperationHandler.OPERATION_NAME);
+        final ModelNode addr = op.get(VALUE);
+        addr.add("socket-binding-group", "standard-sockets");
+        addr.add("socket-binding", "http");
+        final ModelNode result = executeOperation(op);
+        assertTrue(result.hasDefined(VALID));
+        final ModelNode value = result.get(VALID);
+        assertTrue(value.asBoolean());
+        assertFalse(result.hasDefined(PROBLEM));
+    }
+
+    @Test
+    public void testInvalidPath() throws IOException, MgmtOperationException {
+        ModelNode op = ModelUtil.createOpNode(null, ValidateAddressOperationHandler.OPERATION_NAME);
+        final ModelNode addr = op.get(VALUE);
+        addr.add("socket-binding-group", "standard-sockets");
+        addr.add("wrong", "illegal");
+        final ModelNode result = executeOperation(op);
+        assertTrue(result.hasDefined(VALID));
+        final ModelNode value = result.get(VALID);
+        assertFalse(value.asBoolean());
+        assertTrue(result.hasDefined(PROBLEM));
+        final ModelNode problem = result.get(PROBLEM);
+        assertTrue(problem.asString().contains("JBAS014808: Child resource '\"wrong\" => \"illegal\"' not found"));
+    }
+}
