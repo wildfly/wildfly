972a15c5a1569131f556f6719709f8bff9850aa9
jaikiran <jaikiran.pai@gmail.com>
2012-02-03 16:24:24 +0530

    AS7-3000 Add a testcase for deployments with distinct-name and EJB invocations

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/distinctname/EarDeploymentDistinctNameTestCase.java
@@ -0,0 +1,73 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.remote.distinctname;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests that the distinct-name configured in the jboss-app.xml of a EAR deployment is taken into
+ * consideration during remote EJB invocations.
+ *
+ * @author Jaikiran Pai
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class EarDeploymentDistinctNameTestCase extends DistinctNameTestCase {
+
+    private static final String APP_NAME = "remote-ejb-distinct-name";
+    private static final String DISTINCT_NAME = "distinct-name-in-jboss-app-xml";
+    private static final String MODULE_NAME = "remote-ejb-distinct-name-ear-test-case";
+
+    @Deployment(testable = false)
+    public static EnterpriseArchive createDeployment() {
+        final JavaArchive jar = ShrinkWrap.create(JavaArchive.class, MODULE_NAME + ".jar");
+        jar.addPackage(EarDeploymentDistinctNameTestCase.class.getPackage());
+
+        final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, APP_NAME + ".ear");
+        ear.addAsModule(jar);
+        ear.addAsManifestResource(EarDeploymentDistinctNameTestCase.class.getPackage(), "jboss-app.xml", "jboss-app.xml");
+
+        return ear;
+    }
+
+    @Override
+    protected String getAppName() {
+        return APP_NAME;
+    }
+
+    @Override
+    protected String getModuleName() {
+        return MODULE_NAME;
+    }
+
+    @Override
+    protected String getDistinctName() {
+        return DISTINCT_NAME;
+    }
+}
