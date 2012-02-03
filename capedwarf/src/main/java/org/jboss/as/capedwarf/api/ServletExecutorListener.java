1209377ea8fc009c0a5febabe62679db5bdacce1
Ales Justin <ales.justin@gmail.com>
2012-02-03 23:31:53 +0100

    Fix listener, no more producer caching.

--- capedwarf/src/main/java/org/jboss/as/capedwarf/api/ServletExecutorListener.java
+++ capedwarf/src/main/java/org/jboss/as/capedwarf/api/ServletExecutorListener.java
@@ -5,7 +5,6 @@ import org.jboss.as.capedwarf.services.ServletExecutor;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.lang.reflect.Method;
 
 /**
  * Servlet executor listener.
@@ -13,35 +12,15 @@ import java.lang.reflect.Method;
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class ServletExecutorListener implements ServletContextListener {
-    private static final String PRODUCER_CLASS = "org.jboss.capedwarf.common.jms.ServletExecutorProducer";
-
     public void contextInitialized(ServletContextEvent sce) {
         final ServletContext context = sce.getServletContext();
         final String appId = (String) context.getAttribute("org.jboss.capedwarf.appId");
         ServletExecutor.registerContext(appId, context);
-        // start producer
-        checkProducer("start");
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
         final ServletContext context = sce.getServletContext();
         final String appId = (String) context.getAttribute("org.jboss.capedwarf.appId");
         ServletExecutor.unregisterContext(appId);
-        // stop producer
-        checkProducer("stop");
-    }
-
-    // Using reflection -- as we don't know how we're adding CD module; bundled or ref-ed as module
-    private void checkProducer(final String state) {
-        try {
-            final ClassLoader cl = Thread.currentThread().getContextClassLoader(); // app CL?
-            final Class<?> clazz = cl.loadClass(PRODUCER_CLASS);
-            final Method instance = clazz.getDeclaredMethod("getInstance");
-            final Method method = clazz.getDeclaredMethod(state);
-            final Object target = instance.invoke(null);
-            method.invoke(target);
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e);
-        }
     }
 }
