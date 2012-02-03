a41f269c257a5adea3fe5471cf7da810e78007a8
Ondrej Chaloupka <ochaloup@redhat.com>
2012-02-03 15:29:41 +0100

    [JBQA-5268] singleton test enhance - dependson annotation

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/singleton/dependson/CallCounterSingleton.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.singleton.dependson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Singleton;
+
+/**
+ * Counting ordering of calls.
+ *  
+ * @author Ondrej Chaloupka
+ */
+@Singleton
+public class CallCounterSingleton {
+    private List<String> orderLog = new ArrayList<String>(); 
+    
+    public void addCall(String call) {
+        this.orderLog.add(call);
+    }
+    
+    public List<String> getCalls() {
+        return this.orderLog;
+    }
+}
+ 
