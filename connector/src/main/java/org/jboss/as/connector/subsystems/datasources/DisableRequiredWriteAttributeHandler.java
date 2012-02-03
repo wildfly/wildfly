4f6d101e71766a4b57ee2237b04ae22ce70a0ee7
Stefano Maestri <stefano.maestri@redhat.com>
2012-02-03 10:36:34 +0100

    AS7-3572 Changing a DS JNDI name leads to DS that cannot be enabled

--- connector/src/main/java/org/jboss/as/connector/subsystems/datasources/DisableRequiredWriteAttributeHandler.java
+++ connector/src/main/java/org/jboss/as/connector/subsystems/datasources/DisableRequiredWriteAttributeHandler.java
@@ -50,7 +50,14 @@ public class DisableRequiredWriteAttributeHandler extends AbstractWriteAttribute
 
     @Override
     protected boolean applyUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName, ModelNode resolvedValue, ModelNode currentValue, HandbackHolder<Void> voidHandback) {
-        return false;
+        ModelNode submodel = context.readResource(PathAddress.EMPTY_ADDRESS).getModel();
+        if ((submodel.hasDefined(Constants.ENABLED.getName()) && submodel.get(Constants.ENABLED.getName()).asBoolean()) ||
+                Constants.JNDINAME.getName().equals(attributeName)) {
+           return true;
+        } else {
+            //do thejob
+            return false;
+        }
     }
 
     @Override
