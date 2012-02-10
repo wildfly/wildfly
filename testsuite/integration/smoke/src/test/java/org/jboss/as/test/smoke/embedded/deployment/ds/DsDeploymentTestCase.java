47faa8c5dcdcacb48f8a5b7257f86c4bcb8e6786
Vladimir Rastseluev <vrastsel@redhat.com>
2012-02-10 11:33:21 +0100

    JBQA-5872

---
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/embedded/deployment/ds/DsDeploymentTestCase.java
@@ -0,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.smoke.embedded.deployment.ds;
+
+import static org.jboss.as.test.smoke.embedded.deployment.ds.DsUtil.testConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Ignore;
+
+/**
+ * Tests an ability to deploy *-ds.xml datasource definition JBQA-5872
+ *
+ * @author Vladimir Rastseluev
+ */
+
+@RunWith(Arquillian.class)
+
+public class DsDeploymentTestCase {
+	private String user1 = "SA";
+
+
+    @Deployment
+    public static Archive<?> deploy() throws Exception{
+        
+    	JavaArchive ja = ShrinkWrap.create(JavaArchive.class,"test.jar").addClasses(DsDeploymentTestCase.class,DsUtil.class);
+    	final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class,"test.ear").addAsLibrary(ja).addAsManifestResource("ds/h2-ds.xml", "h2-ds.xml");
+        return ear;
+    }
+    
+
+    @ArquillianResource
+    private InitialContext ctx;
+
+
+    @Test 
+    public void testDataSourceDefinition() throws Exception {
+    	
+    	DataSource ds = (DataSource)ctx.lookup("java:jboss/datasources/DDs");
+    	Connection conn = null;
+    	
+    	try{
+        	conn = ds.getConnection();
+         	testConnection(conn,"select current_user()",user1);
+    	
+        }
+    	finally{
+					  if (conn != null)
+					  {
+					     try
+					     {
+					        conn.close();
+					     }
+					     catch (SQLException ignore)
+					     {
+					        // Ignore
+					     }
+
+					  }
+    	}
+    }
+}
---
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/embedded/deployment/ds/DsUtil.java
@@ -0,0 +1,57 @@
+package org.jboss.as.test.smoke.embedded.deployment.ds;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import static junit.framework.Assert.*;
+
+public class DsUtil {
+	public static void testConnection(Connection con, String query, String resultContains){
+		String result = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try
+		{
+		  st = con.createStatement();
+		  rs = st.executeQuery(query);
+		  if (rs != null && rs.next())
+			  result = rs.getString(1);
+		}
+		catch (Throwable t)
+		{
+		  fail("it's impossible to execute query:"+query);
+		  t.printStackTrace();
+		}
+		finally
+		{
+		  if (rs != null)
+		  {
+		     try
+		     {
+		        rs.close();
+		     }
+		     catch (SQLException ignore)
+		     {
+		        // Ignore
+		     }
+		  }
+		  if (st != null)
+		  {
+		     try
+		     {
+		        st.close();
+		     }
+		     catch (SQLException ignore)
+		     {
+		        // Ignore
+		     }
+		  }
+
+		}
+
+		assertTrue("result='"+result+"', but must contain '"+resultContains+"' substring",result.indexOf(resultContains) >= 0);
+
+	}
+}
