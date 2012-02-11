da5887138b8d817fb76d2a5252081d8a98a78490
bstansberry@jboss.com <brian.stansberry@redhat.com>
2012-02-11 00:04:03 -0600

    AS7-3041 Use scope id when matching IPv6 addresses

--- controller/src/main/java/org/jboss/as/controller/interfaces/InterfaceCriteria.java
+++ controller/src/main/java/org/jboss/as/controller/interfaces/InterfaceCriteria.java
@@ -18,19 +18,17 @@ public interface InterfaceCriteria extends Serializable {
     /**
      * Gets whether the given network interface and address are acceptable for
      * use. Acceptance is indicated by returning the address which should be
-     * used for binding against
-     * the network interface, typically this is the input address. For those
-     * criteria which override the bind address, the overriden address should
+     * used for binding against the network interface; typically this is the given {@code address}
+     * parameter. For those criteria which override the configured address, the override address should
      * be returned.
      *
      * @param networkInterface the network interface. Cannot be <code>null</code>
      * @param address an address that is associated with <code>networkInterface</code>.
      * Cannot be <code>null</code>
      * @return <code>InetAddress</code> the non-null address to bind to if the
-     * criteria is met, null if the criteria does not apply
+     * criteria is met, {@code null} if the criteria is not satisfied
      *
-     * @throws SocketException
+     * @throws SocketException if evaluating the state of {@code networkInterface} results in one
      */
-
     InetAddress isAcceptable(NetworkInterface networkInterface, InetAddress address) throws SocketException;
 }
