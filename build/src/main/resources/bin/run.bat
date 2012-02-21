460fdfdeb0072bf7ebabd3a99aa25257ac1c5408
kabir <kkhan@redhat.com>
2012-02-21 19:16:52 +0000

    [AS7-3675] Add run.sh/bat placeholders

---
+++ build/src/main/resources/bin/run.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+REM Placeholder for people used to run.bat from older version redirecting them to read the readme
+echo.
+echo =======================================================================================
+echo.
+echo To start JBoss Application Server please see %CD%\..\README.txt
+echo.
+echo ========================================================================================
+echo.
