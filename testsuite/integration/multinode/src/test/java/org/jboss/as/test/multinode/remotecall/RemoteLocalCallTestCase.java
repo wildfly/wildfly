6f60660543a95ad901a8cde202d5f69139d78160
Ondrej Chaloupka <ochaloup@redhat.com>
2012-02-22 18:18:45 +0100

    [JBQA-5855] Ejb cluster tests - remote call

---
+++ testsuite/integration/multinode/src/test/java/org/jboss/as/test/multinode/remotecall/RemoteLocalCallTestCase.java
@@ -0,0 +1,207 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.multinode.remotecall;
+
+import javax.ejb.EJBException;
+import javax.naming.InitialContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Testing multinode communication - calling Stateless and Stateful beans (via remote and local and home interfaces).
+ * 
+ * @author William DeCoste, Ondrej Chaloupka
+ */
+@RunWith(Arquillian.class)
+public class RemoteLocalCallTestCase {
+    private static final Logger log = Logger.getLogger(RemoteLocalCallTestCase.class);
+    public static final String ARCHIVE_NAME_CLIENT = "remotelocalcall-test-client";
+    public static final String ARCHIVE_NAME_SERVER = "remotelocalcall-test-server";
+
+    @BeforeClass
+    public static void printSysProps() {
+        log.info("System properties:\n" + System.getProperties());
+    }
+
+    @Deployment(name = "server")
+    @TargetsContainer("multinode-server")
+    public static Archive<?> deployment0() {
+        JavaArchive jar = createJar(ARCHIVE_NAME_SERVER);
+        return jar;
+    }
+
+    @Deployment(name = "client")
+    @TargetsContainer("multinode-client")
+    public static Archive<?> deployment1() {
+        JavaArchive jar = createJar(ARCHIVE_NAME_CLIENT);
+        jar.addClasses(RemoteLocalCallTestCase.class);
+        jar.addAsManifestResource("META-INF/jboss-ejb-client.xml", "jboss-ejb-client.xml");
+        return jar;
+    }
+
+    private static JavaArchive createJar(String archiveName) {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, archiveName + ".jar");
+        jar.addClasses(StatefulBean.class, StatefulLocal.class, StatefulLocalHome.class, StatefulRemote.class,
+                StatefulRemoteHome.class, StatelessBean.class, StatelessLocal.class, StatelessLocalHome.class,
+                StatelessRemote.class, StatelessRemoteHome.class);
+        return jar;
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatelessLocalFromRemote(@ArquillianResource InitialContext ctx) throws Exception {
+        StatefulRemote bean = (StatefulRemote) ctx.lookup("java:module/" + StatefulBean.class.getSimpleName() + "!"
+                + StatefulRemote.class.getName());
+        Assert.assertNotNull(bean);
+
+        try {
+            bean.localCall();
+            Assert.fail("should not be allowed to call local interface remotely");
+        } catch (EJBException e) {
+            // it's OK
+        } catch (Exception ee) {
+            if(!(ee.getCause() instanceof EJBException)) {
+                Assert.fail("should be " + EJBException.class.getName());
+            }
+        }
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatelessLocalHomeFromRemoteHome(@ArquillianResource InitialContext ctx) throws Exception {
+        StatefulRemoteHome home = (StatefulRemoteHome) ctx.lookup("java:module/" + StatefulBean.class.getSimpleName() + "!"
+                + StatefulRemoteHome.class.getName());
+        StatefulRemote bean = home.create();
+        Assert.assertNotNull(bean);
+
+        try {
+            bean.localHomeCall();
+            Assert.fail("should not be allowed to call local interface remotely");
+        } catch (EJBException e) {
+            // it's OK
+        } catch (Exception ee) {
+            if(!(ee.getCause() instanceof EJBException)) {
+                Assert.fail("should be " + EJBException.class.getName());
+            }
+        }
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatelessRemoteFromRemote(@ArquillianResource InitialContext ctx) throws Exception {
+        StatefulRemote bean = (StatefulRemote) ctx.lookup("java:module/" + StatefulBean.class.getSimpleName() + "!"
+                + StatefulRemote.class.getName());
+        Assert.assertNotNull(bean);
+
+        int methodCount = bean.remoteCall();
+        Assert.assertEquals(1, methodCount);
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatelessRemoteHomeFromRemoteHome(@ArquillianResource InitialContext ctx) throws Exception {
+        StatefulRemoteHome home = (StatefulRemoteHome) ctx.lookup("java:module/" + StatefulBean.class.getSimpleName() + "!"
+                + StatefulRemoteHome.class.getName());
+        StatefulRemote bean = home.create();
+        Assert.assertNotNull(bean);
+
+        int methodCount = bean.remoteHomeCall();
+        Assert.assertEquals(1, methodCount);
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatefulRemoteFromRemote(@ArquillianResource InitialContext ctx) throws Exception {
+        StatelessRemote bean = (StatelessRemote) ctx.lookup("java:module/" + StatelessBean.class.getSimpleName() + "!"
+                + StatelessRemote.class.getName());
+        Assert.assertNotNull(bean);
+
+        int methodCount = bean.remoteCall();
+        Assert.assertEquals(1, methodCount);
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatefulRemoteHomeFromRemoteHome(@ArquillianResource InitialContext ctx) throws Exception {
+        StatelessRemoteHome home = (StatelessRemoteHome) ctx.lookup("java:module/" + StatelessBean.class.getSimpleName() + "!"
+                + StatelessRemoteHome.class.getName());
+        StatelessRemote bean = home.create();
+        Assert.assertNotNull(bean);
+
+        int methodCount = bean.remoteHomeCall();
+        Assert.assertEquals(1, methodCount);
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatefulLocalFromRemote(@ArquillianResource InitialContext ctx) throws Exception {
+        InitialContext jndiContext = new InitialContext();
+        StatelessRemote bean = (StatelessRemote) jndiContext.lookup("java:module/" + StatelessBean.class.getSimpleName() + "!"
+                + StatelessRemote.class.getName());
+        Assert.assertNotNull(bean);
+
+        try {
+            bean.localCall();
+            Assert.fail("should not be allowed to call local interface remotely");
+        } catch (EJBException e) {
+            // it's OK
+        } catch (Exception ee) {
+            if(!(ee.getCause() instanceof EJBException)) {
+                Assert.fail("should be " + EJBException.class.getName());
+            }
+        }
+    }
+
+    @Test
+    @OperateOnDeployment("client")
+    public void testStatefulLocalHomeFromRemoteHome(@ArquillianResource InitialContext ctx) throws Exception {
+        InitialContext jndiContext = new InitialContext();
+        StatelessRemoteHome home = (StatelessRemoteHome) jndiContext.lookup("java:module/"
+                + StatelessBean.class.getSimpleName() + "!" + StatelessRemoteHome.class.getName());
+        StatelessRemote bean = home.create();
+        Assert.assertNotNull(bean);
+
+        try {
+            bean.localHomeCall();
+            Assert.fail("should not be allowed to call local interface remotely");
+        } catch (EJBException e) {
+            // it's OK
+        } catch (Exception ee) {
+            if(!(ee.getCause() instanceof EJBException)) {
+                Assert.fail("should be " + EJBException.class.getName());
+            }
+        }
+    }
+}
---
+++ testsuite/integration/multinode/src/test/java/org/jboss/as/test/multinode/remotecall/StatelessBean.java
@@ -0,0 +1,110 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.multinode.remotecall;
+
+import java.util.Properties;
+
+import javax.ejb.Local;
+import javax.ejb.LocalHome;
+import javax.ejb.Remote;
+import javax.ejb.RemoteHome;
+import javax.ejb.Stateless;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.jboss.logging.Logger;
+
+/**
+ * @author <a href="mailto:bdecoste@jboss.com">William DeCoste</a>
+ */
+@Stateless
+@Local(StatelessLocal.class)
+@LocalHome(StatelessLocalHome.class)
+@Remote(StatelessRemote.class)
+@RemoteHome(StatelessRemoteHome.class)
+public class StatelessBean {
+    private static final Logger log = Logger.getLogger(StatelessBean.class);
+
+    private static int methodCount = 0;
+    private static int homeMethodCount = 0;
+
+    private InitialContext getInitialContext() throws NamingException {
+        final Properties props = new Properties();
+        // setup the ejb: namespace URL factory
+        props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        return new InitialContext(props);
+    }
+
+    public void localCall() throws Exception {
+        InitialContext jndiContext = getInitialContext();
+        log.info("Calling Local remotely... " + jndiContext.getEnvironment());
+        StatefulLocal stateful = (StatefulLocal) jndiContext.lookup("ejb:/" + RemoteLocalCallTestCase.ARCHIVE_NAME_SERVER
+                + "//" + StatefulBean.class.getSimpleName() + "!" + StatefulLocal.class.getName());
+        stateful.method();
+    }
+
+    public void localHomeCall() throws Exception {
+        InitialContext jndiContext = getInitialContext();
+        log.info("Calling LocalHome remotely... " + jndiContext.getEnvironment());
+        StatefulLocalHome statefulHome = (StatefulLocalHome) jndiContext.lookup("ejb:/" + StatefulBean.class.getSimpleName()
+                + "!" + StatefulLocalHome.class.getName());
+        StatefulLocal stateful = statefulHome.create();
+        stateful.homeMethod();
+    }
+
+    public int remoteCall() throws Exception {
+        ++methodCount;
+        InitialContext jndiContext = getInitialContext();
+        log.info("Calling Remote... " + jndiContext.getEnvironment());
+        StatelessRemote stateless = (StatelessRemote) jndiContext.lookup("ejb:/" + RemoteLocalCallTestCase.ARCHIVE_NAME_SERVER
+                + "//" + StatelessBean.class.getSimpleName() + "!" + StatelessRemote.class.getName());
+        return stateless.method();
+    }
+
+    public int remoteHomeCall() throws Exception {
+        ++homeMethodCount;
+        InitialContext jndiContext = getInitialContext();
+        StatelessRemoteHome statelessHome = (StatelessRemoteHome) jndiContext.lookup("ejb:/"
+                + RemoteLocalCallTestCase.ARCHIVE_NAME_SERVER + "//" + StatelessBean.class.getSimpleName() + "!"
+                + StatelessRemoteHome.class.getName());
+        StatelessRemote stateless = statelessHome.create();
+        return stateless.homeMethod();
+    }
+
+    public int method() throws Exception {
+        ++methodCount;
+        log.info("Method called " + methodCount);
+        return methodCount;
+    }
+
+    public int homeMethod() throws Exception {
+        ++homeMethodCount;
+        log.info("HomeMethod called " + homeMethodCount);
+        return homeMethodCount;
+    }
+
+    public void ejbCreate() throws java.rmi.RemoteException, javax.ejb.CreateException {
+        log.debug("Creating method for home interface...");
+    }
+}
