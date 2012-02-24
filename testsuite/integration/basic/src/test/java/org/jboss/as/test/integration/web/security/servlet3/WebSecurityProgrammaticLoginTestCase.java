90f3c6320044ccff179c7851471b2b3e6232954e
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-02-24 15:29:38 +1100

    Allow non-public classes to be used

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/web/security/servlet3/WebSecurityProgrammaticLoginTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/web/security/servlet3/WebSecurityProgrammaticLoginTestCase.java
@@ -134,7 +134,7 @@ public class WebSecurityProgrammaticLoginTestCase {
         return "web-secure-programmatic-login";
     }
 
-    public static class SecurityDomainSetup extends AbstractSecurityDomainSetup {
+    static class SecurityDomainSetup extends AbstractSecurityDomainSetup {
 
         @Override
         protected String getSecurityDomainName() {
