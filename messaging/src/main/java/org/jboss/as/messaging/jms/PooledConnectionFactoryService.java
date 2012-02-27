f4706e3e56fc50b3851320cfed0b1763b6b02ee5
Justin Bertram <jbertram@redhat.com>
2012-02-27 11:09:24 -0600

    [AS7-3903] change metadata to 1.5 classes

--- messaging/src/main/java/org/jboss/as/messaging/jms/PooledConnectionFactoryService.java
+++ messaging/src/main/java/org/jboss/as/messaging/jms/PooledConnectionFactoryService.java
@@ -54,8 +54,7 @@ import org.jboss.jca.common.api.metadata.ra.RequiredConfigProperty;
 import org.jboss.jca.common.api.metadata.ra.ResourceAdapter1516;
 import org.jboss.jca.common.api.metadata.ra.SecurityPermission;
 import org.jboss.jca.common.api.metadata.ra.XsdString;
-import org.jboss.jca.common.api.metadata.ra.ra16.ConfigProperty16;
-import org.jboss.jca.common.api.metadata.ra.ra16.Connector16;
+import org.jboss.jca.common.api.metadata.ra.ra15.Connector15;
 import org.jboss.jca.common.api.validator.ValidateException;
 import org.jboss.jca.common.metadata.common.CommonConnDefImpl;
 import org.jboss.jca.common.metadata.common.CommonPoolImpl;
@@ -72,9 +71,8 @@ import org.jboss.jca.common.metadata.ra.common.MessageAdapterImpl;
 import org.jboss.jca.common.metadata.ra.common.MessageListenerImpl;
 import org.jboss.jca.common.metadata.ra.common.OutboundResourceAdapterImpl;
 import org.jboss.jca.common.metadata.ra.common.ResourceAdapter1516Impl;
-import org.jboss.jca.common.metadata.ra.ra16.Activationspec16Impl;
-import org.jboss.jca.common.metadata.ra.ra16.ConfigProperty16Impl;
-import org.jboss.jca.common.metadata.ra.ra16.Connector16Impl;
+import org.jboss.jca.common.metadata.ra.ra15.Activationspec15Impl;
+import org.jboss.jca.common.metadata.ra.ra15.Connector15Impl;
 import org.jboss.jca.core.api.connectionmanager.ccm.CachedConnectionManager;
 import org.jboss.jca.core.api.management.ManagementRepository;
 import org.jboss.jca.core.spi.mdr.MetadataRepository;
@@ -107,6 +105,7 @@ import static org.jboss.as.messaging.MessagingMessages.MESSAGES;
  * A service which translates a pooled connection factory into a resource adapter driven connection pool
  *
  * @author <a href="mailto:andy.taylor@jboss.com">Andy Taylor</a>
+ * @author <a href="mailto:jbertram@redhat.com">Justin Bertram</a>
  * @author Jason T. Greene
  *         Date: 5/13/11
  *         Time: 2:21 PM
@@ -189,7 +188,7 @@ public class PooledConnectionFactoryService implements Service<Void> {
     private void createService(ServiceTarget serviceTarget, ServiceContainer container) throws Exception {
         InputStream is = null;
         InputStream isIj = null;
-        List<ConfigProperty16> properties = new ArrayList<ConfigProperty16>();
+        List<ConfigProperty> properties = new ArrayList<ConfigProperty>();
         try {
             StringBuilder connectorClassname = new StringBuilder();
             StringBuilder connectorParams = new StringBuilder();
@@ -215,18 +214,18 @@ public class PooledConnectionFactoryService implements Service<Void> {
             }
 
             if (connectorClassname.length() > 0) {
-                properties.add(simpleProperty(CONNECTOR_CLASSNAME, STRING_TYPE, connectorClassname.toString()));
+                properties.add(simpleProperty15(CONNECTOR_CLASSNAME, STRING_TYPE, connectorClassname.toString()));
             }
             if (connectorParams.length() > 0) {
-                properties.add(simpleProperty(CONNECTION_PARAMETERS, STRING_TYPE, connectorParams.toString()));
+                properties.add(simpleProperty15(CONNECTION_PARAMETERS, STRING_TYPE, connectorParams.toString()));
             }
 
             if(discoveryGroupName != null) {
                 DiscoveryGroupConfiguration discoveryGroupConfiguration = hornetQService.getValue().getConfiguration().getDiscoveryGroupConfigurations().get(discoveryGroupName);
-                properties.add(simpleProperty(GROUP_ADDRESS, STRING_TYPE, discoveryGroupConfiguration.getGroupAddress()));
-                properties.add(simpleProperty(DISCOVERY_INITIAL_WAIT_TIMEOUT, LONG_TYPE, "" + discoveryGroupConfiguration.getDiscoveryInitialWaitTimeout()));
-                properties.add(simpleProperty(GROUP_PORT, INTEGER_TYPE, "" + discoveryGroupConfiguration.getGroupPort()));
-                properties.add(simpleProperty(REFRESH_TIMEOUT, LONG_TYPE, "" + discoveryGroupConfiguration.getRefreshTimeout()));
+                properties.add(simpleProperty15(GROUP_ADDRESS, STRING_TYPE, discoveryGroupConfiguration.getGroupAddress()));
+                properties.add(simpleProperty15(DISCOVERY_INITIAL_WAIT_TIMEOUT, LONG_TYPE, "" + discoveryGroupConfiguration.getDiscoveryInitialWaitTimeout()));
+                properties.add(simpleProperty15(GROUP_PORT, INTEGER_TYPE, "" + discoveryGroupConfiguration.getGroupPort()));
+                properties.add(simpleProperty15(REFRESH_TIMEOUT, LONG_TYPE, "" + discoveryGroupConfiguration.getRefreshTimeout()));
             }
 
             boolean hasReconnect = false;
@@ -234,23 +233,23 @@ public class PooledConnectionFactoryService implements Service<Void> {
             for (PooledConnectionFactoryConfigProperties adapterParam : adapterParams) {
                 hasReconnect |= reconnectName.equals(adapterParam.getName());
 
-                properties.add(simpleProperty(adapterParam.getName(), adapterParam.getType(), adapterParam.getValue()));
+                properties.add(simpleProperty15(adapterParam.getName(), adapterParam.getType(), adapterParam.getValue()));
             }
 
             // The default -1, which will hang forever until a server appears
             if (!hasReconnect) {
-                properties.add(simpleProperty(reconnectName, Integer.class.getName(), DEFAULT_MAX_RECONNECTS));
+                properties.add(simpleProperty15(reconnectName, Integer.class.getName(), DEFAULT_MAX_RECONNECTS));
             }
 
             TransactionManagerLocator.container = container;
             AS7RecoveryRegistry.container = container;
-            properties.add(simpleProperty("TransactionManagerLocatorClass", STRING_TYPE, TransactionManagerLocator.class.getName()));
-            properties.add(simpleProperty("TransactionManagerLocatorMethod", STRING_TYPE, "getTransactionManager"));
+            properties.add(simpleProperty15("TransactionManagerLocatorClass", STRING_TYPE, TransactionManagerLocator.class.getName()));
+            properties.add(simpleProperty15("TransactionManagerLocatorMethod", STRING_TYPE, "getTransactionManager"));
 
             OutboundResourceAdapter outbound = createOutbound();
             InboundResourceAdapter inbound = createInbound();
-            ResourceAdapter1516 ra = createResourceAdapter(properties, outbound, inbound);
-            Connector16 cmd = createConnector(ra);
+            ResourceAdapter1516 ra = createResourceAdapter15(properties, outbound, inbound);
+            Connector15 cmd = createConnector15(ra);
 
             CommonConnDef common = createConnDef(jndiName);
             IronJacamar ijmd = createIron(common, txSupport);
@@ -311,11 +310,11 @@ public class PooledConnectionFactoryService implements Service<Void> {
         return new CommonConnDefImpl(Collections.<String, String>emptyMap(), RAMANAGED_CONN_FACTORY, jndiName, HQ_CONN_DEF, true, true, true, pool, timeOut, validation, security, recovery);
     }
 
-    private static Connector16Impl createConnector(ResourceAdapter1516 ra) {
-        return new Connector16Impl(null, str("Red Hat"), str("JMS 1.1 Server"), str("1.0"), null, ra, Collections.<String>emptyList(), false, EMPTY_LOCL, EMPTY_LOCL, Collections.<Icon>emptyList(), null);
+    private static Connector15Impl createConnector15(ResourceAdapter1516 ra) {
+        return new Connector15Impl(str("Red Hat"), str("JMS 1.1 Server"), str("1.0"), null, ra, EMPTY_LOCL, EMPTY_LOCL, Collections.<Icon>emptyList(), null);
     }
 
-    private ResourceAdapter1516Impl createResourceAdapter(List<ConfigProperty16> properties, OutboundResourceAdapter outbound, InboundResourceAdapter inbound) {
+    private ResourceAdapter1516Impl createResourceAdapter15(List<ConfigProperty> properties, OutboundResourceAdapter outbound, InboundResourceAdapter inbound) {
         return new ResourceAdapter1516Impl(HQ_ADAPTER, properties, outbound, inbound, Collections.<AdminObject>emptyList(), Collections.<SecurityPermission>emptyList(), null);
     }
 
@@ -328,8 +327,8 @@ public class PooledConnectionFactoryService implements Service<Void> {
             final ConfigProperty configProp = new ConfigPropertyImpl(EMPTY_LOCL, str(activationConfigProp), str(STRING_TYPE), null, null);
             jmsActivationConfigProps.add(configProp);
         }
-        Activationspec16Impl activation = new Activationspec16Impl(str(HQ_ACTIVATION), destination, jmsActivationConfigProps, null);
-        List<MessageListener> messageListeners = Collections.<MessageListener>singletonList(new MessageListenerImpl(str(JMS_MESSAGE_LISTENER), activation, null));
+        Activationspec15Impl activation15 = new Activationspec15Impl(str(HQ_ACTIVATION), destination, null);
+        List<MessageListener> messageListeners = Collections.<MessageListener>singletonList(new MessageListenerImpl(str(JMS_MESSAGE_LISTENER), activation15, null));
         Messageadapter message = new MessageAdapterImpl(messageListeners, null);
 
         return new InboundResourceAdapterImpl(message, null);
@@ -337,9 +336,9 @@ public class PooledConnectionFactoryService implements Service<Void> {
 
     private static OutboundResourceAdapter createOutbound() {
         List<ConnectionDefinition> definitions = new ArrayList<ConnectionDefinition>();
-        List<ConfigProperty16> props = new ArrayList<ConfigProperty16>();
-        props.add(simpleProperty(SESSION_DEFAULT_TYPE, STRING_TYPE, JMS_QUEUE));
-        props.add(simpleProperty(TRY_LOCK, INTEGER_TYPE, "0"));
+        List<ConfigProperty> props = new ArrayList<ConfigProperty>();
+        props.add(simpleProperty15(SESSION_DEFAULT_TYPE, STRING_TYPE, JMS_QUEUE));
+        props.add(simpleProperty15(TRY_LOCK, INTEGER_TYPE, "0"));
         definitions.add(new ConnectionDefinitionImpl(str(RAMANAGED_CONN_FACTORY), props, str(RA_CONN_FACTORY), str(RA_CONN_FACTORY_IMPL), str(JMS_SESSION), str(HQ_SESSION), null));
 
         AuthenticationMechanism basicPassword = new AuthenticationMechanismImpl(Collections.<LocalizedXsdString>emptyList(), str(BASIC_PASS), CredentialInterfaceEnum.PasswordCredential, null);
@@ -350,8 +349,8 @@ public class PooledConnectionFactoryService implements Service<Void> {
         return new XsdString(str, null);
     }
 
-    private static ConfigProperty16 simpleProperty(String name, String type, String value) {
-        return new ConfigProperty16Impl(EMPTY_LOCL, str(name), str(type), str(value), Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, null, null);
+    private static ConfigProperty simpleProperty15(String name, String type, String value) {
+        return new ConfigPropertyImpl(EMPTY_LOCL, str(name), str(type), str(value), null);
     }
 
 
