a1b6ea77082ce8e18ff54c0d34b2f8bf22227f7e
jaikiran <jaikiran.pai@gmail.com>
2012-02-29 15:59:44 +0530

    Fix JBossAppXMLSecurityTestCase which was using a local EJB view to invoke remotely

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/jbossappxml/FirstBean.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/jbossappxml/FirstBean.java
@@ -21,25 +21,28 @@
  */
 package org.jboss.as.test.integration.ejb.security.jbossappxml;
 
+import org.jboss.ejb3.annotation.RunAsPrincipal;
+
 import javax.annotation.security.RunAs;
 import javax.ejb.EJB;
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
-
-import org.jboss.ejb3.annotation.RunAsPrincipal;
 
 /**
  * Simple EJB3 bean that calls another bean with a run as
+ *
  * @author anil saldhana
  */
 @RunAs("Employee")
 @RunAsPrincipal("javajoe")
 @Stateless
+@Remote(BeanInterface.class)
 public class FirstBean implements BeanInterface {
 
     @EJB
     private SecondBean secondBean;
 
-    public String getCallerPrincipal(){
+    public String getCallerPrincipal() {
         return secondBean.getCallerPrincipal();
     }
 
