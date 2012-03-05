5778c5cb8a1a4dbdcdb1ce270b86c46b6d8423ae
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-03-06 09:15:45 +1100

    Increase timeout

--- testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/stilts/SimpleStompletTestCase.java
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/stilts/SimpleStompletTestCase.java
@@ -16,8 +16,6 @@
  */
 package org.jboss.as.test.smoke.stilts;
 
-import static org.jboss.as.test.smoke.stilts.bundle.SimpleStomplet.DESTINATION_QUEUE_ONE;
-
 import java.io.InputStream;
 import java.net.URL;
 import java.util.HashSet;
@@ -28,7 +26,6 @@ import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 
 import junit.framework.Assert;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -57,6 +54,8 @@ import org.projectodd.stilts.stomp.client.MessageHandler;
 import org.projectodd.stilts.stomp.client.StompClient;
 import org.projectodd.stilts.stomp.client.SubscriptionBuilder;
 import org.projectodd.stilts.stomplet.Stomplet;
+
+import static org.jboss.as.test.smoke.stilts.bundle.SimpleStomplet.DESTINATION_QUEUE_ONE;
 
 /**
  * A simple {@link Stomplet} test case.
@@ -115,7 +114,7 @@ public class SimpleStompletTestCase {
         client.send(StompMessages.createStompMessage(DESTINATION_QUEUE_ONE, "msg1"));
         client.send(StompMessages.createStompMessage(DESTINATION_QUEUE_ONE, "msg2"));
 
-        Assert.assertTrue("No latch timeout", outboundLatch.await(3, TimeUnit.SECONDS));
+        Assert.assertTrue("No latch timeout", outboundLatch.await(10, TimeUnit.SECONDS));
         Assert.assertTrue("Contains msg1", outbound.contains("msg1"));
         Assert.assertTrue("Contains msg2", outbound.contains("msg2"));
 
