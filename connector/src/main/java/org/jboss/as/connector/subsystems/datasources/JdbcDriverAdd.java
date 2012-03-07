376ff95b7847930d0b7c4a46b90eb41d29933962
Stefano Maestri <stefano.maestri@redhat.com>
2012-03-07 15:31:40 +0100

    AS7-4041 JDBCDriverAdd should not require driver-name param

--- connector/src/main/java/org/jboss/as/connector/subsystems/datasources/JdbcDriverAdd.java
+++ connector/src/main/java/org/jboss/as/connector/subsystems/datasources/JdbcDriverAdd.java
@@ -31,6 +31,7 @@ import static org.jboss.as.connector.subsystems.datasources.Constants.DRIVER_MIN
 import static org.jboss.as.connector.subsystems.datasources.Constants.DRIVER_MODULE_NAME;
 import static org.jboss.as.connector.subsystems.datasources.Constants.DRIVER_NAME;
 import static org.jboss.as.connector.subsystems.datasources.Constants.DRIVER_XA_DATASOURCE_CLASS_NAME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 
 import java.lang.reflect.Constructor;
 import java.sql.Driver;
@@ -44,6 +45,7 @@ import org.jboss.as.connector.registry.InstalledDriver;
 import org.jboss.as.controller.AbstractAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.dmr.ModelNode;
 import org.jboss.modules.Module;
@@ -62,8 +64,9 @@ public class JdbcDriverAdd extends AbstractAddStepHandler {
     static final JdbcDriverAdd INSTANCE = new JdbcDriverAdd();
 
     protected void populateModel(ModelNode operation, ModelNode model) {
-        final String driverName = operation.require(DRIVER_NAME.getName()).asString();
         final String moduleName = operation.require(DRIVER_MODULE_NAME.getName()).asString();
+        final ModelNode address = operation.require(OP_ADDR);
+        final String driverName = PathAddress.pathAddress(address).getLastElement().getValue();
 
         final Integer majorVersion = operation.hasDefined(DRIVER_MAJOR_VERSION.getName()) ? operation.get(DRIVER_MAJOR_VERSION.getName()).asInt() : null;
         final Integer minorVersion = operation.hasDefined(DRIVER_MINOR_VERSION.getName()) ? operation.get(DRIVER_MINOR_VERSION.getName()).asInt() : null;
@@ -87,7 +90,11 @@ public class JdbcDriverAdd extends AbstractAddStepHandler {
     }
 
     protected void performRuntime(OperationContext context, ModelNode operation, ModelNode model, ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers) throws OperationFailedException {
-        final String driverName = operation.require(DRIVER_NAME.getName()).asString();
+        final ModelNode address = operation.require(OP_ADDR);
+        final String driverName = PathAddress.pathAddress(address).getLastElement().getValue();
+        if (operation.get(DRIVER_NAME.getName()).isDefined() && ! driverName.equals(operation.get(DRIVER_NAME.getName()).asString()))  {
+            throw SUBSYSTEM_DATASOURCES_LOGGER.driverNameAndResourceNameNotEquals(operation.get(DRIVER_NAME.getName()).asString(), driverName);
+        }
         String moduleName = operation.require(DRIVER_MODULE_NAME.getName()).asString();
         final Integer majorVersion = operation.hasDefined(DRIVER_MAJOR_VERSION.getName()) ? operation.get(DRIVER_MAJOR_VERSION.getName()).asInt() : null;
         final Integer minorVersion = operation.hasDefined(DRIVER_MINOR_VERSION.getName()) ? operation.get(DRIVER_MINOR_VERSION.getName()).asInt() : null;
