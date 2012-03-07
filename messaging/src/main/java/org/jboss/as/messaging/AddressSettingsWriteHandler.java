ce42443342461353e434b7a4ff5448e8982993cc
Emanuel Muckenhuber <emuckenh@redhat.com>
2012-03-07 17:32:30 +0100

    [AS7-4073] update address-setting write-attribute handler

--- messaging/src/main/java/org/jboss/as/messaging/AddressSettingsWriteHandler.java
+++ messaging/src/main/java/org/jboss/as/messaging/AddressSettingsWriteHandler.java
@@ -22,7 +22,7 @@
 
 package org.jboss.as.messaging;
 
-import static org.jboss.as.messaging.MessagingMessages.MESSAGES;
+import org.jboss.as.controller.AbstractWriteAttributeHandler;
 
 import java.util.EnumSet;
 
@@ -32,7 +32,6 @@ import org.hornetq.core.settings.impl.AddressSettings;
 import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.controller.registry.AttributeAccess;
@@ -43,10 +42,9 @@ import org.jboss.dmr.ModelNode;
 /**
  * @author Emanuel Muckenhuber
  */
-class AddressSettingsWriteHandler implements OperationStepHandler {
+class AddressSettingsWriteHandler extends AbstractWriteAttributeHandler<AddressSettingsWriteHandler.RevertHandback> {
 
     static final AddressSettingsWriteHandler INSTANCE = new AddressSettingsWriteHandler();
-
 
     public void registerAttributes(final ManagementResourceRegistration registry, boolean registerRuntimeOnly) {
         final EnumSet<AttributeAccess.Flag> flags = EnumSet.of(AttributeAccess.Flag.RESTART_NONE);
@@ -57,53 +55,46 @@ class AddressSettingsWriteHandler implements OperationStepHandler {
         }
     }
 
-    @Override
-    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
-        final Resource resource = context.readResourceForUpdate(PathAddress.EMPTY_ADDRESS);
-        final String attribute = operation.require(ModelDescriptionConstants.NAME).asString();
-        final AttributeDefinition def = getAttributeDefinition(attribute);
-        if(def == null) {
-            context.getFailureDescription().set(new ModelNode().set(MESSAGES.unknownAttribute(attribute)));
-        } else {
-            def.getValidator().validateParameter(ModelDescriptionConstants.VALUE, operation.get(ModelDescriptionConstants.VALUE));
-            resource.getModel().get(attribute).set(operation.get(ModelDescriptionConstants.VALUE));
-
-            if(context.isNormalServer()) {
-                context.addStep(new OperationStepHandler() {
-                    @Override
-                    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
-                        final HornetQServer server = AddressSettingAdd.getServer(context, operation);
-                        PathAddress address = null;
-                        HierarchicalRepository<AddressSettings> repository = null;
-                        AddressSettings existingSettings = null;
-                        if(server != null) {
-                            final ModelNode model = resource.getModel();
-                            address = PathAddress.pathAddress(operation.require(ModelDescriptionConstants.OP_ADDR));
-                            final AddressSettings settings = AddressSettingAdd.createSettings(context, model);
-                            repository = server.getAddressSettingsRepository();
-                            String match = address.getLastElement().getValue();
-                            existingSettings = repository.getMatch(match);
-                            repository.addMatch(match, settings);
-                        }
-
-                        if (context.completeStep() != OperationContext.ResultAction.KEEP && existingSettings != null) {
-                            // Restore the old settings
-                            repository.addMatch(address.getLastElement().getValue(), existingSettings);
-                        }
-                    }
-                }, OperationContext.Stage.RUNTIME);
-            }
-        }
-        context.completeStep();
+    protected AddressSettingsWriteHandler() {
+        super(AddressSettingAdd.ATTRIBUTES);
     }
 
-    static AttributeDefinition getAttributeDefinition(final String attributeName) {
-        for(final AttributeDefinition def : AddressSettingAdd.ATTRIBUTES) {
-            if(def.getName().equals(attributeName)) {
-                return def;
+    @Override
+    protected boolean applyUpdateToRuntime(final OperationContext context, final ModelNode operation, final String attributeName, final ModelNode resolvedValue,
+                                           final ModelNode currentValue, final HandbackHolder<RevertHandback> handbackHolder) throws OperationFailedException {
+        final Resource resource = context.readResourceForUpdate(PathAddress.EMPTY_ADDRESS);
+        final HornetQServer server = AddressSettingAdd.getServer(context, operation);
+        if(server != null) {
+            final ModelNode model = resource.getModel();
+            final PathAddress address = PathAddress.pathAddress(operation.require(ModelDescriptionConstants.OP_ADDR));
+            final AddressSettings settings = AddressSettingAdd.createSettings(context, model);
+            final HierarchicalRepository<AddressSettings> repository = server.getAddressSettingsRepository();
+            final String match = address.getLastElement().getValue();
+            final AddressSettings existingSettings = repository.getMatch(match);
+            repository.addMatch(match, settings);
+            if(existingSettings != null) {
+                handbackHolder.setHandback(new RevertHandback() {
+                    @Override
+                    public void doRevertUpdateToRuntime() {
+                        // Restore the old settings
+                        repository.addMatch(address.getLastElement().getValue(), existingSettings);
+                    }
+                });
             }
         }
-        return null;
+        return false;
+    }
+
+    @Override
+    protected void revertUpdateToRuntime(final OperationContext context, final ModelNode operation, final String attributeName, final ModelNode valueToRestore,
+                                         final ModelNode valueToRevert, final RevertHandback handback) throws OperationFailedException {
+        if(handback != null) {
+            handback.doRevertUpdateToRuntime();
+        }
+    }
+
+    static interface RevertHandback {
+        void doRevertUpdateToRuntime();
     }
 
 }
