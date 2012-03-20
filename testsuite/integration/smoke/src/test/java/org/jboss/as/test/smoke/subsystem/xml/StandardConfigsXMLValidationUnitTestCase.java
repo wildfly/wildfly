55623b040dd4fc7a3e671ef6b7a18aec5a559492
Jeff Mesnil <jmesnil@gmail.com>
2012-03-20 18:34:20 +0100

    AS7-3881: HornetQ multicast configuration
    * replace jboss.messaging.group.port property to preven a validation
      failure

--- testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/subsystem/xml/StandardConfigsXMLValidationUnitTestCase.java
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/subsystem/xml/StandardConfigsXMLValidationUnitTestCase.java
@@ -184,6 +184,7 @@ public class StandardConfigsXMLValidationUnitTestCase extends AbstractValidation
         result = result.replace("${jboss.management.http.port:9990}", "9990");
         result = result.replace("${jboss.management.https.port:9443}", "9443");
         result = result.replace("${jboss.domain.master.port:9999}", "9999");
+        result = result.replace("${jboss.messaging.group.port:9876}", "9876");
         result = result.replace("${jboss.socket.binding.port-offset:0}", "0");
         return result;
     }
