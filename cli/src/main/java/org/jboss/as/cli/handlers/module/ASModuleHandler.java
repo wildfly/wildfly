8de6381220d5527525c069a18f6a47581f6bc491
Alexey Loubyansky <olubyans@tstudio.(none)>
2012-03-22 19:56:31 +0100

    AS7-4265 command to add and remove AS modules

--- cli/src/main/java/org/jboss/as/cli/handlers/module/ASModuleHandler.java
+++ cli/src/main/java/org/jboss/as/cli/handlers/module/ASModuleHandler.java
@@ -29,6 +29,8 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import javax.xml.stream.XMLOutputFactory;
@@ -45,6 +47,8 @@ import org.jboss.as.cli.handlers.DefaultFilenameTabCompleter;
 import org.jboss.as.cli.handlers.FilenameTabCompleter;
 import org.jboss.as.cli.handlers.WindowsFilenameTabCompleter;
 import org.jboss.as.cli.impl.ArgumentWithValue;
+import org.jboss.as.cli.impl.DefaultCompleter;
+import org.jboss.as.cli.impl.DefaultCompleter.CandidatesProvider;
 import org.jboss.as.cli.operation.ParsedCommandLine;
 import org.jboss.staxmapper.XMLExtendedStreamWriter;
 
@@ -58,14 +62,33 @@ public class ASModuleHandler extends CommandHandlerWithHelp {
 
     private static final String PATH_SEPARATOR = ","; // to make same command work on different systems
 
+    private static final String ACTION_ADD = "add";
+    private static final String ACTION_REMOVE = "remove";
+
+    private final ArgumentWithValue action = new ArgumentWithValue(this, new DefaultCompleter(new CandidatesProvider(){
+        @Override
+        public Collection<String> getAllCandidates(CommandContext ctx) {
+            return Arrays.asList(new String[]{ACTION_ADD, ACTION_REMOVE});
+        }}), 0, "--action");
+
     private final ArgumentWithValue name = new ArgumentWithValue(this, "--name");
-    private final ArgumentWithValue mainClass = new ArgumentWithValue(this, "--main-class");
+    private final ArgumentWithValue mainClass;
     private final ArgumentWithValue resources;
     private final ArgumentWithValue dependencies;
     private final ArgumentWithValue props;
 
     public ASModuleHandler(CommandContext ctx) {
         super("module", false);
+
+        name.addRequiredPreceding(action);
+
+        mainClass = new ArgumentWithValue(this, "--main-class") {
+            @Override
+            public boolean canAppearNext(CommandContext ctx) throws CommandFormatException {
+                final String actionValue = action.getValue(ctx.getParsedCommandLine());
+                return ACTION_ADD.equals(actionValue) && name.isPresent(ctx.getParsedCommandLine()) && super.canAppearNext(ctx);
+            }
+        };
 
         final FilenameTabCompleter pathCompleter = Util.isWindows() ? new WindowsFilenameTabCompleter(ctx) : new DefaultFilenameTabCompleter(ctx);
         resources = new ArgumentWithValue(this, new CommandLineCompleter(){
@@ -88,22 +111,50 @@ public class ASModuleHandler extends CommandHandlerWithHelp {
                 }
                 return value;
             }
+            @Override
+            public boolean canAppearNext(CommandContext ctx) throws CommandFormatException {
+                final String actionValue = action.getValue(ctx.getParsedCommandLine());
+                return ACTION_ADD.equals(actionValue) && name.isPresent(ctx.getParsedCommandLine()) && super.canAppearNext(ctx);
+            }
         };
 
-        dependencies = new ArgumentWithValue(this, "--dependencies");
-        props = new ArgumentWithValue(this, "--properties");
+        dependencies = new ArgumentWithValue(this, "--dependencies") {
+            @Override
+            public boolean canAppearNext(CommandContext ctx) throws CommandFormatException {
+                final String actionValue = action.getValue(ctx.getParsedCommandLine());
+                return ACTION_ADD.equals(actionValue) && name.isPresent(ctx.getParsedCommandLine()) && super.canAppearNext(ctx);
+            }
+        };
+        props = new ArgumentWithValue(this, "--properties") {
+            @Override
+            public boolean canAppearNext(CommandContext ctx) throws CommandFormatException {
+                final String actionValue = action.getValue(ctx.getParsedCommandLine());
+                return ACTION_ADD.equals(actionValue) && name.isPresent(ctx.getParsedCommandLine()) && super.canAppearNext(ctx);
+            }
+        };
     }
 
     @Override
     protected void doHandle(CommandContext ctx) throws CommandLineException {
 
         final ParsedCommandLine parsedCmd = ctx.getParsedCommandLine();
-        final String moduleName = name.getValue(parsedCmd, true);
-
-        final String modulesDir = SecurityActions.getSystemProperty(MODULE_PATH);
-        if(modulesDir == null) {
-            throw new CommandLineException(MODULE_PATH + " system property is not available.");
+        final String actionValue = action.getValue(parsedCmd);
+        if(actionValue == null) {
+            throw new CommandFormatException("Action argument is missing: " + ACTION_ADD + " or " + ACTION_REMOVE);
         }
+
+        if(ACTION_ADD.equals(actionValue)) {
+            addModule(parsedCmd);
+        } else if(ACTION_REMOVE.equals(actionValue)) {
+            removeModule(parsedCmd);
+        } else {
+            throw new CommandFormatException("Unexpected action '" + actionValue + "', expected values: " + ACTION_ADD + ", " + ACTION_REMOVE);
+        }
+    }
+
+    protected void addModule(final ParsedCommandLine parsedCmd) throws CommandLineException {
+
+        final String moduleName = name.getValue(parsedCmd, true);
 
         final String resourcePaths = resources.getValue(parsedCmd, true);
         final String[] resourceArr = resourcePaths.split(PATH_SEPARATOR);
@@ -116,11 +167,11 @@ public class ASModuleHandler extends CommandHandlerWithHelp {
             resourceFiles[i] = f;
         }
 
-        final String modulePath = modulesDir + File.separatorChar + moduleName.replace('.', File.separatorChar);
-        final File moduleDir = new File(modulePath);
+        final File moduleDir = getModulePath(getModulesDir(), moduleName);
         if(moduleDir.exists()) {
-            throw new CommandLineException("Module " + moduleName + " already exists.");
+            throw new CommandLineException("Module " + moduleName + " already exists at " + moduleDir.getAbsolutePath());
         }
+
         if(!moduleDir.mkdirs()) {
             throw new CommandLineException("Failed to create directory " + moduleDir.getAbsolutePath());
         }
@@ -185,6 +236,51 @@ public class ASModuleHandler extends CommandHandlerWithHelp {
                 } catch (IOException e) {}
             }
         }
+    }
+
+    private void removeModule(ParsedCommandLine parsedCmd) throws CommandLineException {
+
+        final String moduleName = name.getValue(parsedCmd, true);
+        final File modulesDir = getModulesDir();
+        File modulePath = getModulePath(modulesDir, moduleName);
+        if(!modulePath.exists()) {
+            throw new CommandLineException("Failed to locate module " + moduleName + " at " + modulePath.getAbsolutePath());
+        }
+
+        final File[] moduleFiles = modulePath.listFiles();
+        if(moduleFiles != null) {
+            for(File f : moduleFiles) {
+                if(!f.delete()) {
+                    throw new CommandLineException("Failed to delete " + f.getAbsolutePath());
+                }
+            }
+        }
+
+        while(!modulesDir.equals(modulePath)) {
+            if(modulePath.list().length > 0) {
+                break;
+            }
+            if(!modulePath.delete()) {
+                throw new CommandLineException("Failed to delete " + modulePath.getAbsolutePath());
+            }
+            modulePath = modulePath.getParentFile();
+        }
+    }
+
+    protected File getModulePath(File modulesDir, final String moduleName) throws CommandLineException {
+        return new File(modulesDir, moduleName.replace('.', File.separatorChar));
+    }
+
+    protected File getModulesDir() throws CommandLineException {
+        final String modulesDirStr = SecurityActions.getSystemProperty(MODULE_PATH);
+        if(modulesDirStr == null) {
+            throw new CommandLineException(MODULE_PATH + " system property is not available.");
+        }
+        final File modulesDir = new File(modulesDirStr);
+        if(!modulesDir.exists()) {
+            throw new CommandLineException("Failed to locate the modules dir on the filesystem: " + modulesDir.getAbsolutePath());
+        }
+        return modulesDir;
     }
 
     public static XMLExtendedStreamWriter create(XMLStreamWriter writer) throws CommandLineException {
