f3e0191bfb65005a09a67315492df4f73ee64803
Emanuel Muckenhuber <emuckenh@redhat.com>
2012-03-26 19:08:08 +0200

    fix the exception handling

--- testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/util/DomainLifecycleUtil.java
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/util/DomainLifecycleUtil.java
@@ -304,10 +304,13 @@ public class DomainLifecycleUtil {
                 return result;
             }
         } catch(Exception e) {
-            if(e instanceof ExecutionException) {
-                // ignore, this might happen if the channel gets closed before we got the response
-            } else if(e instanceof IOException) {
-                throw (IOException) e;
+            if(e instanceof IOException) {
+                final Throwable cause = e.getCause();
+                if(cause instanceof ExecutionException) {
+                    // ignore, this might happen if the channel gets closed before we got the response
+                } else {
+                    throw (IOException) e;
+                }
             } else {
                 throw new RuntimeException(e);
             }
