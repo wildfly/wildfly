165ae6667c71a839b3041e5469192c3f64ac6cb0
flemming.harms@gmail.com <flemming.harms@gmail.com>
2012-03-29 20:14:00 +0200

    Rename test cases according to the naming standard and corrected the copyright header

--- domain-management/src/test/java/org/jboss/as/domain/management/security/ConsoleMock.java
+++ domain-management/src/test/java/org/jboss/as/domain/management/security/ConsoleMock.java
@@ -1,25 +1,23 @@
 /*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
  *
- *  JBoss, Home of Professional Open Source.
- *  Copyright 2011, Red Hat, Inc., and individual contributors
- *  as indicated by the @author tags. See the copyright.txt file in the
- *  distribution for a full listing of individual contributors.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- *  This is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation; either version 2.1 of
- *  the License, or (at your option) any later version.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this software; if not, write to the Free
- *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
 package org.jboss.as.domain.management.security;
@@ -28,23 +26,20 @@ import java.io.IOError;
 import java.util.IllegalFormatException;
 
 /**
- * Describe the purpose
+ * Mock the Java console to simulate console commands for
+ * test purpose
  *
  * @author <a href="mailto:flemming.harms@gmail.com">Flemming Harms</a>
  */
 public class ConsoleMock implements ConsoleWrapper {
-
     private AssertConsoleBuilder responses;
-    private int responseNo;
 
     public ConsoleMock() {
-        this.responseNo = 0;
     }
     
     public void setResponses(AssertConsoleBuilder responses) {
         this.responses = responses;
     }
-
 
     @Override
     public Object format(String fmt, Object... args) throws IllegalFormatException {
