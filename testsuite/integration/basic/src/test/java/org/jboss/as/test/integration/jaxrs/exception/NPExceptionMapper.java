1af4fa3bbf03959c4d8b75f8173631f3ac8f7890
pjanouse <pjanouse@redhat.com>
2012-03-30 14:18:14 +0200

    implementation of Exception handling TestCase

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jaxrs/exception/NPExceptionMapper.java
@@ -0,0 +1,39 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.jaxrs.exception;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Exception mapper - occurred NPE exception remaps to HTTP 404 Error code
+ * 
+ * @author Pavel Janousek
+ */
+@Provider
+public class NPExceptionMapper implements ExceptionMapper<NullPointerException> {
+    public Response toResponse(NullPointerException ex) {
+        return Response.status(HttpServletResponse.SC_NOT_FOUND).build();
+    }
+}
