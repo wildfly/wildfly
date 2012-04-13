6e68c824f284a17ab84ada51eb086738afbd87a2
Thomas Diesler <thomas.diesler@jboss.com>
2012-04-13 09:06:47 +0200

    Revisit and cleanup eclipse compiler warnings

--- osgi/service/src/test/java/org/jboss/as/osgi/parser/BundleRuntimeHandlerTestCase.java
+++ osgi/service/src/test/java/org/jboss/as/osgi/parser/BundleRuntimeHandlerTestCase.java
@@ -35,7 +35,6 @@ import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceRegistry;
 import org.jboss.osgi.framework.Services;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.osgi.framework.Bundle;
@@ -47,8 +46,8 @@ import org.osgi.service.startlevel.StartLevel;
 
 /**
  * @author David Bosschaert
+ * @author Thomas.Diesler@jboss.com
  */
-//@Ignore("[AS7-3556] Replace mocked subsystem model tests with functional tests")
 public class BundleRuntimeHandlerTestCase {
     private BundleContext bundleContext;
     private ModelNode contextResult;
