028eb78c69e11271cdb4678ceb4a75720ca399a5
Ondrej Chaloupka <ochaloup@redhat.com>
2012-04-30 14:33:09 +0200

    [JBQA-5855] EJB2 beans in cluster (failover, loadbalancing, passivation) - to extendedTests

---
+++ testsuite/integration/clust/src/test/java/org/jboss/as/test/clustering/extended/ejb2/stateful/remote/failover/CounterBaseBean.java
@@ -0,0 +1,32 @@
+package org.jboss.as.test.clustering.extended.ejb2.stateful.remote.failover;
+
+import org.jboss.as.test.clustering.NodeNameGetter;
+
+/**
+ * @author Ondrej Chaloupka
+ */
+public abstract class CounterBaseBean {
+    private int count;
+    
+    public void ejbCreate() throws java.rmi.RemoteException, javax.ejb.CreateException {
+        // Creating method for home interface...
+    }
+    
+    public CounterResult increment() {
+        this.count++;
+        return new CounterResult(this.count, getNodeName());
+    }
+
+    public CounterResult decrement() {
+        this.count--;
+        return new CounterResult(this.count, getNodeName());
+    }
+
+    public CounterResult getCount() {
+        return new CounterResult(this.count, getNodeName());
+    }
+
+    private String getNodeName() {
+        return NodeNameGetter.getNodeName();
+    }
+}
