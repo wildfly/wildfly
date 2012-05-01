3e69918331b1ef599b3f67d18225b55d3f03f964
Emanuel Muckenhuber <emuckenh@redhat.com>
2012-05-01 18:26:40 +0200

    don't load the attachment stream into memory

--- protocol/src/main/java/org/jboss/as/protocol/StreamUtils.java
+++ protocol/src/main/java/org/jboss/as/protocol/StreamUtils.java
@@ -23,6 +23,7 @@
 package org.jboss.as.protocol;
 
 import java.io.Closeable;
+import java.io.DataOutput;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -38,17 +39,28 @@ import static org.jboss.as.protocol.ProtocolLogger.ROOT_LOGGER;
  */
 public final class StreamUtils {
 
+    private static final int BUFFER_SIZE = 8192;
+
     private StreamUtils() {
         //
     }
 
     public static void copyStream(final InputStream in, final OutputStream out) throws IOException {
-        final byte[] bytes = new byte[8192];
+        final byte[] bytes = new byte[BUFFER_SIZE];
         int cnt;
         while ((cnt = in.read(bytes)) != -1) {
             out.write(bytes, 0, cnt);
         }
     }
+
+    public static void copyStream(final InputStream in, final DataOutput out) throws IOException {
+        final byte[] bytes = new byte[BUFFER_SIZE];
+        int cnt;
+        while ((cnt = in.read(bytes)) != -1) {
+            out.write(bytes, 0, cnt);
+        }
+    }
+
     public static void safeClose(final Closeable closeable) {
         if (closeable != null) try {
             closeable.close();
