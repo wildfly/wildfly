5b211948fc8b51342ea9714a4861b0c5082c2a68
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-05-01 14:35:18 +1000

    AS7-4683 Fix ThreadLocal leak in Arquillian

--- arquillian/testenricher-msc/src/main/java/org/jboss/arquillian/testenricher/msc/ServiceTargetAssociation.java
+++ arquillian/testenricher-msc/src/main/java/org/jboss/arquillian/testenricher/msc/ServiceTargetAssociation.java
@@ -16,23 +16,31 @@
  */
 package org.jboss.arquillian.testenricher.msc;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import org.jboss.msc.service.ServiceTarget;
 
 /**
  * A thread local {@link ServiceTarget} association
  *
  * @author thomas.diesler@jboss.com
+ * @author Stuart Douglas
  * @since 18-Nov-2010
  */
 public final class ServiceTargetAssociation {
 
-    private static ThreadLocal<ServiceTarget> association = new ThreadLocal<ServiceTarget>();
+    private static ConcurrentMap<String, ServiceTarget> association = new ConcurrentHashMap<String, ServiceTarget>();
 
-    public static ServiceTarget getServiceTarget() {
-        return association.get();
+    public static ServiceTarget getServiceTarget(final String className) {
+        return association.get(className);
     }
 
-    public static void setServiceTarget(ServiceTarget type) {
-        association.set(type);
+    public static void setServiceTarget(final String className, ServiceTarget type) {
+        association.put(className, type);
+    }
+
+    public static void clearServiceTarget(final String className) {
+        association.remove(className);
     }
 }
