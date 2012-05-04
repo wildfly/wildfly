a23847da875605e6312b67edbb0c77f86723f6a9
Shelly McGowan <smcgowan@redhat.com>
2012-05-04 12:39:21 -0400

    JBPAPP-8532 - fix run scripts

--- build/src/main/resources/bin/run.bat
+++ build/src/main/resources/bin/run.bat
@@ -3,7 +3,16 @@ REM Placeholder for people used to run.bat from older version redirecting them t
 echo.
 echo =======================================================================================
 echo.
-echo To start JBoss Application Server please see %CD%\..\README.txt
+echo To start a JBoss Enterprise Application Platform 6 Standalone Server, a single
+echo server instance, use the command:
+echo.
+echo    %CD%\standalone.bat
+echo.
+echo.
+echo To start a JBoss Enterprise Application Platform 6 Managed Domain, allowing control
+echo and management of multiple instances, use the command:
+echo.
+echo    %CD%\domain.bat
 echo.
 echo ========================================================================================
 echo.
--- build/src/main/resources/bin/run.sh
+++ build/src/main/resources/bin/run.sh
@@ -4,7 +4,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo 
 echo ========================================================================================
 echo 
-echo To start JBoss Application Server please see $DIR/../README.txt
+echo To start a JBoss Enterprise Application Platform 6 Standalone Server, a single
+echo server instance, use the command:
+echo
+echo  $DIR/standalone.sh
+echo
+echo
+echo To start a JBoss Enterprise Application Platform 6 Managed Domain, allowing control
+echo and management of multiple instances, use the command:
+echo
+echo  $DIR/domain.sh
 echo 
 echo ========================================================================================
 echo
