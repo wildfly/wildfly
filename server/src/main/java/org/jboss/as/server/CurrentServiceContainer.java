c6df894006a23259b2020b636c261b5f05390a3b
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-05-08 17:02:00 +1000

    AS7-4736 Add permission check to CurrentServiceContainer

--- server/src/main/java/org/jboss/as/server/CurrentServiceContainer.java
+++ server/src/main/java/org/jboss/as/server/CurrentServiceContainer.java
@@ -21,25 +21,38 @@
  */
 package org.jboss.as.server;
 
+import java.security.Permission;
+
 import org.jboss.msc.service.ServiceContainer;
 
 /**
  * Class that provides static access to the current servers ServiceRegistry.
- *
+ * <p/>
  * This is not ideal, however there are some places that require access to this
  * where there is no other way of getting it.
  *
  * @author Stuart Douglas
  */
 public class CurrentServiceContainer {
+    private static final RuntimePermission LOOKUP_CURRENT_SERVICE_CONTAINER = new RuntimePermission("org.jboss.as.server.LOOKUP_CURRENT_SERVICE_CONTAINER");
+    private static final RuntimePermission SET_CURRENT_SERVICE_CONTAINER = new RuntimePermission("org.jboss.as.server.SET_CURRENT_SERVICE_CONTAINER");
 
     private static volatile ServiceContainer serviceContainer;
 
     public static ServiceContainer getServiceContainer() {
+        checkPermission(LOOKUP_CURRENT_SERVICE_CONTAINER);
         return serviceContainer;
     }
 
     static void setServiceContainer(final ServiceContainer serviceContainer) {
+        checkPermission(SET_CURRENT_SERVICE_CONTAINER);
         CurrentServiceContainer.serviceContainer = serviceContainer;
+    }
+
+    private static void checkPermission(final Permission permission) {
+        SecurityManager securityManager = System.getSecurityManager();
+        if (securityManager != null) {
+            securityManager.checkPermission(permission);
+        }
     }
 }
