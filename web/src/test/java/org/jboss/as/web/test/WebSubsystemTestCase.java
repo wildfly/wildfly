7565c97701ab7668649a20c077d5bbedcc2bbb18
kabir <kkhan@redhat.com>
2012-05-10 14:35:53 +0100

    [AS7-3309][AS7-4677] Update web subsystem to use new aliases
    Conflicts:
    
    	web/src/main/java/org/jboss/as/web/WebExtension.java
    	web/src/test/java/org/jboss/as/web/test/WebSubsystemTestCase.java

--- web/src/test/java/org/jboss/as/web/test/WebSubsystemTestCase.java
+++ web/src/test/java/org/jboss/as/web/test/WebSubsystemTestCase.java
@@ -21,11 +21,31 @@
 */
 package org.jboss.as.web.test;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_ATTRIBUTE_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION;
+import static org.jboss.as.web.Constants.ACCESS_LOG;
+import static org.jboss.as.web.Constants.CONFIGURATION;
+import static org.jboss.as.web.Constants.CONNECTOR;
+import static org.jboss.as.web.Constants.DIRECTORY;
+import static org.jboss.as.web.Constants.SETTING;
+import static org.jboss.as.web.Constants.SSL;
+import static org.jboss.as.web.Constants.SSO;
+import static org.jboss.as.web.Constants.VIRTUAL_SERVER;
+import static org.jboss.as.web.WebExtension.SUBSYSTEM_NAME;
+
 import java.io.IOException;
+
+import junit.framework.Assert;
 
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
 import org.jboss.as.subsystem.test.AdditionalInitialization;
+import org.jboss.as.subsystem.test.KernelServices;
 import org.jboss.as.web.WebExtension;
+import org.jboss.dmr.ModelNode;
+import org.junit.Test;
 
 /**
  *
@@ -56,4 +76,100 @@ public class WebSubsystemTestCase extends AbstractSubsystemBaseTest {
     protected void compareXml(String configId, String original, String marshalled) throws Exception {
         super.compareXml(configId, original, marshalled, true);
     }
+
+    @Test
+    public void testAliases() throws Exception {
+        KernelServices services = super.installInController(readResource("subsystem.xml"));
+        ModelNode noAliasModel = services.readWholeModel();
+        ModelNode aliasModel = services.readWholeModel(true);
+
+        testSSLAlias(services, noAliasModel, aliasModel);
+        testSSOAlias(services, noAliasModel, aliasModel);
+        testAccessLogAlias(services, noAliasModel, aliasModel);
+    }
+
+    private void testSSLAlias(KernelServices services, ModelNode noAliasModel, ModelNode aliasModel) throws Exception {
+        //Check the aliased entry is not there
+        String[] targetAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, CONNECTOR, "https", CONFIGURATION, SSL);
+        String[] aliasAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, CONNECTOR, "https", SSL, CONFIGURATION);
+        testAliases(services, noAliasModel, aliasModel, targetAddr, aliasAddr);
+
+        testChangeAttribute(services, "ca-certificate-password", "pwd123", "123pwd", targetAddr, aliasAddr);
+    }
+
+    private void testSSOAlias(KernelServices services, ModelNode noAliasModel, ModelNode aliasModel) throws Exception {
+        String[] targetAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", CONFIGURATION, SSO);
+        String[] aliasAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", SSO, CONFIGURATION);
+        testAliases(services, noAliasModel, aliasModel, targetAddr, aliasAddr);
+
+        testChangeAttribute(services, "domain", "domain123", "123domain", targetAddr, aliasAddr);
+    }
+
+    public void testAccessLogAlias(KernelServices services, ModelNode noAliasModel, ModelNode aliasModel) throws Exception {
+        String[] targetAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", CONFIGURATION, ACCESS_LOG);
+        String[] aliasAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", ACCESS_LOG, CONFIGURATION);
+        testAliases(services, noAliasModel, aliasModel, targetAddr, aliasAddr);
+
+
+        //Check the aliased child entry is not there
+        String[] targetChildMainAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", CONFIGURATION, ACCESS_LOG, SETTING, DIRECTORY);
+        String[] targetChildAliasAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", CONFIGURATION, ACCESS_LOG, DIRECTORY, CONFIGURATION);
+        Assert.assertTrue(noAliasModel.get(targetChildMainAddr).isDefined());
+        Assert.assertFalse(noAliasModel.get(targetChildAliasAddr).isDefined());
+
+        //Check the aliased child entry is there
+        String[] aliasChildMainAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", ACCESS_LOG, CONFIGURATION, SETTING, DIRECTORY);
+        String[] aliasChildAliasAddr = getAddress(SUBSYSTEM, SUBSYSTEM_NAME, VIRTUAL_SERVER, "default-host", ACCESS_LOG, CONFIGURATION, DIRECTORY, CONFIGURATION);
+        Assert.assertEquals(aliasModel.get(targetChildMainAddr), aliasModel.get(targetChildAliasAddr));
+        Assert.assertEquals(aliasModel.get(aliasChildMainAddr), aliasModel.get(targetChildAliasAddr));
+        Assert.assertEquals(aliasModel.get(aliasChildMainAddr), aliasModel.get(aliasChildAliasAddr));
+
+        testChangeAttribute(services, "pattern", "pattern123", "123pattern", targetAddr, aliasAddr);
+
+        testChangeAttribute(services, "path", "path123", "123path", targetChildMainAddr, targetChildAliasAddr);
+        testChangeAttribute(services, "path", "path345", "345path", targetChildMainAddr, aliasChildAliasAddr);
+        testChangeAttribute(services, "path", "path678", "678path", targetChildMainAddr, aliasChildMainAddr);
+    }
+
+    private void testAliases(KernelServices services, ModelNode noAliasModel, ModelNode aliasModel, String[] targetAddr, String[] aliasAddr) throws Exception {
+        //Check the aliased entry is not there
+        Assert.assertTrue(noAliasModel.get(targetAddr).isDefined());
+        Assert.assertFalse(noAliasModel.get(aliasAddr).isDefined());
+
+        //Check the aliased version is there
+        Assert.assertEquals(aliasModel.get(targetAddr), aliasModel.get(aliasAddr));
+    }
+
+    private void testChangeAttribute(KernelServices services, String attributeName, String value1, String value2, String[] targetAddr, String[] aliasAddr) throws Exception {
+        writeAttribute(services, attributeName, value1, aliasAddr);
+        Assert.assertEquals(value1, readAttribute(services, attributeName, aliasAddr));
+        Assert.assertEquals(value1, readAttribute(services, attributeName, targetAddr));
+
+        writeAttribute(services, attributeName, value2, targetAddr);
+        Assert.assertEquals(value2, readAttribute(services, attributeName, aliasAddr));
+        Assert.assertEquals(value2, readAttribute(services, attributeName, targetAddr));
+    }
+
+    private void writeAttribute(KernelServices services, String name, String value, String...address) throws Exception {
+        ModelNode op = services.createOperation(WRITE_ATTRIBUTE_OPERATION, address);
+        op.get(NAME).set(name);
+        op.get(VALUE).set(value);
+        services.executeForResult(op);
+    }
+
+    private String readAttribute(KernelServices services, String name, String...address) throws Exception {
+        ModelNode op = services.createOperation(READ_ATTRIBUTE_OPERATION, address);
+        op.get(NAME).set(name);
+        ModelNode result =  services.executeForResult(op);
+        if (result.isDefined()) {
+            return result.asString();
+        }
+        return null;
+    }
+
+    private String[] getAddress(String...addr) {
+        return addr;
+    }
+
 }
+
