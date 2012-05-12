8842838bfe55c4e45ae3b4b694de2b902b2478ce
Eduardo Sant'Ana da Silva <eduardo.santanadasilva@gmail.com>
2012-05-12 20:49:00 -0300

    AS7-2164 Warn if CDI annotations present but no beans.xml

---
+++ weld/src/main/java/org/jboss/as/weld/deployment/CdiAnnotationProcessor.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.weld.deployment;
+
+import org.jboss.as.server.deployment.Attachments;
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.jboss.as.server.deployment.annotation.CompositeIndex;
+import org.jboss.as.weld.CdiAnnotations;
+
+/**
+ * CdiAnnotationProcessor class. Used to verify the presence of CDI annotations.
+ */
+public class CdiAnnotationProcessor implements DeploymentUnitProcessor {
+    @Override
+    public void deploy(final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+
+        final CompositeIndex index = deploymentUnit.getAttachment(Attachments.COMPOSITE_ANNOTATION_INDEX);
+
+        for (final CdiAnnotations annotation : CdiAnnotations.values()) {
+            if (!index.getAnnotations(annotation.getDotName()).isEmpty()) {
+                CdiAnnotationMarker.mark(deploymentUnit);
+                return;
+            }
+        }
+
+    }
+
+    @Override
+    public void undeploy(final DeploymentUnit context) {
+    }
+}
