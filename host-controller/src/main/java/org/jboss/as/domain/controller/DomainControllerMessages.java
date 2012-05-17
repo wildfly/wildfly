8eccd617170f8cd8e2d993d5eaca12ae99b88e14
James Perkins <jperkins@redhat.com>
2012-05-17 13:26:45 -0700

    [JBPAPP-8973] Fix duplicate id's in the host-controller.

--- host-controller/src/main/java/org/jboss/as/domain/controller/DomainControllerMessages.java
+++ host-controller/src/main/java/org/jboss/as/domain/controller/DomainControllerMessages.java
@@ -511,7 +511,7 @@ public interface DomainControllerMessages {
      *
      * @return an {@link IllegalArgumentException} for the error.
      */
-    @Message(id = 15873, value = "Repository does not contain any deployment with hash %s")
+    @Message(id = 10873, value = "Repository does not contain any deployment with hash %s")
     IllegalStateException deploymentHashNotFoundInRepository(String hash);
 
     /**
@@ -521,7 +521,7 @@ public interface DomainControllerMessages {
      *
      * @return an {@link IllegalStateException} for the error.
      */
-    @Message(id = 15874, value = "Expected only one deployment, found %d")
+    @Message(id = 10874, value = "Expected only one deployment, found %d")
     IllegalStateException expectedOnlyOneDeployment(int i);
 
     /**
