91711195ffb97d63200390e4eca18fd9e1d8b1a6
Scott Marlow <smarlow@redhat.com>
2012-05-30 11:43:16 -0400

    AS7-4786 changed PersistenceUnitServiceImpl to start asynchronously

--- jpa/core/src/main/java/org/jboss/as/jpa/service/PersistenceUnitServiceImpl.java
+++ jpa/core/src/main/java/org/jboss/as/jpa/service/PersistenceUnitServiceImpl.java
@@ -23,6 +23,7 @@
 package org.jboss.as.jpa.service;
 
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.spi.PersistenceProvider;
@@ -53,12 +54,10 @@ import static org.jboss.as.jpa.JpaLogger.JPA_LOGGER;
  * @author Scott Marlow
  */
 public class PersistenceUnitServiceImpl implements Service<PersistenceUnitServiceImpl>, PersistenceUnitService {
-
-
     private final InjectedValue<Map> properties = new InjectedValue<Map>();
-
     private final InjectedValue<DataSource> jtaDataSource = new InjectedValue<DataSource>();
     private final InjectedValue<DataSource> nonJtaDataSource = new InjectedValue<DataSource>();
+    private final InjectedValue<ExecutorService> executorInjector = new InjectedValue<ExecutorService>();
 
     private final PersistenceProviderAdaptor persistenceProviderAdaptor;
     private final PersistenceProvider persistenceProvider;
@@ -75,33 +74,59 @@ public class PersistenceUnitServiceImpl implements Service<PersistenceUnitServic
     }
 
     @Override
-    public void start(StartContext context) throws StartException {
-        try {
-            JPA_LOGGER.startingService("Persistence Unit", pu.getScopedPersistenceUnitName());
-            pu.setTempClassLoaderFactory(new TempClassLoaderFactoryImpl(classLoader));
-            pu.setJtaDataSource(jtaDataSource.getOptionalValue());
-            pu.setNonJtaDataSource(nonJtaDataSource.getOptionalValue());
-            WritableServiceBasedNamingStore.pushOwner(context.getController().getServiceContainer().subTarget());
-            this.entityManagerFactory = createContainerEntityManagerFactory();
-        } finally {
-            pu.setTempClassLoaderFactory(null);    // release the temp classloader factory (only needed when creating the EMF)
-            WritableServiceBasedNamingStore.popOwner();
-        }
+    public void start(final StartContext context) throws StartException {
+        final ExecutorService executor = executorInjector.getValue();
+        final Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JPA_LOGGER.startingService("Persistence Unit", pu.getScopedPersistenceUnitName());
+                    pu.setTempClassLoaderFactory(new TempClassLoaderFactoryImpl(classLoader));
+                    pu.setJtaDataSource(jtaDataSource.getOptionalValue());
+                    pu.setNonJtaDataSource(nonJtaDataSource.getOptionalValue());
+                    WritableServiceBasedNamingStore.pushOwner(context.getController().getServiceContainer().subTarget());
+                    entityManagerFactory = createContainerEntityManagerFactory();
+                    context.complete();
+                } catch (Throwable t) {
+                    context.failed(new StartException(t));
+                } finally {
+                    pu.setTempClassLoaderFactory(null);    // release the temp classloader factory (only needed when creating the EMF)
+                    WritableServiceBasedNamingStore.popOwner();
+                }
+            }
+        };
+        context.asynchronous();
+        executor.execute(task);
     }
 
     @Override
-    public void stop(StopContext context) {
-        JPA_LOGGER.stoppingService("Persistence Unit", pu.getScopedPersistenceUnitName());
-        if (entityManagerFactory != null) {
-            WritableServiceBasedNamingStore.pushOwner(context.getController().getServiceContainer().subTarget());
-            try {
-                entityManagerFactory.close();
-            } finally {
-                entityManagerFactory = null;
-                pu.setTempClassLoaderFactory(null);
-                WritableServiceBasedNamingStore.popOwner();
+    public void stop(final StopContext context) {
+        final ExecutorService executor = executorInjector.getValue();
+        final Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                JPA_LOGGER.stoppingService("Persistence Unit", pu.getScopedPersistenceUnitName());
+                if (entityManagerFactory != null) {
+                    WritableServiceBasedNamingStore.pushOwner(context.getController().getServiceContainer().subTarget());
+                    try {
+                        entityManagerFactory.close();
+                    } catch (Throwable t) {
+                        JPA_LOGGER.failedToStopPUService(t, pu.getScopedPersistenceUnitName());
+                    } finally {
+                        entityManagerFactory = null;
+                        pu.setTempClassLoaderFactory(null);
+                        WritableServiceBasedNamingStore.popOwner();
+                    }
+                }
+                context.complete();
             }
-        }
+        };
+        context.asynchronous();
+        executor.execute(task);
+    }
+
+    public InjectedValue<ExecutorService> getExecutorInjector() {
+        return executorInjector;
     }
 
     @Override
