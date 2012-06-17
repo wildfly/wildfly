8863120b5a7b7663e41ac19f6d4de73676cdd40e
Bernd Eckenfels <bernd@eckenfels.net>
2012-06-17 02:18:10 +0200

    Removed JBoss 6 cruft, adopt to HC/PC JAVA_OPTS

--- build/src/main/resources/bin/domain.conf.bat
+++ build/src/main/resources/bin/domain.conf.bat
@@ -4,12 +4,10 @@ rem #  JBoss Bootstrap Script Configuration                                    #
 rem #                                                                          ##
 rem #############################################################################
 
-rem # $Id: run.conf.bat 88820 2009-05-13 15:25:44Z dimitris@jboss.org $
-
 rem #
-rem # This batch file is executed by run.bat to initialize the environment
-rem # variables that run.bat uses. It is recommended to use this file to
-rem # configure these variables, rather than modifying run.bat itself.
+rem # This batch file is executed by domain.bat to initialize the environment
+rem # variables that domain.bat uses. It is recommended to use this file to
+rem # configure these variables, rather than modifying domain.bat itself.
 rem #
 
 if not "x%JAVA_OPTS%" == "x" (
@@ -29,13 +27,13 @@ rem # Specify the location of the Java home directory (it is recommended that
 rem # this always be set). If set, then "%JAVA_HOME%\bin\java" will be used as
 rem # the Java VM executable; otherwise, "%JAVA%" will be used (see below).
 rem #
-rem set "JAVA_HOME=C:\opt\jdk1.6.0_23"
+rem set "JAVA_HOME=C:\opt\jdk1.7.0_04"
 
 rem #
 rem # Specify the exact Java VM executable to use - only used if JAVA_HOME is
 rem # not set. Default is "java".
 rem #
-rem set "JAVA=C:\opt\jdk1.6.0_23\bin\java"
+rem set "JAVA=%JAVA_HOME%\bin\java"
 
 rem #
 rem # Specify options to pass to the Java VM. Note, there are some additional
@@ -61,6 +59,8 @@ set "JAVA_OPTS=%JAVA_OPTS% -Djboss.domain.default.config=domain.xml -Djboss.host
 rem # Use JBoss Modules lockless mode
 rem set "JAVA_OPTS=%JAVA_OPTS% -Djboss.modules.lockless=true"
 
+:JAVA_OPTS_SET
+
 rem The ProcessController process uses its own set of java options
 set "PROCESS_CONTROLLER_JAVA_OPTS=%JAVA_OPTS%"
 
@@ -74,6 +74,3 @@ rem set "HOST_CONTROLLER_JAVA_OPTS=%HOST_CONTROLLER_JAVA_OPTS% -Xrunjdwp:transpo
 rem # Sample JPDA settings for shared memory debugging
 rem set "PROCESS_CONTROLLER_JAVA_OPTS=%PROCESS_CONTROLLER_JAVA_OPTS% -Xrunjdwp:transport=dt_shmem,address=jboss,server=y,suspend=n"
 rem set "HOST_CONTROLLER_JAVA_OPTS=%HOST_CONTROLLER_JAVA_OPTS% -Xrunjdwp:transport=dt_shmem,address=jboss,server=y,suspend=n"
-
-:JAVA_OPTS_SET
-
