ef44504797d40d1b0627548ec4284f556bd0807b
Tomaz Cerar <tomaz.cerar@gmail.com>
2012-07-04 19:52:52 +0200

    JBPAPP-9353 ibm.jdk module
    for ibm jdk specific paths

--- server/src/main/java/org/jboss/as/server/deployment/module/ServerDependenciesProcessor.java
+++ server/src/main/java/org/jboss/as/server/deployment/module/ServerDependenciesProcessor.java
@@ -38,6 +38,7 @@ import org.jboss.modules.ModuleLoader;
 public class ServerDependenciesProcessor implements DeploymentUnitProcessor {
 
     private static ModuleIdentifier SUN_JDK = ModuleIdentifier.create("sun.jdk");
+    private static ModuleIdentifier IBM_JDK = ModuleIdentifier.create("ibm.jdk");
     private static ModuleIdentifier JAVAX_API = ModuleIdentifier.create("javax.api");
     private static ModuleIdentifier JBOSS_LOGGING = ModuleIdentifier.create("org.jboss.logging");
     private static ModuleIdentifier JBOSS_VFS = ModuleIdentifier.create("org.jboss.vfs");
@@ -52,6 +53,7 @@ public class ServerDependenciesProcessor implements DeploymentUnitProcessor {
         final ModuleSpecification moduleSpecification = deploymentUnit.getAttachment(Attachments.MODULE_SPECIFICATION);
         final ModuleLoader moduleLoader = Module.getBootModuleLoader();
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, SUN_JDK, false, false, true, false));
+        moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, IBM_JDK, false, false, true, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JAVAX_API, false, false, false, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JBOSS_LOGGING, false, false, false, false));
         moduleSpecification.addSystemDependency(new ModuleDependency(moduleLoader, JBOSS_VFS, false, false, false, false));
