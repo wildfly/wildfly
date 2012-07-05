cdee23d8c788da06f462599db83ccb0071ebda9a
jaikiran <jaikiran.pai@gmail.com>
2012-07-05 12:48:24 +0530

    AS7-5103 Don't allow access to local business interface(s) via ejb: namespace

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/view/LocalViewRemoteInvocationTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/view/LocalViewRemoteInvocationTestCase.java
@@ -46,7 +46,6 @@ import java.util.Hashtable;
  * @see https://issues.jboss.org/browse/AS7-3939
  */
 @RunWith(Arquillian.class)
-@RunAsClient
 public class LocalViewRemoteInvocationTestCase {
 
     private static final Logger logger = Logger.getLogger(LocalViewRemoteInvocationTestCase.class);
@@ -77,6 +76,7 @@ public class LocalViewRemoteInvocationTestCase {
      * @throws Exception
      */
     @Test
+    @RunAsClient
     public void testLocalViewInvocationRemotelyOnSLSB() throws Exception {
         final LocalEcho localEcho = (LocalEcho) context.lookup("ejb:" + APP_NAME + "/" + MODULE_NAME + "/" + DISTINCT_NAME + "/" + StatelessEcho.class.getSimpleName() + "!" + LocalEcho.class.getName());
         final String message = "Silence!";
@@ -95,6 +95,7 @@ public class LocalViewRemoteInvocationTestCase {
      * @throws Exception
      */
     @Test
+    @RunAsClient
     public void testLocalViewInvocationRemotelyOnSFSB() throws Exception {
         final LocalEcho localEcho = (LocalEcho) context.lookup("ejb:" + APP_NAME + "/" + MODULE_NAME + "/" + DISTINCT_NAME + "/" + StatefulEcho.class.getSimpleName() + "!" + LocalEcho.class.getName() + "?stateful");
         final String message = "Silence!";
@@ -113,6 +114,7 @@ public class LocalViewRemoteInvocationTestCase {
      * @throws Exception
      */
     @Test
+    @RunAsClient
     public void testLocalViewInvocationRemotelyOnSingleton() throws Exception {
         final LocalEcho localEcho = (LocalEcho) context.lookup("ejb:" + APP_NAME + "/" + MODULE_NAME + "/" + DISTINCT_NAME + "/" + SingletonEcho.class.getSimpleName() + "!" + LocalEcho.class.getName());
         final String message = "Silence!";
@@ -125,5 +127,65 @@ public class LocalViewRemoteInvocationTestCase {
         }
     }
 
+
+    /**
+     * Test that an in-vm invocation on a local business interface, of a SLSB, using the ejb: namespace fails, since only
+     * remote view invocations are allowed for ejb: namespace
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testServerInVMInvocationOnLocalViewOfSLSB() throws Exception {
+        final Context jndiContext = new InitialContext();
+        final LocalEcho localEcho = (LocalEcho) jndiContext.lookup("ejb:" + APP_NAME + "/" + MODULE_NAME + "/" + DISTINCT_NAME + "/" + StatelessEcho.class.getSimpleName() + "!" + LocalEcho.class.getName());
+        final String message = "Silence!";
+        try {
+            final String echo = localEcho.echo(message);
+            Assert.fail("Remote invocation on a local view " + LocalEcho.class.getName() + " was expected to fail");
+        } catch (IllegalStateException nsee) {
+            // expected
+            logger.info("Got the expected exception on invoking on a local view, remotely", nsee);
+        }
+    }
+
+    /**
+     * Test that an in-vm invocation on a local business interface, of a SFSB, using the ejb: namespace fails, since only
+     * remote view invocations are allowed for ejb: namespace
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testServerInVMInvocationOnLocalViewOfSFSB() throws Exception {
+        final Context jndiContext = new InitialContext();
+        final LocalEcho localEcho = (LocalEcho) jndiContext.lookup("ejb:" + APP_NAME + "/" + MODULE_NAME + "/" + DISTINCT_NAME + "/" + StatefulEcho.class.getSimpleName() + "!" + LocalEcho.class.getName() + "?stateful");
+        final String message = "Silence!";
+        try {
+            final String echo = localEcho.echo(message);
+            Assert.fail("Remote invocation on a local view " + LocalEcho.class.getName() + " was expected to fail");
+        } catch (IllegalStateException nsee) {
+            // expected
+            logger.info("Got the expected exception on invoking on a local view, remotely", nsee);
+        }
+    }
+
+    /**
+     * Test that an in-vm invocation on a local business interface, of a singleton bean, using the ejb: namespace fails, since only
+     * remote view invocations are allowed for ejb: namespace
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testServerInVMInvocationOnLocalViewOfSingleton() throws Exception {
+        final Context jndiContext = new InitialContext();
+        final LocalEcho localEcho = (LocalEcho) jndiContext.lookup("ejb:" + APP_NAME + "/" + MODULE_NAME + "/" + DISTINCT_NAME + "/" + SingletonEcho.class.getSimpleName() + "!" + LocalEcho.class.getName());
+        final String message = "Silence!";
+        try {
+            final String echo = localEcho.echo(message);
+            Assert.fail("Remote invocation on a local view " + LocalEcho.class.getName() + " was expected to fail");
+        } catch (IllegalStateException nsee) {
+            // expected
+            logger.info("Got the expected exception on invoking on a local view, remotely", nsee);
+        }
+    }
 
 }
