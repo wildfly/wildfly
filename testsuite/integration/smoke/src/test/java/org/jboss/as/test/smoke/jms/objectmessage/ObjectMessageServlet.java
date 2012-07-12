5a3cb9f252197d74e53dafd83681791a5af4fd86
Jeff Mesnil <jmesnil@gmail.com>
2012-07-12 12:24:16 +0200

    [AS7-1271] ObjectMessage ClassLoader isolation
    * add a test to check that a ObjectMessage with a custom serializable
      can properly be produced from a WAR and consumed by a MDB (provided
      the EAR is built appropriately)

---
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/jms/objectmessage/ObjectMessageServlet.java
@@ -0,0 +1,90 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.smoke.jms.objectmessage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author <a href="http://jmesnil.net">Jeff Mesnil</a> (c) 2012 Red Hat Inc.
+ */
+@WebServlet("/objectmessage")
+public class ObjectMessageServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -4858704774886318215L;
+
+    @Resource(mappedName = "java:/ConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Resource(mappedName = "java:/queue/objectMessageQueue")
+    private Destination destination;
+
+    @Resource(mappedName = "java:/queue/replyToQueue")
+    private Destination replyTo;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Connection connection = null;
+        resp.setContentType("text/plain");
+        PrintWriter out = resp.getWriter();
+        try {
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(destination);
+            ObjectMessage message = session.createObjectMessage();
+            MyPayload resource = new MyPayload("This is my payload");
+            message.setObject(resource);
+            message.setJMSReplyTo(replyTo);
+            producer.send(message);
+            out.write("OK");
+        } catch (JMSException e) {
+            e.printStackTrace(out);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
