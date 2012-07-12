475dc789e224f549818567c956db25b0773823d9
Scott Marlow <smarlow@redhat.com>
2012-07-12 12:25:25 -0400

    add class transformer test using mock persistence provider

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/mockprovider/classtransformer/ClassFileTransformerTestCase.java
@@ -0,0 +1,124 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.jpa.mockprovider.classtransformer;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Hibernate "hibernate.ejb.use_class_enhancer" test that causes hibernate to add a
+ * javax.persistence.spi.ClassTransformer to the pu.
+ *
+ * @author Scott Marlow
+ */
+@RunWith(Arquillian.class)
+public class ClassFileTransformerTestCase {
+
+    private static final String ARCHIVE_NAME = "jpa_classTransformerTestWithMockProvider";
+
+    private static final String persistence_xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
+            "<persistence xmlns=\"http://java.sun.com/xml/ns/persistence\" version=\"1.0\">" +
+            "  <persistence-unit name=\"mypc\">" +
+            "    <description>Persistence Unit." +
+            "    </description>" +
+            " <provider>org.jboss.as.test.integration.jpa.mockprovider.classtransformer.TestPersistenceProvider</provider>" +
+            "  <jta-data-source>java:jboss/datasources/ExampleDS</jta-data-source>" +
+            "<properties> <property name=\"hibernate.hbm2ddl.auto\" value=\"create-drop\"/>" +
+            "<property name=\"hibernate.show_sql\" value=\"true\"/>" +
+            "<property name=\"hibernate.ejb.use_class_enhancer\" value=\"true\"/>" +
+            "</properties>" +
+            "  </persistence-unit>" +
+            "</persistence>";
+
+    @Deployment
+    public static Archive<?> deploy() {
+        JavaArchive persistenceProvider = ShrinkWrap.create(JavaArchive.class, "testpersistenceprovider.jar");
+        persistenceProvider.addClasses(
+                    TestClassTransformer.class,
+                    TestEntityManagerFactory.class,
+                    TestPersistenceProvider.class
+                );
+
+        // META-INF/services/javax.persistence.spi.PersistenceProvider
+        persistenceProvider.addAsResource(new StringAsset("org.jboss.as.test.integration.jpa.mockprovider.classtransformer.TestPersistenceProvider"),
+                "META-INF/services/javax.persistence.spi.PersistenceProvider");
+
+        EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, ARCHIVE_NAME + ".ear");
+
+        JavaArchive ejbjar = ShrinkWrap.create(JavaArchive.class, "ejbjar.jar");
+        ejbjar.addAsManifestResource(emptyEjbJar(), "ejb-jar.xml");
+        ejbjar.addClasses(ClassFileTransformerTestCase.class,
+                SFSB1.class
+        );
+        ejbjar.addAsManifestResource(new StringAsset(persistence_xml), "persistence.xml");
+
+        ear.addAsModule(ejbjar);        // add ejbjar to root of ear
+
+        JavaArchive lib = ShrinkWrap.create(JavaArchive.class, "lib.jar");
+        lib.addClasses(Employee.class, ClassFileTransformerTestCase.class);
+        ear.addAsLibraries(lib, persistenceProvider);
+        return ear;
+
+    }
+
+    private static StringAsset emptyEjbJar() {
+        return new StringAsset(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<ejb-jar xmlns=\"http://java.sun.com/xml/ns/javaee\" \n" +
+                "         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n" +
+                "         xsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/ejb-jar_3_0.xsd\"\n" +
+                "         version=\"3.0\">\n" +
+                "   \n" +
+                "</ejb-jar>");
+    }
+
+    @ArquillianResource
+    private InitialContext iniCtx;
+
+    @Test
+    public void test_use_class_enhancer() throws Exception {
+        try {
+            assertTrue("entity classes are enhanced", TestClassTransformer.getTransformedClasses().size() > 0 );
+        } finally {
+            TestClassTransformer.clearTransformedClasses();
+        }
+    }
+
+
+}
