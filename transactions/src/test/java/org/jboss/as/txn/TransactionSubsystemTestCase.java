b27d0a500950670203e3d14e08709a516517f4af
Emanuel Muckenhuber <emuckenh@redhat.com>
2012-07-20 14:25:45 +0200

    in case an older version is regsitered reject expressions for now.

--- transactions/src/test/java/org/jboss/as/txn/TransactionSubsystemTestCase.java
+++ transactions/src/test/java/org/jboss/as/txn/TransactionSubsystemTestCase.java
@@ -21,10 +21,25 @@
 */
 package org.jboss.as.txn;
 
-import java.io.IOException;
-
+import org.jboss.as.controller.ModelVersion;
+import org.jboss.as.controller.OperationFailedException;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION;
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
+import org.jboss.as.subsystem.test.KernelServices;
+import org.jboss.as.subsystem.test.KernelServicesBuilder;
 import org.jboss.as.txn.subsystem.TransactionExtension;
+import org.jboss.dmr.ModelNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  *
@@ -46,4 +61,41 @@ public class TransactionSubsystemTestCase extends AbstractSubsystemBaseTest {
     protected void compareXml(String configId, String original, String marshalled) throws Exception {
         super.compareXml(configId, original, marshalled, true);
     }
+
+    @Test
+    public void testTransformers() throws Exception {
+        String subsystemXml = readResource("subsystem.xml");
+        ModelVersion modelVersion = ModelVersion.create(1, 1, 0);
+        KernelServicesBuilder builder = createKernelServicesBuilder(null)
+                .setSubsystemXml(subsystemXml);
+
+        // Add legacy subsystems
+        builder.createLegacyKernelServicesBuilder(null, modelVersion)
+            .addMavenResourceURL("org.jboss.as:jboss-as-transactions:7.1.2.Final");
+
+        KernelServices mainServices = builder.build();
+        KernelServices legacyServices = mainServices.getLegacyServices(modelVersion);
+        Assert.assertNotNull(legacyServices);
+
+        checkSubsystemTransformer(mainServices, modelVersion);
+
+        final ModelNode operation = new ModelNode();
+        operation.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
+        operation.get(OP_ADDR).add(SUBSYSTEM, TransactionExtension.SUBSYSTEM_NAME);
+        operation.get(NAME).set("status-socket-binding");
+        operation.get(VALUE).set("${org.jboss.test:default-socket-binding}");
+
+        final ModelNode mainResult = mainServices.executeOperation(operation);
+        Assert.assertTrue(SUCCESS.equals(mainResult.get(OUTCOME).asString()));
+
+        try {
+            mainServices.transformOperation(modelVersion, operation);
+            // legacyServices.executeOperation(operation); would actually work - however it does not understand the expr
+            // so we need to reject the expression on the DC already
+            Assert.fail("should reject the expression");
+        } catch (OperationFailedException e) {
+            // OK
+        }
+    }
+
 }
