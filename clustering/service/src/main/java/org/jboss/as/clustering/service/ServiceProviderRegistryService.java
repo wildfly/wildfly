3fd9d57d2837deabed9c54b2d8f658ac23f57036
Paul Ferraro <paul.ferraro@redhat.com>
2012-07-24 17:30:41 -0400

    AS7-5218 SingletonService is not stopped after cluster split/merge

--- clustering/service/src/main/java/org/jboss/as/clustering/service/ServiceProviderRegistryService.java
+++ clustering/service/src/main/java/org/jboss/as/clustering/service/ServiceProviderRegistryService.java
@@ -22,9 +22,9 @@
 
 package org.jboss.as.clustering.service;
 
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -37,6 +37,7 @@ import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
 import org.jboss.as.clustering.ClusterNode;
 import org.jboss.as.clustering.GroupMembershipListener;
 import org.jboss.as.clustering.GroupMembershipNotifier;
+import org.jboss.as.clustering.GroupRpcDispatcher;
 import org.jboss.as.clustering.impl.CoreGroupCommunicationService;
 import org.jboss.as.clustering.infinispan.atomic.AtomicMapCache;
 import org.jboss.as.clustering.infinispan.invoker.BatchOperation;
@@ -52,23 +53,29 @@ import org.jboss.msc.value.Value;
  * @author Paul Ferraro
  */
 @org.infinispan.notifications.Listener(sync = false)
-public class ServiceProviderRegistryService implements ServiceProviderRegistry, GroupMembershipListener, Service<ServiceProviderRegistry> {
+public class ServiceProviderRegistryService implements ServiceProviderRegistry, ServiceProviderRegistryRpcHandler, GroupMembershipListener, Service<ServiceProviderRegistry> {
 
     public static ServiceName getServiceName(String name) {
         return CoreGroupCommunicationService.getServiceName(name).append("registry");
     }
 
+    final ServiceName name;
+    final ServiceProviderRegistryRpcHandler handler = new RpcDispatcher();
     @SuppressWarnings("rawtypes")
     private final Value<Cache> cacheRef;
     private final Value<GroupMembershipNotifier> notifierRef;
+    private final Value<GroupRpcDispatcher> dispatcherRef;
     private final Map<String, Listener> listeners = new ConcurrentHashMap<String, Listener>();
 
+    volatile GroupRpcDispatcher dispatcher;
     private volatile GroupMembershipNotifier notifier;
     private volatile Cache<String, Map<ClusterNode, Void>> cache;
 
-    public ServiceProviderRegistryService(@SuppressWarnings("rawtypes") Value<Cache> cacheRef, Value<GroupMembershipNotifier> notifierRef) {
+    public ServiceProviderRegistryService(ServiceName name, @SuppressWarnings("rawtypes") Value<Cache> cacheRef, Value<GroupRpcDispatcher> dispatcherRef, Value<GroupMembershipNotifier> notifierRef) {
+        this.name = name;
         this.cacheRef = cacheRef;
         this.notifierRef = notifierRef;
+        this.dispatcherRef = dispatcherRef;
     }
 
     @Override
@@ -79,7 +86,9 @@ public class ServiceProviderRegistryService implements ServiceProviderRegistry, 
             @Override
             public Set<ClusterNode> invoke(Cache<String, Map<ClusterNode, Void>> cache) {
                 Map<ClusterNode, Void> map = cache.putIfAbsent(service, null);
-                map.put(node, null);
+                if (!map.containsKey(node)) {
+                    map.put(node, null);
+                }
                 return map.keySet();
             }
         };
@@ -107,12 +116,32 @@ public class ServiceProviderRegistryService implements ServiceProviderRegistry, 
     }
 
     @Override
+    public List<String> getServices(final ClusterNode node) {
+        Operation<List<String>> operation = new Operation<List<String>>() {
+            @Override
+            public List<String> invoke(Cache<String, Map<ClusterNode, Void>> cache) {
+                Set<String> services = cache.keySet();
+                List<String> result = new ArrayList<String>(services.size());
+                for (String service: services) {
+                    if (cache.get(service).keySet().contains(node)) {
+                        result.add(service);
+                    }
+                }
+                return result;
+            }
+        };
+        return this.invoke(operation);
+    }
+
+    @Override
     public ServiceProviderRegistry getValue() throws IllegalStateException, IllegalArgumentException {
         return this;
     }
 
     @Override
     public void start(StartContext context) throws StartException {
+        this.dispatcher = this.dispatcherRef.getValue();
+        this.dispatcher.registerRPCHandler(this.name.getCanonicalName(), this);
         this.notifier = this.notifierRef.getValue();
         this.notifier.registerGroupMembershipListener(this);
         @SuppressWarnings("unchecked")
@@ -125,38 +154,67 @@ public class ServiceProviderRegistryService implements ServiceProviderRegistry, 
     public void stop(StopContext context) {
         this.cache.removeListener(this);
         this.notifier.unregisterGroupMembershipListener(this);
+        this.dispatcher.unregisterRPCHandler(this.name.getCanonicalName(), this);
     }
 
     @Override
-    public void membershipChanged(List<ClusterNode> deadMembers, List<ClusterNode> newMembers, List<ClusterNode> allMembers) {
-        this.purgeDeadMembers(deadMembers, false);
-    }
-
-    @Override
-    public void membershipChangedDuringMerge(List<ClusterNode> deadMembers, List<ClusterNode> newMembers, List<ClusterNode> allMembers, List<List<ClusterNode>> originatingGroups) {
-        this.purgeDeadMembers(deadMembers, true);
-    }
-
-    private void purgeDeadMembers(final List<ClusterNode> deadNodes, final boolean merge) {
-        Operation<List<Map.Entry<String, Set<ClusterNode>>>> operation = new Operation<List<Map.Entry<String, Set<ClusterNode>>>>() {
+    public void membershipChanged(final List<ClusterNode> deadMembers, List<ClusterNode> newMembers, List<ClusterNode> allMembers) {
+        Operation<Map<String, Set<ClusterNode>>> operation = new Operation<Map<String, Set<ClusterNode>>>() {
             @Override
-            public List<Map.Entry<String, Set<ClusterNode>>> invoke(Cache<String, Map<ClusterNode, Void>> cache) {
-                // Collect services whose set of providing nodes has changed
-                List<Map.Entry<String, Set<ClusterNode>>> entries = new ArrayList<Map.Entry<String, Set<ClusterNode>>>(cache.size());
-                // Remove dead nodes for each service
-                for (String key: cache.keySet()) {
-                    Map<ClusterNode, Void> map = cache.getAdvancedCache().withFlags(Flag.CACHE_MODE_LOCAL).get(key);
-                    if (map != null) {
-                        Set<ClusterNode> nodes = map.keySet();
-                        if (nodes.removeAll(deadNodes)) {
-                            entries.add(new AbstractMap.SimpleImmutableEntry<String, Set<ClusterNode>>(key, nodes));
+            public Map<String, Set<ClusterNode>> invoke(Cache<String, Map<ClusterNode, Void>> cache) {
+                // Collect service provider updates
+                Map<String, Set<ClusterNode>> updates = new HashMap<String, Set<ClusterNode>>();
+                ServiceProviderRegistryService.this.purgeDeadMembers(deadMembers, updates);
+                return updates;
+            }
+        };
+        this.notifyListeners(this.invoke(operation), false);
+    }
+
+    @Override
+    public void membershipChangedDuringMerge(final List<ClusterNode> deadMembers, final List<ClusterNode> newMembers, List<ClusterNode> allMembers, final List<List<ClusterNode>> originatingGroups) {
+        Operation<Map<String, Set<ClusterNode>>> operation = new Operation<Map<String, Set<ClusterNode>>>() {
+            @Override
+            public Map<String, Set<ClusterNode>> invoke(Cache<String, Map<ClusterNode, Void>> cache) {
+                // Collect service provider updates
+                Map<String, Set<ClusterNode>> updates = new HashMap<String, Set<ClusterNode>>();
+                if (newMembers.isEmpty()) {
+                    for (String service: cache.keySet()) {
+                        updates.put(service, cache.get(service).keySet());
+                    }
+                } else {
+                    for (ClusterNode node: newMembers) {
+                        // Re-assert services for new members following merge since these may have been lost following split
+                        List<String> services = ServiceProviderRegistryService.this.handler.getServices(node);
+                        for (String service: services) {
+                            Map<ClusterNode, Void> nodes = cache.getAdvancedCache().withFlags(Flag.CACHE_MODE_LOCAL).putIfAbsent(service, null);
+                            nodes.put(node, null);
+                            updates.put(service, Collections.unmodifiableSet(nodes.keySet()));
                         }
                     }
                 }
-                return entries;
+                ServiceProviderRegistryService.this.purgeDeadMembers(deadMembers, updates);
+                return updates;
             }
         };
-        for (Map.Entry<String, Set<ClusterNode>> entry: this.invoke(operation)) {
+        this.notifyListeners(this.invoke(operation), true);
+    }
+
+    void purgeDeadMembers(List<ClusterNode> deadNodes, Map<String, Set<ClusterNode>> updates) {
+        // Remove dead nodes for each service
+        for (String key: cache.keySet()) {
+            Map<ClusterNode, Void> map = cache.getAdvancedCache().withFlags(Flag.CACHE_MODE_LOCAL).get(key);
+            if (map != null) {
+                Set<ClusterNode> nodes = map.keySet();
+                if (nodes.removeAll(deadNodes)) {
+                    updates.put(key, Collections.unmodifiableSet(nodes));
+                }
+            }
+        }
+    }
+
+    private void notifyListeners(Map<String, Set<ClusterNode>> updates, boolean merge) {
+        for (Map.Entry<String, Set<ClusterNode>> entry: updates.entrySet()) {
             Listener listener = this.listeners.get(entry.getKey());
             if (listener != null) {
                 listener.serviceProvidersChanged(entry.getValue(), merge);
@@ -168,17 +226,24 @@ public class ServiceProviderRegistryService implements ServiceProviderRegistry, 
     public void modified(CacheEntryModifiedEvent<String, Map<ClusterNode, Void>> event) {
         // Only respond to remote post-modify events
         if (event.isPre() || event.isOriginLocal()) return;
-        final String service = event.getKey();
-        Listener listener = this.listeners.get(service);
-        if (listener != null) {
-            listener.serviceProvidersChanged(event.getValue().keySet(), false);
-        }
+        this.notifyListeners(Collections.singletonMap(event.getKey(), event.getValue().keySet()), false);
     }
 
     private <R> R invoke(Operation<R> operation) {
         return new BatchOperation<String, Map<ClusterNode, Void>, R>(operation).invoke(this.cache);
     }
 
-    abstract class Operation<R> implements CacheInvoker.Operation<String, Map<ClusterNode, Void>, R> {
+    static interface Operation<R> extends CacheInvoker.Operation<String, Map<ClusterNode, Void>, R> {
+    }
+
+    class RpcDispatcher implements ServiceProviderRegistryRpcHandler {
+        @Override
+        public List<String> getServices(ClusterNode node) {
+            try {
+                return ServiceProviderRegistryService.this.dispatcher.callMethodOnNode(ServiceProviderRegistryService.this.name.getCanonicalName(), "getServices", new Object[] { node }, new Class[] { ClusterNode.class }, node);
+            } catch (Exception e) {
+                return Collections.emptyList();
+            }
+        }
     }
 }
