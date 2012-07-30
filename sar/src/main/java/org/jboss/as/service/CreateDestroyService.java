6abc80982f77325cf924d87d24cfb4f5a5fdc3e7
Eduardo Martins <emartins@redhat.com>
2012-07-30 17:19:25 +0100

    AS7-1699 feature

--- sar/src/main/java/org/jboss/as/service/CreateDestroyService.java
+++ sar/src/main/java/org/jboss/as/service/CreateDestroyService.java
@@ -2,6 +2,8 @@ package org.jboss.as.service;
 
 import java.lang.reflect.Method;
 
+import org.jboss.as.naming.ManagedReference;
+import org.jboss.as.service.component.ServiceComponentInstantiator;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
@@ -11,16 +13,21 @@ import org.jboss.msc.service.StopContext;
  *
  * @author John E. Bailey
  * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
+ * @author Eduardo Martins
  */
 final class CreateDestroyService extends AbstractService {
 
     private final Method createMethod;
     private final Method destroyMethod;
 
-    CreateDestroyService(final Object mBeanInstance, final Method createMethod, final Method destroyMethod) {
+    private final ServiceComponentInstantiator componentInstantiator;
+    private ManagedReference managedReference;
+
+    CreateDestroyService(final Object mBeanInstance, final Method createMethod, final Method destroyMethod, ServiceComponentInstantiator componentInstantiator) {
         super(mBeanInstance);
         this.createMethod = createMethod;
         this.destroyMethod = destroyMethod;
+        this.componentInstantiator = componentInstantiator;
     }
 
     /** {@inheritDoc} */
@@ -33,12 +40,18 @@ final class CreateDestroyService extends AbstractService {
         } catch (final Exception e) {
             throw SarMessages.MESSAGES.failedExecutingLegacyMethod(e, "create()");
         }
+        if(componentInstantiator != null) {
+            managedReference = componentInstantiator.initializeInstance(getValue());
+        }
     }
 
     /** {@inheritDoc} */
     public void stop(final StopContext context) {
         if (SarLogger.ROOT_LOGGER.isTraceEnabled()) {
             SarLogger.ROOT_LOGGER.tracef("Destroying Service: %s", context.getController().getName());
+        }
+        if(managedReference != null) {
+            managedReference.release();
         }
         try {
             invokeLifecycleMethod(destroyMethod);
