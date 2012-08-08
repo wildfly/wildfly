190de8d1b01ad07d9438bf98fd0c8ebf7dbdca43
James Livingston <jlivings@redhat.com>
2012-08-08 10:06:59 +1000

    Fix AS-5172. Allow access to META-INF/ in WAR files via Classloader.getResource()

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/deployment/classloading/war/WarClassLoadingTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/deployment/classloading/war/WarClassLoadingTestCase.java
@@ -21,10 +21,16 @@
  */
 package org.jboss.as.test.integration.deployment.classloading.war;
 
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
@@ -37,6 +43,8 @@ public class WarClassLoadingTestCase {
     public static Archive<?> deploy() {
         WebArchive war = ShrinkWrap.create(WebArchive.class);
         war.addClass(WarClassLoadingTestCase.class);
+        war.add(EmptyAsset.INSTANCE, "META-INF/example.txt");
+        war.add(EmptyAsset.INSTANCE, "example2.txt");
         JavaArchive libJar = ShrinkWrap.create(JavaArchive.class);
         libJar.addClass(WebInfLibClass.class);
         war.addAsLibraries(libJar);
@@ -46,6 +54,26 @@ public class WarClassLoadingTestCase {
     @Test
     public void testWebInfLibAccessible() throws ClassNotFoundException {
         loadClass("org.jboss.as.test.integration.deployment.classloading.war.WebInfLibClass");
+    }
+
+    /*
+     * Test case for AS7-5172. Ensure META-INF/* is accessible
+     */
+    @Test
+    public void testMetaInfAccessible() throws ClassNotFoundException {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL manifestResource = cl.getResource("META-INF/example.txt");
+        assertNotNull(manifestResource);
+    }
+
+    /*
+     * Test case for AS7-5172. Ensure that non-META-INF/* is not accessible
+     */
+    @Test
+    public void testNonMetaInfNotAccessible() throws ClassNotFoundException {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL nonManifestResource = cl.getResource("example2.txt");
+        assertNull(nonManifestResource);
     }
 
     private static Class<?> loadClass(String name) throws ClassNotFoundException {
