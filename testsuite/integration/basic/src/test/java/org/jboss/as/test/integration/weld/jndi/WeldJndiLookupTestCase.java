1af1f2871ef60469b2cc3bee324779c800c5c936
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-08-21 12:25:46 +1000

    Add test case

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/jndi/WeldJndiLookupTestCase.java
@@ -0,0 +1,68 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.weld.jndi;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Stuart Douglas
+ */
+@RunWith(Arquillian.class)
+public class WeldJndiLookupTestCase {
+
+    @Deployment
+    public static Archive<?> deploy() {
+
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "WeldJndiLookupTestCase.jar");
+        jar.addPackage(WeldJndiLookupTestCase.class.getPackage());
+        jar.add(EmptyAsset.INSTANCE, "META-INF/beans.xml");
+        return jar;
+    }
+
+    @Inject
+    private StartupSingletonEjb ejb;
+
+    @Test
+    public void testBeanManagerCanBeLookedUp() throws NamingException {
+        BeanManager bm = (BeanManager) new InitialContext().lookup("java:comp/BeanManager");
+        Assert.assertNotNull(bm);
+    }
+
+    @Test
+    public void testOtherJNDIbindingsAreAvailbleAtStartup() {
+        Assert.assertEquals("WeldJndiLookupTestCase", ejb.getName());
+    }
+
+}
