83e727d244687300020846701c6d4175f140ff59
Paul Ferraro <paul.ferraro@redhat.com>
2012-08-24 18:49:39 -0400

    AS7-4728 Sessions sometimes fail to passivate with unable to acquire lock after with 0 milliseconds timeout AS7-4753 Use Flag.FAIL_SILENTLY for eviction cache operations

--- clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/invoker/BatchCacheInvoker.java
+++ clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/invoker/BatchCacheInvoker.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -25,35 +25,33 @@ import org.infinispan.Cache;
 import org.infinispan.context.Flag;
 
 /**
- * Encapsulates logic used to invoke an operation on a cache.
- *
+ * Invoker that starts and ends a batch, if none exists.
  * @author Paul Ferraro
  */
-public interface CacheInvoker {
-    /**
-     * Invokes the specified operation on the specified cache.
-     *
-     * @param <R> the type of the cache operation result
-     * @param cache an infinispan cache
-     * @param operation a cache operation
-     * @param flags an optional set of invocation flags
-     * @return the result of the cache operation
-     */
-    <K, V, R> R invoke(Cache<K, V> cache, Operation<K, V, R> operation, Flag... flags);
+public class BatchCacheInvoker implements CacheInvoker {
 
-    /**
-     * Encapsulates a cache operation.
-     * @param <K> the cache key
-     * @param <V> the cache value
-     * @param <R> the return type of the cache operation
-     */
-    interface Operation<K, V, R> {
-        /**
-         * Invoke some operation on the specified cache.
-         *
-         * @param cache an infinispan cache
-         * @return the result of the cache operation
-         */
-        R invoke(Cache<K, V> cache);
+    private final CacheInvoker invoker;
+
+    public BatchCacheInvoker(CacheInvoker invoker) {
+        this.invoker = invoker;
+    }
+
+    public BatchCacheInvoker() {
+        this(new SimpleCacheInvoker());
+    }
+
+    @Override
+    public <K, V, R> R invoke(Cache<K, V> cache, Operation<K, V, R> operation, Flag... flags) {
+        boolean started = cache.startBatch();
+        boolean success = false;
+        try {
+            R result = this.invoker.invoke(cache, operation, flags);
+            success = true;
+            return result;
+        } finally {
+            if (started) {
+                cache.endBatch(success);
+            }
+        }
     }
 }
