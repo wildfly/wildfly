502659322b97ec7fb6b3cd1a73eb1214be319715
Thomas Diesler <thomas.diesler@jboss.com>
2012-08-29 11:49:45 +0200

    [AS7-5364] Allow WebService deployments as OSGi bundles

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/osgi/webapp/TestServletContext.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/osgi/webapp/TestServletContext.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.smoke.osgi.bundleA;
+package org.jboss.as.test.integration.osgi.webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
