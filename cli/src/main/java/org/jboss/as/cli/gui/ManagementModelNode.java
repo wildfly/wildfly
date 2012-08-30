eb448275d14a72eb57166f1744d6ffc707b02660
Stan Silvert <ssilvert@redhat.com>
2012-08-30 12:22:15 -0400

    AS7-5463 CLI GUI: Unable to open node with a space in the address.

--- cli/src/main/java/org/jboss/as/cli/gui/ManagementModelNode.java
+++ cli/src/main/java/org/jboss/as/cli/gui/ManagementModelNode.java
@@ -153,6 +153,7 @@ public class ManagementModelNode extends DefaultMutableTreeNode {
         element = element.replace(":", "\\:");
         element = element.replace("/", "\\/");
         element = element.replace("=", "\\=");
+        element = element.replace(" ", "\\ ");
         return element;
     }
 
