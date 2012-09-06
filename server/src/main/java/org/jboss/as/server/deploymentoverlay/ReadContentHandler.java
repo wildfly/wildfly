f40711b8476ca95cca9d9d51c26b99b90d175651
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-09-06 18:53:06 +1000

    Fix up description providers a bit and add read-content operation

---
+++ server/src/main/java/org/jboss/as/server/deploymentoverlay/ReadContentHandler.java
@@ -0,0 +1,97 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.server.deploymentoverlay;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.registry.Resource;
+import org.jboss.as.repository.ContentRepository;
+import org.jboss.as.repository.DeploymentFileRepository;
+import org.jboss.as.server.ServerMessages;
+import org.jboss.dmr.ModelNode;
+import org.jboss.vfs.VirtualFile;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CONTENT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HASH;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
+/**
+ * @author Stuart Douglas
+ */
+public class ReadContentHandler implements OperationStepHandler {
+
+
+    protected final ContentRepository contentRepository;
+
+    public ReadContentHandler(final ContentRepository contentRepository) {
+        this.contentRepository = contentRepository;
+    }
+
+    @Override
+    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
+        final PathAddress address = PathAddress.pathAddress(operation.get(OP_ADDR));
+        Resource resource = context.getOriginalRootResource();
+        for(final PathElement element : address) {
+            resource = resource.getChild(element);
+        }
+        byte[] content = resource.getModel().get(CONTENT).get(HASH).asBytes();
+        final VirtualFile file = contentRepository.getContent(content);
+        try {
+            context.getResult().set(readFile(file));
+        } catch (IOException e) {
+            throw ServerMessages.MESSAGES.failedToLoadFile(file, e);
+        }
+        context.completeStep(OperationContext.RollbackHandler.NOOP_ROLLBACK_HANDLER);
+    }
+
+
+    public static String readFile(VirtualFile file) throws IOException {
+        BufferedInputStream stream = null;
+        try {
+            stream = new BufferedInputStream(file.openStream());
+            byte[] buff = new byte[1024];
+            StringBuilder builder = new StringBuilder();
+            int read = -1;
+            while ((read = stream.read(buff)) != -1) {
+                builder.append(new String(buff, 0, read));
+            }
+            return builder.toString();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    //ignore
+                }
+            }
+        }
+    }
+}
