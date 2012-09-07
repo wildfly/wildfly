9c444eacab868df2bd86091ce4633cddde918a17
Alexey Loubyansky <loubyansky@gmail.com>
2012-09-07 20:47:24 +0200

    cli: close cli archive reader in finally

--- cli/src/main/java/org/jboss/as/cli/handlers/DeployHandler.java
+++ cli/src/main/java/org/jboss/as/cli/handlers/DeployHandler.java
@@ -533,8 +533,9 @@ public class DeployHandler extends BatchModeCommandHandler {
                     throw new CommandFormatException("ERROR: script '" + script + "' not found.");
                 }
 
+                BufferedReader reader = null;
                 try {
-                    BufferedReader reader = new BufferedReader(new FileReader(scriptFile));
+                    reader = new BufferedReader(new FileReader(scriptFile));
                     String line = reader.readLine();
                     while (!ctx.isTerminated() && line != null) {
                         ctx.handle(line);
@@ -546,6 +547,13 @@ public class DeployHandler extends BatchModeCommandHandler {
                     throw new CommandFormatException("Failed to read the next command from " + scriptFile.getName() + ": " + e.getMessage(), e);
                 } catch (CommandLineException e) {
                     throw new CommandFormatException(e.getMessage(), e);
+                } finally {
+                    if(reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                        }
+                    }
                 }
 
                 return ctx.getBatchManager().getActiveBatch().toRequest();
