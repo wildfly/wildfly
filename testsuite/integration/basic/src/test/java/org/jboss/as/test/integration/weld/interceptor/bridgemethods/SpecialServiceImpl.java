95912a9a8f649fa21f16c7f23a63480cdc5e1038
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-09-25 10:20:00 +1000

    AS7-5626 Take method parameters into account when determining the 'real' method for a bridge method

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/interceptor/bridgemethods/SpecialServiceImpl.java
@@ -0,0 +1,17 @@
+package org.jboss.as.test.integration.weld.interceptor.bridgemethods;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+/**
+ *
+ */
+@Stateless
+@Local(SpecialService.class)
+@SomeInterceptorBinding
+public class SpecialServiceImpl implements SpecialService {
+
+    public void doSomething(String param) {
+        System.out.println("SpecialServiceImpl.doSomething");
+    }
+}
