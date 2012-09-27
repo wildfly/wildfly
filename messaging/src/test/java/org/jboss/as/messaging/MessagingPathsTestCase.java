693145d629b578cc82a4758f61b8e085a979f7d2
Jeff Mesnil <jmesnil@gmail.com>
2012-09-27 15:13:21 +0200

    AS7-5600 fix test to make it work on Windows

--- messaging/src/test/java/org/jboss/as/messaging/MessagingPathsTestCase.java
+++ messaging/src/test/java/org/jboss/as/messaging/MessagingPathsTestCase.java
@@ -27,6 +27,7 @@ import static org.jboss.as.messaging.HornetQServerAdd.DEFAULT_RELATIVE_TO;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.jboss.as.controller.services.path.PathManagerService;
@@ -41,11 +42,11 @@ import org.junit.Test;
  */
 public class MessagingPathsTestCase {
 
-    private static final String MY_SERVER_DATA_DIR = "/my/server/data/dir/";
+    private static final String MY_SERVER_DATA_DIR = System.getProperty("java.io.tmpdir") + "datadir";
     private static final String MY_RELATIVE_JOURNAL_DIR = "my-relative-journal";
-    private static final String MY_ABSOLUTE_BINDINGS_DIR = "/my/absolute/bindings/dir";
+    private static final String MY_ABSOLUTE_BINDINGS_DIR = System.getProperty("java.io.tmpdir") + "bindingsdir";
     private static final String MY_PAGING_RELATIVE_TO = "paging.relative-to.dir";
-    private static final String MY_PAGING_RELATIVE_TO_DIR = "/another/dir/for/paging/";
+    private static final String MY_PAGING_RELATIVE_TO_DIR = System.getProperty("java.io.tmpdir") + "pagingdir";
 
     private ServiceContainer container;
 
@@ -90,18 +91,22 @@ public class MessagingPathsTestCase {
 
 
         String resolvedJournalPath = pathConfig.resolveJournalPath(pathManagerService);
-        assertTrue("the specific relative path is prepended by the resolved default relative-to", resolvedJournalPath.startsWith(MY_SERVER_DATA_DIR));
+        assertTrue("the specific relative path must be prepended by the resolved default relative-to, resolvedJournalPath=" + resolvedJournalPath + ", MY_SERVER_DATA_DIR" + MY_SERVER_DATA_DIR,
+                resolvedJournalPath.startsWith(MY_SERVER_DATA_DIR));
         assertTrue(resolvedJournalPath.endsWith(MY_RELATIVE_JOURNAL_DIR));
 
         String resolvedBindingsPath = pathConfig.resolveBindingsPath(pathManagerService);
-        assertEquals("the speficic absolute path is not prepended by the resolved default relative-to", MY_ABSOLUTE_BINDINGS_DIR, resolvedBindingsPath);
+        assertEquals("the speficic absolute path must not be prepended by the resolved default relative-to, resolvedBindingsPath=" + resolvedBindingsPath,
+                MY_ABSOLUTE_BINDINGS_DIR, resolvedBindingsPath);
 
         String resolvedPagingPath = pathConfig.resolvePagingPath(pathManagerService);
-        assertTrue("the default path is prepended by the resolved specific relative-to", resolvedPagingPath.startsWith(MY_PAGING_RELATIVE_TO_DIR));
+        assertTrue("the default path must be prepended by the resolved specific relative-to, resolvedPagingPath=" + resolvedPagingPath,
+                resolvedPagingPath.startsWith(MY_PAGING_RELATIVE_TO_DIR));
         assertTrue(resolvedPagingPath.endsWith(DEFAULT_PAGING_DIR));
 
         String resolvedLargeMessagePath = pathConfig.resolveLargeMessagePath(pathManagerService);
-        assertTrue("by default, the default path is prepended by the resolved default relative-to", resolvedLargeMessagePath.startsWith(MY_SERVER_DATA_DIR));
+        assertTrue("by default, the default path MUST prepended by the resolved default relative-to, resolvedLargeMessagePath=" + resolvedLargeMessagePath,
+                resolvedLargeMessagePath.startsWith(MY_SERVER_DATA_DIR));
         assertTrue(resolvedLargeMessagePath.endsWith(DEFAULT_LARGE_MESSAGE_DIR));
     }
 }
