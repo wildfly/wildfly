195156c05fc8d8f38927930c1aa64249791960d2
Thomas Diesler <thomas.diesler@jboss.com>
2012-10-09 14:15:58 +0200

    [AS7-5697] Framework may not resolve optional imports

--- testsuite/integration/osgi/src/test/java/org/jboss/as/test/integration/osgi/classloading/subb/TestBA.java
+++ testsuite/integration/osgi/src/test/java/org/jboss/as/test/integration/osgi/classloading/subb/TestBA.java
@@ -19,8 +19,15 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.integration.osgi.classloading.bundle;
+package org.jboss.as.test.integration.osgi.classloading.subb;
 
-public class WebInfLibClass {
+import org.jboss.as.test.integration.osgi.classloading.suba.TestA;
+
+public class TestBA {
+
+    static TestA someA;
+    static {
+        someA = new TestA();
+    }
 
 }
