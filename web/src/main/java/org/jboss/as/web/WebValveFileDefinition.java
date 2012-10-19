a08435ec8453be246558a5cd797ef0920cc33c1f
Jean-Frederic Clere <jfclere@gmail.com>
2012-10-19 14:22:06 +0200

    Arrange the global valve handling logic.

---
+++ web/src/main/java/org/jboss/as/web/WebValveFileDefinition.java
@@ -0,0 +1,52 @@
+package org.jboss.as.web;
+
+import org.jboss.as.controller.ReloadRequiredRemoveStepHandler;
+import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
+import org.jboss.as.controller.SimpleResourceDefinition;
+import org.jboss.as.controller.operations.validation.StringLengthValidator;
+import org.jboss.as.controller.registry.AttributeAccess;
+import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
+
+/**
+ * @author Jean-Frederic Clere
+ */
+public class WebValveFileDefinition extends SimpleResourceDefinition {
+    public static final WebValveFileDefinition INSTANCE = new WebValveFileDefinition();
+
+    protected static final SimpleAttributeDefinition RELATIVE_TO =
+            new SimpleAttributeDefinitionBuilder(Constants.RELATIVE_TO, ModelType.STRING)
+                    .setXmlName(Constants.RELATIVE_TO)
+                    .setAllowNull(true)
+                    .setFlags(AttributeAccess.Flag.RESTART_ALL_SERVICES)
+                    .setValidator(new StringLengthValidator(1, true))
+                    .setDefaultValue(new ModelNode("jboss.server.log.dir"))
+                    .build();
+
+
+    protected static final SimpleAttributeDefinition PATH =
+            new SimpleAttributeDefinitionBuilder(Constants.PATH, ModelType.STRING)
+                    .setXmlName(Constants.PATH)
+                    .setAllowNull(true)
+                    .setAllowExpression(true)
+                    .setFlags(AttributeAccess.Flag.RESTART_ALL_SERVICES)
+                    .setValidator(new StringLengthValidator(1, true, true))
+                    .build();
+
+
+    private WebValveFileDefinition() {
+        super(WebExtension.FILE_PATH,
+                WebExtension.getResourceDescriptionResolver("valve.file"),
+                WebValveFileAdd.INSTANCE,
+                new ReloadRequiredRemoveStepHandler());
+    }
+
+    @Override
+    public void registerAttributes(ManagementResourceRegistration directory) {
+        directory.registerReadWriteAttribute(RELATIVE_TO, null, new ReloadRequiredWriteAttributeHandler(RELATIVE_TO));
+        directory.registerReadWriteAttribute(PATH, null, new ReloadRequiredWriteAttributeHandler(PATH));
+    }
+}
