7bc0ecfd2a607b916eaaee5ffad514d0364a4f89
mmatloka <mmatloka@gmail.com>
2012-10-23 23:13:42 +0200

    [AS7-974] embedded configuration improvements

--- arquillian/container-embedded/src/main/java/org/jboss/as/arquillian/container/embedded/EmbeddedContainerConfiguration.java
+++ arquillian/container-embedded/src/main/java/org/jboss/as/arquillian/container/embedded/EmbeddedContainerConfiguration.java
@@ -16,14 +16,82 @@
  */
 package org.jboss.as.arquillian.container.embedded;
 
+import org.jboss.arquillian.container.spi.ConfigurationException;
 import org.jboss.arquillian.container.spi.client.container.ContainerConfiguration;
-import org.jboss.as.arquillian.container.DistributionContainerConfiguration;
+import org.jboss.arquillian.container.spi.client.deployment.Validate;
+import org.jboss.as.arquillian.container.CommonContainerConfiguration;
 
 /**
  * {@link ContainerConfiguration} implementation for JBoss AS Embedded
  *
  * @author <a href="mailto:alr@jboss.org">Andrew Lee Rubinger</a>
+ * @author <a href="mailto:mmatloka@gmail.com">Michal Matloka</a>
  */
-public class EmbeddedContainerConfiguration extends DistributionContainerConfiguration {
+public class EmbeddedContainerConfiguration extends CommonContainerConfiguration {
 
+    private String jbossHome = System.getenv("JBOSS_HOME");
+
+    private String modulePath = System.getProperty("module.path");
+
+    private String bundlePath = System.getProperty("bundle.path");
+
+    public EmbeddedContainerConfiguration() {
+        super();
+
+        // if no jbossHome is set use jboss.home of already running jvm
+        if (jbossHome == null || jbossHome.isEmpty()) {
+            jbossHome = System.getProperty("jboss.home");
+        }
+
+        if(modulePath == null || modulePath.isEmpty()) {
+            modulePath = jbossHome + "/modules";
+        }
+
+        if(bundlePath == null || bundlePath.isEmpty()) {
+            bundlePath = jbossHome + "/bundles";
+        }
+    }
+
+    /**
+     * @return the jbossHome
+     */
+    public String getJbossHome() {
+        return jbossHome;
+    }
+
+    /**
+     * @param jbossHome the jbossHome to set
+     */
+    public void setJbossHome(String jbossHome) {
+        this.jbossHome = jbossHome;
+    }
+
+    public String getModulePath() {
+        return modulePath;
+    }
+
+    public void setModulePath(final String modulePath) {
+        this.modulePath = modulePath;
+    }
+
+    public String getBundlePath() {
+        return bundlePath;
+    }
+
+    public void setBundlePath(String bundlePath) {
+        this.bundlePath = bundlePath;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see org.jboss.as.arquillian.container.CommonContainerConfiguration#validate()
+     */
+    @Override
+    public void validate() throws ConfigurationException {
+        super.validate();
+        Validate.configurationDirectoryExists(jbossHome, "jbossHome '" + jbossHome + "' must exist");
+        Validate.configurationDirectoryExists(jbossHome, "modulePath '" + modulePath + "' must exist");
+        Validate.configurationDirectoryExists(jbossHome, "bundlePath '" + bundlePath + "' must exist");
+    }
 }
