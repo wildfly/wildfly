9f005992e3b6c27253e462745075cab646c83a9e
Stefano Maestri <stefano.maestri@redhat.com>
2012-10-30 15:06:08 +0100

    AS7-5851 Resource adapter deployments should have an explicit dependency on their BootstrapContext

--- connector/src/main/java/org/jboss/as/connector/subsystems/jca/Constants.java
+++ connector/src/main/java/org/jboss/as/connector/subsystems/jca/Constants.java
@@ -25,7 +25,7 @@ package org.jboss.as.connector.subsystems.jca;
  * @author @author <a href="mailto:stefano.maestri@redhat.com">Stefano Maestri</a>
  * @author @author <a href="mailto:jesper.pedersen@jboss.org">Jesper Pedersen</a>
  */
-class Constants {
+public class Constants {
     static final String JCA = "jca";
 
     static final String ARCHIVE_VALIDATION = "archive-validation";
@@ -40,8 +40,8 @@ class Constants {
     static final String CACHED_CONNECTION_MANAGER_DEBUG = "debug";
     static final String CACHED_CONNECTION_MANAGER_ERROR = "error";
 
-    static final String DEFAULT_NAME = "default";
     static final String THREAD_POOL = "thread-pool";
+    public static final String DEFAULT_NAME = "default";
     static final String WORKMANAGER_SHORT_RUNNING = "short-running-threads";
     static final String WORKMANAGER_LONG_RUNNING = "long-running-threads";
 
