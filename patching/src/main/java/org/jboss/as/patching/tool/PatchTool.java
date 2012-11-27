a9b882015b15ec9262ec5c59adbf2f7251838362
Emanuel Muckenhuber <emuckenh@redhat.com>
2012-11-27 15:25:14 +0100

    [AS7-462] fix content policy processing

--- patching/src/main/java/org/jboss/as/patching/tool/PatchTool.java
+++ patching/src/main/java/org/jboss/as/patching/tool/PatchTool.java
@@ -132,13 +132,13 @@ public interface PatchTool {
             if(operation.hasDefined(Constants.OVERRIDE)) {
                 final ModelNode overrides = operation.get(Constants.OVERRIDE);
                 for(final ModelNode override : overrides.asList()) {
-                    builder.overrideItem(override.toString());
+                    builder.overrideItem(override.asString());
                 }
             }
             if(operation.hasDefined(Constants.PRESERVE)) {
                 final ModelNode preserves = operation.get(Constants.PRESERVE);
                 for(final ModelNode preserve : preserves.asList()) {
-                    builder.preserveItem(preserve.toString());
+                    builder.preserveItem(preserve.asString());
                 }
             }
             return builder.createPolicy();
