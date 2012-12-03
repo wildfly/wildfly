c769b4f2da954ed4861513e3af54a72dc4b6ebde
baranowb <baranowb@gmail.com>
2012-12-03 12:27:37 +0100

    AS7-5449

--- threads/src/main/java/org/jboss/as/threads/PoolAttributeDefinitions.java
+++ threads/src/main/java/org/jboss/as/threads/PoolAttributeDefinitions.java
@@ -64,8 +64,8 @@ public interface PoolAttributeDefinitions {
 
     SimpleAttributeDefinition THREAD_NAME_PATTERN = new SimpleAttributeDefinition(CommonAttributes.THREAD_NAME_PATTERN, ModelType.STRING, true);
 
-    SimpleAttributeDefinition PRIORITY = new SimpleAttributeDefinition(CommonAttributes.PRIORITY, CommonAttributes.PRIORITY, new ModelNode().set(-1),
-            ModelType.INT, true, true, MeasurementUnit.NONE, new IntRangeValidator(-1, 10, true, true));
+    SimpleAttributeDefinition PRIORITY = new SimpleAttributeDefinition(CommonAttributes.PRIORITY, CommonAttributes.PRIORITY, null,
+            ModelType.INT, true, true, MeasurementUnit.NONE, new IntRangeValidator(Thread.MIN_PRIORITY, Thread.MAX_PRIORITY, true, true));
 
     // Metrics
 
