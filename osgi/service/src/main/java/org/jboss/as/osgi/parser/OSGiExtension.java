206d8de63647b3d355ff84b180b00598fa64bea2
Brian Stansberry <brian.stansberry@redhat.com>
2012-12-05 16:05:43 -0600

    Revert the OSGi management bump

--- osgi/service/src/main/java/org/jboss/as/osgi/parser/OSGiExtension.java
+++ osgi/service/src/main/java/org/jboss/as/osgi/parser/OSGiExtension.java
@@ -51,7 +51,7 @@ public class OSGiExtension implements Extension {
     public static final String SUBSYSTEM_NAME = "osgi";
 
     private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
-    private static final int MANAGEMENT_API_MINOR_VERSION = 1;
+    private static final int MANAGEMENT_API_MINOR_VERSION = 0;
     private static final int MANAGEMENT_API_MICRO_VERSION = 0;
 
     @Override
