8acc36d6da5c3610943d7cc2b3a729e0e675d470
Thomas Diesler <thomas.diesler@jboss.com>
2012-12-11 19:18:14 +0100

    [AS7-5201] 128.3.8 Stopping the Web Application Bundle

--- testsuite/integration/osgi/src/test/java/org/jboss/as/test/integration/osgi/webapp/WebAppTestCase.java
+++ testsuite/integration/osgi/src/test/java/org/jboss/as/test/integration/osgi/webapp/WebAppTestCase.java
@@ -35,6 +35,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.osgi.web.WebExtension;
 import org.jboss.as.test.integration.common.HttpRequest;
 import org.jboss.as.test.integration.osgi.api.Echo;
 import org.jboss.as.test.integration.osgi.deployment.bundle.DeferredFailActivator;
@@ -56,6 +57,7 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * Test webapp deployemnts as OSGi bundles
@@ -111,7 +113,7 @@ public class WebAppTestCase {
         deployer.deploy(SIMPLE_WAR);
         try {
             String result = performCall("/simple/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("null called with: Hello", result);
             // Test resource access
             result = performCall("/simple/message.txt");
             Assert.assertEquals("Hello from Resource", result);
@@ -125,7 +127,7 @@ public class WebAppTestCase {
         deployer.deploy(BUNDLE_A_WAR);
         try {
             String result = performCall("/bundle-a/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-a.war called with: Hello", result);
             result = performCall("/bundle-a/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -138,7 +140,7 @@ public class WebAppTestCase {
         deployer.deploy(BUNDLE_B_WAR);
         try {
             String result = performCall("/bundle-b/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-b.war called with: Hello", result);
             result = performCall("/bundle-b/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -151,7 +153,7 @@ public class WebAppTestCase {
         deployer.deploy(BUNDLE_G_WAR);
         try {
             String result = performCall("/bundle-g/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("null called with: Hello", result);
             result = performCall("/bundle-g/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -164,7 +166,7 @@ public class WebAppTestCase {
         deployer.deploy(BUNDLE_H_WAR);
         try {
             String result = performCall("/bundle-h/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("null called with: Hello", result);
             result = performCall("/bundle-h/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -177,7 +179,7 @@ public class WebAppTestCase {
         deployer.deploy(BUNDLE_C_WAB);
         try {
             String result = performCall("/bundle-c/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-c.wab called with: Hello", result);
             result = performCall("/bundle-c/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -193,7 +195,7 @@ public class WebAppTestCase {
             Bundle bundle = context.getBundle(BUNDLE_D_WAB);
 
             String result = performCall("/bundle-d/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-d.wab called with: Hello", result);
             result = performCall("/bundle-d/message.txt");
             Assert.assertEquals("Hello from Resource", result);
 
@@ -218,7 +220,7 @@ public class WebAppTestCase {
             Assert.assertEquals("ACTIVE", Bundle.ACTIVE, bundle.getState());
 
             result = performCall("/bundle-d/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-d.wab called with: Hello", result);
             result = performCall("/bundle-d/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -231,7 +233,7 @@ public class WebAppTestCase {
         deployer.deploy(BUNDLE_E_JAR);
         try {
             String result = performCall("/bundle-e/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-e.jar called with: Hello", result);
             result = performCall("/bundle-e/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -262,7 +264,7 @@ public class WebAppTestCase {
             Assert.assertEquals("ACTIVE", Bundle.ACTIVE, bundle.getState());
 
             String result = performCall("/bundle-c/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
+            Assert.assertEquals("bundle-c.wab called with: Hello", result);
             result = performCall("/bundle-c/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
@@ -278,7 +280,7 @@ public class WebAppTestCase {
         try {
             Assert.assertEquals("INSTALLED", Bundle.INSTALLED, bundle.getState());
             try {
-                performCall("/bundle-d/servlet?input=Hello");
+                performCall("/bundle-f/servlet?input=Hello");
                 Assert.fail("IOException expected");
             } catch (IOException ex) {
                 // expected
@@ -292,7 +294,7 @@ public class WebAppTestCase {
             }
             Assert.assertEquals("RESOLVED", Bundle.RESOLVED, bundle.getState());
             try {
-                performCall("/bundle-d/servlet?input=Hello");
+                performCall("/bundle-f/servlet?input=Hello");
                 Assert.fail("IOException expected");
             } catch (IOException ex) {
                 // expected
@@ -301,9 +303,9 @@ public class WebAppTestCase {
             bundle.start();
             Assert.assertEquals("ACTIVE", Bundle.ACTIVE, bundle.getState());
 
-            String result = performCall("/bundle-c/servlet?input=Hello");
-            Assert.assertEquals("Simple Servlet called with input=Hello", result);
-            result = performCall("/bundle-c/message.txt");
+            String result = performCall("/bundle-f/servlet?input=Hello");
+            Assert.assertEquals("bundle-f.wab called with: Hello", result);
+            result = performCall("/bundle-f/message.txt");
             Assert.assertEquals("Hello from Resource", result);
         } finally {
             bundle.uninstall();
@@ -336,6 +338,7 @@ public class WebAppTestCase {
                 builder.addBundleManifestVersion(2);
                 builder.addImportPackages(PostConstruct.class, WebServlet.class);
                 builder.addImportPackages(Servlet.class, HttpServlet.class);
+                builder.addImportPackages(FrameworkUtil.class);
                 builder.addBundleClasspath("WEB-INF/classes");
                 return builder.openStream();
             }
@@ -356,6 +359,7 @@ public class WebAppTestCase {
                 builder.addBundleManifestVersion(2);
                 builder.addImportPackages(PostConstruct.class, WebServlet.class);
                 builder.addImportPackages(Servlet.class, HttpServlet.class);
+                builder.addImportPackages(FrameworkUtil.class);
                 return builder.openStream();
             }
         });
@@ -375,6 +379,7 @@ public class WebAppTestCase {
                 builder.addBundleManifestVersion(2);
                 builder.addImportPackages(PostConstruct.class, WebServlet.class);
                 builder.addImportPackages(Servlet.class, HttpServlet.class);
+                builder.addImportPackages(FrameworkUtil.class);
                 return builder.openStream();
             }
         });
@@ -394,7 +399,8 @@ public class WebAppTestCase {
                 builder.addBundleManifestVersion(2);
                 builder.addImportPackages(PostConstruct.class, WebServlet.class);
                 builder.addImportPackages(Servlet.class, HttpServlet.class);
-                builder.addManifestHeader("Web-ContextPath", "/bundle-d");
+                builder.addImportPackages(FrameworkUtil.class);
+                builder.addManifestHeader(WebExtension.WEB_CONTEXT_PATH, "/bundle-d");
                 return builder.openStream();
             }
         });
@@ -414,7 +420,8 @@ public class WebAppTestCase {
                 builder.addBundleManifestVersion(2);
                 builder.addImportPackages(PostConstruct.class, WebServlet.class);
                 builder.addImportPackages(Servlet.class, HttpServlet.class);
-                builder.addManifestHeader("Web-ContextPath", "/bundle-e");
+                builder.addImportPackages(FrameworkUtil.class);
+                builder.addManifestHeader(WebExtension.WEB_CONTEXT_PATH, "/bundle-e");
                 return builder.openStream();
             }
         });
