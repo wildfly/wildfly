48394160fafcfd60f882d12a1317d363f34297ca
Tomaz Cerar <tomaz.cerar@gmail.com>
2012-12-11 16:27:09 +0100

    StringListAttribute should support expressions

--- controller/src/main/java/org/jboss/as/controller/ObjectListAttributeDefinition.java
+++ controller/src/main/java/org/jboss/as/controller/ObjectListAttributeDefinition.java
@@ -52,7 +52,7 @@ public class ObjectListAttributeDefinition extends ListAttributeDefinition {
 
     private ObjectListAttributeDefinition(final String name, final String xmlName, final ObjectTypeAttributeDefinition valueType, final boolean allowNull, final int minSize, final int maxSize, final String[] alternatives, final String[] requires,
                                           final AttributeMarshaller attributeMarshaller, final boolean resourceOnly, final DeprecationData deprecated, final AttributeAccess.Flag... flags) {
-        super(name, xmlName, allowNull, minSize, maxSize, valueType.getValidator(), alternatives, requires, attributeMarshaller, resourceOnly, deprecated, flags);
+        super(name, xmlName, allowNull, false, minSize, maxSize, valueType.getValidator(), alternatives, requires, attributeMarshaller, resourceOnly, deprecated, flags);
         this.valueType = valueType;
     }
 
