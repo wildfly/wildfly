7c9dce8c0cd8fd9fa1724b82db68b48ebac742bf
Jan Martiska <jmartisk@redhat.com>
2012-12-13 11:57:22 +0100

    [AS7-2785] tests to verify that PostConstruct is called only once

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/singleton/startup/postconstruct/SinglePostConstructInvocationTestCase.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.singleton.startup.postconstruct;
+
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.naming.InitialContext;
+
+/**
+ * Test for AS7-[3855|2785]: Do not call @PostConstruct multiple times on @Startup @Singleton bean.
+ * @author Jan Martiska / jmartisk@redhat.com
+ */
+@RunWith(Arquillian.class)
+public class SinglePostConstructInvocationTestCase {
+
+    @Deployment
+    public static Archive<?> getDeployment() {
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "single-postconstruct-invocation-test");
+        archive.addClasses(Client.class, Controller.class);
+        return archive;
+    }
+
+    @Test
+    public void doTest() throws Exception {
+        InitialContext ctx = new InitialContext();
+        Controller controller = (Controller)ctx.lookup("java:module/Controller");
+        Assert.assertEquals(1, controller.getPostConstructInvocationCounter());
+        ctx.close();
+    }
+}
