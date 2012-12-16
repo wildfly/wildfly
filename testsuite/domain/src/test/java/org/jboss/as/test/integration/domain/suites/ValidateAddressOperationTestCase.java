b28283337eda058e512896844cff5c12fcd28d9b
Brian Stansberry <brian.stansberry@redhat.com>
2012-12-15 23:22:32 -0600

    Fix ValidateAddressOperationTestCase

--- testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/suites/ValidateAddressOperationTestCase.java
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/suites/ValidateAddressOperationTestCase.java
@@ -21,34 +21,44 @@
 */
 package org.jboss.as.test.integration.domain.suites;
 
-import java.io.IOException;
-
-import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.junit.Arquillian;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESULT;
-import org.jboss.as.controller.operations.common.ValidateAddressOperationHandler;
-import org.jboss.as.test.integration.management.base.ContainerResourceMgmtTestBase;
-import org.jboss.as.test.integration.management.util.MgmtOperationException;
-import org.jboss.as.test.integration.management.util.ModelUtil;
-import org.jboss.dmr.ModelNode;
-import static org.junit.Assert.assertThat;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.PROBLEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESULT;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALID;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.jboss.as.controller.operations.common.ValidateAddressOperationHandler;
+import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
+import org.jboss.as.test.integration.domain.management.util.DomainTestUtils;
+import org.jboss.as.test.integration.management.util.MgmtOperationException;
+import org.jboss.as.test.integration.management.util.ModelUtil;
+import org.jboss.dmr.ModelNode;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Tests that the validate-address operation works as it should
  *
  * @author <a href="alex@jboss.org">Alexey Loubyansky</a>
  */
-@RunWith(Arquillian.class)
-@RunAsClient
-public class ValidateAddressOperationTestCase extends ContainerResourceMgmtTestBase {
+public class ValidateAddressOperationTestCase  {
+
+    private static DomainTestSupport testSupport;
+
+    @BeforeClass
+    public static void setupDomain() throws Exception {
+        testSupport = DomainTestSuite.createSupport(ValidateAddressOperationTestCase.class.getSimpleName());
+    }
+
+    @AfterClass
+    public static void tearDownDomain() throws Exception {
+        testSupport = null;
+        DomainTestSuite.stopSupport();
+    }
 
     @Test
     public void testValidRootAddress() throws IOException, MgmtOperationException {
@@ -94,17 +104,21 @@ public class ValidateAddressOperationTestCase extends ContainerResourceMgmtTestB
         ModelNode op = ModelUtil.createOpNode(null, ValidateAddressOperationHandler.OPERATION_NAME);
         final ModelNode addr = op.get(VALUE);
         addr.add("host", "slave");
-        assertTrue(executeOperation(op).get(RESULT, VALID).asBoolean());
+        assertTrue(executeOperation(op).get(VALID).asBoolean());
 
         addr.add("server", "main-three");
-        assertTrue(executeOperation(op).get(RESULT, VALID).asBoolean());
+        assertTrue(executeOperation(op).get(VALID).asBoolean());
 
         addr.add("core-service", "platform-mbean");
         addr.add("type", "garbage-collector");
-        assertTrue(executeOperation(op).get(RESULT, VALID).asBoolean());
+        assertTrue(executeOperation(op).get(VALID).asBoolean());
 
         addr.add("non-existent", "resource");
-        assertFalse(executeOperation(op).get(RESULT, VALID).asBoolean());
+        assertFalse(executeOperation(op).get(VALID).asBoolean());
+    }
+
+    private ModelNode executeOperation(final ModelNode op) throws IOException, MgmtOperationException {
+        return DomainTestUtils.executeForResult(op, testSupport.getDomainMasterLifecycleUtil().getDomainClient());
     }
 
 }
