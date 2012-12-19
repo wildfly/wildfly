4d1eb75070732489fda6ec59bf7e2d593c27243b
Brian Stansberry <brian.stansberry@redhat.com>
2012-12-19 11:52:27 -0600

    AS7-6154 Only use AttributeDefinition for the console handler target validation

--- logging/src/main/java/org/jboss/as/logging/LoggingSubsystemParser.java
+++ logging/src/main/java/org/jboss/as/logging/LoggingSubsystemParser.java
@@ -473,9 +473,6 @@ public class LoggingSubsystemParser implements XMLStreamConstants, XMLElementRea
                 }
                 case TARGET: {
                     final String target = readStringAttributeElement(reader, "name");
-                    if (!(target.equals("System.out") || target.equals("System.err"))) {
-                        throw new XMLStreamException(MESSAGES.invalidTargetName(EnumSet.allOf(Target.class)), reader.getLocation());
-                    }
                     TARGET.parseAndSetParameter(target, node, reader);
                     break;
                 }
