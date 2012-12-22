c1da1f884708ef2b500d706bad1369b75db57c08
Manuel Fehlhammer <fehlhammer@gmx.de>
2012-12-22 18:53:14 +0100

    changed subsystem to read settings from model instead of operation. Introduced new abstract superclass for key generator resource desriptions, which holds jndi-name.

--- cmp/src/main/java/org/jboss/as/cmp/subsystem/UUIDKeyGeneratorAdd.java
+++ cmp/src/main/java/org/jboss/as/cmp/subsystem/UUIDKeyGeneratorAdd.java
@@ -25,6 +25,7 @@ package org.jboss.as.cmp.subsystem;
 import org.jboss.as.cmp.keygenerator.KeyGeneratorFactory;
 import org.jboss.as.cmp.keygenerator.uuid.UUIDKeyGeneratorFactory;
 import org.jboss.as.controller.AttributeDefinition;
+import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.Service;
@@ -39,7 +40,7 @@ public class UUIDKeyGeneratorAdd extends AbstractKeyGeneratorAdd {
     private UUIDKeyGeneratorAdd() {
     }
 
-    protected Service<KeyGeneratorFactory> getKeyGeneratorFactory(final ModelNode operation) {
+    protected Service<KeyGeneratorFactory> getKeyGeneratorFactory(final OperationContext context, final ModelNode operation) {
         return new UUIDKeyGeneratorFactory();
     }
 
