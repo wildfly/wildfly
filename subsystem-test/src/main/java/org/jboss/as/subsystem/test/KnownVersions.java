46d3b366068db10b759f3c5570a573f431e715b5
Tomaz Cerar <tomaz.cerar@gmail.com>
2013-01-03 15:33:24 +0100

    security transformer version registration

--- subsystem-test/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
+++ subsystem-test/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
@@ -1,24 +1,26 @@
 /*
-* JBoss, Home of Professional Open Source.
-* Copyright 2012, Red Hat Middleware LLC, and individual contributors
-* as indicated by the @author tags. See the copyright.txt file in the
-* distribution for a full listing of individual contributors.
-*
-* This is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version 2.1 of
-* the License, or (at your option) any later version.
-*
-* This software is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this software; if not, write to the Free
-* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-*/
+ *
+ *  JBoss, Home of Professional Open Source.
+ *  Copyright 2013, Red Hat, Inc., and individual contributors
+ *  as indicated by the @author tags. See the copyright.txt file in the
+ *  distribution for a full listing of individual contributors.
+ *
+ *  This is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this software; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * /
+ */
 package org.jboss.as.subsystem.test;
 
 import java.util.Collections;
@@ -59,6 +61,7 @@ class KnownVersions {
         addSubsystemVersion(map, "messaging", "1.1.0", "1.3.0");
         addSubsystemVersion(map, "naming", "1.1.0", "1.3.0");
         addSubsystemVersion(map, "osgi", "1.0.0", "1.3.0");
+        addSubsystemVersion(map, "security", "1.1.0", "1.2.0");
         addSubsystemVersion(map, "remoting", "1.1.0", "1.3.0");
         addSubsystemVersion(map, "transactions", "1.1.0", "1.2.0");
         addSubsystemVersion(map, "transactions", "1.1.1", "1.3.0");
