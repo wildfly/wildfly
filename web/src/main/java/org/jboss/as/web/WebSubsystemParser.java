3ce05806c0a4f515d4baf62b9d28f64e57266e4b
Jean-Frederic Clere <jfclere@gmail.com>
2013-01-07 09:27:45 +0100

    Fix for AS7-6276.

--- web/src/main/java/org/jboss/as/web/WebSubsystemParser.java
+++ web/src/main/java/org/jboss/as/web/WebSubsystemParser.java
@@ -220,11 +220,9 @@ class WebSubsystemParser implements XMLStreamConstants, XMLElementReader<List<Mo
                 writer.writeEndElement();
             }
             if (node.hasDefined(VALVE)) {
-                boolean valvestartwritten = false;
                 for (final Property valve : node.get(VALVE).asPropertyList()) {
                     final ModelNode config = valve.getValue();
                     writer.writeStartElement(Element.VALVE.getLocalName());
-                    valvestartwritten = true;
                     writer.writeAttribute(NAME, valve.getName());
                     for (SimpleAttributeDefinition attr : WebValveDefinition.ATTRIBUTES) {
                         attr.marshallAsAttribute(config, false, writer);
@@ -236,10 +234,9 @@ class WebSubsystemParser implements XMLStreamConstants, XMLElementReader<List<Mo
                             writer.writeAttribute(Attribute.PARAM_VALUE.getLocalName(), entry.getValue().asString());
                         }
                     }
+                    writer.writeEndElement();
 
                 }
-                if (valvestartwritten)
-                    writer.writeEndElement();
             }
 
         }
