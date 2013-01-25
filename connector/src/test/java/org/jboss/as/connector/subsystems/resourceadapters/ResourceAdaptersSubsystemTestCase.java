7cc3155e6ff03f80f91df5c3779eef5ee5b819b5
Stefano Maestri <stefano.maestri@redhat.com>
2013-01-25 15:16:02 +0100

    AS7-6334 implementation and test cases for RA transformer

--- connector/src/test/java/org/jboss/as/connector/subsystems/resourceadapters/ResourceAdaptersSubsystemTestCase.java
+++ connector/src/test/java/org/jboss/as/connector/subsystems/resourceadapters/ResourceAdaptersSubsystemTestCase.java
@@ -26,10 +26,12 @@ import java.util.List;
 
 import junit.framework.Assert;
 import org.jboss.as.connector.subsystems.resourceadapters.ResourceAdaptersExtension;
+import org.jboss.as.controller.ModelVersion;
 import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
 import org.jboss.as.subsystem.test.AdditionalInitialization;
 import org.jboss.as.subsystem.test.KernelServices;
+import org.jboss.as.subsystem.test.KernelServicesBuilder;
 import org.jboss.as.subsystem.test.ModelDescriptionValidator.ValidationConfiguration;
 import org.jboss.dmr.ModelNode;
 import org.junit.Test;
@@ -55,7 +57,6 @@ public class ResourceAdaptersSubsystemTestCase extends AbstractSubsystemBaseTest
     }
 
 
-
     @Test
     public void testFullConfigXa() throws Exception {
         standardSubsystemTest("resource-adapters-xapool.xml", null, true);
@@ -71,69 +72,120 @@ public class ResourceAdaptersSubsystemTestCase extends AbstractSubsystemBaseTest
         standardSubsystemTest("resource-adapters-xapool-expression.xml", "resource-adapters-xapool.xml", true);
     }
 
+    @Test
+    public void testTransformerAS712() throws Exception {
+        testTransformer1_1_0("7.1.2.Final", "resource-adapters-xapool.xml");
+    }
+
+    @Test
+    public void testTransformerAS713() throws Exception {
+        testTransformer1_1_0("7.1.3.Final", "resource-adapters-xapool.xml");
+    }
+
+    @Test
+    public void tesExpressionsAS712() throws Exception {
+        //this file contain expression for all supported fields except bean-validation-groups and recovery-plugin-properties
+        // for a limitation in test suite not permitting to have expression in type LIST or OBJECT for legacyServices
+        testTransformer1_1_0("7.1.2.Final", "resource-adapters-xapool-expression2.xml");
+    }
+
+    @Test
+    public void testExpressionsAS713() throws Exception {
+        //this file contain expression for all supported fields except bean-validation-groups and recovery-plugin-properties
+        // for a limitation in test suite not permitting to have expression in type LIST or OBJECT for legacyServices
+        testTransformer1_1_0("7.1.3.Final", "resource-adapters-xapool-expression2.xml");
+    }
+
+
+    /**
+     * Tests transformation of model from 1.1.1 version into 1.1.0 version.
+     *
+     * @throws Exception
+     */
+    private void testTransformer1_1_0(String mavenVersion, String subsystemXml) throws Exception {
+        ModelVersion modelVersion = ModelVersion.create(1, 1, 0); //The old model version
+        //Use the non-runtime version of the extension which will happen on the HC
+        KernelServicesBuilder builder = createKernelServicesBuilder(AdditionalInitialization.MANAGEMENT)
+                .setSubsystemXmlResource(subsystemXml);
+
+        // Add legacy subsystems
+        builder.createLegacyKernelServicesBuilder(null, modelVersion)
+                .addMavenResourceURL("org.jboss.as:jboss-as-connector:" + mavenVersion)
+                .setExtensionClassName("org.jboss.as.connector.subsystems.resourceadapters.ResourceAdaptersExtension");
+
+        KernelServices mainServices = builder.build();
+        org.junit.Assert.assertTrue(mainServices.isSuccessfulBoot());
+        KernelServices legacyServices = mainServices.getLegacyServices(modelVersion);
+        org.junit.Assert.assertTrue(legacyServices.isSuccessfulBoot());
+        org.junit.Assert.assertNotNull(legacyServices);
+
+        checkSubsystemModelTransformation(mainServices, modelVersion);
+    }
+
+
     protected AdditionalInitialization createAdditionalInitialization() {
         return AdditionalInitialization.MANAGEMENT;
     }
 
     //TODO: remove this special method as soon as RA will have a unique name in DMR marshalled in xml
     protected KernelServices standardSubsystemTest(final String configId, final String configIdResolvedModel, boolean compareXml) throws Exception {
-            final AdditionalInitialization additionalInit = createAdditionalInitialization();
+        final AdditionalInitialization additionalInit = createAdditionalInitialization();
 
 
-            // Parse the subsystem xml and install into the first controller
-            final String subsystemXml = configId == null ? getSubsystemXml() : getSubsystemXml(configId);
-            final KernelServices servicesA = super.createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemXml).build();
-            Assert.assertTrue("Subsystem boot failed!", servicesA.isSuccessfulBoot());
-            //Get the model and the persisted xml from the first controller
-            final ModelNode modelA = servicesA.readWholeModel();
-            validateModel(modelA);
+        // Parse the subsystem xml and install into the first controller
+        final String subsystemXml = configId == null ? getSubsystemXml() : getSubsystemXml(configId);
+        final KernelServices servicesA = super.createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemXml).build();
+        Assert.assertTrue("Subsystem boot failed!", servicesA.isSuccessfulBoot());
+        //Get the model and the persisted xml from the first controller
+        final ModelNode modelA = servicesA.readWholeModel();
+        validateModel(modelA);
 
-            // Test marshaling
-            final String marshalled = servicesA.getPersistedSubsystemXml();
-            servicesA.shutdown();
+        // Test marshaling
+        final String marshalled = servicesA.getPersistedSubsystemXml();
+        servicesA.shutdown();
 
 
-            // validate the the normalized xmls
-            String normalizedSubsystem = normalizeXML(subsystemXml);
+        // validate the the normalized xmls
+        String normalizedSubsystem = normalizeXML(subsystemXml);
 
-            if (compareXml) {
-                compareXml(configId, normalizedSubsystem, normalizeXML(marshalled));
-            }
+        if (compareXml) {
+            compareXml(configId, normalizedSubsystem, normalizeXML(marshalled));
+        }
 
-            //Install the persisted xml from the first controller into a second controller
-            final KernelServices servicesB = super.createKernelServicesBuilder(additionalInit).setSubsystemXml(marshalled).build();
-            final ModelNode modelB = servicesB.readWholeModel();
+        //Install the persisted xml from the first controller into a second controller
+        final KernelServices servicesB = super.createKernelServicesBuilder(additionalInit).setSubsystemXml(marshalled).build();
+        final ModelNode modelB = servicesB.readWholeModel();
 
-            //WE CANT DO THAT FOR RA BECAUSE THEIR NAME IS GENERATED ON THE FLY W/ archive/module concatenated to a counter.
-            //IT'S AN ISSUE TO BE FIXED IN SUBSYSTEM
-            //Make sure the models from the two controllers are identical
-            //compare(modelA, modelB);
+        //WE CANT DO THAT FOR RA BECAUSE THEIR NAME IS GENERATED ON THE FLY W/ archive/module concatenated to a counter.
+        //IT'S AN ISSUE TO BE FIXED IN SUBSYSTEM
+        //Make sure the models from the two controllers are identical
+        //compare(modelA, modelB);
 
-            // Test the describe operation
-            final ModelNode operation = createDescribeOperation();
-            final ModelNode result = servicesB.executeOperation(operation);
-            Assert.assertTrue("the subsystem describe operation has to generate a list of operations to recreate the subsystem",
-                    !result.hasDefined(ModelDescriptionConstants.FAILURE_DESCRIPTION));
-            final List<ModelNode> operations = result.get(ModelDescriptionConstants.RESULT).asList();
-            servicesB.shutdown();
+        // Test the describe operation
+        final ModelNode operation = createDescribeOperation();
+        final ModelNode result = servicesB.executeOperation(operation);
+        Assert.assertTrue("the subsystem describe operation has to generate a list of operations to recreate the subsystem",
+                !result.hasDefined(ModelDescriptionConstants.FAILURE_DESCRIPTION));
+        final List<ModelNode> operations = result.get(ModelDescriptionConstants.RESULT).asList();
+        servicesB.shutdown();
 
-            final KernelServices servicesC = super.createKernelServicesBuilder(additionalInit).setBootOperations(operations).build();
-            final ModelNode modelC = servicesC.readWholeModel();
+        final KernelServices servicesC = super.createKernelServicesBuilder(additionalInit).setBootOperations(operations).build();
+        final ModelNode modelC = servicesC.readWholeModel();
 
-            compare(modelB, modelC);
+        compare(modelB, modelC);
 
-            assertRemoveSubsystemResources(servicesC, getIgnoredChildResourcesForRemovalTest());
+        assertRemoveSubsystemResources(servicesC, getIgnoredChildResourcesForRemovalTest());
 
-            if (configIdResolvedModel != null) {
-                final String subsystemResolvedXml = getSubsystemXml(configIdResolvedModel);
-                final KernelServices servicesD = super.createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemResolvedXml).build();
-                Assert.assertTrue("Subsystem w/ reolved xml boot failed!", servicesD.isSuccessfulBoot());
-                final ModelNode modelD = servicesD.readWholeModel();
-                validateModel(modelD);
-                resolveandCompareModel(modelA, modelD);
-            }
+        if (configIdResolvedModel != null) {
+            final String subsystemResolvedXml = getSubsystemXml(configIdResolvedModel);
+            final KernelServices servicesD = super.createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemResolvedXml).build();
+            Assert.assertTrue("Subsystem w/ reolved xml boot failed!", servicesD.isSuccessfulBoot());
+            final ModelNode modelD = servicesD.readWholeModel();
+            validateModel(modelD);
+            resolveandCompareModel(modelA, modelD);
+        }
 
         return servicesA;
-        }
+    }
 
 }
