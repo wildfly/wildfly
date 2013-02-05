b801b89c7c6fa2c1dbbe7bdbe4663c05eb2af088
Vladimir Rastseluev <vrastsel@redhat.com>
2013-02-05 14:51:30 +0100

    AS7-6451 tests

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/annorar/AnnoAdminObjectImpl1.java
@@ -0,0 +1,196 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.jca.annorar;
+
+import javax.naming.NamingException;
+import javax.naming.Reference;
+
+import javax.resource.spi.AdministeredObject;
+import javax.resource.spi.ConfigProperty;
+import javax.resource.spi.ResourceAdapter;
+import javax.resource.spi.ResourceAdapterAssociation;
+
+/**
+ * AnnoAdminObjectImpl
+ * 
+ * @version $Revision: $
+ */
+@AdministeredObject(adminObjectInterfaces = { AnnoAdminObject1.class })
+public class AnnoAdminObjectImpl1 implements AnnoAdminObject1,
+		ResourceAdapterAssociation {
+	/** Serial version uid */
+	private static final long serialVersionUID = 1L;
+
+	/** The resource adapter */
+	private ResourceAdapter ra;
+
+	/** Reference */
+	private Reference reference;
+
+	/** first */
+	@ConfigProperty(defaultValue = "3.14", description = { "1st", "first" }, ignore = true, supportsDynamicUpdates = false, confidential = true)
+	private Float first;
+
+	/** second */
+	private String second;
+
+	/**
+	 * Default constructor
+	 */
+	public AnnoAdminObjectImpl1() {
+
+	}
+
+	/**
+	 * Set first
+	 * 
+	 * @param first
+	 *            The value
+	 */
+	public void setFirst(Float first) {
+		this.first = first;
+	}
+
+	/**
+	 * Get first
+	 * 
+	 * @return The value
+	 */
+	public Float getFirst() {
+		return first;
+	}
+
+	/**
+	 * Set second
+	 * 
+	 * @param second
+	 *            The value
+	 */
+	@ConfigProperty(defaultValue = "B", description = { "2nd", "second" }, ignore = false, supportsDynamicUpdates = true, confidential = false)
+	public void setSecond(String second) {
+		this.second = second;
+	}
+
+	/**
+	 * Get second
+	 * 
+	 * @return The value
+	 */
+	public String getSecond() {
+		return second;
+	}
+
+	/**
+	 * Get the resource adapter
+	 * 
+	 * @return The handle
+	 */
+	public ResourceAdapter getResourceAdapter() {
+		return ra;
+	}
+
+	/**
+	 * Set the resource adapter
+	 * 
+	 * @param ra
+	 *            The handle
+	 */
+	public void setResourceAdapter(ResourceAdapter ra) {
+		this.ra = ra;
+	}
+
+	/**
+	 * Get the Reference instance.
+	 * 
+	 * @return Reference instance
+	 * @exception NamingException
+	 *                Thrown if a reference can't be obtained
+	 */
+	@Override
+	public Reference getReference() throws NamingException {
+		return reference;
+	}
+
+	/**
+	 * Set the Reference instance.
+	 * 
+	 * @param reference
+	 *            A Reference instance
+	 */
+	@Override
+	public void setReference(Reference reference) {
+		this.reference = reference;
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * 
+	 * @return A hash code value for this object.
+	 */
+	@Override
+	public int hashCode() {
+		int result = 17;
+		if (first != null)
+			result += 31 * result + 7 * first.hashCode();
+		else
+			result += 31 * result + 7;
+		if (second != null)
+			result += 31 * result + 7 * second.hashCode();
+		else
+			result += 31 * result + 7;
+		return result;
+	}
+
+	/**
+	 * Indicates whether some other object is equal to this one.
+	 * 
+	 * @param other
+	 *            The reference object with which to compare.
+	 * @return true if this object is the same as the obj argument, false
+	 *         otherwise.
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (other == null)
+			return false;
+		if (other == this)
+			return true;
+		if (!(other instanceof AnnoAdminObjectImpl1))
+			return false;
+		boolean result = true;
+		AnnoAdminObjectImpl1 obj = (AnnoAdminObjectImpl1) other;
+		if (result) {
+			if (first == null)
+				result = obj.getFirst() == null;
+			else
+				result = first.equals(obj.getFirst());
+		}
+		if (result) {
+			if (second == null)
+				result = obj.getSecond() == null;
+			else
+				result = second.equals(obj.getSecond());
+		}
+		return result;
+	}
+
+}
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/annorar/AnnoManagedConnection1.java
@@ -0,0 +1,251 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.jca.annorar;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ConnectionEvent;
+import javax.resource.spi.ConnectionEventListener;
+import javax.resource.spi.ConnectionRequestInfo;
+import javax.resource.spi.LocalTransaction;
+import javax.resource.spi.ManagedConnection;
+import javax.resource.spi.ManagedConnectionMetaData;
+
+import javax.security.auth.Subject;
+import javax.transaction.xa.XAResource;
+
+/**
+ * AnnoManagedConnection
+ * 
+ * @version $Revision: $
+ */
+public class AnnoManagedConnection1 implements ManagedConnection {
+
+	/** The logger */
+	private static Logger log = Logger.getLogger("AnnoManagedConnection");
+
+	/** The logwriter */
+	private PrintWriter logwriter;
+
+	/** ManagedConnectionFactory */
+	private AnnoManagedConnectionFactory1 mcf;
+
+	/** Listeners */
+	private List<ConnectionEventListener> listeners;
+
+	/** Connection */
+	private AnnoConnectionImpl1 connection;
+
+	/**
+	 * Default constructor
+	 * 
+	 * @param mcf
+	 *            mcf
+	 */
+	public AnnoManagedConnection1(AnnoManagedConnectionFactory1 mcf) {
+		this.mcf = mcf;
+		this.logwriter = null;
+		this.listeners = Collections
+				.synchronizedList(new ArrayList<ConnectionEventListener>(1));
+		this.connection = null;
+	}
+
+	/**
+	 * Creates a new connection handle for the underlying physical connection
+	 * represented by the ManagedConnection instance.
+	 * 
+	 * @param subject
+	 *            Security context as JAAS subject
+	 * @param cxRequestInfo
+	 *            ConnectionRequestInfo instance
+	 * @return generic Object instance representing the connection handle.
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public Object getConnection(Subject subject,
+			ConnectionRequestInfo cxRequestInfo) throws ResourceException {
+		log.finest("getConnection()");
+		connection = new AnnoConnectionImpl1(this, mcf);
+		return connection;
+	}
+
+	/**
+	 * Used by the container to change the association of an application-level
+	 * connection handle with a ManagedConneciton instance.
+	 * 
+	 * @param connection
+	 *            Application-level connection handle
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public void associateConnection(Object connection) throws ResourceException {
+		log.finest("associateConnection()");
+
+		if (connection == null)
+			throw new ResourceException("Null connection handle");
+
+		if (!(connection instanceof AnnoConnectionImpl))
+			throw new ResourceException("Wrong connection handle");
+
+		this.connection = (AnnoConnectionImpl1) connection;
+	}
+
+	/**
+	 * Application server calls this method to force any cleanup on the
+	 * ManagedConnection instance.
+	 * 
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public void cleanup() throws ResourceException {
+		log.finest("cleanup()");
+	}
+
+	/**
+	 * Destroys the physical connection to the underlying resource manager.
+	 * 
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public void destroy() throws ResourceException {
+		log.finest("destroy()");
+	}
+
+	/**
+	 * Adds a connection event listener to the ManagedConnection instance.
+	 * 
+	 * @param listener
+	 *            A new ConnectionEventListener to be registered
+	 */
+	public void addConnectionEventListener(ConnectionEventListener listener) {
+		log.finest("addConnectionEventListener()");
+		if (listener == null)
+			throw new IllegalArgumentException("Listener is null");
+		listeners.add(listener);
+	}
+
+	/**
+	 * Removes an already registered connection event listener from the
+	 * ManagedConnection instance.
+	 * 
+	 * @param listener
+	 *            already registered connection event listener to be removed
+	 */
+	public void removeConnectionEventListener(ConnectionEventListener listener) {
+		log.finest("removeConnectionEventListener()");
+		if (listener == null)
+			throw new IllegalArgumentException("Listener is null");
+		listeners.remove(listener);
+	}
+
+	/**
+	 * Close handle
+	 * 
+	 * @param handle
+	 *            The handle
+	 */
+	void closeHandle(AnnoConnection1 handle) {
+		ConnectionEvent event = new ConnectionEvent(this,
+				ConnectionEvent.CONNECTION_CLOSED);
+		event.setConnectionHandle(handle);
+		for (ConnectionEventListener cel : listeners) {
+			cel.connectionClosed(event);
+		}
+
+	}
+
+	/**
+	 * Gets the log writer for this ManagedConnection instance.
+	 * 
+	 * @return Character output stream associated with this Managed-Connection
+	 *         instance
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public PrintWriter getLogWriter() throws ResourceException {
+		log.finest("getLogWriter()");
+		return logwriter;
+	}
+
+	/**
+	 * Sets the log writer for this ManagedConnection instance.
+	 * 
+	 * @param out
+	 *            Character Output stream to be associated
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public void setLogWriter(PrintWriter out) throws ResourceException {
+		log.finest("setLogWriter()");
+		logwriter = out;
+	}
+
+	/**
+	 * Returns an <code>javax.resource.spi.LocalTransaction</code> instance.
+	 * 
+	 * @return LocalTransaction instance
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public LocalTransaction getLocalTransaction() throws ResourceException {
+		log.finest("getLocalTransaction()");
+		return null;
+	}
+
+	/**
+	 * Returns an <code>javax.transaction.xa.XAresource</code> instance.
+	 * 
+	 * @return XAResource instance
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public XAResource getXAResource() throws ResourceException {
+		log.finest("getXAResource()");
+		return null;
+	}
+
+	/**
+	 * Gets the metadata information for this connection's underlying EIS
+	 * resource manager instance.
+	 * 
+	 * @return ManagedConnectionMetaData instance
+	 * @throws ResourceException
+	 *             generic exception if operation fails
+	 */
+	public ManagedConnectionMetaData getMetaData() throws ResourceException {
+		log.finest("getMetaData()");
+		return new AnnoManagedConnectionMetaData();
+	}
+
+	/**
+	 * Call me
+	 */
+	void callMe() {
+		log.finest("callMe()");
+	}
+
+}
