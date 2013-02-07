2ff9802a996f3c57a2455534f3d099f16ca36cd5
Carlo de Wolf <cdewolf@redhat.com>
2013-02-07 12:39:49 +0100

    EJB_SPEC-60: create a proper test case

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/mdb/dynamic/DynamicMessageListenerTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/mdb/dynamic/DynamicMessageListenerTestCase.java
@@ -27,14 +27,26 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.test.integration.ejb.mdb.dynamic.adapter.TelnetResourceAdapter;
 import org.jboss.as.test.integration.ejb.mdb.dynamic.api.TelnetListener;
 import org.jboss.as.test.integration.ejb.mdb.dynamic.application.MyMdb;
+import org.jboss.as.test.integration.ejb.mdb.dynamic.impl.TelnetInputStream;
+import org.jboss.as.test.integration.ejb.mdb.dynamic.impl.TelnetPrintStream;
 import org.jboss.as.test.integration.ejb.mdb.dynamic.impl.TelnetServer;
+import org.jboss.as.test.integration.ejb.remote.common.EJBManagementUtil;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+import static org.jboss.as.test.integration.ejb.mdb.dynamic.impl.TtyCodes.TTY_Bright;
+import static org.jboss.as.test.integration.ejb.mdb.dynamic.impl.TtyCodes.TTY_Reset;
 import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
@@ -42,6 +54,14 @@ import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
 @RunWith(Arquillian.class)
 @RunAsClient
 public class DynamicMessageListenerTestCase {
+    private static void assertEmptyLine(final DataInputStream in) throws IOException {
+        assertEquals("", in.readLine());
+    }
+
+    private static void assertReply(final String expected, final String actual) {
+        assertEquals(TTY_Reset + TTY_Bright + "pronto> " + TTY_Reset + expected, actual);
+    }
+
     @Deployment
     public static Archive createDeplyoment() {
         final EnterpriseArchive ear = create(EnterpriseArchive.class, "ear-with-rar.ear")
@@ -55,6 +75,27 @@ public class DynamicMessageListenerTestCase {
 
     @Test
     public void test1() throws Exception {
-        Thread.currentThread().sleep(5000);
+        final Socket socket = new Socket(EJBManagementUtil.getNodeName(), 2020);
+        final OutputStream sockOut = socket.getOutputStream();
+        final DataInputStream in = new DataInputStream(new TelnetInputStream(socket.getInputStream(), sockOut));
+        final PrintStream out = new TelnetPrintStream(sockOut);
+
+        assertEmptyLine(in);
+        assertEquals("type 'help' for a list of commands", in.readLine());
+
+        out.println("set a b");
+        out.flush();
+        assertReply("set a to b", in.readLine());
+        assertEmptyLine(in);
+
+        out.println("get a");
+        out.flush();
+        assertReply("b", in.readLine());
+        assertEmptyLine(in);
+
+        out.println("list");
+        out.flush();
+        assertReply("a = b", in.readLine());
+        assertEmptyLine(in);
     }
 }
