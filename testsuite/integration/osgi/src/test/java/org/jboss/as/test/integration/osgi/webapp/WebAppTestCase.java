9e56569434c745a35a7f515a65bc3ad0c77f5c8a
Richard Opalka <opalka.richard@gmail.com>
2013-02-12 15:27:50 +0100

    [AS7-6511] excluding another OSGi tests

--- testsuite/integration/osgi/src/test/java/org/jboss/as/test/integration/osgi/webapp/WebAppTestCase.java
+++ testsuite/integration/osgi/src/test/java/org/jboss/as/test/integration/osgi/webapp/WebAppTestCase.java
@@ -243,6 +243,7 @@ public class WebAppTestCase {
     }
 
     @Test
+    @org.junit.Ignore("AS7-6511")
     public void testDeferredBundleWithWabExtension() throws Exception {
         InputStream input = deployer.getDeployment(BUNDLE_C_WAB);
         Bundle bundle = context.installBundle(BUNDLE_C_WAB, input);
