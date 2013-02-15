c29d067448ee50082e118618cf5101c255cd1e6f
Vladimir Rastseluev <vrastsel@redhat.com>
2013-02-15 13:53:53 +0100

    AS7-6525 test changes

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/moduledeployment/TwoRaJarTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/moduledeployment/TwoRaJarTestCase.java
@@ -21,10 +21,17 @@
  */
 package org.jboss.as.test.integration.jca.moduledeployment;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.dmr.ModelNode;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
@@ -45,15 +52,14 @@ import org.junit.runner.RunWith;
  *         modulename/main/module.jar
  */
 @RunWith(Arquillian.class)
-@ServerSetup(TwoRaJarTestCase.ModuleAcDeploymentTestCaseSetup.class)
+@ServerSetup(TwoRaJarTestCase.ModuleAcDeploymentTestCaseSetup1.class)
 public class TwoRaJarTestCase extends TwoRaFlatTestCase {
 
-
-	static class ModuleAcDeploymentTestCaseSetup extends
+	static class ModuleAcDeploymentTestCaseSetup1 extends
 			ModuleDeploymentTestCaseSetup {
-		
+
 		public static ModelNode address1;
-		
+
 		@Override
 		public void doSetup(ManagementClient managementClient) throws Exception {
 
@@ -64,7 +70,7 @@ public class TwoRaJarTestCase extends TwoRaFlatTestCase {
 			setConfiguration("basic.xml");
 
 		}
-		
+
 		@Override
 		public void tearDown(ManagementClient managementClient,
 				String containerId) throws Exception {
@@ -73,4 +79,42 @@ public class TwoRaJarTestCase extends TwoRaFlatTestCase {
 		}
 
 	}
+
+	/**
+	 * Define the deployment
+	 * 
+	 * @return The deployment archive
+	 */
+	@Deployment
+	public static JavaArchive createDeployment() throws Exception {
+		JavaArchive ja = createDeployment(TwoRaFlatTestCase.class);
+		ja.addClass(TwoRaJarTestCase.class);
+		return ja;
+	}
+
+	/**
+	 * Tests connection in pool
+	 * 
+	 * @throws Exception
+	 *             in case of error
+	 */
+	@Test
+	@RunAsClient
+	public void testConnection2() throws Exception {
+		final ModelNode address1 = ModuleAcDeploymentTestCaseSetup1.address1
+				.clone();
+		address1.add("connection-definitions", cf1);
+		address1.protect();
+		final ModelNode operation1 = new ModelNode();
+		operation1.get(OP).set("test-connection-in-pool");
+		operation1.get(OP_ADDR).set(address1);
+		executeOperation(operation1);
+
+	}
+
+	@Override
+	protected ModelNode getAddress() {
+		return ModuleAcDeploymentTestCaseSetup1.getAddress();
+	}
+
 }
