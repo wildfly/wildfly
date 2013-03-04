3a9f7baaa121ced67f21b45dd1c7823081968c40
David M. Lloyd <david.lloyd@redhat.com>
2013-03-04 11:29:01 -0600

    Add a privileged action for MessageDrivenComponent getClass()

--- ejb3/src/main/java/org/jboss/as/ejb3/component/messagedriven/MessageDrivenComponent.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/messagedriven/MessageDrivenComponent.java
@@ -42,10 +42,12 @@ import org.jboss.as.ejb3.inflow.MessageEndpointService;
 import org.jboss.as.ejb3.pool.Pool;
 import org.jboss.as.ejb3.pool.StatelessObjectFactory;
 import org.jboss.as.naming.ManagedReference;
+import org.jboss.as.util.security.GetClassLoaderAction;
 import org.jboss.invocation.Interceptor;
 import org.jboss.invocation.InterceptorFactoryContext;
 import org.jboss.jca.core.spi.rar.Endpoint;
 
+import static java.security.AccessController.doPrivileged;
 import static java.util.Collections.emptyMap;
 import static javax.ejb.TransactionAttributeType.REQUIRED;
 import static org.jboss.as.ejb3.EjbLogger.ROOT_LOGGER;
@@ -101,6 +103,7 @@ public class MessageDrivenComponent extends EJBComponent implements PooledCompon
 
         this.activationSpec = activationSpec;
         this.messageListenerInterface = messageListenerInterface;
+        final ClassLoader componentClassLoader = doPrivileged(new GetClassLoaderAction(ejbComponentCreateService.getComponentClass()));
         final MessageEndpointService<?> service = new MessageEndpointService<Object>() {
             @Override
             public Class<Object> getMessageListenerInterface() {
@@ -134,10 +137,10 @@ public class MessageDrivenComponent extends EJBComponent implements PooledCompon
 
             @Override
             public ClassLoader getClassLoader() {
-                return ejbComponentCreateService.getComponentClass().getClassLoader();
+                return componentClassLoader;
             }
         };
-        this.endpointFactory = new JBossMessageEndpointFactory(getComponentClass().getClassLoader(), service, (Class<Object>) getComponentClass());
+        this.endpointFactory = new JBossMessageEndpointFactory(componentClassLoader, service, (Class<Object>) getComponentClass());
     }
 
     @Override
