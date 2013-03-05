ab71141bd45d0aafaac6c6828b4a5d042634657b
Stefano Maestri <stefano.maestri@redhat.com>
2013-03-05 15:29:30 +0100

    AS7-6677 Cannot deploy application using datasource with empty password value

--- connector/src/main/java/org/jboss/as/connector/deployers/ds/DsXmlParser.java
+++ connector/src/main/java/org/jboss/as/connector/deployers/ds/DsXmlParser.java
@@ -97,7 +97,7 @@ public class DsXmlParser extends DsParser {
                     switch (tag) {
                         case PASSWORD: {
                             password = elementAsString(reader);
-                            if (propertyResolver != null && password != null) {
+                            if (propertyResolver != null && password != null && password.trim().length() != 0) {
                                 String resolvedPassword = propertyResolver.resolve(password);
                                 if (resolvedPassword != null)
                                     password = resolvedPassword;
