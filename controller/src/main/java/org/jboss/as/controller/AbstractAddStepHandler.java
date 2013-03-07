d51bc216f879ec2d19511c6e0b60dc374987684e
Jeff Mesnil <jmesnil@gmail.com>
2013-03-07 16:12:07 +0100

    [WFLY-266] Notification Support
    * emit notifications:
      * when a resource is added using AbstractAddStepHandler
      * when a resource is removed using AbstractRemoveStepHandler

--- controller/src/main/java/org/jboss/as/controller/AbstractAddStepHandler.java
+++ controller/src/main/java/org/jboss/as/controller/AbstractAddStepHandler.java
@@ -22,12 +22,21 @@
 
 package org.jboss.as.controller;
 
+import static org.jboss.as.controller.ControllerMessages.MESSAGES;
+import static org.jboss.as.controller.PathAddress.pathAddress;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.FAILED;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESOURCE_ADDED_NOTIFICATION;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import org.jboss.as.controller.notification.Notification;
+import org.jboss.as.controller.notification.NotificationService;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceController;
@@ -86,6 +95,23 @@ public class AbstractAddStepHandler implements OperationStepHandler {
                     });
                 }
             }, OperationContext.Stage.RUNTIME);
+            context.addStep(new OperationStepHandler() {
+                @Override
+                public void execute(OperationContext context, ModelNode operation) throws OperationFailedException {
+                    try {
+                        if (FAILED.equals(context.getResult().get(OUTCOME).asString())) {
+                            return;
+                        }
+                        PathAddress sourceAddress = pathAddress(operation.get(OP_ADDR));
+                        Notification notification = new Notification(RESOURCE_ADDED_NOTIFICATION,
+                                sourceAddress,
+                                MESSAGES.resourceWasAdded(sourceAddress));
+                        NotificationService.emitNotification(context, notification);
+                    } finally {
+                        context.stepCompleted();
+                    }
+                }
+            }, OperationContext.Stage.VERIFY);
         }
         context.stepCompleted();
     }
