f4e7e396f8ade0f23c9f100cd8f0be7ab705b8e4
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-03-11 15:59:10 +1100

    AS7-5737 If the factory class in the reference is null search for a URL provider

--- naming/src/main/java/org/jboss/as/naming/context/ObjectFactoryBuilder.java
+++ naming/src/main/java/org/jboss/as/naming/context/ObjectFactoryBuilder.java
@@ -25,11 +25,14 @@ package org.jboss.as.naming.context;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Hashtable;
+import java.util.StringTokenizer;
 
 import javax.naming.Context;
 import javax.naming.Name;
 import javax.naming.NamingException;
+import javax.naming.RefAddr;
 import javax.naming.Reference;
+import javax.naming.StringRefAddr;
 import javax.naming.directory.Attributes;
 import javax.naming.spi.DirObjectFactory;
 import javax.naming.spi.ObjectFactory;
@@ -59,7 +62,7 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
      * from the reference.  If the parameter is not a reference, or the reference does not create an {@link javax.naming.spi.ObjectFactory}
      * it will return {@code this} as the {@link javax.naming.spi.ObjectFactory} to use.
      *
-     * @param obj The object bound in the naming context
+     * @param obj         The object bound in the naming context
      * @param environment The environment information
      * @return The object factory the object resolves to
      * @throws NamingException If any problems occur
@@ -69,7 +72,7 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
             if (obj instanceof Reference) {
                 return factoryFromReference((Reference) obj, environment);
             }
-        } catch(Throwable ignored) {
+        } catch (Throwable ignored) {
         }
         return this;
     }
@@ -77,30 +80,30 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
     /**
      * Create an object instance.
      *
-     * @param ref Object containing reference information
-     * @param name The name relative to nameCtx
-     * @param nameCtx The naming context
+     * @param ref         Object containing reference information
+     * @param name        The name relative to nameCtx
+     * @param nameCtx     The naming context
      * @param environment The environment information
      * @return The object
      * @throws Exception If any error occur
      */
     public Object getObjectInstance(final Object ref, final Name name, final Context nameCtx, final Hashtable<?, ?> environment) throws Exception {
         final ClassLoader classLoader = SecurityActions.getContextClassLoader();
-        if(classLoader == null) {
+        if (classLoader == null) {
             return ref;
         }
-        final String factoriesProp = (String)environment.get(Context.OBJECT_FACTORIES);
-        if(factoriesProp != null) {
+        final String factoriesProp = (String) environment.get(Context.OBJECT_FACTORIES);
+        if (factoriesProp != null) {
             final String[] classes = factoriesProp.split(":");
-            for(String className : classes) {
+            for (String className : classes) {
                 try {
                     final Class<?> factoryClass = classLoader.loadClass(className);
                     final ObjectFactory objectFactory = ObjectFactory.class.cast(factoryClass.newInstance());
                     final Object result = objectFactory.getObjectInstance(ref, name, nameCtx, environment);
-                    if(result != null) {
+                    if (result != null) {
                         return result;
                     }
-                } catch(Throwable ignored) {
+                } catch (Throwable ignored) {
                 }
             }
         }
@@ -110,36 +113,36 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
     /**
      * Create an object instance.
      *
-     * @param ref Object containing reference information
-     * @param name The name relative to nameCtx
-     * @param nameCtx The naming context
+     * @param ref         Object containing reference information
+     * @param name        The name relative to nameCtx
+     * @param nameCtx     The naming context
      * @param environment The environment information
-     * @param attributes The directory attributes
+     * @param attributes  The directory attributes
      * @return The object
      * @throws Exception If any error occur
      */
     public Object getObjectInstance(final Object ref, final Name name, final Context nameCtx, final Hashtable<?, ?> environment, final Attributes attributes) throws Exception {
         final ClassLoader classLoader = SecurityActions.getContextClassLoader();
-        if(classLoader == null) {
+        if (classLoader == null) {
             return ref;
         }
-        final String factoriesProp = (String)environment.get(Context.OBJECT_FACTORIES);
-        if(factoriesProp != null) {
+        final String factoriesProp = (String) environment.get(Context.OBJECT_FACTORIES);
+        if (factoriesProp != null) {
             final String[] classes = factoriesProp.split(":");
-            for(String className : classes) {
+            for (String className : classes) {
                 try {
                     final Class<?> factoryClass = classLoader.loadClass(className);
                     final ObjectFactory objectFactory = ObjectFactory.class.cast(factoryClass.newInstance());
                     final Object result;
-                    if(objectFactory instanceof DirObjectFactory) {
+                    if (objectFactory instanceof DirObjectFactory) {
                         result = DirObjectFactory.class.cast(objectFactory).getObjectInstance(ref, name, nameCtx, environment, attributes);
                     } else {
                         result = objectFactory.getObjectInstance(ref, name, nameCtx, environment);
                     }
-                    if(result != null) {
+                    if (result != null) {
                         return result;
                     }
-                } catch(Throwable ignored) {
+                } catch (Throwable ignored) {
                 }
             }
         }
@@ -147,6 +150,9 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
     }
 
     private ObjectFactory factoryFromReference(final Reference reference, final Hashtable<?, ?> environment) throws Exception {
+        if (reference.getFactoryClassName() == null) {
+            return lookForURLs(reference, environment);
+        }
         if (reference instanceof ModularReference) {
             return factoryFromModularReference(ModularReference.class.cast(reference), environment);
         }
@@ -172,6 +178,99 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
         }
     }
 
+    static ObjectFactory lookForURLs(Reference ref, Hashtable environment)
+            throws NamingException {
+
+        for (int i = 0; i < ref.size(); i++) {
+            RefAddr addr = ref.get(i);
+            if (addr instanceof StringRefAddr &&
+                    addr.getType().equalsIgnoreCase("URL")) {
+
+                String url = (String) addr.getContent();
+                ObjectFactory answer = processURL(url, environment);
+                if (answer != null) {
+                    return answer;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static ObjectFactory processURL(Object refInfo, Hashtable environment) throws NamingException {
+
+        if (refInfo instanceof String) {
+            String url = (String) refInfo;
+            String scheme = getURLScheme(url);
+            if (scheme != null) {
+                ObjectFactory answer = getURLObjectFactory(scheme, url, environment);
+                if (answer != null) {
+                    return answer;
+                }
+            }
+        }
+
+        if (refInfo instanceof String[]) {
+            String[] urls = (String[]) refInfo;
+            for (int i = 0; i < urls.length; i++) {
+                String scheme = getURLScheme(urls[i]);
+                if (scheme != null) {
+                    ObjectFactory answer = getURLObjectFactory(scheme, urls[i], environment);
+                    if (answer != null) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static ObjectFactory getURLObjectFactory(String scheme, String url, Hashtable environment) throws NamingException {
+
+        String facProp = (String) environment.get(Context.URL_PKG_PREFIXES);
+        if (facProp != null) {
+            facProp += ":" + "com.sun.jndi.url";
+        } else {
+            facProp = "com.sun.jndi.url";
+        }
+
+        ClassLoader loader = SecurityActions.getContextClassLoader();
+
+        String suffix = "." + scheme + "." + scheme + "URLContextFactory";
+
+        // Not cached; find first factory and cache
+        StringTokenizer parser = new StringTokenizer(facProp, ":");
+        String className;
+        ObjectFactory factory = null;
+        while (parser.hasMoreTokens()) {
+            className = parser.nextToken() + suffix;
+            try {
+                Class<?> clazz;
+                if (loader == null) {
+                    clazz = Class.forName(className);
+                } else {
+                    clazz = Class.forName(className, true, loader);
+                }
+                return new ReferenceUrlContextFactoryWrapper((ObjectFactory) clazz.newInstance(), url);
+            } catch (InstantiationException | IllegalAccessException e) {
+                NamingException ne = new NamingException(className);
+                ne.setRootCause(e);
+                throw ne;
+            } catch (Exception e) {
+            }
+        }
+
+        return factory;
+    }
+
+
+    private static String getURLScheme(String str) {
+        int colon = str.indexOf(':');
+        int slash = str.indexOf('/');
+
+        if (colon > 0 && (slash == -1 || colon + 1 == slash))
+            return str.substring(0, colon);
+        return null;
+    }
 
     private static ServiceContainer currentServiceContainer() {
         return AccessController.doPrivileged(new PrivilegedAction<ServiceContainer>() {
@@ -180,5 +279,21 @@ public class ObjectFactoryBuilder implements javax.naming.spi.ObjectFactoryBuild
                 return CurrentServiceContainer.getServiceContainer();
             }
         });
+    }
+
+    private static final class ReferenceUrlContextFactoryWrapper implements ObjectFactory {
+
+        private final ObjectFactory factory;
+        private final String url;
+
+        private ReferenceUrlContextFactoryWrapper(final ObjectFactory factory, final String url) {
+            this.factory = factory;
+            this.url = url;
+        }
+
+        @Override
+        public Object getObjectInstance(final Object obj, final Name name, final Context nameCtx, final Hashtable<?, ?> environment) throws Exception {
+            return factory.getObjectInstance(url, name, nameCtx, environment);
+        }
     }
 }
