2dd17eac8e85dea0abc77ad0757248b7f6ada381
Tomaz Cerar <tomaz.cerar@gmail.com>
2013-03-14 01:58:54 +0100

    Upgrade few plugins & dependencies
    * arquillian
    * shrinkwrap
    * checkstyle
    * + fix problems that resulted as part of upgrades

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/moduledeployment/AbstractModuleDeploymentTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/moduledeployment/AbstractModuleDeploymentTestCase.java
@@ -21,8 +21,15 @@
  */
 package org.jboss.as.test.integration.jca.moduledeployment;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Iterator;
 import java.util.Set;
+import javax.naming.InitialContext;
+import javax.resource.cci.ConnectionFactory;
 
 import org.jboss.as.test.integration.jca.JcaMgmtBase;
 import org.jboss.as.test.integration.jca.JcaMgmtServerSetupTask;
@@ -36,124 +43,110 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.staxmapper.XMLElementReader;
 import org.jboss.staxmapper.XMLElementWriter;
 
-import javax.naming.InitialContext;
-import javax.resource.cci.ConnectionFactory;
-
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 /**
  * AS7-5768 -Support for RA module deployment
- * 
+ *
  * @author <a href="vrastsel@redhat.com">Vladimir Rastseluev</a>
- * 
  */
 public abstract class AbstractModuleDeploymentTestCase extends
-		ContainerResourceMgmtTestBase {
+        ContainerResourceMgmtTestBase {
 
-	/**
-	 * Define the deployment
-	 * 
-	 * @return The deployment archive
-	 */
-	public static JavaArchive createDeployment(
-			Class<? extends AbstractModuleDeploymentTestCase> clazz)
-			throws Exception {
+    /**
+     * Define the deployment
+     *
+     * @return The deployment archive
+     */
+    public static JavaArchive createDeployment(
+            Class<? extends AbstractModuleDeploymentTestCase> clazz)
+            throws Exception {
 
-		JavaArchive ja = ShrinkWrap.create(JavaArchive.class, "multiple.jar");
-		ja.addClasses(clazz, JcaMgmtServerSetupTask.class, JcaMgmtBase.class,
-				MgmtOperationException.class, XMLElementReader.class,
-				XMLElementWriter.class, AbstractModuleDeploymentTestCase.class,
-				ModuleDeploymentTestCaseSetup.class);
+        JavaArchive ja = ShrinkWrap.create(JavaArchive.class, "multiple.jar");
+        ja.addClasses(clazz, JcaMgmtServerSetupTask.class, JcaMgmtBase.class,
+                MgmtOperationException.class, XMLElementReader.class,
+                XMLElementWriter.class, AbstractModuleDeploymentTestCase.class,
+                ModuleDeploymentTestCaseSetup.class);
 
-		ja.addPackage(AbstractMgmtTestBase.class.getPackage());
+        ja.addPackage(AbstractMgmtTestBase.class.getPackage());
 
-		ja.addAsManifestResource(
-				new StringAsset(
-						"Dependencies: org.jboss.as.controller-client,org.jboss.dmr,org.jboss.as.cli,javax.inject.api,org.jboss.as.connector\n"),
-				"MANIFEST.MF");
+        ja.addAsManifestResource(
+                new StringAsset(
+                        "Dependencies: org.jboss.as.controller-client,org.jboss.dmr,org.jboss.as.cli,javax.inject.api,org.jboss.as.connector\n"),
+                "MANIFEST.MF");
 
-		return ja;
+        return ja;
 
-	}
+    }
 
-	/**
-	 * Test configuration
-	 * 
-	 * @throws Throwable
-	 *             in case of an error
-	 */
-	public void testConnectionFactory(ConnectionFactory connectionFactory)
-			throws Throwable {
-		assertNotNull(connectionFactory);
-		assertNotNull(connectionFactory.getConnection());
-	}
+    /**
+     * Test configuration
+     *
+     * @throws Throwable in case of an error
+     */
+    public void testConnectionFactory(ConnectionFactory connectionFactory)
+            throws Throwable {
+        assertNotNull(connectionFactory);
+        assertNotNull(connectionFactory.getConnection());
+    }
 
-	/**
-	 * Tests connection in pool
-	 * 
-	 * @throws Exception
-	 *             in case of error
-	 */
-	public void testConnection(String conName) throws Exception {
-		final ModelNode address1 = getAddress().clone();
-		address1.add("connection-definitions", conName);
-		address1.protect();
+    /**
+     * Tests connection in pool
+     *
+     * @throws Exception in case of error
+     */
+    public void testConnection(String conName) throws Exception {
+        final ModelNode address1 = getAddress().clone();
+        address1.add("connection-definitions", conName);
+        address1.protect();
 
-		final ModelNode operation1 = new ModelNode();
-		operation1.get(OP).set("test-connection-in-pool");
-		operation1.get(OP_ADDR).set(address1);
-		executeOperation(operation1);
-	}
+        final ModelNode operation1 = new ModelNode();
+        operation1.get(OP).set("test-connection-in-pool");
+        operation1.get(OP_ADDR).set(address1);
+        executeOperation(operation1);
+    }
 
-	/**
-	 * Returns basic address of resource adapter
-	 * 
-	 * @return address
-	 */
-	abstract protected ModelNode getAddress();
+    /**
+     * Returns basic address of resource adapter
+     *
+     * @return address
+     */
+    abstract protected ModelNode getAddress();
 
-	/**
-	 * Finding object by JNDI name and checks, if its String representation
-	 * contains expected substrings
-	 * 
-	 * @param jndiName
-	 *            of object
-	 * @param contains
-	 *            - substring, must be contained
-	 * @throws Exception
-	 */
-	public void testJndiObject(String jndiName, String... contains)
-			throws Exception {
-		Object o = new InitialContext().lookup(jndiName);
-		assertNotNull(o);
-		for (String c : contains)
-			assertTrue(o.toString() + " should contain " + c, o.toString()
-					.contains(c));
+    /**
+     * Finding object by JNDI name and checks, if its String representation
+     * contains expected substrings
+     *
+     * @param jndiName of object
+     * @param contains - substring, must be contained
+     * @throws Exception
+     */
+    public void testJndiObject(String jndiName, String... contains)
+            throws Exception {
+        Object o = new InitialContext().lookup(jndiName);
+        assertNotNull(o);
+        for (String c : contains) {
+            assertTrue(o.toString() + " should contain " + c, o.toString()
+                    .contains(c));
+        }
 
-	}
+    }
 
-	/**
-	 * Checks Set if there is a String element, containing some substring and
-	 * returns it
-	 * 
-	 * @param ids
-	 *            - Set
-	 * @param contain
-	 *            - substring
-	 * @return String
-	 */
-	public String getElementContaining(Set<String> ids, String contain) {
-		Iterator<String> it = ids.iterator();
-		while (it.hasNext()) {
-			String t = it.next();
-			if (t.contains(contain)) {
-				return t;
-			}
-		}
-		return null;
-	}
+    /**
+     * Checks Set if there is a String element, containing some substring and
+     * returns it
+     *
+     * @param ids     - Set
+     * @param contain - substring
+     * @return String
+     */
+    public String getElementContaining(Set<String> ids, String contain) {
+        Iterator<String> it = ids.iterator();
+        while (it.hasNext()) {
+            String t = it.next();
+            if (t.contains(contain)) {
+                return t;
+            }
+        }
+        return null;
+    }
 
 }
--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/moduledeployment/TwoModulesOfDifferentTypeTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/moduledeployment/TwoModulesOfDifferentTypeTestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat, Inc., and individual contributors
+ * Copyright 2013, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -26,12 +26,12 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.as.test.integration.jca.JcaMgmtBase;
 import org.jboss.as.test.integration.jca.JcaMgmtServerSetupTask;
-import org.jboss.as.test.integration.jca.moduledeployment.TwoModulesFlatTestCase.ModuleAcDeploymentTestCaseSetup;
 import org.jboss.as.test.integration.management.base.AbstractMgmtTestBase;
 import org.jboss.as.test.integration.management.util.MgmtOperationException;
 import org.jboss.dmr.ModelNode;
@@ -46,18 +46,18 @@ import org.junit.runner.RunWith;
 
 /**
  * AS7-5768 -Support for RA module deployment
- * 
+ *
  * @author <a href="vrastsel@redhat.com">Vladimir Rastseluev</a>
- * 
+ *         <p/>
  *         Tests for module deployment of resource adapter archive in
  *         uncompressed form with classes, packed in .jar file
- * 
- *         Structure of module is: 
- *         modulename 
+ *         <p/>
+ *         Structure of module is:
+ *         modulename
  *         modulename/main
- *         modulename/main/module.xml 
+ *         modulename/main/module.xml
  *         modulename/main/META-INF
- *         modulename/main/META-INF/ra.xml 
+ *         modulename/main/META-INF/ra.xml
  *         modulename/main/module.jar
  */
 @RunWith(Arquillian.class)
@@ -65,82 +65,82 @@ import org.junit.runner.RunWith;
 public class TwoModulesOfDifferentTypeTestCase extends TwoModulesFlatTestCase {
 
 
-	static class ModuleAcDeploymentTestCaseSetup extends
-			ModuleDeploymentTestCaseSetup {
-		
-		public static ModelNode address1;
-		
-		@Override
-		public void doSetup(ManagementClient managementClient) throws Exception {
+    static class ModuleAcDeploymentTestCaseSetup extends
+            ModuleDeploymentTestCaseSetup {
 
-			super.doSetup(managementClient);
-			fillModuleWithFlatClasses("ra1.xml");
-			addModule("org/jboss/ironjacamar/ra16out1", "module1-jar.xml");
-			fillModuleWithJar("ra1.xml");
-			setConfiguration("mod-2.xml");
-			address1 = address.clone();
-			setConfiguration("basic.xml");
+        public static ModelNode address1;
 
-		}
-		
-		@Override
-		public void tearDown(ManagementClient managementClient,
-				String containerId) throws Exception {
-			super.tearDown(managementClient, containerId);
-			remove(address1);
-			removeModule("org/jboss/ironjacamar/ra16out1");
-		}
+        @Override
+        public void doSetup(ManagementClient managementClient) throws Exception {
 
-	}
+            super.doSetup(managementClient);
+            fillModuleWithFlatClasses("ra1.xml");
+            addModule("org/jboss/ironjacamar/ra16out1", "module1-jar.xml");
+            fillModuleWithJar("ra1.xml");
+            setConfiguration("mod-2.xml");
+            address1 = address.clone();
+            setConfiguration("basic.xml");
 
-	/**
-	 * Define the deployment
-	 * 
-	 * @return The deployment archive
-	 */
-	@Deployment
-	public static JavaArchive createDeployment() throws Exception {
-		JavaArchive ja = ShrinkWrap.create(JavaArchive.class, "multiple.jar");
-		ja.addClasses(JcaMgmtServerSetupTask.class, JcaMgmtBase.class,
-				MgmtOperationException.class, XMLElementReader.class,
-				XMLElementWriter.class);
+        }
 
-		ja.addPackage(AbstractMgmtTestBase.class.getPackage())
-			.addPackage(AbstractModuleDeploymentTestCase.class.getPackage());
+        @Override
+        public void tearDown(ManagementClient managementClient,
+                             String containerId) throws Exception {
+            super.tearDown(managementClient, containerId);
+            remove(address1);
+            removeModule("org/jboss/ironjacamar/ra16out1");
+        }
 
-		ja.addAsManifestResource(
-				new StringAsset(
-						"Dependencies: org.jboss.as.controller-client,org.jboss.dmr,org.jboss.as.cli,javax.inject.api,org.jboss.as.connector\n"),
-				"MANIFEST.MF");
+    }
 
-		return ja;
+    /**
+     * Define the deployment
+     *
+     * @return The deployment archive
+     */
+    @Deployment(name = "multiple")
+    @TargetsContainer("jboss")
+    public static JavaArchive createDeployment() throws Exception {
+        JavaArchive ja = ShrinkWrap.create(JavaArchive.class, "multiple.jar");
+        ja.addClasses(JcaMgmtServerSetupTask.class, JcaMgmtBase.class,
+                MgmtOperationException.class, XMLElementReader.class,
+                XMLElementWriter.class);
 
-	}
+        ja.addPackage(AbstractMgmtTestBase.class.getPackage())
+                .addPackage(AbstractModuleDeploymentTestCase.class.getPackage());
 
-	/**
-	 * Tests connection in pool
-	 * 
-	 * @throws Exception
-	 *             in case of error
-	 */
-	@Test
-	@RunAsClient
-	public void testConnection2() throws Exception {
-		final ModelNode address1 = ModuleAcDeploymentTestCaseSetup.address1
-				.clone();
-		address1.add("connection-definitions", cf1);
-		address1.protect();
-		final ModelNode operation1 = new ModelNode();
-		operation1.get(OP).set("test-connection-in-pool");
-		operation1.get(OP_ADDR).set(address1);
-		executeOperation(operation1);
+        ja.addAsManifestResource(
+                new StringAsset(
+                        "Dependencies: org.jboss.as.controller-client,org.jboss.dmr,org.jboss.as.cli,javax.inject.api,org.jboss.as.connector\n"),
+                "MANIFEST.MF");
 
-	}
+        return ja;
 
-	@Override
-	protected ModelNode getAddress() {
-		return ModuleAcDeploymentTestCaseSetup.getAddress();
-	}
+    }
+
+    /**
+     * Tests connection in pool
+     *
+     * @throws Exception in case of error
+     */
+    @Test
+    @RunAsClient
+    public void testConnection2() throws Exception {
+        final ModelNode address1 = ModuleAcDeploymentTestCaseSetup.address1
+                .clone();
+        address1.add("connection-definitions", cf1);
+        address1.protect();
+        final ModelNode operation1 = new ModelNode();
+        operation1.get(OP).set("test-connection-in-pool");
+        operation1.get(OP_ADDR).set(address1);
+        executeOperation(operation1);
+
+    }
+
+    @Override
+    protected ModelNode getAddress() {
+        return ModuleAcDeploymentTestCaseSetup.getAddress();
+    }
 
 
 }
