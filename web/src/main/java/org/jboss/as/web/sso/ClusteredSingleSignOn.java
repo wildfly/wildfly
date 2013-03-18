940a9677148c36190977fd6930c6f4de7370b214
Dennis Reed <dereed@redhat.com>
2013-03-18 17:35:44 -0500

    [AS7-6681] Fix ClassCastException in ClusteredSingleSignOn that aborts session expirations

--- web/src/main/java/org/jboss/as/web/sso/ClusteredSingleSignOn.java
+++ web/src/main/java/org/jboss/as/web/sso/ClusteredSingleSignOn.java
@@ -523,13 +523,18 @@ public class ClusteredSingleSignOn extends org.apache.catalina.authenticator.Sin
             synchronized (reverse) {
                 reverse.remove(session);
             }
-            // Invalidate this session, but do not propagate to other nodes
-            ClusteredSession<OutgoingDistributableSessionData> clusteredSession =
-                    (ClusteredSession<OutgoingDistributableSessionData>) session ;
-            boolean notify = true ;
-            boolean localCall = false ;
-            boolean localOnly = true ;
-            clusteredSession.expire(notify, localCall, localOnly, ClusteredSessionNotificationCause.INVALIDATE);
+            if (session instanceof ClusteredSession) {
+                // Invalidate this session, but do not propagate to other nodes
+                ClusteredSession<OutgoingDistributableSessionData> clusteredSession =
+                        (ClusteredSession<OutgoingDistributableSessionData>) session ;
+                boolean notify = true ;
+                boolean localCall = false ;
+                boolean localOnly = true ;
+                clusteredSession.expire(notify, localCall, localOnly, ClusteredSessionNotificationCause.INVALIDATE);
+            } else {
+                // Invalidate this session
+                session.expire();
+            }
         }
 
         // NOTE: Clients may still possess the old single sign on cookie,
