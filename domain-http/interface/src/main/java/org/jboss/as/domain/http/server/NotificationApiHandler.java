3b40e719c0913109614e10be30ca7dece84f99f0
Jeff Mesnil <jmesnil@gmail.com>
2013-03-20 17:21:30 +0100

    [WFLY-266] Notification Support
    * add tests for the Notification HTTP API
    * emit a notification when a logging resource is added

--- domain-http/interface/src/main/java/org/jboss/as/domain/http/server/NotificationApiHandler.java
+++ domain-http/interface/src/main/java/org/jboss/as/domain/http/server/NotificationApiHandler.java
@@ -22,10 +22,6 @@
 
 package org.jboss.as.domain.http.server;
 
-import static io.undertow.util.Headers.ACCEPT;
-import static io.undertow.util.Headers.CACHE_CONTROL;
-import static io.undertow.util.Headers.CONNECTION;
-import static io.undertow.util.Headers.CONTENT_LENGTH;
 import static io.undertow.util.Headers.CONTENT_TYPE;
 import static io.undertow.util.Headers.LOCATION;
 import static io.undertow.util.HttpString.tryFromString;
@@ -35,7 +31,6 @@ import static io.undertow.util.Methods.POST;
 import static io.undertow.util.StatusCodes.CREATED;
 import static io.undertow.util.StatusCodes.INTERNAL_SERVER_ERROR;
 import static io.undertow.util.StatusCodes.METHOD_NOT_ALLOWED;
-import static io.undertow.util.StatusCodes.NOT_ACCEPTABLE;
 import static io.undertow.util.StatusCodes.NOT_FOUND;
 import static io.undertow.util.StatusCodes.NO_CONTENT;
 import static io.undertow.util.StatusCodes.OK;
@@ -43,14 +38,12 @@ import static java.lang.String.format;
 import static org.jboss.as.controller.notification.NotificationFilter.ALL;
 import static org.jboss.as.domain.http.server.Common.APPLICATION_JSON;
 import static org.jboss.as.domain.http.server.Common.LINK;
-import static org.jboss.as.domain.http.server.Common.TEXT_HTML;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -60,17 +53,14 @@ import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
-import io.undertow.server.ExchangeCompletionListener;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.notification.Notification;
 import org.jboss.as.controller.notification.NotificationHandler;
 import org.jboss.as.controller.notification.NotificationSupport;
 import org.jboss.dmr.ModelNode;
-import org.xnio.IoUtils;
 import org.xnio.streams.ChannelInputStream;
 import org.xnio.streams.ChannelOutputStream;
 
@@ -84,19 +74,23 @@ public class NotificationApiHandler implements HttpHandler {
 
     /**
      * Maximum number of notifications held *per handler*
+     * TODO make it configurable.
      */
     public static final int MAX_NOTIFICATIONS = 1024;
 
     static final String PATH = "/notification";
-    private static final String NOTIFICATION_SSE_ENDPOINT = PATH + "/sse";
     private static final String HANDLER_PREFIX = "handler";
     private static final String NOTIFICATIONS = "notifications";
+    public static final String RESOURCES = "resources";
 
     private final NotificationSupport notificationSupport;
     /**
      * Map of HttpNotificationHandler holding the notifications for a given handlerID
      */
-    private final Map<String, HttpNotificationHandler> handlers = new HashMap<String, HttpNotificationHandler>();
+    private final Map<String, HttpNotificationHandler> handlers = new HashMap<>();
+    /**
+     * counter to generate unique ID for the registered handlers
+     */
     private final AtomicLong handlerCounter = new AtomicLong();
 
     public NotificationApiHandler(NotificationSupport notificationSupport) {
@@ -115,16 +109,14 @@ public class NotificationApiHandler implements HttpHandler {
         HttpString method = http.getRequestMethod();
 
         final String path = http.getRequestPath();
+        // FIXME check for trailing /
         if (path.equals(PATH)) {
             if (POST.equals(method)) {
                 // POST /notification => create a handler resource that listens to a list of resources
                 String handlerID = generateHandlerID();
-                InputStream in = new ChannelInputStream(http.getRequestChannel());
                 ModelNode operation;
-                try {
+                try (InputStream in = new ChannelInputStream(http.getRequestChannel())) {
                     operation = ModelNode.fromJSONStream(in);
-                } finally {
-                    IoUtils.safeClose(in);
                 }
                 registerNotificationHandler(handlerID, operation);
                 http.getResponseHeaders().add(LOCATION, getHandlerURL(handlerID));
@@ -135,16 +127,6 @@ public class NotificationApiHandler implements HttpHandler {
                 sendResponse(http, METHOD_NOT_ALLOWED);
                 return;
             }
-        } else if (path.equals(PATH + "/test")) {
-            displayTestPageForNotificationSSE(http);
-            return;
-        } else if (path.equals(NOTIFICATION_SSE_ENDPOINT)) {
-            if (Common.TEXT_EVENT_STREAM.equals(http.getRequestHeaders().getFirst(ACCEPT))) {
-                handleServerSentEvent(http);
-            } else {
-                sendResponse(http, NOT_ACCEPTABLE);
-            }
-            return;
         } else {
             String[] splits = path.split("/");
             final String handlerID = splits[2];
@@ -158,7 +140,7 @@ public class NotificationApiHandler implements HttpHandler {
                         return;
                     } else {
                         http.getResponseHeaders().add(tryFromString(LINK), getHandlerNotificationsURL(handlerID));
-                        sendResponse(http, OK);
+                        sendResponse(http, OK, addresses.toJSONString(true), APPLICATION_JSON);
                         return;
                     }
                 } else if (POST.equals(method)) {
@@ -203,79 +185,11 @@ public class NotificationApiHandler implements HttpHandler {
         sendResponse(http, NOT_FOUND);
     }
 
-    private void handleServerSentEvent(HttpServerExchange http) throws IOException {
-        final Set<PathAddress> addresses = parseAddresses(http.getQueryParameters());
-        System.out.println("registring for addresses: " + addresses);
-        // send the header for the server-sent events
-        http.getResponseHeaders().add(CONTENT_TYPE, Common.TEXT_EVENT_STREAM);
-        http.getResponseHeaders().add(CACHE_CONTROL, Common.NO_CACHE);
-        http.getResponseHeaders().add(CONNECTION, Headers.KEEP_ALIVE.toString());
-        http.getResponseHeaders().put(CONTENT_LENGTH, 0);
-        http.setResponseCode(OK);
-        http.getResponseChannel().flush();
-
-        final OutputStream out = new ChannelOutputStream(http.getResponseChannel());
-        final PrintWriter print = new PrintWriter(out, true);
-        ModelNode dummy = new ModelNode();
-        dummy.get("hello").set("world");
-        print.write(formatData(dummy));
-        final NotificationHandler handler = new NotificationHandler() {
-            @Override
-            public void handleNotification(Notification notification) {
-                print.write(formatData(notification.toModelNode()));
-                print.flush();
-                try {
-                    out.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        http.addExchangeCompleteListener(new ExchangeCompletionListener() {
-            @Override
-            public void exchangeEvent(HttpServerExchange exchange, NextListener nextListener) {
-                if (exchange.isComplete()) {
-                    System.err.println("unregister from " + addresses);
-                    // unregister from all sources
-                    for (PathAddress address : addresses) {
-                        notificationSupport.unregisterNotificationHandler(PathAddress.pathAddress(address), handler, ALL);
-                    }
-                }
-            }
-        });
-
-        for (PathAddress address : addresses) {
-            notificationSupport.registerNotificationHandler(PathAddress.pathAddress(address), handler, ALL);
-        }
-    }
-
-    private void displayTestPageForNotificationSSE(HttpServerExchange http) throws IOException {
-        String body = "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<body onload =\"registerSSE()\" >\n" +
-                "    <script>\n" +
-                "        function registerSSE()\n" +
-                "        {\n" +
-                "            var source = new EventSource('/notification/sse?address=/subsystem%3Dmessaging/hornetq-server%3D*&address=/subsystem%3Dmessaging/hornetq-server%3D*/jms-queue%3D*');  \n" +
-                "            source.onmessage=function(event)\n" +
-                "            {\n" +
-                "                var notification = JSON.parse(event.data);\n" +
-                "                document.getElementById(\"result\").innerHTML += JSON.stringify(notification, null, '  ') + '\\n';\n" +
-                "            };\n" +
-                "        }\n" +
-                "    </script>\n" +
-                "    <pre id =\"result\"></pre>\n" +
-                "</body>\n" +
-                "</html>\n";
-        sendResponse(http, OK, body, TEXT_HTML);
-    }
-
     private String generateHandlerID() {
         return HANDLER_PREFIX + handlerCounter.incrementAndGet();
     }
 
     private ModelNode getAddressesListeningTo(String handlerID) {
-        System.out.println(">>> NotificationApiHandler.getAddressesListeningTo handlerID = [" + handlerID + "]");
         if (!handlers.containsKey(handlerID)) {
             return null;
         }
@@ -288,7 +202,6 @@ public class NotificationApiHandler implements HttpHandler {
     }
 
     private ModelNode fetchNotifications(String handlerID) {
-        System.out.println(">>> NotificationApiHandler.fetchNotifications handlerID = [" + handlerID + "]");
         if (!handlers.containsKey(handlerID)) {
             return null;
         }
@@ -302,16 +215,9 @@ public class NotificationApiHandler implements HttpHandler {
     }
 
     private void registerNotificationHandler(final String handlerID, final ModelNode operation) {
-        System.out.println("NotificationApiHandler.registerNotificationHandler handlerID = [" + handlerID + "], operation = [" + operation + "]");
-        final Set<PathAddress> addresses = new HashSet<PathAddress>();
-        for (ModelNode resource : operation.get("resources").asList()) {
+        final Set<PathAddress> addresses = new HashSet<>();
+        for (ModelNode resource : operation.get(RESOURCES).asList()) {
             addresses.add(PathAddress.pathAddress(resource));
-        }
-        System.out.println("addresses = " + addresses);
-        for (PathAddress address : addresses) {
-            if (address.isMultiTarget()) {
-                System.out.println("got pattern:" + address);
-            }
         }
         final HttpNotificationHandler handler = new HttpNotificationHandler(handlerID, addresses);
         for (PathAddress address : addresses) {
@@ -321,7 +227,6 @@ public class NotificationApiHandler implements HttpHandler {
     }
 
     private boolean unregisterNotificationHandler(String handlerID) {
-        System.out.println(">>> NotificationApiHandler.unregisterNotificationHandler handlerID = [" + handlerID + "]");
         HttpNotificationHandler handler = handlers.remove(handlerID);
         if (handler != null) {
             for (PathAddress address : handler.getListeningAddresses()) {
@@ -343,22 +248,11 @@ public class NotificationApiHandler implements HttpHandler {
         }
         http.getResponseHeaders().add(CONTENT_TYPE, contentType);
 
-        OutputStream out = new ChannelOutputStream(http.getResponseChannel());
-        PrintWriter print = new PrintWriter(out);
-        try {
-            try {
-                print.write(body);
-            } finally {
-                print.flush();
-                try {
-                    out.flush();
-                } finally {
-                    IoUtils.safeClose(print);
-                    IoUtils.safeClose(out);
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try (OutputStream out = new ChannelOutputStream(http.getResponseChannel());
+             PrintWriter print = new PrintWriter(out)) {
+            print.write(body);
+            print.flush();
+            out.flush();
         }
         http.endExchange();
     }
@@ -367,7 +261,7 @@ public class NotificationApiHandler implements HttpHandler {
 
         private final String handlerID;
         private final Set<PathAddress> addresses;
-        private final Queue<ModelNode> notifications = new ArrayBlockingQueue<ModelNode>(MAX_NOTIFICATIONS);
+        private final Queue<ModelNode> notifications = new ArrayBlockingQueue<>(MAX_NOTIFICATIONS);
 
         public HttpNotificationHandler(String handlerID, Set<PathAddress> addresses) {
             this.handlerID = handlerID;
@@ -379,7 +273,7 @@ public class NotificationApiHandler implements HttpHandler {
         }
 
         public List<ModelNode> getNotifications() {
-            return new ArrayList<ModelNode>(notifications);
+            return new ArrayList<>(notifications);
         }
 
         public void clear() {
@@ -414,26 +308,4 @@ public class NotificationApiHandler implements HttpHandler {
                 NOTIFICATIONS,
                 NOTIFICATIONS);
     }
-
-
-
-    private static String formatData(final ModelNode node) {
-        return format("data: %s\n\n", node.toJSONString(true));
-    }
-
-    public static Set<PathAddress> parseAddresses(Map<String,Deque<String>> query) {
-        Set<PathAddress> addresses = new HashSet<PathAddress>();
-        for (String address : query.get("address")) {
-            ModelNode node = new ModelNode();
-            for(String element : address.split("/")) {
-                if(element.isEmpty()) {
-                    continue;
-                }
-                String[] split1 = element.split("=");
-                node.add(split1[0], split1[1]);
-            }
-            addresses.add(PathAddress.pathAddress(node));
-        }
-        return addresses;
-   }
 }
