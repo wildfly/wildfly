99343c723df171294e8ce243946e3663378812da
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-03-25 08:41:04 +1100

    927021 JAX-RS does not work with EJB's with a non-default name

--- jaxrs/src/main/java/org/jboss/as/jaxrs/deployment/JaxrsComponentDeployer.java
+++ jaxrs/src/main/java/org/jboss/as/jaxrs/deployment/JaxrsComponentDeployer.java
@@ -85,7 +85,7 @@ public class JaxrsComponentDeployer implements DeploymentUnitProcessor {
                 final String jndiName;
                 if (component.getViews().size() == 1) {
                     //only 1 view, just use the simple JNDI name
-                    jndiName = "java:app/" + moduleDescription.getModuleName() + "/" + componentClass.getSimpleName();
+                    jndiName = "java:app/" + moduleDescription.getModuleName() + "/" + component.getComponentName();
                 } else {
                     boolean found = false;
                     String foundType = null;
@@ -104,7 +104,7 @@ public class JaxrsComponentDeployer implements DeploymentUnitProcessor {
                     if (!found) {
                         throw JaxrsMessages.MESSAGES.typeNameNotAnEjbView(Arrays.asList(jaxrsType), component.getComponentName());
                     }
-                    jndiName = "java:app/" + moduleDescription.getModuleName() + "/" + componentClass.getSimpleName() + "!" + foundType;
+                    jndiName = "java:app/" + moduleDescription.getModuleName() + "/" + component.getComponentName() + "!" + foundType;
                 }
 
                 JAXRS_LOGGER.debugf("Found JAX-RS Managed Bean: %s local jndi jaxRsTypeName: %s", component.getComponentClassName(), jndiName);
