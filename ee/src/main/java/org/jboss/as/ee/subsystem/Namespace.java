53a3b6424e80024f7b134b5b751c4e69ab01309b
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-04-01 15:18:56 +1100

    AS7-5651 Add annotation support to global-modules

--- ee/src/main/java/org/jboss/as/ee/subsystem/Namespace.java
+++ ee/src/main/java/org/jboss/as/ee/subsystem/Namespace.java
@@ -32,13 +32,13 @@ enum Namespace {
     UNKNOWN(null),
 
     EE_1_0("urn:jboss:domain:ee:1.0"),
-    EE_1_1("urn:jboss:domain:ee:1.1")
+    EE_1_1("urn:jboss:domain:ee:1.1"),
+    EE_2_0("urn:jboss:domain:ee:2.0"),
     ;
-
     /**
      * The current namespace version.
      */
-    public static final Namespace CURRENT = EE_1_1;
+    public static final Namespace CURRENT = EE_2_0;
 
     private final String name;
 
