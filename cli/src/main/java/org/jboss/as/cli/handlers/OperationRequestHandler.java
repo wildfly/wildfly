facd8dc5ca08d45214b47e394b78e59f71d588f5
Alexey Loubyansky <loubyansky@gmail.com>
2013-04-03 20:01:43 +0200

    CLI: throw CommandLineException's instead of CommandFormatException's in case of operation processing errors

--- cli/src/main/java/org/jboss/as/cli/handlers/OperationRequestHandler.java
+++ cli/src/main/java/org/jboss/as/cli/handlers/OperationRequestHandler.java
@@ -66,7 +66,7 @@ public class OperationRequestHandler implements CommandHandler, OperationCommand
 
         ModelNode request = (ModelNode) ctx.get("OP_REQ");
         if(request == null) {
-            throw new CommandFormatException("Parsed request isn't available.");
+            throw new CommandLineException("Parsed request isn't available.");
         }
 
         if(ctx.getConfig().isValidateOperationRequests()) {
@@ -78,17 +78,17 @@ public class OperationRequestHandler implements CommandHandler, OperationCommand
             if(Util.isSuccess(result)) {
                 ctx.printLine(result.toString());
             } else {
-                throw new CommandFormatException(result.toString());
+                throw new CommandLineException(result.toString());
             }
         } catch(NoSuchElementException e) {
-            throw new CommandFormatException("ModelNode request is incomplete", e);
+            throw new CommandLineException("ModelNode request is incomplete", e);
         } catch (CancellationException e) {
-            throw new CommandFormatException("The result couldn't be retrieved (perhaps the task was cancelled", e);
+            throw new CommandLineException("The result couldn't be retrieved (perhaps the task was cancelled", e);
         } catch (IOException e) {
             ctx.disconnectController();
-            throw new CommandFormatException("Communication error", e);
+            throw new CommandLineException("Communication error", e);
         } catch (RuntimeException e) {
-            throw new CommandFormatException("Failed to execute operation.", e);
+            throw new CommandLineException("Failed to execute operation.", e);
         }
     }
 
