b99b63c3bda4907977d7d635f50b67cc066e1502
Tomaz Cerar <tomaz.cerar@gmail.com>
2013-04-06 00:00:02 +0200

    Fix filter adding

--- osgi/integration/web/src/main/java/org/jboss/as/osgi/web/WebBundleContextProcessor.java
+++ osgi/integration/web/src/main/java/org/jboss/as/osgi/web/WebBundleContextProcessor.java
@@ -1,32 +1,32 @@
 /*
- * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat, Inc., and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ *  JBoss, Home of Professional Open Source.
+ *  Copyright 2013, Red Hat, Inc., and individual contributors
+ *  as indicated by the @author tags. See the copyright.txt file in the
+ *  distribution for a full listing of individual contributors.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *  This is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this software; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * /
  */
 package org.jboss.as.osgi.web;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -42,6 +42,8 @@ import org.jboss.as.server.deployment.DeploymentUnitProcessor;
 import org.jboss.as.web.common.WarMetaData;
 import org.jboss.metadata.web.jboss.JBossWebMetaData;
 import org.jboss.metadata.web.spec.FilterMappingMetaData;
+import org.jboss.metadata.web.spec.FilterMetaData;
+import org.jboss.metadata.web.spec.FiltersMetaData;
 import org.jboss.osgi.metadata.OSGiMetaData;
 
 /**
@@ -58,24 +60,35 @@ public class WebBundleContextProcessor implements DeploymentUnitProcessor {
         DeploymentUnit depUnit = phaseContext.getDeploymentUnit();
         WarMetaData warMetaData = depUnit.getAttachment(WarMetaData.ATTACHMENT_KEY);
         OSGiMetaData metadata = depUnit.getAttachment(OSGiConstants.OSGI_METADATA_KEY);
-        if (warMetaData == null || metadata == null)
-            return;
+
+        if (warMetaData == null || metadata == null) {return;}
 
         // [TODO] [AS7-5200]
         // For confidentiality reasons, a Web Runtime must not return any static content for paths that start with
         // WEB-INF, OSGI-INF, META-INF, OSGI-OPT
-        FilterMappingMetaData filterMetaData = new FilterMappingMetaData();
-        filterMetaData.setFilterName(WebBundleContextFilter.class.getName());
-        filterMetaData.setServletNames(Collections.singletonList("io.undertow.DefaultServlet"));
-        filterMetaData.setUrlPatterns(Arrays.asList("/OSGI-INF/*", "/OSGI-OPT/*"));
+        FilterMetaData filterMetaData = new FilterMetaData();
+        filterMetaData.setFilterClass(WebBundleContextFilter.class.getName());
+        filterMetaData.setFilterName("Filter forbidden resources");
+
+        FilterMappingMetaData filterMappingMetaData = new FilterMappingMetaData();
+        filterMappingMetaData.setFilterName(filterMetaData.getName());
+        //filterMappingMetaData.setServletNames(Collections.singletonList("io.undertow.DefaultServlet"));
+        filterMappingMetaData.setUrlPatterns(Arrays.asList("/OSGI-INF/*", "/OSGI-OPT/*"));
 
         JBossWebMetaData jbossWebMetaData = warMetaData.getMergedJBossWebMetaData();
+        FiltersMetaData filters = jbossWebMetaData.getFilters();
+        if (filters == null) {
+            filters = new FiltersMetaData();
+            jbossWebMetaData.setFilters(filters);
+        }
+        filters.add(filterMetaData);
+
         List<FilterMappingMetaData> filterMappings = jbossWebMetaData.getFilterMappings();
         if (filterMappings == null) {
-            filterMappings = new ArrayList<FilterMappingMetaData>();
+            filterMappings = new ArrayList<>();
             jbossWebMetaData.setFilterMappings(filterMappings);
         }
-        filterMappings.add(filterMetaData);
+        filterMappings.add(filterMappingMetaData);
     }
 
     @Override
