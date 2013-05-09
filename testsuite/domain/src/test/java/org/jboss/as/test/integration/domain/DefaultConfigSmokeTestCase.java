5dc55691ce88b4a56b050ce4e52eb5e778c0b80e
Brian Stansberry <brian.stansberry@redhat.com>
2013-05-09 09:46:26 -0500

    WFLY-1324 BZ 961523 Add testing of slave Host Controller join to DefaultConfigSmokeTestCase

--- testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/DefaultConfigSmokeTestCase.java
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/DefaultConfigSmokeTestCase.java
@@ -22,19 +22,135 @@
 
 package org.jboss.as.test.integration.domain;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_RESOURCE_DESCRIPTION_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RECURSIVE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESULT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import junit.framework.Assert;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.registry.AttributeAccess;
+import org.jboss.as.test.integration.domain.management.util.DomainLifecycleUtil;
+import org.jboss.as.test.integration.domain.management.util.JBossAsManagedConfiguration;
+import org.jboss.as.test.shared.TestSuiteEnvironment;
+import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.Property;
+import org.junit.Test;
+
 /**
  * Ensures the default domain.xml and host.xml start.
  *
- * @author Brian Stansberry (c) 2011 Red Hat Inc.
+ * @author Brian Stansberry (c) 2013 Red Hat Inc.
  */
 public class DefaultConfigSmokeTestCase extends BuildConfigurationTestBase {
-    @Override
-    protected String getDomainConfigFile() {
-        return "domain.xml";
+
+
+    public static final String slaveAddress = System.getProperty("jboss.test.host.slave.address", "127.0.0.1");
+
+    @Test
+    public void testStandardHost() throws Exception {
+        final JBossAsManagedConfiguration config = createConfiguration("domain.xml", "host.xml", getClass().getSimpleName());
+        final DomainLifecycleUtil utils = new DomainLifecycleUtil(config);
+        try {
+            utils.start();
+            // Double-check server status by confirming server-one can accept a web request to the root
+            URLConnection connection = new URL("http://" + TestSuiteEnvironment.formatPossibleIpv6Address(masterAddress) + ":8080").openConnection();
+            connection.connect();
+
+            if (Boolean.getBoolean("expression.audit")) {
+                writeExpressionAudit(utils);
+            }
+        } finally {
+            utils.stop(); // Stop
+        }
     }
 
-    @Override
-    protected String getHostConfigFile() {
-        return "host.xml";
+    @Test
+    public void testMasterAndSlave() throws Exception {
+        final JBossAsManagedConfiguration masterConfig = createConfiguration("domain.xml", "host-master.xml", getClass().getSimpleName());
+        final DomainLifecycleUtil masterUtils = new DomainLifecycleUtil(masterConfig);
+        final JBossAsManagedConfiguration slaveConfig = createConfiguration("domain.xml", "host-slave.xml", getClass().getSimpleName(),
+                "slave", slaveAddress, 19999);
+        final DomainLifecycleUtil slaveUtils = new DomainLifecycleUtil(slaveConfig);
+        try {
+            masterUtils.start();
+            slaveUtils.start();
+            // Double-check server status by confirming server-one can accept a web request to the root
+            URLConnection connection = new URL("http://" + TestSuiteEnvironment.formatPossibleIpv6Address(slaveAddress) + ":8080").openConnection();
+            connection.connect();
+        } finally {
+            try {
+                slaveUtils.stop();
+            } finally {
+                masterUtils.stop();
+            }
+        }
+    }
+
+    private void writeExpressionAudit(final DomainLifecycleUtil utils) throws IOException {
+
+        final ModelNode operation = new ModelNode();
+        operation.get(OP).set(READ_RESOURCE_DESCRIPTION_OPERATION);
+        operation.get(OP_ADDR).setEmptyList();
+        operation.get(RECURSIVE).set(true);
+
+        final ModelNode result = utils.getDomainClient().execute(operation);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        Assert.assertTrue(result.hasDefined(RESULT));
+
+        PathAddress pa = PathAddress.EMPTY_ADDRESS;
+        writeExpressionAudit(pa, result.get(RESULT));
+    }
+
+    private static void writeExpressionAudit(PathAddress pa, ModelNode resourceDescription) {
+        String paString = getPaString(pa);
+        if (resourceDescription.hasDefined(ModelDescriptionConstants.ATTRIBUTES)) {
+            for (Property property : resourceDescription.get(ModelDescriptionConstants.ATTRIBUTES).asPropertyList()) {
+                ModelNode attrdesc = property.getValue();
+                if (!attrdesc.hasDefined(ModelDescriptionConstants.STORAGE) ||
+                        AttributeAccess.Storage.CONFIGURATION.name().toLowerCase().equals(attrdesc.get(ModelDescriptionConstants.STORAGE).asString().toLowerCase())) {
+                    StringBuilder sb = new StringBuilder(paString);
+                    sb.append(",").append(property.getName());
+                    sb.append(",").append(attrdesc.get(ModelDescriptionConstants.TYPE).asString());
+                    sb.append(",").append(attrdesc.get(ModelDescriptionConstants.EXPRESSIONS_ALLOWED).asBoolean(false));
+                    sb.append(",").append(attrdesc.get(ModelDescriptionConstants.DESCRIPTION).asString());
+                    System.out.println(sb.toString());
+                }
+            }
+        }
+
+        if (resourceDescription.hasDefined(ModelDescriptionConstants.CHILDREN)) {
+            for (Property childTypeProp : resourceDescription.get(ModelDescriptionConstants.CHILDREN).asPropertyList()) {
+                String childType = childTypeProp.getName();
+                ModelNode childTypeDesc = childTypeProp.getValue();
+                if (childTypeDesc.hasDefined(ModelDescriptionConstants.MODEL_DESCRIPTION)) {
+                    for (Property childInstanceProp : childTypeDesc.get(ModelDescriptionConstants.MODEL_DESCRIPTION).asPropertyList()) {
+                        PathAddress childAddress = pa.append(childType, childInstanceProp.getName());
+                        writeExpressionAudit(childAddress, childInstanceProp.getValue());
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static String getPaString(PathAddress pa) {
+        if (pa.size() == 0) {
+            return "/";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (PathElement pe : pa) {
+            sb.append("/").append(pe.getKey()).append("=").append(pe.getValue());
+        }
+        return sb.toString();
     }
 }
