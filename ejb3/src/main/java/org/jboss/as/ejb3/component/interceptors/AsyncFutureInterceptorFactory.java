7e8b3f6e978906f52b2a36322b3c46756ee298fd
jaikiran <jaikiran.pai@gmail.com>
2013-05-22 19:05:18 +0530

    Bugzilla#1005110 WFLY-1375 Fix potential classloader leak with EJB async invocations

--- ejb3/src/main/java/org/jboss/as/ejb3/component/interceptors/AsyncFutureInterceptorFactory.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/interceptors/AsyncFutureInterceptorFactory.java
@@ -36,6 +36,7 @@ import org.jboss.invocation.InterceptorFactoryContext;
 import org.jboss.security.SecurityContext;
 import org.jboss.security.SecurityContextAssociation;
 
+
 /**
  * An asynchronous execution interceptor for methods returning {@link Future}.  Because asynchronous invocations
  * necessarily run in a concurrent thread, any thread context setup interceptors should run <b>after</b> this
@@ -69,7 +70,7 @@ public final class AsyncFutureInterceptorFactory implements InterceptorFactory {
                 asyncInterceptorContext.putPrivateData(InvocationType.class, InvocationType.ASYNC);
                 final CancellationFlag flag = new CancellationFlag();
                 final SecurityContext securityContext = SecurityContextAssociation.getSecurityContext();
-                final AsyncInvocationTask task = new AsyncInvocationTask( flag) {
+                final AsyncInvocationTask task = new AsyncInvocationTask(flag) {
                     @Override
                     protected Object runInvocation() throws Exception {
                         setSecurityContextOnAssociation(securityContext);
@@ -81,7 +82,18 @@ public final class AsyncFutureInterceptorFactory implements InterceptorFactory {
                     }
                 };
                 asyncInterceptorContext.putPrivateData(CancellationFlag.class, flag);
-                component.getAsynchronousExecutor().execute(task);
+                // This interceptor runs in user application's context classloader. Triggering an execute via a executor service from here can potentially lead to
+                // new thread creation which will assign themselves the context classloader of the parent thread (i.e. this thread). This effectively can lead to
+                // deployment's classloader leak. See https://issues.jboss.org/browse/WFLY-1375
+                // To prevent this, we set the TCCL of this thread to null and then trigger the "execute" before "finally" setting the TCCL back to the original one.
+                final ClassLoader oldClassLoader = SecurityActions.getContextClassLoader();
+                try {
+                    SecurityActions.setContextClassLoader(null);
+                    component.getAsynchronousExecutor().execute(task);
+                } finally {
+                    // reset to the original TCCL
+                    SecurityActions.setContextClassLoader(oldClassLoader);
+                }
                 return task;
             }
         };
@@ -97,6 +109,7 @@ public final class AsyncFutureInterceptorFactory implements InterceptorFactory {
             }
         });
     }
+
     private static void clearSecurityContextOnAssociation() {
         AccessController.doPrivileged(new PrivilegedAction<Void>() {
 
