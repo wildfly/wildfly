25900a3cd1de12d09e53bc2d1a13b4f46bd81902
Ivo Studensky <istudens@redhat.com>
2013-05-31 14:21:05 +0200

    WFLY-512 BZ967794 backporting some fixes of test ordering issues on JDK7

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/interceptor/method/EjbMethodInterceptorTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/interceptor/method/EjbMethodInterceptorTestCase.java
@@ -26,6 +26,7 @@ import javax.naming.NamingException;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
