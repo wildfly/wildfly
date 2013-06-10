5da45f5e0d6c470072af3a3ca9eee785cd3cdccf
Martin Kouba <mkouba@redhat.com>
2013-06-10 09:10:56 +0200

    WeldJaxwsInjectionServices - add support for EE component resource setter injection - see also WELD-1364

--- weld/src/main/java/org/jboss/as/weld/services/bootstrap/WeldJaxwsInjectionServices.java
+++ weld/src/main/java/org/jboss/as/weld/services/bootstrap/WeldJaxwsInjectionServices.java
@@ -1,4 +1,27 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.as.weld.services.bootstrap;
+
+import static org.jboss.as.weld.util.ResourceInjectionUtilities.getResourceAnnotated;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.ParameterizedType;
@@ -29,7 +52,7 @@ public class WeldJaxwsInjectionServices implements JaxwsInjectionServices {
     @Override
     public <T> ResourceReferenceFactory<T> registerWebServiceRefInjectionPoint(final InjectionPoint injectionPoint) {
 
-        WebServiceRef annotation = injectionPoint.getAnnotated().getAnnotation(WebServiceRef.class);
+        WebServiceRef annotation = getResourceAnnotated(injectionPoint).getAnnotation(WebServiceRef.class);
         if(annotation == null) {
             return null;
         }
