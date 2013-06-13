229422004884a15a688fcca1c2d80d08b7d85e4a
Paul Ferraro <paul.ferraro@redhat.com>
2013-06-13 12:46:48 -0400

    Retry on tx prepare failures. Break out of retry loop if interrupted.

--- clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/invoker/RetryingCacheInvoker.java
+++ clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/invoker/RetryingCacheInvoker.java
@@ -28,7 +28,10 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.Set;
 
+import javax.transaction.xa.XAException;
+
 import org.infinispan.Cache;
+import org.infinispan.CacheException;
 import org.infinispan.context.Flag;
 import org.infinispan.remoting.transport.jgroups.SuspectException;
 import org.infinispan.util.concurrent.TimeoutException;
@@ -77,29 +80,50 @@ public class RetryingCacheInvoker implements CacheInvoker {
 
         for (int i = 0; i <= this.backOffIntervals.length; ++i) {
             // Make sure Flag.FAIL_SILENTLY, if specified, is applied to the last try only
+            boolean retry = (i < this.backOffIntervals.length);
             try {
-                return this.invoker.invoke(cache, operation, (i < this.backOffIntervals.length) ? attemptFlags : allFlags);
+                return this.invoker.invoke(cache, operation, retry ? attemptFlags : allFlags);
             } catch (TimeoutException e) {
                 exception = e;
             } catch (SuspectException e) {
                 exception = e;
+            } catch (CacheException e) {
+                // If the tx commit (e.g. Cache.endBatch(true)) was rolled back due to failure on prepare, then retry
+                Throwable cause = e.getCause();
+                if (cause instanceof XAException) {
+                    XAException xaCause = (XAException) cause;
+                    // TimeoutException/SuspectException on prepare throws XAException.XA_RBROLLBACK
+                    // See org.infinispan.transaction.TransactionCoordinator.prepare(...)
+                    if (xaCause.errorCode == XAException.XA_RBROLLBACK) {
+                        exception = e;
+                    } else {
+                        throw e;
+                    }
+                } else {
+                    throw e;
+                }
             }
 
-            if (i < this.backOffIntervals.length) {
+            if (retry) {
                 int delay = this.backOffIntervals[i];
 
-                try {
-                    if (ROOT_LOGGER.isTraceEnabled()) {
-                        ROOT_LOGGER.tracef(exception, "Cache operation failed.  Retrying in %d ms", Integer.valueOf(delay));
-                    }
-
-                    Thread.sleep(delay);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                if (ROOT_LOGGER.isTraceEnabled()) {
+                    ROOT_LOGGER.tracef(exception, "Cache operation failed.  Retrying in %d ms", delay);
                 }
+
+                if (delay > 0) {
+                    try {
+                        Thread.sleep(delay);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                } else {
+                    Thread.yield();
+                }
+                if (Thread.currentThread().isInterrupted()) break;
             }
         }
 
-        throw MESSAGES.abortingCacheOperation(exception, Integer.valueOf(this.backOffIntervals.length + 1));
+        throw MESSAGES.abortingCacheOperation(exception, this.backOffIntervals.length);
     }
 }
