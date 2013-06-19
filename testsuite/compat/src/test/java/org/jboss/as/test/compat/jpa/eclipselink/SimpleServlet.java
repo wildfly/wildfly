759dd2c36e26dbbe93e85de67548dd739164dbaa
Scott Marlow <smarlow@redhat.com>
2013-06-19 15:37:01 -0400

    enable EclipseLink test.  Add missing vfs dependency to EclipseLink module.xml

--- testsuite/compat/src/test/java/org/jboss/as/test/compat/jpa/eclipselink/SimpleServlet.java
+++ testsuite/compat/src/test/java/org/jboss/as/test/compat/jpa/eclipselink/SimpleServlet.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.compat.jpa.toplink;
+package org.jboss.as.test.compat.jpa.eclipselink;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
