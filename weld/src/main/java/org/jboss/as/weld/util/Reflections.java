89e75cb17931043cbac4c27e686a7f04fcce13e4
Jozef Hartinger <jharting@redhat.com>
2013-06-28 14:27:09 +0200

    WFLY-1573 Fix how @WithAnnotations candidates are discovered using Jandex indexes

--- weld/src/main/java/org/jboss/as/weld/util/Reflections.java
+++ weld/src/main/java/org/jboss/as/weld/util/Reflections.java
@@ -23,6 +23,12 @@ package org.jboss.as.weld.util;
 
 import static org.jboss.weld.util.reflection.Reflections.cast;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Inherited;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 
 /**
  *
@@ -54,6 +60,69 @@ public class Reflections {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    /**
+     * A simple implementation of the {@link org.jboss.weld.resources.spi.AnnotationDiscovery#containsAnnotation(Class, Class)} contract.
+     * This implementation uses reflection.
+     *
+     * @see org.jboss.as.weld.discovery.WeldAnnotationDiscovery
+     */
+    public static boolean containsAnnotation(Class<?> javaClass, Class<? extends Annotation> requiredAnnotation) {
+        for (Class<?> clazz = javaClass; clazz != null && clazz != Object.class; clazz = clazz.getSuperclass()) {
+            // class level annotations
+            if (clazz == javaClass || requiredAnnotation.isAnnotationPresent(Inherited.class)) {
+                if (containsAnnotations(clazz.getAnnotations(), requiredAnnotation)) {
+                    return true;
+                }
+            }
+            // fields
+            for (Field field : clazz.getDeclaredFields()) {
+                if (containsAnnotations(field.getAnnotations(), requiredAnnotation)) {
+                    return true;
+                }
+            }
+            // constructors
+            for (Constructor<?> constructor : clazz.getConstructors()) {
+                if (containsAnnotations(constructor.getAnnotations(), requiredAnnotation)) {
+                    return true;
+                }
+                for (Annotation[] parameterAnnotations : constructor.getParameterAnnotations()) {
+                    if (containsAnnotations(parameterAnnotations, requiredAnnotation)) {
+                        return true;
+                    }
+                }
+            }
+            // methods
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (containsAnnotations(method.getAnnotations(), requiredAnnotation)) {
+                    return true;
+                }
+                for (Annotation[] parameterAnnotations : method.getParameterAnnotations()) {
+                    if (containsAnnotations(parameterAnnotations, requiredAnnotation)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean containsAnnotations(Annotation[] annotations, Class<? extends Annotation> requiredAnnotation) {
+        return containsAnnotation(annotations, requiredAnnotation, true);
+    }
+
+    private static boolean containsAnnotation(Annotation[] annotations, Class<? extends Annotation> requiredAnnotation, boolean checkMetaAnnotations) {
+        for (Annotation annotation : annotations) {
+            Class<? extends Annotation> annotationType = annotation.annotationType();
+            if (requiredAnnotation.equals(annotationType)) {
+                return true;
+            }
+            if (checkMetaAnnotations && containsAnnotation(annotationType.getAnnotations(), requiredAnnotation, false)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
