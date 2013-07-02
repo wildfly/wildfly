e8da3749d3aada6346b179ec4366623b75557b35
Jesse Jaggars <jhjaggars@gmail.com>
2013-07-02 11:08:04 -0500

    fixing JDR message ids [WFLY-1626]

--- jdr/jboss-as-jdr/src/main/java/org/jboss/as/jdr/JdrLogger.java
+++ jdr/jboss-as-jdr/src/main/java/org/jboss/as/jdr/JdrLogger.java
@@ -93,20 +93,20 @@ public interface JdrLogger extends BasicLogger {
      * JDR could not create a zipfile to store the report.
      */
     @LogMessage(level = ERROR)
-    @Message(id=13306, value="Could not create zipfile.")
+    @Message(id=13106, value="Could not create zipfile.")
     void couldNotCreateZipfile(@Cause Throwable cause);
 
     /**
      * One of the configuration steps in JDR threw an exception.
      */
     @LogMessage(level = ERROR)
-    @Message(id=13307, value="Could not configure JDR.")
+    @Message(id=13107, value="Could not configure JDR.")
     void couldNotConfigureJDR(@Cause Throwable cause);
 
     /**
      * No Commands to run, probably no valid plugin loaded
      */
     @LogMessage(level = ERROR)
-    @Message(id = 13308, value = "No JDR commands were loaded. Be sure that a valid Plugin class is specified in plugins.properties.")
+    @Message(id = 13108, value = "No JDR commands were loaded. Be sure that a valid Plugin class is specified in plugins.properties.")
     void noCommandsToRun();
 }
