c94b8278eb3334eb771ef4585abc7205d24b11ea
Ladislav Thon <lthon@redhat.com>
2013-07-09 14:40:10 +0200

    [WFLY-490] [WFLY-1789] added one TODO to OperationContextImpl (missing 'authorize' call IMHO)

--- controller/src/main/java/org/jboss/as/controller/OperationContextImpl.java
+++ controller/src/main/java/org/jboss/as/controller/OperationContextImpl.java
@@ -567,6 +567,7 @@ final class OperationContextImpl extends AbstractOperationContext {
             throw MESSAGES.stageAlreadyComplete(Stage.MODEL);
         }
         rejectUserDomainServerUpdates();
+        // TODO I believe that >>   authorize(false, READ_WRITE_CONFIG);   << belongs here; see readModelForUpdate
         if (!isModelAffected()) {
             takeWriteLock();
             model = model.clone();
