20fee941fbf4af70dd6b05f8be0b002cc0cb73d4
James Perkins <jperkins@redhat.com>
2013-07-10 10:09:59 -0700

    [WFLY-1059] Add color-map attribute to allow override of output color. Added a named-formatter option and a pattern-formatter to allow for shared formatters. Changed defaults to use the named formatters.

--- subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
+++ subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
@@ -68,6 +68,7 @@ class KnownVersions {
         addSubsystemVersion(map, "jgroups", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "jpa", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "logging", "1.1.0", CORE_MODEL_7_1_3);
+        addSubsystemVersion(map, "logging", "1.2.0", CORE_MODEL_7_2_0);
         addSubsystemVersion(map, "mail", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "messaging", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "messaging", "1.2.0", CORE_MODEL_7_2_0);
