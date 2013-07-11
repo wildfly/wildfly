f022f2ed503e98da179720e29d8e40b3b6dd50df
jimma <ema@apache.org>
2013-07-11 17:44:20 +0800

    [JBWS-3660]:Fix can not modify endpoint failure in ResourceResolverDeploymentAspect

--- webservices/server-integration/src/main/java/org/jboss/as/webservices/deployers/EndpointServiceDeploymentAspect.java
+++ webservices/server-integration/src/main/java/org/jboss/as/webservices/deployers/EndpointServiceDeploymentAspect.java
@@ -53,13 +53,14 @@ public final class EndpointServiceDeploymentAspect extends AbstractDeploymentAsp
 
     @Override
     public void stop(Deployment dep) {
-        if (stopServices) {
-            final DeploymentUnit unit = getRequiredAttachment(dep, DeploymentUnit.class);
-            for (final Endpoint ep : dep.getService().getEndpoints()) {
+        for (final Endpoint ep : dep.getService().getEndpoints()) {
+            if (ep.getLifecycleHandler() != null) {
+                ep.getLifecycleHandler().stop(ep);
+            }
+            if (stopServices) {
+                final DeploymentUnit unit = getRequiredAttachment(dep, DeploymentUnit.class);
                 EndpointService.uninstall(ep, unit);
             }
-        } else {
-            super.stop(dep);
         }
     }
 
