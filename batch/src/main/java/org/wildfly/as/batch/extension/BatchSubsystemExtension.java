26c43570f2bdefe0befb47ab166ffd073b0fdc71
Cheng Fang <cfang@redhat.com>
2013-07-18 16:05:21 -0400

    WFLY-508 more integration work (improve batch subsystem xml and parsing code); WFLY-1680 upgrade to jboss-batch-api_1.0_spec 1.0.0.Beta1 and jberet-core 1.0.0.Alpha2.

--- batch/src/main/java/org/wildfly/as/batch/extension/BatchSubsystemExtension.java
+++ batch/src/main/java/org/wildfly/as/batch/extension/BatchSubsystemExtension.java
@@ -28,10 +28,12 @@ import javax.xml.stream.XMLStreamException;
 
 import org.jboss.as.controller.Extension;
 import org.jboss.as.controller.ExtensionContext;
+import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.SubsystemRegistration;
 import org.jboss.as.controller.descriptions.StandardResourceDescriptionResolver;
 import org.jboss.as.controller.operations.common.GenericSubsystemDescribeHandler;
+import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.controller.parsing.ExtensionParsingContext;
 import org.jboss.as.controller.parsing.ParseUtils;
 import org.jboss.as.controller.persistence.SubsystemMarshallingContext;
@@ -42,14 +44,12 @@ import org.jboss.staxmapper.XMLElementReader;
 import org.jboss.staxmapper.XMLElementWriter;
 import org.jboss.staxmapper.XMLExtendedStreamReader;
 import org.jboss.staxmapper.XMLExtendedStreamWriter;
+import org.wildfly.as.batch.messages.BatchLogger;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DESCRIBE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
 
-public class SubsystemExtension implements Extension {
+public class BatchSubsystemExtension implements Extension {
 
     /**
      * The name space used for the {@code substystem} element
@@ -62,16 +62,23 @@ public class SubsystemExtension implements Extension {
     public static final String SUBSYSTEM_NAME = "batch";
 
     /**
+     * Version numbers for batch subsystem management interface.
+     */
+    private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
+    private static final int MANAGEMENT_API_MINOR_VERSION = 0;
+    private static final int MANAGEMENT_API_MICRO_VERSION = 0;
+
+    /**
      * The parser used for parsing our subsystem
      */
-    private final SubsystemParser parser = new SubsystemParser();
+    private final BatchSubsystemParser parser = new BatchSubsystemParser();
 
     protected static final PathElement SUBSYSTEM_PATH = PathElement.pathElement(SUBSYSTEM, SUBSYSTEM_NAME);
-    private static final String RESOURCE_NAME = SubsystemExtension.class.getPackage().getName() + ".LocalDescriptions";
+    private static final String RESOURCE_NAME = BatchSubsystemExtension.class.getPackage().getName() + ".LocalDescriptions";
 
     static StandardResourceDescriptionResolver getResourceDescriptionResolver(final String keyPrefix) {
         String prefix = SUBSYSTEM_NAME + (keyPrefix == null ? "" : "." + keyPrefix);
-        return new StandardResourceDescriptionResolver(prefix, RESOURCE_NAME, SubsystemExtension.class.getClassLoader(), true, false);
+        return new StandardResourceDescriptionResolver(prefix, RESOURCE_NAME, BatchSubsystemExtension.class.getClassLoader(), true, false);
     }
 
     @Override
@@ -79,46 +86,66 @@ public class SubsystemExtension implements Extension {
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, NAMESPACE, parser);
     }
 
-
     @Override
     public void initialize(ExtensionContext context) {
-        final SubsystemRegistration subsystem = context.registerSubsystem(SUBSYSTEM_NAME, 1, 0);
-        final ManagementResourceRegistration registration = subsystem.registerSubsystemModel(SubsystemDefinition.INSTANCE);
+        final SubsystemRegistration subsystem = context.registerSubsystem(SUBSYSTEM_NAME,
+                MANAGEMENT_API_MAJOR_VERSION, MANAGEMENT_API_MINOR_VERSION, MANAGEMENT_API_MICRO_VERSION);
+        final ManagementResourceRegistration registration = subsystem.registerSubsystemModel(BatchSubsystemDefinition.INSTANCE);
         registration.registerOperationHandler(DESCRIBE, GenericSubsystemDescribeHandler.INSTANCE, GenericSubsystemDescribeHandler.INSTANCE, false, OperationEntry.EntryType.PRIVATE);
 
         subsystem.registerXMLElementWriter(parser);
     }
 
-    private static ModelNode createAddSubsystemOperation() {
-        final ModelNode subsystem = new ModelNode();
-        subsystem.get(OP).set(ADD);
-        subsystem.get(OP_ADDR).add(SUBSYSTEM, SUBSYSTEM_NAME);
-        return subsystem;
-    }
-
     /**
      * The subsystem parser, which uses stax to read and write to and from xml
      */
-    private static class SubsystemParser implements XMLStreamConstants, XMLElementReader<List<ModelNode>>, XMLElementWriter<SubsystemMarshallingContext> {
+    private static class BatchSubsystemParser implements XMLStreamConstants, XMLElementReader<List<ModelNode>>, XMLElementWriter<SubsystemMarshallingContext> {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void readElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
+            String jobRepositoryTypeVal = null;
+            ModelNode subsystemAdd = Util.createAddOperation(PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, SUBSYSTEM_NAME)));
+            while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+                String localName = reader.getLocalName();
+                if (BatchSubsystemDefinition.JOB_REPOSITORY.equals(localName)) {
+                    for (int i = 0, count = reader.getAttributeCount(); i < count; i++) {
+                        String attrName = reader.getAttributeLocalName(i);
+                        if (BatchSubsystemDefinition.JOB_REPOSITORY_TYPE.equals(attrName)) {
+                            jobRepositoryTypeVal = reader.getAttributeValue(i);
+                        } else {
+                            throw ParseUtils.unexpectedAttribute(reader, i);
+                        }
+                    }
+                } else {
+                    throw ParseUtils.unexpectedElement(reader);
+                }
+            }
+            if (jobRepositoryTypeVal != null) {
+                BatchSubsystemDefinition.jobRepositoryTypeAttribute.parseAndSetParameter(jobRepositoryTypeVal, subsystemAdd, reader);
+            }
+            BatchLogger.BATCH_LOGGER.configuredJobRepository(jobRepositoryTypeVal);
+            ParseUtils.requireNoContent(reader);
+            list.add(subsystemAdd);
+        }
 
         /**
          * {@inheritDoc}
          */
         @Override
         public void writeContent(XMLExtendedStreamWriter writer, SubsystemMarshallingContext context) throws XMLStreamException {
-            context.startSubsystemElement(SubsystemExtension.NAMESPACE, false);
-            writer.writeEndElement();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void readElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
-            // Require no content
-            ParseUtils.requireNoContent(reader);
-            list.add(createAddSubsystemOperation());
+            ModelNode node = context.getModelNode();
+            if (node.hasDefined(BatchSubsystemDefinition.JOB_REPOSITORY_TYPE)) {
+                context.startSubsystemElement(BatchSubsystemExtension.NAMESPACE, false);
+                writer.writeStartElement(BatchSubsystemDefinition.JOB_REPOSITORY);
+                BatchSubsystemDefinition.jobRepositoryTypeAttribute.marshallAsAttribute(node, writer);
+                writer.writeEndElement();
+                writer.writeEndElement();
+            } else {
+                context.startSubsystemElement(BatchSubsystemExtension.NAMESPACE, false);
+                writer.writeEndElement();
+            }
         }
     }
-
 }
