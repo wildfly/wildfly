f3ed4be398d0dc2179024dddcf28a736a0f65c4d
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-07-30 10:01:14 +0200

    Refactor session cookie config not to rely on optional dependencies

--- undertow/src/main/java/org/wildfly/extension/undertow/SessionCookieDefinition.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/SessionCookieDefinition.java
@@ -22,28 +22,24 @@
 
 package org.wildfly.extension.undertow;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.jboss.as.controller.AbstractAddStepHandler;
 import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.PersistentResourceDefinition;
-import org.jboss.as.controller.ServiceRemoveStepHandler;
+import org.jboss.as.controller.RestartParentResourceAddHandler;
+import org.jboss.as.controller.RestartParentResourceRemoveHandler;
 import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.controller.SimpleAttributeDefinition;
 import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
 import org.jboss.as.controller.registry.AttributeAccess;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.ModelType;
-import org.jboss.msc.service.ServiceBuilder;
-import org.jboss.msc.service.ServiceController;
+import org.jboss.msc.service.ServiceName;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Global session cookie config
@@ -52,10 +48,7 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_
  */
 class SessionCookieDefinition extends PersistentResourceDefinition {
 
-    private static final SessionCookieAdd ADD_OPERATION = new SessionCookieAdd();
-
     static final SessionCookieDefinition INSTANCE = new SessionCookieDefinition();
-
 
     protected static final SimpleAttributeDefinition NAME =
             new SimpleAttributeDefinitionBuilder(Constants.NAME, ModelType.STRING, true)
@@ -110,8 +103,8 @@ class SessionCookieDefinition extends PersistentResourceDefinition {
     private SessionCookieDefinition() {
         super(UndertowExtension.PATH_SESSION_COOKIE,
                 UndertowExtension.getResolver(UndertowExtension.PATH_SESSION_COOKIE.getKeyValuePair()),
-                ADD_OPERATION,
-                new ServiceRemoveStepHandler(SessionCookieConfigService.SERVICE_NAME, ADD_OPERATION));
+                new SessionCookieAdd(),
+                new SessionCookieRemove());
     }
 
     @Override
@@ -119,7 +112,31 @@ class SessionCookieDefinition extends PersistentResourceDefinition {
         return ATTRIBUTES_MAP.values();
     }
 
-    private static class SessionCookieAdd extends AbstractAddStepHandler {
+    public SessionCookieConfig getConfig(final OperationContext context, final ModelNode model) throws OperationFailedException {
+        if(!model.isDefined()) {
+            return null;
+        }
+        ModelNode nameValue = NAME.resolveModelAttribute(context, model);
+        ModelNode domainValue = DOMAIN.resolveModelAttribute(context, model);
+        ModelNode commentValue = COMMENT.resolveModelAttribute(context, model);
+        ModelNode secureValue = SECURE.resolveModelAttribute(context, model);
+        ModelNode httpOnlyValue = HTTP_ONLY.resolveModelAttribute(context, model);
+        ModelNode maxAgeValue = MAX_AGE.resolveModelAttribute(context, model);
+        final String name = nameValue.isDefined() ? nameValue.asString() : null;
+        final String domain = domainValue.isDefined() ? domainValue.asString() : null;
+        final String comment = commentValue.isDefined() ? commentValue.asString() : null;
+        final Boolean secure = secureValue.isDefined() ? secureValue.asBoolean() : null;
+        final Boolean httpOnly = httpOnlyValue.isDefined() ? httpOnlyValue.asBoolean() : null;
+        final Integer maxAge = maxAgeValue.isDefined() ? maxAgeValue.asInt() : null;
+        return new SessionCookieConfig(name, domain, comment, httpOnly, secure, maxAge);
+    }
+
+
+    private static class SessionCookieAdd extends RestartParentResourceAddHandler {
+        protected SessionCookieAdd() {
+            super(ServletContainerDefinition.INSTANCE.getPathElement().getKey());
+        }
+
         @Override
         protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
             for (AttributeDefinition def : ATTRIBUTES) {
@@ -128,32 +145,30 @@ class SessionCookieDefinition extends PersistentResourceDefinition {
         }
 
         @Override
-        protected void performRuntime(final OperationContext context, final ModelNode operation, final ModelNode model, final ServiceVerificationHandler verificationHandler, final List<ServiceController<?>> newControllers) throws OperationFailedException {
-            final PathAddress address = PathAddress.pathAddress(operation.get(OP_ADDR));
-            final String servletContainerName = address.getElement(address.size() - 2).getValue();
+        protected void recreateParentService(OperationContext context, PathAddress parentAddress, ModelNode parentModel, ServiceVerificationHandler verificationHandler) throws OperationFailedException {
+            ServletContainerAdd.INSTANCE.installRuntimeServices(context, parentModel, null, parentAddress.getLastElement().getValue());
+        }
 
-            ModelNode nameValue = NAME.resolveModelAttribute(context, model);
-            ModelNode domainValue = DOMAIN.resolveModelAttribute(context, model);
-            ModelNode commentValue = COMMENT.resolveModelAttribute(context, model);
-            ModelNode secureValue = SECURE.resolveModelAttribute(context, model);
-            ModelNode httpOnlyValue = HTTP_ONLY.resolveModelAttribute(context, model);
-            ModelNode maxAgeValue = MAX_AGE.resolveModelAttribute(context, model);
-            final String name = nameValue.isDefined() ? nameValue.asString() : null;
-            final String domain = domainValue.isDefined() ? domainValue.asString() : null;
-            final String comment = commentValue.isDefined() ? commentValue.asString() : null;
-            final Boolean secure = secureValue.isDefined() ? secureValue.asBoolean() : null;
-            final Boolean httpOnly = httpOnlyValue.isDefined() ? httpOnlyValue.asBoolean() : null;
-            final Integer maxAge = maxAgeValue.isDefined() ? maxAgeValue.asInt() : null;
-            final SessionCookieConfigService service = new SessionCookieConfigService(name, domain, comment, httpOnly, secure, maxAge);
-            ServiceBuilder<SessionCookieConfigService> builder = context.getServiceTarget().addService(SessionCookieConfigService.SERVICE_NAME.append(servletContainerName), service);
-            if(verificationHandler != null) {
-                builder.addListener(verificationHandler);
-            }
-            ServiceController<SessionCookieConfigService> controller = builder.install();
-            if(newControllers != null) {
-                newControllers.add(controller);
-            }
+        @Override
+        protected ServiceName getParentServiceName(PathAddress parentAddress) {
+            return UndertowService.SERVLET_CONTAINER.append(parentAddress.getLastElement().getValue());
+        }
+    }
 
+    private static class SessionCookieRemove extends RestartParentResourceRemoveHandler {
+
+        protected SessionCookieRemove() {
+            super(ServletContainerDefinition.INSTANCE.getPathElement().getKey());
+        }
+
+        @Override
+        protected void recreateParentService(OperationContext context, PathAddress parentAddress, ModelNode parentModel, ServiceVerificationHandler verificationHandler) throws OperationFailedException {
+            ServletContainerAdd.INSTANCE.installRuntimeServices(context, parentModel, null, parentAddress.getLastElement().getValue());
+        }
+
+        @Override
+        protected ServiceName getParentServiceName(PathAddress parentAddress) {
+            return UndertowService.SERVLET_CONTAINER.append(parentAddress.getLastElement().getValue());
         }
     }
 }
