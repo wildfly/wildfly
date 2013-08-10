9dc8e818f89aedf7952c4a6a3825feb72dc1ae42
Brian Stansberry <brian.stansberry@redhat.com>
2013-08-09 21:21:38 -0500

    [WFLY-490] Set up infrastructure for domain RBAC tests; start on a basic test of the 'simple' provider using 'roles' headers
    Conflicts:
    	testsuite/integration/basic/pom.xml
    	testsuite/shared/src/main/java/org/jboss/as/test/integration/domain/management/util/DomainTestSupport.java

---
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/rbac/StandardRolesTestCase.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.domain.rbac;
+
+import org.jboss.as.test.integration.domain.management.util.DomainLifecycleUtil;
+import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
+import org.jboss.as.test.integration.domain.suites.DomainRbacTestSuite;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * TODO class javadoc.
+ *
+ * @author Brian Stansberry (c) 2013 Red Hat Inc.
+ */
+public class StandardRolesTestCase {
+
+    private static DomainTestSupport testSupport;
+    private static DomainLifecycleUtil domainMasterLifecycleUtil;
+    private static DomainLifecycleUtil domainSlaveLifecycleUtil;
+
+    @BeforeClass
+    public static void setupDomain() throws Exception {
+        testSupport = DomainRbacTestSuite.createSupport(StandardRolesTestCase.class.getSimpleName());
+
+        domainMasterLifecycleUtil = testSupport.getDomainMasterLifecycleUtil();
+        domainSlaveLifecycleUtil = testSupport.getDomainSlaveLifecycleUtil();
+    }
+
+    @AfterClass
+    public static void tearDownDomain() throws Exception {
+        DomainRbacTestSuite.stopSupport();
+        testSupport = null;
+        domainMasterLifecycleUtil = null;
+        domainSlaveLifecycleUtil = null;
+    }
+
+    @Test
+    public void test() throws Exception {
+        // TODO test something -- currently just a placeholder
+    }
+}
