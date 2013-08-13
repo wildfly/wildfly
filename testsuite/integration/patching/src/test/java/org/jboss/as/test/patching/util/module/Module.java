54b834f1a1b99de78a32d79583ac842002ed19a3
Jan Martiska <jmartisk@redhat.com>
2013-08-13 13:57:55 +0200

    [WFLY-405] little fixes; modules should always contain '.' as resource path

--- testsuite/integration/patching/src/test/java/org/jboss/as/test/patching/util/module/Module.java
+++ testsuite/integration/patching/src/test/java/org/jboss/as/test/patching/util/module/Module.java
@@ -1,15 +1,15 @@
 package org.jboss.as.test.patching.util.module;
 
-import org.jboss.as.patching.IoUtils;
-import org.jboss.as.patching.metadata.ModuleItem;
-import org.jboss.as.test.patching.PatchingTestUtil;
-import org.jboss.as.test.patching.ResourceItem;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+
+import org.jboss.as.patching.IoUtils;
+import org.jboss.as.patching.metadata.ModuleItem;
+import org.jboss.as.test.patching.PatchingTestUtil;
+import org.jboss.as.test.patching.ResourceItem;
 
 /**
  * @author Martin Simka
@@ -23,7 +23,7 @@ public class Module {
     private List<ResourceItem> resourceRoots;
     private List<ResourceItem> miscFiles;
 
-    private Module (Builder builder) {
+    private Module(Builder builder) {
         this.namespace = builder.namespace;
         this.name = builder.name;
         this.slot = builder.slot;
@@ -39,30 +39,29 @@ public class Module {
 
     public String generateXml() {
         StringBuilder stringBuilder = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-        String rootElementTemplate =  "<module xmlns=\"%s\" name=\"%s\" slot=\"%s\">\n";
+        String rootElementTemplate = "<module xmlns=\"%s\" name=\"%s\" slot=\"%s\">\n";
         stringBuilder.append(String.format(rootElementTemplate, namespace, name, slot));
-        if(!properties.isEmpty()) {
+        if (!properties.isEmpty()) {
             stringBuilder.append("     <properties>\n");
             String propertyTemplate = "          <property name=\"%s\" value=\"%s\"/>\n";
-            for(String key : properties.stringPropertyNames()) {
+            for (String key : properties.stringPropertyNames()) {
                 stringBuilder.append(String.format(propertyTemplate, key, properties.getProperty(key)));
             }
             stringBuilder.append("     </properties>\n");
         }
 
-        if(!resourceRoots.isEmpty()) {
-            stringBuilder.append("     <resources>\n");
-            String resourceRootTemplate = "          <resource-root path=\"%s\"/>\n";
-            for(ResourceItem resourceRoot : resourceRoots) {
-                stringBuilder.append(String.format(resourceRootTemplate, resourceRoot.getItemName()));
-            }
-            stringBuilder.append("     </resources>\n");
+        stringBuilder.append("     <resources>\n");
+        String resourceRootTemplate = "          <resource-root path=\"%s\"/>\n";
+        for (ResourceItem resourceRoot : resourceRoots) {
+            stringBuilder.append(String.format(resourceRootTemplate, resourceRoot.getItemName()));
         }
+        stringBuilder.append(String.format(resourceRootTemplate, "."));
+        stringBuilder.append("     </resources>\n");
 
-        if(!resourceRoots.isEmpty()) {
+        if (!resourceRoots.isEmpty()) {
             stringBuilder.append("     <dependencies>\n");
             String dependencyTemplate = "          <module name=\"%s\"/>\n";
-            for(String module : dependencies) {
+            for (String module : dependencies) {
                 stringBuilder.append(String.format(dependencyTemplate, module));
             }
             stringBuilder.append("     </dependencies>\n");
@@ -73,6 +72,7 @@ public class Module {
 
     /**
      * writes module to disk
+     *
      * @param baseDir usually modules dir, written path starts with first part of module name
      * @return main dir
      * @throws IOException
@@ -81,11 +81,11 @@ public class Module {
         File mainDir = IoUtils.mkdir(baseDir, (name + "." + slot).split("\\."));
         File moduleXml = PatchingTestUtil.touch(mainDir, "module.xml");
         PatchingTestUtil.dump(moduleXml, generateXml().getBytes());
-        for(ResourceItem resourceRoot : resourceRoots) {
+        for (ResourceItem resourceRoot : resourceRoots) {
             File f = PatchingTestUtil.touch(mainDir, resourceRoot.getItemName());
             PatchingTestUtil.dump(f, resourceRoot.getContent());
         }
-        for(ResourceItem miscFile : miscFiles) {
+        for (ResourceItem miscFile : miscFiles) {
             File f = PatchingTestUtil.touch(mainDir, miscFile.getItemName());
             PatchingTestUtil.dump(f, miscFile.getContent());
         }
@@ -102,11 +102,11 @@ public class Module {
         private List<ResourceItem> resourceRoots;
         private List<ResourceItem> miscFiles;
 
-        public  Builder(String name, String namespace) {
-            if(name == null) {
+        public Builder(String name, String namespace) {
+            if (name == null) {
                 throw new NullPointerException("name");
             }
-            if(namespace == null) {
+            if (namespace == null) {
                 throw new NullPointerException("namespace");
             }
             this.name = name;
@@ -117,8 +117,8 @@ public class Module {
             miscFiles = new ArrayList<ResourceItem>();
         }
 
-        public  Builder(String name) {
-            if(name == null) {
+        public Builder(String name) {
+            if (name == null) {
                 throw new NullPointerException("name");
             }
             this.name = name;
@@ -130,7 +130,7 @@ public class Module {
         }
 
         public Builder slot(String slot) {
-            if(slot == null) {
+            if (slot == null) {
                 throw new NullPointerException("slot");
             }
             this.slot = slot;
@@ -138,10 +138,10 @@ public class Module {
         }
 
         public Builder property(String name, String value) {
-            if(name == null) {
+            if (name == null) {
                 throw new NullPointerException("name");
             }
-            if(value == null) {
+            if (value == null) {
                 throw new NullPointerException("value");
             }
             properties.setProperty(name, value);
@@ -149,7 +149,7 @@ public class Module {
         }
 
         public Builder dependency(String moduleName) {
-            if(moduleName == null) {
+            if (moduleName == null) {
                 throw new NullPointerException("moduleName");
             }
             dependencies.add(moduleName);
@@ -157,7 +157,7 @@ public class Module {
         }
 
         public Builder resourceRoot(ResourceItem resourceRoot) {
-            if(resourceRoot == null) {
+            if (resourceRoot == null) {
                 throw new NullPointerException("resourceRoot");
             }
             resourceRoots.add(resourceRoot);
@@ -165,7 +165,7 @@ public class Module {
         }
 
         public Builder miscFile(ResourceItem miscFile) {
-            if(miscFile == null) {
+            if (miscFile == null) {
                 throw new NullPointerException("miscFile");
             }
             miscFiles.add(miscFile);
@@ -175,7 +175,7 @@ public class Module {
         public Module build() {
             assert notNull(name);
             assert notNull(namespace);
-            if(slot == null) {
+            if (slot == null) {
                 slot = ModuleItem.MAIN_SLOT;
             }
             return new Module(this);
