bc4a2695da08a3939382dedecbc4a0e3d69c2a47
Josef Cacek <josef.cacek@gmail.com>
2013-08-21 13:00:14 +0200

    WFLY-853 directory names with spaces and ampersands supported in vault.bat

--- build/src/main/resources/bin/vault.bat
+++ build/src/main/resources/bin/vault.bat
@@ -4,12 +4,10 @@ SETLOCAL
 rem Author: Gregory Charles, JBoss Community Member
 rem Date: January 24, 2012
 
-set DIRNAME="%~dp0"
-call :DeQuote DIRNAME
-set PROGNAME=%0
+set "DIRNAME=%~dp0"
 
 rem Setup JBOSS_HOME
-set JBOSS_HOME=%DIRNAME%\..
+set "JBOSS_HOME=%DIRNAME%.."
 
 rem Setup the JVM
 IF NOT DEFINED JAVA (
@@ -23,19 +21,16 @@ IF NOT DEFINED JAVA (
 
 IF NOT DEFINED MODULEPATH (
     set "MODULEPATH=%JBOSS_HOME%\modules"
-    call :DeQuote MODULEPATH
 )
-
-
 
 rem Display our environment
 echo =========================================================================
 echo.
 echo   JBoss Vault
 echo.
-echo   JBOSS_HOME: %JBOSS_HOME%
+echo   JBOSS_HOME: "%JBOSS_HOME%"
 echo.
-echo   JAVA: %JAVA%
+echo   JAVA: "%JAVA%"
 echo.
 echo =========================================================================
 echo.
@@ -43,12 +38,3 @@ echo.
 "%JAVA%" -jar "%JBOSS_HOME%\jboss-modules.jar" -mp "%MODULEPATH%" org.jboss.as.vault-tool %*
 
 ENDLOCAL
-
-:END
-goto :EOF
-
-:DeQuote
-for /f "delims=" %%A in ('echo %%%1%%') do set %1=%%~A
-goto :EOF
-
-:EOF
