wildfly/wildfly-core

[WFLY-456] Fixes from code review
Bump up jmx subsystem to 2.0
Better syncing of writing in FileAuditLogHandler
Various other small fixes

was: 973214111b880e55ef03af1cb8f7e3187a4e1159

jmx/src/main/java/org/jboss/as/jmx/JMXExtension.java
@@ -102,14 +102,14 @@ public class JMXExtension implements Extension {
             new SensitivityClassification(SUBSYSTEM_NAME, "jmx", false, false, true);
 
     static final SensitiveTargetAccessConstraintDefinition JMX_SENSITIVITY_DEF = new SensitiveTargetAccessConstraintDefinition(JMX_SENSITIVITY);
-
-    static final JMXSubsystemParser_1_2 parserCurrent = new JMXSubsystemParser_1_2();
+    static final JMXSubsystemParser_2_0 parserCurrent = new JMXSubsystemParser_2_0();
+    static final JMXSubsystemParser_1_2 parser12 = new JMXSubsystemParser_1_2();
     static final JMXSubsystemParser_1_1 parser11 = new JMXSubsystemParser_1_1();
     static final JMXSubsystemParser_1_0 parser10 = new JMXSubsystemParser_1_0();
     static final JMXSubsystemWriter writer = new JMXSubsystemWriter();
 
-    private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
-    private static final int MANAGEMENT_API_MINOR_VERSION = 1;
+    private static final int MANAGEMENT_API_MAJOR_VERSION = 2;
+    private static final int MANAGEMENT_API_MINOR_VERSION = 0;
     private static final int MANAGEMENT_API_MICRO_VERSION = 0;
 
     /**
@@ -139,7 +139,8 @@ public class JMXExtension implements Extension {
     public void initializeParsers(ExtensionParsingContext context) {
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_0.getUriString(), parser10);
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_1.getUriString(), parser11);
-        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_2.getUriString(), parserCurrent);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_2.getUriString(), parser12);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_2_0.getUriString(), parserCurrent);
     }
 
     private static ModelNode createAddOperation() {
@@ -505,13 +506,6 @@ public class JMXExtension implements Extension {
                         connectorAdd = true;
                         list.add(parseRemoteConnector(reader));
                         break;
-                    case AUDIT_LOG:
-                        if (auditLog) {
-                            throw ParseUtils.duplicateNamedElement(reader, Element.AUDIT_LOG.getLocalName());
-                        }
-                        auditLog = true;
-                        parseAuditLogElement(reader, list);
-                        break;
                     default: {
                         throw ParseUtils.unexpectedElement(reader);
                     }
@@ -519,7 +513,7 @@ public class JMXExtension implements Extension {
             }
         }
 
-        private ModelNode parseShowModelElement(XMLExtendedStreamReader reader, String showModelChild) throws XMLStreamException {
+        protected ModelNode parseShowModelElement(XMLExtendedStreamReader reader, String showModelChild) throws XMLStreamException {
 
             ModelNode op = createOperation(ADD, CommonAttributes.EXPOSE_MODEL, showModelChild);
 
@@ -550,7 +544,57 @@ public class JMXExtension implements Extension {
             }
             return op;
         }
+    }
 
+    private static class JMXSubsystemParser_2_0 extends JMXSubsystemParser_1_2 {
+        @Override
+        public void readElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
+            boolean showResolvedModel = false;
+            boolean showExpressionModel = false;
+            boolean connectorAdd = false;
+            boolean auditLog = false;
+
+            ParseUtils.requireNoAttributes(reader);
+
+            list.add(createAddOperation());
+
+            while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+                final Element element = Element.forName(reader.getLocalName());
+                switch (element) {
+                    case EXPOSE_RESOLVED_MODEL:
+                        if (showResolvedModel) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.EXPOSE_RESOLVED_MODEL.getLocalName());
+                        }
+                        showResolvedModel = true;
+                        list.add(parseShowModelElement(reader, CommonAttributes.RESOLVED));
+                        break;
+                    case EXPOSE_EXPRESSION_MODEL:
+                        if (showExpressionModel) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.EXPOSE_EXPRESSION_MODEL.getLocalName());
+                        }
+                        showExpressionModel = true;
+                        list.add(parseShowModelElement(reader, CommonAttributes.EXPRESSION));
+                        break;
+                    case REMOTING_CONNECTOR:
+                        if (connectorAdd) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.REMOTING_CONNECTOR.getLocalName());
+                        }
+                        connectorAdd = true;
+                        list.add(parseRemoteConnector(reader));
+                        break;
+                    case AUDIT_LOG:
+                        if (auditLog) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.AUDIT_LOG.getLocalName());
+                        }
+                        auditLog = true;
+                        parseAuditLogElement(reader, list);
+                        break;
+                    default: {
+                        throw ParseUtils.unexpectedElement(reader);
+                    }
+                }
+            }
+        }
 
         private void parseAuditLogElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
 
@@ -636,6 +680,7 @@ public class JMXExtension implements Extension {
             ParseUtils.requireNoContent(reader);
         }
     }
+
 
     private static class JMXSubsystemWriter implements XMLStreamConstants, XMLElementWriter<SubsystemMarshallingContext> {
         /**

jmx/src/test/java/org/jboss/as/jmx/JMXSubsystemTestCase.java
@@ -413,10 +413,70 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
         super.compare(modelA, modelB);
     }
 
+
     @Test
-    public void testParseAndMarshallModel1_2WithAuditLogButNoHandlerReferences() throws Exception {
+    public void testParseAndMarshalModel2_0WithShowModels() throws Exception {
+        //Parse the subsystem xml and install into the first controller
         String subsystemXml =
-                "<subsystem xmlns=\"" + Namespace.JMX_1_2.getUriString() + "\">" +
+                "<subsystem xmlns=\"" + Namespace.JMX_2_0.getUriString() + "\">" +
+                "   <expose-resolved-model domain-name=\"jboss.RESOLVED\"/>" +
+                "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
+                "</subsystem>";
+
+        AdditionalInitialization additionalInit = new BaseAdditionalInitalization();
+
+        KernelServices servicesA = createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemXml).build();
+        //Get the model and the persisted xml from the first controller
+        ModelNode modelA = servicesA.readWholeModel();
+        String marshalled = servicesA.getPersistedSubsystemXml();
+        servicesA.shutdown();
+
+        Assert.assertTrue(marshalled.contains(Namespace.CURRENT.getUriString()));
+        compareXml(null, subsystemXml, marshalled, true);
+
+        //Install the persisted xml from the first controller into a second controller
+        KernelServices servicesB = createKernelServicesBuilder(additionalInit).setSubsystemXml(marshalled).build();
+        ModelNode modelB = servicesB.readWholeModel();
+
+        //Make sure the models from the two controllers are identical
+        super.compare(modelA, modelB);
+    }
+
+    @Test
+    public void testParseAndMarshalModel2_0WithShowModelsAndOldPropertyFormat() throws Exception {
+        //Parse the subsystem xml and install into the first controller
+        String subsystemXml =
+                "<subsystem xmlns=\"" + Namespace.JMX_2_0.getUriString() + "\">" +
+                "   <expose-resolved-model domain-name=\"jboss.RESOLVED\" proper-property-format=\"false\"/>" +
+                "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
+                "</subsystem>";
+
+        AdditionalInitialization additionalInit = new BaseAdditionalInitalization();
+
+        KernelServices servicesA = createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemXml).build();
+        Assert.assertTrue(servicesA.isSuccessfulBoot());
+        //Get the model and the persisted xml from the first controller
+        ModelNode modelA = servicesA.readWholeModel();
+        Assert.assertTrue(modelA.get(SUBSYSTEM, "jmx", CommonAttributes.EXPOSE_MODEL, CommonAttributes.RESOLVED).hasDefined(CommonAttributes.PROPER_PROPERTY_FORMAT));
+        Assert.assertFalse(modelA.get(SUBSYSTEM, "jmx", CommonAttributes.EXPOSE_MODEL, CommonAttributes.RESOLVED, CommonAttributes.PROPER_PROPERTY_FORMAT).asBoolean());
+        String marshalled = servicesA.getPersistedSubsystemXml();
+        servicesA.shutdown();
+
+        Assert.assertTrue(marshalled.contains(Namespace.CURRENT.getUriString()));
+        compareXml(null, subsystemXml, marshalled, true);
+
+        //Install the persisted xml from the first controller into a second controller
+        KernelServices servicesB = createKernelServicesBuilder(additionalInit).setSubsystemXml(marshalled).build();
+        ModelNode modelB = servicesB.readWholeModel();
+
+        //Make sure the models from the two controllers are identical
+        super.compare(modelA, modelB);
+    }
+
+    @Test
+    public void testParseAndMarshallModelWithAuditLogButNoHandlerReferences() throws Exception {
+        String subsystemXml =
+                "<subsystem xmlns=\"" + Namespace.CURRENT.getUriString() + "\">" +
                 "   <expose-resolved-model domain-name=\"jboss.RESOLVED\" proper-property-format=\"false\"/>" +
                 "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
                 "   <audit-log log-boot=\"true\" log-read-only=\"false\" enabled=\"false\"/>" +
@@ -445,9 +505,9 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
     }
 
     @Test
-    public void testParseAndMarshallModel1_2WithAuditLogAndHandlerReferences() throws Exception {
+    public void testParseAndMarshallModelWithAuditLogAndHandlerReferences() throws Exception {
         String subsystemXml =
-                "<subsystem xmlns=\"" + Namespace.JMX_1_2.getUriString() + "\">" +
+                "<subsystem xmlns=\"" + Namespace.CURRENT.getUriString() + "\">" +
                 "   <expose-resolved-model domain-name=\"jboss.RESOLVED\" proper-property-format=\"false\"/>" +
                 "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
                 "   <audit-log log-boot=\"true\" log-read-only=\"false\" enabled=\"false\">" +
@@ -491,6 +551,7 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
         //Make sure the models from the two controllers are identical
         super.compare(modelA, modelB);
     }
+
     @Test
     public void testDescribeHandler() throws Exception {
         //Parse the subsystem xml and install into the first controller
@@ -711,6 +772,7 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
         checkDiscardedOperation(Util.createAddOperation(address), mainServices, oldVersion);
         checkDiscardedOperation(Util.createRemoveOperation(address), mainServices, oldVersion);
     }
+
 
     private void checkDiscardedOperation(ModelNode op, KernelServices mainServices, ModelVersion oldVersion) throws OperationFailedException {
         TransformedOperation transformedOp = mainServices.transformOperation(oldVersion, op);

wildfly/wildfly

jmx/src/main/java/org/jboss/as/jmx/JMXExtension.java
@@ -102,14 +102,14 @@ public class JMXExtension implements Extension {
             new SensitivityClassification(SUBSYSTEM_NAME, "jmx", false, false, true);
 
     static final SensitiveTargetAccessConstraintDefinition JMX_SENSITIVITY_DEF = new SensitiveTargetAccessConstraintDefinition(JMX_SENSITIVITY);
-
-    static final JMXSubsystemParser_1_2 parserCurrent = new JMXSubsystemParser_1_2();
+    static final JMXSubsystemParser_2_0 parserCurrent = new JMXSubsystemParser_2_0();
+    static final JMXSubsystemParser_1_2 parser12 = new JMXSubsystemParser_1_2();
     static final JMXSubsystemParser_1_1 parser11 = new JMXSubsystemParser_1_1();
     static final JMXSubsystemParser_1_0 parser10 = new JMXSubsystemParser_1_0();
     static final JMXSubsystemWriter writer = new JMXSubsystemWriter();
 
     private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
-    private static final int MANAGEMENT_API_MINOR_VERSION = 2;
+    private static final int MANAGEMENT_API_MINOR_VERSION = 3;
     private static final int MANAGEMENT_API_MICRO_VERSION = 0;
 
     /**
@@ -139,7 +139,8 @@ public class JMXExtension implements Extension {
     public void initializeParsers(ExtensionParsingContext context) {
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_0.getUriString(), parser10);
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_1.getUriString(), parser11);
-        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_2.getUriString(), parserCurrent);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_2.getUriString(), parser12);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, Namespace.JMX_1_3.getUriString(), parserCurrent);
     }
 
     private static ModelNode createAddOperation() {
@@ -505,13 +506,6 @@ public class JMXExtension implements Extension {
                         connectorAdd = true;
                         list.add(parseRemoteConnector(reader));
                         break;
-                    case AUDIT_LOG:
-                        if (auditLog) {
-                            throw ParseUtils.duplicateNamedElement(reader, Element.AUDIT_LOG.getLocalName());
-                        }
-                        auditLog = true;
-                        parseAuditLogElement(reader, list);
-                        break;
                     default: {
                         throw ParseUtils.unexpectedElement(reader);
                     }
@@ -519,7 +513,7 @@ public class JMXExtension implements Extension {
             }
         }
 
-        private ModelNode parseShowModelElement(XMLExtendedStreamReader reader, String showModelChild) throws XMLStreamException {
+        protected ModelNode parseShowModelElement(XMLExtendedStreamReader reader, String showModelChild) throws XMLStreamException {
 
             ModelNode op = createOperation(ADD, CommonAttributes.EXPOSE_MODEL, showModelChild);
 
@@ -550,7 +544,57 @@ public class JMXExtension implements Extension {
             }
             return op;
         }
+    }
 
+    private static class JMXSubsystemParser_2_0 extends JMXSubsystemParser_1_2 {
+        @Override
+        public void readElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
+            boolean showResolvedModel = false;
+            boolean showExpressionModel = false;
+            boolean connectorAdd = false;
+            boolean auditLog = false;
+
+            ParseUtils.requireNoAttributes(reader);
+
+            list.add(createAddOperation());
+
+            while (reader.hasNext() && reader.nextTag() != END_ELEMENT) {
+                final Element element = Element.forName(reader.getLocalName());
+                switch (element) {
+                    case EXPOSE_RESOLVED_MODEL:
+                        if (showResolvedModel) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.EXPOSE_RESOLVED_MODEL.getLocalName());
+                        }
+                        showResolvedModel = true;
+                        list.add(parseShowModelElement(reader, CommonAttributes.RESOLVED));
+                        break;
+                    case EXPOSE_EXPRESSION_MODEL:
+                        if (showExpressionModel) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.EXPOSE_EXPRESSION_MODEL.getLocalName());
+                        }
+                        showExpressionModel = true;
+                        list.add(parseShowModelElement(reader, CommonAttributes.EXPRESSION));
+                        break;
+                    case REMOTING_CONNECTOR:
+                        if (connectorAdd) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.REMOTING_CONNECTOR.getLocalName());
+                        }
+                        connectorAdd = true;
+                        list.add(parseRemoteConnector(reader));
+                        break;
+                    case AUDIT_LOG:
+                        if (auditLog) {
+                            throw ParseUtils.duplicateNamedElement(reader, Element.AUDIT_LOG.getLocalName());
+                        }
+                        auditLog = true;
+                        parseAuditLogElement(reader, list);
+                        break;
+                    default: {
+                        throw ParseUtils.unexpectedElement(reader);
+                    }
+                }
+            }
+        }
 
         private void parseAuditLogElement(XMLExtendedStreamReader reader, List<ModelNode> list) throws XMLStreamException {
 
@@ -636,6 +680,7 @@ public class JMXExtension implements Extension {
             ParseUtils.requireNoContent(reader);
         }
     }
+
 
     private static class JMXSubsystemWriter implements XMLStreamConstants, XMLElementWriter<SubsystemMarshallingContext> {
         /**

jmx/src/test/java/org/jboss/as/jmx/JMXSubsystemTestCase.java
@@ -413,10 +413,70 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
         super.compare(modelA, modelB);
     }
 
+
     @Test
-    public void testParseAndMarshallModel1_2WithAuditLogButNoHandlerReferences() throws Exception {
+    public void testParseAndMarshalModel2_0WithShowModels() throws Exception {
+        //Parse the subsystem xml and install into the first controller
         String subsystemXml =
-                "<subsystem xmlns=\"" + Namespace.JMX_1_2.getUriString() + "\">" +
+                "<subsystem xmlns=\"" + Namespace.JMX_1_3.getUriString() + "\">" +
+                "   <expose-resolved-model domain-name=\"jboss.RESOLVED\"/>" +
+                "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
+                "</subsystem>";
+
+        AdditionalInitialization additionalInit = new BaseAdditionalInitalization();
+
+        KernelServices servicesA = createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemXml).build();
+        //Get the model and the persisted xml from the first controller
+        ModelNode modelA = servicesA.readWholeModel();
+        String marshalled = servicesA.getPersistedSubsystemXml();
+        servicesA.shutdown();
+
+        Assert.assertTrue(marshalled.contains(Namespace.CURRENT.getUriString()));
+        compareXml(null, subsystemXml, marshalled, true);
+
+        //Install the persisted xml from the first controller into a second controller
+        KernelServices servicesB = createKernelServicesBuilder(additionalInit).setSubsystemXml(marshalled).build();
+        ModelNode modelB = servicesB.readWholeModel();
+
+        //Make sure the models from the two controllers are identical
+        super.compare(modelA, modelB);
+    }
+
+    @Test
+    public void testParseAndMarshalModel2_0WithShowModelsAndOldPropertyFormat() throws Exception {
+        //Parse the subsystem xml and install into the first controller
+        String subsystemXml =
+                "<subsystem xmlns=\"" + Namespace.JMX_1_3.getUriString() + "\">" +
+                "   <expose-resolved-model domain-name=\"jboss.RESOLVED\" proper-property-format=\"false\"/>" +
+                "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
+                "</subsystem>";
+
+        AdditionalInitialization additionalInit = new BaseAdditionalInitalization();
+
+        KernelServices servicesA = createKernelServicesBuilder(additionalInit).setSubsystemXml(subsystemXml).build();
+        Assert.assertTrue(servicesA.isSuccessfulBoot());
+        //Get the model and the persisted xml from the first controller
+        ModelNode modelA = servicesA.readWholeModel();
+        Assert.assertTrue(modelA.get(SUBSYSTEM, "jmx", CommonAttributes.EXPOSE_MODEL, CommonAttributes.RESOLVED).hasDefined(CommonAttributes.PROPER_PROPERTY_FORMAT));
+        Assert.assertFalse(modelA.get(SUBSYSTEM, "jmx", CommonAttributes.EXPOSE_MODEL, CommonAttributes.RESOLVED, CommonAttributes.PROPER_PROPERTY_FORMAT).asBoolean());
+        String marshalled = servicesA.getPersistedSubsystemXml();
+        servicesA.shutdown();
+
+        Assert.assertTrue(marshalled.contains(Namespace.CURRENT.getUriString()));
+        compareXml(null, subsystemXml, marshalled, true);
+
+        //Install the persisted xml from the first controller into a second controller
+        KernelServices servicesB = createKernelServicesBuilder(additionalInit).setSubsystemXml(marshalled).build();
+        ModelNode modelB = servicesB.readWholeModel();
+
+        //Make sure the models from the two controllers are identical
+        super.compare(modelA, modelB);
+    }
+
+    @Test
+    public void testParseAndMarshallModelWithAuditLogButNoHandlerReferences() throws Exception {
+        String subsystemXml =
+                "<subsystem xmlns=\"" + Namespace.CURRENT.getUriString() + "\">" +
                 "   <expose-resolved-model domain-name=\"jboss.RESOLVED\" proper-property-format=\"false\"/>" +
                 "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
                 "   <audit-log log-boot=\"true\" log-read-only=\"false\" enabled=\"false\"/>" +
@@ -445,9 +505,9 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
     }
 
     @Test
-    public void testParseAndMarshallModel1_2WithAuditLogAndHandlerReferences() throws Exception {
+    public void testParseAndMarshallModelWithAuditLogAndHandlerReferences() throws Exception {
         String subsystemXml =
-                "<subsystem xmlns=\"" + Namespace.JMX_1_2.getUriString() + "\">" +
+                "<subsystem xmlns=\"" + Namespace.CURRENT.getUriString() + "\">" +
                 "   <expose-resolved-model domain-name=\"jboss.RESOLVED\" proper-property-format=\"false\"/>" +
                 "   <expose-expression-model domain-name=\"jboss.EXPRESSION\"/>" +
                 "   <audit-log log-boot=\"true\" log-read-only=\"false\" enabled=\"false\">" +
@@ -491,6 +551,7 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
         //Make sure the models from the two controllers are identical
         super.compare(modelA, modelB);
     }
+
     @Test
     public void testDescribeHandler() throws Exception {
         //Parse the subsystem xml and install into the first controller
@@ -711,6 +772,7 @@ public class JMXSubsystemTestCase extends AbstractSubsystemTest {
         checkDiscardedOperation(Util.createAddOperation(address), mainServices, oldVersion);
         checkDiscardedOperation(Util.createRemoveOperation(address), mainServices, oldVersion);
     }
+
 
     private void checkDiscardedOperation(ModelNode op, KernelServices mainServices, ModelVersion oldVersion) throws OperationFailedException {
         TransformedOperation transformedOp = mainServices.transformOperation(oldVersion, op);