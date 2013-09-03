57fefe63731374c0962439fb30094e73d5d9d148
kabir <kkhan@redhat.com>
2013-09-03 13:18:59 +0100

    [WFLY-456] Fix search for handler when running in domain mode

--- jmx/src/main/java/org/jboss/as/jmx/JmxAuditLogHandlerReferenceResourceDefinition.java
+++ jmx/src/main/java/org/jboss/as/jmx/JmxAuditLogHandlerReferenceResourceDefinition.java
@@ -42,8 +42,10 @@ import org.jboss.as.controller.RunningMode;
 import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.controller.SimpleResourceDefinition;
 import org.jboss.as.controller.audit.ManagedAuditLogger;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.as.controller.registry.Resource;
+import org.jboss.as.controller.registry.Resource.ResourceEntry;
 import org.jboss.as.domain.management.CoreManagementResourceDefinition;
 import org.jboss.as.domain.management.audit.AccessAuditResourceDefinition;
 import org.jboss.dmr.ModelNode;
@@ -80,7 +82,8 @@ public class JmxAuditLogHandlerReferenceResourceDefinition extends SimpleResourc
         protected void populateModel(final OperationContext context, final ModelNode operation, final Resource resource) throws  OperationFailedException {
             final PathAddress addr = PathAddress.pathAddress(operation.require(OP_ADDR));
             String name = addr.getLastElement().getValue();
-            if (!lookForHandler(context, name)) {
+            boolean found = context.getProcessType().isServer() ? lookForHandlerForServer(context, name) : lookForHandlerForHc(context, name);
+            if (!found) {
                 throw JmxMessages.MESSAGES.noHandlerCalled(name);
             }
             resource.getModel().setEmptyObject();
@@ -120,19 +123,32 @@ public class JmxAuditLogHandlerReferenceResourceDefinition extends SimpleResourc
             auditLogger.getUpdater().rollbackChanges();
         }
 
-        private boolean lookForHandler(OperationContext context, String name) {
-            PathAddress addr = PathAddress.pathAddress(
+        private boolean lookForHandlerForServer(OperationContext context, String name) {
+            final Resource root = context.readResourceFromRoot(PathAddress.EMPTY_ADDRESS);
+            return lookForHandler(PathAddress.EMPTY_ADDRESS, root, name);
+        }
+
+        private boolean lookForHandlerForHc(OperationContext context, String name) {
+            final Resource root = context.readResourceFromRoot(PathAddress.EMPTY_ADDRESS);
+            for (ResourceEntry entry : root.getChildren(ModelDescriptionConstants.HOST)) {
+                if (entry.getModel().isDefined()) {
+                    return lookForHandler(PathAddress.pathAddress(ModelDescriptionConstants.HOST, entry.getName()), root, name);
+                }
+            }
+            return false;
+        }
+
+        private boolean lookForHandler(PathAddress rootAddress, Resource root, String name) {
+            PathAddress addr = rootAddress.append(
                                     CoreManagementResourceDefinition.PATH_ELEMENT,
                                     AccessAuditResourceDefinition.PATH_ELEMENT);
             PathAddress referenceAddress = addr.append(FILE_HANDLER, name);
-            final Resource root = context.readResourceFromRoot(PathAddress.EMPTY_ADDRESS);
             if (lookForResource(root, referenceAddress)) {
                 return true;
             }
             referenceAddress  = addr.append(SYSLOG_HANDLER, name);
             return lookForResource(root, referenceAddress);
         }
-
         private boolean lookForResource(final Resource root, final PathAddress pathAddress) {
             Resource current = root;
             for (PathElement element : pathAddress) {
