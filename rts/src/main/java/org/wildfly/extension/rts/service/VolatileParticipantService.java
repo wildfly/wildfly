d073f85fabf4b54ac650908758320e9ddae24bd5
Gytis Trikleris <gytist@gmail.com>
2013-09-04 12:24:06 +0100

    JBTM-1721. REST-AT participant support framework should support synchronizations

--- rts/src/main/java/org/wildfly/extension/rts/service/VolatileParticipantService.java
+++ rts/src/main/java/org/wildfly/extension/rts/service/VolatileParticipantService.java
@@ -22,33 +22,32 @@
 package org.wildfly.extension.rts.service;
 
 import io.undertow.servlet.api.DeploymentInfo;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.jboss.msc.service.Service;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
-import org.jboss.narayana.rest.integration.ParticipantResource;
+import org.jboss.narayana.rest.integration.VolatileParticipantResource;
 import org.jboss.narayana.rest.integration.api.ParticipantsManagerFactory;
 import org.wildfly.extension.rts.logging.RTSLogger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
  *
  */
-public final class ParticipantService extends AbstractRTSService implements Service<ParticipantService> {
+public final class VolatileParticipantService extends AbstractRTSService implements Service<VolatileParticipantService> {
 
-    public static final String CONTEXT_PATH = ParticipantResource.BASE_PATH_SEGMENT;
+    public static final String CONTEXT_PATH = VolatileParticipantResource.BASE_PATH_SEGMENT;
 
-    private static final String DEPLOYMENT_NAME = "REST-AT Participant";
+    private static final String DEPLOYMENT_NAME = "Volatile REST-AT Participant";
 
     @Override
-    public ParticipantService getValue() throws IllegalStateException, IllegalArgumentException {
+    public VolatileParticipantService getValue() throws IllegalStateException, IllegalArgumentException {
         if (RTSLogger.ROOT_LOGGER.isTraceEnabled()) {
-            RTSLogger.ROOT_LOGGER.trace("ParticipantService.getValue");
+            RTSLogger.ROOT_LOGGER.trace("VolatileParticipantService.getValue");
         }
 
         return this;
@@ -57,7 +56,7 @@ public final class ParticipantService extends AbstractRTSService implements Serv
     @Override
     public void start(StartContext context) throws StartException {
         if (RTSLogger.ROOT_LOGGER.isTraceEnabled()) {
-            RTSLogger.ROOT_LOGGER.trace("ParticipantService.start");
+            RTSLogger.ROOT_LOGGER.trace("VolatileParticipantService.start");
         }
 
         deployParticipant();
@@ -77,7 +76,7 @@ public final class ParticipantService extends AbstractRTSService implements Serv
         undeployServlet();
 
         final Map<String, String> initialParameters = new HashMap<String, String>();
-        initialParameters.put("javax.ws.rs.Application", "org.wildfly.extension.rts.jaxrs.ParticipantApplication");
+        initialParameters.put("javax.ws.rs.Application", "org.wildfly.extension.rts.jaxrs.VolatileParticipantApplication");
 
         final DeploymentInfo participantDeploymentInfo = getDeploymentInfo(DEPLOYMENT_NAME, CONTEXT_PATH, initialParameters);
 
