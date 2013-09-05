5727ceab1d4e6c89a257cf4b57c8cc82dd7c1c40
Scott Marlow <smarlow@redhat.com>
2013-09-05 08:24:15 -0400

    WFLY-2001 JPA getNewTempClassLoader should support getPackage(String name)

--- jpa/core/src/test/java/org/jboss/as/jpa/classloader/TempClassLoaderTestCase.java
+++ jpa/core/src/test/java/org/jboss/as/jpa/classloader/TempClassLoaderTestCase.java
@@ -67,6 +67,15 @@ public class TempClassLoaderTestCase {
         resourceStream.close();
     }
 
+    @Test
+    public void testLoadEntityClassPackage() throws Exception {
+        ClassLoader tempClassLoader = factory.createNewTempClassLoader();
+        String className = TestEntity.class.getName();
+
+        Class<?> entityClass = tempClassLoader.loadClass(className);
+        assertNotNull("could not load package for entity class that came from NewTempClassLoader", entityClass.getPackage());
+    }
+
     @Entity
     public static class TestEntity {
     }
