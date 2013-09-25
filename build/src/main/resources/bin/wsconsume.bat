e743b51c18bead21a3f58517563a5e317cfed597
Josef Cacek <josef.cacek@gmail.com>
2013-09-25 08:51:42 +0200

    WFLY-2152 fix windows batch scripts to support ampesand character in paths

--- build/src/main/resources/bin/wsconsume.bat
+++ build/src/main/resources/bin/wsconsume.bat
@@ -1,13 +1,35 @@
 @echo off
 
 @if not "%ECHO%" == ""  echo %ECHO%
-@if "%OS%" == "Windows_NT"  setlocal
+@if "%OS%" == "Windows_NT" setlocal
 
-set DIRNAME=.\
-if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
-set PROGNAME=run.bat
-if "%OS%" == "Windows_NT" set PROGNAME=%~nx0%
+if "%OS%" == "Windows_NT" (
+  set "DIRNAME=%~dp0%"
+) else (
+  set DIRNAME=.\
+)
 
+pushd "%DIRNAME%.."
+set "RESOLVED_JBOSS_HOME=%CD%"
+popd
+
+if "x%JBOSS_HOME%" == "x" (
+  set "JBOSS_HOME=%RESOLVED_JBOSS_HOME%"
+)
+
+pushd "%JBOSS_HOME%"
+set "SANITIZED_JBOSS_HOME=%CD%"
+popd
+
+if /i "%RESOLVED_JBOSS_HOME%" NEQ "%SANITIZED_JBOSS_HOME%" (
+   echo.
+   echo   WARNING:  JBOSS_HOME may be pointing to a different installation - unpredictable results may occur.
+   echo.
+   echo       JBOSS_HOME: "%JBOSS_HOME%"
+   echo.
+)
+
+rem Setup JBoss specific properties
 if "x%JAVA_HOME%" == "x" (
   set  JAVA=java
   echo JAVA_HOME is not set. Unexpected results may occur.
@@ -15,14 +37,25 @@ if "x%JAVA_HOME%" == "x" (
 ) else (
   set "JAVA=%JAVA_HOME%\bin\java"
 )
-set JBOSS_HOME=%DIRNAME%\..
 
-set "JAVA_OPTS=%JAVA_OPTS% -Dprogram.name=wsconsume.bat"
+rem Find jboss-modules.jar, or we can't continue
+set "JBOSS_RUNJAR=%JBOSS_HOME%\jboss-modules.jar"
+if not exist "%JBOSS_RUNJAR%" (
+  echo Could not locate "%JBOSS_RUNJAR%".
+  echo Please check that you are in the bin directory when running this script.
+  goto END
+)
 
-rem Execute the command
+rem Set default module root paths
+if "x%JBOSS_MODULEPATH%" == "x" (
+  set "JBOSS_MODULEPATH=%JBOSS_HOME%\modules"
+)
+
 "%JAVA%" %JAVA_OPTS% ^
-    -classpath "%JAVA_HOME%\lib\tools.jar;%JBOSS_HOME%\jboss-modules.jar" ^
+    -classpath "%JAVA_HOME%\lib\tools.jar;%JBOSS_RUNJAR%" ^
     org.jboss.modules.Main ^
-    -mp "%JBOSS_HOME%\modules" ^
+    -mp "%JBOSS_MODULEPATH%" ^
     org.jboss.ws.tools.wsconsume ^
-    %*
+     %*
+
+:END
