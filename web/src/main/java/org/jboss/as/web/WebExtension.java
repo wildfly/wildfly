125ad0d88e2768c561c740dedaefda02aac9962a
Brian Stansberry <brian.stansberry@redhat.com>
2013-10-08 16:41:31 -0500

    [WFLY-490] / [WFLY-2262] Fix web security classification settings
    Conflicts:
    	legacy/web/src/main/java/org/jboss/as/web/WebExtension.java

--- web/src/main/java/org/jboss/as/web/WebExtension.java
+++ web/src/main/java/org/jboss/as/web/WebExtension.java
@@ -103,10 +103,10 @@ public class WebExtension implements Extension {
     }
 
     static final SensitiveTargetAccessConstraintDefinition WEB_CONNECTOR_CONSTRAINT = new SensitiveTargetAccessConstraintDefinition(
-            new SensitivityClassification(SUBSYSTEM_NAME, "web-connector", true, false, false));
+            new SensitivityClassification(SUBSYSTEM_NAME, "web-connector", false, false, false));
 
     static final SensitiveTargetAccessConstraintDefinition WEB_VALVE_CONSTRAINT = new SensitiveTargetAccessConstraintDefinition(
-            new SensitivityClassification(SUBSYSTEM_NAME, "web-valve", true, false, false));
+            new SensitivityClassification(SUBSYSTEM_NAME, "web-valve", false, false, false));
 
     private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
     private static final int MANAGEMENT_API_MINOR_VERSION = 3;
