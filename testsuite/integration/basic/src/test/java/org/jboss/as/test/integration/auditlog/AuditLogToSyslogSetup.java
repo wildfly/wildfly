eacaeee08d8c7748e9f3706898a6c9fc64eb13d5
Josef Cacek <josef.cacek@gmail.com>
2013-10-08 19:34:18 +0200

    [WFLY-1584] Fix existing and add new syslog-handler tests (audit-log - UDP,TCP,TLS)

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/auditlog/AuditLogToSyslogSetup.java
@@ -0,0 +1,193 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.auditlog;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ACCESS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ALLOW_RESOURCE_SERVICE_RESTART;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.AUDIT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.AUDIT_LOG;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.COMPOSITE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CORE_SERVICE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HANDLER;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HOST;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.LOGGER;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.LOG_READ_ONLY;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.MANAGEMENT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OPERATION_HEADERS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.PORT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ROLLBACK_ON_RUNTIME_FAILURE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.STEPS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SYSLOG_FORMAT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SYSLOG_HANDLER;
+
+import java.util.List;
+
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.operations.common.Util;
+import org.jboss.as.test.integration.logging.syslogserver.BlockedSyslogServerEventHandler;
+import org.jboss.as.test.integration.security.common.Utils;
+import org.jboss.dmr.ModelNode;
+import org.productivity.java.syslog4j.server.SyslogServer;
+import org.productivity.java.syslog4j.server.SyslogServerConfigIF;
+import org.productivity.java.syslog4j.server.SyslogServerIF;
+
+/**
+ * Abstract parent for Syslog handler setup in AuditLog.
+ * 
+ * @author Josef Cacek
+ */
+public abstract class AuditLogToSyslogSetup implements ServerSetupTask {
+
+    protected static final String SYSLOG_HANDLER_NAME = "audit-test-syslog-handler";
+    protected static final int SYSLOG_PORT = 9176;
+
+    protected static final PathAddress AUDIT_LOG_ADDRESS = PathAddress.pathAddress().append(CORE_SERVICE, MANAGEMENT)
+            .append(ACCESS, AUDIT);
+    protected static final PathAddress AUDIT_LOG_LOGGER_ADDR = AUDIT_LOG_ADDRESS.append(LOGGER, AUDIT_LOG);
+    protected static final PathAddress AUDIT_SYSLOG_HANDLER_ADDR = AUDIT_LOG_ADDRESS
+            .append(SYSLOG_HANDLER, SYSLOG_HANDLER_NAME);
+    protected static final PathAddress AUDIT_LOG_LOGGER_SYSLOG_HANDLER_ADDR = AUDIT_LOG_LOGGER_ADDR.append(HANDLER,
+            SYSLOG_HANDLER_NAME);
+
+    private SyslogServerIF server;
+
+    private static final String FORMATTER = "formatter";
+    private static final String JSON_FORMATTER = "json-formatter";
+
+    /**
+     * Returns name of syslog protocol used. It should be one of "tcp", "udp", "tls"
+     * 
+     * @return
+     */
+    protected abstract String getSyslogProtocol();
+
+    /**
+     * Returns a new instance of {@link SyslogServerConfigIF}. It's not necessary to specify host or port, these attributes are
+     * configured in {@link #setup(ManagementClient, String)} method.
+     * 
+     * @return
+     */
+    protected abstract SyslogServerConfigIF getSyslogConfig();
+
+    /**
+     * Implementation should return a {@link ModelNode} which configures the syslog protocol in auditlog. It's not necessary to
+     * specify host or port, these attributes are configured in {@link #setup(ManagementClient, String)} method.
+     * 
+     * @return
+     */
+    protected abstract ModelNode addAuditlogSyslogProtocol();
+
+    /**
+     * If protocol configured in {@link #addAuditlogSyslogProtocol()} has additional settings, child classes can override this
+     * method for configuration.
+     * 
+     * @return
+     */
+    protected List<ModelNode> addProtocolSettings() {
+        return null;
+    }
+
+    /**
+     * Starts Syslog server and configures syslog handler in the AS.
+     * 
+     * @see org.jboss.as.arquillian.api.ServerSetupTask#setup(org.jboss.as.arquillian.container.ManagementClient,
+     *      java.lang.String)
+     */
+    @Override
+    public void setup(ManagementClient managementClient, String containerId) throws Exception {
+        // clear created server instances (TCP/UDP)
+        SyslogServer.shutdown();
+
+        // start and set syslog server
+        final String host = Utils.getHost(managementClient);
+        SyslogServerConfigIF config = getSyslogConfig();
+        config.setUseStructuredData(true);
+        config.setHost(host);
+        config.setPort(SYSLOG_PORT);
+        config.addEventHandler(new BlockedSyslogServerEventHandler());
+        final String syslogProtocol = getSyslogProtocol();
+        server = SyslogServer.createInstance(syslogProtocol, config);
+        // start syslog server
+        SyslogServer.getThreadedInstance(syslogProtocol);
+
+        ModelNode op;
+
+        // set logging to syslog
+        final ModelNode compositeOp = new ModelNode();
+        compositeOp.get(OP).set(COMPOSITE);
+        compositeOp.get(OP_ADDR).setEmptyList();
+        ModelNode steps = compositeOp.get(STEPS);
+        op = Util.createAddOperation(AUDIT_SYSLOG_HANDLER_ADDR);
+        op.get(FORMATTER).set(JSON_FORMATTER);
+        op.get(SYSLOG_FORMAT).set("RFC5424");
+        steps.add(op);
+        final ModelNode auditlogSyslogProtocol = addAuditlogSyslogProtocol();
+        auditlogSyslogProtocol.get(PORT).set(SYSLOG_PORT);
+        auditlogSyslogProtocol.get(HOST).set(host);
+        steps.add(auditlogSyslogProtocol);
+        Utils.applyUpdate(compositeOp, managementClient.getControllerClient());
+
+        List<ModelNode> protocolSettings = addProtocolSettings();
+        if (protocolSettings != null) {
+            Utils.applyUpdates(protocolSettings, managementClient.getControllerClient());
+        }
+
+        op = Util.createAddOperation(AUDIT_LOG_LOGGER_SYSLOG_HANDLER_ADDR);
+        Utils.applyUpdate(op, managementClient.getControllerClient());
+
+        op = Util.getWriteAttributeOperation(AUDIT_LOG_LOGGER_ADDR, LOG_READ_ONLY, false);
+        Utils.applyUpdate(op, managementClient.getControllerClient());
+
+    }
+
+    /**
+     * Stops syslog server and removes auditlog configuration.
+     * 
+     * @see org.jboss.as.arquillian.api.ServerSetupTask#tearDown(org.jboss.as.arquillian.container.ManagementClient,
+     *      java.lang.String)
+     */
+    @Override
+    public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
+        // stop syslog server
+        SyslogServer.shutdown();
+        server.setThread(null);
+        server.getConfig().removeAllEventHandlers();
+
+        ModelNode op = Util.createRemoveOperation(AUDIT_LOG_LOGGER_SYSLOG_HANDLER_ADDR);
+        op.get(OPERATION_HEADERS, ROLLBACK_ON_RUNTIME_FAILURE).set(false);
+        op.get(OPERATION_HEADERS, ALLOW_RESOURCE_SERVICE_RESTART).set(true);
+        Utils.applyUpdate(op, managementClient.getControllerClient());
+
+        op = Util.createRemoveOperation(AUDIT_SYSLOG_HANDLER_ADDR);
+        op.get(OPERATION_HEADERS, ROLLBACK_ON_RUNTIME_FAILURE).set(false);
+        op.get(OPERATION_HEADERS, ALLOW_RESOURCE_SERVICE_RESTART).set(true);
+        Utils.applyUpdate(op, managementClient.getControllerClient());
+
+        Utils.applyUpdate(Util.getWriteAttributeOperation(AUDIT_LOG_LOGGER_ADDR, LOG_READ_ONLY, false),
+                managementClient.getControllerClient());
+    }
+
+}
--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/auditlog/AuditLogToSyslogTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/auditlog/AuditLogToSyslogTestCase.java
@@ -1,298 +1,148 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.as.test.integration.auditlog;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ACCESS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.AUDIT;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.AUDIT_LOG;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.COMPOSITE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CORE_SERVICE;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ENABLED;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.HANDLER;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.LOGGER;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.LOG_READ_ONLY;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.MANAGEMENT;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.PROTOCOL;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REMOVE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.STEPS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SYSLOG_FORMAT;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SYSLOG_HANDLER;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SYSTEM_PROPERTY;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.UDP;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.api.ContainerResource;
-import org.jboss.as.arquillian.api.ServerSetup;
-import org.jboss.as.arquillian.api.ServerSetupTask;
 import org.jboss.as.arquillian.container.ManagementClient;
-import org.jboss.as.controller.client.ModelControllerClient;
-import org.jboss.as.controller.client.OperationBuilder;
-import org.jboss.as.test.integration.security.common.BlockedFileSyslogServerEventHandler;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.operations.common.Util;
+import org.jboss.as.test.integration.logging.syslogserver.BlockedSyslogServerEventHandler;
+import org.jboss.as.test.integration.security.common.Utils;
 import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.dmr.ModelNode;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.productivity.java.syslog4j.server.SyslogServer;
+import org.productivity.java.syslog4j.server.SyslogServerEventIF;
 import org.productivity.java.syslog4j.server.SyslogServerIF;
-import org.xnio.IoUtils;
 
 /**
- * Test that syslog-handler logs in Audit Log
+ * Test that syslog-handler logs in Audit Log.
  * 
  * @author: Ondrej Lukas
+ * @author: Josef Cacek
  */
-@RunWith(Arquillian.class)
-@RunAsClient
-@ServerSetup(AuditLogToSyslogTestCase.AuditLogToSyslogSetup.class)
-public class AuditLogToSyslogTestCase {
+public abstract class AuditLogToSyslogTestCase {
+
+    private static final int ADJUSTED_SECOND = TimeoutUtil.adjust(1000);
 
     @ContainerResource
     private ManagementClient managementClient;
 
-    private static final String FILE_NAME = "tempSyslogFile.log";
-    private static File logFile;
-    private static SyslogServerIF server;
-    private static BlockingQueue<String> queue;
-    private static final int ADJUSTED_SECOND = TimeoutUtil.adjust(1000);
-    private static List<Long> properties = new ArrayList<Long>();
+    protected static SyslogServerIF server;
 
+    private List<Long> properties = new ArrayList<Long>();
+
+    /**
+     * Tests following steps in a test syslog server.
+     * <ol>
+     * <li>throw auditable event with auditlog disabled - check no message came to the syslog</li>
+     * <li>enable the auditlog (it's auditable event itself) - check the message in syslog</li>
+     * <li>throw auditable event with auditlog enabled - check the mesage in syslog</li>
+     * <li>disable and check auditlog (it's auditable event itself) - check the message in syslog</li>
+     * <li>check auditable event with auditlog disabled - check no message came to the syslog</li>
+     * </ol>
+     * 
+     * @throws Exception
+     */
     @Test
     public void testAuditLoggingToSyslog() throws Exception {
-        if (logFile.exists()) {
-            logFile.delete();
-            server.getConfig().removeAllEventHandlers();
-            server.getConfig()
-                    .addEventHandler(new BlockedFileSyslogServerEventHandler(queue, logFile.getAbsolutePath(), false));
-        }
-        Assert.assertEquals(0, readFile(logFile));
+        final BlockingQueue<SyslogServerEventIF> queue = BlockedSyslogServerEventHandler.getQueue();
+        queue.clear();
+
+        SyslogServerEventIF syslogEvent = null;
         makeOneLog();
-        queue.poll(3 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
-        Assert.assertEquals("Audit log is logged to syslog even it isn't enabled", 0, readFile(logFile));
+        syslogEvent = queue.poll(1 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
+        Assert.assertNull("No message was expected in the syslog", syslogEvent);
+
         try {
-            enableLog();
-            queue.poll(15 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
-            Assert.assertEquals("Enabled message of audit log isn't logged to syslog", 1, readFile(logFile));
+            setAuditlogEnabled(true);
+            syslogEvent = queue.poll(5 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
+            Assert.assertNotNull("Enabling audit log wasn't logged into the syslog", syslogEvent);
             makeOneLog();
-            queue.poll(15 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
-            Assert.assertEquals("Audit log isn't logged to syslog", 2, readFile(logFile));
+            syslogEvent = queue.poll(5 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
+            Assert.assertNotNull("Auditable event was't logged into the syslog", syslogEvent);
         } finally {
-            disableLog();
+            setAuditlogEnabled(false);
         }
-        if (logFile.exists()) {
-            logFile.delete();
-            server.getConfig().removeAllEventHandlers();
-            server.getConfig()
-                    .addEventHandler(new BlockedFileSyslogServerEventHandler(queue, logFile.getAbsolutePath(), false));
-        }
+        syslogEvent = queue.poll(5 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
+        Assert.assertNotNull("Disabling audit log wasn't logged into the syslog", syslogEvent);
         makeOneLog();
-        queue.poll(3 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
-        Assert.assertEquals("Audit log is logged to syslog even it was disabled", 0, readFile(logFile));
-    }
+        syslogEvent = queue.poll(1 * ADJUSTED_SECOND, TimeUnit.MILLISECONDS);
+        Assert.assertNull("No message was expected in the syslog", syslogEvent);
 
-    private void enableLog() throws Exception {
-        ModelNode op = new ModelNode();
-        op.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
-        op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-        op.get(OP_ADDR).add(ACCESS, AUDIT);
-        op.get(OP_ADDR).add(LOGGER, AUDIT_LOG);
-        op.get(NAME).set(ENABLED);
-        op.get(VALUE).set("true");
-        AuditLogToSyslogSetup.applyUpdate(managementClient.getControllerClient(), op, false);
-    }
-
-    private void disableLog() throws Exception {
-        ModelNode op = new ModelNode();
-        op.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
-        op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-        op.get(OP_ADDR).add(ACCESS, AUDIT);
-        op.get(OP_ADDR).add(LOGGER, AUDIT_LOG);
-        op.get(NAME).set(ENABLED);
-        op.get(VALUE).set("false");
-        AuditLogToSyslogSetup.applyUpdate(managementClient.getControllerClient(), op, false);
-    }
-
-    private void makeOneLog() throws Exception {
-        long timeStamp = System.currentTimeMillis();
-        properties.add(Long.valueOf(timeStamp));
-        ModelNode op = new ModelNode();
-        op.get(OP).set(ADD);
-        op.get(OP_ADDR).add(SYSTEM_PROPERTY, timeStamp);
-        op.get(NAME).set(NAME);
-        op.get(VALUE).set("someValue");
-        AuditLogToSyslogSetup.applyUpdate(managementClient.getControllerClient(), op, false);
-    }
-
-    private final Pattern DATE_STAMP_PATTERN = Pattern.compile("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d - \\{");
-
-    protected int readFile(File file) throws IOException {
-        int counter = 0;
-        final BufferedReader reader = new BufferedReader(new FileReader(file));
-        try {
-            String line = reader.readLine();
-            while (line != null) {
-                if (DATE_STAMP_PATTERN.matcher(line).find()) {
-                    counter++;
-                }
-                line = reader.readLine();
-            }
-        } finally {
-            IoUtils.safeClose(reader);
+        for (Long property : properties) {
+            Utils.applyUpdate(
+                    Util.createRemoveOperation(PathAddress.pathAddress().append(SYSTEM_PROPERTY, Long.toString(property))),
+                    managementClient.getControllerClient());
         }
-        return counter;
+        properties.clear();
     }
 
+    /**
+     * Creates a dummy deployment.
+     * 
+     * @return
+     */
     @Deployment
     public static WebArchive deployment() {
         final WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war");
         return war;
     }
 
-    static class AuditLogToSyslogSetup implements ServerSetupTask {
+    /**
+     * Throws auditable event. This implemetation writes a system-property to an AS configuration
+     * 
+     * @throws Exception
+     */
+    protected void makeOneLog() throws Exception {
+        long timeStamp = System.currentTimeMillis();
+        properties.add(Long.valueOf(timeStamp));
+        ModelNode op = Util.createAddOperation(PathAddress.pathAddress().append(SYSTEM_PROPERTY, Long.toString(timeStamp)));
+        op.get(NAME).set(NAME);
+        op.get(VALUE).set("someValue");
+        Utils.applyUpdate(op, managementClient.getControllerClient());
+    }
 
-        private static final String FORMATTER = "formatter";
-        private static final String JSON_FORMATTER = "json-formatter";
-        private static final String SYSLOG_HANDLER_NAME = "audit-test-syslog-handler";
-
-        @Override
-        public void setup(ManagementClient managementClient, String containerId) throws Exception {
-
-            final int PORT = 9176;
-
-            logFile = new File(System.getProperty("java.io.tmpdir"), FILE_NAME);
-
-            if (logFile.exists()) {
-                logFile.delete();
-            }
-
-            // start and set syslog server
-            server = SyslogServer.getInstance("udp");
-            server.getConfig().setPort(PORT);
-            queue = new LinkedBlockingQueue<String>();
-            server.getConfig()
-                    .addEventHandler(new BlockedFileSyslogServerEventHandler(queue, logFile.getAbsolutePath(), false));
-            SyslogServer.getThreadedInstance("udp");
-
-            ModelNode op;
-
-            // nastavit logovani do syslog
-            final ModelNode compositeOp = new ModelNode();
-            compositeOp.get(OP).set(COMPOSITE);
-            compositeOp.get(OP_ADDR).setEmptyList();
-            ModelNode steps = compositeOp.get(STEPS);
-            op = new ModelNode();
-            op.get(OP).set(ADD);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(SYSLOG_HANDLER, SYSLOG_HANDLER_NAME);
-            op.get(FORMATTER).set(JSON_FORMATTER);
-            op.get(SYSLOG_FORMAT).set("RFC5424");
-            steps.add(op);
-            op = new ModelNode();
-            op.get(OP).set(ADD);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(SYSLOG_HANDLER, SYSLOG_HANDLER_NAME);
-            op.get(OP_ADDR).add(PROTOCOL, UDP);
-            op.get("port").set(PORT);
-            op.get("host").set("localhost");
-            steps.add(op);
-            applyUpdate(managementClient.getControllerClient(), compositeOp, false);
-
-            op = new ModelNode();
-            op.get(OP).set(ADD);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(LOGGER, AUDIT_LOG);
-            op.get(OP_ADDR).add(HANDLER, SYSLOG_HANDLER_NAME);
-            applyUpdate(managementClient.getControllerClient(), op, false);
-            op = new ModelNode();
-            op.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(LOGGER, AUDIT_LOG);
-            op.get(NAME).set(LOG_READ_ONLY);
-            op.get(VALUE).set("false");
-            applyUpdate(managementClient.getControllerClient(), op, false);
-
-        }
-
-        @Override
-        public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
-            // stop syslog server
-            SyslogServer.shutdown();
-
-            ModelNode op;
-
-            for (Long property : properties) {
-                op = new ModelNode();
-                op.get(OP).set(REMOVE);
-                op.get(OP_ADDR).add(SYSTEM_PROPERTY, property);
-                AuditLogToSyslogSetup.applyUpdate(managementClient.getControllerClient(), op, false);
-            }
-
-            op = new ModelNode();
-            op.get(OP).set(REMOVE);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(LOGGER, AUDIT_LOG);
-            op.get(OP_ADDR).add(HANDLER, SYSLOG_HANDLER_NAME);
-            applyUpdate(managementClient.getControllerClient(), op, false);
-
-            op = new ModelNode();
-            op.get(OP).set(REMOVE);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(SYSLOG_HANDLER, SYSLOG_HANDLER_NAME);
-            applyUpdate(managementClient.getControllerClient(), op, false);
-
-            op = new ModelNode();
-            op.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
-            op.get(OP_ADDR).add(CORE_SERVICE, MANAGEMENT);
-            op.get(OP_ADDR).add(ACCESS, AUDIT);
-            op.get(OP_ADDR).add(LOGGER, AUDIT_LOG);
-            op.get(NAME).set(LOG_READ_ONLY);
-            op.get(VALUE).set("false");
-            applyUpdate(managementClient.getControllerClient(), op, false);
-
-            if (logFile.exists()) {
-                logFile.delete();
-            }
-
-        }
-
-        static void applyUpdate(final ModelControllerClient client, ModelNode update, boolean allowFailure) throws Exception {
-            ModelNode result = client.execute(new OperationBuilder(update).build());
-            if (result.hasDefined("outcome") && (allowFailure || "success".equals(result.get("outcome").asString()))) {
-                if (result.hasDefined("result")) {
-                    System.out.println(result.get("result"));
-                }
-            } else if (result.hasDefined("failure-description")) {
-                throw new RuntimeException(result.get("failure-description").toString());
-            } else {
-                throw new RuntimeException("Operation not successful; outcome = " + result.get("outcome"));
-            }
-        }
-
+    /**
+     * Enables/disables the auditlog.
+     * 
+     * @throws Exception
+     */
+    private void setAuditlogEnabled(boolean value) throws Exception {
+        ModelNode op = Util.getWriteAttributeOperation(AuditLogToSyslogSetup.AUDIT_LOG_LOGGER_ADDR, ENABLED, value);
+        Utils.applyUpdate(op, managementClient.getControllerClient());
     }
 
 }
