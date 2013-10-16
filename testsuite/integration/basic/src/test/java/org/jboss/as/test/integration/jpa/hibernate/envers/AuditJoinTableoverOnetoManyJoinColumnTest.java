82b7d1bb951586523d203faa3ccd3c27927dab41
Josef Cacek <josef.cacek@gmail.com>
2013-10-16 09:12:31 +0200

    [WFLY-2313] fix for filename too long

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hibernate/envers/AuditJoinTableoverOnetoManyJoinColumnTest.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hibernate/envers/AuditJoinTableoverOnetoManyJoinColumnTest.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.integration.jpa.hibernate.envers.auditjointableoveronetomanyjoincolumntest;
+package org.jboss.as.test.integration.jpa.hibernate.envers;
 
 import java.util.List;
 
