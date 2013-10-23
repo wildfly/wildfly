a05de7f258f4c809fccd8396618c7c18d5e9dc06
kabir <kkhan@redhat.com>
2013-10-23 11:35:06 +0100

    [WFLY-2218] Put known versions in alphabetical order

--- subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
+++ subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
@@ -45,6 +45,7 @@ class KnownVersions {
         //no idea so we need to log a warning instead.
         //The core model versions are 1.2.0 for AS 7.1.2 and 1.3.0 for AS 7.1.3
         //7.2.x starts on core model version 1.4.0
+        //Beyond knowing if a version is pre-7.2.0 or 7.2.0 it currently isn't really important to match the exact AS version.
 
         //Keep this list in alphabetical and subsystem version order
 
@@ -55,9 +56,9 @@ class KnownVersions {
 
         addSubsystemVersion(map, "configadmin", "1.0.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "cmp", "1.0.0", CORE_MODEL_7_1_2);
+        addSubsystemVersion(map, "datasources", "1.1.0", CORE_MODEL_7_1_2);
         addSubsystemVersion(map, "ee", "1.0.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "jacorb", "1.1.0", CORE_MODEL_7_1_3);
-        addSubsystemVersion(map, "datasources", "1.1.0", CORE_MODEL_7_1_2);
         addSubsystemVersion(map, "ejb3", "1.1.0", CORE_MODEL_7_1_2);
         addSubsystemVersion(map, "ejb3", "1.2.0", CORE_MODEL_7_2_0);
         addSubsystemVersion(map, "infinispan", "1.3.0", CORE_MODEL_7_1_3);
