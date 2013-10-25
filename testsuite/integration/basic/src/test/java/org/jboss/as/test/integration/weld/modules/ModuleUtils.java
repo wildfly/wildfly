eee5a26a2e09003b5d6a8a10e144ee414a0e0a29
Jozef Hartinger <jharting@redhat.com>
2013-10-25 13:21:07 +0200

    WFLY-1746 Test for inter-module accessibility

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/modules/ModuleUtils.java
@@ -0,0 +1,131 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.weld.modules;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.jboss.jandex.Index;
+import org.jboss.jandex.IndexWriter;
+import org.jboss.jandex.Indexer;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.xnio.IoUtils;
+
+class ModuleUtils {
+
+    static void createTestModule(String moduleName, String moduleXml, Class<?>... classes) throws IOException {
+        File testModuleRoot = new File(getModulePath(), "test/" + moduleName);
+        if (testModuleRoot.exists()) {
+            throw new IllegalArgumentException(testModuleRoot + " already exists");
+        }
+        File file = new File(testModuleRoot, "main");
+        if (!file.mkdirs()) {
+            throw new IllegalArgumentException("Could not create " + file);
+        }
+
+        URL url = ModuleUtils.class.getResource(moduleXml);
+        if (url == null) {
+            throw new IllegalStateException("Could not find module.xml: " + moduleXml);
+        }
+        copyFile(new File(file, "module.xml"), url.openStream());
+
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, moduleName + ".jar");
+        jar.addClasses(classes);
+        jar.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+
+        Indexer indexer = new Indexer();
+        for (Class<?> clazz : classes) {
+            try (final InputStream resource = ModuleUtils.class.getResourceAsStream(clazz.getSimpleName() + ".class")) {
+                indexer.index(resource);
+            }
+        }
+
+        Index index = indexer.complete();
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        IndexWriter writer = new IndexWriter(data);
+        writer.write(index);
+        jar.addAsManifestResource(new ByteArrayAsset(data.toByteArray()), "jandex.idx");
+        FileOutputStream jarFile = new FileOutputStream(new File(file, moduleName + ".jar"));
+        try {
+            jar.as(ZipExporter.class).exportTo(jarFile);
+        } finally {
+            jarFile.flush();
+            jarFile.close();
+        }
+
+    }
+
+    private static void copyFile(File target, InputStream src) throws IOException {
+        final BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(target));
+        try {
+            int i = src.read();
+            while (i != -1) {
+                out.write(i);
+                i = src.read();
+            }
+        } finally {
+            IoUtils.safeClose(out);
+        }
+    }
+
+    static File getModulePath() {
+        String modulePath = System.getProperty("module.path", null);
+        if (modulePath == null) {
+            String jbossHome = System.getProperty("jboss.home", null);
+            if (jbossHome == null) {
+                throw new IllegalStateException("Neither -Dmodule.path nor -Djboss.home were set");
+            }
+            modulePath = jbossHome + File.separatorChar + "modules";
+        } else {
+            modulePath = modulePath.split(File.pathSeparator)[0];
+        }
+        File moduleDir = new File(modulePath);
+        if (!moduleDir.exists()) {
+            throw new IllegalStateException("Determined module path does not exist");
+        }
+        if (!moduleDir.isDirectory()) {
+            throw new IllegalStateException("Determined module path is not a dir");
+        }
+        return moduleDir;
+    }
+
+    static void deleteRecursively(File file) {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                for (String name : file.list()) {
+                    deleteRecursively(new File(file, name));
+                }
+            }
+            file.delete();
+        }
+    }
+}
