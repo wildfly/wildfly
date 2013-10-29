4794523afa0d6c1eb8e1c41aa9feb1680d5f4bec
Emanuel Muckenhuber <emuckenh@redhat.com>
2013-10-29 15:37:25 +0100

    [WFLY-2411] fix marshalling of authentication=users

--- domain-management/src/main/java/org/jboss/as/domain/management/parsing/ManagementXml.java
+++ domain-management/src/main/java/org/jboss/as/domain/management/parsing/ManagementXml.java
@@ -2922,7 +2922,7 @@ public class ManagementXml {
             PropertiesAuthenticationResourceDefinition.PATH.marshallAsAttribute(properties, writer);
             PropertiesAuthenticationResourceDefinition.RELATIVE_TO.marshallAsAttribute(properties, writer);
             PropertiesAuthenticationResourceDefinition.PLAIN_TEXT.marshallAsAttribute(properties, writer);
-        } else if (authentication.hasDefined(USERS)) {
+        } else if (authentication.has(USERS)) {
             ModelNode userDomain = authentication.get(USERS);
             ModelNode users = userDomain.hasDefined(USER) ? userDomain.require(USER) : new ModelNode().setEmptyObject();
             writer.writeStartElement(Element.USERS.getLocalName());
