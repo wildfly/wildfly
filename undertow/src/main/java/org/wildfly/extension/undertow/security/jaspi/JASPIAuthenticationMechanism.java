c1ad1cc40300e1b7d5ec06e8bc3cfc2ae9ad34c7
Pedro Igor <pigor.craveiro@gmail.com>
2013-11-06 16:16:10 -0200

    [WFLY-531] - JSR-196 JASPIC 1.1 Support.

---
+++ undertow/src/main/java/org/wildfly/extension/undertow/security/jaspi/JASPIAuthenticationMechanism.java
@@ -0,0 +1,194 @@
+package org.wildfly.extension.undertow.security.jaspi;
+
+import io.undertow.security.api.AuthenticationMechanism;
+import io.undertow.security.api.SecurityContext;
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.PasswordCredential;
+import io.undertow.server.ExchangeCompletionListener;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.servlet.handlers.ServletRequestContext;
+import io.undertow.util.AttachmentKey;
+import org.jboss.security.SecurityContextAssociation;
+import org.jboss.security.SimplePrincipal;
+import org.jboss.security.auth.callback.JBossCallbackHandler;
+import org.jboss.security.auth.message.GenericMessageInfo;
+import org.jboss.security.plugins.auth.JASPIServerAuthenticationManager;
+import org.wildfly.extension.undertow.security.AccountImpl;
+
+import javax.security.auth.Subject;
+import javax.security.auth.message.AuthException;
+import javax.security.auth.message.callback.CallerPrincipalCallback;
+import javax.security.auth.message.callback.GroupPrincipalCallback;
+import javax.security.auth.message.callback.PasswordValidationCallback;
+import javax.servlet.ServletRequest;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.wildfly.extension.undertow.UndertowLogger.*;
+import static org.wildfly.extension.undertow.UndertowMessages.*;
+
+/**
+ * <p>{@link AuthenticationMechanism} implementation that enables JASPI-based authentication.</p>
+ *
+ * @author Pedro Igor
+ * @author <a href="mailto:sguilhen@redhat.com">Stefan Guilhen</a>
+ */
+public class JASPIAuthenticationMechanism implements AuthenticationMechanism {
+
+    private static final String JASPI_HTTP_SERVLET_LAYER = "HttpServlet";
+    private static final String MECHANISM_NAME = "JASPI";
+
+    public static final AttachmentKey<HttpServerExchange> HTTP_SERVER_EXCHANGE_ATTACHMENT_KEY = AttachmentKey.create(HttpServerExchange.class);
+    public static final AttachmentKey<SecurityContext> SECURITY_CONTEXT_ATTACHMENT_KEY = AttachmentKey.create(SecurityContext.class);
+
+    private final String securityDomain;
+
+    public JASPIAuthenticationMechanism(final String securityDomain) {
+        this.securityDomain = securityDomain;
+    }
+
+    @Override
+    public AuthenticationMechanismOutcome authenticate(final HttpServerExchange exchange, final SecurityContext securityContext) {
+        final ServletRequestContext requestContext = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY);
+        final JASPIServerAuthenticationManager sam = createJASPIAuthenticationManager();
+        final GenericMessageInfo messageInfo = createMessageInfo(exchange, securityContext);
+        final String applicationIdentifier = buildApplicationIdentifier(requestContext);
+        final JASPICallbackHandler cbh = new JASPICallbackHandler();
+
+        ROOT_LOGGER.debugf("validateRequest for layer [%s] and applicationContextIdentifier [%s]", JASPI_HTTP_SERVLET_LAYER, applicationIdentifier);
+
+        if (sam.isValid(messageInfo, new Subject(), JASPI_HTTP_SERVLET_LAYER, applicationIdentifier, cbh)) {
+            Account account = securityContext.getAuthenticatedAccount();
+
+            // if the there is no account at this time, we check if the JASPI authentication have populated the callbackhandler
+            // with user information.
+            if (account == null) {
+                ROOT_LOGGER.debug("Creating account with credentials from JASPI callbackhandler.");
+                account = createAccount(cbh);
+                securityContext.authenticationComplete(account, MECHANISM_NAME);
+            } else {
+                ROOT_LOGGER.debug("Account already setup from JASPI modules.");
+            }
+
+            if (account == null) {
+                securityContext.authenticationFailed("JASPI authentication failed.", MECHANISM_NAME);
+            }
+        }
+
+        secureResponse(exchange, securityContext, sam, messageInfo, cbh);
+
+        return securityContext.getAuthenticatedAccount() != null ? AuthenticationMechanismOutcome.AUTHENTICATED : AuthenticationMechanismOutcome.NOT_AUTHENTICATED;
+    }
+
+    @Override
+    public ChallengeResult sendChallenge(final HttpServerExchange exchange, final SecurityContext securityContext) {
+        return new ChallengeResult(true);
+    }
+
+    private boolean isSecureResponse(final ServletRequestContext attachment, final SecurityContext securityContext) {
+        return !wasAuthExceptionThrown() && (securityContext.getAuthenticatedAccount() != null || !isMandatory(attachment));
+    }
+
+    private boolean wasAuthExceptionThrown() {
+        return SecurityContextAssociation.getSecurityContext().getData().get(AuthException.class.getName()) != null;
+    }
+
+    private JASPIServerAuthenticationManager createJASPIAuthenticationManager() {
+        return new JASPIServerAuthenticationManager(this.securityDomain, new JBossCallbackHandler());
+    }
+
+    private String buildApplicationIdentifier(final ServletRequestContext attachment) {
+        ServletRequest servletRequest = attachment.getServletRequest();
+        return servletRequest.getLocalName() + " " + servletRequest.getServletContext().getContextPath();
+    }
+
+    private GenericMessageInfo createMessageInfo(final HttpServerExchange exchange, final SecurityContext securityContext) {
+        ServletRequestContext servletRequestContext = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY);
+
+        GenericMessageInfo messageInfo = new GenericMessageInfo();
+
+        messageInfo.setRequestMessage(servletRequestContext.getServletRequest());
+        messageInfo.setResponseMessage(servletRequestContext.getServletResponse());
+
+        messageInfo.getMap().put("javax.security.auth.message.MessagePolicy.isMandatory", isMandatory(servletRequestContext).toString());
+
+        // additional context data, useful to provide access to Undertow resources during the modules processing
+        messageInfo.getMap().put(SECURITY_CONTEXT_ATTACHMENT_KEY, securityContext);
+        messageInfo.getMap().put(HTTP_SERVER_EXCHANGE_ATTACHMENT_KEY, exchange);
+
+        return messageInfo;
+    }
+
+    private Account createAccount(final JASPICallbackHandler cbh) {
+        if (cbh == null) {
+            throw MESSAGES.nullParamter("JASPICallbackHandler");
+        }
+
+        CallerPrincipalCallback cpc = cbh.getCallerPrincipalCallback();
+
+        if (cpc == null) {
+            throw MESSAGES.nullParamter("CallerPrincipalCallback from JASPI CallbackHandler");
+        }
+
+        String userName;
+
+        if (cpc.getPrincipal() != null) {
+            userName = cpc.getPrincipal().getName();
+        } else {
+            userName = cpc.getName();
+        }
+
+        if (userName == null) {
+            throw MESSAGES.nullParamter("Username from CallerPrincipalCallback");
+        }
+
+        PasswordValidationCallback pcb = cbh.getPasswordValidationCallback();
+        PasswordCredential credential = null;
+
+        if (pcb != null && pcb.getPassword() != null) {
+            credential = new PasswordCredential(pcb.getPassword());
+        }
+
+        GroupPrincipalCallback gpc = cbh.getGroupPrincipalCallback();
+        Set<String> groups = null;
+
+        if (gpc != null && gpc.getGroups() != null) {
+            groups = new HashSet<String>(Arrays.asList(gpc.getGroups()));
+        }
+
+        return new AccountImpl(new SimplePrincipal(userName), groups, credential);
+    }
+
+    private void secureResponse(final HttpServerExchange exchange, final SecurityContext securityContext, final JASPIServerAuthenticationManager sam, final GenericMessageInfo messageInfo, final JASPICallbackHandler cbh) {
+        // we add the a response listener to properly invoke the secureResponse, after processing the destination
+        exchange.addExchangeCompleteListener(new ExchangeCompletionListener() {
+            @Override
+            public void exchangeEvent(final HttpServerExchange exchange, final NextListener nextListener) {
+                ServletRequestContext requestContext = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY);
+                String applicationIdentifier = buildApplicationIdentifier(requestContext);
+
+                if (isSecureResponse(requestContext, securityContext)) {
+                    ROOT_LOGGER.debugf("secureResponse for layer [%s] and applicationContextIdentifier [%s].", JASPI_HTTP_SERVLET_LAYER, applicationIdentifier);
+                    sam.secureResponse(messageInfo, new Subject(), JASPI_HTTP_SERVLET_LAYER, applicationIdentifier, cbh);
+                }
+                nextListener.proceed();
+            }
+        });
+    }
+    /**
+     * <p>The authentication is mandatory if the servlet has http constraints (eg.: {@link
+     * javax.servlet.annotation.HttpConstraint}).</p>
+     *
+     * @param attachment
+     * @return
+     */
+    private Boolean isMandatory(final ServletRequestContext attachment) {
+        return attachment.getCurrentServlet() != null
+                && attachment.getCurrentServlet().getManagedServlet() != null
+                && attachment.getCurrentServlet().getManagedServlet().getServletInfo() != null
+                && attachment.getCurrentServlet().getManagedServlet().getServletInfo().getServletSecurityInfo() != null
+                && attachment.getCurrentServlet().getManagedServlet().getServletInfo().getServletSecurityInfo().getRolesAllowed() != null
+                && !attachment.getCurrentServlet().getManagedServlet().getServletInfo().getServletSecurityInfo().getRolesAllowed().isEmpty();
+    }
+}
