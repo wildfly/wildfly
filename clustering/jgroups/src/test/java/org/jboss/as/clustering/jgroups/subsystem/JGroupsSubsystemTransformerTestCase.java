15cb5891c06286a0b9b3c3483cdd8da69662aec0
Radoslav Husar <radosoft@gmail.com>
2013-11-13 16:18:26 +0100

    [WFLY-2236] JGroups subsystem transformers for 7.2.0

--- clustering/jgroups/src/test/java/org/jboss/as/clustering/jgroups/subsystem/JGroupsSubsystemTransformerTestCase.java
+++ clustering/jgroups/src/test/java/org/jboss/as/clustering/jgroups/subsystem/JGroupsSubsystemTransformerTestCase.java
@@ -1,24 +1,24 @@
-/*
-* JBoss, Home of Professional Open Source.
-* Copyright 2011, Red Hat Middleware LLC, and individual contributors
-* as indicated by the @author tags. See the copyright.txt file in the
-* distribution for a full listing of individual contributors.
-*
-* This is free software; you can redistribute it and/or modify it
-* under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version 2.1 of
-* the License, or (at your option) any later version.
-*
-* This software is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this software; if not, write to the Free
-* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
-*/
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.as.clustering.jgroups.subsystem;
 
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
@@ -26,8 +26,6 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUB
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
 import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
 
 import org.jboss.as.controller.ModelVersion;
 import org.jboss.as.controller.PathAddress;
@@ -49,29 +47,37 @@ import org.junit.runner.RunWith;
 /**
  * Test cases for transformers used in the JGroups subsystem.
  *
+ * AS version / model version / schema version overview
+ * 7.1.1 / 1.1.0 / 1_1
+ * 7.1.2 / 1.1.0 / 1_1
+ * 7.1.3 / 1.1.0 / 1_1
+ * 7.2.0 / 1.2.0 / 1_1
+ * 8.0.0 / 2.0.0 / 2_0
+ *
  * @author <a href="tomaz.cerar@redhat.com">Tomaz Cerar</a>
  * @author Richard Achmatowicz (c) 2011 Red Hat Inc.
+ * @author Radoslav Husar
  */
-
 @RunWith(BMUnitRunner.class)
 public class JGroupsSubsystemTransformerTestCase extends OperationTestCaseBase {
 
-    protected String getSubsystemXml() throws IOException {
-        return "jgroups-transformer_1_1.xml";
-    }
-
     @Test
     public void testTransformerAS712() throws Exception {
-        testTransformer_1_1_0(ModelTestControllerVersion.V7_1_2_FINAL);
+        testTransformer_1_x(ModelTestControllerVersion.V7_1_2_FINAL, ModelVersion.create(1, 1, 0));
     }
 
     @Test
     public void testTransformerAS713() throws Exception {
-        testTransformer_1_1_0(ModelTestControllerVersion.V7_1_3_FINAL);
+        testTransformer_1_x(ModelTestControllerVersion.V7_1_3_FINAL, ModelVersion.create(1, 1, 0));
+    }
+
+    @Test
+    public void testTransformerAS720() throws Exception {
+        testTransformer_1_x(ModelTestControllerVersion.V7_2_0_FINAL, ModelVersion.create(1, 2, 0));
     }
 
     /**
-     * Tests transformation of model from 1.1.1 version into 1.1.0 version.
+     * Tests transformation of model from 2.0.0 version into 1.1.0 or 1.2.0 version.
      *
      * This test does not pass because of an error in the 1.1.0 model:
      * The model entry 'protocols' in /subsystem=jgroups/stack=* is used
@@ -81,10 +87,10 @@ public class JGroupsSubsystemTransformerTestCase extends OperationTestCaseBase {
      *
      * @throws Exception
      */
-    private void testTransformer_1_1_0(ModelTestControllerVersion controllerVersion) throws Exception {
-        ModelVersion version = ModelVersion.create(1, 1, 0);
+    private void testTransformer_1_x(ModelTestControllerVersion controllerVersion, ModelVersion version) throws Exception {
+        String subsystemXml = readResource("jgroups-transformer_1_1.xml");
         KernelServicesBuilder builder = createKernelServicesBuilder(AdditionalInitialization.MANAGEMENT)
-                .setSubsystemXmlResource(getSubsystemXml());
+                .setSubsystemXml(subsystemXml);
         builder.createLegacyKernelServicesBuilder(null, controllerVersion, version)
                 .addMavenResourceURL("org.jboss.as:jboss-as-clustering-jgroups:" + controllerVersion.getMavenGavVersion());
 
@@ -140,6 +146,11 @@ public class JGroupsSubsystemTransformerTestCase extends OperationTestCaseBase {
         testRejectExpressions_1_1_0(ModelTestControllerVersion.V7_1_3_FINAL);
     }
 
+    @Test
+    public void testRejectExpressionsAS720() throws Exception {
+        testDiscards_1_2_0(ModelTestControllerVersion.V7_2_0_FINAL);
+    }
+
     /**
      * Tests rejection of expressions in 1.1.0 model.
      *
@@ -186,6 +197,38 @@ public class JGroupsSubsystemTransformerTestCase extends OperationTestCaseBase {
                                 .append("property"),
                             new FailedOperationTransformationConfig.RejectExpressionsConfig(VALUE))
                 );
+    }
+
+    /**
+     * Tests that relay is discarded, no need to check channel.
+     *
+     * @throws Exception
+     */
+    private void testDiscards_1_2_0(ModelTestControllerVersion controllerVersion) throws Exception {
+        KernelServicesBuilder builder = createKernelServicesBuilder(AdditionalInitialization.MANAGEMENT);
+        ModelVersion version_1_2_0 = ModelVersion.create(1, 2, 0);
+        builder.createLegacyKernelServicesBuilder(null, controllerVersion, version_1_2_0)
+                .addMavenResourceURL("org.jboss.as:jboss-as-clustering-jgroups:" + controllerVersion.getMavenGavVersion())
+                .dontPersistXml();
+
+        KernelServices mainServices = builder.build();
+        Assert.assertTrue(mainServices.isSuccessfulBoot());
+        KernelServices legacyServices = mainServices.getLegacyServices(version_1_2_0);
+        Assert.assertNotNull(legacyServices);
+        Assert.assertTrue(legacyServices.isSuccessfulBoot());
+
+        // Use the real xml with expressions for testing all the attributes
+        PathAddress subsystemAddress = PathAddress.pathAddress(PathElement.pathElement(SUBSYSTEM, JGroupsExtension.SUBSYSTEM_NAME));
+        ModelTestUtils.checkFailedTransformedBootOperations(
+                mainServices,
+                version_1_2_0,
+                builder.parseXmlResource("jgroups-transformer_2_0-reject.xml"),
+                new FailedOperationTransformationConfig().
+                        addFailedAttribute(
+                                subsystemAddress.append("stack").append("relay"),
+                                FailedOperationTransformationConfig.DISCARDED_RESOURCE)
+
+        );
     }
 
 }
