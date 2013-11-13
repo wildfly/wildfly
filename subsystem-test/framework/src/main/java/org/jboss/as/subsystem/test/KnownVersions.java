886acf2dfd9c42dd3d73268b73b5c8ef71d15260
Radoslav Husar <radosoft@gmail.com>
2013-11-13 10:36:51 +0100

    [WFLY-2236] Add legacy 1.2.0.dmr

--- subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
+++ subsystem-test/framework/src/main/java/org/jboss/as/subsystem/test/KnownVersions.java
@@ -1,25 +1,23 @@
-/*
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
  *
- *  JBoss, Home of Professional Open Source.
- *  Copyright 2013, Red Hat, Inc., and individual contributors
- *  as indicated by the @author tags. See the copyright.txt file in the
- *  distribution for a full listing of individual contributors.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- *  This is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation; either version 2.1 of
- *  the License, or (at your option) any later version.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- *  This software is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this software; if not, write to the Free
- *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
- * /
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.jboss.as.subsystem.test;
 
@@ -71,10 +69,11 @@ class KnownVersions {
         addSubsystemVersion(map, "jdr", "1.0.0", CORE_MODEL_7_1_2);
         addSubsystemVersion(map, "jdr", "1.1.0", CORE_MODEL_7_2_0);
         addSubsystemVersion(map, "jsr77", "1.0.0", CORE_MODEL_7_2_0);
-        addSubsystemVersion(map, "jgroups", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "jmx", "1.0.0", CORE_MODEL_7_1_2);
         addSubsystemVersion(map, "jmx", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "jgroups", "1.1.0", CORE_MODEL_7_1_3);
+        addSubsystemVersion(map, "jgroups", "1.2.0", CORE_MODEL_7_2_0);
+        addSubsystemVersion(map, "jgroups", "2.0.0", CORE_MODEL_8_0_0);
         addSubsystemVersion(map, "jpa", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "logging", "1.1.0", CORE_MODEL_7_1_3);
         addSubsystemVersion(map, "logging", "1.2.0", CORE_MODEL_7_2_0);
