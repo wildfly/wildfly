b11a50b2e2adbcc5f6cf8c11868a177ae4bd7c04
Darran Lofthouse <darran.lofthouse@jboss.com>
2013-11-19 13:49:58 +0000

    [WFLY-475] Firstly refactoring of ProtocolConnectionUtils to enable a ProtocolTimeoutHandler to be supplied to handle timeouts.
    This allows clients to supply a ProtocolTimeoutHandler that can track all user 'think time' during the connection establishment process.

--- protocol/src/main/java/org/jboss/as/protocol/ProtocolConnectionUtils.java
+++ protocol/src/main/java/org/jboss/as/protocol/ProtocolConnectionUtils.java
@@ -45,7 +45,6 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Protocol Connection utils.
@@ -93,34 +92,24 @@ public class ProtocolConnectionUtils {
      * @throws IOException
      */
     public static Connection connectSync(final ProtocolConnectionConfiguration configuration) throws IOException {
-        final CallbackHandler handler = configuration.getCallbackHandler();
-        final CallbackHandler actualHandler = handler != null ? handler : new AnonymousCallbackHandler();
-        final WrapperCallbackHandler wrapperHandler = new WrapperCallbackHandler(actualHandler);
-        final IoFuture<Connection> future = connect(wrapperHandler, configuration);
         long timeoutMillis = configuration.getConnectionTimeout();
-        IoFuture.Status status = future.await(timeoutMillis, TimeUnit.MILLISECONDS);
-        while (status == IoFuture.Status.WAITING) {
-            if (wrapperHandler.isInCall()) {
-                // If there is currently an interaction with the user just wait again.
-                status = future.await(timeoutMillis, TimeUnit.MILLISECONDS);
-            } else {
-                long lastInteraction = wrapperHandler.getCallFinished();
-                if (lastInteraction > 0) {
-                    long now = System.currentTimeMillis();
-                    long timeSinceLast = now - lastInteraction;
-                    if (timeSinceLast < timeoutMillis) {
-                        // As this point we are setting the timeout based on the time of the last interaction
-                        // with the user, if there is any time left we will wait for that time but dont wait for
-                        // a full timeout.
-                        status = future.await(timeoutMillis - timeSinceLast, TimeUnit.MILLISECONDS);
-                    } else {
-                        status = null;
-                    }
-                } else {
-                    status = null; // Just terminate status processing.
-                }
-            }
+        CallbackHandler handler = configuration.getCallbackHandler();
+        final CallbackHandler actualHandler;
+        ProtocolTimeoutHandler timeoutHandler = configuration.getTimeoutHandler();
+        // Note: If a client supplies a ProtocolTimeoutHandler it is taking on full responsibility for timeout management.
+        if (timeoutHandler == null) {
+            GeneralTimeoutHandler defaultTimeoutHandler = new GeneralTimeoutHandler();
+            // No point wrapping our AnonymousCallbackHandler.
+            actualHandler = handler != null ? new WrapperCallbackHandler(defaultTimeoutHandler, handler)
+                    : new AnonymousCallbackHandler();
+            timeoutHandler = defaultTimeoutHandler;
+        } else {
+            actualHandler = handler != null ? handler : new AnonymousCallbackHandler();
         }
+
+        final IoFuture<Connection> future = connect(actualHandler, configuration);
+
+        IoFuture.Status status = timeoutHandler.await(future, timeoutMillis);
 
         if (status == IoFuture.Status.DONE) {
             return future.get();
@@ -215,35 +204,40 @@ public class ProtocolConnectionUtils {
 
     private static final class WrapperCallbackHandler implements CallbackHandler {
 
-        private volatile boolean inCall = false;
-
-        private volatile long callFinished = -1;
-
+        private final GeneralTimeoutHandler timeoutHandler;
         private final CallbackHandler wrapped;
 
-        WrapperCallbackHandler(final CallbackHandler toWrap) {
+        WrapperCallbackHandler(final GeneralTimeoutHandler timeoutHandler, final CallbackHandler toWrap) {
+            this.timeoutHandler = timeoutHandler;
             this.wrapped = toWrap;
         }
 
-        public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-            inCall = true;
+        public void handle(final Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
             try {
-                wrapped.handle(callbacks);
-            } finally {
-                // Set the time first so if a read is made between these two calls it will say inCall=true until
-                // callFinished is set.
-                callFinished = System.currentTimeMillis();
-                inCall = false;
+                timeoutHandler.suspendAndExecute(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            wrapped.handle(callbacks);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        } catch (UnsupportedCallbackException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof IOException) {
+                    throw (IOException) e.getCause();
+                } else if (e.getCause() instanceof UnsupportedCallbackException) {
+                    throw (UnsupportedCallbackException) e.getCause();
+                }
+                throw e;
             }
         }
 
-        boolean isInCall() {
-            return inCall;
-        }
-
-        long getCallFinished() {
-            return callFinished;
-        }
     }
 
     private static final class AnonymousCallbackHandler implements CallbackHandler {
