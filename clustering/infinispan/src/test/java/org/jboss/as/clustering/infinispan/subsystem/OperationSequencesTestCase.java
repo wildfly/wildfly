3d0f16bd22a612f6fa950445618fd518cb8c2f9e
Tomaz Cerar <tomaz.cerar@gmail.com>
2013-11-25 00:00:29 +0100

    Revert "Revert "Remove some deps leftovers & testsuite cleanup""
    This reverts commit b05a7ff4d35cedc85d5ac82e611d44b5c7464f1e.

--- clustering/infinispan/src/test/java/org/jboss/as/clustering/infinispan/subsystem/OperationSequencesTestCase.java
+++ clustering/infinispan/src/test/java/org/jboss/as/clustering/infinispan/subsystem/OperationSequencesTestCase.java
@@ -88,7 +88,6 @@ public class OperationSequencesTestCase extends OperationTestCaseBase {
             targetLocation="AT ENTRY",
             action="$1.setRollbackOnly()")
     public void testCacheContainerRemoveRollback() throws Exception {
-
         // Parse and install the XML into the controller
         String subsystemXml = getSubsystemXml() ;
         KernelServices servicesA = createKernelServicesBuilder(null).setSubsystemXml(subsystemXml).build();
