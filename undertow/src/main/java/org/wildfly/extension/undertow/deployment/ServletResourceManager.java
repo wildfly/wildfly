95a072c1674837905e4fec319dc9ba7c82382fda
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-12-03 08:21:42 +0100

    Revert "Use virtual file based resource manager to make sure deployment overlays work correctly with Undertow"
    This reverts commit bd7fa6ed03188a74c89663826d7e6b77df64d8df.
    
    This caused problems JSP, as getResource() on a jar would return the mounted directory, rather than the jar file.

--- undertow/src/main/java/org/wildfly/extension/undertow/deployment/ServletResourceManager.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/deployment/ServletResourceManager.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
 
+import io.undertow.server.handlers.resource.FileResourceManager;
 import io.undertow.server.handlers.resource.Resource;
 import io.undertow.server.handlers.resource.ResourceChangeListener;
 import io.undertow.server.handlers.resource.ResourceManager;
@@ -17,11 +18,11 @@ import org.jboss.vfs.VirtualFile;
  */
 public class ServletResourceManager implements ResourceManager {
 
-    private final VirtualFileResourceManager deploymentResourceManager;
+    private final FileResourceManager deploymentResourceManager;
     private final Collection<VirtualFile> overlays;
 
     public ServletResourceManager(final VirtualFile resourcesRoot, final Collection<VirtualFile> overlays) throws IOException {
-        deploymentResourceManager = new VirtualFileResourceManager(resourcesRoot, 1024 * 1024);
+        deploymentResourceManager = new FileResourceManager(resourcesRoot.getPhysicalFile(), 1024 * 1024);
         this.overlays = overlays;
     }
 
