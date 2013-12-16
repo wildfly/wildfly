a13514c8f1be611f253c02d20874bf00d5c92151
Ales Justin <ales.justin@gmail.com>
2013-12-16 17:10:30 +0100

    Add some ROOT.war test. * https://github.com/wildfly/wildfly/pull/5612

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/web/rootwar/RootWarTestCase.java
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.web.rootwar;
+
+import java.net.URL;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Add some ROOT.war tests.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+public class RootWarTestCase {
+    private static final String INDEX_HTML = "<html><body>Test!</body></html>";
+
+    @Deployment
+    public static WebArchive getDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "ROOT.war");
+        war.add(new StringAsset(INDEX_HTML), "index.html");
+        return war;
+    }
+
+    @RunAsClient
+    @Test
+    public void testPing(@ArquillianResource URL url) throws Exception {
+        final DefaultHttpClient client = new DefaultHttpClient();
+        try {
+            HttpGet httpget = new HttpGet(url.toExternalForm() + "/");
+
+            HttpResponse response = client.execute(httpget);
+            HttpEntity entity = response.getEntity();
+
+            StatusLine statusLine = response.getStatusLine();
+            assertEquals(200, statusLine.getStatusCode());
+            assertEquals(INDEX_HTML, EntityUtils.toString(entity));
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+    }
+}
