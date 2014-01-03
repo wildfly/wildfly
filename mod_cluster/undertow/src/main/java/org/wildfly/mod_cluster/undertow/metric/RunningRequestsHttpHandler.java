a1ee2ebbce62cba498ec09b4be51165cfd62321d
Radoslav Husar <radosoft@gmail.com>
2014-01-03 15:26:18 +0100

    [WFLY-2171] Rework running requests metric into a HttpHandler

--- mod_cluster/undertow/src/main/java/org/wildfly/mod_cluster/undertow/metric/RunningRequestsHttpHandler.java
+++ mod_cluster/undertow/src/main/java/org/wildfly/mod_cluster/undertow/metric/RunningRequestsHttpHandler.java
@@ -22,35 +22,54 @@
 
 package org.wildfly.mod_cluster.undertow.metric;
 
+import io.undertow.server.ExchangeCompletionListener;
+import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.servlet.api.ThreadSetupAction;
 import org.wildfly.mod_cluster.undertow.metric.jdk8backported.LongAdder;
 
 /**
- * {@link ThreadSetupAction} implementation that counts number of active / running requests to replace the busyness
+ * {@link HttpHandler} implementation that counts number of active / running requests to replace the busyness
  * metric.
  *
  * @author Radoslav Husar
  * @version Aug 2013
  * @since 8.0
  */
-public class RunningRequestsThreadSetupAction implements ThreadSetupAction {
+public class RunningRequestsHttpHandler implements HttpHandler {
 
     private static final LongAdder runningCount = new LongAdder();
 
+    private final HttpHandler wrappedHandler;
+
+    public RunningRequestsHttpHandler(final HttpHandler handler) {
+        this.wrappedHandler = handler;
+    }
+
+    /**
+     * Increments the counter and registers a listener to decrement the counter upon exchange complete event.
+     *
+     * @param exchange
+     * @throws Exception
+     */
     @Override
-    public Handle setup(final HttpServerExchange exchange) {
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
         runningCount.increment();
 
-        return new Handle() {
+        exchange.addExchangeCompleteListener(new ExchangeCompletionListener() {
             @Override
-            public void tearDown() {
+            public void exchangeEvent(HttpServerExchange exchange, NextListener nextListener) {
                 runningCount.decrement();
+
+                // Proceed to next listener must be called!
+                nextListener.proceed();
             }
-        };
+        });
+
+        wrappedHandler.handleRequest(exchange);
     }
 
     public static int getRunningRequestCount() {
         return runningCount.intValue();
     }
+
 }
