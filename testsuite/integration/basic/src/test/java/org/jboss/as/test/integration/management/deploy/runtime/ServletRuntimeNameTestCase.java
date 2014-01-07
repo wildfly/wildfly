d89b495655dba5e39df11d3cbd3e28586da51a85
Brian Stansberry <brian.stansberry@redhat.com>
2014-01-06 20:31:32 -0600

    [WFLY-2061] Use runtime name when finding deployment service to read status

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/management/deploy/runtime/ServletRuntimeNameTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/management/deploy/runtime/ServletRuntimeNameTestCase.java
@@ -1,13 +1,44 @@
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright 2014, Red Hat, Inc. and individual contributors
+* as indicated by the @author tags. See the copyright.txt file in the
+* distribution for a full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
 package org.jboss.as.test.integration.management.deploy.runtime;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_ATTRIBUTE_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESULT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
+
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.as.controller.client.OperationBuilder;
 import org.jboss.as.controller.client.helpers.Operations;
 import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
+import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.test.integration.common.HttpRequest;
 import org.jboss.as.test.integration.management.deploy.runtime.servlet.Servlet;
 import org.jboss.as.test.integration.management.util.ModelUtil;
@@ -129,6 +160,18 @@ public class ServletRuntimeNameTestCase extends AbstractRuntimeTestCase {
                 + Servlet.URL_PATTERN;
         String res = HttpRequest.get(url, 2, TimeUnit.SECONDS);
         Assert.assertEquals(Servlet.SUCCESS, res);
+    }
+
+    /** WFLY-2061 test */
+    @Test
+    public void testDeploymentStatus() throws IOException {
+        PathAddress address = PathAddress.pathAddress(PathElement.pathElement(ModelDescriptionConstants.DEPLOYMENT, DEPLOYMENT_NAME));
+        final ModelNode operation = Util.createEmptyOperation(READ_ATTRIBUTE_OPERATION, address);
+        operation.get(NAME).set("status");
+
+        final ModelNode outcome = controllerClient.execute(operation);
+        Assert.assertEquals(SUCCESS, outcome.get(OUTCOME).asString());
+        Assert.assertEquals("OK", outcome.get(RESULT).asString());
     }
 
 }
