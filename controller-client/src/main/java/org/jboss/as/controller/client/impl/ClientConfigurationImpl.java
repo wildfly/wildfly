d54676ab699707d1b593b3b324695dc5414e2dc0
Jan Martiska <jmartisk@redhat.com>
2014-01-17 14:31:49 +0100

    [WFLY-2773] don't translate IP address to host name in ClientConfigurationImpl

--- controller-client/src/main/java/org/jboss/as/controller/client/impl/ClientConfigurationImpl.java
+++ controller-client/src/main/java/org/jboss/as/controller/client/impl/ClientConfigurationImpl.java
@@ -143,26 +143,26 @@ public class ClientConfigurationImpl implements ModelControllerClientConfigurati
     }
 
     public static ModelControllerClientConfiguration create(final InetAddress address, final int port) {
-        return new ClientConfigurationImpl(address.getHostName(), port, null, null, null, createDefaultExecutor(), true, null);
+        return new ClientConfigurationImpl(address.getHostAddress(), port, null, null, null, createDefaultExecutor(), true, null);
     }
     public static ModelControllerClientConfiguration create(final String protocol, final InetAddress address, final int port) {
-        return new ClientConfigurationImpl(address.getHostName(), port, null, null, null, createDefaultExecutor(), true, protocol);
+        return new ClientConfigurationImpl(address.getHostAddress(), port, null, null, null, createDefaultExecutor(), true, protocol);
     }
 
     public static ModelControllerClientConfiguration create(final InetAddress address, final int port, final CallbackHandler handler){
-        return new ClientConfigurationImpl(address.getHostName(), port, handler, null, null, createDefaultExecutor(), true, null);
+        return new ClientConfigurationImpl(address.getHostAddress(), port, handler, null, null, createDefaultExecutor(), true, null);
     }
 
     public static ModelControllerClientConfiguration create(final String protocol,final InetAddress address, final int port, final CallbackHandler handler){
-        return new ClientConfigurationImpl(address.getHostName(), port, handler, null, null, createDefaultExecutor(), true, protocol);
+        return new ClientConfigurationImpl(address.getHostAddress(), port, handler, null, null, createDefaultExecutor(), true, protocol);
     }
 
     public static ModelControllerClientConfiguration create(final InetAddress address, final int port, final CallbackHandler handler, final Map<String, String> saslOptions){
-        return new ClientConfigurationImpl(address.getHostName(), port, handler, saslOptions, null, createDefaultExecutor(), true, null);
+        return new ClientConfigurationImpl(address.getHostAddress(), port, handler, saslOptions, null, createDefaultExecutor(), true, null);
     }
 
     public static ModelControllerClientConfiguration create(final String protocol, final InetAddress address, final int port, final CallbackHandler handler, final Map<String, String> saslOptions){
-        return new ClientConfigurationImpl(address.getHostName(), port, handler, saslOptions, null, createDefaultExecutor(), true, protocol);
+        return new ClientConfigurationImpl(address.getHostAddress(), port, handler, saslOptions, null, createDefaultExecutor(), true, protocol);
     }
 
     public static ModelControllerClientConfiguration create(final String hostName, final int port) throws UnknownHostException {
