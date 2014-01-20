9fd4991e842aeca078bace9c1108fffc0efeabcd
Jozef Hartinger <jharting@redhat.com>
2014-01-20 16:32:22 +0100

    WFLY-2761 Testcase

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/modules/deployment/StaticModuleToDeploymentVisibilityEarTest.java
@@ -0,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.weld.modules.deployment;
+
+import static org.jboss.as.test.integration.weld.modules.ModuleUtils.createTestModule;
+import static org.jboss.as.test.integration.weld.modules.ModuleUtils.deleteRecursively;
+import static org.jboss.as.test.integration.weld.modules.ModuleUtils.getModulePath;
+
+import java.io.File;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Verifies that a bean (FooImpl3) in the top-level deployment unit is visible from a static CDI-enabled module.
+ * At the same time beans from sub-deployments (FooImpl1, FooImpl2) are not visible.
+ *
+ * @author Jozef Hartinger
+ *
+ */
+@RunWith(Arquillian.class)
+public class StaticModuleToDeploymentVisibilityEarTest {
+
+    public static void doSetup() throws Exception {
+        tearDown();
+        createTestModule("foo", "foo-module.xml", ModuleBean.class, Foo.class);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        deleteRecursively(new File(getModulePath(), "test"));
+    }
+
+    @Deployment
+    public static Archive<?> getDeployment() throws Exception {
+        doSetup();
+        WebArchive war1 = ShrinkWrap.create(WebArchive.class)
+                .addClasses(StaticModuleToDeploymentVisibilityEarTest.class, FooImpl1.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        WebArchive war2 = ShrinkWrap.create(WebArchive.class)
+                .addClasses(FooImpl2.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        JavaArchive library = ShrinkWrap.create(JavaArchive.class)
+                .addClass(FooImpl3.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return ShrinkWrap.create(EnterpriseArchive.class)
+                .addAsModules(war1, war2)
+                .addAsLibrary(library)
+                .addAsManifestResource(new StringAsset("Dependencies: test.foo meta-inf\n"), "MANIFEST.MF");
+    }
+
+    @Inject
+    private ModuleBean alpha;
+
+    @Test
+    public void testBeanAccessibility() {
+        Assert.assertNotNull(alpha.getFoo());
+        Assert.assertTrue(alpha.getFoo() instanceof FooImpl3);
+    }
+}
