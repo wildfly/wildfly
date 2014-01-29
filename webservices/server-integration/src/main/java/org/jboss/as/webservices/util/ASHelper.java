e0694c257c9ae36f9babb6a3b1c1af1ea1d82b82
Alessio Soldano <asoldano@redhat.com>
2014-01-29 10:28:30 +0100

    [JBWS-3737] Build PolicyAttachmentStore when the first @PolicySets annotation is found
    Conflicts:
    	pom.xml

--- webservices/server-integration/src/main/java/org/jboss/as/webservices/util/ASHelper.java
+++ webservices/server-integration/src/main/java/org/jboss/as/webservices/util/ASHelper.java
@@ -21,6 +21,8 @@
  */
 package org.jboss.as.webservices.util;
 
+import static org.jboss.as.server.deployment.Attachments.DEPLOYMENT_ROOT;
+import static org.jboss.as.server.deployment.Attachments.RESOURCE_ROOTS;
 import static org.jboss.as.webservices.WSLogger.ROOT_LOGGER;
 import static org.jboss.as.webservices.util.DotNames.JAXWS_SERVICE_CLASS;
 import static org.jboss.as.webservices.util.DotNames.WEB_SERVICE_ANNOTATION;
@@ -39,9 +41,12 @@ import org.jboss.as.ee.component.EEModuleClassDescription;
 import org.jboss.as.ee.metadata.ClassAnnotationInformation;
 import org.jboss.as.server.CurrentServiceContainer;
 import org.jboss.as.server.deployment.AttachmentKey;
+import org.jboss.as.server.deployment.AttachmentList;
 import org.jboss.as.server.deployment.Attachments;
 import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.EjbDeploymentMarker;
 import org.jboss.as.server.deployment.annotation.CompositeIndex;
+import org.jboss.as.server.deployment.module.ResourceRoot;
 import org.jboss.as.web.common.WarMetaData;
 import org.jboss.as.webservices.deployers.WebServiceAnnotationInfo;
 import org.jboss.as.webservices.deployers.WebServiceProviderAnnotationInfo;
@@ -343,5 +348,17 @@ public final class ASHelper {
             unit.putAttachment(WSAttachmentKeys.WS_REFREGISTRY, refRegistry);
         }
         return refRegistry;
+    }
+
+    public static List<ResourceRoot> getResourceRoots(DeploymentUnit unit) {
+        // wars define resource roots
+        AttachmentList<ResourceRoot> resourceRoots = unit.getAttachment(RESOURCE_ROOTS);
+        if (!unit.getName().endsWith(".war") && EjbDeploymentMarker.isEjbDeployment(unit)) {
+            // ejb archives don't define resource roots, using root resource
+            resourceRoots = new AttachmentList<ResourceRoot>(ResourceRoot.class);
+            final ResourceRoot root = unit.getAttachment(DEPLOYMENT_ROOT);
+            resourceRoots.add(root);
+        }
+        return resourceRoots;
     }
 }
