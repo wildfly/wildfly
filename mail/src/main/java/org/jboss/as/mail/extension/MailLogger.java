3a684b216deb70aa9d18a52308c9ab23fcde3f80
David M. Lloyd <david.lloyd@redhat.com>
2014-02-06 11:52:47 -0600

    [WFLY-2864] WildFly Mail module

--- mail/src/main/java/org/jboss/as/mail/extension/MailLogger.java
+++ mail/src/main/java/org/jboss/as/mail/extension/MailLogger.java
@@ -25,24 +25,29 @@ package org.jboss.as.mail.extension;
 import static org.jboss.logging.Logger.Level.DEBUG;
 import static org.jboss.logging.Logger.Level.INFO;
 
+import java.net.UnknownHostException;
+
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
+import org.jboss.logging.annotations.Cause;
 import org.jboss.logging.annotations.LogMessage;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
+import org.jboss.msc.service.StartException;
 
 /**
  * Date: 05.11.2011
  *
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
-@MessageLogger(projectCode = "JBAS")
+@MessageLogger(projectCode = "WFLYMAIL", length = 4)
 interface MailLogger extends BasicLogger {
 
     /**
      * A logger with a category of the package name.
      */
-    MailLogger ROOT_LOGGER = Logger.getMessageLogger(MailLogger.class, MailLogger.class.getPackage().getName());
+    MailLogger ROOT_LOGGER = Logger.getMessageLogger(MailLogger.class, "org.jboss.as.mail.extension");
 
     /**
      * Logs an info message indicating a javax.mail.Session was bound into JNDI.
@@ -50,7 +55,7 @@ interface MailLogger extends BasicLogger {
      * @param jndiName the JNDI name under which the session was bound.
      */
     @LogMessage(level = INFO)
-    @Message(id = 15400, value = "Bound mail session [%s]")
+    @Message(id = 1, value = "Bound mail session [%s]")
     void boundMailSession(String jndiName);
 
     /**
@@ -59,7 +64,7 @@ interface MailLogger extends BasicLogger {
      * @param jndiName the JNDI name under which the session was bound.
      */
     @LogMessage(level = INFO)
-    @Message(id = 15401, value = "Unbound mail session [%s]")
+    @Message(id = 2, value = "Unbound mail session [%s]")
     void unboundMailSession(String jndiName);
 
     /**
@@ -68,6 +73,34 @@ interface MailLogger extends BasicLogger {
      * @param jndiName the JNDI name under which the session had been bound.
      */
     @LogMessage(level = DEBUG)
-    @Message(id = 15402, value = "Removed mail session [%s]")
+    @Message(id = 3, value = "Removed mail session [%s]")
     void removedMailSession(String jndiName);
+
+    /**
+     * Creates an exception indicating the outgoing socket binding, represented by the {@code outgoingSocketBindingRef}
+     * parameter, could not be found.
+     *
+     * @param outgoingSocketBindingRef the name of the socket binding configuration.
+     * @return a {@link StartException} for the error.
+     */
+    @Message(id = 4, value = "No outbound socket binding configuration '%s' is available.")
+    StartException outboundSocketBindingNotAvailable(String outgoingSocketBindingRef);
+
+    /**
+     * Creates an exception indicating that the destination address of the outgoing socket binding, that the
+     * mail service depends on, could not be resolved
+     *
+     * @param outgoingSocketBindingRef the name of the socket binding configuration.
+     * @return a {@link RuntimeException} for the error.
+     */
+    @Message(id = 5, value = "Unknown host for outbound socket binding configuration '%s'.")
+    RuntimeException unknownOutboundSocketBindingDestination(@Cause UnknownHostException cause, String outgoingSocketBindingRef);
+
+    /**
+     * Creates exception indicating that host was not configured properly
+     *
+     * @return a {@link RuntimeException} for the error.
+     */
+    @Message(id = 6, value = "Host not configured")
+    RuntimeException serverHostNotSet();
 }
