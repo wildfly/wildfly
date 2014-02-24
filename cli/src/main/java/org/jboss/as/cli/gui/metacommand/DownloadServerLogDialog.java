8dcf4693923df4488d25e6c0e287246dba1f8b92
Stan Silvert <ssilvert@redhat.com>
2014-02-24 10:06:49 -0500

    Add progress dialog for log downloads.

--- cli/src/main/java/org/jboss/as/cli/gui/metacommand/DownloadServerLogDialog.java
+++ cli/src/main/java/org/jboss/as/cli/gui/metacommand/DownloadServerLogDialog.java
@@ -20,7 +20,6 @@ package org.jboss.as.cli.gui.metacommand;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Cursor;
 import java.awt.Dialog;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -28,6 +27,8 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -43,6 +44,8 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.ProgressMonitor;
+import javax.swing.SwingWorker;
 import org.jboss.as.cli.CommandFormatException;
 import org.jboss.as.cli.gui.CliGuiContext;
 import org.jboss.dmr.ModelNode;
@@ -52,26 +55,24 @@ import org.jboss.dmr.ModelNode;
  *
  * @author Stan Silvert ssilvert@redhat.com (C) 2014 Red Hat Inc.
  */
-public class DownloadServerLogDialog extends JDialog implements ActionListener {
+public class DownloadServerLogDialog extends JDialog implements ActionListener, PropertyChangeListener {
     // make this static so that it always retains the last directory chosen
     private static JFileChooser fileChooser = new JFileChooser(new File("."));
 
     private CliGuiContext cliGuiCtx;
     private String fileName;
-
-    // we'll use this if we decide to show a progress indicator
-    // might not be perfectly accurate
     private Long fileSize;
-
     private JPanel inputPanel = new JPanel(new GridBagLayout());
     private JTextField pathField = new JTextField(40);
+    private ProgressMonitor progressMonitor;
+    private DownloadLogTask downloadTask;
 
     public DownloadServerLogDialog(CliGuiContext cliGuiCtx, String fileName, Long fileSize) {
         super(cliGuiCtx.getMainWindow(), "Download " + fileName, Dialog.ModalityType.APPLICATION_MODAL);
         this.cliGuiCtx = cliGuiCtx;
         this.fileName = fileName;
         this.fileSize = fileSize;
-        fileChooser.setSelectedFile(new File(fileName));
+        fileChooser.setSelectedFile(new File(fileChooser.getCurrentDirectory(), fileName));
         setPathField();
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -89,7 +90,7 @@ public class DownloadServerLogDialog extends JDialog implements ActionListener {
         try {
             pathField.setText(fileChooser.getSelectedFile().getCanonicalPath());
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -165,41 +166,93 @@ public class DownloadServerLogDialog extends JDialog implements ActionListener {
         }
 
         this.dispose();
-        cliGuiCtx.getMainWindow().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 
-        PrintStream out = null;
-        try {
-            out = new PrintStream(new BufferedOutputStream(new FileOutputStream(selectedFile)));
-            int linesToRead = 5000;
-            int skip = 0;
-            List<ModelNode> dataLines = null;
-            long bytesRead = 0; // we'll use this if we decide to show a progress indicator
-            int lineSepLength = System.getProperty("line.separator").length();
+        progressMonitor = new ProgressMonitor(cliGuiCtx.getMainWindow(), "Downloading " + fileName, "", 0, 100);
+        progressMonitor.setProgress(0);
+        downloadTask = new DownloadLogTask(selectedFile);
+        downloadTask.addPropertyChangeListener(this);
+        downloadTask.execute();
+    }
 
-            do {
-                String command = "/subsystem=logging/:read-log-file(name=" + fileName + ",lines=" + linesToRead + ",skip=" + skip + ",tail=false)";
-                ModelNode result = cliGuiCtx.getExecutor().doCommand(command);
-                if (result.get("outcome").asString().equals("failed")) {
-                    String error = "Failure at server: " + result.get("failure-description").toString();
-                    JOptionPane.showMessageDialog(cliGuiCtx.getMainWindow(), error, "Download Failed", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
-
-                dataLines = result.get("result").asList();
-                for (ModelNode line : dataLines) {
-                    String strLine = line.asString();
-                    bytesRead += strLine.length() + lineSepLength;
-                    out.println(strLine);
-                }
-                skip += linesToRead;
-            } while (dataLines.size() == linesToRead);
-        } catch (IOException | CommandFormatException ex) {
-            throw new RuntimeException(ex);
-        } finally {
-            if (out != null) out.close();
-            cliGuiCtx.getMainWindow().setCursor(Cursor.getDefaultCursor());
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress".equals(evt.getPropertyName())){
+            int percentRead = (Integer) evt.getNewValue();
+            progressMonitor.setProgress(percentRead);
         }
 
-        JOptionPane.showMessageDialog(cliGuiCtx.getMainWindow(), "Download complete.");
+        if ("bytesRead".equals(evt.getPropertyName())) {
+            progressMonitor.setNote(evt.getNewValue() + " of " + fileSize + " bytes received.");
+        }
+
+        if (progressMonitor.isCanceled()) {
+            downloadTask.cancel(false);
+        }
+    }
+
+    class DownloadLogTask extends SwingWorker<Void, Void> {
+        private File selectedFile;
+
+        public DownloadLogTask(File selectedFile) {
+            this.selectedFile = selectedFile;
+        }
+
+        @Override
+        public Void doInBackground() {
+            PrintStream out = null;
+            try {
+                out = new PrintStream(new BufferedOutputStream(new FileOutputStream(selectedFile)));
+                int linesToRead = 5000;
+                int skip = 0;
+                List<ModelNode> dataLines = null;
+                long bytesRead = 0;
+                long bytesReadOldValue = 0;
+                int lineSepLength = System.getProperty("line.separator").length();
+
+                do {
+                    String command = "/subsystem=logging/:read-log-file(name=" + fileName + ",lines=" + linesToRead + ",skip=" + skip + ",tail=false)";
+                    ModelNode result = cliGuiCtx.getExecutor().doCommand(command);
+                    if (result.get("outcome").asString().equals("failed")) {
+                        cancel(false);
+                        String error = "Failure at server: " + result.get("failure-description").toString();
+                        JOptionPane.showMessageDialog(cliGuiCtx.getMainWindow(), error, "Download Failed", JOptionPane.ERROR_MESSAGE);
+                        return null;
+                    }
+
+                    dataLines = result.get("result").asList();
+                    for (ModelNode line : dataLines) {
+                        String strLine = line.asString();
+                        bytesRead += strLine.length() + lineSepLength;
+                        out.println(strLine);
+                    }
+                    skip += linesToRead;
+
+                    setProgress(Math.min(Math.round(((float)bytesRead / (float)fileSize)*100), 100));
+                    firePropertyChange("bytesRead", bytesReadOldValue, bytesRead);
+                    bytesReadOldValue = bytesRead;
+                } while ((dataLines.size() == linesToRead) && !isCancelled());
+            } catch (IOException | CommandFormatException ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+
+                if (isCancelled()) {
+                    selectedFile.delete();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        public void done() {
+            String message = "Download " + fileName + " ";
+            if (isCancelled()) {
+                JOptionPane.showMessageDialog(cliGuiCtx.getMainWindow(), message + "cancelled.", message + "cancelled.", JOptionPane.ERROR_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(cliGuiCtx.getMainWindow(), message + "complete.");
+            }
+        }
     }
 }
