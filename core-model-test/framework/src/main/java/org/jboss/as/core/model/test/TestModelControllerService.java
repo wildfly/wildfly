802f36066b867926ce184fa9364f4a505e757ea0
Brian Stansberry <brian.stansberry@redhat.com>
2014-02-25 09:05:24 -0600

    [WFLY-1904] Support deferring system property expressions until after vault is installed

--- core-model-test/framework/src/main/java/org/jboss/as/core/model/test/TestModelControllerService.java
+++ core-model-test/framework/src/main/java/org/jboss/as/core/model/test/TestModelControllerService.java
@@ -71,6 +71,7 @@ import org.jboss.as.model.test.StringConfigurationPersister;
 import org.jboss.as.protocol.mgmt.ManagementChannelHandler;
 import org.jboss.as.repository.ContentRepository;
 import org.jboss.as.repository.HostFileRepository;
+import org.jboss.as.server.RuntimeExpressionResolver;
 import org.jboss.as.server.ServerEnvironment;
 import org.jboss.as.server.ServerEnvironment.LaunchType;
 import org.jboss.as.server.ServerEnvironmentResourceDescription;
@@ -99,11 +100,14 @@ class TestModelControllerService extends ModelTestModelControllerService {
     private final DelegatingResourceDefinition rootResourceDefinition;
     private final ControlledProcessState processState;
     private final ExtensionRegistry extensionRegistry;
+    private final AbstractVaultReader vaultReader;
     private volatile Initializer initializer;
 
     TestModelControllerService(ProcessType processType, RunningModeControl runningModeControl, StringConfigurationPersister persister, ModelTestOperationValidatorFilter validateOpsFilter,
-            TestModelType type, ModelInitializer modelInitializer, DelegatingResourceDefinition rootResourceDefinition, ControlledProcessState processState, ExtensionRegistry extensionRegistry) {
-        super(processType, runningModeControl, null, persister, validateOpsFilter, rootResourceDefinition, processState, Controller80x.INSTANCE);
+            TestModelType type, ModelInitializer modelInitializer, DelegatingResourceDefinition rootResourceDefinition, ControlledProcessState processState, ExtensionRegistry extensionRegistry,
+            AbstractVaultReader vaultReader) {
+        super(processType, runningModeControl, null, persister, validateOpsFilter, rootResourceDefinition, processState,
+                new RuntimeExpressionResolver(vaultReader), Controller80x.INSTANCE);
         this.type = type;
         this.runningModeControl = runningModeControl;
         this.pathManagerService = type == TestModelType.STANDALONE ? new ServerPathManagerService() : new HostPathManagerService();
@@ -111,6 +115,7 @@ class TestModelControllerService extends ModelTestModelControllerService {
         this.rootResourceDefinition = rootResourceDefinition;
         this.processState = processState;
         this.extensionRegistry = extensionRegistry;
+        this.vaultReader = vaultReader;
 
         if (type == TestModelType.STANDALONE) {
             initializer = new ServerInitializer();
@@ -133,7 +138,8 @@ class TestModelControllerService extends ModelTestModelControllerService {
 
     static TestModelControllerService create(ProcessType processType, RunningModeControl runningModeControl, StringConfigurationPersister persister, ModelTestOperationValidatorFilter validateOpsFilter,
             TestModelType type, ModelInitializer modelInitializer, ExtensionRegistry extensionRegistry) {
-        return new TestModelControllerService(processType, runningModeControl, persister, validateOpsFilter, type, modelInitializer, new DelegatingResourceDefinition(type), new ControlledProcessState(true), extensionRegistry);
+        return new TestModelControllerService(processType, runningModeControl, persister, validateOpsFilter, type, modelInitializer,
+                new DelegatingResourceDefinition(type), new ControlledProcessState(true), extensionRegistry, new TestVaultReader());
     }
 
     InjectedValue<ContentRepository> getContentRepositoryInjector(){
@@ -392,7 +398,6 @@ class TestModelControllerService extends ModelTestModelControllerService {
         final ExtensibleConfigurationPersister persister = new NullConfigurationPersister();
         final ServerEnvironment environment = createStandaloneServerEnvironment();
         final boolean parallelBoot = false;
-        final AbstractVaultReader vaultReader = null;
 
         public void setRootResourceDefinitionDelegate() {
             rootResourceDefinition.setDelegate(new ServerRootResourceDefinition(
@@ -447,7 +452,7 @@ class TestModelControllerService extends ModelTestModelControllerService {
                             injectedContentRepository.getValue(),
                             domainController,
                             extensionRegistry,
-                            null /*vaultReader*/,
+                            vaultReader,
                             ignoredRegistry,
                             processState,
                             pathManagerService,
