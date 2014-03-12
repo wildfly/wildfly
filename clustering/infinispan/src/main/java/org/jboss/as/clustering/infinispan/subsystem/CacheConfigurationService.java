751ff78938e1233a8c52b5e43f7b9109477b2f70
Richard Achmatowicz <rachmatowicz@jboss.com>
2014-03-12 16:33:53 -0400

    [WFLY-3106] Allow Infinispan cache to set statistics-enabled independently of cache manager.

--- clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/subsystem/CacheConfigurationService.java
+++ clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/subsystem/CacheConfigurationService.java
@@ -71,7 +71,6 @@ public class CacheConfigurationService extends AbstractCacheConfigurationService
                 throw new IllegalArgumentException(e);
             }
         }
-        this.builder.jmxStatistics().enabled(this.dependencies.getCacheContainer().getCacheManagerConfiguration().globalJmxStatistics().enabled());
         TransactionManager tm = this.dependencies.getTransactionManager();
         if (tm != null) {
             this.builder.transaction().transactionManagerLookup(new TransactionManagerProvider(tm));
