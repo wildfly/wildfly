c795e467c36ca21b0524f5a63b4fdf54f15f76da
Pedro Igor <pigor.craveiro@gmail.com>
2014-03-18 19:41:00 -0300

    [WFLY-2733] - Adding sensitive data information.

--- picketlink/src/main/java/org/wildfly/extension/picketlink/federation/model/FederationResourceDefinition.java
+++ picketlink/src/main/java/org/wildfly/extension/picketlink/federation/model/FederationResourceDefinition.java
@@ -24,17 +24,27 @@ package org.wildfly.extension.picketlink.federation.model;
 
 import org.jboss.as.controller.ExtensionContext;
 import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.access.constraint.SensitivityClassification;
+import org.jboss.as.controller.access.management.AccessConstraintDefinition;
+import org.jboss.as.controller.access.management.SensitiveTargetAccessConstraintDefinition;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.wildfly.extension.picketlink.common.model.ModelElement;
+import org.wildfly.extension.picketlink.federation.FederationExtension;
 import org.wildfly.extension.picketlink.federation.model.idp.IdentityProviderResourceDefinition;
 import org.wildfly.extension.picketlink.federation.model.saml.SAMLResourceDefinition;
 import org.wildfly.extension.picketlink.federation.model.sp.ServiceProviderResourceDefinition;
+
+import java.util.List;
 
 /**
  * @author <a href="mailto:psilva@redhat.com">Pedro Silva</a>
  * @since Mar 16, 2012
  */
 public class FederationResourceDefinition extends AbstractFederationResourceDefinition {
+
+    private static final List<AccessConstraintDefinition> CONSTRAINTS = new SensitiveTargetAccessConstraintDefinition(
+        new SensitivityClassification(FederationExtension.SUBSYSTEM_NAME, "federation", false, true, true)
+    ).wrapAsList();
 
     public static final SimpleAttributeDefinition[] ATTRIBUTE_DEFINITIONS = new SimpleAttributeDefinition[0];
 
@@ -51,5 +61,10 @@ public class FederationResourceDefinition extends AbstractFederationResourceDefi
         addChildResourceDefinition(new IdentityProviderResourceDefinition(this.extensionContext), resourceRegistration);
         addChildResourceDefinition(new ServiceProviderResourceDefinition(this.extensionContext), resourceRegistration);
         addChildResourceDefinition(SAMLResourceDefinition.INSTANCE, resourceRegistration);
+    }
+
+    @Override
+    public List<AccessConstraintDefinition> getAccessConstraints() {
+        return CONSTRAINTS;
     }
 }
--- picketlink/src/main/java/org/wildfly/extension/picketlink/idm/model/PartitionManagerResourceDefinition.java
+++ picketlink/src/main/java/org/wildfly/extension/picketlink/idm/model/PartitionManagerResourceDefinition.java
@@ -24,15 +24,26 @@ package org.wildfly.extension.picketlink.idm.model;
 
 import org.jboss.as.controller.SimpleAttributeDefinition;
 import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
+import org.jboss.as.controller.access.constraint.SensitivityClassification;
+import org.jboss.as.controller.access.management.AccessConstraintDefinition;
+import org.jboss.as.controller.access.management.SensitiveTargetAccessConstraintDefinition;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.dmr.ModelType;
 import org.wildfly.extension.picketlink.common.model.ModelElement;
+import org.wildfly.extension.picketlink.federation.FederationExtension;
+import org.wildfly.extension.picketlink.idm.IDMExtension;
+
+import java.util.List;
 
 /**
  * @author <a href="mailto:psilva@redhat.com">Pedro Silva</a>
  * @since Mar 16, 2012
  */
 public class PartitionManagerResourceDefinition extends AbstractIDMResourceDefinition {
+
+    private static final List<AccessConstraintDefinition> CONSTRAINTS = new SensitiveTargetAccessConstraintDefinition(
+        new SensitivityClassification(IDMExtension.SUBSYSTEM_NAME, "partition-manager", false, true, true)
+    ).wrapAsList();
 
     public static final SimpleAttributeDefinition IDENTITY_MANAGEMENT_JNDI_URL = new SimpleAttributeDefinitionBuilder(ModelElement.IDENTITY_MANAGEMENT_JNDI_NAME.getName(), ModelType.STRING, false).setAllowExpression(true).build();
     public static final PartitionManagerResourceDefinition INSTANCE = new PartitionManagerResourceDefinition();
@@ -45,5 +56,10 @@ public class PartitionManagerResourceDefinition extends AbstractIDMResourceDefin
     @Override
     public void registerChildren(ManagementResourceRegistration resourceRegistration) {
         addChildResourceDefinition(IdentityConfigurationResourceDefinition.INSTANCE, resourceRegistration);
+    }
+
+    @Override
+    public List<AccessConstraintDefinition> getAccessConstraints() {
+        return CONSTRAINTS;
     }
 }
