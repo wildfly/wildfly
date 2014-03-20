e861eedc47595359a8722925deb9a26b5c5ade37
Emmanuel Hugonnet <ehugonne@redhat.com>
2014-03-20 18:16:45 +0100

    [BZ-976228] : Operation which require server reload should check if something was changed. Adding tests and modifying ReloadRequiredWriteAttributeHandler. In the implementation we are comparing currentValue which is not a resolved with resolvedValue, this is done on purpose. As the context might have changed we can't resolve currentValue being sure that the resolved value is the effective value.

--- controller/src/main/java/org/jboss/as/controller/ReloadRequiredWriteAttributeHandler.java
+++ controller/src/main/java/org/jboss/as/controller/ReloadRequiredWriteAttributeHandler.java
@@ -23,9 +23,23 @@
 package org.jboss.as.controller;
 
 import java.util.Collection;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.BYTES;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.TYPE;
 
 import org.jboss.as.controller.operations.validation.ParameterValidator;
 import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
+import static org.jboss.dmr.ModelType.BIG_DECIMAL;
+import static org.jboss.dmr.ModelType.BIG_INTEGER;
+import static org.jboss.dmr.ModelType.BOOLEAN;
+import static org.jboss.dmr.ModelType.DOUBLE;
+import static org.jboss.dmr.ModelType.INT;
+import static org.jboss.dmr.ModelType.LIST;
+import static org.jboss.dmr.ModelType.LONG;
+import static org.jboss.dmr.ModelType.OBJECT;
+import static org.jboss.dmr.ModelType.PROPERTY;
+import static org.jboss.dmr.ModelType.STRING;
+import static org.jboss.dmr.ModelType.UNDEFINED;
 
 /**
  * Simple {@link AbstractWriteAttributeHandler} that triggers putting the process in a restart-required state
@@ -67,7 +81,67 @@ public class ReloadRequiredWriteAttributeHandler extends AbstractWriteAttributeH
 
     @Override
     protected boolean applyUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName, ModelNode resolvedValue, ModelNode currentValue, HandbackHolder<Void> voidHandback) throws OperationFailedException {
-        return true;
+//      Some might ask why we are comparing a resolvedValue with the currentValuewhich is not resolved.
+//      Well the issue is that the context that would permit to resolve the currentValue might have changed thus
+//      the resolved value for the currentValue wouldn't be correct.
+//      In fact we just can't resolve the currentValue without any doubt. When in doubt reload, so we will return true in this case.
+//      For example if the currentValue is ${foo} and that for some reason foo has changed in between, then we should reload even if now ${foo} resolves
+//      as resolvedValue.
+        ModelNode resolvedTypedValue = convertToType(attributeName, resolvedValue);
+        return !resolvedTypedValue.equals(currentValue);
+    }
+
+    private ModelNode convertToType(String attributeName, ModelNode resolvedValue) {
+        AttributeDefinition attributeDefinition = getAttributeDefinition(attributeName);
+        ModelType type;
+        if (attributeDefinition != null) {
+            type = attributeDefinition.getType();
+        } else {
+            type = ModelType.STRING;
+        }
+        ModelNode converted = resolvedValue.clone();
+        try {
+            switch (type) {
+                case BIG_DECIMAL:
+                    converted.set(resolvedValue.asBigDecimal());
+                    break;
+                case BIG_INTEGER:
+                    converted.set(resolvedValue.asBigInteger());
+                    break;
+                case BOOLEAN:
+                    converted.set(resolvedValue.asBoolean());
+                    break;
+                case BYTES:
+                    converted.set(resolvedValue.asBytes());
+                    break;
+                case DOUBLE:
+                    converted.set(resolvedValue.asDouble());
+                    break;
+                case INT:
+                    converted.set(resolvedValue.asInt());
+                    break;
+                case LIST:
+                    break;
+                case LONG:
+                    converted.set(resolvedValue.asLong());
+                    break;
+                case OBJECT:
+                    break;
+                case PROPERTY:
+                    break;
+                case STRING:
+                    converted.set(resolvedValue.asString());
+                    break;
+                case TYPE:
+                    converted.set(resolvedValue.asType());
+                    break;
+                case UNDEFINED:
+                    break;
+            }
+        } catch (IllegalArgumentException ex) {
+
+        }
+        return converted;
     }
 
     @Override
