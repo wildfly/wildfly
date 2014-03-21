maeste/jboss-as

[WFLY-2951] Minor fix to the XML parsing and some additional tests to test parsing and marshalling the configuration.

domain-management/src/main/java/org/jboss/as/domain/management/parsing/ManagementXml.java
@@ -1924,10 +1924,10 @@ public class ManagementXml {
             case DOMAIN_1_4:
             case DOMAIN_1_5:
             case DOMAIN_2_0:
-                parseKeystore_1_3(reader, op, true);
+                parseKeystore_1_3(reader, op, false);
                 break;
             default:
-                parseKeystore_3_0(reader, op, true);
+                parseKeystore_3_0(reader, op, false);
         }
 
         list.add(op);

wildfly/wildfly

domain-management/src/main/java/org/jboss/as/domain/management/parsing/ManagementXml.java
@@ -1924,10 +1924,10 @@ public class ManagementXml {
             case DOMAIN_1_4:
             case DOMAIN_1_5:
             case DOMAIN_2_0:
-                parseKeystore_1_3(reader, op, true);
+                parseKeystore_1_3(reader, op, false);
                 break;
             default:
-                parseKeystore_2_1(reader, op, true);
+                parseKeystore_2_1(reader, op, false);
         }
 
         list.add(op);