d8e3f1211966f7794d4dedb438391fa61f1d1a54
Tomasz Adamski <tadamski@redhat.com>
2014-03-25 13:23:30 +0100

    WFLY-850 Adding transaction timeout info to programmatic timer

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/timerservice/tx/timeout/AbstractTxBean.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.ejb.timerservice.tx.timeout;
+
+import javax.annotation.Resource;
+import javax.transaction.TransactionManager;
+
+import com.arjuna.ats.jta.transaction.Transaction;
+
+/**
+ * @author Tomasz Adamski
+ */
+public abstract class AbstractTxBean {
+
+    @Resource(lookup = "java:jboss/TransactionManager")
+    private TransactionManager transactionManager;
+
+    private Transaction transaction;
+
+    protected int checkTimeoutValue() {
+        try {
+            transaction = (Transaction) transactionManager.getTransaction();
+            return transaction.getTimeout();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
