7564a1a47ed1164fcb0f7fd381182e4bf3fb2016
Tomasz Adamski <tadamski@redhat.com>
2014-04-01 21:50:43 +0200

    WFLY-2654 SecurityContextInterceptor added to timeout methods of beans with class level security annotations

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/timerservice/security/runas/TimerBean.java
@@ -0,0 +1,80 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.ejb.timerservice.security.runas;
+
+
+/**
+ *  @author Tomasz Adamski
+ */
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RunAs;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.ejb.Timeout;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
+
+@Stateless(name = "TimerBean")
+@RunAs("bob")
+@PermitAll
+public class TimerBean {
+
+    private static final int TIMEOUT = 100;
+    private static final CountDownLatch latch = new CountDownLatch(1);
+    private static final int TIMER_CALL_WAITING_S = 30;
+
+    private static volatile boolean timerServiceCalled = false;
+
+    @Resource
+    private TimerService timerService;
+
+    @Resource
+    private SessionContext ctx;
+
+    @EJB
+    private SecureBean secureBean;
+
+    public void startTimer() {
+        timerService.createSingleActionTimer(TIMEOUT, new TimerConfig());
+    }
+
+    @Timeout
+    private void timeout() {
+        secureBean.secureMethod();
+        timerServiceCalled = true;
+        latch.countDown();
+    }
+
+    public static boolean awaitTimerCall() {
+        try {
+            latch.await(TIMER_CALL_WAITING_S, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return timerServiceCalled;
+    }
+}
