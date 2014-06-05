5624b4782caddea3f6327ef52fc525fe2363847e
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-06-05 11:14:15 -0500

    Wrap getCallerPrincipal() and isCallerInRole() in a privilidged action

--- ejb3/src/main/java/org/jboss/as/ejb3/component/EJBComponent.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/EJBComponent.java
@@ -39,6 +39,7 @@ import javax.transaction.UserTransaction;
 import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.Principal;
+import java.security.PrivilegedAction;
 import java.util.Collections;
 import java.util.Map;
 
@@ -65,6 +66,7 @@ import org.jboss.logging.Logger;
 import org.jboss.msc.service.ServiceContainer;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
+import org.wildfly.security.manager.WildFlySecurityManager;
 
 /**
  * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
@@ -102,6 +104,13 @@ public abstract class EJBComponent extends BasicComponent {
     private final TransactionSynchronizationRegistry transactionSynchronizationRegistry;
     private final UserTransaction userTransaction;
     private final ServerSecurityManager serverSecurityManager;
+
+    private final PrivilegedAction<Principal> getCaller = new PrivilegedAction<Principal>() {
+        @Override
+        public Principal run() {
+            return serverSecurityManager.getCallerPrincipal();
+        }
+    };
 
     /**
      * Construct a new instance.
@@ -236,7 +245,11 @@ public abstract class EJBComponent extends BasicComponent {
     }
 
     public Principal getCallerPrincipal() {
-        return this.serverSecurityManager.getCallerPrincipal();
+        if(WildFlySecurityManager.isChecking()) {
+            return WildFlySecurityManager.doUnchecked(getCaller);
+        } else {
+            return this.serverSecurityManager.getCallerPrincipal();
+        }
     }
 
     protected TransactionAttributeType getCurrentTransactionAttribute() {
@@ -370,7 +383,15 @@ public abstract class EJBComponent extends BasicComponent {
     }
 
     public boolean isCallerInRole(final String roleName) throws IllegalStateException {
-        return this.serverSecurityManager.isCallerInRole(securityMetaData.getSecurityRoles(), securityMetaData.getSecurityRoleLinks(), roleName);
+        if (WildFlySecurityManager.isChecking()) {
+            return WildFlySecurityManager.doUnchecked(new PrivilegedAction<Boolean>() {
+                public Boolean run() {
+                    return serverSecurityManager.isCallerInRole(securityMetaData.getSecurityRoles(), securityMetaData.getSecurityRoleLinks(), roleName);
+                }
+            });
+        } else {
+            return this.serverSecurityManager.isCallerInRole(securityMetaData.getSecurityRoles(), securityMetaData.getSecurityRoleLinks(), roleName);
+        }
     }
 
     public boolean isStatisticsEnabled() {
