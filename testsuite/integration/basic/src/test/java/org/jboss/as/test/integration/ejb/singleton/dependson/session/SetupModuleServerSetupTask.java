fbcd1b1f398b919ea895567ec339d4752c6ea7c3
baranowb <baranowb@gmail.com>
2014-07-07 09:59:27 +0200

    WFLY_2732_TEST

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/singleton/dependson/session/SetupModuleServerSetupTask.java
@@ -0,0 +1,49 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.singleton.dependson.session;
+
+import java.io.File;
+
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.integration.ejb.singleton.dependson.mdb.CallCounterInterface;
+import org.jboss.as.test.integration.weld.modules.ModuleUtils;
+
+/**
+ * @author baranowb
+ *
+ */
+public class SetupModuleServerSetupTask implements ServerSetupTask{
+
+    @Override
+    public void setup(ManagementClient arg0, String arg1) throws Exception {
+        ModuleUtils.createSimpleTestModule(SessionConstants.TEST_MODULE_NAME, CallCounterInterface.class,Trigger.class);
+    }
+
+    @Override
+    public void tearDown(ManagementClient arg0, String arg1) throws Exception {
+        //'test' is prefix from ModuleUtils.
+        ModuleUtils.deleteRecursively(new File(ModuleUtils.getModulePath(), "test"));
+    }
+
+}
