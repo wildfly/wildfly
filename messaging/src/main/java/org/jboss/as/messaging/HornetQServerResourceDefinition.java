3a6103637478307b37d1c299d454e7b99fe48bed
Kabir Khan <kkhan@redhat.com>
2014-08-15 17:21:22 +0100

    [WFLY-2807] Make hornetq runtime-queue resources runtime

--- messaging/src/main/java/org/jboss/as/messaging/HornetQServerResourceDefinition.java
+++ messaging/src/main/java/org/jboss/as/messaging/HornetQServerResourceDefinition.java
@@ -139,6 +139,13 @@ public class HornetQServerResourceDefinition extends SimpleResourceDefinition {
         // getTopicNames, getQueueNames, getConnectionFactoryNames,
     }
 
+
+
+    @Override
+    public void registerChildren(ManagementResourceRegistration resourceRegistration) {
+        super.registerChildren(resourceRegistration);
+    }
+
     /**
      * {@inheritDoc}
      * <p/>
