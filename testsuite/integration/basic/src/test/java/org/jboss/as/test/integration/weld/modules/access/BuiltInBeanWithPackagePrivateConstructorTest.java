c3193d1112bb222732869e63f15c0da00d3cf7c6
Jozef Hartinger <jharting@redhat.com>
2014-08-27 15:21:05 +0200

    Testcase for BZ1138192 (WFLY-3779)

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/modules/access/BuiltInBeanWithPackagePrivateConstructorTest.java
@@ -0,0 +1,86 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.weld.modules.access;
+
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.test.module.util.TestModule;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class BuiltInBeanWithPackagePrivateConstructorTest {
+
+    private static final String[] MODULE_DEPENDENCIES = new String[] {
+        "javax.enterprise.api",
+        "javax.inject.api",
+        "org.javassist",
+        "org.jboss.weld.core"
+    };
+
+    private static final String MODULE_NAME = "module-accessibility";
+
+    @Inject
+    private InjectedBean injectedBean;
+
+    private static TestModule module;
+
+    public static void doSetup() throws Exception {
+        module = new TestModule(MODULE_NAME, MODULE_DEPENDENCIES);
+        JavaArchive jar = module.addResource("module-accessibility.jar");
+        jar.addClass(BuiltInBeanWithPackagePrivateConstructor.class);
+        jar.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        module.create();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        module.remove();
+    }
+
+    @Deployment
+    public static Archive<?> getDeployment() throws Exception {
+        doSetup();
+        return ShrinkWrap.create(WebArchive.class).addClasses(InjectedBean.class, BuiltInBeanWithPackagePrivateConstructorTest.class, TestModule.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource(new StringAsset("Dependencies: " + MODULE_NAME + " meta-inf\n"), "MANIFEST.MF");
+
+    }
+
+    @Test
+    public void testBeanInjectable() throws IllegalArgumentException, IllegalAccessException {
+        BuiltInBeanWithPackagePrivateConstructor bean = injectedBean.getBean();
+        bean.setValue("foo");
+        Assert.assertEquals("foo", bean.getValue());
+    }
+}
