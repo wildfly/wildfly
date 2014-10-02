bdf01a09efb8f6e510ede75a17d9e8e2c95ab868
Mustafa Musaji <mmusaji@redhat.com>
2014-10-02 11:21:22 +0100

    Fixed username in remote-outbound-connection to resolve expression from system property. Copied code change from wildfly-core

--- remoting/src/main/java/org/jboss/as/remoting/RemoteOutboundConnectionAdd.java
+++ remoting/src/main/java/org/jboss/as/remoting/RemoteOutboundConnectionAdd.java
@@ -75,7 +75,7 @@ class RemoteOutboundConnectionAdd extends AbstractOutboundConnectionAddHandler {
         final ServiceName outboundSocketBindingDependency = OutboundSocketBinding.OUTBOUND_SOCKET_BINDING_BASE_SERVICE_NAME.append(outboundSocketBindingRef);
         // fetch the connection creation options from the model
         final OptionMap connectionCreationOptions = ConnectorResource.getOptions(context, fullModel.get(CommonAttributes.PROPERTY));
-        final String username = fullModel.hasDefined(CommonAttributes.USERNAME) ? fullModel.require(CommonAttributes.USERNAME).asString() : null;
+        final String username = RemoteOutboundConnectionResourceDefinition.USERNAME.resolveModelAttribute(context, fullModel).asString();
         final String securityRealm = fullModel.hasDefined(CommonAttributes.SECURITY_REALM) ? fullModel.require(CommonAttributes.SECURITY_REALM).asString() : null;
 
         // create the service
