0ff00d26b15808016845e1b9ceb4e971522e860f
James Perkins <jperkins@redhat.com>
2014-10-09 11:00:26 -0700

    [BZ1087246] Updated Infinispan model version.

--- clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/subsystem/InfinispanExtension.java
+++ clustering/infinispan/src/main/java/org/jboss/as/clustering/infinispan/subsystem/InfinispanExtension.java
@@ -47,7 +47,7 @@ public class InfinispanExtension implements Extension {
     public static final String RESOURCE_NAME = InfinispanExtension.class.getPackage().getName() + "." +"LocalDescriptions";
 
     private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
-    private static final int MANAGEMENT_API_MINOR_VERSION = 5;
+    private static final int MANAGEMENT_API_MINOR_VERSION = 6;
     private static final int MANAGEMENT_API_MICRO_VERSION = 0;
 
     static ResourceDescriptionResolver getResourceDescriptionResolver(final String... keyPrefix) {
