bb2e14a0d8b0b443952a0858116c589166282a8a
Scott Marlow <smarlow@redhat.com>
2014-10-13 16:55:47 -0400

    WFLY-3975 show scoped pu name for transaction scoped entity manager

--- jpa/src/main/java/org/jboss/as/jpa/container/TransactionScopedEntityManager.java
+++ jpa/src/main/java/org/jboss/as/jpa/container/TransactionScopedEntityManager.java
@@ -145,7 +145,7 @@ public class TransactionScopedEntityManager extends AbstractEntityManager implem
         if (entityManager == null) {
             entityManager = createEntityManager(emf, properties, synchronizationType);
             if (JPA_LOGGER.isDebugEnabled())
-                JPA_LOGGER.debugf("%s: created entity manager session %s", TransactionUtil.getEntityManagerDetails(entityManager),
+                JPA_LOGGER.debugf("%s: created entity manager session %s", TransactionUtil.getEntityManagerDetails(entityManager, scopedPuName),
                     TransactionUtil.getTransaction().toString());
             TransactionUtil.registerSynchronization(entityManager, scopedPuName);
             TransactionUtil.putEntityManagerInTransactionRegistry(scopedPuName, entityManager);
@@ -153,7 +153,7 @@ public class TransactionScopedEntityManager extends AbstractEntityManager implem
         else {
             testForMixedSynchronizationTypes(entityManager, puScopedName, synchronizationType);
             if (JPA_LOGGER.isDebugEnabled()) {
-                JPA_LOGGER.debugf("%s: reuse entity manager session already in tx %s", TransactionUtil.getEntityManagerDetails(entityManager),
+                JPA_LOGGER.debugf("%s: reuse entity manager session already in tx %s", TransactionUtil.getEntityManagerDetails(entityManager, scopedPuName),
                     TransactionUtil.getTransaction().toString());
             }
         }
