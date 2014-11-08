wildfly/wildfly-core

https://issues.jboss.org/browse/WFLY-3614 enhance patch command to display applied patch's description (in addition, unified the output with the 'patch inspect' and added a verbose mode)

patching/src/main/java/org/jboss/as/patching/management/PatchInfoHandler.java
@@ -1,0 +1,116 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.patching.management;
+
+import java.io.IOException;
+
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.patching.Constants;
+import org.jboss.as.patching.installation.InstallationManager;
+import org.jboss.as.patching.installation.InstallationManagerService;
+import org.jboss.as.patching.installation.PatchableTarget;
+import org.jboss.as.patching.logging.PatchLogger;
+import org.jboss.as.patching.metadata.Identity;
+import org.jboss.as.patching.metadata.Patch;
+import org.jboss.as.patching.metadata.PatchElement;
+import org.jboss.as.patching.tool.PatchingHistory;
+import org.jboss.as.patching.tool.PatchingHistory.Entry;
+import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.ServiceController;
+
+/**
+ * This handler returns the info about specific patch
+ *
+ * @author Alexey Loubyansky
+ */
+public class PatchInfoHandler implements OperationStepHandler {
+
+    public static final PatchInfoHandler INSTANCE = new PatchInfoHandler();
+
+    @Override
+    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
+
+        final String patchId = PatchResourceDefinition.PATCH_ID.resolveModelAttribute(context, operation).asString();
+        final boolean verbose = PatchResourceDefinition.VERBOSE.resolveModelAttribute(context, operation).asBoolean();
+
+        final InstallationManager mgr = getInstallationManager(context);
+        if(mgr == null) {
+            throw new OperationFailedException(PatchLogger.ROOT_LOGGER.failedToLoadIdentity());
+        }
+        final PatchableTarget.TargetInfo info;
+        try {
+            info = mgr.getIdentity().loadTargetInfo();
+        } catch (IOException e) {
+            throw new OperationFailedException(PatchLogger.ROOT_LOGGER.failedToLoadIdentity(), e);
+        }
+
+        ModelNode result = null;
+        final PatchingHistory.Iterator i = PatchingHistory.Factory.iterator(mgr, info);
+        while(i.hasNext()) {
+            final Entry entry = i.next();
+            if(patchId.equals(entry.getPatchId())) {
+                result = new ModelNode();
+                result.get(Constants.PATCH_ID).set(entry.getPatchId());
+                result.get(Constants.TYPE).set(entry.getType().getName());
+                result.get(Constants.DESCRIPTION).set(entry.getMetadata().getDescription());
+                final Identity identity = entry.getMetadata().getIdentity();
+                result.get(Constants.IDENTITY_NAME).set(identity.getName());
+                result.get(Constants.IDENTITY_VERSION).set(identity.getVersion());
+
+                if(verbose) {
+                    final ModelNode list = result.get(Constants.ELEMENTS).setEmptyList();
+                    final Patch metadata = entry.getMetadata();
+                    for(PatchElement e : metadata.getElements()) {
+                        final ModelNode element = new ModelNode();
+                        element.get(Constants.PATCH_ID).set(e.getId());
+                        element.get(Constants.TYPE).set(e.getProvider().isAddOn() ? Constants.ADD_ON : Constants.LAYER);
+                        element.get(Constants.NAME).set(e.getProvider().getName());
+                        element.get(Constants.DESCRIPTION).set(e.getDescription());
+                        list.add(element);
+                    }
+                }
+
+                context.getResult().set(result);
+                break;
+            }
+        }
+        if(result == null) {
+            context.getFailureDescription().set(PatchLogger.ROOT_LOGGER.patchNotFoundInHistory(patchId).getLocalizedMessage());
+        }
+        context.completeStep(OperationContext.RollbackHandler.NOOP_ROLLBACK_HANDLER);
+    }
+
+    private InstallationManager getInstallationManager(OperationContext ctx) {
+        final ServiceController<?> imController = ctx.getServiceRegistry(false).getRequiredService(InstallationManagerService.NAME);
+        while (imController != null && imController.getState() == ServiceController.State.UP) {
+            try {
+                return (InstallationManager) imController.getValue();
+            } catch (IllegalStateException e) {
+                // ignore, caused by race from WFLY-3505
+            }
+        }
+        return null;
+    }
+}

patching/src/main/java/org/jboss/as/patching/tool/PatchOperationTarget.java
@@ -48,10 +48,13 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.patching.Constants;
 import org.jboss.as.patching.ContentConflictsException;
 import org.jboss.as.patching.PatchInfo;
-import org.jboss.as.patching.logging.PatchLogger;
 import org.jboss.as.patching.PatchingException;
+import org.jboss.as.patching.logging.PatchLogger;
 import org.jboss.as.patching.metadata.ContentItem;
 import org.jboss.as.patching.metadata.ContentType;
+import org.jboss.as.patching.metadata.Patch;
+import org.jboss.as.patching.metadata.PatchElement;
+import org.jboss.as.patching.tool.PatchingHistory.Entry;
 import org.jboss.dmr.ModelNode;
 
 /**
@@ -102,6 +105,7 @@ public abstract class PatchOperationTarget {
     //
 
     protected abstract ModelNode info() throws IOException;
+    protected abstract ModelNode info(String patchId, boolean verbose) throws IOException;
     protected abstract ModelNode history() throws IOException;
     protected abstract ModelNode applyPatch(final File file, final ContentPolicyBuilderImpl builder) throws IOException;
     protected abstract ModelNode rollback(final String patchId, final ContentPolicyBuilderImpl builder, boolean rollbackTo, final boolean restoreConfiguration) throws IOException;
@@ -183,6 +187,47 @@ public abstract class PatchOperationTarget {
             }
             return result;
         }
+
+        @Override
+        protected ModelNode info(String patchId, boolean verbose) throws IOException {
+            if(patchId == null) {
+                throw new IllegalArgumentException("patchId is null");
+            }
+            PatchingHistory history = tool.getPatchingHistory();
+            try {
+                final PatchingHistory.Iterator iterator = history.iterator();
+                while(iterator.hasNext()) {
+                    final Entry next = iterator.next();
+                    if(patchId.equals(next.getPatchId())) {
+                        final ModelNode response = new ModelNode();
+                        response.get(OUTCOME).set(SUCCESS);
+                        final ModelNode result = response.get(RESULT);
+                        result.get(Constants.PATCH_ID).set(next.getPatchId());
+                        result.get(Constants.TYPE).set(next.getType().getName());
+                        final Patch metadata = next.getMetadata();
+                        result.get(Constants.IDENTITY_NAME).set(metadata.getIdentity().getName());
+                        result.get(Constants.IDENTITY_VERSION).set(metadata.getIdentity().getVersion());
+                        result.get(Constants.DESCRIPTION).set(next.getMetadata().getDescription());
+
+                        if (verbose) {
+                            final ModelNode elements = result.get(Constants.ELEMENTS).setEmptyList();
+                            for(PatchElement e : metadata.getElements()) {
+                                final ModelNode element = new ModelNode();
+                                element.get(Constants.PATCH_ID).set(e.getId());
+                                element.get(Constants.NAME).set(e.getProvider().getName());
+                                element.get(Constants.TYPE).set(e.getProvider().isAddOn() ? Constants.ADD_ON : Constants.LAYER);
+                                element.get(Constants.DESCRIPTION).set(e.getDescription());
+                                elements.add(element);
+                            }
+                        }
+                        return response;
+                    }
+                }
+            } catch (PatchingException e) {
+                return formatFailedResponse(e);
+            }
+            return formatFailedResponse(PatchLogger.ROOT_LOGGER.patchNotFoundInHistory(patchId).getLocalizedMessage());
+        }
     }
 
     protected static class RemotePatchOperationTarget extends PatchOperationTarget {
@@ -237,6 +282,25 @@ public abstract class PatchOperationTarget {
             operation.get(Constants.RESET_CONFIGURATION).set(restoreConfiguration);
             return client.execute(operation);
         }
+
+        @Override
+        protected ModelNode info(String patchId, boolean verbose) throws IOException {
+            final ModelNode operation = new ModelNode();
+            operation.get(ModelDescriptionConstants.OP).set("patch-info");
+            operation.get(ModelDescriptionConstants.OP_ADDR).set(address.toModelNode());
+            operation.get(Constants.PATCH_ID).set(patchId);
+            if(verbose) {
+                operation.get(Constants.VERBOSE).set(true);
+            }
+            return client.execute(operation);
+        }
+    }
+
+    static ModelNode formatFailedResponse(final String msg) {
+        final ModelNode result = new ModelNode();
+        result.get(OUTCOME).set(FAILED);
+        result.get(FAILURE_DESCRIPTION, Constants.MESSAGE).set(msg);
+        return result;
     }
 
     static ModelNode formatFailedResponse(final PatchingException e) {

wildfly/wildfly

patching/src/main/java/org/jboss/as/patching/management/PatchInfoHandler.java
@@ -1,0 +1,116 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.patching.management;
+
+import java.io.IOException;
+
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.patching.Constants;
+import org.jboss.as.patching.PatchMessages;
+import org.jboss.as.patching.installation.InstallationManager;
+import org.jboss.as.patching.installation.InstallationManagerService;
+import org.jboss.as.patching.installation.PatchableTarget;
+import org.jboss.as.patching.metadata.Identity;
+import org.jboss.as.patching.metadata.Patch;
+import org.jboss.as.patching.metadata.PatchElement;
+import org.jboss.as.patching.tool.PatchingHistory;
+import org.jboss.as.patching.tool.PatchingHistory.Entry;
+import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.ServiceController;
+
+/**
+ * This handler returns the info about specific patch
+ *
+ * @author Alexey Loubyansky
+ */
+public class PatchInfoHandler implements OperationStepHandler {
+
+    public static final PatchInfoHandler INSTANCE = new PatchInfoHandler();
+
+    @Override
+    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
+
+        final String patchId = PatchResourceDefinition.PATCH_ID.resolveModelAttribute(context, operation).asString();
+        final boolean verbose = PatchResourceDefinition.VERBOSE.resolveModelAttribute(context, operation).asBoolean();
+
+        final InstallationManager mgr = getInstallationManager(context);
+        if(mgr == null) {
+            throw new OperationFailedException(PatchManagementMessages.MESSAGES.failedToLoadIdentity());
+        }
+        final PatchableTarget.TargetInfo info;
+        try {
+            info = mgr.getIdentity().loadTargetInfo();
+        } catch (IOException e) {
+            throw new OperationFailedException(PatchManagementMessages.MESSAGES.failedToLoadIdentity(), e);
+        }
+
+        ModelNode result = null;
+        final PatchingHistory.Iterator i = PatchingHistory.Factory.iterator(mgr, info);
+        while(i.hasNext()) {
+            final Entry entry = i.next();
+            if(patchId.equals(entry.getPatchId())) {
+                result = new ModelNode();
+                result.get(Constants.PATCH_ID).set(entry.getPatchId());
+                result.get(Constants.TYPE).set(entry.getType().getName());
+                result.get(Constants.DESCRIPTION).set(entry.getMetadata().getDescription());
+                final Identity identity = entry.getMetadata().getIdentity();
+                result.get(Constants.IDENTITY_NAME).set(identity.getName());
+                result.get(Constants.IDENTITY_VERSION).set(identity.getVersion());
+
+                if(verbose) {
+                    final ModelNode list = result.get(Constants.ELEMENTS).setEmptyList();
+                    final Patch metadata = entry.getMetadata();
+                    for(PatchElement e : metadata.getElements()) {
+                        final ModelNode element = new ModelNode();
+                        element.get(Constants.PATCH_ID).set(e.getId());
+                        element.get(Constants.TYPE).set(e.getProvider().isAddOn() ? Constants.ADD_ON : Constants.LAYER);
+                        element.get(Constants.NAME).set(e.getProvider().getName());
+                        element.get(Constants.DESCRIPTION).set(e.getDescription());
+                        list.add(element);
+                    }
+                }
+
+                context.getResult().set(result);
+                break;
+            }
+        }
+        if(result == null) {
+            context.getFailureDescription().set(PatchMessages.MESSAGES.patchNotFoundInHistory(patchId).getLocalizedMessage());
+        }
+        context.completeStep(OperationContext.RollbackHandler.NOOP_ROLLBACK_HANDLER);
+    }
+
+    private InstallationManager getInstallationManager(OperationContext ctx) {
+        final ServiceController<?> imController = ctx.getServiceRegistry(false).getRequiredService(InstallationManagerService.NAME);
+        while (imController != null && imController.getState() == ServiceController.State.UP) {
+            try {
+                return (InstallationManager) imController.getValue();
+            } catch (IllegalStateException e) {
+                // ignore, caused by race from WFLY-3505
+            }
+        }
+        return null;
+    }
+}

patching/src/main/java/org/jboss/as/patching/tool/PatchOperationTarget.java
@@ -52,6 +52,9 @@ import org.jboss.as.patching.PatchMessages;
 import org.jboss.as.patching.PatchingException;
 import org.jboss.as.patching.metadata.ContentItem;
 import org.jboss.as.patching.metadata.ContentType;
+import org.jboss.as.patching.metadata.Patch;
+import org.jboss.as.patching.metadata.PatchElement;
+import org.jboss.as.patching.tool.PatchingHistory.Entry;
 import org.jboss.dmr.ModelNode;
 
 /**
@@ -102,6 +105,7 @@ public abstract class PatchOperationTarget {
     //
 
     protected abstract ModelNode info() throws IOException;
+    protected abstract ModelNode info(String patchId, boolean verbose) throws IOException;
     protected abstract ModelNode history() throws IOException;
     protected abstract ModelNode applyPatch(final File file, final ContentPolicyBuilderImpl builder) throws IOException;
     protected abstract ModelNode rollback(final String patchId, final ContentPolicyBuilderImpl builder, boolean rollbackTo, final boolean restoreConfiguration) throws IOException;
@@ -183,6 +187,47 @@ public abstract class PatchOperationTarget {
             }
             return result;
         }
+
+        @Override
+        protected ModelNode info(String patchId, boolean verbose) throws IOException {
+            if(patchId == null) {
+                throw new IllegalArgumentException("patchId is null");
+            }
+            PatchingHistory history = tool.getPatchingHistory();
+            try {
+                final PatchingHistory.Iterator iterator = history.iterator();
+                while(iterator.hasNext()) {
+                    final Entry next = iterator.next();
+                    if(patchId.equals(next.getPatchId())) {
+                        final ModelNode response = new ModelNode();
+                        response.get(OUTCOME).set(SUCCESS);
+                        final ModelNode result = response.get(RESULT);
+                        result.get(Constants.PATCH_ID).set(next.getPatchId());
+                        result.get(Constants.TYPE).set(next.getType().getName());
+                        final Patch metadata = next.getMetadata();
+                        result.get(Constants.IDENTITY_NAME).set(metadata.getIdentity().getName());
+                        result.get(Constants.IDENTITY_VERSION).set(metadata.getIdentity().getVersion());
+                        result.get(Constants.DESCRIPTION).set(next.getMetadata().getDescription());
+
+                        if (verbose) {
+                            final ModelNode elements = result.get(Constants.ELEMENTS).setEmptyList();
+                            for(PatchElement e : metadata.getElements()) {
+                                final ModelNode element = new ModelNode();
+                                element.get(Constants.PATCH_ID).set(e.getId());
+                                element.get(Constants.NAME).set(e.getProvider().getName());
+                                element.get(Constants.TYPE).set(e.getProvider().isAddOn() ? Constants.ADD_ON : Constants.LAYER);
+                                element.get(Constants.DESCRIPTION).set(e.getDescription());
+                                elements.add(element);
+                            }
+                        }
+                        return response;
+                    }
+                }
+            } catch (PatchingException e) {
+                return formatFailedResponse(e);
+            }
+            return formatFailedResponse(PatchMessages.MESSAGES.patchNotFoundInHistory(patchId).getLocalizedMessage());
+        }
     }
 
     protected static class RemotePatchOperationTarget extends PatchOperationTarget {
@@ -237,6 +282,25 @@ public abstract class PatchOperationTarget {
             operation.get(Constants.RESET_CONFIGURATION).set(restoreConfiguration);
             return client.execute(operation);
         }
+
+        @Override
+        protected ModelNode info(String patchId, boolean verbose) throws IOException {
+            final ModelNode operation = new ModelNode();
+            operation.get(ModelDescriptionConstants.OP).set("patch-info");
+            operation.get(ModelDescriptionConstants.OP_ADDR).set(address.toModelNode());
+            operation.get(Constants.PATCH_ID).set(patchId);
+            if(verbose) {
+                operation.get(Constants.VERBOSE).set(true);
+            }
+            return client.execute(operation);
+        }
+    }
+
+    static ModelNode formatFailedResponse(final String msg) {
+        final ModelNode result = new ModelNode();
+        result.get(OUTCOME).set(FAILED);
+        result.get(FAILURE_DESCRIPTION, Constants.MESSAGE).set(msg);
+        return result;
     }
 
     static ModelNode formatFailedResponse(final PatchingException e) {