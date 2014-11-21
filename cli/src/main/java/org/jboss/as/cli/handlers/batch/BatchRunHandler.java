e2602d823fa451bca7b7a6b53e22d3e4086d5fe9
Alexey Loubyansky <loubyansky@gmail.com>
2014-11-21 18:25:25 +0100

    second commit for https://issues.jboss.org/browse/WFCORE-145 which fixes the verbose argument handling for batch-run command

--- cli/src/main/java/org/jboss/as/cli/handlers/batch/BatchRunHandler.java
+++ cli/src/main/java/org/jboss/as/cli/handlers/batch/BatchRunHandler.java
@@ -41,6 +41,7 @@ import org.jboss.as.cli.handlers.WindowsFilenameTabCompleter;
 import org.jboss.as.cli.impl.ArgumentWithValue;
 import org.jboss.as.cli.impl.ArgumentWithoutValue;
 import org.jboss.as.cli.impl.FileSystemPathArgument;
+import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.dmr.ModelNode;
 
 /**
@@ -67,9 +68,22 @@ public class BatchRunHandler extends BaseOperationCommand {
     @Override
     protected void doHandle(CommandContext ctx) throws CommandLineException {
 
+        final boolean v = verbose.isPresent(ctx.getParsedCommandLine());
+
+        final ModelNode response;
         boolean failed = false;
         try {
-            super.doHandle(ctx);
+            final ModelNode request = buildRequest(ctx);
+
+            final ModelControllerClient client = ctx.getModelControllerClient();
+            try {
+                response = client.execute(request);
+            } catch (Exception e) {
+                throw new CommandFormatException("Failed to perform operation: " + e.getLocalizedMessage());
+            }
+            if (!Util.isSuccess(response)) {
+                throw new CommandFormatException(Util.getFailureDescription(response));
+            }
         } catch(CommandLineException e) {
             failed = true;
             throw e;
@@ -79,6 +93,13 @@ public class BatchRunHandler extends BaseOperationCommand {
                     ctx.getBatchManager().discardActiveBatch();
                 }
             }
+        }
+
+        if(v) {
+            ctx.printLine(response.toString());
+        } else {
+            ctx.printLine("The batch executed successfully");
+            super.handleResponse(ctx, response, true);
         }
     }
 
@@ -154,12 +175,7 @@ public class BatchRunHandler extends BaseOperationCommand {
 
     @Override
     protected void handleResponse(CommandContext ctx, ModelNode response, boolean composite) throws CommandLineException {
-        if(verbose.isPresent(ctx.getParsedCommandLine())) {
-            ctx.printLine(response.toString());
-        } else {
-            ctx.printLine("The batch executed successfully");
-            super.handleResponse(ctx, response, composite);
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
