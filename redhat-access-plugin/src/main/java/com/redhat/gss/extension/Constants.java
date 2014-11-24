23c9c9289f1b03a9f604ff562a18e73976f1e3cd
Keith Robertson <kroberts@redhat.com>
2014-11-24 09:38:52 -0500

    BZ1164382 Add RH support lib module and get rid of the RH Access subsystem

--- redhat-access-plugin/src/main/java/com/redhat/gss/extension/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.redhat.gss.extension;
-
-public class Constants {
-    public static final String userAgent = "EAP6.3";
-    public static final String RHAccessURL = "http://api.access.redhat.com";
-}
