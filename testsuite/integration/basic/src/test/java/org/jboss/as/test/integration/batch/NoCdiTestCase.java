90f9fce635c225cb3d6e8e055b2af02c25d077a0
Brent Douglas <brent.n.douglas@gmail.com>
2014-11-28 11:25:17 +0100

    [WFLY-4139] Allow batch to start without CDI.
    The batch subsystem no longer requires a bean manager to start.
    Validation that a transaction manager is present is now performed on
    subsystem startup rather than at runtime. Non-CDI deployments can now
    use batch, as can deployments that do not start jobs.

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/batch/NoCdiTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.batch;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+
+/**
+ * @author <a href="mailto:brent.n.douglas@gmail.com">Brent Douglas</a>
+ * @since 9.0
+ */
+@RunWith(Arquillian.class)
+public class NoCdiTestCase extends Assert {
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(JavaArchive.class, NoCdiTestCase.class.getSimpleName() + ".jar")
+                .addClass(NoCdiTestCase.class);
+    }
+
+    @Test
+    public void testJobOperatorIsAvailable() throws Exception {
+        final JobOperator operator = BatchRuntime.getJobOperator();
+        assertNotNull(operator);
+    }
+}
