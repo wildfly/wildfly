b93b1457de27c24d46d392284577e99e97d164e0
Ivo Studensky <istudens@redhat.com>
2014-12-04 15:18:10 +0100

    bz1057835 WFLY-4156 test-cases for EJB 2.0 based MDBs

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/mdb/ejb2x/MDB21TestCase.java
@@ -0,0 +1,112 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.ejb.mdb.ejb2x;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.integration.common.jms.JMSOperations;
+import org.jboss.as.test.integration.common.jms.JMSOperationsProvider;
+import org.jboss.as.test.shared.TimeoutUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+
+/**
+ * Tests EJB2.1 MDB deployments.
+ *
+ * @author <a href="mailto:istudens@redhat.com">Ivo Studensky</a>
+ */
+@RunWith(Arquillian.class)
+@ServerSetup({MDB21TestCase.JmsQueueSetup.class})
+public class MDB21TestCase extends AbstractMDB2xTestCase {
+
+    private Queue queue;
+    private Queue replyQueue;
+
+    static class JmsQueueSetup implements ServerSetupTask {
+
+        private JMSOperations jmsAdminOperations;
+
+        @Override
+        public void setup(ManagementClient managementClient, String containerId) throws Exception {
+            jmsAdminOperations = JMSOperationsProvider.getInstance(managementClient);
+            jmsAdminOperations.createJmsQueue("ejb2x/queue", "java:jboss/ejb2x/queue");
+            jmsAdminOperations.createJmsQueue("ejb2x/replyQueue", "java:jboss/ejb2x/replyQueue");
+        }
+
+        @Override
+        public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
+            if (jmsAdminOperations != null) {
+                jmsAdminOperations.removeJmsQueue("ejb2x/queue");
+                jmsAdminOperations.removeJmsQueue("ejb2x/replyQueue");
+                jmsAdminOperations.close();
+            }
+        }
+    }
+
+    @Deployment
+    public static Archive getDeployment() {
+        final JavaArchive ejbJar = ShrinkWrap.create(JavaArchive.class, "mdb.jar");
+        ejbJar.addClasses(EJB2xMDB.class, AbstractMDB2xTestCase.class);
+        ejbJar.addPackage(JMSOperations.class.getPackage());
+        ejbJar.addClasses(JmsQueueSetup.class, TimeoutUtil.class);
+        ejbJar.addAsManifestResource(MDB21TestCase.class.getPackage(), "ejb-jar-21.xml", "ejb-jar.xml");
+        ejbJar.addAsManifestResource(new StringAsset("Dependencies: org.jboss.as.controller-client, org.jboss.dmr \n"), "MANIFEST.MF");
+        logger.info(ejbJar.toString(true));
+        return ejbJar;
+    }
+
+    @Before
+    public void initQueues() {
+        try {
+            final InitialContext ic = new InitialContext();
+
+            queue = (Queue) ic.lookup("java:jboss/ejb2x/queue");
+            replyQueue = (Queue) ic.lookup("java:jboss/ejb2x/replyQueue");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Test a simple 2.1 MDB.
+     */
+    @Test
+    public void testSimple21MDB() {
+        sendTextMessage("Say hello to " + EJB2xMDB.class.getName(), queue, replyQueue);
+        final Message reply = receiveMessage(replyQueue, TimeoutUtil.adjust(1000));
+        Assert.assertNotNull("Reply message was null on reply queue: " + replyQueue, reply);
+    }
+}
