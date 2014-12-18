76f9038448e4693c2dafa7c4251ee3e4fc69d8da
Jan Martiska <jmartisk@redhat.com>
2014-12-18 13:34:42 +0100

    JBQA-11244 Test coverage for nested expressions in annotations/dd in EJB applications
    test for @Pool annotation using a nested expression
    
    test for access-timeout attribute with a nested expression

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/cmt/timeout/TransactionTimeoutTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/cmt/timeout/TransactionTimeoutTestCase.java
@@ -25,6 +25,11 @@ import javax.naming.InitialContext;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.controller.client.helpers.ClientConstants;
+import org.jboss.dmr.ModelNode;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -33,6 +38,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -49,7 +60,10 @@ public class TransactionTimeoutTestCase {
         jar.addClass(BeanWithTimeoutValue.class);
         jar.addClass(TimeoutRemoteView.class);
         jar.addClass(TimeoutLocalView.class);
-        jar.addAsManifestResource(TransactionTimeoutTestCase.class.getPackage(), "jboss-ejb3.xml", "jboss-ejb3.xml");
+        jar.addAsManifestResource(TransactionTimeoutTestCase.class.getPackage(), "jboss-ejb3.xml",
+                "jboss-ejb3.xml");
+        jar.addAsManifestResource(TransactionTimeoutTestCase.class.getPackage(), "jboss.properties",
+                "jboss.properties");
         return jar;
     }
 
@@ -57,23 +71,34 @@ public class TransactionTimeoutTestCase {
      */
     @Test
     public void testBeanTimeouts() throws Exception {
-        TimeoutRemoteView remoteView = (TimeoutRemoteView) (new InitialContext().lookup("java:module/BeanWithTimeoutValue!org.jboss.as.test.integration.ejb.transaction.cmt.timeout.TimeoutRemoteView"));
-        TimeoutLocalView localView = (TimeoutLocalView) (new InitialContext().lookup("java:module/BeanWithTimeoutValue!org.jboss.as.test.integration.ejb.transaction.cmt.timeout.TimeoutLocalView"));
+        TimeoutRemoteView remoteView = (TimeoutRemoteView)(new InitialContext()
+                .lookup("java:module/BeanWithTimeoutValue!org.jboss.as.test.integration.ejb.transaction.cmt.timeout.TimeoutRemoteView"));
+        TimeoutLocalView localView = (TimeoutLocalView)(new InitialContext()
+                .lookup("java:module/BeanWithTimeoutValue!org.jboss.as.test.integration.ejb.transaction.cmt.timeout.TimeoutLocalView"));
 
         long timeoutValue = -1;
-        timeoutValue = (long) remoteView.getBeanTimeout();
+        timeoutValue = (long)remoteView.getBeanTimeout();
         Assert.assertEquals("Bean-level timeout failed", 5l, timeoutValue);
-        timeoutValue = (long) remoteView.getBeanMethodTimeout();
+        timeoutValue = (long)remoteView.getBeanMethodTimeout();
         Assert.assertEquals("Bean-method timeout failed", 6l, timeoutValue);
-        timeoutValue = (long) remoteView.getRemoteMethodTimeout();
+        timeoutValue = (long)remoteView.getRemoteMethodTimeout();
         Assert.assertEquals("Remote-method timeout failed", 7l, timeoutValue);
-        timeoutValue = (long) localView.getLocalViewTimeout();
+        timeoutValue = (long)localView.getLocalViewTimeout();
         Assert.assertEquals("Local-view timeout failed", 8l, timeoutValue);
     }
 
     @Test
     public void testDescriptor() throws Exception {
-        final TimeoutLocalView localView = (TimeoutLocalView) new InitialContext().lookup("java:module/DDBeanWithTimeoutValue!" + TimeoutLocalView.class.getName());
+        final TimeoutLocalView localView = (TimeoutLocalView)new InitialContext()
+                .lookup("java:module/DDBeanWithTimeoutValue!" + TimeoutLocalView.class.getName());
         assertEquals(10, localView.getLocalViewTimeout());
+    }
+
+    @Test
+    public void testDescriptorWithNestedExpressions() throws Exception {
+        final TimeoutLocalView localView = (TimeoutLocalView)new InitialContext()
+                .lookup("java:module/DDBeanWithTimeoutValueUsingNestedExpression!" + TimeoutLocalView.class
+                        .getName());
+        assertEquals(90, localView.getLocalViewTimeout());
     }
 }
