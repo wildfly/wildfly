afa4f4f2807d9d961a39a50bc45c4d0074d268ad
James Livingston <jlivings@redhat.com>
2015-01-09 14:35:12 +1000

    Add test case to ensure the default servlet is overridable

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/web/servlet/registration/DefaultServletReplacmentTestCase.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2015, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.web.servlet.registration;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.test.integration.common.HttpRequest;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+public class DefaultServletReplacmentTestCase {
+
+    @ArquillianResource
+    private URL url;
+
+    @Deployment
+    public static WebArchive single() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "single.war");
+        war.addClasses(HttpRequest.class, ReplacementServlet.class, DefaultReplacingServletContextListener.class);
+        return war;
+    }
+
+    private String performCall(URL url,String urlPattern) throws Exception {
+        return HttpRequest.get(url.toExternalForm() + urlPattern, 1000, SECONDS);
+    }
+
+
+    @Test
+    public void testLifeCycle() throws Exception {
+        String result = performCall(url, "/");
+        assertEquals("ok", result);
+    }
+}
