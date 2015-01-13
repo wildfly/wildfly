7ecf52ba69206c9a3a04027f53173aff5fbc1915
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-01-14 10:07:41 +1100

    WFLY-4247 Don't fail deployment if web fragment name is not found

--- undertow/src/main/java/org/wildfly/extension/undertow/logging/UndertowLogger.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/logging/UndertowLogger.java
@@ -228,8 +228,9 @@ public interface UndertowLogger extends BasicLogger {
     @Message(id = 45, value = "Duplicate name declared in JAR: %s")
     String invalidRelativeOrderingDuplicateName(String jar);
 
-    @Message(id = 46, value = "Unknown name declared in JAR: %s")
-    String invalidRelativeOrderingUnknownName(String jar);
+    @LogMessage(level = WARN)
+    @Message(id = 46, value = "Unknown web fragment name declared in JAR: %s")
+    void invalidRelativeOrderingUnknownName(String jar);
 
     @Message(id = 47, value = "Relative ordering conflict with JAR: %s")
     String invalidRelativeOrderingConflict(String jar);
