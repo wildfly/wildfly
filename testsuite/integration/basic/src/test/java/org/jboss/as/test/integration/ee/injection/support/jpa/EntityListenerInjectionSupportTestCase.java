609b2fc5cefde54b4a58ea6cd336bf27e939fb65
Tomas Remes <tremes@redhat.com>
2015-03-18 08:35:05 +0100

    WFLY-2387 - test injection to EntityListener.

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ee/injection/support/jpa/EntityListenerInjectionSupportTestCase.java
@@ -0,0 +1,91 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ee.injection.support.jpa;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.test.integration.ee.injection.support.Alpha;
+import org.jboss.as.test.integration.ee.injection.support.Bravo;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Matus Abaffy
+ */
+@RunWith(Arquillian.class)
+public class EntityListenerInjectionSupportTestCase {
+
+    private static final String ARCHIVE_NAME = "test";
+
+    @Deployment
+    public static Archive<?> deploy() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, ARCHIVE_NAME + ".war");
+        war.addPackage(EntityListenerInjectionSupportTestCase.class.getPackage());
+        war.addClasses(Alpha.class, Bravo.class);
+        war.addAsWebInfResource(EntityListenerInjectionSupportTestCase.class.getPackage(), "persistence.xml",
+                "classes/META-INF/persistence.xml");
+        war.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        return war;
+    }
+
+    @ArquillianResource
+    private static InitialContext iniCtx;
+
+    protected <T> T lookup(String beanName, Class<T> interfaceType) throws NamingException {
+        return interfaceType
+                .cast(iniCtx.lookup("java:global/" + ARCHIVE_NAME + "/" + beanName + "!" + interfaceType.getName()));
+    }
+
+    @Test
+    public void testCDIInjectionPerformed() throws Exception {
+        MyBean cmt = lookup("MyBean", MyBean.class);
+        cmt.createEmployee("Joe Black", "Brno 2", 0);
+        assertTrue(MyListener.isIjectionPerformed());
+    }
+
+    @Test
+    public void testInterceptorCalled() throws Exception {
+        MyListener.setInvocationCount(0);
+        int id = 1;
+        MyBean bean = lookup("MyBean", MyBean.class);
+        bean.createEmployee("Joe Black", "Brno 2", id);
+        assertEquals("EntityListener was not called.", 1, MyListener.getInvocationCount());
+        assertTrue("Interceptor was not called.", MyListenerInterceptor.wasCalled);
+        // ID should be increased by 1 in MyListenerInterceptor
+        // id++;
+        Employee emp = bean.getEmployeeById(id);
+        assertNotNull("Could not load added employee.", emp);
+    }
+}
