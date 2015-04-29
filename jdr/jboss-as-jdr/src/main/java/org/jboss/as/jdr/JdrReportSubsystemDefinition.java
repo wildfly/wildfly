6305d1f630e79c4a96a2e361c7c80a5d01214fca
Josh Kinlaw <jkinlaw@redhat.com>
2015-04-29 19:53:05 -0400

    adds UUID to subsystem definition

--- jdr/jboss-as-jdr/src/main/java/org/jboss/as/jdr/JdrReportSubsystemDefinition.java
+++ jdr/jboss-as-jdr/src/main/java/org/jboss/as/jdr/JdrReportSubsystemDefinition.java
@@ -22,18 +22,83 @@
 
 package org.jboss.as.jdr;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.jboss.as.controller.SimpleResourceDefinition;
 
 /**
  * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a> (c) 2012 Red Hat Inc.
  */
 public class JdrReportSubsystemDefinition extends SimpleResourceDefinition {
+
+    public static final String JBOSS_PROPERTY_DIR = "jboss.server.data.dir";
+
+    public static final String JDR_PROPERTY_FILE_NAME = "jdr.properties";
+
+    public static final String UUID_NAME = "UUID";
+
+    public static final String JDR_PROPERTIES_COMMENT = "JDR Properties";
+
     static final JdrReportSubsystemDefinition INSTANCE = new JdrReportSubsystemDefinition();
+
+    private String uuid;
 
     private JdrReportSubsystemDefinition() {
         super(JdrReportExtension.SUBSYSTEM_PATH, JdrReportExtension.getResourceDescriptionResolver(),
                 JdrReportSubsystemAdd.INSTANCE,
                 JdrReportSubsystemRemove.INSTANCE);
+        setUUID();
+        System.out.println("UUID: " + uuid);
     }
 
+    private void setUUID() {
+        String jbossConfig = System.getProperty(JBOSS_PROPERTY_DIR);
+        String jdrPropertiesFilePath = jbossConfig + File.separator + JdrReportExtension.SUBSYSTEM_NAME + File.separator + JDR_PROPERTY_FILE_NAME;
+        Properties jdrProperties = new Properties();
+        InputStream jdrIS = getClass().getClassLoader().getResourceAsStream(jdrPropertiesFilePath);
+        if(jdrIS != null) {
+            try {
+                jdrProperties.load(jdrIS);
+                uuid = jdrProperties.getProperty(UUID_NAME);
+                if(uuid == null) {
+                    uuid = java.util.UUID.randomUUID().toString();
+                    jdrProperties.setProperty(UUID_NAME, uuid);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            uuid = java.util.UUID.randomUUID().toString();
+            jdrProperties.setProperty(UUID_NAME, java.util.UUID.randomUUID().toString());
+            FileOutputStream fileOut = null;
+            try {
+                File file = new File(jdrPropertiesFilePath);
+                file.getParentFile().mkdirs();
+                fileOut = new FileOutputStream(jdrPropertiesFilePath);
+                jdrProperties.store(fileOut, JDR_PROPERTIES_COMMENT);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if(fileOut != null) {
+                    try {
+                        fileOut.close();
+                    }
+                    catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public String getUUID() {
+        return uuid;
+    }
 }
