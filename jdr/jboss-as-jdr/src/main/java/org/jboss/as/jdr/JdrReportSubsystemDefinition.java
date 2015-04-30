e5583b94da76f26bd9dd585d17e48bad90c4b3ef
Josh Kinlaw <jkinlaw@redhat.com>
2015-04-30 13:33:41 -0400

    removes unnecessary change

--- jdr/jboss-as-jdr/src/main/java/org/jboss/as/jdr/JdrReportSubsystemDefinition.java
+++ jdr/jboss-as-jdr/src/main/java/org/jboss/as/jdr/JdrReportSubsystemDefinition.java
@@ -28,7 +28,6 @@ import org.jboss.as.controller.SimpleResourceDefinition;
  * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a> (c) 2012 Red Hat Inc.
  */
 public class JdrReportSubsystemDefinition extends SimpleResourceDefinition {
-
     static final JdrReportSubsystemDefinition INSTANCE = new JdrReportSubsystemDefinition();
 
     private JdrReportSubsystemDefinition() {
@@ -36,5 +35,4 @@ public class JdrReportSubsystemDefinition extends SimpleResourceDefinition {
                 JdrReportSubsystemAdd.INSTANCE,
                 JdrReportSubsystemRemove.INSTANCE);
     }
-
 }
