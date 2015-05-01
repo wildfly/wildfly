2a923a288c548d5ab8ac0525dd17c94f2fc2b43d
Richard Achmatowicz <rachmatowicz@jboss.com>
2015-05-01 18:06:26 -0400

    Use proper names for extended cluster test framework constants.

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/ExtendedClusterAbstractTestCase.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/ExtendedClusterAbstractTestCase.java
@@ -33,14 +33,14 @@ public abstract class ExtendedClusterAbstractTestCase extends ClusterAbstractTes
 
     @Override
     public void beforeTestMethod() {
-        this.start(XSITE_CONTAINERS);
-        this.deploy(XSITE_DEPLOYMENTS);
+        this.start(EXTENDED_CONTAINERS);
+        this.deploy(EXTENDED_DEPLOYMENTS);
     }
 
     @Override
     public void afterTestMethod() {
-        this.start(XSITE_CONTAINERS);
-        this.undeploy(XSITE_DEPLOYMENTS);
+        this.start(EXTENDED_CONTAINERS);
+        this.undeploy(EXTENDED_DEPLOYMENTS);
     }
 }
 
