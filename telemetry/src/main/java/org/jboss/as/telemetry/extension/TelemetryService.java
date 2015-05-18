6d29621acd84bdfdb91c99916d4561698538e846
Josh Kinlaw <jkinlaw@redhat.com>
2015-05-18 09:40:51 -0400

    adds redhat-support-lib

--- telemetry/src/main/java/org/jboss/as/telemetry/extension/TelemetryService.java
+++ telemetry/src/main/java/org/jboss/as/telemetry/extension/TelemetryService.java
@@ -1,5 +1,16 @@
 package org.jboss.as.telemetry.extension;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Properties;
+
+import org.apache.commons.net.ftp.FTPClient;
 import org.jboss.as.jdr.JdrReport;
 import org.jboss.as.jdr.JdrReportCollector;
 import org.jboss.logging.Logger;
@@ -9,14 +20,34 @@ import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 
+import com.redhat.gss.redhat_support_lib.errors.FTPException;
+import com.redhat.gss.redhat_support_lib.errors.RequestException;
+import com.redhat.gss.redhat_support_lib.helpers.ConfigHelper;
+import com.redhat.gss.redhat_support_lib.infrastructure.BaseQuery;
+import com.redhat.gss.redhat_support_lib.web.ConnectionManager;
+
 /**
  * @author <a href="mailto:jkinlaw@redhat.com">Josh Kinlaw</a>
  */
-public class TelemetryService implements Service<TelemetryService> {
+public class TelemetryService extends BaseQuery implements Service<TelemetryService> {
 
     public static final long MILLISECOND_TO_DAY = 86400000;
 
+    public static final String JBOSS_PROPERTY_DIR = "jboss.server.data.dir";
+
+    public static final String TELEMETRY_PROPERTY_FILE_NAME = "telemetry.properties";
+
+    public static final String BASE_URI = "baseUri";
+
+    public static final String TELEMETRY_PATH = "telemetryPath";
+
+    public static final String JDR_DESCRIPTION = "JDR for UUID {uuid}";
+
     private static volatile TelemetryService instance;
+
+    private ConnectionManager connectionManager = null;
+
+    private String telemetryUri;
 
     Logger log = Logger.getLogger(TelemetryService.class);
 
@@ -32,6 +63,17 @@ public class TelemetryService implements Service<TelemetryService> {
     private TelemetryService(long tick, boolean enabled) {
         this.frequency = tick;
         this.enabled = enabled;
+        // TODO: move properties to properties file
+        String username = "";
+        String password = "";
+        String url = "";
+        String proxyUser = null;
+        String proxyPassword = null;
+        URL proxyUrl = null;
+        int proxyPort = 0;
+        String userAgent = null;
+        this.connectionManager = new ConnectionManager(new ConfigHelper(
+                username, password, url, proxyUser, proxyPassword, proxyUrl, proxyPort, userAgent, false));
     }
 
     public static TelemetryService getInstance(long tick, boolean enabled) {
@@ -57,6 +99,9 @@ public class TelemetryService implements Service<TelemetryService> {
                     while(enabled) {
                         try{
                             JdrReport report = jdrCollector.collect();
+                            System.out.println("report location: " + report.getLocation());
+                            String response = sendJdr(report.getLocation());
+                            System.out.println("telemetry response: " + response);
                             output.wait(frequency * MILLISECOND_TO_DAY);
                         } catch (InterruptedException e) {
                             return;
@@ -136,5 +181,81 @@ public class TelemetryService implements Service<TelemetryService> {
 
     public void setJdrReportCollector(JdrReportCollector jdrCollector) {
         this.jdrCollector = jdrCollector;
+    }
+
+    public void setTelemetryUri() {
+        String jbossConfig = System.getProperty(JBOSS_PROPERTY_DIR);
+        String telemetryPropFilePath = jbossConfig + File.separator + TelemetryExtension.SUBSYSTEM_NAME +
+                    File.separator + TELEMETRY_PROPERTY_FILE_NAME;
+        Properties telemetryProperties = new Properties();
+        InputStream telemetryIs = getClass().getClassLoader().getResourceAsStream(telemetryPropFilePath);
+        if(telemetryIs != null) {
+            try {
+                telemetryProperties.load(telemetryIs);
+                telemetryUri = telemetryProperties.getProperty(BASE_URI) +
+                        telemetryProperties.getProperty(TELEMETRY_PATH);
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String sendJdr(String fileName) {
+        // TODO: replace with property
+        String url = "/rs/telemetry/api/uploads";
+        String uuid = "[uuid placeholder]";
+        String description = JDR_DESCRIPTION.replace("{uuid}", uuid);
+        File file = new File(fileName);
+//        List<String> queryParams = new ArrayList<String>();
+//        queryParams.add("public=" + Boolean.toString(publicVis));
+        String uri = null;
+        // TODO: move 2GB to constant
+        if (file.length() > 200000L) {
+            FTPClient ftp = null;
+            FileInputStream fis = null;
+            try {
+                ftp = connectionManager.getFTP();
+                ftp.cwd(connectionManager.getConfig().getFtpDir());
+                ftp.enterLocalPassiveMode();
+                fis = new FileInputStream(file);
+                if (!ftp.storeFile(file.getName(), fis)) {
+                    throw new FTPException("Error during FTP store file.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(fis != null) {
+                        fis.close();
+                    }
+                    if(ftp != null) {
+                        ftp.logout();
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            String fullUrl = connectionManager.getConfig().getUrl() + url;
+            try {
+                uri = upload(connectionManager.getConnection(), fullUrl, file,
+                        description).getStringHeaders().getFirst("location");
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (RequestException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return uri;
     }
 }
