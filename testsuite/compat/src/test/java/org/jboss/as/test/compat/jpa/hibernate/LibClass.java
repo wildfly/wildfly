c98b73ce9beb1260c2be90cd74e03e136285f899
Scott Marlow <smarlow@redhat.com>
2015-05-19 14:59:19 -0400

    WFLY-3449 add test for deploying Hibernate orm 4.1.x jars with application

--- testsuite/compat/src/test/java/org/jboss/as/test/compat/jpa/hibernate/LibClass.java
+++ testsuite/compat/src/test/java/org/jboss/as/test/compat/jpa/hibernate/LibClass.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2011, Red Hat, Inc., and individual contributors
+ * Copyright 2015, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -22,46 +22,11 @@
 
 package org.jboss.as.test.compat.jpa.hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 /**
- * Employee entity class
+ * LibClass
  *
  * @author Scott Marlow
  */
-@Entity
-public class Employee  {
+public class LibClass {
 
-    @Id
-    private int id;
-
-    private String name;
-
-    private String address;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getId() {
-
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
