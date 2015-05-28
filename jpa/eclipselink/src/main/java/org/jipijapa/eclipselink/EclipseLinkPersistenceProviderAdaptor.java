johnaoahra80/jboss-as

WFLY-4658 merge jipijapa source into WildFly and remove Hibernate 3 integration (no longer needed in WildFly)

jpa/eclipselink/src/main/java/org/jipijapa/eclipselink/EclipseLinkPersistenceProviderAdaptor.java
@@ -1,0 +1,99 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jipijapa.eclipselink;
+
+import java.util.Map;
+
+import org.jboss.logging.Logger;
+import org.jipijapa.plugin.spi.JtaManager;
+import org.jipijapa.plugin.spi.ManagementAdaptor;
+import org.jipijapa.plugin.spi.PersistenceProviderAdaptor;
+import org.jipijapa.plugin.spi.PersistenceUnitMetadata;
+import org.jipijapa.plugin.spi.Platform;
+
+public class EclipseLinkPersistenceProviderAdaptor implements
+        PersistenceProviderAdaptor {
+
+    private final Logger logger = Logger.getLogger(EclipseLinkPersistenceProviderAdaptor.class);
+
+    public static final String
+        ECLIPSELINK_TARGET_SERVER = "eclipselink.target-server",
+        ECLIPSELINK_ARCHIVE_FACTORY = "eclipselink.archive.factory",
+        ECLIPSELINK_LOGGING_LOGGER = "eclipselink.logging.logger";
+
+    public EclipseLinkPersistenceProviderAdaptor() {
+        logger.trace("EclipseLink integration activated");
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public void addProviderProperties(Map properties, PersistenceUnitMetadata pu) {
+        if (!properties.containsKey(ECLIPSELINK_TARGET_SERVER)) {
+            properties.put(ECLIPSELINK_TARGET_SERVER, JBossAS7ServerPlatform.class.getName());
+            properties.put(ECLIPSELINK_ARCHIVE_FACTORY, JBossArchiveFactoryImpl.class.getName());
+            properties.put(ECLIPSELINK_LOGGING_LOGGER, JBossLogger.class.getName());
+        }
+        logger.trace("Property " + ECLIPSELINK_TARGET_SERVER + " set to " + properties.get(ECLIPSELINK_TARGET_SERVER));
+        logger.trace("Property " + ECLIPSELINK_ARCHIVE_FACTORY + " set to " + properties.get(ECLIPSELINK_ARCHIVE_FACTORY));
+        logger.trace("Property " + ECLIPSELINK_LOGGING_LOGGER + " set to " + properties.get(ECLIPSELINK_LOGGING_LOGGER));
+    }
+
+    @Override
+    public void injectJtaManager(JtaManager jtaManager) {
+        // No action required, EclipseLink looks this up from JNDI
+    }
+
+    @Override
+    public void injectPlatform(Platform platform) {
+
+    }
+
+    @Override
+    public void addProviderDependencies(PersistenceUnitMetadata pu) {
+        // No action required
+    }
+
+    @Override
+    public void beforeCreateContainerEntityManagerFactory(
+            PersistenceUnitMetadata pu) {
+        // no action required
+    }
+
+    @Override
+    public void afterCreateContainerEntityManagerFactory(
+            PersistenceUnitMetadata pu) {
+        // TODO: Force creation of metamodel here?
+    }
+
+    @Override
+    public ManagementAdaptor getManagementAdaptor() {
+        // no action required
+        return null;
+    }
+
+    @Override
+    public boolean doesScopedPersistenceUnitNameIdentifyCacheRegionName(PersistenceUnitMetadata pu) {
+        return false;
+    }
+
+    @Override
+    public void cleanup(PersistenceUnitMetadata pu) {
+        // no action required
+    }
+
+}

jpa/hibernate4_1/src/main/java/org/jboss/as/jpa/hibernate4/HibernateLazyNamedInputStream.java
@@ -1,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.as.jpa.hibernate4;
+
+import static org.jboss.as.jpa.hibernate4.JpaMessages.MESSAGES;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.hibernate.ejb.packaging.NamedInputStream;
+
+/**
+ * Lazy named input stream.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ *         Scott Marlow
+ */
+public abstract class HibernateLazyNamedInputStream extends NamedInputStream {
+    public HibernateLazyNamedInputStream(String name) {
+        super(name, null);
+    }
+
+    /**
+     * Get lazy input stream.
+     *
+     * @return the input stream
+     * @throws java.io.IOException for any I/O error
+     */
+    protected abstract InputStream getLazyStream() throws IOException;
+
+    @Override
+    public InputStream getStream() {
+        try {
+            return getLazyStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void setStream(InputStream stream) {
+        throw MESSAGES.cannotChangeInputStream();
+    }
+}

jpa/hibernate4_1/src/main/java/org/jboss/as/jpa/hibernate4/HibernatePatternFilter.java
@@ -1,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.as.jpa.hibernate4;
+
+import static org.jboss.as.jpa.hibernate4.JpaMessages.MESSAGES;
+
+import org.jboss.vfs.VirtualFile;
+import org.jboss.vfs.VirtualFileFilter;
+
+/**
+ * Mock work of NativeScanner matching.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author Scott Marlow
+ */
+public class HibernatePatternFilter implements VirtualFileFilter {
+    private static final String SLASH = "/";
+    private static final String PREFIX = "**/*";
+    private final String pattern;
+    private final boolean exact;
+
+    public HibernatePatternFilter(String pattern) {
+        if (pattern == null)
+            throw MESSAGES.nullVar("pattern");
+
+        exact = !pattern.contains(SLASH); // no path split or glob
+        if (exact == false && (pattern.startsWith(PREFIX))) {
+            this.pattern = pattern.substring(4);
+        } else {
+            this.pattern = pattern;
+        }
+    }
+
+    protected boolean accepts(String name) {
+        return exact ? name.equals(pattern) : name.endsWith(pattern);
+    }
+
+    public boolean accepts(VirtualFile file) {
+        String name = exact ? file.getName() : file.getPathName();
+        return accepts(name);
+    }
+
+}

jpa/hibernate4_1/src/main/java/org/jboss/as/jpa/hibernate4/HibernateVirtualFileNamedInputStream.java
@@ -1,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.as.jpa.hibernate4;
+
+import static org.jboss.as.jpa.hibernate4.JpaMessages.MESSAGES;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jboss.vfs.VirtualFile;
+
+/**
+ * VFS named input stream.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author Scott Marlow
+ */
+public class HibernateVirtualFileNamedInputStream extends HibernateLazyNamedInputStream {
+    private VirtualFile file;
+
+    private static String name(VirtualFile file) {
+        if (file == null)
+            throw MESSAGES.nullVar("file");
+        return file.getName();
+    }
+
+    public HibernateVirtualFileNamedInputStream(VirtualFile file) {
+        super(name(file));
+        this.file = file;
+    }
+
+    protected InputStream getLazyStream() throws IOException {
+        return file.openStream();
+    }
+}

wildfly/wildfly

jpa/eclipselink/src/main/java/org/jipijapa/eclipselink/EclipseLinkPersistenceProviderAdaptor.java
@@ -1,0 +1,90 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jipijapa.eclipselink;
+
+import java.util.Map;
+
+
+import org.jipijapa.plugin.spi.JtaManager;
+import org.jipijapa.plugin.spi.ManagementAdaptor;
+import org.jipijapa.plugin.spi.PersistenceProviderAdaptor;
+import org.jipijapa.plugin.spi.PersistenceUnitMetadata;
+import org.jipijapa.plugin.spi.Platform;
+
+public class EclipseLinkPersistenceProviderAdaptor implements
+        PersistenceProviderAdaptor {
+
+    public static final String
+        ECLIPSELINK_TARGET_SERVER = "eclipselink.target-server",
+        ECLIPSELINK_ARCHIVE_FACTORY = "eclipselink.archive.factory",
+        ECLIPSELINK_LOGGING_LOGGER = "eclipselink.logging.logger";
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public void addProviderProperties(Map properties, PersistenceUnitMetadata pu) {
+        if (!properties.containsKey(ECLIPSELINK_TARGET_SERVER)) {
+            properties.put(ECLIPSELINK_TARGET_SERVER, WildFlyServerPlatform.class.getName());
+            properties.put(ECLIPSELINK_ARCHIVE_FACTORY, JBossArchiveFactoryImpl.class.getName());
+            properties.put(ECLIPSELINK_LOGGING_LOGGER, JBossLogger.class.getName());
+        }
+    }
+
+    @Override
+    public void injectJtaManager(JtaManager jtaManager) {
+        // No action required, EclipseLink looks this up from JNDI
+    }
+
+    @Override
+    public void injectPlatform(Platform platform) {
+
+    }
+
+    @Override
+    public void addProviderDependencies(PersistenceUnitMetadata pu) {
+        // No action required
+    }
+
+    @Override
+    public void beforeCreateContainerEntityManagerFactory(
+            PersistenceUnitMetadata pu) {
+        // no action required
+    }
+
+    @Override
+    public void afterCreateContainerEntityManagerFactory(
+            PersistenceUnitMetadata pu) {
+        // TODO: Force creation of metamodel here?
+    }
+
+    @Override
+    public ManagementAdaptor getManagementAdaptor() {
+        // no action required
+        return null;
+    }
+
+    @Override
+    public boolean doesScopedPersistenceUnitNameIdentifyCacheRegionName(PersistenceUnitMetadata pu) {
+        return false;
+    }
+
+    @Override
+    public void cleanup(PersistenceUnitMetadata pu) {
+        // no action required
+    }
+
+}

jpa/hibernate4_1/src/main/java/org/jboss/as/jpa/hibernate4/HibernateLazyNamedInputStream.java
@@ -1,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.as.jpa.hibernate4;
+
+import static org.jboss.as.jpa.hibernate4.JpaLogger.JPA_LOGGER;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.hibernate.ejb.packaging.NamedInputStream;
+
+/**
+ * Lazy named input stream.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ *         Scott Marlow
+ */
+public abstract class HibernateLazyNamedInputStream extends NamedInputStream {
+    public HibernateLazyNamedInputStream(String name) {
+        super(name, null);
+    }
+
+    /**
+     * Get lazy input stream.
+     *
+     * @return the input stream
+     * @throws java.io.IOException for any I/O error
+     */
+    protected abstract InputStream getLazyStream() throws IOException;
+
+    @Override
+    public InputStream getStream() {
+        try {
+            return getLazyStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void setStream(InputStream stream) {
+        throw JPA_LOGGER.cannotChangeInputStream();
+    }
+}

jpa/hibernate4_1/src/main/java/org/jboss/as/jpa/hibernate4/HibernatePatternFilter.java
@@ -1,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.as.jpa.hibernate4;
+
+import static org.jboss.as.jpa.hibernate4.JpaLogger.JPA_LOGGER;
+
+import org.jboss.vfs.VirtualFile;
+import org.jboss.vfs.VirtualFileFilter;
+
+/**
+ * Mock work of NativeScanner matching.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author Scott Marlow
+ */
+public class HibernatePatternFilter implements VirtualFileFilter {
+    private static final String SLASH = "/";
+    private static final String PREFIX = "**/*";
+    private final String pattern;
+    private final boolean exact;
+
+    public HibernatePatternFilter(String pattern) {
+        if (pattern == null)
+            throw JPA_LOGGER.nullVar("pattern");
+
+        exact = !pattern.contains(SLASH); // no path split or glob
+        if (exact == false && (pattern.startsWith(PREFIX))) {
+            this.pattern = pattern.substring(4);
+        } else {
+            this.pattern = pattern;
+        }
+    }
+
+    protected boolean accepts(String name) {
+        return exact ? name.equals(pattern) : name.endsWith(pattern);
+    }
+
+    public boolean accepts(VirtualFile file) {
+        String name = exact ? file.getName() : file.getPathName();
+        return accepts(name);
+    }
+
+}

jpa/hibernate4_1/src/main/java/org/jboss/as/jpa/hibernate4/HibernateVirtualFileNamedInputStream.java
@@ -1,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.as.jpa.hibernate4;
+
+import static org.jboss.as.jpa.hibernate4.JpaLogger.JPA_LOGGER;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jboss.vfs.VirtualFile;
+
+/**
+ * VFS named input stream.
+ *
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ * @author Scott Marlow
+ */
+public class HibernateVirtualFileNamedInputStream extends HibernateLazyNamedInputStream {
+    private VirtualFile file;
+
+    private static String name(VirtualFile file) {
+        if (file == null)
+            throw JPA_LOGGER.nullVar("file");
+        return file.getName();
+    }
+
+    public HibernateVirtualFileNamedInputStream(VirtualFile file) {
+        super(name(file));
+        this.file = file;
+    }
+
+    protected InputStream getLazyStream() throws IOException {
+        return file.openStream();
+    }
+}