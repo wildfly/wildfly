f14582d15aa5411876bd07f127f5f9af4a31fbda
Josh Kinlaw <jkinlaw@redhat.com>
2015-06-18 10:21:22 -0400

    adds insights system check and creates new system if current systems UUID is not in insights; uploads jdr to insights for system

--- telemetry/src/main/java/org/jboss/as/telemetry/extension/TelemetryService.java
+++ telemetry/src/main/java/org/jboss/as/telemetry/extension/TelemetryService.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.text.ParseException;
 import java.util.Properties;
 
+import javax.ws.rs.core.Response;
 
 //import org.apache.commons.net.ftp.FTPClient;
 import org.jboss.as.jdr.JdrReport;
@@ -21,20 +22,20 @@ import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 
-
 //import com.redhat.gss.redhat_support_lib.errors.FTPException;
 import com.redhat.gss.redhat_support_lib.errors.RequestException;
 import com.redhat.gss.redhat_support_lib.helpers.ConfigHelper;
-import com.redhat.gss.redhat_support_lib.infrastructure.BaseQuery;
+import com.redhat.gss.redhat_support_lib.infrastructure.Telemetry;
 import com.redhat.gss.redhat_support_lib.web.ConnectionManager;
 
 /**
  * @author <a href="mailto:jkinlaw@redhat.com">Josh Kinlaw</a>
  */
-public class TelemetryService extends BaseQuery implements Service<TelemetryService> {
+public class TelemetryService extends Telemetry implements
+        Service<TelemetryService> {
 
-    //public static final long MILLISECOND_TO_DAY = 86400000;
-    public static final long MILLISECOND_TO_DAY = 1;
+    public static final long MILLISECOND_TO_DAY = 86400000;
+//    public static final long MILLISECOND_TO_DAY = 1; //for testing purposes
 
     public static final String JBOSS_PROPERTY_DIR = "jboss.server.data.dir";
 
@@ -42,8 +43,9 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
 
     public static final String TELEMETRY_DESCRIPTION = "Properties file consisting of RHN login information and telemetry/insights URL";
 
-    public static final String DEFAULT_BASE_URL = "https://api.access.qa.redhat.com";
-    public static final String DEFAULT_ENDPOINT = "/api/telemetry/uploads/";
+    public static final String DEFAULT_BASE_URL = "https://access.redhat.com";
+    public static final String DEFAULT_TELEMETRY_ENDPOINT = "/r/insights/v1/uploads/";
+    public static final String DEFAULT_SYSTEM_ENDPOINT = "/r/insights/v1/systems/";
 
     /**
      * Properties that can be set via the properties file.
@@ -57,7 +59,8 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
     public static final String PROXY_PORT = "proxyPort";
     public static final String USER_AGENT = "userAgent";
     public static final String BASE_URI = "baseUri";
-    public static final String TELEMETRY_ENDPOINT = "endpoint";
+    public static final String TELEMETRY_ENDPOINT = "telemetryEndpoint";
+    public static final String SYSTEM_ENDPOINT = "systemEndpoint";
 
     public static final String JDR_DESCRIPTION = "JDR for UUID {uuid}";
 
@@ -65,10 +68,16 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
 
     /**
      * Endpoint of url which when added to the end of the url reveals the
-     * location of where to send the JDR
-     * Should take the format of /telemetry/endpoint/
+     * location of where to send the JDR Should take the format of
+     * /telemetry/endpoint/
      */
     private String telemetryEndpoint;
+
+    /**
+     * Endponit of url which when added to the end of the url reveals the
+     * location of where to query for the current system uuid
+     */
+    private String systemEndpoint;
 
     private ConnectionManager connectionManager = null;
 
@@ -94,14 +103,13 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
     }
 
     public static TelemetryService getInstance(long tick, boolean enabled) {
-        if(instance == null) {
+        if (instance == null) {
             synchronized (TelemetryService.class) {
-                if(instance == null) {
+                if (instance == null) {
                     instance = new TelemetryService(tick, enabled);
                 }
             }
-        }
-        else {
+        } else {
             instance.frequency = tick;
             instance.enabled = enabled;
         }
@@ -112,24 +120,22 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
         return new Thread() {
             @Override
             public void run() {
-                synchronized(output) {
-                    while(enabled) {
-                        try{
+                synchronized (output) {
+                    while (enabled) {
+                        try {
                             JdrReport report = jdrCollector.collect();
-                            System.out.println("report location: " + report.getLocation());
-                            String response = sendJdr(report.getLocation());
-                            System.out.println("telemetry response: " + response);
+                            sendJdr(report.getLocation());
                             output.wait(frequency * MILLISECOND_TO_DAY);
                         } catch (InterruptedException e) {
                             return;
                         } catch (Exception e) {
                             // TODO: log.error
-                            // TODO: implement catch blocks for JDR exceptions that can be handled
+                            // TODO: implement catch blocks for JDR exceptions
+                            // that can be handled
                             e.printStackTrace();
                             try {
                                 output.wait(frequency * MILLISECOND_TO_DAY);
-                            }
-                            catch (InterruptedException e2) {
+                            } catch (InterruptedException e2) {
                                 e2.printStackTrace();
                             }
                         }
@@ -152,7 +158,7 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
 
     @Override
     public void stop(StopContext arg0) {
-        if(output != null && output.isAlive()) {
+        if (output != null && output.isAlive()) {
             output.interrupt();
         }
     }
@@ -160,7 +166,7 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
     void setFrequency(long frequency) {
         log.info("Frequency of Telemetry Subsystem updated to " + frequency);
         this.frequency = frequency;
-        synchronized(output) {
+        synchronized (output) {
             output.notify();
         }
     }
@@ -170,7 +176,8 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
     }
 
     public static ServiceName createServiceName() {
-        return ServiceName.JBOSS.append(org.jboss.as.telemetry.extension.TelemetryExtension.SUBSYSTEM_NAME);
+        return ServiceName.JBOSS
+                .append(org.jboss.as.telemetry.extension.TelemetryExtension.SUBSYSTEM_NAME);
     }
 
     public boolean getEnabled() {
@@ -179,7 +186,7 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
-        if(enabled && (output == null || !output.isAlive())) {
+        if (enabled && (output == null || !output.isAlive())) {
             log.info("Telemetry Subsystem Enabled");
             output = initializeOutput();
             try {
@@ -187,11 +194,10 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
             } catch (StartException e) {
                 e.printStackTrace();
             }
-        }
-        else if(!enabled) {
+        } else if (!enabled) {
             log.info("Telemetry Subsystem Disabled");
         }
-        synchronized(output) {
+        synchronized (output) {
             output.notify();
         }
     }
@@ -226,34 +232,37 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
         setRhnUid(rhnUid);
         setRhnPw(rhnPw);
         String jbossConfig = System.getProperty(JBOSS_PROPERTY_DIR);
-        String propertiesFilePath = jbossConfig + File.separator + TelemetryExtension.SUBSYSTEM_NAME + File.separator + TELEMETRY_PROPERTY_FILE_NAME;
+        String propertiesFilePath = jbossConfig + File.separator
+                + TelemetryExtension.SUBSYSTEM_NAME + File.separator
+                + TELEMETRY_PROPERTY_FILE_NAME;
         Properties properties = new Properties();
         FileOutputStream fileOut = null;
         File file = new File(propertiesFilePath);
         try {
-            if(!file.exists()) {
+            if (!file.exists()) {
                 file.getParentFile().mkdirs();
                 properties.setProperty(URL, DEFAULT_BASE_URL);
-                properties.setProperty(TELEMETRY_ENDPOINT, DEFAULT_ENDPOINT);
-            }
-            else {
+                properties.setProperty(TELEMETRY_ENDPOINT,
+                        DEFAULT_TELEMETRY_ENDPOINT);
+            } else {
                 properties.load(new FileInputStream(propertiesFilePath));
             }
             properties.setProperty(USERNAME, rhnUid);
             properties.setProperty(PASSWORD, rhnPw);
+            properties.setProperty(URL, DEFAULT_BASE_URL);
+            properties.setProperty(TELEMETRY_ENDPOINT,
+                    DEFAULT_TELEMETRY_ENDPOINT);
+            properties.setProperty(SYSTEM_ENDPOINT, DEFAULT_SYSTEM_ENDPOINT);
             fileOut = new FileOutputStream(file);
             properties.store(fileOut, TELEMETRY_DESCRIPTION);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            if(fileOut != null) {
+        } finally {
+            if (fileOut != null) {
                 try {
                     fileOut.close();
                     setConnectionManager();
-                }
-                catch(IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -270,11 +279,13 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
         int proxyPort = 0;
         String userAgent = null;
         String jbossConfig = System.getProperty(JBOSS_PROPERTY_DIR);
-        String propertiesFilePath = jbossConfig + File.separator + TelemetryExtension.SUBSYSTEM_NAME + File.separator + TELEMETRY_PROPERTY_FILE_NAME;
+        String propertiesFilePath = jbossConfig + File.separator
+                + TelemetryExtension.SUBSYSTEM_NAME + File.separator
+                + TELEMETRY_PROPERTY_FILE_NAME;
         Properties properties = new Properties();
         FileInputStream fis = null;
         File file = new File(propertiesFilePath);
-        if(file.exists()) {
+        if (file.exists()) {
             try {
                 fis = new FileInputStream(propertiesFilePath);
                 properties.load(fis);
@@ -283,29 +294,17 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
                 password = properties.getProperty(PASSWORD);
                 url = properties.getProperty(URL);
                 telemetryEndpoint = properties.getProperty(TELEMETRY_ENDPOINT);
+                systemEndpoint = properties.getProperty(SYSTEM_ENDPOINT);
 
-                // setting optional fields
-//                proxyUser = properties.getProperty(PROXY_USER);
-//                proxyPassword = properties.getProperty(PROXY_PASSWORD);
-//                if(properties.containsKey(USER_AGENT)) {
-//                    userAgent = properties.getProperty(USER_AGENT);
-//                }
-//                if(properties.containsKey(PROXY_PORT)) {
-//                    proxyPort = Integer.parseInt(properties.getProperty(PROXY_PORT));
-//                }
-//                if(properties.containsKey(PROXY_URL)) {
-//                    proxyUrl = new URL(properties.getProperty(PROXY_URL));
-//                }
-                this.connectionManager = new ConnectionManager(new ConfigHelper(propertiesFilePath));
+                this.connectionManager = new ConnectionManager(
+                        new ConfigHelper(propertiesFilePath));
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            finally {
-                if(fis != null) {
+            } finally {
+                if (fis != null) {
                     try {
                         fis.close();
-                    }
-                    catch(IOException e) {
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
@@ -313,17 +312,35 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
         }
     }
 
-    private String sendJdr(String fileName) {
+    private void sendJdr(String fileName) {
         // TODO: replace with JdrReport uuid property
-        String uuid = "uuid_placeholder";
+        String uuid = "6fdc7b27-2c64-4d73-b04c-aa3ddafeb05e"; // need to replace this with wildfly UUID
         String description = JDR_DESCRIPTION.replace("{uuid}", uuid);
         File file = new File(fileName);
-        String uri = null;
-        String fullUrl = connectionManager.getConfig().getUrl() + telemetryEndpoint + uuid;
-        System.out.println("connectionManager: " + connectionManager + "\nfullUrl: " + fullUrl + "\nfile : " + file.getPath() + "\ndescription: " + description);
+        String fullUrl = connectionManager.getConfig().getUrl()
+                + telemetryEndpoint + uuid;
+        String systemUrl = "";
         try {
-            uri = upload(connectionManager.getConnection(), fullUrl, file,
-                    description).getStringHeaders().getFirst("location");
+            systemUrl = new URL(new URL(connectionManager.getConfig().getUrl()), systemEndpoint).toString();
+            Response systemUuid = get(connectionManager.getConnection(),
+                    new URL(new URL(systemUrl), uuid).toString());
+        } catch (RequestException e) {
+            if(e.getMessage().contains("" + Response.Status.NOT_FOUND.getStatusCode())) {
+                try {
+                    addSystem(connectionManager.getConnection(), systemUrl, uuid);
+                } catch (RequestException exception) {
+                    exception.printStackTrace();
+                }
+                catch (MalformedURLException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            upload(connectionManager.getConnection(), new URL(new URL(new URL(connectionManager.getConfig().getUrl()), telemetryEndpoint), uuid).toString(), file,
+                    description);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
@@ -333,6 +350,5 @@ public class TelemetryService extends BaseQuery implements Service<TelemetryServ
         } catch (RequestException e) {
             e.printStackTrace();
         }
-        return uri;
     }
 }
