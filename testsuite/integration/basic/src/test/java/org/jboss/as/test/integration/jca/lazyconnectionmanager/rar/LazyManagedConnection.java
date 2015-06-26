24443bf47d694553f8b01256901b2f9a21d1a0e2
Martin Simka <msimka@redhat.com>
2015-06-26 17:01:39 +0200

    tests for JBJCA-747, JBJCA-825, JBJCA-826
    https://issues.jboss.org/browse/JBJCA-747 LazyAssociatableConnectionManager/NoTx
    https://issues.jboss.org/browse/JBJCA-825 LazyAssociatableConnectionManager/Tx
    https://issues.jboss.org/browse/JBJCA-826 LazyEnlistableConnectionManager

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/lazyconnectionmanager/rar/LazyManagedConnection.java
@@ -0,0 +1,221 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.jca.lazyconnectionmanager.rar;
+
+import org.jboss.logging.Logger;
+
+import javax.resource.NotSupportedException;
+import javax.resource.ResourceException;
+import javax.resource.spi.ConnectionEvent;
+import javax.resource.spi.ConnectionEventListener;
+import javax.resource.spi.ConnectionManager;
+import javax.resource.spi.ConnectionRequestInfo;
+import javax.resource.spi.DissociatableManagedConnection;
+import javax.resource.spi.LazyEnlistableConnectionManager;
+import javax.resource.spi.LazyEnlistableManagedConnection;
+import javax.resource.spi.LocalTransaction;
+import javax.resource.spi.ManagedConnection;
+import javax.resource.spi.ManagedConnectionMetaData;
+import javax.security.auth.Subject;
+import javax.transaction.xa.XAResource;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:jesper.pedersen@ironjacamar.org">Jesper Pedersen</a>
+ * @author <a href="mailto:msimka@redhat.com">Martin Simka</a>
+ */
+public class LazyManagedConnection implements ManagedConnection, DissociatableManagedConnection,
+        LazyEnlistableManagedConnection {
+
+    private static Logger logger = Logger.getLogger(LazyManagedConnection.class);
+
+    private boolean localTransaction;
+    private boolean xaTransaction;
+    private boolean enlisted;
+    private LazyConnectionImpl connection;
+    private List<ConnectionEventListener> listeners;
+    private PrintWriter logwriter;
+    private LazyLocalTransaction lazyLocalTransaction;
+    private LazyXAResource lazyXAResource;
+    private ConnectionManager cm;
+    private LazyManagedConnectionFactory mcf;
+
+    public LazyManagedConnection(boolean localTransaction, boolean xaTransaction, ConnectionManager cm, LazyManagedConnectionFactory mcf) {
+        logger.trace("#LazyManagedConnection");
+
+        this.localTransaction = localTransaction;
+        this.xaTransaction = xaTransaction;
+        this.cm = cm;
+        this.mcf = mcf;
+        this.enlisted = false;
+        this.listeners = Collections.synchronizedList(new ArrayList<ConnectionEventListener>(1));
+
+        if (localTransaction)
+            this.lazyLocalTransaction = new LazyLocalTransaction(this);
+        if (xaTransaction)
+            this.lazyXAResource = new LazyXAResource(this);
+    }
+
+    @Override
+    public void dissociateConnections() throws ResourceException {
+        logger.trace("#LazyManagedConnection.dissociateConnections");
+        if (connection != null) {
+            connection.setManagedConnection(null);
+            connection = null;
+        }
+
+    }
+
+    @Override
+    public Object getConnection(Subject subject, ConnectionRequestInfo cri) throws ResourceException {
+        logger.trace("#LazyManagedConnection.getConnection");
+
+        if (connection != null) {
+            connection.setManagedConnection(null);
+        }
+
+        connection = new LazyConnectionImpl(cm, this, mcf, cri);
+        return connection;
+    }
+
+    @Override
+    public void destroy() throws ResourceException {
+        logger.trace("#LazyManagedConnection.destroy");
+    }
+
+    @Override
+    public void cleanup() throws ResourceException {
+        logger.trace("#LazyManagedConnection.cleanup");
+        if (connection != null) {
+            connection.setManagedConnection(null);
+            connection = null;
+        }
+    }
+
+    @Override
+    public void associateConnection(Object connection) throws ResourceException {
+        logger.trace("#LazyManagedConnection.associateConnection");
+        if (this.connection != null) {
+            this.connection.setManagedConnection(null);
+        }
+
+        if (connection != null) {
+            if (!(connection instanceof LazyConnectionImpl))
+                throw new ResourceException("Connection isn't LazyConnectionImpl: " + connection.getClass().getName());
+
+            this.connection = (LazyConnectionImpl) connection;
+            this.connection.setManagedConnection(this);
+        } else {
+            this.connection = null;
+        }
+    }
+
+    @Override
+    public void addConnectionEventListener(ConnectionEventListener listener) {
+        logger.trace("#LazyManagedConnection.addConnectionEventListener");
+        if (listener == null)
+            throw new IllegalArgumentException("Listener is null");
+        listeners.add(listener);
+
+    }
+
+    @Override
+    public void removeConnectionEventListener(ConnectionEventListener listener) {
+        logger.trace("#LazyManagedConnection.removeConnectionEventListener");
+        if (listener == null)
+            throw new IllegalArgumentException("Listener is null");
+        listeners.remove(listener);
+    }
+
+    @Override
+    public XAResource getXAResource() throws ResourceException {
+        logger.trace("#LazyManagedConnection.getXAResource");
+        if (!xaTransaction || localTransaction) {
+            throw new NotSupportedException("GetXAResource not supported not supported");
+        } else {
+            return lazyXAResource;
+        }
+    }
+
+    @Override
+    public LocalTransaction getLocalTransaction() throws ResourceException {
+        logger.trace("#LazyManagedConnection.getLocalTransaction");
+        if (!localTransaction || xaTransaction) {
+            throw new NotSupportedException("LocalTransaction not supported");
+        } else {
+            return lazyLocalTransaction;
+        }
+    }
+
+    @Override
+    public ManagedConnectionMetaData getMetaData() throws ResourceException {
+        logger.trace("#LazyManagedConnection.getMetaData");
+        return new LazyManagedConnectionMetaData();
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter printWriter) throws ResourceException {
+        logger.trace("#LazyManagedConnection.setLogWriter");
+        logwriter = printWriter;
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws ResourceException {
+        logger.trace("#LazyManagedConnection.getLogWriter");
+        return logwriter;
+    }
+
+    boolean isEnlisted() {
+        logger.trace("#LazyManagedConnection.isEnlisted");
+        return enlisted;
+    }
+
+    void setEnlisted(boolean enlisted) {
+        logger.trace("#LazyManagedConnection.setEnlisted");
+        this.enlisted = enlisted;
+    }
+
+    boolean enlist() {
+        logger.trace("#LazyManagedConnection.enlist");
+        try {
+            if (cm instanceof LazyEnlistableConnectionManager) {
+                LazyEnlistableConnectionManager lecm = (LazyEnlistableConnectionManager) cm;
+                lecm.lazyEnlist(this);
+                return true;
+            }
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+        }
+        return false;
+    }
+
+    void closeHandle(LazyConnection handle) {
+        ConnectionEvent event = new ConnectionEvent(this, ConnectionEvent.CONNECTION_CLOSED);
+        event.setConnectionHandle(handle);
+        for (ConnectionEventListener cel : listeners) {
+            cel.connectionClosed(event);
+        }
+    }
+}
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/lazyconnectionmanager/rar/LazyResourceAdapter.java
@@ -0,0 +1,145 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.jca.lazyconnectionmanager.rar;
+
+import org.jboss.logging.Logger;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ActivationSpec;
+import javax.resource.spi.BootstrapContext;
+import javax.resource.spi.ResourceAdapter;
+import javax.resource.spi.ResourceAdapterInternalException;
+import javax.resource.spi.endpoint.MessageEndpointFactory;
+import javax.transaction.xa.XAResource;
+
+/**
+ * @author <a href="mailto:jesper.pedersen@ironjacamar.org">Jesper Pedersen</a>
+ * @author <a href="mailto:msimka@redhat.com">Martin Simka</a>
+ */
+public class LazyResourceAdapter implements ResourceAdapter {
+
+    private static Logger logger = Logger.getLogger(LazyResourceAdapter.class);
+
+    private Boolean enable;
+    private Boolean localTransaction;
+    private Boolean xaTransaction;
+
+    public LazyResourceAdapter() {
+        logger.trace("#LazyResourceAdapter");
+        enable = Boolean.TRUE;
+        localTransaction = Boolean.FALSE;
+        xaTransaction = Boolean.FALSE;
+    }
+
+    @Override
+    public void start(BootstrapContext ctx) throws ResourceAdapterInternalException {
+        logger.trace("#LazyResourceAdapter.start");
+    }
+
+    @Override
+    public void stop() {
+        logger.trace("#LazyResourceAdapter.stop");
+    }
+
+    @Override
+    public void endpointActivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) throws ResourceException {
+        logger.trace("#LazyResourceAdapter.endpointActivation");
+    }
+
+    @Override
+    public void endpointDeactivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) {
+        logger.trace("#LazyResourceAdapter.endpointDeactivation");
+    }
+
+    @Override
+    public XAResource[] getXAResources(ActivationSpec[] specs) throws ResourceException {
+        logger.trace("#LazyResourceAdapter.getXAResources");
+        return null;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    public Boolean getXATransaction() {
+        return xaTransaction;
+    }
+
+    public void setXATransaction(Boolean xaTransaction) {
+        this.xaTransaction = xaTransaction;
+    }
+
+    public Boolean getLocalTransaction() {
+        return localTransaction;
+    }
+
+    public void setLocalTransaction(Boolean localTransaction) {
+        this.localTransaction = localTransaction;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        if (enable != null)
+            result += 31 * result + 7 * enable.hashCode();
+        else
+            result += 31 * result + 7;
+        if (localTransaction != null)
+            result += 31 * result + 7 * localTransaction.hashCode();
+        else
+            result += 31 * result + 7;
+        if (xaTransaction != null)
+            result += 31 * result + 7 * xaTransaction.hashCode();
+        else
+            result += 31 * result + 7;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (!(other instanceof LazyResourceAdapter))
+            return false;
+        LazyResourceAdapter obj = (LazyResourceAdapter) other;
+        boolean result = true;
+        if (result) {
+            if (localTransaction == null)
+                result = obj.getLocalTransaction() == null;
+            else
+                result = localTransaction.equals(obj.getLocalTransaction());
+        }
+        if (result) {
+            if (xaTransaction == null)
+                result = obj.getXATransaction() == null;
+            else
+                result = xaTransaction.equals(obj.getXATransaction());
+        }
+        return result;
+    }
+}
