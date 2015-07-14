ca9537b6160a295d5e8a1aa2639d563aaa96a5a7
Josh Kinlaw <jkinlaw@redhat.com>
2015-07-14 14:32:09 -0400

    removes println statements

--- redhat-support-lib/src/main/java/com/redhat/gss/redhat_support_lib/web/ConnectionManager.java
+++ redhat-support-lib/src/main/java/com/redhat/gss/redhat_support_lib/web/ConnectionManager.java
@@ -78,18 +78,13 @@ public class ConnectionManager {
 
     public FTPClient getFTP() throws IOException, FTPException {
         FTPClient ftp = null;
-        System.out.println("ftp username, ftp password: "
-                + config.getFtpUsername() + ", " + config.getFtpPassword());
         if (config.getProxyUrl() == null) {
             ftp = new FTPClient();
-            System.out.println("created new ftp: " + ftp);
         } else {
             ftp = new FTPHTTPClient(config.getProxyUrl().getHost(),
                     config.getProxyPort(), config.getProxyUser(),
                     config.getProxyPassword());
         }
-        System.out.println("ftpHost, ftpPort: " + config.getFtpHost() + ", "
-                + config.getFtpPort());
         ftp.connect(config.getFtpHost(), config.getFtpPort());
         if (!ftp.login(config.getFtpUsername(), config.getFtpPassword())) {
             throw new FTPException("Error during FTP login");
