6c368337708b3014f4610cfca32db68ee39cd86f
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-08-18 10:28:42 +1000

    CMC timer interceptor fix

--- ejb3/src/main/java/org/jboss/as/ejb3/component/singleton/SingletonComponentDescription.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/singleton/SingletonComponentDescription.java
@@ -93,7 +93,8 @@ public class SingletonComponentDescription extends SessionBeanComponentDescripti
             @Override
             public void configure(final DeploymentPhaseContext context, final ComponentDescription description, final ComponentConfiguration configuration) throws DeploymentUnitProcessingException {
                 configuration.addTimeoutViewInterceptor(SingletonComponentInstanceAssociationInterceptor.FACTORY, InterceptorOrder.View.ASSOCIATING_INTERCEPTOR);
-                if (getConcurrencyManagementType() == ConcurrencyManagementType.CONTAINER) {
+                ConcurrencyManagementType concurrencyManagementType = getConcurrencyManagementType();
+                if (concurrencyManagementType == null || concurrencyManagementType == ConcurrencyManagementType.CONTAINER) {
                     configuration.addTimeoutViewInterceptor(new ContainerManagedConcurrencyInterceptorFactory(Collections.emptyMap()), InterceptorOrder.View.SINGLETON_CONTAINER_MANAGED_CONCURRENCY_INTERCEPTOR);
                 }
 
