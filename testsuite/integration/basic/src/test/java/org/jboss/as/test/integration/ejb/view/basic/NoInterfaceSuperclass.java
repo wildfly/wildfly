0d55295446b3041730cf90d02b9f1829b716a6fd
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-08-26 17:02:21 +1000

    WFLY-5197 Fix issue with EJB's when a non public method was overriden by a public one

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/view/basic/NoInterfaceSuperclass.java
@@ -0,0 +1,16 @@
+package org.jboss.as.test.integration.ejb.view.basic;
+
+/**
+ * @author Stuart Douglas
+ */
+public class NoInterfaceSuperclass {
+
+    protected String sayHello() {
+        return "Hello";
+    }
+
+    String sayGoodbye() {
+        return "Goodbye";
+    }
+
+}
