e44640d96c87b9d633b1a7558ea874edfdf12165
Jeff Mesnil <jmesnil@gmail.com>
2015-09-22 13:30:17 +0200

    [WFLY-5380]  Do not create legacy connection factory using in-vm connector
    Artemis in-vm connectors are not compatible with HornetQ ones.
    Instead of creating legacy-connection-factory that would generate errors
    during reload, discard it and add a warning.
    
    JIRA: https://issues.jboss.org/browse/WFLY-5380

--- legacy/messaging/src/test/java/org/jboss/as/messaging/test/MigrateTestCase.java
+++ legacy/messaging/src/test/java/org/jboss/as/messaging/test/MigrateTestCase.java
@@ -159,7 +159,12 @@ public class MigrateTestCase extends AbstractSubsystemTest {
         // 5 warnings about discovery-group attributes that can not be migrated.
         // 2 warnings about interceptors that can not be migrated.
         // 1 warning about HA migration (attributes have expressions)
-        assertEquals(warnings.toString(), 6 + 5 + 2 + 1, warnings.asList().size());
+        int expectedNumberOfWarnings = 6 + 5 + 2 + 1;
+        // 1 warning if add-legacy-entries is true because an in-vm connector can not be used in a legacy-connection-factory
+        if (addLegacyEntries) {
+            expectedNumberOfWarnings += 1;
+        }
+        assertEquals(warnings.toString(), expectedNumberOfWarnings, warnings.asList().size());
 
         model = services.readWholeModel();
 
