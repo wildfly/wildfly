5c1bc04e0479f2df407e40d952a2fb8f82f48ac7
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-09-22 13:10:47 +1000

    WFLY-5236 migrate true and false

--- legacy/web/src/main/java/org/jboss/as/web/WebMigrateOperation.java
+++ legacy/web/src/main/java/org/jboss/as/web/WebMigrateOperation.java
@@ -761,6 +761,12 @@ public class WebMigrateOperation implements OperationStepHandler {
             case "none": {
                 return "NOT_REQUESTED";
             }
+            case "true": {
+                return "REQUIRED";
+            }
+            case "false": {
+                return "NOT_REQUESTED";
+            }
             default: {
                 warnings.add(WebLogger.ROOT_LOGGER.couldNotTranslateVerifyClient(s));
                 return null;
