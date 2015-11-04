112dd724707cf018474df7c26a9d2442007946ea
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-11-04 22:41:18 +1100

    WFLY-5624 Request limit handler is called connection-limit-handler in the model

--- undertow/src/main/java/org/wildfly/extension/undertow/UndertowSubsystemParser_1_2.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/UndertowSubsystemParser_1_2.java
@@ -35,7 +35,7 @@ import org.jboss.dmr.ModelNode;
 import org.wildfly.extension.undertow.filters.CustomFilterDefinition;
 import org.wildfly.extension.undertow.filters.ErrorPageDefinition;
 import org.wildfly.extension.undertow.filters.BasicAuthHandler;
-import org.wildfly.extension.undertow.filters.ConnectionLimitHandler;
+import org.wildfly.extension.undertow.filters.RequestLimitHandler;
 import org.wildfly.extension.undertow.filters.FilterDefinitions;
 import org.wildfly.extension.undertow.filters.FilterRefDefinition;
 import org.wildfly.extension.undertow.filters.GzipFilter;
@@ -211,8 +211,9 @@ public class UndertowSubsystemParser_1_2 extends PersistentResourceXMLParser {
                                                 .addAttributes(BasicAuthHandler.SECURITY_DOMAIN)
                                 )
                                 .addChild(
-                                        builder(ConnectionLimitHandler.INSTANCE)
-                                                .addAttributes(ConnectionLimitHandler.MAX_CONCURRENT_REQUESTS, ConnectionLimitHandler.QUEUE_SIZE)
+                                        builder(RequestLimitHandler.INSTANCE)
+                                                .setXmlElementName("connection-limit")
+                                                .addAttributes(RequestLimitHandler.MAX_CONCURRENT_REQUESTS, RequestLimitHandler.QUEUE_SIZE)
                                 ).addChild(
                                 builder(ResponseHeaderFilter.INSTANCE)
                                         .addAttributes(ResponseHeaderFilter.NAME, ResponseHeaderFilter.VALUE)
