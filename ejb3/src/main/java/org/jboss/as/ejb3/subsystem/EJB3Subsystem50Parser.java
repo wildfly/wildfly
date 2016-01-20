fjuma/jboss-as

[WFLY-5689] Refactor the EJB subsystem to use individual resources for the security domain mappings

ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3Subsystem50Parser.java
@@ -22,24 +22,25 @@
 
 package org.jboss.as.ejb3.subsystem;
 
-import static org.jboss.as.controller.parsing.ParseUtils.duplicateNamedElement;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 import static org.jboss.as.controller.parsing.ParseUtils.missingRequired;
 import static org.jboss.as.controller.parsing.ParseUtils.missingRequiredElement;
+import static org.jboss.as.controller.parsing.ParseUtils.requireNoAttributes;
 import static org.jboss.as.controller.parsing.ParseUtils.requireNoContent;
 import static org.jboss.as.controller.parsing.ParseUtils.requireNoNamespaceAttribute;
 import static org.jboss.as.controller.parsing.ParseUtils.unexpectedAttribute;
 import static org.jboss.as.controller.parsing.ParseUtils.unexpectedElement;
-import static org.jboss.as.ejb3.subsystem.EJB3SubsystemModel.DEFAULT_SECURITY_DOMAIN;
-import static org.jboss.as.ejb3.subsystem.EJB3SubsystemModel.SECURITY_DOMAINS;
+import static org.jboss.as.ejb3.subsystem.EJB3SubsystemModel.APPLICATION_SECURITY_DOMAIN;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.operations.common.Util;
 import org.jboss.dmr.ModelNode;
 import org.jboss.staxmapper.XMLExtendedStreamReader;
 
@@ -63,16 +64,8 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
     @Override
     protected void readElement(final XMLExtendedStreamReader reader, final EJB3SubsystemXMLElement element, final List<ModelNode> operations, final ModelNode ejb3SubsystemAddOperation) throws XMLStreamException {
         switch (element) {
-            case SECURITY_DOMAINS: {
-                parseSecurityDomains(reader, ejb3SubsystemAddOperation);
-                break;
-            }
-            case DEFAULT_SECURITY_DOMAIN: {
-                if ((ejb3SubsystemAddOperation.hasDefined(SECURITY_DOMAINS)) && (ejb3SubsystemAddOperation.hasDefined(DEFAULT_SECURITY_DOMAIN))) {
-                    // Already defined using the security-domains default attribute
-                    throw unexpectedElement(reader);
-                }
-                parseDefaultSecurityDomain(reader, ejb3SubsystemAddOperation);
+            case APPLICATION_SECURITY_DOMAINS: {
+                parseApplicationSecurityDomains(reader, operations);
                 break;
             }
             default: {
@@ -81,31 +74,14 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
         }
     }
 
-    private void parseSecurityDomains(final XMLExtendedStreamReader reader, final ModelNode ejb3SubsystemAddOperation) throws XMLStreamException {
-        final int count = reader.getAttributeCount();
-        for (int i = 0; i < count; i++) {
-            requireNoNamespaceAttribute(reader, i);
-            final String attributeValue = reader.getAttributeValue(i);
-            final EJB3SubsystemXMLAttribute attribute = EJB3SubsystemXMLAttribute.forName(reader.getAttributeLocalName(i));
-            switch (attribute) {
-                case DEFAULT: {
-                    if (ejb3SubsystemAddOperation.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
-                        // Already defined using the default-security-domain element
-                        throw unexpectedAttribute(reader, i);
-                    }
-                    EJB3SubsystemRootResourceDefinition.DEFAULT_SECURITY_DOMAIN.parseAndSetParameter(attributeValue, ejb3SubsystemAddOperation, reader);
-                    break;
-                }
-                default: {
-                    throw unexpectedAttribute(reader, i);
-                }
-            }
-        }
-        final Set<String> securityDomainNames = new HashSet<String>();
+    private void parseApplicationSecurityDomains(final XMLExtendedStreamReader reader, final List<ModelNode> operations) throws XMLStreamException {
+        requireNoAttributes(reader);
+        boolean applicationSecurityDomainFound = false;
         while (reader.hasNext() && reader.nextTag() != XMLStreamConstants.END_ELEMENT) {
             switch (EJB3SubsystemXMLElement.forName(reader.getLocalName())) {
-                case SECURITY_DOMAIN: {
-                    parseSecurityDomain(reader, ejb3SubsystemAddOperation, securityDomainNames);
+                case APPLICATION_SECURITY_DOMAIN: {
+                    parseApplicationSecurityDomain(reader, operations);
+                    applicationSecurityDomainFound = true;
                     break;
                 }
                 default: {
@@ -113,39 +89,36 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
                 }
             }
         }
-        if (securityDomainNames.isEmpty()) {
-            throw missingRequiredElement(reader, Collections.singleton(EJB3SubsystemXMLElement.SECURITY_DOMAIN.getLocalName()));
+        if (! applicationSecurityDomainFound) {
+            throw missingRequiredElement(reader, Collections.singleton(EJB3SubsystemXMLElement.APPLICATION_SECURITY_DOMAIN.getLocalName()));
         }
     }
 
-    private void parseSecurityDomain(final XMLExtendedStreamReader reader, final ModelNode ejb3SubsystemAddOperation,
-                                     final Set<String> securityDomainNames) throws XMLStreamException {
-        String securityDomainName = null;
-        ModelNode securityDomain = new ModelNode();
+    private void parseApplicationSecurityDomain(final XMLExtendedStreamReader reader, final List<ModelNode> operations) throws XMLStreamException {
+        String applicationSecurityDomain = null;
+        ModelNode operation = Util.createAddOperation();
         final int count = reader.getAttributeCount();
         for (int i = 0; i < count; i++) {
             requireNoNamespaceAttribute(reader, i);
             final String attributeValue = reader.getAttributeValue(i);
             final EJB3SubsystemXMLAttribute attribute = EJB3SubsystemXMLAttribute.forName(reader.getAttributeLocalName(i));
             switch (attribute) {
-                case NAME:
-                    securityDomainName = attributeValue;
-                    if (! securityDomainNames.add(securityDomainName)) {
-                        throw duplicateNamedElement(reader, securityDomainName);
-                    }
-                    EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_NAME.parseAndSetParameter(securityDomainName, securityDomain, reader);
+                case SECURITY_DOMAIN:
+                    applicationSecurityDomain = attributeValue;
                     break;
-                case ALIAS:
-                    EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_ALIAS.parseAndSetParameter(attributeValue, securityDomain, reader);
+                case SECURITY_DOMAIN_REF:
+                    ApplicationSecurityDomainDefinition.SECURITY_DOMAIN_REF.parseAndSetParameter(attributeValue, operation, reader);
                     break;
                 default:
                     throw unexpectedAttribute(reader, i);
             }
         }
-        if (securityDomainName == null) {
-            throw missingRequired(reader, Collections.singleton(EJB3SubsystemXMLAttribute.NAME.getLocalName()));
+        if (applicationSecurityDomain == null) {
+            throw missingRequired(reader, Collections.singleton(EJB3SubsystemXMLAttribute.SECURITY_DOMAIN.getLocalName()));
         }
         requireNoContent(reader);
-        ejb3SubsystemAddOperation.get(SECURITY_DOMAINS).add(securityDomain);
+        final PathAddress address = this.getEJB3SubsystemAddress().append(PathElement.pathElement(APPLICATION_SECURITY_DOMAIN, applicationSecurityDomain));
+        operation.get(OP_ADDR).set(address.toModelNode());
+        operations.add(operation);
     }
 }

ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3SubsystemXMLPersister.java
@@ -225,18 +225,17 @@ public class EJB3SubsystemXMLPersister implements XMLElementWriter<SubsystemMars
             writer.writeEndElement();
         }
 
-        if (model.hasDefined(SECURITY_DOMAINS)) {
-            // security-domains element
-            writer.writeStartElement(EJB3SubsystemXMLElement.SECURITY_DOMAINS.getLocalName());
-            if (model.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
-                writer.writeAttribute(EJB3SubsystemXMLAttribute.DEFAULT.getLocalName(), model.get(DEFAULT_SECURITY_DOMAIN).asString());
-            }
-            writeSecurityDomains(writer, model);
-            writer.writeEndElement();
-        } else if (model.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
+        if (model.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
             // default-security-domain element
             writer.writeStartElement(EJB3SubsystemXMLElement.DEFAULT_SECURITY_DOMAIN.getLocalName());
             writer.writeAttribute(EJB3SubsystemXMLAttribute.VALUE.getLocalName(), model.get(DEFAULT_SECURITY_DOMAIN).asString());
+            writer.writeEndElement();
+        }
+
+        // application-security-domains element
+        if (model.hasDefined(APPLICATION_SECURITY_DOMAIN)) {
+            writer.writeStartElement(EJB3SubsystemXMLElement.APPLICATION_SECURITY_DOMAINS.getLocalName());
+            writeApplicationSecurityDomains(writer, model);
             writer.writeEndElement();
         }
 
@@ -596,17 +595,17 @@ public class EJB3SubsystemXMLPersister implements XMLElementWriter<SubsystemMars
         }
     }
 
-    private void writeSecurityDomains(final XMLExtendedStreamWriter writer, final ModelNode model) throws XMLStreamException {
-        List<ModelNode> securityDomains = model.get(SECURITY_DOMAINS).asList();
-        for (ModelNode current : securityDomains) {
-            writeSecurityDomain(writer, current);
+    private void writeApplicationSecurityDomains(final XMLExtendedStreamWriter writer, final ModelNode model) throws XMLStreamException {
+        List<Property> applicationSecurityDomains = model.get(APPLICATION_SECURITY_DOMAIN).asPropertyList();
+        for (Property property : applicationSecurityDomains) {
+            writeApplicationSecurityDomain(writer, property);
         }
     }
 
-    private void writeSecurityDomain(final XMLExtendedStreamWriter writer, final ModelNode securityDomain) throws XMLStreamException {
-        writer.writeStartElement(SECURITY_DOMAIN);
-        EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_NAME.marshallAsAttribute(securityDomain, writer);
-        EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_ALIAS.marshallAsAttribute(securityDomain, writer);
+    private void writeApplicationSecurityDomain(final XMLExtendedStreamWriter writer, final Property property) throws XMLStreamException {
+        writer.writeStartElement(APPLICATION_SECURITY_DOMAIN);
+        writer.writeAttribute(EJB3SubsystemXMLAttribute.SECURITY_DOMAIN.getLocalName(), property.getName());
+        ApplicationSecurityDomainDefinition.SECURITY_DOMAIN_REF.marshallAsAttribute(property.getValue(), writer);
         writer.writeEndElement();
     }
 

wildfly/wildfly

ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3Subsystem50Parser.java
@@ -22,24 +22,25 @@
 
 package org.jboss.as.ejb3.subsystem;
 
-import static org.jboss.as.controller.parsing.ParseUtils.duplicateNamedElement;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
 import static org.jboss.as.controller.parsing.ParseUtils.missingRequired;
 import static org.jboss.as.controller.parsing.ParseUtils.missingRequiredElement;
+import static org.jboss.as.controller.parsing.ParseUtils.requireNoAttributes;
 import static org.jboss.as.controller.parsing.ParseUtils.requireNoContent;
 import static org.jboss.as.controller.parsing.ParseUtils.requireNoNamespaceAttribute;
 import static org.jboss.as.controller.parsing.ParseUtils.unexpectedAttribute;
 import static org.jboss.as.controller.parsing.ParseUtils.unexpectedElement;
-import static org.jboss.as.ejb3.subsystem.EJB3SubsystemModel.DEFAULT_SECURITY_DOMAIN;
-import static org.jboss.as.ejb3.subsystem.EJB3SubsystemModel.SECURITY_DOMAINS;
+import static org.jboss.as.ejb3.subsystem.EJB3SubsystemModel.APPLICATION_SECURITY_DOMAIN;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.operations.common.Util;
 import org.jboss.dmr.ModelNode;
 import org.jboss.staxmapper.XMLExtendedStreamReader;
 
@@ -63,16 +64,8 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
     @Override
     protected void readElement(final XMLExtendedStreamReader reader, final EJB3SubsystemXMLElement element, final List<ModelNode> operations, final ModelNode ejb3SubsystemAddOperation) throws XMLStreamException {
         switch (element) {
-            case SECURITY_DOMAINS: {
-                parseSecurityDomains(reader, ejb3SubsystemAddOperation);
-                break;
-            }
-            case DEFAULT_SECURITY_DOMAIN: {
-                if ((ejb3SubsystemAddOperation.hasDefined(SECURITY_DOMAINS)) && (ejb3SubsystemAddOperation.hasDefined(DEFAULT_SECURITY_DOMAIN))) {
-                    // Already defined using the security-domains default attribute
-                    throw unexpectedElement(reader);
-                }
-                parseDefaultSecurityDomain(reader, ejb3SubsystemAddOperation);
+            case APPLICATION_SECURITY_DOMAINS: {
+                parseApplicationSecurityDomains(reader, operations);
                 break;
             }
             default: {
@@ -81,31 +74,14 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
         }
     }
 
-    private void parseSecurityDomains(final XMLExtendedStreamReader reader, final ModelNode ejb3SubsystemAddOperation) throws XMLStreamException {
-        final int count = reader.getAttributeCount();
-        for (int i = 0; i < count; i++) {
-            requireNoNamespaceAttribute(reader, i);
-            final String attributeValue = reader.getAttributeValue(i);
-            final EJB3SubsystemXMLAttribute attribute = EJB3SubsystemXMLAttribute.forName(reader.getAttributeLocalName(i));
-            switch (attribute) {
-                case DEFAULT: {
-                    if (ejb3SubsystemAddOperation.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
-                        // Already defined using the default-security-domain element
-                        throw unexpectedAttribute(reader, i);
-                    }
-                    EJB3SubsystemRootResourceDefinition.DEFAULT_SECURITY_DOMAIN.parseAndSetParameter(attributeValue, ejb3SubsystemAddOperation, reader);
-                    break;
-                }
-                default: {
-                    throw unexpectedAttribute(reader, i);
-                }
-            }
-        }
-        final Set<String> securityDomainNames = new HashSet<String>();
+    private void parseApplicationSecurityDomains(final XMLExtendedStreamReader reader, final List<ModelNode> operations) throws XMLStreamException {
+        requireNoAttributes(reader);
+        boolean applicationSecurityDomainFound = false;
         while (reader.hasNext() && reader.nextTag() != XMLStreamConstants.END_ELEMENT) {
             switch (EJB3SubsystemXMLElement.forName(reader.getLocalName())) {
-                case SECURITY_DOMAIN: {
-                    parseSecurityDomain(reader, ejb3SubsystemAddOperation, securityDomainNames);
+                case APPLICATION_SECURITY_DOMAIN: {
+                    parseApplicationSecurityDomain(reader, operations);
+                    applicationSecurityDomainFound = true;
                     break;
                 }
                 default: {
@@ -113,15 +89,14 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
                 }
             }
         }
-        if (securityDomainNames.isEmpty()) {
-            throw missingRequiredElement(reader, Collections.singleton(EJB3SubsystemXMLElement.SECURITY_DOMAIN.getLocalName()));
+        if (! applicationSecurityDomainFound) {
+            throw missingRequiredElement(reader, Collections.singleton(EJB3SubsystemXMLElement.APPLICATION_SECURITY_DOMAIN.getLocalName()));
         }
     }
 
-    private void parseSecurityDomain(final XMLExtendedStreamReader reader, final ModelNode ejb3SubsystemAddOperation,
-                                     final Set<String> securityDomainNames) throws XMLStreamException {
-        String securityDomainName = null;
-        ModelNode securityDomain = new ModelNode();
+    private void parseApplicationSecurityDomain(final XMLExtendedStreamReader reader, final List<ModelNode> operations) throws XMLStreamException {
+        String applicationSecurityDomain = null;
+        ModelNode operation = Util.createAddOperation();
         final int count = reader.getAttributeCount();
         for (int i = 0; i < count; i++) {
             requireNoNamespaceAttribute(reader, i);
@@ -129,23 +104,21 @@ public class EJB3Subsystem50Parser extends EJB3Subsystem40Parser {
             final EJB3SubsystemXMLAttribute attribute = EJB3SubsystemXMLAttribute.forName(reader.getAttributeLocalName(i));
             switch (attribute) {
                 case NAME:
-                    securityDomainName = attributeValue;
-                    if (! securityDomainNames.add(securityDomainName)) {
-                        throw duplicateNamedElement(reader, securityDomainName);
-                    }
-                    EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_NAME.parseAndSetParameter(securityDomainName, securityDomain, reader);
+                    applicationSecurityDomain = attributeValue;
                     break;
-                case ALIAS:
-                    EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_ALIAS.parseAndSetParameter(attributeValue, securityDomain, reader);
+                case SECURITY_DOMAIN:
+                    ApplicationSecurityDomainDefinition.SECURITY_DOMAIN.parseAndSetParameter(attributeValue, operation, reader);
                     break;
                 default:
                     throw unexpectedAttribute(reader, i);
             }
         }
-        if (securityDomainName == null) {
+        if (applicationSecurityDomain == null) {
             throw missingRequired(reader, Collections.singleton(EJB3SubsystemXMLAttribute.NAME.getLocalName()));
         }
         requireNoContent(reader);
-        ejb3SubsystemAddOperation.get(SECURITY_DOMAINS).add(securityDomain);
+        final PathAddress address = this.getEJB3SubsystemAddress().append(PathElement.pathElement(APPLICATION_SECURITY_DOMAIN, applicationSecurityDomain));
+        operation.get(OP_ADDR).set(address.toModelNode());
+        operations.add(operation);
     }
 }

ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3SubsystemXMLPersister.java
@@ -225,18 +225,17 @@ public class EJB3SubsystemXMLPersister implements XMLElementWriter<SubsystemMars
             writer.writeEndElement();
         }
 
-        if (model.hasDefined(SECURITY_DOMAINS)) {
-            // security-domains element
-            writer.writeStartElement(EJB3SubsystemXMLElement.SECURITY_DOMAINS.getLocalName());
-            if (model.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
-                writer.writeAttribute(EJB3SubsystemXMLAttribute.DEFAULT.getLocalName(), model.get(DEFAULT_SECURITY_DOMAIN).asString());
-            }
-            writeSecurityDomains(writer, model);
-            writer.writeEndElement();
-        } else if (model.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
+        if (model.hasDefined(DEFAULT_SECURITY_DOMAIN)) {
             // default-security-domain element
             writer.writeStartElement(EJB3SubsystemXMLElement.DEFAULT_SECURITY_DOMAIN.getLocalName());
             writer.writeAttribute(EJB3SubsystemXMLAttribute.VALUE.getLocalName(), model.get(DEFAULT_SECURITY_DOMAIN).asString());
+            writer.writeEndElement();
+        }
+
+        // application-security-domains element
+        if (model.hasDefined(APPLICATION_SECURITY_DOMAIN)) {
+            writer.writeStartElement(EJB3SubsystemXMLElement.APPLICATION_SECURITY_DOMAINS.getLocalName());
+            writeApplicationSecurityDomains(writer, model);
             writer.writeEndElement();
         }
 
@@ -597,17 +596,17 @@ public class EJB3SubsystemXMLPersister implements XMLElementWriter<SubsystemMars
         }
     }
 
-    private void writeSecurityDomains(final XMLExtendedStreamWriter writer, final ModelNode model) throws XMLStreamException {
-        List<ModelNode> securityDomains = model.get(SECURITY_DOMAINS).asList();
-        for (ModelNode current : securityDomains) {
-            writeSecurityDomain(writer, current);
+    private void writeApplicationSecurityDomains(final XMLExtendedStreamWriter writer, final ModelNode model) throws XMLStreamException {
+        List<Property> applicationSecurityDomains = model.get(APPLICATION_SECURITY_DOMAIN).asPropertyList();
+        for (Property property : applicationSecurityDomains) {
+            writeApplicationSecurityDomain(writer, property);
         }
     }
 
-    private void writeSecurityDomain(final XMLExtendedStreamWriter writer, final ModelNode securityDomain) throws XMLStreamException {
-        writer.writeStartElement(SECURITY_DOMAIN);
-        EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_NAME.marshallAsAttribute(securityDomain, writer);
-        EJB3SubsystemRootResourceDefinition.SECURITY_DOMAIN_ALIAS.marshallAsAttribute(securityDomain, writer);
+    private void writeApplicationSecurityDomain(final XMLExtendedStreamWriter writer, final Property property) throws XMLStreamException {
+        writer.writeStartElement(APPLICATION_SECURITY_DOMAIN);
+        writer.writeAttribute(EJB3SubsystemXMLAttribute.NAME.getLocalName(), property.getName());
+        ApplicationSecurityDomainDefinition.SECURITY_DOMAIN.marshallAsAttribute(property.getValue(), writer);
         writer.writeEndElement();
     }
 