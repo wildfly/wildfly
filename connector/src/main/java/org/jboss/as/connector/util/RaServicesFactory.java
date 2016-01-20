0c85daf87645bb565a50e9e4ba128d261321f011
Lin Gao <lgao@redhat.com>
2016-01-20 14:34:35 +0800

    [WFLY-6035] Race condition during server start-up - config property of ConnectionDefinition is not injected

--- connector/src/main/java/org/jboss/as/connector/util/RaServicesFactory.java
+++ connector/src/main/java/org/jboss/as/connector/util/RaServicesFactory.java
@@ -28,6 +28,7 @@ import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 
+import org.jboss.as.connector.metadata.deployment.ResourceAdapterDeployment;
 import org.jboss.as.connector.metadata.xmldescriptors.ConnectorXmlDescriptor;
 import org.jboss.as.connector.services.mdr.AS7MetadataRepository;
 import org.jboss.as.connector.services.resourceadapters.deployment.ResourceAdapterXmlDeploymentService;
@@ -49,6 +50,7 @@ import org.jboss.modules.Module;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceRegistry;
 import org.jboss.msc.service.ServiceTarget;
 import org.jboss.security.SubjectFactory;
 
@@ -56,7 +58,7 @@ public class RaServicesFactory {
 
     public static void createDeploymentService(final ManagementResourceRegistration registration, ConnectorXmlDescriptor connectorXmlDescriptor, Module module,
                                                ServiceTarget serviceTarget, final String deploymentUnitName, ServiceName deploymentUnitServiceName, String deployment,
-                                               Activation raxml, final Resource deploymentResource) {
+                                               Activation raxml, final Resource deploymentResource, final ServiceRegistry serviceRegistry) {
         // Create the service
 
         ServiceName serviceName = ConnectorServices.getDeploymentServiceName(deploymentUnitName,raxml);
@@ -66,7 +68,7 @@ public class RaServicesFactory {
         if (raxml.getBootstrapContext() != null && !raxml.getBootstrapContext().equals("undefined")) {
             bootStrapCtxName = raxml.getBootstrapContext();
         }
-        ServiceBuilder builder =
+        ServiceBuilder<ResourceAdapterDeployment> builder =
                 Services.addServerExecutorDependency(
                         serviceTarget.addService(serviceName, service),
                         service.getExecutorServiceInjector(), false)
@@ -89,6 +91,17 @@ public class RaServicesFactory {
                 .addDependency(NamingService.SERVICE_NAME)
                 .addDependency(ConnectorServices.BOOTSTRAP_CONTEXT_SERVICE.append(bootStrapCtxName))
                 .addDependency(ConnectorServices.RESOURCE_ADAPTER_DEPLOYER_SERVICE_PREFIX.append(connectorXmlDescriptor.getDeploymentName()));
+        String raName = deployment;
+        if (raxml.getId() != null) {
+            raName = raxml.getId();
+        }
+        ServiceName parentName = ServiceName.of(ConnectorServices.RA_SERVICE, raName);
+        for(ServiceName subServiceName: serviceRegistry.getServiceNames()) {
+            if (parentName.isParentOf(subServiceName)
+                    && !subServiceName.getSimpleName().equals(ConnectorServices.STATISTICS_SUFFIX)) {
+                builder.addDependency(subServiceName);
+            }
+        }
 
         if (registration != null && deploymentResource != null) {
             if (registration.isAllowsOverride() && registration.getOverrideModel(deploymentUnitName) == null) {
