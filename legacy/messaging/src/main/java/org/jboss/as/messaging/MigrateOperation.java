3139e1f8cde1c8b67c8e4c9e09a94286a4a6ed49
Jeff Mesnil <jmesnil@gmail.com>
2016-02-08 15:28:07 +0100

    [WFLY-6068] Migration warning for failback-delay
    Discard the failback-delay from the server's add operation during
    migration (and not from the ha-policy's add).
    
    JIRA: https://issues.jboss.org/browse/WFLY-6068

--- legacy/messaging/src/main/java/org/jboss/as/messaging/MigrateOperation.java
+++ legacy/messaging/src/main/java/org/jboss/as/messaging/MigrateOperation.java
@@ -657,11 +657,11 @@ public class MigrateOperation implements OperationStepHandler {
                 haPolicyAddress = serverAddress.append(HA_POLICY, "shared-store-slave");
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, ALLOW_FAILBACK, "allow-failback");
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, FAILOVER_ON_SHUTDOWN, "failover-on-server-shutdown");
-                discardFailbackDelay(haPolicyAddOperation, warnings);
+                discardFailbackDelay(serverAddOperation, warnings);
             } else {
                 haPolicyAddress = serverAddress.append(HA_POLICY, "shared-store-master");
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, FAILOVER_ON_SHUTDOWN, "failover-on-server-shutdown");
-                discardFailbackDelay(haPolicyAddOperation, warnings);
+                discardFailbackDelay(serverAddOperation, warnings);
             }
         } else {
             if (backup) {
@@ -669,7 +669,7 @@ public class MigrateOperation implements OperationStepHandler {
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, ALLOW_FAILBACK, "allow-failback");
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, MAX_SAVED_REPLICATED_JOURNAL_SIZE, "max-saved-replicated-journal-size");
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, BACKUP_GROUP_NAME, "group-name");
-                discardFailbackDelay(haPolicyAddOperation, warnings);
+                discardFailbackDelay(serverAddOperation, warnings);
             } else {
                 haPolicyAddress = serverAddress.append(HA_POLICY, "replication-master");
                 setAndDiscard(haPolicyAddOperation, serverAddOperation, CHECK_FOR_LIVE_SERVER, "check-for-live-server");
