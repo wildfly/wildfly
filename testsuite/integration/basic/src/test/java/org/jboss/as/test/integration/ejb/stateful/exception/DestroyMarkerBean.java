49fbd8ec554a7b4958a0f86b1aa02c1cc965d92b
ochaloup <ochaloup@redhat.com>
2016-02-17 21:50:05 +0100

    [WFLY-4672] Adding tests for exception being thrown from SFSB

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/exception/DestroyMarkerBean.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/exception/DestroyMarkerBean.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2011, Red Hat, Inc., and individual contributors
+ * Copyright 2016, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -22,52 +22,21 @@
 
 package org.jboss.as.test.integration.ejb.stateful.exception;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.ejb.EJB;
-import javax.ejb.EJBException;
 import javax.ejb.LocalBean;
 import javax.ejb.Remote;
-import javax.ejb.Remove;
-import javax.ejb.Stateful;
+import javax.ejb.Singleton;
 
-/**
- * stateful session bean
- */
-@Stateful
+@Singleton
 @Remote
 @LocalBean
-public class SFSB1 implements SFSB1Interface {
-    public final static String MESSAGE = "Expected Exception";
+public class DestroyMarkerBean implements DestroyMarkerBeanInterface {
+    private boolean preDestroy = false;
 
-    @EJB
-    private DestroyMarkerBean marker;
-
-
-    @PostConstruct
-    public void postConstruct() {
-        marker.set(false);
-    }
-    
-    @PreDestroy
-    public void preDestroy() {
-        marker.set(true);
+    public boolean is() {
+        return preDestroy;
     }
 
-    public void systemException() {
-        throw new RuntimeException(MESSAGE);
-    }
-
-    public void ejbException() {
-        throw new EJBException(MESSAGE);
-    }
-    
-    public void userException() throws TestException {
-        throw new TestException(MESSAGE);
-    }
-
-    @Remove
-    public void remove() {
-        // just removed
+    public void set(boolean preDestroy) {
+        this.preDestroy = preDestroy;
     }
 }
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/exception/ExceptionEjbClientTestCase.java
@@ -0,0 +1,60 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.stateful.exception;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunAsClient
+@RunWith(Arquillian.class)
+public class ExceptionEjbClientTestCase extends ExceptionTestCase {
+    private static Context context;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        final Properties props = new Properties();
+        props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        context = new InitialContext(props);
+    }
+
+    private <T> T lookup(Class<? extends T> beanType, Class<T> interfaceType,boolean isStateful) throws NamingException {
+        String ejbLookup = String.format("ejb:/%s/%s!%s%s", ARCHIVE_NAME, beanType.getSimpleName(), interfaceType.getName(),
+                (isStateful ? "?stateful" : ""));
+        return interfaceType.cast(context.lookup(ejbLookup));
+    }
+
+    protected SFSB1Interface getBean() throws NamingException {
+        return (SFSB1Interface) lookup(SFSB1.class, SFSB1Interface.class, true);
+    }
+
+    protected DestroyMarkerBeanInterface getMarker() throws NamingException {
+        return (DestroyMarkerBeanInterface) lookup(DestroyMarkerBean.class, DestroyMarkerBeanInterface.class, false);
+    }
+}
