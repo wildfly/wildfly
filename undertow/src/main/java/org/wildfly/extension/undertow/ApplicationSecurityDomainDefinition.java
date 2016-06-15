7f0866434aad634834f152a160dfc87461ef4a15
Farah Juma <fjuma@redhat.com>
2016-06-15 17:45:37 -0400

    [WFLY-6732] Fix IllegalStateException in ApplicationSecurityDomainService#initialSecurityHandler when loginConfig is null

--- undertow/src/main/java/org/wildfly/extension/undertow/ApplicationSecurityDomainDefinition.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/ApplicationSecurityDomainDefinition.java
@@ -336,11 +336,11 @@ public class ApplicationSecurityDomainDefinition extends PersistentResourceDefin
             final Map<String, String> baseConfiguration = Collections.unmodifiableMap(tempBaseConfiguration);
 
             final Map<String, Map<String, String>> selectedMechanisms = new LinkedHashMap<>();
-            if (overrideDeploymentConfig) {
+            if (overrideDeploymentConfig || (loginConfig == null)) {
                 final Map<String, String> mechanismConfiguration = baseConfiguration;
                 availableMechanisms.forEach(n -> selectedMechanisms.put(n, mechanismConfiguration));
             } else {
-                final List<AuthMethodConfig> authMethods = loginConfig == null ? Collections.<AuthMethodConfig> emptyList() : loginConfig.getAuthMethods();
+                final List<AuthMethodConfig> authMethods = loginConfig.getAuthMethods();
                 if (authMethods.isEmpty()) {
                     throw ROOT_LOGGER.noMechanismsSelected();
                 }
