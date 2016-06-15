76525341a08d46b2afd0cc666b2e415489970fd4
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2016-06-15 12:27:18 +0100

    WFLY-6725 Test case for JBERET-231

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/batch/transaction/TransactedReader.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.batch.transaction;
+
+import javax.batch.api.BatchProperty;
+import javax.batch.api.chunk.AbstractItemReader;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+public class TransactedReader extends AbstractItemReader {
+
+    @Inject
+    @BatchProperty(name="job.timeout")
+    private int timeout;
+
+    @Inject
+    private TransactedService transactedService;
+
+    @Override
+    public Object readItem() throws Exception {
+        transactedService.query(timeout);
+        return null;
+    }
+
+}
