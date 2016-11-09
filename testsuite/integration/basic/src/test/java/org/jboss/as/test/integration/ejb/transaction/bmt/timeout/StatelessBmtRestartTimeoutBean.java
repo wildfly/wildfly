zezulka/wildfly-1

[WFLY-7205] Adding tests for transaction timeout behavior
[WFLY-7205] Adding tests for transaction timeout behavior when BMT is used
[WFLY-7205] Adding tests for transaction timeout behavior for MDB

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/bmt/timeout/StatelessBmtRestartTimeoutBean.java
@@ -1,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.bmt.timeout;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.transaction.RollbackException;
+import javax.transaction.TransactionManager;
+import org.jboss.as.test.integration.ejb.transaction.utils.TxTestUtil;
+import org.jboss.logging.Logger;
+import org.junit.Assert;
+
+@Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
+public class StatelessBmtRestartTimeoutBean {
+    private static Logger log = Logger.getLogger(StatelessBmtRestartTimeoutBean.class);
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    public void test() throws Exception {
+        String txnAsString = null;
+
+        tm.setTransactionTimeout(1);
+
+        try {
+            tm.begin();
+            txnAsString = tm.getTransaction().toString();
+            TxTestUtil.waitForTimeout(tm);
+            tm.commit();
+        } catch (RollbackException e) {
+            try {
+                log.debugf("Rollbacking transaction '%s'", txnAsString);
+                tm.rollback();
+                Assert.fail("Rollback should throw IllegalStateException: BaseTransaction.rollback - ARJUNA016074: no transaction!"
+                    + " as the original transaction should be aborted by transaction manager");
+            } catch (Exception rollbacke) {
+                // expected transaction as there is no txn to rollback - it was aborted by TM
+                log.debugf(rollbacke, "Got expected transaction: %s'", rollbacke.getClass());
+            }
+        }
+
+        // reseting transaction timeout to default one
+        tm.setTransactionTimeout(0);
+        tm.begin();
+        txnAsString = tm.getTransaction().toString();
+        TxTestUtil.waitForTimeout(tm);
+        tm.commit();
+    }
+}

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/mdb/timeout/ActivationPropertyTimeoutMDB.java
@@ -1,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.mdb.timeout;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+import org.jboss.logging.Logger;
+import org.jboss.as.test.integration.transactions.TransactionCheckerSingleton;
+import org.jboss.as.test.integration.transactions.TxTestUtil;
+
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destination", propertyValue = TransactionTimeoutQueueSetupTask.PROPERTY_TIMEOUT_JNDI_NAME),
+    @ActivationConfigProperty(propertyName = "transactionTimeout", propertyValue = "1"),
+})
+public class ActivationPropertyTimeoutMDB implements MessageListener {
+    private static final Logger log = Logger.getLogger(ActivationPropertyTimeoutMDB.class);
+    public static final String REPLY_PREFIX = "replying ";
+
+    @Resource(lookup = "java:/JmsXA")
+    private ConnectionFactory factory;
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    @Inject
+    private SingletonChecker checker;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            log.infof("onMessage received message: %s '%s'", message, ((TextMessage) message).getText());
+
+            // this should mean that second attempt for calling onMessage comes to play
+            if(checker.getRolledback() > 0) {
+                log.infof("Discarding message '%s' as onMessage called for second time", message);
+                return;
+            }
+
+            final Destination replyTo = message.getJMSReplyTo();
+
+            if (replyTo == null) {
+                throw new RuntimeException("ReplyTo info in message was not specified"
+                    + " and bean does not know where to reply to");
+            }
+
+            TxTestUtil.enlistTestXAResource(tm.getTransaction());
+
+            try (
+                JMSContext context = factory.createContext()
+            ) {
+                context.createProducer()
+                    .setJMSCorrelationID(message.getJMSMessageID())
+                    .send(replyTo, REPLY_PREFIX + ((TextMessage) message).getText());
+            }
+
+            // would timeout txn when TransactionTimeout be cared
+            TxTestUtil.waitForTimeout(tm);
+        } catch (Exception e) {
+            throw new RuntimeException("onMessage method execution failed", e);
+        }
+
+    }
+}

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/mdb/timeout/AnnotationTimeoutMDB.java
@@ -1,0 +1,93 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.mdb.timeout;
+
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.TransactionAttribute;
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+import org.jboss.as.test.integration.ejb.transaction.utils.SingletonChecker;
+import org.jboss.as.test.integration.ejb.transaction.utils.TxTestUtil;
+import org.jboss.ejb3.annotation.TransactionTimeout;
+import org.jboss.logging.Logger;
+import org.jboss.as.test.integration.transactions.TxTestUtil;
+
+/**
+ * It's expected that {@link TransactionTimeout} annotation does not work for MDB
+ * with {@link TransactionAttribute} set as REQUIRED because transaction is already started
+ * at time of calling onMessage method and as such timeout can't be changed at that time
+ * (can't change timeout of already started txn).
+ */
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = TransactionTimeoutQueueSetupTask.ANNOTATION_TIMEOUT_JNDI_NAME)
+})
+@TransactionTimeout(value = 1, unit = TimeUnit.SECONDS)
+public class AnnotationTimeoutMDB implements MessageListener {
+    private static final Logger log = Logger.getLogger(AnnotationTimeoutMDB.class);
+    public static final String REPLY_PREFIX = "replying ";
+
+    @Resource(lookup = "java:/JmsXA")
+    private ConnectionFactory factory;
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            log.infof("onMessage received message: %s '%s'", message, ((TextMessage) message).getText());
+
+            final Destination replyTo = message.getJMSReplyTo();
+
+            if (replyTo == null) {
+                throw new RuntimeException("ReplyTo info in message was not specified"
+                    + " and bean does not know where to reply to");
+            }
+
+            TxTestUtil.enlistTestXAResource(tm.getTransaction());
+
+            try (
+                JMSContext context = factory.createContext()
+            ) {
+                context.createProducer()
+                    .setJMSCorrelationID(message.getJMSMessageID())
+                    .send(replyTo, REPLY_PREFIX + ((TextMessage) message).getText());
+            }
+
+            // would timeout txn when TransactionTimeout be cared
+            TxTestUtil.waitForTimeout(tm);
+        } catch (Exception e) {
+            throw new RuntimeException("onMessage method execution failed", e);
+        }
+
+    }
+}

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/mdb/timeout/NoTimeoutMDB.java
@@ -1,0 +1,81 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.mdb.timeout;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+import javax.transaction.TransactionSynchronizationRegistry;
+import org.jboss.as.test.integration.ejb.transaction.utils.TxTestUtil;
+import org.jboss.logging.Logger;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = TransactionTimeoutQueueSetupTask.NO_TIMEOUT_JNDI_NAME)
+})
+public class NoTimeoutMDB implements MessageListener {
+    private static final Logger log = Logger.getLogger(NoTimeoutMDB.class);
+    public static final String REPLY_PREFIX = "replying ";
+
+    @Resource(lookup = "java:/JmsXA")
+    private ConnectionFactory factory;
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    @Resource
+    private TransactionSynchronizationRegistry synchroRegistry;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            log.infof("onMessage received message: %s '%s'", message, ((TextMessage) message).getText());
+
+            final Destination replyTo = message.getJMSReplyTo();
+
+            if (replyTo == null) {
+                throw new RuntimeException("ReplyTo info in message was not specified"
+                    + " and bean does not know where to reply to");
+            }
+
+            TxTestUtil.enlistTestXAResource(tm.getTransaction());
+            TxTestUtil.addSynchronization(tm.getTransaction());
+
+            try (
+                JMSContext context = factory.createContext()
+            ) {
+                context.createProducer()
+                    .setJMSCorrelationID(message.getJMSMessageID())
+                    .send(replyTo, REPLY_PREFIX + ((TextMessage) message).getText());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("onMessage method execution failed", e);
+        }
+    }
+}

wildfly/wildfly

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/bmt/timeout/StatelessBmtRestartTimeoutBean.java
@@ -1,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.bmt.timeout;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.transaction.RollbackException;
+import javax.transaction.TransactionManager;
+import org.jboss.as.test.integration.ejb.transaction.utils.TxTestUtil;
+import org.jboss.logging.Logger;
+import org.junit.Assert;
+
+@Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
+public class StatelessBmtRestartTimeoutBean {
+    private static Logger log = Logger.getLogger(StatelessBmtRestartTimeoutBean.class);
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    public void test() throws Exception {
+        String txnAsString = null;
+
+        tm.setTransactionTimeout(1);
+
+        try {
+            tm.begin();
+            txnAsString = tm.getTransaction().toString();
+            TxTestUtil.waitForTimeout(tm);
+            tm.commit();
+        } catch (RollbackException e) {
+            try {
+                log.tracef("Rollbacking transaction '%s'", txnAsString);
+                tm.rollback();
+                Assert.fail("Rollback should throw IllegalStateException: BaseTransaction.rollback - ARJUNA016074: no transaction!"
+                    + " as the original transaction should be aborted by transaction manager");
+            } catch (Exception rollbacke) {
+                // expected transaction as there is no txn to rollback - it was aborted by TM
+                log.debugf(rollbacke, "Got expected transaction: %s'", rollbacke.getClass());
+            }
+        } finally {
+            // reseting transaction timeout to default one
+            tm.setTransactionTimeout(0);
+        }
+
+        // after reset additional check if default value is used
+        // value depends on settings under txn subsystem but default is 5 minutes
+        tm.begin();
+        txnAsString = tm.getTransaction().toString();
+        TxTestUtil.waitForTimeout(tm);
+        tm.commit();
+    }
+}

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/mdb/timeout/ActivationPropertyTimeoutMDB.java
@@ -1,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.mdb.timeout;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+import org.jboss.logging.Logger;
+import org.jboss.as.test.integration.transactions.TransactionCheckerSingleton;
+import org.jboss.as.test.integration.transactions.TxTestUtil;
+
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destination", propertyValue = TransactionTimeoutQueueSetupTask.PROPERTY_TIMEOUT_JNDI_NAME),
+    @ActivationConfigProperty(propertyName = "transactionTimeout", propertyValue = "1"),
+})
+public class ActivationPropertyTimeoutMDB implements MessageListener {
+    private static final Logger log = Logger.getLogger(ActivationPropertyTimeoutMDB.class);
+    public static final String REPLY_PREFIX = "replying ";
+
+    @Resource(lookup = "java:/JmsXA")
+    private ConnectionFactory factory;
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    @Inject
+    private SingletonChecker checker;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            log.tracef("onMessage received message: %s '%s'", message, ((TextMessage) message).getText());
+
+            // this should mean that second attempt for calling onMessage comes to play
+            if(checker.getRolledback() > 0) {
+                log.tracef("Discarding message '%s' as onMessage called for second time", message);
+                return;
+            }
+
+            final Destination replyTo = message.getJMSReplyTo();
+
+            if (replyTo == null) {
+                throw new RuntimeException("ReplyTo info in message was not specified"
+                    + " and bean does not know where to reply to");
+            }
+
+            TxTestUtil.enlistTestXAResource(tm.getTransaction());
+
+            try (
+                JMSContext context = factory.createContext()
+            ) {
+                context.createProducer()
+                    .setJMSCorrelationID(message.getJMSMessageID())
+                    .send(replyTo, REPLY_PREFIX + ((TextMessage) message).getText());
+            }
+
+            // would timeout txn when TransactionTimeout be cared
+            TxTestUtil.waitForTimeout(tm);
+        } catch (Exception e) {
+            throw new RuntimeException("onMessage method execution failed", e);
+        }
+
+    }
+}

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/mdb/timeout/AnnotationTimeoutMDB.java
@@ -1,0 +1,93 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.mdb.timeout;
+
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.TransactionAttribute;
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+import org.jboss.as.test.integration.ejb.transaction.utils.SingletonChecker;
+import org.jboss.as.test.integration.ejb.transaction.utils.TxTestUtil;
+import org.jboss.ejb3.annotation.TransactionTimeout;
+import org.jboss.logging.Logger;
+import org.jboss.as.test.integration.transactions.TxTestUtil;
+
+/**
+ * It's expected that {@link TransactionTimeout} annotation does not work for MDB
+ * with {@link TransactionAttribute} set as REQUIRED because transaction is already started
+ * at time of calling onMessage method and as such timeout can't be changed at that time
+ * (can't change timeout of already started txn).
+ */
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = TransactionTimeoutQueueSetupTask.ANNOTATION_TIMEOUT_JNDI_NAME)
+})
+@TransactionTimeout(value = 1, unit = TimeUnit.SECONDS)
+public class AnnotationTimeoutMDB implements MessageListener {
+    private static final Logger log = Logger.getLogger(AnnotationTimeoutMDB.class);
+    public static final String REPLY_PREFIX = "replying ";
+
+    @Resource(lookup = "java:/JmsXA")
+    private ConnectionFactory factory;
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            log.tracef("onMessage received message: %s '%s'", message, ((TextMessage) message).getText());
+
+            final Destination replyTo = message.getJMSReplyTo();
+
+            if (replyTo == null) {
+                throw new RuntimeException("ReplyTo info in message was not specified"
+                    + " and bean does not know where to reply to");
+            }
+
+            TxTestUtil.enlistTestXAResource(tm.getTransaction());
+
+            try (
+                JMSContext context = factory.createContext()
+            ) {
+                context.createProducer()
+                    .setJMSCorrelationID(message.getJMSMessageID())
+                    .send(replyTo, REPLY_PREFIX + ((TextMessage) message).getText());
+            }
+
+            // would timeout txn when TransactionTimeout be cared
+            TxTestUtil.waitForTimeout(tm);
+        } catch (Exception e) {
+            throw new RuntimeException("onMessage method execution failed", e);
+        }
+
+    }
+}

testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/mdb/timeout/NoTimeoutMDB.java
@@ -1,0 +1,81 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.mdb.timeout;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.transaction.TransactionManager;
+import javax.transaction.TransactionSynchronizationRegistry;
+import org.jboss.as.test.integration.ejb.transaction.utils.TxTestUtil;
+import org.jboss.logging.Logger;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = TransactionTimeoutQueueSetupTask.NO_TIMEOUT_JNDI_NAME)
+})
+public class NoTimeoutMDB implements MessageListener {
+    private static final Logger log = Logger.getLogger(NoTimeoutMDB.class);
+    public static final String REPLY_PREFIX = "replying ";
+
+    @Resource(lookup = "java:/JmsXA")
+    private ConnectionFactory factory;
+
+    @Resource(name = "java:jboss/TransactionManager")
+    private TransactionManager tm;
+
+    @Resource
+    private TransactionSynchronizationRegistry synchroRegistry;
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            log.tracef("onMessage received message: %s '%s'", message, ((TextMessage) message).getText());
+
+            final Destination replyTo = message.getJMSReplyTo();
+
+            if (replyTo == null) {
+                throw new RuntimeException("ReplyTo info in message was not specified"
+                    + " and bean does not know where to reply to");
+            }
+
+            TxTestUtil.enlistTestXAResource(tm.getTransaction());
+            TxTestUtil.addSynchronization(tm.getTransaction());
+
+            try (
+                JMSContext context = factory.createContext()
+            ) {
+                context.createProducer()
+                    .setJMSCorrelationID(message.getJMSMessageID())
+                    .send(replyTo, REPLY_PREFIX + ((TextMessage) message).getText());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("onMessage method execution failed", e);
+        }
+    }
+}
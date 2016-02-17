8ea4bc8e494c10c1a052f4bc65a19d66cbc9156e
ochaloup <ochaloup@redhat.com>
2016-02-17 21:50:58 +0100

    [WFLY-7205] Adding tests for transaction timeout behavior
    [WFLY-7205] Adding tests for transaction timeout behavior when BMT is used
    [WFLY-7205] Adding tests for transaction timeout behavior for MDB

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/bmt/timeout/StatelessTimeoutTestCase.java
@@ -0,0 +1,123 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.bmt.timeout;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.test.integration.transactions.TransactionTestLookupUtil;
+import org.jboss.as.test.integration.transactions.TransactionCheckerSingleton;
+import org.jboss.as.test.integration.transactions.TxTestUtil;
+import org.jboss.as.test.shared.TimeoutUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * BMT test where transaction timeout is involved.
+ * The bean uses mock XAResources to simulate to get 2PC happen.
+ */
+@RunWith(Arquillian.class)
+public class StatelessTimeoutTestCase {
+
+    @ArquillianResource
+    private InitialContext initCtx;
+
+    @Inject
+    private SingletonChecker checker;
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        final JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "stateless-btm-txn-timeout.jar")
+            .addPackage(StatelessTimeoutTestCase.class.getPackage())
+            .addPackage(TxTestUtil.class.getPackage())
+            .addClasses(TimeoutUtil.class)
+            .addAsManifestResource(new StringAsset("<beans></beans>"),  "beans.xml");
+        return jar;
+    }
+
+    @Before
+    public void startUp() throws NamingException {
+        checker.resetAll();
+    }
+
+    /**
+     * Calling BTM method where transaction timeout is not modified.
+     * Call is expected to be processed.
+     */
+    @Test
+    public void noTimeout() throws Exception {
+        StatelessBmtBean bean = TestLookupUtil.lookupModule(initCtx, StatelessBmtBean.class);
+        bean.testTransaction(1, 0);
+    }
+
+    /**
+     * Calling BTM method where 3 transactions are started and one of them timeouts.
+     * Each transaction contains 2 XA Resources.
+     * <br>
+     * The assertion checks that 2 transactions were committed and one rolled-back.
+     */
+    @Test
+    public void timeout() throws Exception {
+        StatelessBmtBean bean = TestLookupUtil.lookupModule(initCtx, StatelessBmtBean.class);
+        bean.testTransaction(3, 1);
+
+        Assert.assertEquals("Two times two XA resources - for each commit happened", 4, checker.getCommitted());
+        Assert.assertEquals("One time two XA resources - for each rollback happened", 2, checker.getRolledback());
+    }
+
+    /**
+     * Calling BTM method where 4 transactions are started and 3 of them timeouts.
+     * Each transaction contains 2 XA Resources.
+     * <br>
+     * The assertion checks that 1 transaction was committed and 3 rolled-back.
+     */
+    @Test
+    public void timeoutMultiple() throws Exception {
+        StatelessBmtBean bean = TestLookupUtil.lookupModule(initCtx, StatelessBmtBean.class);
+        bean.testTransaction(4, 3);
+        
+        Assert.assertEquals("One time two XA resources - for each commit happened", 2, checker.getCommitted());
+        Assert.assertEquals("Three times two XA resources - for each rollback happened", 6, checker.getRolledback());
+    }
+
+    /**
+     * Calling BTM method where transaction timeout is defined small enough to get transaction timeout happens.
+     * Such transaction is rolled-back.
+     * Then transaction timeout is set to default value and other transaction is started which successfully commits.
+     */
+    @Test
+    public void resetTimeoutToDefault() throws Exception {
+        StatelessBmtRestartTimeoutBean bean = TestLookupUtil.lookupModule(initCtx, StatelessBmtRestartTimeoutBean.class);
+        bean.test();
+    }
+    
+}
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/transaction/utils/SingletonChecker.java
@@ -0,0 +1,154 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright (c) 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.transaction.utils;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.LocalBean;
+import javax.ejb.Remote;
+import javax.ejb.Singleton;
+
+@Singleton
+@LocalBean
+@Remote
+@ManagedBean
+public class SingletonChecker implements SingletonCheckerRemote {
+    private int committed, rolledback;
+    private int  synchronizedBegin, synchronizedBefore, synchronizedAfter,
+        synchronizedAfterCommitted, synchronizedAfterRolledBack; 
+
+    @Override
+    public int getCommitted() {
+        return committed;
+    }
+
+    @Override
+    public void addCommit() {
+        committed++;
+    }
+
+    @Override
+    public int getRolledback() {
+        return rolledback;
+    }
+
+    @Override
+    public void addRollback() {
+        rolledback++;
+    }
+
+    @Override
+    public boolean isSynchronizedBefore() {
+        return synchronizedBefore > 0;
+    }
+
+    @Override
+    public void setSynchronizedBefore() {
+        synchronizedBefore++;
+    }
+
+    @Override
+    public boolean isSynchronizedAfter() {
+        return synchronizedAfter > 0;
+    }
+
+    @Override
+    public void setSynchronizedAfter(boolean isCommit) {
+        synchronizedAfter++;
+        if(isCommit) {
+            synchronizedAfterCommitted++;
+        } else {
+            synchronizedAfterRolledBack++;
+        }
+    }
+    
+    @Override
+    public boolean isSynchronizedBegin() {
+        return synchronizedBegin > 0;
+    }
+    
+    @Override
+    public void setSynchronizedBegin() {
+        synchronizedBegin++;
+    }
+
+    @Override
+    public void resetCommitted() {
+        committed = 0;
+    }
+
+    @Override
+    public void resetRolledback() {
+        rolledback = 0;
+    }
+
+    @Override
+    public void resetSynchronizedBefore() {
+        synchronizedBefore = 0;
+    }
+
+    @Override
+    public void resetSynchronizedAfter() {
+        synchronizedAfter = 0;
+        synchronizedAfterCommitted = 0;
+        synchronizedAfterRolledBack = 0;
+    }
+    
+    @Override
+    public void resetSynchronizedBegin() {
+        synchronizedBegin = 0;
+    }
+
+    @Override
+    public int countSynchronizedBefore() {
+        return synchronizedBefore;
+    }
+
+    @Override
+    public int countSynchronizedAfter() {
+        return synchronizedAfter;
+    }
+    
+    @Override
+    public int countSynchronizedAfterCommitted() {
+        return synchronizedAfterCommitted;
+    }
+    
+    @Override
+    public int countSynchronizedAfterRolledBack() {
+        return synchronizedAfterRolledBack;
+    }
+
+    @Override
+    public int countSynchronizedBegin() {
+        return synchronizedBegin;
+    }
+    
+    @Override
+    public void resetAll() {
+        resetCommitted();
+        resetRolledback();
+        resetSynchronizedAfter();
+        resetSynchronizedBefore();
+        resetSynchronizedBegin();
+    }
+}
