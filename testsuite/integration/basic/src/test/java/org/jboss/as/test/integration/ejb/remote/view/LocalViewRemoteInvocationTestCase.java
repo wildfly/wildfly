8616612734924c0a8a9781aed94669c866b12981
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-02-03 17:15:58 +1100

    Fix issue with LocalViewRemoteInvocationTestCase

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/view/LocalViewRemoteInvocationTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/view/LocalViewRemoteInvocationTestCase.java
@@ -145,7 +145,7 @@ public class LocalViewRemoteInvocationTestCase {
         try {
             final String echo = localEcho.echo(message);
             Assert.fail("Remote invocation on a local view " + LocalEcho.class.getName() + " was expected to fail");
-        } catch (EJBException nsee) {
+        } catch (IllegalStateException nsee) {
             // expected
             logger.trace("Got the expected exception on invoking on a local view, remotely", nsee);
         }
@@ -165,7 +165,7 @@ public class LocalViewRemoteInvocationTestCase {
         try {
             final String echo = localEcho.echo(message);
             Assert.fail("Remote invocation on a local view " + LocalEcho.class.getName() + " was expected to fail");
-        } catch (EJBException nsee) {
+        } catch (IllegalStateException nsee) {
             // expected
             logger.trace("Got the expected exception on invoking on a local view, remotely", nsee);
         }
@@ -185,7 +185,7 @@ public class LocalViewRemoteInvocationTestCase {
         try {
             final String echo = localEcho.echo(message);
             Assert.fail("Remote invocation on a local view " + LocalEcho.class.getName() + " was expected to fail");
-        } catch (EJBException nsee) {
+        } catch (IllegalStateException nsee) {
             // expected
             logger.trace("Got the expected exception on invoking on a local view, remotely", nsee);
         }
