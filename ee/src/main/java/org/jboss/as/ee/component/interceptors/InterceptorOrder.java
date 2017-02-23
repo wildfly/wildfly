38f8f5915b40d036bd0fd1a904d6a13916f3fa2c
Farah Juma <fjuma@redhat.com>
2017-02-23 16:59:37 -0500

    [WFLY-8227] Ensure principalVsRolesMap always gets processed when using Elytron regardless of whether or not JACC is enabled

--- ee/src/main/java/org/jboss/as/ee/component/interceptors/InterceptorOrder.java
+++ ee/src/main/java/org/jboss/as/ee/component/interceptors/InterceptorOrder.java
@@ -176,6 +176,7 @@ public class InterceptorOrder {
         public static final int USER_APP_SPECIFIC_CONTAINER_INTERCEPTORS = 0x249;
         public static final int SECURITY_CONTEXT = 0x250;
         public static final int POLICY_CONTEXT = 0x260;
+        public static final int SECURITY_ROLES = 0x270;
         public static final int EJB_SECURITY_AUTHORIZATION_INTERCEPTOR = 0x300;
         public static final int RUN_AS_PRINCIPAL = 0x310;
         public static final int EXTRA_PRINCIPAL_ROLES = 0x320;
