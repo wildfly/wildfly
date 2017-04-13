9c6eef77b5a8f2dce2d53b303034f2c57e314ece
Paul Ferraro <paul.ferraro@redhat.com>
2017-04-13 18:03:17 -0400

    Transport capabilities should have only 1 dynamic name component, i.e. the stack name

--- clustering/jgroups/extension/src/main/java/org/jboss/as/clustering/jgroups/subsystem/TransportResourceDefinition.java
+++ clustering/jgroups/extension/src/main/java/org/jboss/as/clustering/jgroups/subsystem/TransportResourceDefinition.java
@@ -107,7 +107,7 @@ public class TransportResourceDefinition<T extends TP> extends AbstractProtocolR
 
         @Override
         public RuntimeCapability<Void> resolve(PathAddress address) {
-            return this.definition.fromBaseCapability(address.getParent().getLastElement().getValue(), address.getLastElement().getValue());
+            return this.definition.fromBaseCapability(address.getParent().getLastElement().getValue());
         }
     }
 
