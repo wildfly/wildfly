362368db669a177b0087fafa276fbd2f306bb51e
Farah Juma <fjuma@redhat.com>
2017-04-19 19:08:59 -0400

    [WFLY-8562] Update Elytron-specific tests to make use of the new test utility method for switching identities

--- testsuite/integration/elytron/src/test/java/org/wildfly/test/integration/elytron/ejb/WhoAmIServlet.java
+++ testsuite/integration/elytron/src/test/java/org/wildfly/test/integration/elytron/ejb/WhoAmIServlet.java
@@ -21,15 +21,13 @@
  */
 package org.wildfly.test.integration.elytron.ejb;
 
-import static org.jboss.as.test.shared.integration.ejb.security.Util.getCLMLoginContext;
-
 import java.io.IOException;
 import java.io.Writer;
+import java.util.concurrent.Callable;
+
 import javax.annotation.security.DeclareRoles;
 import javax.ejb.EJB;
 import javax.ejb.EJBAccessException;
-import javax.security.auth.login.LoginContext;
-import javax.security.auth.login.LoginException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
@@ -37,6 +35,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.as.test.shared.integration.ejb.security.Util;
 
 /**
  * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
@@ -58,23 +58,15 @@ public class WhoAmIServlet extends HttpServlet {
         String role = req.getParameter("role");
 
         if ("whoAmI".equals(method)) {
-            LoginContext lc = null;
             try {
-                if (username != null && password != null) {
-                    lc = getCLMLoginContext(username, password);
-                    lc.login();
-                }
-                try {
+                Callable<Void> callable = () -> {
                     writer.write(bean.whoAmI());
-                } finally {
-                    if (lc != null) {
-                        lc.logout();
-                    }
-                }
-            } catch (LoginException le) {
-                throw new IOException("Unexpected failure", le);
+                    return null;
+                };
+                Util.switchIdentity(username, password, callable);
+            } catch (Exception e) {
+                throw new IOException("Unexpected failure", e);
             }
-
         } else if ("doubleWhoAmI".equals(method)) {
             String[] response;
             try {
@@ -86,26 +78,19 @@ public class WhoAmIServlet extends HttpServlet {
             } catch (EJBAccessException e) {
                 resp.sendError(HttpServletResponse.SC_FORBIDDEN, e.toString());
                 return;
-            } catch (LoginException e) {
+            } catch (Exception e) {
                 throw new ServletException("Unexpected failure", e);
             }
             writer.write(response[0] + "," + response[1]);
         } else if ("doIHaveRole".equals(method)) {
-            LoginContext lc = null;
             try {
-                if (username != null && password != null) {
-                    lc = getCLMLoginContext(username, password);
-                    lc.login();
-                }
-                try {
+                Callable<Void> callable = () -> {
                     writer.write(String.valueOf(bean.doIHaveRole(role)));
-                } finally {
-                    if (lc != null) {
-                        lc.logout();
-                    }
-                }
-            } catch (LoginException le) {
-                throw new IOException("Unexpected failure", le);
+                    return null;
+                };
+                Util.switchIdentity(username, password, callable);
+            } catch (Exception e) {
+                throw new IOException("Unexpected failure", e);
             }
         } else if ("doubleDoIHaveRole".equals(method)) {
             try {
