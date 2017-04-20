15baa29c3afa2bc6393a258f4a7f29fa6d35a23c
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-04-20 12:05:40 +1000

    WFLY-8608 Application client ignores remote host address in ejb-client-properties file passed to it in argument

--- appclient/src/main/java/org/jboss/as/appclient/subsystem/AppClientSubsystemAdd.java
+++ appclient/src/main/java/org/jboss/as/appclient/subsystem/AppClientSubsystemAdd.java
@@ -46,9 +46,11 @@ import org.jboss.as.server.Services;
 import org.jboss.as.server.deployment.Phase;
 import org.jboss.as.server.deployment.jbossallxml.JBossAllXmlParserRegisteringProcessor;
 import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.Service;
 import org.jboss.msc.service.ServiceName;
-import org.jboss.msc.service.ValueService;
-import org.jboss.msc.value.ImmediateValue;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
 
 import static org.jboss.as.appclient.subsystem.Constants.CONNECTION_PROPERTIES_URL;
 import static org.jboss.as.appclient.subsystem.Constants.HOST_URL;
@@ -61,6 +63,7 @@ import static org.jboss.as.appclient.subsystem.Constants.HOST_URL;
 class AppClientSubsystemAdd extends AbstractBoottimeAddStepHandler {
 
     public static final ServiceName APP_CLIENT_URI_SERVICE_NAME = ServiceName.JBOSS.append("ejb3", "ejbClientContext", "appClientUri");
+    public static final ServiceName APP_CLIENT_EJB_PROPERTIES_SERVICE_NAME = ServiceName.JBOSS.append("ejb3", "ejbClientContext", "appClientEjbProperties");
 
     static final AppClientSubsystemAdd INSTANCE = new AppClientSubsystemAdd();
 
@@ -93,7 +96,7 @@ class AppClientSubsystemAdd extends AbstractBoottimeAddStepHandler {
                 processorTarget.addDeploymentProcessor(Constants.SUBSYSTEM_NAME, Phase.POST_MODULE, Phase.POST_MODULE_APPLICATION_CLIENT_MANIFEST, new ApplicationClientManifestProcessor());
                 processorTarget.addDeploymentProcessor(Constants.SUBSYSTEM_NAME, Phase.POST_MODULE, Phase.POST_MODULE_APPLICATION_CLIENT_ACTIVE, new ActiveApplicationClientProcessor(deployment));
                 processorTarget.addDeploymentProcessor(Constants.SUBSYSTEM_NAME, Phase.DEPENDENCIES, Phase.DEPENDENCIES_APPLICATION_CLIENT, new ApplicationClientDependencyProcessor());
-                processorTarget.addDeploymentProcessor(Constants.SUBSYSTEM_NAME, Phase.INSTALL, Phase.INSTALL_APPLICATION_CLIENT, new ApplicationClientStartProcessor(hostUrl, connectionPropertiesUrl, parameters.toArray(EMPTY_STRING)));
+                processorTarget.addDeploymentProcessor(Constants.SUBSYSTEM_NAME, Phase.INSTALL, Phase.INSTALL_APPLICATION_CLIENT, new ApplicationClientStartProcessor(parameters.toArray(EMPTY_STRING)));
 
             }
         }, OperationContext.Stage.RUNTIME);
@@ -106,18 +109,50 @@ class AppClientSubsystemAdd extends AbstractBoottimeAddStepHandler {
                         .install();
 
         try {
-            URI uri;
-            if(hostUrl == null) {
-                uri = new URI("remote+http://localhost:8080");
+            if(connectionPropertiesUrl != null) {
+                context.getServiceTarget().addService(APP_CLIENT_URI_SERVICE_NAME, new ConstantService<>(null))
+                        .install();
+                context.getServiceTarget().addService(APP_CLIENT_EJB_PROPERTIES_SERVICE_NAME, new ConstantService<>(connectionPropertiesUrl))
+                        .install();
             } else {
-                uri = new URI(hostUrl);
+                URI uri;
+                if (hostUrl == null) {
+                    uri = new URI("remote+http://localhost:8080");
+                } else {
+                    uri = new URI(hostUrl);
+                }
+                context.getServiceTarget().addService(APP_CLIENT_URI_SERVICE_NAME, new ConstantService<>(uri))
+                        .install();
+                context.getServiceTarget().addService(APP_CLIENT_EJB_PROPERTIES_SERVICE_NAME, new ConstantService<>(connectionPropertiesUrl))
+                        .install();
             }
-            context.getServiceTarget().addService(APP_CLIENT_URI_SERVICE_NAME, new ValueService<>(new ImmediateValue<>(uri)))
-                    .install();
         } catch (URISyntaxException e) {
             throw new OperationFailedException(e);
         }
 
     }
 
+    private static final class ConstantService<T> implements Service<T> {
+
+        private final T value;
+
+        private ConstantService(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public void start(StartContext startContext) throws StartException {
+
+        }
+
+        @Override
+        public void stop(StopContext stopContext) {
+
+        }
+
+        @Override
+        public T getValue() throws IllegalStateException, IllegalArgumentException {
+            return value;
+        }
+    }
 }
