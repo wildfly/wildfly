c8964b3a66902e53961a90a2fa49e09aa3a1f88e
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2017-05-22 14:02:00 +0100

    [WFLY-8432] Fixing review issues

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/messaging/mgmt/ConnectionFactoryClientMappingTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/messaging/mgmt/ConnectionFactoryClientMappingTestCase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.as.test.integration.messaging.mgmt;
 
 import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
@@ -6,10 +22,13 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.controller.client.helpers.ClientConstants;
 import org.jboss.as.test.integration.common.jms.JMSOperations;
 import org.jboss.as.test.integration.common.jms.JMSOperationsProvider;
 import org.jboss.as.test.shared.ServerReload;
 import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -19,8 +38,13 @@ import org.junit.runner.RunWith;
 
 import javax.annotation.Resource;
 import javax.jms.ConnectionFactory;
+import java.io.IOException;
 import java.util.Map;
 
+import static org.jboss.as.controller.client.helpers.ClientConstants.*;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -33,12 +57,14 @@ public class ConnectionFactoryClientMappingTestCase {
     private static final String CONNECTION_FACTORY_JNDI_NAME = "java:jboss/exported/jms/TestConnectionFactory";
 
     static class SetupTask implements ServerSetupTask {
+        private static final Logger logger = Logger.getLogger(ConnectionFactoryClientMappingTestCase.SetupTask.class);
+
         @Override
         public void setup(org.jboss.as.arquillian.container.ManagementClient managementClient, String s) throws Exception {
             JMSOperations ops = JMSOperationsProvider.getInstance(managementClient.getControllerClient());
 
-            ops.addSocketBinding("test-binding", clientMapping("test", "8000"));
-            ops.addJmsConnector("http-test-connector", "test-binding", "http-acceptor");
+            addSocketBinding(managementClient, "test-binding", clientMapping("test", "8000"));
+            ops.addHttpConnector("http-test-connector", "test-binding", "http-acceptor");
             ModelNode attr = new ModelNode();
             attr.get("connectors").add("http-test-connector");
             ops.addJmsConnectionFactory("TestConnectionFactory", CONNECTION_FACTORY_JNDI_NAME, attr);
@@ -58,11 +84,55 @@ public class ConnectionFactoryClientMappingTestCase {
             JMSOperations ops = JMSOperationsProvider.getInstance(managementClient.getControllerClient());
 
             ops.removeJmsConnectionFactory("TestConnectionFactory");
-            ops.removeJmsConnector("http-test-connector");
-            ops.removeSocketBinding("test-binding");
+            ops.removeHttpConnector("http-test-connector");
+            removeSocketBinding(managementClient, "test-binding");
 
             ServerReload.executeReloadAndWaitForCompletion(managementClient.getControllerClient());
         }
+
+        private void addSocketBinding(ManagementClient managementClient, String bindingName, ModelNode clientMapping) throws Exception {
+            ModelNode address = new ModelNode();
+            address.add("socket-binding-group", "standard-sockets");
+            address.add("socket-binding", bindingName);
+
+            ModelNode socketBindingOp = new ModelNode();
+            socketBindingOp.get(OP).set(ADD);
+            socketBindingOp.get(OP_ADDR).set(address);
+
+            execute(managementClient, socketBindingOp);
+
+            ModelNode clientMappingOp = new ModelNode();
+            clientMappingOp.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
+            clientMappingOp.get(OP_ADDR).set(address);
+            clientMappingOp.get(NAME).set("client-mappings");
+            clientMappingOp.get(VALUE).add(clientMapping);
+
+            execute(managementClient, clientMappingOp);
+        }
+
+        private void removeSocketBinding(ManagementClient managementClient, String bindingName) throws Exception {
+            ModelNode address = new ModelNode();
+            address.add("socket-binding-group", "standard-sockets");
+
+            ModelNode socketBindingOp = new ModelNode();
+            socketBindingOp.get(OP).set(REMOVE_OPERATION);
+            socketBindingOp.get(OP_ADDR).set(address);
+
+            execute(managementClient, socketBindingOp);
+        }
+
+        static void execute(ManagementClient managementClient, final ModelNode operation) throws IOException {
+            ModelNode result = managementClient.getControllerClient().execute(operation);
+            if (result.hasDefined(ClientConstants.OUTCOME) && ClientConstants.SUCCESS.equals(result.get(ClientConstants.OUTCOME).asString())) {
+                logger.trace("Operation successful for update = " + operation.toString());
+            } else if (result.hasDefined(ClientConstants.FAILURE_DESCRIPTION)) {
+                final String failureDesc = result.get(ClientConstants.FAILURE_DESCRIPTION).toString();
+                throw new RuntimeException(failureDesc);
+            } else {
+                throw new RuntimeException("Operation not successful; outcome = " + result.get(ClientConstants.OUTCOME));
+            }
+        }
+
     }
 
     @Deployment(testable = true)
