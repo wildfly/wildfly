399b88af2cbc03748d1b31bc7e0413179ff4a7fe
Brian Stansberry <brian.stansberry@redhat.com>
2017-05-26 17:20:21 -0500

    [WFLY-8849] Temporarily disable rollout of 'probe' to the domain if WFCORE-2858 is implemented

--- transactions/src/main/java/org/jboss/as/txn/subsystem/LogStoreDefinition.java
+++ transactions/src/main/java/org/jboss/as/txn/subsystem/LogStoreDefinition.java
@@ -58,6 +58,7 @@ public class LogStoreDefinition extends SimpleResourceDefinition {
     public void registerOperations(ManagementResourceRegistration resourceRegistration) {
         super.registerOperations(resourceRegistration);
         final OperationDefinition probe = new SimpleOperationDefinitionBuilder(LogStoreConstants.PROBE, getResourceDescriptionResolver())
+                                .withFlag(OperationEntry.Flag.HOST_CONTROLLER_ONLY) // TODO WFLY-8852 decide how we want to handle this in a domain
                                 .setRuntimeOnly()
                                 .setReadOnly()
                                 .build();
