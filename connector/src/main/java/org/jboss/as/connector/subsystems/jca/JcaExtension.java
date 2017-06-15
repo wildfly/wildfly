f04fa1f54948557eb280735ee71bcfd3c4598c88
Tomaz Cerar <tomaz.cerar@gmail.com>
2017-06-15 20:47:41 +0200

    WFLY-8941 JCA subsystem model version should be 5.0.0

--- connector/src/main/java/org/jboss/as/connector/subsystems/jca/JcaExtension.java
+++ connector/src/main/java/org/jboss/as/connector/subsystems/jca/JcaExtension.java
@@ -79,7 +79,7 @@ public class JcaExtension implements Extension {
 
     public static final String SUBSYSTEM_NAME = "jca";
 
-    private static final ModelVersion CURRENT_MODEL_VERSION = ModelVersion.create(4, 1, 0);
+    private static final ModelVersion CURRENT_MODEL_VERSION = ModelVersion.create(5, 0, 0);
 
     private static final String RESOURCE_NAME = JcaExtension.class.getPackage().getName() + ".LocalDescriptions";
 
