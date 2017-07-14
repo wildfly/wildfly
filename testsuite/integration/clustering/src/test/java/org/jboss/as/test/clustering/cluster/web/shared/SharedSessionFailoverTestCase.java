ddee9e0432bc059ac498e6907632910e5373c87e
Radim Hatlapatka <rhatlapa@redhat.com>
2017-07-14 16:32:10 +0200

    [WFLY-9087] SharedSessionFailoverTestCase URLs resolving fixed
    URLs were incorrectly resolved all pointing to war2 deployment instead
    of one group to war1 and second group to war2 deployment.

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/web/shared/SharedSessionFailoverTestCase.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/web/shared/SharedSessionFailoverTestCase.java
@@ -92,14 +92,16 @@ public class SharedSessionFailoverTestCase extends ClusterAbstractTestCase {
 
     @Test
     public void test(
-            @ArquillianResource(SimpleServlet.class) @OperateOnDeployment(DEPLOYMENT_1) URL baseURL1,
-            @ArquillianResource(SimpleServlet.class) @OperateOnDeployment(DEPLOYMENT_2) URL baseURL2)
+            @ArquillianResource @OperateOnDeployment(DEPLOYMENT_1) URL baseURLDep1,
+            @ArquillianResource @OperateOnDeployment(DEPLOYMENT_2) URL baseURLDep2)
             throws URISyntaxException, IOException {
+        URL baseURL1 = new URL(baseURLDep1.toExternalForm() + "/");
+        URL baseURL2 = new URL(baseURLDep2.toExternalForm() + "/");
 
-        URI uri11 = SimpleServlet.createURI(baseURL1.toURI().resolve(MODULE_1).toURL());
-        URI uri12 = SimpleServlet.createURI(baseURL1.toURI().resolve(MODULE_2).toURL());
-        URI uri21 = SimpleServlet.createURI(baseURL2.toURI().resolve(MODULE_1).toURL());
-        URI uri22 = SimpleServlet.createURI(baseURL2.toURI().resolve(MODULE_2).toURL());
+        URI uri11 = SimpleServlet.createURI(baseURL1.toURI().resolve(MODULE_1 + "/").toURL());
+        URI uri12 = SimpleServlet.createURI(baseURL1.toURI().resolve(MODULE_2 + "/").toURL());
+        URI uri21 = SimpleServlet.createURI(baseURL2.toURI().resolve(MODULE_1 + "/").toURL());
+        URI uri22 = SimpleServlet.createURI(baseURL2.toURI().resolve(MODULE_2 + "/").toURL());
 
         try (CloseableHttpClient client = TestHttpClientUtils.promiscuousCookieHttpClient()) {
             HttpResponse response = client.execute(new HttpGet(uri11));
