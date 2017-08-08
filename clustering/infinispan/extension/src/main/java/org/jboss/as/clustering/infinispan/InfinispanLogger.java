934bae467fc838122988c8187b42f38ce1ab4f3f
Radoslav Husar <radosoft@gmail.com>
2017-08-08 23:13:18 +0200

    WFLY-9192 InfinispanLogger executorIgnored() has no effect

--- clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/InfinispanLogger.java
+++ clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/InfinispanLogger.java
@@ -127,6 +127,7 @@ public interface InfinispanLogger extends BasicLogger {
     @Message(id = 27, value = "Could not determine 'stack' attribute from JGroups subsystem")
     String indeterminiteStack();
 
+    @LogMessage(level = WARN)
     @Message(id = 28, value = "Executor configuration '%s' was deprecated and will only be used to support legacy slaves in the domain.")
-    String executorIgnored(String executorName);
+    void executorIgnored(String executorName);
 }
