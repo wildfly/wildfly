f737683c2a281a6ecc1bd790c6b046a56e544da2
Radoslav Husar <radosoft@gmail.com>
2017-10-04 23:59:08 +0200

    WFLY-9370 Fix failing SingletonTunnelTestCase * remove tunnel transport; use DISCARD for simulation * make it run by default * use SingletonDefaultCacheRequirement for service name resolution * fix dependency on singleton ceche container * better name

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/singleton/partition/SingletonServiceActivator.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/singleton/partition/SingletonServiceActivator.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2015, Red Hat, Inc., and individual contributors
+ * Copyright 2017, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.jboss.as.test.clustering.tunnel.singleton;
+package org.jboss.as.test.clustering.cluster.singleton.partition;
 
 import org.jboss.as.test.clustering.cluster.singleton.service.NodeService;
 import org.jboss.msc.service.ServiceActivator;
@@ -30,8 +30,8 @@ import org.jboss.msc.service.ServiceRegistryException;
 import org.jboss.msc.service.ServiceTarget;
 import org.jboss.msc.value.InjectedValue;
 import org.wildfly.clustering.group.Group;
+import org.wildfly.clustering.singleton.SingletonDefaultCacheRequirement;
 import org.wildfly.clustering.singleton.SingletonServiceBuilderFactory;
-import org.wildfly.clustering.singleton.SingletonServiceName;
 import org.wildfly.clustering.singleton.election.NamePreference;
 import org.wildfly.clustering.singleton.election.PreferredSingletonElectionPolicy;
 import org.wildfly.clustering.singleton.election.SimpleSingletonElectionPolicy;
@@ -53,7 +53,7 @@ public class SingletonServiceActivator implements ServiceActivator {
     @Override
     public void activate(ServiceActivatorContext context) {
         try {
-            SingletonServiceBuilderFactory factory = (SingletonServiceBuilderFactory) context.getServiceRegistry().getRequiredService(SingletonServiceName.BUILDER.getServiceName(CONTAINER_NAME)).awaitValue();
+            SingletonServiceBuilderFactory factory = (SingletonServiceBuilderFactory) context.getServiceRegistry().getRequiredService(ServiceName.parse(SingletonDefaultCacheRequirement.SINGLETON_SERVICE_BUILDER_FACTORY.resolve(CONTAINER_NAME))).awaitValue();
             ServiceTarget target = context.getServiceTarget();
             install(target, factory, SERVICE_A_NAME, SERVICE_A_PREFERRED_NODE);
             install(target, factory, SERVICE_B_NAME, SERVICE_B_PREFERRED_NODE);
@@ -68,7 +68,9 @@ public class SingletonServiceActivator implements ServiceActivator {
         factory.createSingletonServiceBuilder(name, service)
             .electionPolicy(new PreferredSingletonElectionPolicy(new SimpleSingletonElectionPolicy(), new NamePreference(preferredNode)))
             .build(target)
-                .addDependency(ServiceName.JBOSS.append("clustering", "group", "default"), Group.class, group)
+                // Currently uses public capability name to obtain the service name;
+                // see https://issues.jboss.org/browse/WFLY-9395 for making this part of API
+                .addDependency(ServiceName.parse("org.wildfly.clustering.default-group"), Group.class, group)
                 .install();
     }
 }
