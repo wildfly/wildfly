574eba95526a7b9e458edd582caace229323164e
Tomasz Adamski <tadamski@redhat.com>
2017-11-29 15:13:10 +0100

    [WFLY-7085] EJB: verify business interface methods not declared final - @Local bug fix

--- ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/BusinessViewAnnotationProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/BusinessViewAnnotationProcessor.java
@@ -133,7 +133,7 @@ public class BusinessViewAnnotationProcessor implements DeploymentUnitProcessor 
         // fetch the local business interfaces of the bean
         Collection<Class<?>> localBusinessInterfaces = this.getLocalBusinessInterfaces(deploymentUnit, sessionBeanClass);
         if (localBusinessInterfaces != null && !localBusinessInterfaces.isEmpty()) {
-            verifyViewMethodsNotDeclaredFinal(sessionBeanClass, remoteBusinessInterfaces);
+            verifyViewMethodsNotDeclaredFinal(sessionBeanClass, localBusinessInterfaces);
             sessionBeanComponentDescription.addLocalBusinessInterfaceViews(this.toString(localBusinessInterfaces));
         }
 
@@ -208,7 +208,7 @@ public class BusinessViewAnnotationProcessor implements DeploymentUnitProcessor 
             for (Method ifceMethod : ifce.getMethods()) {
                 try {
                     final Method beanMethod = sessionBeanClass.getMethod(ifceMethod.getName(), ifceMethod.getParameterTypes());
-                    if ((beanMethod.getModifiers() & Modifier.FINAL) == Modifier.FINAL) {
+                    if (Modifier.isFinal(beanMethod.getModifiers())) {
                         throw EjbLogger.ROOT_LOGGER.businessViewMethodDeclaredFinal(beanMethod.getName(), sessionBeanClass.getName());
                     }
                 } catch (NoSuchMethodException e) {
