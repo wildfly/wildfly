57a445322227e6a7f4c2ecfe19c0b42864a65c8e
Radoslav Husar <radosoft@gmail.com>
2017-12-07 17:42:57 +0100

    WFLY-9598 Move/cleanup "db persistence" test case

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/web/persistence/DatabasePersistenceWebFailoverTestCase.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/web/persistence/DatabasePersistenceWebFailoverTestCase.java
@@ -1,4 +1,26 @@
-package org.jboss.as.test.clustering.cluster.session.persistence;
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.clustering.cluster.web.persistence;
 
 import org.infinispan.transaction.TransactionMode;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -17,10 +39,10 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 @RunAsClient
 @org.junit.Ignore("WFLY-2409")
-public class SessionClusterDbPersistenceTestCase extends AbstractWebFailoverTestCase {
+public class DatabasePersistenceWebFailoverTestCase extends AbstractWebFailoverTestCase {
     private static final String DEPLOYMENT_NAME = "session-db-cluster.war";
 
-    public SessionClusterDbPersistenceTestCase() {
+    public DatabasePersistenceWebFailoverTestCase() {
         super(DEPLOYMENT_NAME, TransactionMode.TRANSACTIONAL);
     }
 
@@ -40,8 +62,8 @@ public class SessionClusterDbPersistenceTestCase extends AbstractWebFailoverTest
         WebArchive war = ShrinkWrap.create(WebArchive.class, DEPLOYMENT_NAME);
         war.addClasses(SimpleServlet.class, Mutable.class);
         ClusterTestUtil.addTopologyListenerDependencies(war);
-        war.setWebXML(SessionClusterDbPersistenceTestCase.class.getPackage(), "web.xml");
-        war.addAsWebInfResource("WEB-INF/jboss-web.xml","jboss-web.xml");
+        war.setWebXML(AbstractWebFailoverTestCase.class.getPackage(), "web.xml");
+        war.addAsWebInfResource("jboss-web.xml", "jboss-web.xml");
         return war;
     }
 }
