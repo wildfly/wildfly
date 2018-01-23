7fc65f9374673d265d0b2f0cc401fb2e87d1c305
Richard Opálka <opalka.richard@gmail.com>
2018-01-23 09:35:39 +0100

    [WFLY-9663] Removing dead OSGi code

--- undertow/src/main/java/org/wildfly/extension/undertow/deployment/UndertowDeploymentService.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/deployment/UndertowDeploymentService.java
@@ -30,15 +30,12 @@ import io.undertow.servlet.api.Deployment;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.jboss.as.web.common.StartupContext;
 import org.jboss.as.web.common.WebInjectionContainer;
-import org.jboss.as.web.host.ContextActivator;
 import org.jboss.msc.inject.Injector;
 import org.jboss.msc.service.Service;
-import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
@@ -127,7 +124,6 @@ public class UndertowDeploymentService implements Service<UndertowDeploymentServ
     }
 
     public void stopContext() {
-
         final ClassLoader old = Thread.currentThread().getContextClassLoader();
         DeploymentInfo deploymentInfo = deploymentInfoInjectedValue.getValue();
         Thread.currentThread().setContextClassLoader(deploymentInfo.getClassLoader());
@@ -172,55 +168,6 @@ public class UndertowDeploymentService implements Service<UndertowDeploymentServ
 
     Injector<ExecutorService> getServerExecutorInjector() {
         return this.serverExecutor;
-    }
-
-    /**
-     * Provides an API to start/stop the {@link UndertowDeploymentService}.
-     * This should register/deregister the web context.
-     */
-    protected static class ContextActivatorImpl implements ContextActivator {
-
-        private final ServiceController<UndertowDeploymentService> controller;
-
-        ContextActivatorImpl(ServiceController<UndertowDeploymentService> controller) {
-            this.controller = controller;
-        }
-
-        /**
-         * Start the web context synchronously.
-         * <p/>
-         * This would happen when the OSGi webapp gets explicitly started.
-         */
-        @Override
-        public synchronized boolean startContext() {
-            try {
-                UndertowDeploymentService service = controller.getValue();
-                service.startContext();
-            } catch (Exception ex) {
-                throw UndertowLogger.ROOT_LOGGER.cannotActivateContext(ex, controller.getName());
-            }
-            return true;
-        }
-
-        /**
-         * Stop the web context synchronously.
-         * <p/>
-         * This would happen when the OSGi webapp gets explicitly stops.
-         */
-        @Override
-        public synchronized boolean stopContext() {
-            UndertowDeploymentService service = controller.getValue();
-            service.stopContext();
-            return true;
-        }
-
-        @Override
-        public ServletContext getServletContext() {
-            UndertowDeploymentService service = controller.getValue();
-            DeploymentManager manager = service.deploymentManager;
-            Deployment deployment = manager != null ? manager.getDeployment() : null;
-            return deployment != null ? deployment.getServletContext() : null;
-        }
     }
 
     private static void recursiveDelete(File file) {
