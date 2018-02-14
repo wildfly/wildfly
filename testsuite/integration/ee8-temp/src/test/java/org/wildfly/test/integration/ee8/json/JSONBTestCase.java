3cdd29fdaddfef00c1c6e8d92c02f45e70575331
Kabir Khan <kkhan@redhat.com>
2018-02-14 22:57:23 +0000

    Move all the ee8-temp tests into the same root package

--- testsuite/integration/ee8-temp/src/test/java/org/wildfly/test/integration/ee8/json/JSONBTestCase.java
+++ testsuite/integration/ee8-temp/src/test/java/org/wildfly/test/integration/ee8/json/JSONBTestCase.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.jboss.as.test.integration.json;
+package org.wildfly.test.integration.ee8.json;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
