152418aef22fc2d66c521c1f60194fbb20641ee4
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-02-21 11:37:10 +1100

    WFLY-9872 JspTagTestCase fails intermittently

--- testsuite/integration/web/src/test/java/org/jboss/as/test/integration/jsp/JspTagTestCase.java
+++ testsuite/integration/web/src/test/java/org/jboss/as/test/integration/jsp/JspTagTestCase.java
@@ -63,7 +63,8 @@ public class JspTagTestCase {
 
     @Test
     public void test(@ArquillianResource URL url) throws Exception {
-        Assert.assertEquals(RESULT,HttpRequest.get(url + "index.jsp", 10, TimeUnit.SECONDS));
-        Assert.assertEquals(RESULT,HttpRequest.get(url + "index2.jsp", 10, TimeUnit.SECONDS));
+        //we ignore line ending differences
+        Assert.assertEquals(RESULT.replace("\r", ""),HttpRequest.get(url + "index.jsp", 10, TimeUnit.SECONDS).replace("\r", ""));
+        Assert.assertEquals(RESULT.replace("\r", ""),HttpRequest.get(url + "index2.jsp", 10, TimeUnit.SECONDS).replace("\r", ""));
     }
 }
