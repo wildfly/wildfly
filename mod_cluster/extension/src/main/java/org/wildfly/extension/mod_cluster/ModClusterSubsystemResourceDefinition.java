891fe2360f26f27e27bef7bd87fd073b2d8577db
Radoslav Husar <radosoft@gmail.com>
2018-02-27 12:59:44 +0100

    WFLY-9903 Cannot remove modcluster subsystem for the first time

--- mod_cluster/extension/src/main/java/org/wildfly/extension/mod_cluster/ModClusterSubsystemResourceDefinition.java
+++ mod_cluster/extension/src/main/java/org/wildfly/extension/mod_cluster/ModClusterSubsystemResourceDefinition.java
@@ -24,6 +24,7 @@ package org.wildfly.extension.mod_cluster;
 
 import org.jboss.as.controller.ModelVersion;
 import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.ReloadRequiredRemoveStepHandler;
 import org.jboss.as.controller.SimpleAttributeDefinition;
 import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
 import org.jboss.as.controller.SimpleResourceDefinition;
@@ -89,7 +90,7 @@ public class ModClusterSubsystemResourceDefinition extends SimpleResourceDefinit
         super(PATH,
                 ModClusterExtension.getResourceDescriptionResolver(),
                 ModClusterSubsystemAdd.INSTANCE,
-                ModClusterSubsystemRemove.INSTANCE
+                new ReloadRequiredRemoveStepHandler()
         );
         this.runtimeOnly = runtimeOnly;
     }
