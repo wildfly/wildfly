81e86e7291814e425378355b685d3da7d5953c9a
Jeff Mesnil <jmesnil@gmail.com>
2018-03-09 15:29:55 +0100

    [WFLY-9989] Bump messaging-activemq model version to 4.0.0
    Bump its XSD schema to 4.0
    
    There are no changes compared to the previous version but doing the bump
    in a separate step will make it simpler for upcoming changes to be
    picked up.
    
    JIRA: https://issues.jboss.org/browse/WFLY-9989

--- messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/MessagingExtension.java
+++ messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/MessagingExtension.java
@@ -149,12 +149,14 @@ public class MessagingExtension implements Extension {
 
     static final String RESOURCE_NAME = MessagingExtension.class.getPackage().getName() + ".LocalDescriptions";
 
-    protected static final ModelVersion VERSION_3_1_0 = ModelVersion.create(3, 1, 0);
+    protected static final ModelVersion VERSION_4_0_0 = ModelVersion.create(4, 0, 0);
     protected static final ModelVersion VERSION_3_0_0 = ModelVersion.create(3, 0, 0);
     protected static final ModelVersion VERSION_2_0_0 = ModelVersion.create(2, 0, 0);
     protected static final ModelVersion VERSION_1_0_0 = ModelVersion.create(1, 0, 0);
-    private static final ModelVersion CURRENT_MODEL_VERSION = VERSION_3_1_0;
-    private static final MessagingSubsystemParser_3_0 CURRENT_PARSER = new MessagingSubsystemParser_3_0();
+    private static final ModelVersion CURRENT_MODEL_VERSION = VERSION_4_0_0;
+
+    private static final MessagingSubsystemParser_4_0 CURRENT_PARSER = new MessagingSubsystemParser_4_0();
+
 
     public static ResourceDescriptionResolver getResourceDescriptionResolver(final String... keyPrefix) {
         return getResourceDescriptionResolver(true, keyPrefix);
@@ -212,6 +214,7 @@ public class MessagingExtension implements Extension {
     public void initializeParsers(ExtensionParsingContext context) {
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_1_0.NAMESPACE, MessagingSubsystemParser_1_0::new);
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_2_0.NAMESPACE, MessagingSubsystemParser_2_0::new);
-        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_3_0.NAMESPACE, CURRENT_PARSER);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_3_0.NAMESPACE, MessagingSubsystemParser_3_0::new);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_4_0.NAMESPACE, CURRENT_PARSER);
     }
 }
