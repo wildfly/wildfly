bfa1ee0f8f986e207bede301fe7797c69af36213
Paul Ferraro <paul.ferraro@redhat.com>
2018-05-29 10:42:25 -0400

    WFLY-10463 Ensure hashing expections of locatePrimaryOwner(...) match hasing logic of InfinispanBeanKey and InfinispanBeanGroupKey.

--- clustering/ejb/infinispan/src/main/java/org/wildfly/clustering/ejb/infinispan/Key.java
+++ clustering/ejb/infinispan/src/main/java/org/wildfly/clustering/ejb/infinispan/Key.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2016, Red Hat, Inc., and individual contributors
+ * Copyright 2018, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -19,18 +19,42 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
-package org.wildfly.clustering.ejb.infinispan.group;
-
-import org.wildfly.clustering.ejb.infinispan.BeanGroupKey;
-import org.wildfly.clustering.ejb.infinispan.Key;
+package org.wildfly.clustering.ejb.infinispan;
 
 /**
+ * An EJB cache key.
  * @author Paul Ferraro
  */
-public class InfinispanBeanGroupKey<I> extends Key<I> implements BeanGroupKey<I> {
+public class Key<I> {
+    private final I id;
 
-    public InfinispanBeanGroupKey(I id) {
-        super(id);
+    public Key(I id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the identifier of this key.
+     * @return the key value
+     */
+    public I getId() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if ((object == null) || (object.getClass() != this.getClass())) return false;
+        @SuppressWarnings("unchecked")
+        Key<I> key = (Key<I>) object;
+        return this.id.equals(key.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%s)", this.getClass().getSimpleName(), this.id.toString());
     }
 }
