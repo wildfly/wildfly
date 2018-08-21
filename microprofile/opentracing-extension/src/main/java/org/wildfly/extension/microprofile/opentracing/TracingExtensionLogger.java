0f72007f1abed77bb1a9186ff331979af9ab3f51
Juraci Paixão Kröhling <juraci@kroehling.de>
2018-08-21 10:38:37 +0200

    Update message ID values for TracingExtensionLogger
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- microprofile/opentracing-extension/src/main/java/org/wildfly/extension/microprofile/opentracing/TracingExtensionLogger.java
+++ microprofile/opentracing-extension/src/main/java/org/wildfly/extension/microprofile/opentracing/TracingExtensionLogger.java
@@ -44,14 +44,14 @@ public interface TracingExtensionLogger extends BasicLogger {
     void processingDeployment();
 
     @LogMessage(level = DEBUG)
-    @Message(id = 4, value = "The deployment does not have CDI enabled. Skipping MicroProfile OpenTracing integration.")
+    @Message(id = 3, value = "The deployment does not have CDI enabled. Skipping MicroProfile OpenTracing integration.")
     void noCdiDeployment();
 
     @LogMessage(level = DEBUG)
-    @Message(id = 6, value = "Deriving service name based on the deployment unit's name: %s")
+    @Message(id = 4, value = "Deriving service name based on the deployment unit's name: %s")
     void serviceNameDerivedFromDeploymentUnit(String serviceName);
 
     @LogMessage(level = DEBUG)
-    @Message(id = 7, value = "Registering the TracerInitializer filter")
+    @Message(id = 5, value = "Registering the TracerInitializer filter")
     void registeringTracerInitializer();
 }
