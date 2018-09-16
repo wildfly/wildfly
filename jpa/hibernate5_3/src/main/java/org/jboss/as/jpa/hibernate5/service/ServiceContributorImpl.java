0d151a1a50f39c62fe76ab18275ed5d6f5555c67
Scott Marlow <smarlow@redhat.com>
2018-09-16 06:59:10 -0400

    WFLY-11010 Document 2lc/JTA integration properties and add better detection of ehcache/JTAFactoryClass use

--- jpa/hibernate5_3/src/main/java/org/jboss/as/jpa/hibernate5/service/ServiceContributorImpl.java
+++ jpa/hibernate5_3/src/main/java/org/jboss/as/jpa/hibernate5/service/ServiceContributorImpl.java
@@ -16,6 +16,8 @@
  */
 package org.jboss.as.jpa.hibernate5.service;
 
+import static org.jboss.as.jpa.hibernate5.JpaLogger.JPA_LOGGER;
+
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.spi.ServiceContributor;
 
@@ -28,23 +30,36 @@ import org.hibernate.service.spi.ServiceContributor;
 public class ServiceContributorImpl implements ServiceContributor {
     private static final String CONTROLJTAINTEGRATION = "wildfly.jpa.jtaplatform"; // these properties are documented in org.jboss.as.jpa.config.Configuration
     private static final String CONTROL2LCINTEGRATION = "wildfly.jpa.regionfactory";
+    private static final String TRANSACTION_PLATFORM = "hibernate.transaction.jta.platform";
+    private static final String EHCACHE = "ehcache";
+    private static final String HIBERNATE_REGION_FACTORY_CLASS = "hibernate.cache.region.factory_class";
 
     @Override
     public void contribute(StandardServiceRegistryBuilder serviceRegistryBuilder) {
         // note that the following deprecated getSettings() is agreed to be replaced with method that returns immutable copy of configuration settings.
         final Object jtaPlatformInitiatorEnabled = serviceRegistryBuilder.getSettings().getOrDefault(CONTROLJTAINTEGRATION, true);
 
-        if (jtaPlatformInitiatorEnabled == null ||
+        if (serviceRegistryBuilder.getSettings().get(TRANSACTION_PLATFORM) != null) {
+            // applications that already specify the transaction platform property which will override the WildFlyCustomJtaPlatform.
+            JPA_LOGGER.tracef("ServiceContributorImpl#contribute application configured the JTA Platform to be used instead of WildFlyCustomJtaPlatform (%s=%s)",
+                    TRANSACTION_PLATFORM, serviceRegistryBuilder.getSettings().get(TRANSACTION_PLATFORM));
+        } else if (jtaPlatformInitiatorEnabled == null ||
                 (jtaPlatformInitiatorEnabled instanceof Boolean && ((Boolean) jtaPlatformInitiatorEnabled).booleanValue()) ||
                 Boolean.parseBoolean(jtaPlatformInitiatorEnabled.toString())) {
+            // use WildFlyCustomJtaPlatform unless they explicitly set wildfly.jpa.jtaplatform to false.
+            JPA_LOGGER.tracef("ServiceContributorImpl#contribute application will use WildFlyCustomJtaPlatform");
             serviceRegistryBuilder.addInitiator(new WildFlyCustomJtaPlatformInitiator());
         }
 
         final Object regionFactoryInitiatorEnabled = serviceRegistryBuilder.getSettings().getOrDefault(CONTROL2LCINTEGRATION, true);
-
-        if (regionFactoryInitiatorEnabled == null ||
+        final Object regionFactory = serviceRegistryBuilder.getSettings().get(HIBERNATE_REGION_FACTORY_CLASS);
+        if ((regionFactory instanceof String) && (((String) regionFactory)).contains(EHCACHE)) {
+            JPA_LOGGER.tracef("ServiceContributorImpl#contribute application is using Ehcache via regionFactory=%s",
+                    regionFactory);
+        } else if (regionFactoryInitiatorEnabled == null ||
                 (regionFactoryInitiatorEnabled instanceof Boolean && ((Boolean) regionFactoryInitiatorEnabled).booleanValue()) ||
                 Boolean.parseBoolean(regionFactoryInitiatorEnabled.toString())) {
+            JPA_LOGGER.tracef("ServiceContributorImpl#contribute adding ORM initiator for 2lc region factory");
             serviceRegistryBuilder.addInitiator(new WildFlyCustomRegionFactoryInitiator());
         }
     }
