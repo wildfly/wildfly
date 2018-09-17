ab7b3112d8e429da5a31ae239853fc34d9178ed3
Ondra Chaloupka <ochaloup@redhat.com>
2018-09-17 14:35:06 +0200

    [JBTM-1107] compensation code is dependent on XTS code it needs to be activated in XTS subsystem

--- xts/src/main/java/org/jboss/as/compensations/CompensationsDependenciesDeploymentProcessor.java
+++ xts/src/main/java/org/jboss/as/compensations/CompensationsDependenciesDeploymentProcessor.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.txn.subsystem;
+package org.jboss.as.compensations;
 
 import org.jboss.as.server.deployment.Attachments;
 import org.jboss.as.server.deployment.DeploymentPhaseContext;
