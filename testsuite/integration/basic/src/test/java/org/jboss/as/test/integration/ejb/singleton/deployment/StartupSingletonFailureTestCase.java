4724122504a1ab986110486b8a6be8414ff17340
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2018-10-03 11:08:58 +0100

    [WFLY-10879] Blocking access to all EJB components if one of Startup beans failed

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/singleton/deployment/StartupSingletonFailureTestCase.java
@@ -0,0 +1,108 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2018, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.test.integration.ejb.singleton.deployment;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
+import org.jboss.as.test.integration.management.cli.DeployURLTestCase;
+import org.jboss.as.test.shared.TestSuiteEnvironment;
+import org.jboss.ejb.client.EJBClient;
+import org.jboss.ejb.client.StatelessEJBLocator;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.impl.base.exporter.zip.ZipExporterImpl;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.NoSuchEJBException;
+import java.io.File;
+
+/**
+ * @author Bartosz Spyrko-Smietanko
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class StartupSingletonFailureTestCase extends AbstractCliTestBase {
+
+    public static EnterpriseArchive deployment() {
+        EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, "TestEar.ear");
+
+        JavaArchive ejb1 = ShrinkWrap.create(JavaArchive.class, "ejb1.jar");
+        ejb1.addClass(SingletonOne.class);
+        JavaArchive ejb2 = ShrinkWrap.create(JavaArchive.class, "ejb2.jar");
+        ejb2.addClass(HelloBean.class);
+        ejb2.addClass(HelloRemote.class);
+
+        ear.addAsModule(ejb1);
+        ear.addAsModule(ejb2);
+
+        return ear;
+    }
+
+    // dummy deployment to keep Arquillian happy - not used in test
+    @Deployment
+    public static Archive<?> getDeployment() {
+        JavaArchive ja = ShrinkWrap.create(JavaArchive.class, "dummy.jar");
+        ja.addClass(DeployURLTestCase.class);
+        return ja;
+    }
+
+    private static File earFile;
+
+    @BeforeClass
+    public static void before() throws Exception {
+        EnterpriseArchive ear = deployment();
+        String tempDir = TestSuiteEnvironment.getTmpDir();
+        earFile = new File(tempDir + File.separator + "TestEar.ear");
+        new ZipExporterImpl(ear).exportTo(earFile, true);
+
+        AbstractCliTestBase.initCLI();
+    }
+
+    @AfterClass
+    public static void after() throws Exception {
+        earFile.delete();
+        AbstractCliTestBase.closeCLI();
+    }
+
+    @Test
+    @SuppressWarnings({"unchecked"})
+    public void testEjbInAnotherModuleShouldFail() throws Exception {
+        try {
+            cli.sendLine("deploy --url=" + earFile.toURI().toURL().toExternalForm() + " --name=" + earFile.getName() + " --headers={rollback-on-runtime-failure=false}");
+
+            final StatelessEJBLocator<HelloRemote> locator = new StatelessEJBLocator(HelloRemote.class, "TestEar", "ejb2", HelloBean.class.getSimpleName(), "");
+            final HelloRemote proxy = EJBClient.createProxy(locator);
+            Assert.assertNotNull("Received a null proxy", proxy);
+            try {
+                proxy.hello();
+                Assert.fail("Call should have failed");
+            } catch (NoSuchEJBException e) {
+                Assert.assertTrue(e.getMessage(), e.getMessage().startsWith("EJBCLIENT000079"));
+            }
+        } finally {
+            cli.sendLine("undeploy " + earFile.getName());
+        }
+    }
+}
