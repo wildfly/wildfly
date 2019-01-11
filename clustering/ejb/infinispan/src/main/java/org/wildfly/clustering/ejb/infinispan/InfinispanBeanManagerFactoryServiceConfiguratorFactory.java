a5d3bc31007ab431577308c669e389a8fba09a56
Paul Ferraro <paul.ferraro@redhat.com>
2019-01-11 15:08:08 -0500

    Fix array overallocation.

--- clustering/ejb/infinispan/src/main/java/org/wildfly/clustering/ejb/infinispan/InfinispanBeanManagerFactoryServiceConfiguratorFactory.java
+++ clustering/ejb/infinispan/src/main/java/org/wildfly/clustering/ejb/infinispan/InfinispanBeanManagerFactoryServiceConfiguratorFactory.java
@@ -116,7 +116,7 @@ public class InfinispanBeanManagerFactoryServiceConfiguratorFactory<I> implement
             }
         };
 
-        List<CapabilityServiceConfigurator> builders = new ArrayList<>(4);
+        List<CapabilityServiceConfigurator> builders = new ArrayList<>(3);
         builders.add(new TemplateConfigurationServiceConfigurator(ServiceName.parse(InfinispanCacheRequirement.CONFIGURATION.resolve(containerName, cacheName)), containerName, cacheName, templateCacheName, configurator));
         builders.add(new CacheServiceConfigurator<>(ServiceName.parse(InfinispanCacheRequirement.CACHE.resolve(containerName, cacheName)), containerName, cacheName).require(new ServiceDependency(name.append("marshalling"))));
         builders.add(new ServiceConfiguratorAdapter(new RemoveOnCancelScheduledExecutorServiceConfigurator(name.append(this.name, "expiration"), EXPIRATION_THREAD_FACTORY)));
