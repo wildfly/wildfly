50da2e2ea10dae1d386049953229ea8522dc33cd
Darran Lofthouse <darran.lofthouse@jboss.com>
2019-02-21 10:31:48 +0000

    [WFLY-11754] Remove camel case from package names in xts testsuite.

--- testsuite/integration/xts/src/test/java/org/jboss/as/test/xts/annotation/compensationscoped/CompensationScopedTestCase.java
+++ testsuite/integration/xts/src/test/java/org/jboss/as/test/xts/annotation/compensationscoped/CompensationScopedTestCase.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.xts.annotation.compensationScoped;
+package org.jboss.as.test.xts.annotation.compensationscoped;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
