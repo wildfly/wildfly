c844d920a2f865f604fe4f8899043ce7b74ca302
James Perkins <jperkins@redhat.com>
2019-03-06 11:22:29 -0800

    [WFLY-11816] Bump the Undertow subsystem model version and schema to version 9.

---
+++ undertow/src/test/java/org/wildfly/extension/undertow/UndertowSubsystem80TestCase.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.extension.undertow;
+
+import java.io.IOException;
+
+import org.jboss.as.subsystem.test.KernelServices;
+import org.jboss.as.subsystem.test.KernelServicesBuilder;
+import org.junit.Test;
+
+/**
+ * This is the barebone test example that tests subsystem
+ *
+ * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a>
+ */
+public class UndertowSubsystem80TestCase extends AbstractUndertowSubsystemTestCase {
+
+    private final String virtualHostName = "some-server";
+    private final int flag = 1;
+
+    @Override
+    protected String getSubsystemXml() throws IOException {
+        return readResource("undertow-8.0.xml");
+    }
+
+    @Override
+    protected String getSubsystemXsdPath() throws Exception {
+        return "schema/wildfly-undertow_8_0.xsd";
+    }
+
+    protected KernelServices standardSubsystemTest(String configId, boolean compareXml) throws Exception {
+        return super.standardSubsystemTest(configId, false);
+    }
+
+    @Test
+    public void testRuntime() throws Exception {
+        setProperty();
+        KernelServicesBuilder builder = createKernelServicesBuilder(RUNTIME).setSubsystemXml(getSubsystemXml());
+        KernelServices mainServices = builder.build();
+        testRuntime(mainServices, virtualHostName, flag);
+        testRuntimeOther(mainServices);
+        testRuntimeLast(mainServices);
+    }
+}
