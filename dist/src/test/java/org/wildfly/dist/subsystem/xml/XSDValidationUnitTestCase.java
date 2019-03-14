500f5a3639a9349515a4d4f1f22d4fbba21f3fa0
Jean-Francois Denise <jfdenise@gmail.com>
2019-03-14 15:20:50 +0100

    Fix for WFLY-11955, Full and servlet Galleon feature-packs pom.xml should define deps as provided Initial fix for WFLY-12043, Upgrade to galleon 4.0 beta1

--- dist/src/test/java/org/wildfly/dist/subsystem/xml/XSDValidationUnitTestCase.java
+++ dist/src/test/java/org/wildfly/dist/subsystem/xml/XSDValidationUnitTestCase.java
@@ -25,7 +25,17 @@ package org.wildfly.dist.subsystem.xml;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -49,8 +59,20 @@ import org.w3c.dom.Document;
 public class XSDValidationUnitTestCase extends AbstractValidationUnitTest {
     @Test
     public void testJBossXsds() throws Exception {
-        for (File xsdFile : jbossSchemaFiles(false))
-            validateXsd(xsdFile);
+        /* The test requires all modules to be loadable. Build a classloader
+           with all modules.
+        */
+        URLClassLoader cl = initClassLoader();
+        ClassLoader current = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(cl);
+            for (File xsdFile : jbossSchemaFiles(false)) {
+                validateXsd(xsdFile);
+            }
+        } finally {
+            Thread.currentThread().setContextClassLoader(current);
+            cl.close();
+        }
     }
 
     private void validateXsd(final File xsdFile) throws Exception {
@@ -69,9 +91,29 @@ public class XSDValidationUnitTestCase extends AbstractValidationUnitTest {
     }
 
     private URL resource(final String name) {
-        final ClassLoader classLoader = getClass().getClassLoader();
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         final URL resource = classLoader.getResource(name);
         assertNotNull("Can't locate resource " + name + " on " + classLoader, resource);
         return resource;
+    }
+
+    private URLClassLoader initClassLoader() throws IOException {
+        String path = System.getProperty("jboss.actual.dist");
+        Path modules = Paths.get(path, "modules/system/layers/base");
+        List<URL> urls = new ArrayList<>();
+        Files.walkFileTree(modules, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                    throws IOException {
+                if (file.toString().endsWith(".jar")) {
+                    urls.add(file.toFile().toURI().toURL());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        URL[] arr = new URL[urls.size()];
+        urls.toArray(arr);
+        URLClassLoader loader = new URLClassLoader(arr);
+        return loader;
     }
 }
