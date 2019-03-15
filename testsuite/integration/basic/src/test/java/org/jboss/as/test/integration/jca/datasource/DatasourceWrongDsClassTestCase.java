f000cc59850089e97414c35dfbd784d671c0d5b1
Lin Gao <aoingl@gmail.com>
2019-03-15 10:27:36 +0800

    [WFLY-11352] WildFly registers multiple distinct drivers for current MySQL driver jar - rework
    The proposed fix is trying to start an extra DriverService with DeploymentUnit name as the driver-name for the first one as well if there are more than one Driver classes defined in META-INF/services/java.sql.Driver

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/datasource/DatasourceWrongDsClassTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jca/datasource/DatasourceWrongDsClassTestCase.java
@@ -35,6 +35,8 @@ import java.util.List;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.controller.client.helpers.Operations;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.test.integration.jca.JcaMgmtBase;
 import org.jboss.as.test.integration.management.util.MgmtOperationException;
 import org.jboss.as.test.integration.management.util.ModelUtil;
@@ -59,9 +61,116 @@ public class DatasourceWrongDsClassTestCase extends JcaMgmtBase {
     @Deployment(name = DEPLOYMENT)
     public static JavaArchive jdbcArchive() throws Exception {
         JavaArchive ja = ShrinkWrap.create(JavaArchive.class, DEPLOYMENT + ".jar");
-        ja.addClasses(DummyDataSource.class, DummyXADataSource.class, TestDriver.class);
-        ja.addAsServiceProviderAndClasses(Driver.class, TestDriver.class);
+        ja.addClasses(DummyDataSource.class, DummyXADataSource.class, TestDriver.class, TestDriver2.class);
+        ja.addAsServiceProviderAndClasses(Driver.class, TestDriver.class, TestDriver2.class);
         return ja;
+    }
+
+    @Test
+    public void testJdbcDrivers() throws Exception {
+        String driverName = DEPLOYMENT + ".jar";
+        ModelNode address = new ModelNode().add(SUBSYSTEM, "datasources");
+        ModelNode operation = new ModelNode();
+        operation.get(OP).set("get-installed-driver");
+        operation.get(OP_ADDR).set(address);
+        operation.get("driver-name").set(driverName);
+
+        ModelNode result = executeOperation(operation).asList().get(0);
+        Assert.assertEquals(driverName, result.get("driver-name").asString());
+        Assert.assertEquals(driverName, result.get("deployment-name").asString());
+        Assert.assertEquals(TestDriver.class.getName(), result.get("driver-class-name").asString());
+        Assert.assertEquals(1, result.get("driver-major-version").asInt());
+        Assert.assertEquals(0, result.get("driver-minor-version").asInt());
+        Assert.assertFalse(result.get("jdbc-compliant").asBoolean());
+
+        String driver1FQCN = driverName + "_" + TestDriver.class.getName() + "_1_0";
+        operation.get("driver-name").set(driver1FQCN);
+        result = executeOperation(operation).asList().get(0);
+        Assert.assertEquals(driver1FQCN, result.get("driver-name").asString());
+        Assert.assertEquals(driver1FQCN, result.get("deployment-name").asString());
+        Assert.assertEquals(TestDriver.class.getName(), result.get("driver-class-name").asString());
+        Assert.assertEquals(1, result.get("driver-major-version").asInt());
+        Assert.assertEquals(0, result.get("driver-minor-version").asInt());
+        Assert.assertFalse(result.get("jdbc-compliant").asBoolean());
+
+        String driver2FQCN = driverName + "_" + TestDriver2.class.getName() + "_1_1";
+        operation.get("driver-name").set(driver2FQCN);
+        result = executeOperation(operation).asList().get(0);
+        Assert.assertEquals(driver2FQCN, result.get("driver-name").asString());
+        Assert.assertEquals(driver2FQCN, result.get("deployment-name").asString());
+        Assert.assertEquals(TestDriver2.class.getName(), result.get("driver-class-name").asString());
+        Assert.assertEquals(1, result.get("driver-major-version").asInt());
+        Assert.assertEquals(1, result.get("driver-minor-version").asInt());
+        Assert.assertTrue(result.get("jdbc-compliant").asBoolean());
+
+    }
+
+    private ModelNode getDatasourceAddress(Datasource datasource) {
+        ModelNode address = new ModelNode()
+            .add(SUBSYSTEM, "datasources")
+            .add("data-source", datasource.getName());
+        address.protect();
+        return address;
+    }
+
+    private ModelNode getAddDatasourceOperation(Datasource datasource) {
+        ModelNode operation = new ModelNode();
+        operation.get(OP).set(ADD);
+        operation.get(OP_ADDR).set(getDatasourceAddress(datasource));
+        operation.get("jndi-name").set(datasource.getJndiName());
+        operation.get("driver-name").set(datasource.getDriverName());
+        operation.get("enabled").set(datasource.getEnabled());
+        operation.get("connection-url").set(datasource.getConnectionUrl());
+        return operation;
+    }
+
+    private ModelNode getRemoveDatasourceOperation(Datasource ds) {
+        ModelNode removeOperation = Operations.createRemoveOperation(getDatasourceAddress(ds));
+        removeOperation.get(ModelDescriptionConstants.OPERATION_HEADERS).get("allow-resource-service-restart")
+                .set(true);
+        return removeOperation;
+    }
+
+    @Test
+    public void testDSWithMutipleDrivers() throws Exception {
+        String driverName = DEPLOYMENT + ".jar";
+        String driver1FQCN = driverName + "_" + TestDriver.class.getName() + "_1_0";
+        String driver2FQCN = driverName + "_" + TestDriver2.class.getName() + "_1_1";
+
+        Datasource ds1 = Datasource.Builder("test-ds1").connectionUrl("foo").driverName(driverName).enabled(true)
+            .jndiName("java:jboss/datasources/test-ds1")
+            .build();
+        ModelNode addDS1Operation = getAddDatasourceOperation(ds1);
+        try {
+            ModelNode addDSResult = getManagementClient().getControllerClient().execute(addDS1Operation);
+            Assert.assertEquals("success", addDSResult.get("outcome").asString());
+        } finally {
+            ModelNode removeDSOperation = getRemoveDatasourceOperation(ds1);
+            executeOperation(removeDSOperation);
+        }
+
+        Datasource ds1FQCN = Datasource.Builder("test-ds1FQCN").connectionUrl("foo").driverName(driver1FQCN).enabled(true)
+                .jndiName("java:jboss/datasources/test-ds1FQCN").build();
+        ModelNode addDS1FQCNOperation = getAddDatasourceOperation(ds1FQCN);
+        try {
+            ModelNode addDSResult = getManagementClient().getControllerClient().execute(addDS1FQCNOperation);
+            Assert.assertEquals("success", addDSResult.get("outcome").asString());
+        } finally {
+            ModelNode removeDSOperation = getRemoveDatasourceOperation(ds1FQCN);
+            executeOperation(removeDSOperation);
+        }
+
+        Datasource ds2FQCN = Datasource.Builder("test-ds2FQCN").connectionUrl("foo").driverName(driver2FQCN)
+                .enabled(true).jndiName("java:jboss/datasources/test-ds2FQCN").build();
+        ModelNode addDS2FQCNOperation = getAddDatasourceOperation(ds2FQCN);
+        try {
+            ModelNode addDSResult = getManagementClient().getControllerClient().execute(addDS2FQCNOperation);
+            Assert.assertEquals("success", addDSResult.get("outcome").asString());
+        } finally {
+            ModelNode removeDSOperation = getRemoveDatasourceOperation(ds2FQCN);
+            executeOperation(removeDSOperation);
+        }
+
     }
 
     @Test
