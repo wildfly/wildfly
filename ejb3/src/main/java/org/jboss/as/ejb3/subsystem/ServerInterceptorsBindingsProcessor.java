8b4505054182127c8c11918e4a12580e5f1f38ed
Tomasz Adamski <tadamski@redhat.com>
2019-05-23 21:33:32 +0200

    [WFLY-6143] Server interceptors: load,index once and lazily

--- ejb3/src/main/java/org/jboss/as/ejb3/subsystem/ServerInterceptorsBindingsProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/subsystem/ServerInterceptorsBindingsProcessor.java
@@ -24,26 +24,23 @@ package org.jboss.as.ejb3.subsystem;
 import org.jboss.as.ee.component.Attachments;
 import org.jboss.as.ee.component.ComponentDescription;
 import org.jboss.as.ee.component.EEModuleDescription;
-import org.jboss.as.ee.component.InterceptorDescription;
 import org.jboss.as.ejb3.component.EJBComponentDescription;
+import org.jboss.as.ejb3.interceptor.server.ServerInterceptorCache;
 import org.jboss.as.server.deployment.DeploymentPhaseContext;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.as.server.deployment.DeploymentUnitProcessor;
-
-import java.util.Collection;
 
 /**
  * @author <a href="mailto:tadamski@redhat.com">Tomasz Adamski</a>
  */
 public class ServerInterceptorsBindingsProcessor implements DeploymentUnitProcessor {
 
-    private final Collection<String> interceptorClasses;
+    private final ServerInterceptorCache serverInterceptorCache;
 
-    ServerInterceptorsBindingsProcessor(final Collection<String> interceptorClasses){
-        this.interceptorClasses = interceptorClasses;
+    public ServerInterceptorsBindingsProcessor(final ServerInterceptorCache serverInterceptorCache){
+        this.serverInterceptorCache = serverInterceptorCache;
     }
-
 
     @Override
     public void deploy(final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
@@ -54,9 +51,7 @@ public class ServerInterceptorsBindingsProcessor implements DeploymentUnitProces
                 continue;
             }
             final EJBComponentDescription ejbComponentDescription = (EJBComponentDescription) componentDescription;
-            for(final String interceptorClass : interceptorClasses) {
-                ejbComponentDescription.getServerInterceptors().add(new InterceptorDescription(interceptorClass));
-            }
+            ejbComponentDescription.setServerInterceptorCache(serverInterceptorCache);
         }
     }
 
