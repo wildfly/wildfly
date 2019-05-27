4ebaec0bffdd366a2735658e9026ef2ad20d9e2d
tmiyar <tmiyargi@redhat.com>
2019-05-27 11:20:44 +0200

    [WFLY-12101] Test for EJBCLIENT-335, display host:port on connecting to server error

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/exception/ExceptionEjbClientTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/exception/ExceptionEjbClientTestCase.java
@@ -22,15 +22,22 @@
 
 package org.jboss.as.test.integration.ejb.stateful.exception;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
+import org.codehaus.plexus.util.ExceptionUtils;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.test.shared.TestSuiteEnvironment;
+import org.jboss.ejb.client.RequestSendFailedException;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.naming.client.WildFlyInitialContextFactory;
 
@@ -66,4 +73,25 @@ public class ExceptionEjbClientTestCase extends ExceptionTestCase {
     protected DestroyMarkerBeanInterface getMarker() throws NamingException {
         return lookup(DestroyMarkerBean.class, DestroyMarkerBeanInterface.class, false);
     }
+
+    /**
+     * Test exception contains destination when there is no server running (wrong server)
+     */
+    @Test
+    public void testConnectException() throws Exception {
+        try {
+            Properties props = new Properties();
+            props.put(Context.INITIAL_CONTEXT_FACTORY,  "org.wildfly.naming.client.WildFlyInitialContextFactory");
+            //Wrong server so an exception will be thrown
+            props.put(Context.PROVIDER_URL, "remote+http://localhost:1000");
+            Context ctx = new InitialContext(props);
+
+            DestroyMarkerBeanInterface destroyM = (DestroyMarkerBeanInterface) ctx.lookup(String.format("ejb:/%s/%s!%s", ARCHIVE_NAME, DestroyMarkerBean.class.getSimpleName(), DestroyMarkerBeanInterface.class.getName()));
+            destroyM.is();
+            Assert.fail("It was expected a RequestSendFailedException being thrown");
+        } catch (RequestSendFailedException e) {
+            assertTrue("Destination should be displayed", ExceptionUtils.getFullStackTrace(e).contains("remote+http://localhost:1000"));
+        }
+    }
+
 }
