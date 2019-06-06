9a5a1d3224f8dd0b1a26a9a393057c15851c7866
Radoslav Husar <radosoft@gmail.com>
2019-06-06 12:21:15 +0200

    WFLY-12173 Bump the Undertow subsystem model version and schema to version 10

--- undertow/src/test/java/org/wildfly/extension/undertow/UndertowSubsystem20TestCase.java
+++ undertow/src/test/java/org/wildfly/extension/undertow/UndertowSubsystem20TestCase.java
@@ -67,7 +67,7 @@ public class UndertowSubsystem20TestCase extends AbstractUndertowSubsystemTestCa
     @Test
     public void testRuntime() throws Exception {
         setProperty();
-        KernelServicesBuilder builder = createKernelServicesBuilder(UndertowSubsystemTestCase.RUNTIME)
+        KernelServicesBuilder builder = createKernelServicesBuilder(AbstractUndertowSubsystemTestCase.RUNTIME)
                 .setSubsystemXml(getSubsystemXml());
         KernelServices mainServices = builder.build();
         testRuntime(mainServices, virtualHostName, flag);
@@ -77,7 +77,7 @@ public class UndertowSubsystem20TestCase extends AbstractUndertowSubsystemTestCa
 
     @Override
     protected AdditionalInitialization createAdditionalInitialization() {
-        return UndertowSubsystemTestCase.DEFAULT;
+        return AbstractUndertowSubsystemTestCase.DEFAULT;
     }
 
 }
