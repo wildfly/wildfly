b237364d670a1709297e4816241a8c483e971011
Flavia Rainone <frainone@redhat.com>
2019-06-17 10:55:26 -0300

    [WFLY-11759] Revert UndertowTransformersTestCase workaround
    KernelServicesBuilder.excludeResourceFromParent API is no longer needed, the test runs okay

--- undertow/src/test/java/org/wildfly/extension/undertow/UndertowTransformersTestCase.java
+++ undertow/src/test/java/org/wildfly/extension/undertow/UndertowTransformersTestCase.java
@@ -213,17 +213,6 @@ public class UndertowTransformersTestCase extends AbstractSubsystemTest {
 
         List<ModelNode> ops = builder.parseXmlResource("undertow-reject.xml");
 
-        if (controllerVersion == ModelTestControllerVersion.EAP_7_1_0) {
-            // The EAP 7.1 tests load a legacy version of undertow in the child first classloader
-            // But the current version is in the parent. PredicateValidator's use of PredicateParser
-            // fails in this situation as it results in ServiceLoader trying to load classes from both
-            // jars. So just disable testing of params that involve PredicateValidator.
-            for (ModelNode op : ops) {
-                op.remove(Constants.PREDICATE);
-                op.remove(Constants.MANAGEMENT_ACCESS_PREDICATE);
-            }
-        }
-
         ModelTestUtils.checkFailedTransformedBootOperations(mainServices, targetVersion, ops, config);
     }
 
