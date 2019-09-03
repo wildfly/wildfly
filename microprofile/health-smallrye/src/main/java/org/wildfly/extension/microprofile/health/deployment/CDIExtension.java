4392b576cafd2a2ef338438f139b58f18325d36b
Jeff Mesnil <jmesnil@gmail.com>
2019-09-03 17:08:48 +0200

    [WFLY-12557] Fix CDI injection of health check probes
    * Use BeanManager select() to get references on injected beans *managed*
      by CDI (instead of creating manually unmanaged references
    * add missing javax.entreprise.api to
      org.eclipse.microprofile.health.api JBoss module
    * remove microprofile-tck/health/tck-suite.xml file added to exclude CDI
      producer test now that all tests pass again
    
    JIRA: https://issues.jboss.org/browse/WFLY-12557

--- microprofile/health-smallrye/src/main/java/org/wildfly/extension/microprofile/health/deployment/CDIExtension.java
+++ microprofile/health-smallrye/src/main/java/org/wildfly/extension/microprofile/health/deployment/CDIExtension.java
@@ -23,18 +23,17 @@
 package org.wildfly.extension.microprofile.health.deployment;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
 
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.spi.AfterDeploymentValidation;
-import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.BeforeShutdown;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
-import javax.enterprise.inject.spi.Unmanaged;
+import javax.enterprise.util.AnnotationLiteral;
 
 import io.smallrye.health.SmallRyeHealthReporter;
 import org.eclipse.microprofile.health.Health;
@@ -42,7 +41,6 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.Liveness;
 import org.eclipse.microprofile.health.Readiness;
 import org.wildfly.extension.microprofile.health.HealthReporter;
-import org.wildfly.extension.microprofile.health._private.MicroProfileHealthLogger;
 
 /**
  * @author <a href="http://jmesnil.net/">Jeff Mesnil</a> (c) 2017 Red Hat inc.
@@ -51,14 +49,33 @@ public class CDIExtension implements Extension {
 
     private final HealthReporter reporter;
 
-    private List<AnnotatedType<? extends HealthCheck>> healthDelegates = new ArrayList<>();
-    private Collection<Unmanaged.UnmanagedInstance<HealthCheck>> healthInstances = new ArrayList<>();
+    static final class HealthLiteral extends AnnotationLiteral<Health> implements Health {
 
-    private List<AnnotatedType<? extends HealthCheck>> livenessDelegates = new ArrayList<>();
-    private Collection<Unmanaged.UnmanagedInstance<HealthCheck>> livenessInstances = new ArrayList<>();
+        static final HealthLiteral INSTANCE = new HealthLiteral();
 
-    private List<AnnotatedType<? extends HealthCheck>> readinessDelegates = new ArrayList<>();
-    private Collection<Unmanaged.UnmanagedInstance<HealthCheck>> readinessInstances = new ArrayList<>();
+        private static final long serialVersionUID = 1L;
+
+    }
+    static final class LivenessLiteral extends AnnotationLiteral<Liveness> implements Liveness {
+
+        static final LivenessLiteral INSTANCE = new LivenessLiteral();
+
+        private static final long serialVersionUID = 1L;
+
+    }
+    static final class ReadinessLiteral extends AnnotationLiteral<Readiness> implements Readiness {
+
+        static final ReadinessLiteral INSTANCE = new ReadinessLiteral();
+
+        private static final long serialVersionUID = 1L;
+
+    }
+
+    // Use a single CDI instance to select and destroy all HealthCheck probes instances
+    private Instance<Object> instance;
+    private final List<HealthCheck> healthChecks = new ArrayList<>();
+    private final List<HealthCheck> livenessChecks = new ArrayList<>();
+    private final List<HealthCheck> readinessChecks = new ArrayList<>();
 
 
     public CDIExtension(HealthReporter healthReporter) {
@@ -66,69 +83,48 @@ public class CDIExtension implements Extension {
     }
 
     /**
-     * Discover all classes that implements HealthCheckProcedure
-     */
-    public void observeResources(@Observes ProcessAnnotatedType<? extends HealthCheck> event) {
-        Class<? extends HealthCheck> javaClass = event.getAnnotatedType().getJavaClass();
-        if (event.getAnnotatedType().isAnnotationPresent(Health.class)) {
-            MicroProfileHealthLogger.LOGGER.debugf("Discovered health check procedure %s", javaClass);
-            healthDelegates.add(event.getAnnotatedType());
-        } else if (event.getAnnotatedType().isAnnotationPresent(Liveness.class)) {
-            MicroProfileHealthLogger.LOGGER.debugf("Discovered liveness check procedure %s", javaClass);
-            livenessDelegates.add(event.getAnnotatedType());
-        } else if (event.getAnnotatedType().isAnnotationPresent(Readiness.class)) {
-            MicroProfileHealthLogger.LOGGER.debugf("Discovered liveness check procedure %s", javaClass);
-            readinessDelegates.add(event.getAnnotatedType());
-        }
-    }
-
-
-    /**
-     * Instantiates <em>unmanaged instances</em> of HealthCheck and
-     * handle manually their CDI creation lifecycle.
-     * Add them to the {@link HealthReporter}.
+     * Get CDI <em>instances</em> of HealthCheck and
+     * add them to the {@link HealthReporter}.
      */
     private void afterDeploymentValidation(@Observes final AfterDeploymentValidation avd, BeanManager bm) {
-        addHealthCheck(bm, healthDelegates, healthInstances, reporter::addHealthCheck);
-        addHealthCheck(bm, readinessDelegates, readinessInstances, reporter::addReadinessCheck);
-        addHealthCheck(bm, livenessDelegates, livenessInstances, reporter::addLivenessCheck);
+        instance = bm.createInstance();
+
+        addHealthChecks(HealthLiteral.INSTANCE, reporter::addHealthCheck, healthChecks);
+        addHealthChecks(LivenessLiteral.INSTANCE, reporter::addLivenessCheck, livenessChecks);
+        addHealthChecks(ReadinessLiteral.INSTANCE, reporter::addReadinessCheck, readinessChecks);
     }
 
-    private void addHealthCheck(BeanManager bm,
-                                List<AnnotatedType<? extends HealthCheck>> delegates,
-                                Collection<Unmanaged.UnmanagedInstance<HealthCheck>> instances,
-                                Consumer<HealthCheck> healthFunction) {
-        for (AnnotatedType delegate : delegates) {
-            try {
-                Unmanaged<HealthCheck> unmanagedHealthCheck = new Unmanaged<HealthCheck>(bm, delegate.getJavaClass());
-                Unmanaged.UnmanagedInstance<HealthCheck> healthCheckInstance = unmanagedHealthCheck.newInstance();
-                HealthCheck healthCheck = healthCheckInstance.produce().inject().postConstruct().get();
-                instances.add(healthCheckInstance);
-                healthFunction.accept(healthCheck);
-            } catch (Exception e) {
-                throw new RuntimeException("Failed to register health bean", e);
-            }
+    private void addHealthChecks(AnnotationLiteral qualifier,
+                                 Consumer<HealthCheck> healthFunction, List<HealthCheck> healthChecks) {
+        for (HealthCheck healthCheck : instance.select(HealthCheck.class, qualifier)) {
+            healthFunction.accept(healthCheck);
+            healthChecks.add(healthCheck);
         }
     }
 
     /**
      * Called when the deployment is undeployed.
      * <p>
-     * Remove all the instances of {@link HealthCheck} from the {@link SmallRyeHealthReporter}.
-     * Handle manually their CDI destroy lifecycle.
+     * Remove all the instances of {@link HealthCheck} from the {@link HealthReporter}.
      */
     public void beforeShutdown(@Observes final BeforeShutdown bs) {
-        removeHealthCheck(healthInstances, reporter::removeHealthCheck);
-        removeHealthCheck(readinessInstances, reporter::removeReadinessCheck);
-        removeHealthCheck(livenessInstances, reporter::removeLivenessCheck);
+        removeHealthCheck(healthChecks, reporter::removeHealthCheck);
+        removeHealthCheck(livenessChecks, reporter::removeLivenessCheck);
+        removeHealthCheck(readinessChecks, reporter::removeReadinessCheck);
+
+        instance = null;
     }
 
-    private void removeHealthCheck(Collection<Unmanaged.UnmanagedInstance<HealthCheck>> instances,
+    private void removeHealthCheck(List<HealthCheck> healthChecks,
                                    Consumer<HealthCheck> healthFunction) {
-        instances.forEach(healthCheck -> {
-            healthFunction.accept(healthCheck.get());
-            healthCheck.preDestroy().dispose();
-        });
-        instances.clear();
+        for (HealthCheck healthCheck : healthChecks) {
+            healthFunction.accept(healthCheck);
+            instance.destroy(healthCheck);
+        }
+        healthChecks.clear();
+    }
+
+    public void vetoSmallryeHealthReporter(@Observes ProcessAnnotatedType<SmallRyeHealthReporter> pat) {
+        pat.veto();
     }
 }
