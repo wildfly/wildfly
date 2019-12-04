fjuma/jboss-as

[WFLY-10339]: Broadcast/discovery-group resources have ambiguous requirement specs.
* Adding 6 new resources to have specific boradcast and discovery groups
using socket-bindings or cluster connections
* Updating the schema for these new resources
* Requires connectors for broadcast-group resource

Jira:
https://issues.jboss.org/browse/WFLY-10339
https://issues.jboss.org/browse/WFLY-6607
https://issues.jboss.org/browse/WFLY-11005

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ActiveMQServerService.java
@@ -211,7 +211,7 @@ class ActiveMQServerService implements Service<ActiveMQServer> {
                     if (commandDispatcherFactories.containsKey(key)) {
                         CommandDispatcherFactory commandDispatcherFactory = commandDispatcherFactories.get(key).get();
                         String clusterName = clusterNames.get(key);
-                        newConfigs.add(BroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, commandDispatcherFactory, clusterName));
+                        newConfigs.add(JGroupBroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, commandDispatcherFactory, clusterName));
                     } else {
                         final Supplier<SocketBinding> bindingSupplier = groupBindings.get(key);
                         if (bindingSupplier == null) {
@@ -219,7 +219,7 @@ class ActiveMQServerService implements Service<ActiveMQServer> {
                         }
                         final SocketBinding binding = bindingSupplier.get();
                         binding.getSocketBindings().getNamedRegistry().registerBinding(ManagedBinding.Factory.createSimpleManagedBinding(binding));
-                        newConfigs.add(BroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, binding));
+                        newConfigs.add(SocketBroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, binding));
                     }
                 }
                 configuration.getBroadcastGroupConfigurations().clear();
@@ -234,13 +234,13 @@ class ActiveMQServerService implements Service<ActiveMQServer> {
                     if (commandDispatcherFactories.containsKey(key)) {
                         CommandDispatcherFactory commandDispatcherFactory = commandDispatcherFactories.get(key).get();
                         String clusterName = clusterNames.get(key);
-                        config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), commandDispatcherFactory, clusterName);
+                        config = JGroupDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), commandDispatcherFactory, clusterName);
                     } else {
                         final Supplier<SocketBinding> binding = groupBindings.get(key);
                         if (binding == null) {
                             throw MessagingLogger.ROOT_LOGGER.failedToFindDiscoverySocketBinding(name);
                         }
-                        config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), binding.get());
+                        config = SocketDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), binding.get());
                         binding.get().getSocketBindings().getNamedRegistry().registerBinding(ManagedBinding.Factory.createSimpleManagedBinding(binding.get()));
                     }
                     configuration.getDiscoveryGroupConfigurations().put(name, config);

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/BroadcastGroupWriteAttributeHandler.java
@@ -22,11 +22,24 @@
 
 package org.wildfly.extension.messaging.activemq;
 
+import static org.wildfly.extension.messaging.activemq.CommonAttributes.CONNECTORS;
+import static org.wildfly.extension.messaging.activemq.MessagingExtension.BROADCAST_GROUP_PATH;
+import static org.wildfly.extension.messaging.activemq.MessagingExtension.JGROUP_BROADCAST_GROUP_PATH;
+import static org.wildfly.extension.messaging.activemq.MessagingExtension.SOCKET_BROADCAST_GROUP_PATH;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
+import org.wildfly.extension.messaging.activemq.logging.MessagingLogger;
 /**
  * Write attribute handler for attributes that update a broadcast group resource.
  *
@@ -34,19 +47,54 @@ import org.jboss.dmr.ModelNode;
  */
 public class BroadcastGroupWriteAttributeHandler extends ReloadRequiredWriteAttributeHandler {
 
-    public static final BroadcastGroupWriteAttributeHandler INSTANCE = new BroadcastGroupWriteAttributeHandler();
+    public static final BroadcastGroupWriteAttributeHandler INSTANCE = new BroadcastGroupWriteAttributeHandler(BROADCAST_GROUP_PATH, BroadcastGroupDefinition.ATTRIBUTES);
+    public static final BroadcastGroupWriteAttributeHandler JGROUP_INSTANCE = new BroadcastGroupWriteAttributeHandler(JGROUP_BROADCAST_GROUP_PATH, JGroupBroadcastGroupDefinition.ATTRIBUTES);
+    public static final BroadcastGroupWriteAttributeHandler SOCKET_INSTANCE = new BroadcastGroupWriteAttributeHandler(SOCKET_BROADCAST_GROUP_PATH, JGroupBroadcastGroupDefinition.ATTRIBUTES);
 
-    private BroadcastGroupWriteAttributeHandler() {
-        super(BroadcastGroupDefinition.ATTRIBUTES);
+    private final PathElement path;
+
+    private BroadcastGroupWriteAttributeHandler(final PathElement path, final AttributeDefinition... definitions) {
+        super(definitions);
+        this.path = path;
     }
 
     @Override
     protected void finishModelStage(final OperationContext context,final ModelNode operation,final String attributeName,final ModelNode newValue,
             final ModelNode oldValue,final Resource model) throws OperationFailedException {
-            if(attributeName.equals(BroadcastGroupDefinition.CONNECTOR_REFS.getName())){
-                BroadcastGroupDefinition.validateConnectors(context, operation, newValue);
+            if(attributeName.equals(CONNECTORS)){
+                validateConnectors(context, operation, newValue);
             }
         super.finishModelStage(context, operation, attributeName, newValue, oldValue, model);
     }
 
+    void validateConnectors(OperationContext context, ModelNode operation, ModelNode connectorRefs) throws OperationFailedException {
+        final Set<String> availableConnectors =  getAvailableConnectors(context,operation);
+        final List<ModelNode> operationAddress = operation.get(ModelDescriptionConstants.OP_ADDR).asList();
+        final String broadCastGroup = operationAddress.get(operationAddress.size()-1).get(path.getKey()).asString();
+        if(connectorRefs.isDefined()) {
+            for(ModelNode connectorRef:connectorRefs.asList()){
+                final String connectorName = connectorRef.asString();
+                if(!availableConnectors.contains(connectorName)){
+                    throw MessagingLogger.ROOT_LOGGER.wrongConnectorRefInBroadCastGroup(broadCastGroup, connectorName, availableConnectors);
+                }
+            }
+        }
+    }
+
+    // FIXME use capabilities & requirements
+    private static Set<String> getAvailableConnectors(final OperationContext context,final ModelNode operation) throws OperationFailedException{
+        PathAddress address = PathAddress.pathAddress(operation.get(ModelDescriptionConstants.OP_ADDR));
+        PathAddress active = MessagingServices.getActiveMQServerPathAddress(address);
+        Set<String> availableConnectors = new HashSet<>();
+
+        Resource subsystemResource = context.readResourceFromRoot(active.getParent(), false);
+        availableConnectors.addAll(subsystemResource.getChildrenNames(CommonAttributes.REMOTE_CONNECTOR));
+
+        Resource activeMQServerResource = context.readResourceFromRoot(active, false);
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.HTTP_CONNECTOR));
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.IN_VM_CONNECTOR));
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.REMOTE_CONNECTOR));
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.CONNECTOR));
+        return availableConnectors;
+    }
 }

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ServerDefinition.java
@@ -873,8 +873,12 @@ public class ServerDefinition extends PersistentResourceDefinition {
 
         children.add(new BridgeDefinition(registerRuntimeOnly));
         children.add(new BroadcastGroupDefinition(registerRuntimeOnly));
+        children.add(new SocketBroadcastGroupDefinition(registerRuntimeOnly));
+        children.add(new JGroupBroadcastGroupDefinition(registerRuntimeOnly));
         // WFLY-10518 - keep discovery-group resource under server
         children.add(new DiscoveryGroupDefinition(registerRuntimeOnly, false));
+        children.add(new JGroupDiscoveryGroupDefinition(registerRuntimeOnly, false));
+        children.add(new SocketDiscoveryGroupDefinition(registerRuntimeOnly, false));
         children.add(new ClusterConnectionDefinition(registerRuntimeOnly));
         children.add(new QueueDefinition(registerRuntimeOnly, MessagingExtension.QUEUE_PATH));
         children.add(new JMSQueueDefinition(false, registerRuntimeOnly));
@@ -887,7 +891,6 @@ public class ServerDefinition extends PersistentResourceDefinition {
     @Override
     public void registerChildren(ManagementResourceRegistration resourceRegistration) {
         super.registerChildren(resourceRegistration);
-
         // runtime queues and core-address are only registered when it is ok to register runtime resource (ie they are not registered on HC).
         if (registerRuntimeOnly) {
             resourceRegistration.registerSubModel(new QueueDefinition(registerRuntimeOnly,  MessagingExtension.RUNTIME_QUEUE_PATH));

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/jms/ExternalConnectionFactoryService.java
@@ -33,7 +33,8 @@ import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 import org.wildfly.clustering.dispatcher.CommandDispatcherFactory;
-import org.wildfly.extension.messaging.activemq.DiscoveryGroupAdd;
+import org.wildfly.extension.messaging.activemq.JGroupDiscoveryGroupAdd;
+import org.wildfly.extension.messaging.activemq.SocketDiscoveryGroupAdd;
 import org.wildfly.extension.messaging.activemq.TransportConfigOperationHandlers;
 import org.wildfly.extension.messaging.activemq.logging.MessagingLogger;
 
@@ -109,13 +110,13 @@ public class ExternalConnectionFactoryService implements Service<ConnectionFacto
                 if (commandDispatcherFactories.containsKey(key)) {
                     CommandDispatcherFactory commandDispatcherFactory = commandDispatcherFactories.get(key).get();
                     String clusterName = clusterNames.get(key);
-                    config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, commandDispatcherFactory, clusterName);
+                    config = JGroupDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, commandDispatcherFactory, clusterName);
                 } else {
                     final SocketBinding binding = groupBindings.get(key).get();
                     if (binding == null) {
                         throw MessagingLogger.ROOT_LOGGER.failedToFindDiscoverySocketBinding(name);
                     }
-                    config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, binding);
+                    config = SocketDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, binding);
                     binding.getSocketBindings().getNamedRegistry().registerBinding(ManagedBinding.Factory.createSimpleManagedBinding(binding));
                 }
                 if (ha) {

wildfly/wildfly

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ActiveMQServerService.java
@@ -211,7 +211,7 @@ class ActiveMQServerService implements Service<ActiveMQServer> {
                     if (commandDispatcherFactories.containsKey(key)) {
                         CommandDispatcherFactory commandDispatcherFactory = commandDispatcherFactories.get(key).get();
                         String clusterName = clusterNames.get(key);
-                        newConfigs.add(BroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, commandDispatcherFactory, clusterName));
+                        newConfigs.add(JGroupsBroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, commandDispatcherFactory, clusterName));
                     } else {
                         final Supplier<SocketBinding> bindingSupplier = groupBindings.get(key);
                         if (bindingSupplier == null) {
@@ -219,7 +219,7 @@ class ActiveMQServerService implements Service<ActiveMQServer> {
                         }
                         final SocketBinding binding = bindingSupplier.get();
                         binding.getSocketBindings().getNamedRegistry().registerBinding(ManagedBinding.Factory.createSimpleManagedBinding(binding));
-                        newConfigs.add(BroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, binding));
+                        newConfigs.add(SocketBroadcastGroupAdd.createBroadcastGroupConfiguration(name, config, binding));
                     }
                 }
                 configuration.getBroadcastGroupConfigurations().clear();
@@ -234,13 +234,13 @@ class ActiveMQServerService implements Service<ActiveMQServer> {
                     if (commandDispatcherFactories.containsKey(key)) {
                         CommandDispatcherFactory commandDispatcherFactory = commandDispatcherFactories.get(key).get();
                         String clusterName = clusterNames.get(key);
-                        config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), commandDispatcherFactory, clusterName);
+                        config = JGroupsDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), commandDispatcherFactory, clusterName);
                     } else {
                         final Supplier<SocketBinding> binding = groupBindings.get(key);
                         if (binding == null) {
                             throw MessagingLogger.ROOT_LOGGER.failedToFindDiscoverySocketBinding(name);
                         }
-                        config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), binding.get());
+                        config = SocketDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, entry.getValue(), binding.get());
                         binding.get().getSocketBindings().getNamedRegistry().registerBinding(ManagedBinding.Factory.createSimpleManagedBinding(binding.get()));
                     }
                     configuration.getDiscoveryGroupConfigurations().put(name, config);

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/BroadcastGroupWriteAttributeHandler.java
@@ -22,11 +22,24 @@
 
 package org.wildfly.extension.messaging.activemq;
 
+import static org.wildfly.extension.messaging.activemq.CommonAttributes.CONNECTORS;
+import static org.wildfly.extension.messaging.activemq.MessagingExtension.BROADCAST_GROUP_PATH;
+import static org.wildfly.extension.messaging.activemq.MessagingExtension.JGROUPS_BROADCAST_GROUP_PATH;
+import static org.wildfly.extension.messaging.activemq.MessagingExtension.SOCKET_BROADCAST_GROUP_PATH;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.jboss.as.controller.AttributeDefinition;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
+import org.wildfly.extension.messaging.activemq.logging.MessagingLogger;
 /**
  * Write attribute handler for attributes that update a broadcast group resource.
  *
@@ -34,19 +47,54 @@ import org.jboss.dmr.ModelNode;
  */
 public class BroadcastGroupWriteAttributeHandler extends ReloadRequiredWriteAttributeHandler {
 
-    public static final BroadcastGroupWriteAttributeHandler INSTANCE = new BroadcastGroupWriteAttributeHandler();
+    public static final BroadcastGroupWriteAttributeHandler INSTANCE = new BroadcastGroupWriteAttributeHandler(BROADCAST_GROUP_PATH, BroadcastGroupDefinition.ATTRIBUTES);
+    public static final BroadcastGroupWriteAttributeHandler JGROUP_INSTANCE = new BroadcastGroupWriteAttributeHandler(JGROUPS_BROADCAST_GROUP_PATH, JGroupsBroadcastGroupDefinition.ATTRIBUTES);
+    public static final BroadcastGroupWriteAttributeHandler SOCKET_INSTANCE = new BroadcastGroupWriteAttributeHandler(SOCKET_BROADCAST_GROUP_PATH, JGroupsBroadcastGroupDefinition.ATTRIBUTES);
 
-    private BroadcastGroupWriteAttributeHandler() {
-        super(BroadcastGroupDefinition.ATTRIBUTES);
+    private final PathElement path;
+
+    private BroadcastGroupWriteAttributeHandler(final PathElement path, final AttributeDefinition... definitions) {
+        super(definitions);
+        this.path = path;
     }
 
     @Override
     protected void finishModelStage(final OperationContext context,final ModelNode operation,final String attributeName,final ModelNode newValue,
             final ModelNode oldValue,final Resource model) throws OperationFailedException {
-            if(attributeName.equals(BroadcastGroupDefinition.CONNECTOR_REFS.getName())){
-                BroadcastGroupDefinition.validateConnectors(context, operation, newValue);
+            if(attributeName.equals(CONNECTORS)){
+                validateConnectors(context, operation, newValue);
             }
         super.finishModelStage(context, operation, attributeName, newValue, oldValue, model);
     }
 
+    void validateConnectors(OperationContext context, ModelNode operation, ModelNode connectorRefs) throws OperationFailedException {
+        final Set<String> availableConnectors =  getAvailableConnectors(context,operation);
+        final List<ModelNode> operationAddress = operation.get(ModelDescriptionConstants.OP_ADDR).asList();
+        final String broadCastGroup = operationAddress.get(operationAddress.size()-1).get(path.getKey()).asString();
+        if(connectorRefs.isDefined()) {
+            for(ModelNode connectorRef:connectorRefs.asList()){
+                final String connectorName = connectorRef.asString();
+                if(!availableConnectors.contains(connectorName)){
+                    throw MessagingLogger.ROOT_LOGGER.wrongConnectorRefInBroadCastGroup(broadCastGroup, connectorName, availableConnectors);
+                }
+            }
+        }
+    }
+
+    // FIXME use capabilities & requirements
+    private static Set<String> getAvailableConnectors(final OperationContext context,final ModelNode operation) throws OperationFailedException{
+        PathAddress address = PathAddress.pathAddress(operation.get(ModelDescriptionConstants.OP_ADDR));
+        PathAddress active = MessagingServices.getActiveMQServerPathAddress(address);
+        Set<String> availableConnectors = new HashSet<>();
+
+        Resource subsystemResource = context.readResourceFromRoot(active.getParent(), false);
+        availableConnectors.addAll(subsystemResource.getChildrenNames(CommonAttributes.REMOTE_CONNECTOR));
+
+        Resource activeMQServerResource = context.readResourceFromRoot(active, false);
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.HTTP_CONNECTOR));
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.IN_VM_CONNECTOR));
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.REMOTE_CONNECTOR));
+        availableConnectors.addAll(activeMQServerResource.getChildrenNames(CommonAttributes.CONNECTOR));
+        return availableConnectors;
+    }
 }

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ServerDefinition.java
@@ -873,8 +873,12 @@ public class ServerDefinition extends PersistentResourceDefinition {
 
         children.add(new BridgeDefinition(registerRuntimeOnly));
         children.add(new BroadcastGroupDefinition(registerRuntimeOnly));
+        children.add(new SocketBroadcastGroupDefinition(registerRuntimeOnly));
+        children.add(new JGroupsBroadcastGroupDefinition(registerRuntimeOnly));
         // WFLY-10518 - keep discovery-group resource under server
         children.add(new DiscoveryGroupDefinition(registerRuntimeOnly, false));
+        children.add(new JGroupsDiscoveryGroupDefinition(registerRuntimeOnly, false));
+        children.add(new SocketDiscoveryGroupDefinition(registerRuntimeOnly, false));
         children.add(new ClusterConnectionDefinition(registerRuntimeOnly));
         children.add(new QueueDefinition(registerRuntimeOnly, MessagingExtension.QUEUE_PATH));
         children.add(new JMSQueueDefinition(false, registerRuntimeOnly));
@@ -887,7 +891,6 @@ public class ServerDefinition extends PersistentResourceDefinition {
     @Override
     public void registerChildren(ManagementResourceRegistration resourceRegistration) {
         super.registerChildren(resourceRegistration);
-
         // runtime queues and core-address are only registered when it is ok to register runtime resource (ie they are not registered on HC).
         if (registerRuntimeOnly) {
             resourceRegistration.registerSubModel(new QueueDefinition(registerRuntimeOnly,  MessagingExtension.RUNTIME_QUEUE_PATH));

messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/jms/ExternalConnectionFactoryService.java
@@ -33,7 +33,8 @@ import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 import org.wildfly.clustering.dispatcher.CommandDispatcherFactory;
-import org.wildfly.extension.messaging.activemq.DiscoveryGroupAdd;
+import org.wildfly.extension.messaging.activemq.JGroupsDiscoveryGroupAdd;
+import org.wildfly.extension.messaging.activemq.SocketDiscoveryGroupAdd;
 import org.wildfly.extension.messaging.activemq.TransportConfigOperationHandlers;
 import org.wildfly.extension.messaging.activemq.logging.MessagingLogger;
 
@@ -109,13 +110,13 @@ public class ExternalConnectionFactoryService implements Service<ConnectionFacto
                 if (commandDispatcherFactories.containsKey(key)) {
                     CommandDispatcherFactory commandDispatcherFactory = commandDispatcherFactories.get(key).get();
                     String clusterName = clusterNames.get(key);
-                    config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, commandDispatcherFactory, clusterName);
+                    config = JGroupsDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, commandDispatcherFactory, clusterName);
                 } else {
                     final SocketBinding binding = groupBindings.get(key).get();
                     if (binding == null) {
                         throw MessagingLogger.ROOT_LOGGER.failedToFindDiscoverySocketBinding(name);
                     }
-                    config = DiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, binding);
+                    config = SocketDiscoveryGroupAdd.createDiscoveryGroupConfiguration(name, groupConfiguration, binding);
                     binding.getSocketBindings().getNamedRegistry().registerBinding(ManagedBinding.Factory.createSimpleManagedBinding(binding));
                 }
                 if (ha) {