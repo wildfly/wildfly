5aa038859b76a344ddb56fdbf482d181ae0bec6e
Cheng Fang <cfang@redhat.com>
2019-12-04 09:04:04 -0500

    Remove sleep in testStatefulTimeout0 test since stateful bean with stateful-timeout 0 is directly removed.

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/timeout/StatefulTimeoutTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/stateful/timeout/StatefulTimeoutTestCase.java
@@ -164,14 +164,13 @@ public class StatefulTimeoutTestCase {
     }
 
     /**
-     * Verifies that a stateful bean with timeout value 0 is eligible fore removal immediately, and its
+     * Verifies that a stateful bean with timeout value 0 is eligible for removal immediately, and its
      * preDestroy method is invoked.
      */
     @Test
     public void testStatefulTimeout0() throws Exception {
         Annotated0TimeoutBean timeout0 = lookup(Annotated0TimeoutBean.class);
         timeout0.doStuff();
-        Thread.sleep(2000);
         try {
             timeout0.doStuff();
             throw new RuntimeException("Expecting NoSuchEJBException");
