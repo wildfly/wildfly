57b30de471630c0803d9f17dbf7d71d7ed55531e
Paul Ferraro <paul.ferraro@redhat.com>
2020-01-10 13:42:29 -0500

    Generalize ResourceCapabilityReference

--- clustering/common/src/main/java/org/jboss/as/clustering/controller/AddStepHandler.java
+++ clustering/common/src/main/java/org/jboss/as/clustering/controller/AddStepHandler.java
@@ -249,7 +249,7 @@ public class AddStepHandler extends AbstractAddStepHandler implements Registrati
             }
         }
 
-        for (CapabilityReferenceRecorder recorder : context.getResourceRegistration().getRequirements()) {
+        for (CapabilityReferenceRecorder recorder : registration.getRequirements()) {
             recorder.addCapabilityRequirements(context, resource, null);
         }
     }
