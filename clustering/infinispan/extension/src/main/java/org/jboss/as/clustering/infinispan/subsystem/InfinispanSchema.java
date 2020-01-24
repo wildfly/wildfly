ef503b7457450c7905d8117bf556b4de470e0f28
Paul Ferraro <paul.ferraro@redhat.com>
2020-01-24 18:32:48 -0500

    Version 10.0 of Infinispan subsystem schema

--- clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/InfinispanSchema.java
+++ clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/InfinispanSchema.java
@@ -44,9 +44,10 @@ public enum InfinispanSchema implements Schema<InfinispanSchema> {
     VERSION_6_0(6, 0), // WildFly 13
     VERSION_7_0(7, 0), // WildFly 14-15
     VERSION_8_0(8, 0), // WildFly 16
-    VERSION_9_0(9, 0), // WildFly 17
+    VERSION_9_0(9, 0), // WildFly 17-19
+    VERSION_10_0(10, 0), // WildFly 20
     ;
-    static final InfinispanSchema CURRENT = VERSION_9_0;
+    static final InfinispanSchema CURRENT = VERSION_10_0;
 
     private final int major;
     private final int minor;
