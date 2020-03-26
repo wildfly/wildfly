71e26eb965ceabef58252276cc348e8f594b9780
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-26 17:50:06 +0100

    [WFLY-13295] activate REST-AT for REST endpoints on plain EJBs as well

--- testsuite/integration/rts/src/test/java/org/wildfly/test/extension/rts/InboundBridgeEjbTestCase.java
+++ testsuite/integration/rts/src/test/java/org/wildfly/test/extension/rts/InboundBridgeEjbTestCase.java
@@ -1,6 +1,6 @@
 /*
 * JBoss, Home of Professional Open Source.
-* Copyright 2013, Red Hat Middleware LLC, and individual contributors
+* Copyright 2020, Red Hat Middleware LLC, and individual contributors
 * as indicated by the @author tags. See the copyright.txt file in the
 * distribution for a full listing of individual contributors.
 *
@@ -27,24 +27,20 @@ import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.arquillian.container.ManagementClient;
-import org.jboss.jbossts.star.util.TxStatusMediaType;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.test.extension.rts.common.InboundBridgeResource;
+import org.wildfly.test.extension.rts.common.InboundBridgeResourceEjb;
 import org.wildfly.test.extension.rts.common.LoggingRestATResource;
 import org.wildfly.test.extension.rts.common.LoggingXAResource;
 
-/**
- * @author <a href="mailto:gytis@redhat.com">Gytis Trikleris</a>
- */
 @RunAsClient
 @RunWith(Arquillian.class)
-public final class InboundBridgeTestCase extends AbstractTestCase {
+public final class InboundBridgeEjbTestCase extends AbstractTestCase {
     private static final String DEPENDENCIES = "Dependencies: org.jboss.narayana.rts, org.jboss.jts, org.codehaus.jettison\n";
+
     private InboundBridgeUtilities utils;
 
     @ArquillianResource
@@ -53,8 +49,8 @@ public final class InboundBridgeTestCase extends AbstractTestCase {
     @Deployment
     public static WebArchive getDeployment() {
         return AbstractTestCase.getDeployment()
-                .addClasses(InboundBridgeResource.class, LoggingXAResource.class, LoggingRestATResource.class)
-                .addAsWebInfResource(InboundBridgeTestCase.class.getClassLoader().getResource("web.xml"),"web.xml")
+                .addClasses(InboundBridgeResourceEjb.class, LoggingXAResource.class, LoggingRestATResource.class)
+                .addAsWebInfResource(InboundBridgeEjbTestCase.class.getClassLoader().getResource("web.xml"),"web.xml")
                 .addAsManifestResource(new StringAsset(DEPENDENCIES), "MANIFEST.MF");
     }
 
@@ -62,7 +58,7 @@ public final class InboundBridgeTestCase extends AbstractTestCase {
     public void before() {
         super.before();
         utils = new InboundBridgeUtilities(txSupport,
-                getDeploymentUrl() + InboundBridgeResource.URL_SEGMENT, // inboundBridgeResourceUrl
+                getDeploymentUrl() + InboundBridgeResourceEjb.URL_SEGMENT, // inboundBridgeResourceUrl
                 getDeploymentUrl() + LoggingRestATResource.BASE_URL_SEGMENT, // loggingRestATParticipantUrl
                 getDeploymentUrl() + LoggingRestATResource.BASE_URL_SEGMENT + "/" + LoggingRestATResource.INVOCATIONS_URL_SEGMENT); // loggingRestATParticipantInvocationsUrl
         utils.resetInvocations();
@@ -96,50 +92,6 @@ public final class InboundBridgeTestCase extends AbstractTestCase {
         utils.assertJsonArray(jsonArray, "LoggingXAResource.prepare", 0);
         utils.assertJsonArray(jsonArray, "LoggingXAResource.commit", 0);
         utils.assertJsonArray(jsonArray, "LoggingXAResource.rollback", 1);
-    }
-
-    @Test
-    public void testCommitWithTwoParticipants() throws Exception {
-        txSupport.startTx();
-        utils.enlistLoggingRestATParticipant();
-        utils.enlistInboundBridgeResource();
-        txSupport.commitTx();
-
-        JSONArray participantResourceInvocations = utils.getLoggingRestATParticipantInvocations();
-        JSONArray xaResourceInvocations = utils.getInboundBridgeResourceInvocations();
-
-        Assert.assertEquals(2, participantResourceInvocations.length());
-        Assert.assertEquals("LoggingRestATResource.terminateParticipant(" + TxStatusMediaType.TX_PREPARED + ")",
-                participantResourceInvocations.get(0));
-        Assert.assertEquals("LoggingRestATResource.terminateParticipant(" + TxStatusMediaType.TX_COMMITTED + ")",
-                participantResourceInvocations.get(1));
-
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.start", 1);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.end", 1);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.prepare", 1);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.commit", 1);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.rollback", 0);
-    }
-
-    @Test
-    public void testRollbackWithTwoParticipants() throws Exception {
-        txSupport.startTx();
-        utils.enlistLoggingRestATParticipant();
-        utils.enlistInboundBridgeResource();
-        txSupport.rollbackTx();
-
-        JSONArray participantResourceInvocations = utils.getLoggingRestATParticipantInvocations();
-        JSONArray xaResourceInvocations = utils.getInboundBridgeResourceInvocations();
-
-        Assert.assertEquals(1, participantResourceInvocations.length());
-        Assert.assertEquals("LoggingRestATResource.terminateParticipant(" + TxStatusMediaType.TX_ROLLEDBACK + ")",
-                participantResourceInvocations.get(0));
-
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.start", 1);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.end", 1);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.prepare", 0);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.commit", 0);
-        utils.assertJsonArray(xaResourceInvocations, "LoggingXAResource.rollback", 1);
     }
 
     protected String getBaseUrl() {
