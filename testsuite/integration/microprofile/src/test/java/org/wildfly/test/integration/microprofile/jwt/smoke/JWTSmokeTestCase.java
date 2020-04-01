38ee59424d74484bb25b78af4c406b29f9464d54
Darran Lofthouse <darran.lofthouse@jboss.com>
2020-04-01 14:02:36 +0100

    [WFLY-13318] Add a new test to verify MicroProfile JWT handling.

---
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/jwt/smoke/JWTSmokeTestCase.java
@@ -0,0 +1,113 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2020, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.test.integration.microprofile.jwt.smoke;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.wildfly.test.integration.microprofile.jwt.TokenUtil.generateJWT;
+
+import java.io.File;
+import java.net.URL;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.FileAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * A smoke test case for a simple JWT deployment and invocation.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class JWTSmokeTestCase {
+
+    private static final String ROOT_PATH = "/rest/Sample/";
+    private static final String SUBSCRIPTION = "subscription";
+
+    private static final String DATE = "2017-09-15";
+    private static final String ECHOER_GROUP = "Echoer";
+    private static final String SUBSCRIBER_GROUP = "Subscriber";
+
+    private static final String PRINCIPAL_NAME = "testUser";
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer";
+
+    private static final String DEPLOYMENT_NAME = JWTSmokeTestCase.class.getSimpleName() + ".war";
+
+    private final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+    @ArquillianResource
+    private URL deploymentUrl;
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(WebArchive.class, DEPLOYMENT_NAME)
+                .add(EmptyAsset.INSTANCE, "WEB-INF/beans.xml")
+                .addClasses(App.class, SampleEndPoint.class)
+                .addAsWebInfResource(new FileAsset(new File("src/test/resources/jwt/web.xml")), "web.xml")
+                .addAsManifestResource(new FileAsset(new File("src/test/resources/jwt/microprofile-config.properties")), "microprofile-config.properties")
+                .addAsManifestResource(new FileAsset(new File("src/test/resources/jwt/public.pem")), "public.pem");
+    }
+
+    @Test
+    public void testAuthorizationRequired() throws Exception {
+        HttpGet httpGet = new HttpGet(deploymentUrl.toString() + ROOT_PATH + SUBSCRIPTION);
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+
+        assertEquals("Authorization required", 403, httpResponse.getStatusLine().getStatusCode());
+
+        httpResponse.close();
+    }
+
+    @Test
+    public void testAuthorized() throws Exception {
+        String jwtToken = generateJWT(PRINCIPAL_NAME, DATE, ECHOER_GROUP, SUBSCRIBER_GROUP);
+
+        HttpGet httpGet = new HttpGet(deploymentUrl.toString() + ROOT_PATH + SUBSCRIPTION);
+        httpGet.addHeader(AUTHORIZATION, BEARER + " " + jwtToken);
+
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+
+        assertEquals("Successful call", 200, httpResponse.getStatusLine().getStatusCode());
+        String body = EntityUtils.toString(httpResponse.getEntity());
+        assertTrue("Call was authenticated", body.contains(PRINCIPAL_NAME));
+
+        httpResponse.close();
+    }
+
+}
