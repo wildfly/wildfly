46f8ecb130fe5a16450df213d016211b96e3f766
Ondra Chaloupka <ochaloup@redhat.com>
2020-04-07 14:25:49 +0200

    [WFLY-13283] suspending WFTC transaction on response of REST-AT bridge

--- rts/src/main/java/org/wildfly/extension/rts/logging/RTSLogger.java
+++ rts/src/main/java/org/wildfly/extension/rts/logging/RTSLogger.java
@@ -23,11 +23,16 @@
 package org.wildfly.extension.rts.logging;
 
 import javax.transaction.SystemException;
+import javax.ws.rs.container.ContainerResponseContext;
+
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
 import org.jboss.logging.annotations.Cause;
+import org.jboss.logging.annotations.LogMessage;
 import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
+
+import static org.jboss.logging.Logger.Level.ERROR;
 
 /**
  *
@@ -41,4 +46,8 @@ public interface RTSLogger extends BasicLogger {
 
     @Message(id = 1, value = "Can't import global transaction to wildfly transaction client.")
     IllegalStateException failueOnImportingGlobalTransactionFromWildflyClient(@Cause SystemException se);
+
+    @LogMessage(level = ERROR)
+    @Message(id = 2, value = "Cannot get transaction status on handling response context %s")
+    void cannotGetTransactionStatus(ContainerResponseContext responseCtx, @Cause Throwable cause);
 }
