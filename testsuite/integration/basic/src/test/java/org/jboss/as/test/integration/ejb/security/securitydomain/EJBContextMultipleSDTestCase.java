7b5e51b2f044b2e6c4ffd8e45574a2949139fb4c
Darran Lofthouse <darran.lofthouse@jboss.com>
2020-05-07 16:55:29 +0100

    [EFLY-12516] Correct date of copyright and correct the package declaration to match the location of the class.

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/securitydomain/EJBContextMultipleSDTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/security/securitydomain/EJBContextMultipleSDTestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2011, Red Hat, Inc., and individual contributors
+ * Copyright 2020, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -19,7 +19,8 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.as.test.integration.ejb.security;
+
+package org.jboss.as.test.integration.ejb.security.securitydomain;
 
 import java.io.File;
 import java.io.IOException;
