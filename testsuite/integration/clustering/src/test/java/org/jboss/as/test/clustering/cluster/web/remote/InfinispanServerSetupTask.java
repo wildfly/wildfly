81b57365a78cbe416bb43e5f8781f68eb38829d4
Paul Ferraro <paul.ferraro@redhat.com>
2020-05-11 14:03:32 -0400

    WFLY-13453 Near cache should auto-enable when hotrod session manager is used

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/web/remote/InfinispanServerSetupTask.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/web/remote/InfinispanServerSetupTask.java
@@ -35,7 +35,6 @@ public class InfinispanServerSetupTask extends CLIServerSetupTask {
                 .setup("/socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=infinispan-server:add(port=11622,host=%s)", AbstractClusteringTestCase.TESTSUITE_NODE0)
                 .setup("/subsystem=infinispan/remote-cache-container=web:add(default-remote-cluster=infinispan-server-cluster, module=org.wildfly.clustering.web.hotrod, statistics-enabled=true)")
                 .setup("/subsystem=infinispan/remote-cache-container=web/remote-cluster=infinispan-server-cluster:add(socket-bindings=[infinispan-server])")
-                .setup("/subsystem=infinispan/remote-cache-container=web/near-cache=invalidation:add()")
                 .teardown("/subsystem=infinispan/remote-cache-container=web:remove")
                 .teardown("/socket-binding-group=standard-sockets/remote-destination-outbound-socket-binding=infinispan-server-1:remove")
                 ;
