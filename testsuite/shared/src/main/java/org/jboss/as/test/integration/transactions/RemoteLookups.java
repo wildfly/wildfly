66fa75d39d9ab59818766c128ff45b722ea11d5a
Ondra Chaloupka <ochaloup@redhat.com>
2020-05-26 06:36:20 +0200

    [WFLY-13516] refactoring of the shared transaction utilities to get base for the test

--- testsuite/shared/src/main/java/org/jboss/as/test/integration/transactions/RemoteLookups.java
+++ testsuite/shared/src/main/java/org/jboss/as/test/integration/transactions/RemoteLookups.java
@@ -22,12 +22,17 @@
 
 package org.jboss.as.test.integration.transactions;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.rmi.PortableRemoteObject;
+
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.shared.integration.ejb.security.CallbackHandler;
 import org.jboss.logging.Logger;
 
 /**
@@ -35,16 +40,16 @@ import org.jboss.logging.Logger;
  *
  * @author Ondra Chaloupka <ochaloup@redhat.com>
  */
-public final class TransactionTestLookupUtil {
-    private static final Logger log = Logger.getLogger(TransactionTestLookupUtil.class);
+public final class RemoteLookups {
+    private static final Logger log = Logger.getLogger(RemoteLookups.class);
 
-    private TransactionTestLookupUtil() {
+    private RemoteLookups() {
         // not instance here
     }
 
     public static <T> T lookupModule(InitialContext ctx, Class<T> beanType) throws NamingException {
         String lookupString = String.format("java:module/%s!%s", beanType.getSimpleName(), beanType.getName());
-        log.debug("looking for: " + lookupString);
+        log.debugf("looking for: %s", lookupString);
         return beanType.cast(ctx.lookup(lookupString));
     }
 
@@ -59,13 +64,18 @@ public final class TransactionTestLookupUtil {
     }
 
     public static <T> T lookupEjbStateless(String host, int port, String archiveName, Class<? extends T> beanType,
-            Class<T> remoteInterface) throws NamingException {
+            Class<T> remoteInterface) throws NamingException, URISyntaxException {
         return lookupEjb(host, port, archiveName, beanType, remoteInterface, false);
     }
 
     public static <T> T lookupEjbStateful(String host, int port, String archiveName, Class<? extends T> beanType,
-            Class<T> remoteInterface) throws NamingException {
+            Class<T> remoteInterface) throws NamingException, URISyntaxException {
         return lookupEjb(host, port, archiveName, beanType, remoteInterface, true);
+    }
+
+    public static <T> T lookupEjbStateless(final ManagementClient managementClient, final String archiveName, Class<? extends T> beanType,
+                                           final Class<T> remoteInterface) throws NamingException, URISyntaxException {
+        return lookupEjb(managementClient, archiveName, beanType, remoteInterface, false);
     }
 
     public static <T> T lookupIIOP(String serverHost, int iiopPort, Class<T> homeClass, Class<?> beanClass) throws NamingException {
@@ -81,28 +91,49 @@ public final class TransactionTestLookupUtil {
     private static <T> T lookupEjb(InitialContext ctx, String archiveName, Class<? extends T> beanType,
             Class<T> remoteInterface, boolean isStateful) throws NamingException {
         String ejbLookup = org.jboss.as.test.shared.integration.ejb.security.Util.createRemoteEjbJndiContext(
-                "", archiveName, "", beanType.getSimpleName(), remoteInterface.getName(), isStateful);
-        log.debug("looking for: " + ejbLookup);
+                "",
+                 archiveName,
+                "",
+                 beanType.getSimpleName(),
+                 remoteInterface.getName(),
+                 isStateful);
+        log.debugf("looking for: %s", ejbLookup);
         return remoteInterface.cast(ctx.lookup(ejbLookup));
     }
 
     private static <T> T lookupEjb(String host, int port, String archiveName, Class<? extends T> beanType,
-            Class<T> remoteInterface, boolean isStateful) throws NamingException {
-        InitialContext ctx = new InitialContext(getEjbClientProperties(host, port));
+            Class<T> remoteInterface, boolean isStateful) throws NamingException, URISyntaxException {
+        InitialContext ctx = getRemoteContext(host, port);
         return lookupEjb(ctx, archiveName, beanType, remoteInterface, isStateful);
     }
 
-    private static Properties getEjbClientProperties(String node, int port) {
-        Properties props = new Properties();
-        props.put("org.jboss.ejb.client.scoped.context", true);
-        props.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        props.put("endpoint.name", "client");
-        props.put("remote.connections", "main");
-        props.put("remote.connection.main.host", node);
-        props.put("remote.connection.main.port", Integer.toString(port));
-        props.put("remote.connection.main.connect.options.org.xnio.Options.SASL_POLICY_NOANONYMOUS", "false");
-        props.put("remote.connection.main.connect.options.org.xnio.Options.SASL_POLICY_NOPLAINTEXT", "true");
-        props.put("remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", "false");
+    private static <T> T lookupEjb(ManagementClient managementClient, String archiveName, Class<? extends T> beanType,
+                                   Class<T> remoteInterface, boolean isStateful) throws NamingException, URISyntaxException {
+        InitialContext ctx = getRemoteContext(managementClient);
+        return lookupEjb(ctx, archiveName, beanType, remoteInterface, isStateful);
+    }
+
+    private static InitialContext getRemoteContext(final String host, int port) throws NamingException, URISyntaxException {
+        Properties props = getEjbClientProperties();
+        URI namingUri = new URI("remote+http", null, host, port, "", "", "");
+        props.put(Context.PROVIDER_URL, namingUri.toString());
+        return new InitialContext(props);
+    }
+
+    private static InitialContext getRemoteContext(final ManagementClient managementClient) throws NamingException, URISyntaxException {
+        Properties props = getEjbClientProperties();
+        URI webUri = managementClient.getWebUri();
+        URI namingUri = new URI("remote+http", webUri.getUserInfo(), webUri.getHost(), webUri.getPort(), "", "", "");
+        props.put(Context.PROVIDER_URL, namingUri.toString());
+        return new InitialContext(props);
+    }
+
+    private static Properties getEjbClientProperties() {
+        final Properties props = new Properties();
+        props.put(Context.INITIAL_CONTEXT_FACTORY, org.wildfly.naming.client.WildFlyInitialContextFactory.class.getName());
+        props.put("jboss.naming.client.connect.options.org.xnio.Options.SASL_POLICY_NOPLAINTEXT", "false");
+        props.put("jboss.naming.client.security.callback.handler.class", CallbackHandler.class.getName());
+        props.put("jboss.naming.client.ejb.context", true);
         return props;
     }
 }
