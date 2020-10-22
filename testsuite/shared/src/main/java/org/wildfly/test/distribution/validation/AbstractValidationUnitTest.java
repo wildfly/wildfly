09f887517a402d59fbea4db7b27d1a6cbd12261f
Brian Stansberry <brian.stansberry@redhat.com>
2020-10-21 20:57:13 -0500

    [WFLY-13994] Use the shared base class for the ee-9/dist xml validation tests

--- testsuite/shared/src/main/java/org/wildfly/test/distribution/validation/AbstractValidationUnitTest.java
+++ testsuite/shared/src/main/java/org/wildfly/test/distribution/validation/AbstractValidationUnitTest.java
@@ -493,6 +493,8 @@ public class AbstractValidationUnitTest {
         result = result.replace("${wildfly.webservices.statistics-enabled:${wildfly.statistics-enabled:false}}", "false");
         result = result.replace("${env.MP_HEALTH_EMPTY_LIVENESS_CHECKS_STATUS:UP}", "UP");
         result = result.replace("${env.MP_HEALTH_EMPTY_READINESS_CHECKS_STATUS:UP}", "UP");
+        result = result.replace("${jboss.messaging.connector.host:localhost}", "localhost");
+        result = result.replace("${jboss.messaging.connector.port:61616}", "61616");
         return result;
     }
 }
