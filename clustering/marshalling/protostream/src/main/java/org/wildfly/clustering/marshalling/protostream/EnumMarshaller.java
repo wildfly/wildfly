104a58bae3ae73372c03153dff8f1091efdac698
Paul Ferraro <paul.ferraro@redhat.com>
2020-12-15 17:32:55 -0500

    EnumMarshaller is also a ProtoStreamMarshaller.

--- clustering/marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/EnumMarshaller.java
+++ clustering/marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/EnumMarshaller.java
@@ -22,9 +22,13 @@
 
 package org.wildfly.clustering.marshalling.protostream;
 
+import java.io.IOException;
+import java.io.StreamCorruptedException;
 import java.util.OptionalInt;
 
 import org.infinispan.protostream.ImmutableSerializationContext;
+import org.infinispan.protostream.RawProtoStreamReader;
+import org.infinispan.protostream.RawProtoStreamWriter;
 
 import protostream.com.google.protobuf.CodedOutputStream;
 
@@ -32,7 +36,7 @@ import protostream.com.google.protobuf.CodedOutputStream;
  * ProtoStream marshaller for enums.
  * @author Paul Ferraro
  */
-public class EnumMarshaller<E extends Enum<E>> implements org.infinispan.protostream.EnumMarshaller<E>, Predictable<E> {
+public class EnumMarshaller<E extends Enum<E>> implements org.infinispan.protostream.EnumMarshaller<E>, ProtoStreamMarshaller<E> {
 
     private final Class<E> enumClass;
     private final E[] values;
@@ -66,5 +70,19 @@ public class EnumMarshaller<E extends Enum<E>> implements org.infinispan.protost
     @Override
     public OptionalInt size(ImmutableSerializationContext context, E value) {
         return OptionalInt.of(CodedOutputStream.computeEnumSizeNoTag(value.ordinal()));
+    }
+
+    @Override
+    public E readFrom(ImmutableSerializationContext context, RawProtoStreamReader reader) throws IOException {
+        int ordinal = reader.readUInt32();
+        if (reader.readTag() != 0) {
+            throw new StreamCorruptedException();
+        }
+        return this.decode(ordinal);
+    }
+
+    @Override
+    public void writeTo(ImmutableSerializationContext context, RawProtoStreamWriter writer, E value) throws IOException {
+        writer.writeUInt32NoTag(this.encode(value));
     }
 }
