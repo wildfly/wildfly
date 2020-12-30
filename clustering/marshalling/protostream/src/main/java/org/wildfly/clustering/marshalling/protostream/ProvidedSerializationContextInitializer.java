2929db85d572b99a8187a80a6f47327de16394e1
Paul Ferraro <paul.ferraro@redhat.com>
2020-12-30 10:38:44 -0500

    Generic SerializationContextInitializer that registers a MarshallerProvider from a set of ProtoStreamMarshallerProviders.

---
+++ clustering/marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/ProvidedSerializationContextInitializer.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2020, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.clustering.marshalling.protostream;
+
+import java.util.EnumSet;
+
+import org.infinispan.protostream.SerializationContext;
+
+/**
+ * A {@link org.infinispan.protostream.SerializationContextInitializer} that registers enumerated marshallers.
+ * @author Paul Ferraro
+ */
+public class ProvidedSerializationContextInitializer<E extends Enum<E> & ProtoStreamMarshallerProvider> extends AbstractSerializationContextInitializer {
+
+    private final Class<E> providerClass;
+
+    public ProvidedSerializationContextInitializer(String resourceName, Class<E> providerClass) {
+        super(resourceName);
+        this.providerClass = providerClass;
+    }
+
+    @Override
+    public void registerMarshallers(SerializationContext context) {
+        context.registerMarshallerProvider(new ProvidedMarshallerProvider(EnumSet.allOf(this.providerClass)));
+    }
+}
