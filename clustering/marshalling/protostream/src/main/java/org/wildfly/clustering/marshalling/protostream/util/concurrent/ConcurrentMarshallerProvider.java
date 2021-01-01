a727d59053cec929f20466334a83490c7e2c625b
Paul Ferraro <paul.ferraro@redhat.com>
2021-01-01 12:38:29 -0500

    Remove redundant SerializationContextInitializer classes

--- clustering/marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/util/concurrent/ConcurrentMarshallerProvider.java
+++ clustering/marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/util/concurrent/ConcurrentMarshallerProvider.java
@@ -29,8 +29,10 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.TimeUnit;
 
 import org.wildfly.clustering.marshalling.Externalizer;
+import org.wildfly.clustering.marshalling.protostream.EnumMarshaller;
 import org.wildfly.clustering.marshalling.protostream.ExternalizerMarshaller;
 import org.wildfly.clustering.marshalling.protostream.ProtoStreamMarshallerProvider;
 import org.wildfly.clustering.marshalling.protostream.ProtoStreamMarshaller;
@@ -49,6 +51,7 @@ public enum ConcurrentMarshallerProvider implements ProtoStreamMarshallerProvide
     CONCURRENT_SKIP_LIST_SET(new SortedSetMarshaller<>(ConcurrentSkipListSet::new)),
     COPY_ON_WRITE_ARRAY_LIST(new CopyOnWriteCollectionMarshaller<>(CopyOnWriteArrayList::new)),
     COPY_ON_WRITE_ARRAY_SET(new CopyOnWriteCollectionMarshaller<>(CopyOnWriteArraySet::new)),
+    TIME_UNIT(new EnumMarshaller<>(TimeUnit.class)),
     ;
     private final ProtoStreamMarshaller<Object> marshaller;
 
