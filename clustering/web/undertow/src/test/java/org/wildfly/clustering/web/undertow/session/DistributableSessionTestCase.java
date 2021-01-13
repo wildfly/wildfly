ada1db28230ab1a525ee4e00ba4d401841a05404
Paul Ferraro <paul.ferraro@redhat.com>
2021-01-13 09:20:14 -0500

    WFLY-14276 Store elytron security identity in local context, not as a replicated attribute, as this is not serializable.

--- clustering/web/undertow/src/test/java/org/wildfly/clustering/web/undertow/session/DistributableSessionTestCase.java
+++ clustering/web/undertow/src/test/java/org/wildfly/clustering/web/undertow/session/DistributableSessionTestCase.java
@@ -394,6 +394,11 @@ public class DistributableSessionTestCase {
         this.getLocalContextSessionAttribute(DistributableSession.WEB_SOCKET_CHANNELS_ATTRIBUTE);
     }
 
+    @Test
+    public void getIdentityContainerSessionAttribute() {
+        this.getLocalContextSessionAttribute(DistributableSession.IDENTITY_CONTAINER_ATTRIBUTE);
+    }
+
     private void getLocalContextSessionAttribute(String name) {
         when(this.session.getMetaData()).thenReturn(this.metaData);
         when(this.metaData.isNew()).thenReturn(true);
@@ -643,6 +648,11 @@ public class DistributableSessionTestCase {
         this.setLocalContextSessionAttribute(DistributableSession.WEB_SOCKET_CHANNELS_ATTRIBUTE);
     }
 
+    @Test
+    public void setIdentityContainerSessionAttribute() {
+        this.setLocalContextSessionAttribute(DistributableSession.IDENTITY_CONTAINER_ATTRIBUTE);
+    }
+
     private void setLocalContextSessionAttribute(String name) {
         when(this.session.getMetaData()).thenReturn(this.metaData);
         when(this.metaData.isNew()).thenReturn(true);
@@ -797,6 +807,11 @@ public class DistributableSessionTestCase {
     @Test
     public void removeWebSocketChannelsSessionAttribute() {
         this.removeLocalContextSessionAttribute(DistributableSession.WEB_SOCKET_CHANNELS_ATTRIBUTE);
+    }
+
+    @Test
+    public void removeIdentityContainerSessionAttribute() {
+        this.removeLocalContextSessionAttribute(DistributableSession.IDENTITY_CONTAINER_ATTRIBUTE);
     }
 
     private void removeLocalContextSessionAttribute(String name) {
