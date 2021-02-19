b5d8701ae4dd639540f42c045afa0f3c2f64bb49
Farah Juma <fjuma@redhat.com>
2021-02-19 15:25:25 -0500

    [WFLY-14425] Add tests to the JSF subsystem for attributes that allow expressions

--- jsf/subsystem/src/test/java/org/jboss/as/jsf/subsystem/JSFSubsystemTestCase.java
+++ jsf/subsystem/src/test/java/org/jboss/as/jsf/subsystem/JSFSubsystemTestCase.java
@@ -21,7 +21,13 @@
 */
 package org.jboss.as.jsf.subsystem;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.junit.Assert.assertEquals;
+
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
+import org.jboss.as.subsystem.test.KernelServices;
+import org.jboss.as.subsystem.test.KernelServicesBuilder;
+import org.jboss.dmr.ModelNode;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -37,7 +43,9 @@ public class JSFSubsystemTestCase extends AbstractSubsystemBaseTest {
 
     @Override
     protected String getSubsystemXml() throws IOException {
-        return "<subsystem xmlns=\"urn:jboss:domain:jsf:1.1\" />";
+        return "<subsystem xmlns=\"urn:jboss:domain:jsf:1.1\"" +
+                " default-jsf-impl-slot=\"${exp.default-jsf-impl-slot:main}\"" +
+                " disallow-doctype-decl=\"${exp.disallow-doctype-decl:true}\" />";
     }
 
     @Override
@@ -56,5 +64,16 @@ public class JSFSubsystemTestCase extends AbstractSubsystemBaseTest {
     @Override
     public void testSchemaOfSubsystemTemplates() throws Exception {
         super.testSchemaOfSubsystemTemplates();
+    }
+
+    @Test
+    public void testAttributes() throws Exception {
+        KernelServicesBuilder builder = createKernelServicesBuilder(null)
+                .setSubsystemXml(getSubsystemXml());
+        KernelServices kernelServices = builder.build();
+        ModelNode rootModel = kernelServices.readWholeModel();
+        ModelNode serverModel = rootModel.require(SUBSYSTEM).require(JSFExtension.SUBSYSTEM_NAME);
+        assertEquals("main", serverModel.get(JSFResourceDefinition.DEFAULT_SLOT_ATTR_NAME).resolve().asString());
+        assertEquals(true, serverModel.get(JSFResourceDefinition.DISALLOW_DOCTYPE_DECL_ATTR_NAME).resolve().asBoolean());
     }
 }
