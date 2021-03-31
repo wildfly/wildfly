404914c88fd7ecd6300b3d730650dff4f92b1edb
Paul Ferraro <paul.ferraro@redhat.com>
2021-03-31 12:42:42 -0400

    Bump Infinispan subsystem model to 15.0.0.

--- clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/InfinispanModel.java
+++ clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/InfinispanModel.java
@@ -45,9 +45,10 @@ public enum InfinispanModel implements Model {
     VERSION_11_1_0(11, 1, 0), // EAP 7.3.4
     VERSION_12_0_0(12, 0, 0), // WildFly 20
     VERSION_13_0_0(13, 0, 0), // WildFly 21
-    VERSION_14_0_0(14, 0, 0), // WildFly 23
+    VERSION_14_0_0(14, 0, 0), // WildFly 23, EAP 7.4
+    VERSION_15_0_0(15, 0, 0), // WildFly 24
     ;
-    static final InfinispanModel CURRENT = VERSION_14_0_0;
+    static final InfinispanModel CURRENT = VERSION_15_0_0;
 
     private final ModelVersion version;
 
