f58a097b0ffc51814ce12c71cd7fe760472db271
parsharm <parsharm@redhat.com>
2021-05-07 10:19:59 +0530

    WFLY-14778 Updated the class with a dependency on http-deprecated Elytron modules to no longer rely on deprecated classes

--- undertow/src/main/java/org/wildfly/extension/undertow/ApplicationSecurityDomainDefinition.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/ApplicationSecurityDomainDefinition.java
@@ -95,7 +95,14 @@ import org.wildfly.security.auth.server.SecurityDomain;
 import org.wildfly.security.cache.IdentityCache;
 import org.wildfly.security.http.HttpExchangeSpi;
 import org.wildfly.security.http.HttpServerAuthenticationMechanismFactory;
-import org.wildfly.security.http.impl.ServerMechanismFactoryImpl;
+import org.wildfly.security.http.basic.BasicMechanismFactory;
+import org.wildfly.security.http.bearer.BearerMechanismFactory;
+import org.wildfly.security.http.cert.ClientCertMechanismFactory;
+import org.wildfly.security.http.digest.DigestMechanismFactory;
+import org.wildfly.security.http.external.ExternalMechanismFactory;
+import org.wildfly.security.http.form.FormMechanismFactory;
+import org.wildfly.security.http.spnego.SpnegoMechanismFactory;
+import org.wildfly.security.http.util.AggregateServerMechanismFactory;
 import org.wildfly.security.http.util.FilterServerMechanismFactory;
 import org.wildfly.security.http.util.sso.ProgrammaticSingleSignOnCache;
 import org.wildfly.security.http.util.sso.SingleSignOnServerMechanismFactory;
@@ -354,7 +361,10 @@ public class ApplicationSecurityDomainDefinition extends PersistentResourceDefin
     }
 
     private static HttpAuthenticationFactory toHttpAuthenticationFactory(final SecurityDomain securityDomain, final String realmName) {
-        final HttpServerAuthenticationMechanismFactory mechanismFactory = new FilterServerMechanismFactory(new ServerMechanismFactoryImpl(), SERVLET_MECHANISM);
+        final HttpServerAuthenticationMechanismFactory mechanismFactory = new FilterServerMechanismFactory(
+                new AggregateServerMechanismFactory(new BasicMechanismFactory(), new BearerMechanismFactory(),
+                        new ClientCertMechanismFactory(), new DigestMechanismFactory(), new ExternalMechanismFactory(),
+                        new FormMechanismFactory(), new SpnegoMechanismFactory()), SERVLET_MECHANISM);
         return HttpAuthenticationFactory.builder().setFactory(mechanismFactory)
                 .setSecurityDomain(securityDomain)
                 .setMechanismConfigurationSelector(
