11b8873fde6061e130466a6316ff74144a0723cb
Kabir Khan <kkhan@redhat.com>
2021-05-25 16:37:21 +0100

    [WFLY-14798] Port tests from Quarkus

---
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/reactive/messaging/ported/channels/EmitterExample.java
@@ -0,0 +1,65 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.test.integration.microprofile.reactive.messaging.ported.channels;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+/**
+ * Copied from Quarkus and adjusted
+ *
+ * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
+ */
+@ApplicationScoped
+public class EmitterExample {
+
+    @Inject
+    @Channel("sink")
+    Emitter<String> emitter;
+
+    private final List<String> list = new CopyOnWriteArrayList<>();
+
+    public void run() {
+        emitter.send("a");
+        emitter.send("b");
+        emitter.send("c");
+        emitter.complete();
+    }
+
+    @Incoming("sink")
+    public void consume(String s) {
+        list.add(s);
+    }
+
+    public List<String> list() {
+        return list;
+    }
+
+}
