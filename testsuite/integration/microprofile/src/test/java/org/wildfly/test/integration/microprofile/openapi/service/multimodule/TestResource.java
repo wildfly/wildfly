14063a55bdcfec546f4c8d4429983abd5b13cc34
Joachim <j.grimm@ssc-services.de>
2021-07-05 13:46:05 +0200

    [WF-14962] adds integration test

---
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/openapi/service/multimodule/TestResource.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2019, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.test.integration.microprofile.openapi.service.multimodule;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.infinispan.commons.dataconversion.MediaType;
+
+/**
+ * @author Joachim Grimm
+ */
+@Path("/echo")
+@Produces(MediaType.APPLICATION_JSON_TYPE)
+@Consumes(MediaType.APPLICATION_JSON_TYPE)
+public class TestResource {
+
+    @Inject
+    private TestEjb testEjb;
+
+    @POST
+    @Operation(summary = "Test the indexing of multi module deployments")
+    @APIResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = TestResponse.class)),
+            description = "Indexes found")
+    public TestResponse echo(TestRequest request) {
+        return testEjb.hello(request);
+    }
+}
