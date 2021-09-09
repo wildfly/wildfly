a0c789357f039947c3a7169a2aaf3b83c175f163
Darran Lofthouse <darran.lofthouse@jboss.com>
2021-09-09 16:29:16 +0100

    [WFLY-15199] Convert test cases to Elytron exclusively.

--- testsuite/integration/web/src/test/java/org/jboss/as/test/integration/web/security/external/ExternalAuthSecurityDomainSetup.java
+++ testsuite/integration/web/src/test/java/org/jboss/as/test/integration/web/security/external/ExternalAuthSecurityDomainSetup.java
@@ -1,32 +1,12 @@
 package org.jboss.as.test.integration.web.security.external;
 
 import org.jboss.as.arquillian.container.ManagementClient;
-import org.jboss.as.controller.PathAddress;
-import org.jboss.as.controller.operations.common.Util;
-import org.jboss.as.security.Constants;
 import org.jboss.as.test.integration.management.util.CLIWrapper;
 import org.jboss.as.test.integration.security.common.AbstractSecurityDomainSetup;
-import org.jboss.as.test.integration.web.security.WebSecurityCommon;
 import org.jboss.as.test.shared.ServerReload;
-import org.jboss.dmr.ModelNode;
 import org.jboss.logging.Logger;
 import org.wildfly.test.security.common.elytron.PropertyFileBasedDomain;
 import org.wildfly.test.security.common.elytron.UndertowDomainMapper;
-
-import java.util.Arrays;
-
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ALLOW_RESOURCE_SERVICE_RESTART;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.COMPOSITE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OPERATION_HEADERS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.STEPS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
-import static org.jboss.as.security.Constants.CODE;
-import static org.jboss.as.security.Constants.FLAG;
-import static org.jboss.as.security.Constants.LOGIN_MODULE;
-import static org.jboss.as.security.Constants.MODULE;
-import static org.jboss.as.security.Constants.SECURITY_DOMAIN;
 
 /**
  * @author Stuart Douglas
@@ -43,35 +23,8 @@ public class ExternalAuthSecurityDomainSetup extends AbstractSecurityDomainSetup
 
     @Override
     public void setup(final ManagementClient managementClient, final String containerId) throws Exception {
-        if (WebSecurityCommon.isElytron()) {
-            cli = new CLIWrapper(true);
-            setupElytronBasedSecurityDomain();
-        } else {
-            log.debug("start of the domain creation");
-
-            final ModelNode compositeOp = new ModelNode();
-            compositeOp.get(OP).set(COMPOSITE);
-            compositeOp.get(OP_ADDR).setEmptyList();
-
-            ModelNode steps = compositeOp.get(STEPS);
-            PathAddress address = PathAddress.pathAddress()
-                    .append(SUBSYSTEM, "security")
-                    .append(SECURITY_DOMAIN, getSecurityDomainName());
-
-            steps.add(Util.createAddOperation(address));
-            address = address.append(Constants.AUTHENTICATION, Constants.CLASSIC);
-            steps.add(Util.createAddOperation(address));
-            ModelNode loginModule = Util.createAddOperation(address.append(LOGIN_MODULE, "External"));
-
-            loginModule.get(CODE).set("org.jboss.as.test.integration.web.security.external.ExternalLoginModule");
-            loginModule.get(MODULE).set("deployment.web-secure-external.war");
-            loginModule.get(FLAG).set("required");
-            loginModule.get(OPERATION_HEADERS).get(ALLOW_RESOURCE_SERVICE_RESTART).set(true);
-            steps.add(loginModule);
-
-            applyUpdates(managementClient.getControllerClient(), Arrays.asList(compositeOp));
-            log.debug("end of the domain creation");
-        }
+        cli = new CLIWrapper(true);
+        setupElytronBasedSecurityDomain();
     }
 
     @Override
@@ -81,18 +34,13 @@ public class ExternalAuthSecurityDomainSetup extends AbstractSecurityDomainSetup
 
     @Override
     public void tearDown(ManagementClient managementClient, String s) {
-        if (WebSecurityCommon.isElytron()) {
-            try {
-                domainMapper.remove(cli);
-                ps.remove(cli);
-                cli.close();
-                ServerReload.executeReloadAndWaitForCompletion(managementClient);
-            } catch (Exception e) {
-                log.error("Failed to tear down domain.", e);
-            }
-        }
-        else {
-            super.tearDown(managementClient, s);
+        try {
+            domainMapper.remove(cli);
+            ps.remove(cli);
+            cli.close();
+            ServerReload.executeReloadAndWaitForCompletion(managementClient);
+        } catch (Exception e) {
+            log.error("Failed to tear down domain.", e);
         }
     }
 
--- testsuite/shared/src/main/java/org/jboss/as/test/integration/security/common/AbstractSecurityDomainSetup.java
+++ testsuite/shared/src/main/java/org/jboss/as/test/integration/security/common/AbstractSecurityDomainSetup.java
@@ -1,33 +1,13 @@
 package org.jboss.as.test.integration.security.common;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import org.jboss.as.arquillian.api.ServerSetupTask;
-import org.jboss.as.arquillian.container.ManagementClient;
-import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.as.controller.client.OperationBuilder;
-import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
-import org.jboss.as.controller.operations.common.Util;
-import org.jboss.as.security.Constants;
 import org.jboss.dmr.ModelNode;
 import org.jboss.logging.Logger;
-
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ALLOW_RESOURCE_SERVICE_RESTART;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.COMPOSITE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OPERATION_HEADERS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REMOVE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ROLLBACK_ON_RUNTIME_FAILURE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.STEPS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
-import static org.jboss.as.security.Constants.FLAG;
-import static org.jboss.as.security.Constants.LOGIN_MODULE;
-import static org.jboss.as.security.Constants.SECURITY_DOMAIN;
 
 /**
  * @author Stuart Douglas
@@ -58,55 +38,6 @@ public abstract class AbstractSecurityDomainSetup implements ServerSetupTask {
         }
     }
 
-    @Override
-    public void tearDown(final ManagementClient managementClient, final String containerId) {
-
-        ModelNode op = new ModelNode();
-        op.get(OP).set(REMOVE);
-        op.get(OP_ADDR).add(SUBSYSTEM, "security");
-        op.get(OP_ADDR).add(Constants.SECURITY_DOMAIN, getSecurityDomainName());
-        // Don't rollback when the AS detects the war needs the module
-        op.get(OPERATION_HEADERS, ROLLBACK_ON_RUNTIME_FAILURE).set(false);
-        op.get(OPERATION_HEADERS, ALLOW_RESOURCE_SERVICE_RESTART).set(true);
-
-        try {
-            applyUpdate(managementClient.getControllerClient(), op, true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     protected abstract String getSecurityDomainName();
 
-
-    protected void createSecurityDomain(final Class loginModuleClass, final Map<String, String> moduleOptionsCache, final ModelControllerClient client) {
-        final ModelNode compositeOp = new ModelNode();
-        compositeOp.get(OP).set(COMPOSITE);
-        compositeOp.get(OP_ADDR).setEmptyList();
-        ModelNode steps = compositeOp.get(STEPS);
-        PathAddress address = PathAddress.pathAddress()
-                .append(SUBSYSTEM, "security")
-                .append(SECURITY_DOMAIN, getSecurityDomainName());
-
-        steps.add(Util.createAddOperation(address));
-        address = address.append(Constants.AUTHENTICATION, Constants.CLASSIC);
-        steps.add(Util.createAddOperation(address));
-
-        ModelNode loginModule = Util.createAddOperation(address.append(LOGIN_MODULE, loginModuleClass.getName()));
-        loginModule.get(ModelDescriptionConstants.CODE).set(loginModuleClass.getName());
-        loginModule.get(FLAG).set("required");
-        loginModule.get(OPERATION_HEADERS).get(ALLOW_RESOURCE_SERVICE_RESTART).set(true);
-
-        ModelNode moduleOptions = loginModule.get("module-options");
-        for (Map.Entry<String, String> entry : moduleOptionsCache.entrySet()) {
-            moduleOptions.get(entry.getKey()).set(entry.getValue());
-        }
-        steps.add(loginModule);
-
-        try {
-            applyUpdates(client, Arrays.asList(compositeOp));
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
 }
