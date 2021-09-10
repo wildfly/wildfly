0b6941b7f275815deacbd832f43b0d6213521e84
Darran Lofthouse <darran.lofthouse@jboss.com>
2021-09-10 18:24:44 +0100

    [WFLY-15199] Remove loginmodule tests.

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/rar/RarDeploymentTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/rar/RarDeploymentTestCase.java
@@ -31,7 +31,6 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
