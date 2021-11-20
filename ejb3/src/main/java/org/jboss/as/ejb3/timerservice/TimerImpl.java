e861bb526ee98b88d44f86f7837eed9171c80a80
Cheng Fang <cfang@redhat.com>
2021-11-20 13:39:49 -0500

    WFLY-15711 Remove references to entity bean primary key from timer service impl classes

--- ejb3/src/main/java/org/jboss/as/ejb3/timerservice/TimerImpl.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/timerservice/TimerImpl.java
@@ -86,11 +86,6 @@ public class TimerImpl implements Timer {
     protected final long intervalDuration;
 
     /**
-     * If this is an entity bean then this is the primary key
-     */
-    protected final Object primaryKey;
-
-    /**
      * Next expiry date of this timer
      */
     protected volatile Date nextExpiration;
@@ -140,8 +135,6 @@ public class TimerImpl implements Timer {
             this.nextExpiration = builder.nextDate;
         }
         this.previousRun = builder.previousRun;
-        this.primaryKey = builder.primaryKey;
-
         this.timerState = builder.timerState;
         this.timerService = service;
         this.timedObjectInvoker = service.getInvoker();
@@ -589,10 +582,6 @@ public class TimerImpl implements Timer {
         return sb.toString();
    }
 
-    public Object getPrimaryKey() {
-        return primaryKey;
-    }
-
     public static Builder builder() {
         return new Builder();
     }
@@ -605,7 +594,6 @@ public class TimerImpl implements Timer {
         protected Date nextDate;
         protected Date previousRun;
         protected Serializable info;
-        protected Object primaryKey;
         protected TimerState timerState;
         protected boolean persistent;
         protected boolean newTimer;
@@ -642,11 +630,6 @@ public class TimerImpl implements Timer {
 
         public Builder setInfo(final Serializable info) {
             this.info = info;
-            return this;
-        }
-
-        public Builder setPrimaryKey(final Object primaryKey) {
-            this.primaryKey = primaryKey;
             return this;
         }
 
