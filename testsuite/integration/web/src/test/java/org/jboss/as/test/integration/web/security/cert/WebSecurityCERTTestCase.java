4957df28edd25d739aa15c4a2091c56fcc8a9124
Darran Lofthouse <darran.lofthouse@jboss.com>
2021-11-24 18:28:53 +0000

    [WFLY-15748] / [WFLY-13889] Temporarily disable test case as client is using PicketBox APIs.

--- testsuite/integration/web/src/test/java/org/jboss/as/test/integration/web/security/cert/WebSecurityCERTTestCase.java
+++ testsuite/integration/web/src/test/java/org/jboss/as/test/integration/web/security/cert/WebSecurityCERTTestCase.java
@@ -24,25 +24,10 @@ package org.jboss.as.test.integration.web.security.cert;
 
 import static org.junit.Assert.assertEquals;
 
-import java.net.URL;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.HttpClientConnectionManager;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -52,9 +37,9 @@ import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.as.test.categories.CommonCriteria;
 import org.jboss.as.test.integration.web.security.SecuredServlet;
-import org.jboss.security.JBossJSSESecurityDomain;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -68,6 +53,7 @@ import org.junit.runner.RunWith;
 @RunAsClient
 @ServerSetup(WebCERTTestsSetup.class)
 @Category(CommonCriteria.class)
+@Ignore("[WFLY-15749] Update WebSecurityCERTTestCase to use Elytron SSLContext.")
 public class WebSecurityCERTTestCase {
 
     @ArquillianResource
@@ -88,29 +74,30 @@ public class WebSecurityCERTTestCase {
         try {
             // Ensure the protocol enabled here matches what was configured on server side
             // to avoid failures on IBM JDK
-            SSLContext ctx = SSLContext.getInstance("TLSv1.2");
-            JBossJSSESecurityDomain jsseSecurityDomain = new JBossJSSESecurityDomain("client-cert");
-            jsseSecurityDomain.setKeyStorePassword(WebCERTTestsSetup.getPassword());
-            URL keystore = WebCERTTestsSetup.getClientKeystoreFile().toURI().toURL();
-            jsseSecurityDomain.setKeyStoreURL(keystore.getPath());
-            jsseSecurityDomain.setClientAlias(alias);
-            jsseSecurityDomain.reloadKeyAndTrustStore();
-            KeyManager[] keyManagers = jsseSecurityDomain.getKeyManagers();
-            TrustManager[] trustManagers = jsseSecurityDomain.getTrustManagers();
-            ctx.init(keyManagers, trustManagers, null);
-            HostnameVerifier verifier = (string, ssls) -> true;
-            SSLConnectionSocketFactory ssf = new SSLConnectionSocketFactory(ctx, verifier);
-            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
-                    .register("https", ssf)
-                    .build();
-
-            HttpClientConnectionManager ccm = new BasicHttpClientConnectionManager(registry);
-
-            return HttpClientBuilder.create()
-                    .setSSLSocketFactory(ssf)
-                    .setSSLHostnameVerifier(new NoopHostnameVerifier())
-                    .setConnectionManager(ccm)
-                    .build();
+//            SSLContext ctx = SSLContext.getInstance("TLSv1.2");
+//            JBossJSSESecurityDomain jsseSecurityDomain = new JBossJSSESecurityDomain("client-cert");
+//            jsseSecurityDomain.setKeyStorePassword(WebCERTTestsSetup.getPassword());
+//            URL keystore = WebCERTTestsSetup.getClientKeystoreFile().toURI().toURL();
+//            jsseSecurityDomain.setKeyStoreURL(keystore.getPath());
+//            jsseSecurityDomain.setClientAlias(alias);
+//            jsseSecurityDomain.reloadKeyAndTrustStore();
+//            KeyManager[] keyManagers = jsseSecurityDomain.getKeyManagers();
+//            TrustManager[] trustManagers = jsseSecurityDomain.getTrustManagers();
+//            ctx.init(keyManagers, trustManagers, null);
+//            HostnameVerifier verifier = (string, ssls) -> true;
+//            SSLConnectionSocketFactory ssf = new SSLConnectionSocketFactory(ctx, verifier);
+//            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+//                    .register("https", ssf)
+//                    .build();
+//
+//            HttpClientConnectionManager ccm = new BasicHttpClientConnectionManager(registry);
+//
+//            return HttpClientBuilder.create()
+//                    .setSSLSocketFactory(ssf)
+//                    .setSSLHostnameVerifier(new NoopHostnameVerifier())
+//                    .setConnectionManager(ccm)
+//                    .build();
+            return null;
         } catch (Exception e) {
             throw new RuntimeException("Creating of HTTPS client instance has failed.", e);
         }
