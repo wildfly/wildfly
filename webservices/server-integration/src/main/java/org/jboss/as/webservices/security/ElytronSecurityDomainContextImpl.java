48cc9e628ac3a3e994f9ae097dd4b526e7a12bae
Diana Krepinska Vilkolakova <skyllarr@gmail.com>
2022-01-21 15:28:10 +0100

    [WFLY-15598] WebServices UsernameToken profile integration with Elytron

--- webservices/server-integration/src/main/java/org/jboss/as/webservices/security/ElytronSecurityDomainContextImpl.java
+++ webservices/server-integration/src/main/java/org/jboss/as/webservices/security/ElytronSecurityDomainContextImpl.java
@@ -57,6 +57,12 @@ public class ElytronSecurityDomainContextImpl implements SecurityDomainContext {
     public String getSecurityDomain() {
         return this.securityDomain.toString();
     }
+
+    @Override
+    public SecurityDomain getElytronSecurityDomain() {
+        return this.securityDomain;
+    }
+
     //TODO:refactor/deprecate this after elytron?
     @Override
     public Set<Principal> getUserRoles(Principal principal) {
