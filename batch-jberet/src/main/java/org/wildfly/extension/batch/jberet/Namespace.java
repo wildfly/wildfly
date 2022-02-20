2472f9f3c9eb268a338b31b17fbf90380bd3ef2e
Cheng Fang <cfang@redhat.com>
2022-02-20 14:11:47 -0500

    WFLY-16066 Simplify batch-jberet enum: Attribute and Namespace

--- batch-jberet/src/main/java/org/wildfly/extension/batch/jberet/Namespace.java
+++ batch-jberet/src/main/java/org/wildfly/extension/batch/jberet/Namespace.java
@@ -22,7 +22,6 @@
 
 package org.wildfly.extension.batch.jberet;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -35,6 +34,11 @@ enum Namespace {
     BATCH_1_0("urn:jboss:domain:batch-jberet:1.0"),
     BATCH_2_0("urn:jboss:domain:batch-jberet:2.0"),
     ;
+
+    private static final Map<String, Namespace> MAP = Map.of(
+            BATCH_1_0.name, BATCH_1_0,
+            BATCH_2_0.name, BATCH_2_0
+    );
 
     /**
      * The current namespace version.
@@ -56,20 +60,10 @@ enum Namespace {
         return name;
     }
 
-    private static final Map<String, Namespace> MAP;
-
-    static {
-        final Map<String, Namespace> map = new HashMap<String, Namespace>();
-        for (Namespace namespace : values()) {
-            final String name = namespace.getUriString();
-            if (name != null) {
-                map.put(name, namespace);
-            }
-        }
-        MAP = map;
-    }
-
     public static Namespace forUri(String uri) {
+        if (uri == null) {
+            return UNKNOWN;
+        }
         final Namespace element = MAP.get(uri);
         return element == null ? UNKNOWN : element;
     }
