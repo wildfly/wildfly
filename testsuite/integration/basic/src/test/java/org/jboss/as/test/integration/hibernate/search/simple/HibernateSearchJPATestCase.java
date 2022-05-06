609188477b0aadbaa759d84c2b89c8813b016c8c
Yoann Rodière <yoann@hibernate.org>
2022-05-06 09:46:55 +0200

    WFLY-15838 Disable Hibernate Search 5 tests in EE9 (WF Preview) ITs

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/hibernate/search/simple/HibernateSearchJPATestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/hibernate/search/simple/HibernateSearchJPATestCase.java
@@ -36,7 +36,6 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -45,8 +44,6 @@ import org.junit.runner.RunWith;
  *
  * @author Sanne Grinovero <sanne@hibernate.org> (C) 2014 Red Hat Inc.
  */
-/* Ignore until we upgrade to Hibernate Search 6.1 via WFLY-15838 */
-@Ignore
 @RunWith(Arquillian.class)
 public class HibernateSearchJPATestCase {
 
