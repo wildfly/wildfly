a0db5991304efbb4ae57417c57bcfb5d4c7382b1
Richard Opálka <opalka.richard@gmail.com>
2022-06-02 15:29:41 +0200

    [WFLY-16454][WFLY-15697][WFLY-15698][WFLY-15700] Upgrade Undertow to 2.3.0.Alpha1, Jakarta Servlet API to 6.0.0, Jakarta Websockets API to 2.1.0 and Jakarta Server Pages to 3.1.0 in WildFly Preview
    This PR brings in the following changes:
    Sub-task
        [UNDERTOW-2058] - Session.getRequestURI() must return full URI not just request path
        [UNDERTOW-2065] - ServerEndpointConfig.getUserProperties() must be per connection
        [UNDERTOW-2084] - Adjust ServletResponse.setCharacterEncoding() behavior after spec clarification
        [UNDERTOW-2085] - Implement fallback locale to charset mapping if such mapping wasn't specified in DD
        [UNDERTOW-2086] - ASYNC and REQUEST dispatch types must return redirected path -> servlet mapping
        [UNDERTOW-2087] - All ServletContext modification methods must throw UnsupportedOperationException
        [UNDERTOW-2088] - All SessionCookieConfig modification methods must throw IllegalStateException
        [UNDERTOW-2089] - RFC 6265 treats the attributes of an RFC 2109 cookie as a separate cookies
    
    Feature Request
        [UNDERTOW-1593] - Track processing time of in flight requests
        [UNDERTOW-2051] - Upgrade WebSockets API to 2.1.0
        [UNDERTOW-2052] - Upgrade Servlet API to 6.0.0
        [UNDERTOW-2100] - Upgrade Server Pages API to 3.1.0
    
    Bug
        [UNDERTOW-2036] - AbstractFramedChannel.awaitWritable does not guard against spurious wakes
        [UNDERTOW-2061] - IP address filter with netmask not working as expected
        [UNDERTOW-2070] - Empty reply from Undertow if sendRedirect is called after setting content length
        [UNDERTOW-2073] - JDK 8 / 11 updates breaking Undertow
        [UNDERTOW-2081] - RejectedExecutionException occurs during shutdown if an open websocket session exists
        [UNDERTOW-2091] - spotbugs-maven-plugin needs to be updated to work with JDK17
        [UNDERTOW-2092] - Update GitHub CI configuration to JDK11+
        [UNDERTOW-2093] - Fix spotbugs reported errors
    
    Task
        [UNDERTOW-2049] - Move Undertow to JDK11
        [UNDERTOW-2050] - Move Undertow to Jakarta Specs by Default
        [UNDERTOW-2096] - Add Nexus repository to CI
    
    Component Upgrade
        [UNDERTOW-2074] - Upgrade XNIO to 3.8.7.Final
    
    Enhancement
        [UNDERTOW-1879] - Utilize o.w.common.Assert class for Null-Checks

---
+++ ee-9/source-transform/undertow/src/main/java/org/wildfly/extension/undertow/deployment/JspPropertyGroupDescriptorImpl.java
@@ -0,0 +1,105 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2022, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.extension.undertow.deployment;
+
+import org.apache.jasper.deploy.JspPropertyGroup;
+
+import jakarta.servlet.descriptor.JspPropertyGroupDescriptor;
+import java.util.Collection;
+
+/**
+ * @author Stuart Douglas
+ */
+public class JspPropertyGroupDescriptorImpl implements JspPropertyGroupDescriptor {
+
+    private final JspPropertyGroup propertyGroup;
+
+    public JspPropertyGroupDescriptorImpl(JspPropertyGroup propertyGroup) {
+        this.propertyGroup = propertyGroup;
+    }
+
+    @Override
+    public Collection<String> getUrlPatterns() {
+        return propertyGroup.getUrlPatterns();
+    }
+
+    @Override
+    public String getElIgnored() {
+        return propertyGroup.getElIgnored();
+    }
+
+    @Override
+    public String getErrorOnELNotFound() {
+        return propertyGroup.getErrorOnELNotFound();
+    }
+
+    @Override
+    public String getPageEncoding() {
+        return propertyGroup.getPageEncoding();
+    }
+
+    @Override
+    public String getScriptingInvalid() {
+        return propertyGroup.getScriptingInvalid();
+    }
+
+    @Override
+    public String getIsXml() {
+        return propertyGroup.getIsXml();
+    }
+
+    @Override
+    public Collection<String> getIncludePreludes() {
+        return propertyGroup.getIncludePreludes();
+    }
+
+    @Override
+    public Collection<String> getIncludeCodas() {
+        return propertyGroup.getIncludeCodas();
+    }
+
+    @Override
+    public String getDeferredSyntaxAllowedAsLiteral() {
+        return propertyGroup.getDeferredSyntaxAllowedAsLiteral();
+    }
+
+    @Override
+    public String getTrimDirectiveWhitespaces() {
+        return propertyGroup.getTrimDirectiveWhitespaces();
+    }
+
+    @Override
+    public String getDefaultContentType() {
+        return propertyGroup.getDefaultContentType();
+    }
+
+    @Override
+    public String getBuffer() {
+        return propertyGroup.getBuffer();
+    }
+
+    @Override
+    public String getErrorOnUndeclaredNamespace() {
+        return propertyGroup.getErrorOnUndeclaredNamespace();
+    }
+}
