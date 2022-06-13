fc9689ba8c537f4f400e7730e3dd2291b76ed2ea
Paul Ferraro <paul.ferraro@redhat.com>
2022-06-13 11:19:46 -0400

    Leverage constants.

--- undertow/src/test/java/org/wildfly/extension/undertow/SecurityDomainSingleSignOnAttributesTestCase.java
+++ undertow/src/test/java/org/wildfly/extension/undertow/SecurityDomainSingleSignOnAttributesTestCase.java
@@ -1,15 +1,17 @@
 package org.wildfly.extension.undertow;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.subsystem.test.AbstractSubsystemTest;
 import org.jboss.as.subsystem.test.KernelServices;
 import org.jboss.as.subsystem.test.KernelServicesBuilder;
 import org.jboss.dmr.ModelNode;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class SecurityDomainSingleSignOnAttributesTestCase extends AbstractSubsystemTest {
 
@@ -21,13 +23,10 @@ public class SecurityDomainSingleSignOnAttributesTestCase extends AbstractSubsys
     public void testPathChangeRequiresReload() throws Exception {
         KernelServicesBuilder builder = createKernelServicesBuilder(AbstractUndertowSubsystemTestCase.RUNTIME).setSubsystemXml(readResource("undertow-13.0.xml"));
         KernelServices mainServices = builder.build();
-        PathAddress address = PathAddress.pathAddress("subsystem", "undertow")
-                .append("application-security-domain", "other")
-                .append("setting", "single-sign-on");
+        PathAddress address = PathAddress.pathAddress(UndertowExtension.SUBSYSTEM_PATH, PathElement.pathElement(Constants.APPLICATION_SECURITY_DOMAIN, "other"), UndertowExtension.PATH_SSO);
         ModelNode result = mainServices.executeOperation(Util.getWriteAttributeOperation(address, SingleSignOnDefinition.Attribute.PATH.getName(), new ModelNode("/modified-path")));
-        assertEquals("success", result.get("outcome").asString());
-        assertTrue("It is expected that reload is required after the operation.",
-                result.get("response-headers").get("operation-requires-reload").asBoolean());
+        assertEquals(ModelDescriptionConstants.SUCCESS, result.get(ModelDescriptionConstants.OUTCOME).asString());
+        assertTrue("It is expected that reload is required after the operation.", result.get(ModelDescriptionConstants.RESPONSE_HEADERS).get(ModelDescriptionConstants.OPERATION_REQUIRES_RELOAD).asBoolean());
     }
 
 }
