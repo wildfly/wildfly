pferraro/jboss-as

Add ProtoStream marshaller factory implementation.

clustering/web/extension/src/main/java/org/wildfly/extension/clustering/web/SessionMarshallerFactory.java
@@ -27,6 +27,9 @@ import java.util.function.Function;
 import org.jboss.modules.Module;
 import org.wildfly.clustering.marshalling.jboss.JBossByteBufferMarshaller;
 import org.wildfly.clustering.marshalling.jboss.SimpleMarshallingConfigurationRepository;
+import org.wildfly.clustering.marshalling.protostream.ModuleClassResolver;
+import org.wildfly.clustering.marshalling.protostream.ProtoStreamByteBufferMarshaller;
+import org.wildfly.clustering.marshalling.protostream.SerializationContextBuilder;
 import org.wildfly.clustering.marshalling.spi.ByteBufferMarshaller;
 
 /**
@@ -38,6 +41,12 @@ public enum SessionMarshallerFactory implements Function<Module, ByteBufferMarsh
         @Override
         public ByteBufferMarshaller apply(Module module) {
             return new JBossByteBufferMarshaller(new SimpleMarshallingConfigurationRepository(JBossMarshallingVersion.class, JBossMarshallingVersion.CURRENT, module), module.getClassLoader());
+        }
+    },
+    PROTOSTREAM() {
+        @Override
+        public ByteBufferMarshaller apply(Module module) {
+            return new ProtoStreamByteBufferMarshaller(new SerializationContextBuilder(new ModuleClassResolver(module.getModuleLoader())).load(module.getClassLoader()).build());
         }
     },
     ;

wildfly/wildfly

clustering/web/extension/src/main/java/org/wildfly/extension/clustering/web/SessionMarshallerFactory.java
@@ -29,6 +29,9 @@ import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.modules.Module;
 import org.wildfly.clustering.marshalling.jboss.JBossByteBufferMarshaller;
 import org.wildfly.clustering.marshalling.jboss.SimpleMarshallingConfigurationRepository;
+import org.wildfly.clustering.marshalling.protostream.ModuleClassLoaderMarshaller;
+import org.wildfly.clustering.marshalling.protostream.ProtoStreamByteBufferMarshaller;
+import org.wildfly.clustering.marshalling.protostream.SerializationContextBuilder;
 import org.wildfly.clustering.marshalling.spi.ByteBufferMarshaller;
 
 /**
@@ -41,6 +44,13 @@ public enum SessionMarshallerFactory implements Function<DeploymentUnit, ByteBuf
         public ByteBufferMarshaller apply(DeploymentUnit unit) {
             Module module = unit.getAttachment(Attachments.MODULE);
             return new JBossByteBufferMarshaller(new SimpleMarshallingConfigurationRepository(JBossMarshallingVersion.class, JBossMarshallingVersion.CURRENT, module), module.getClassLoader());
+        }
+    },
+    PROTOSTREAM() {
+        @Override
+        public ByteBufferMarshaller apply(DeploymentUnit unit) {
+            Module module = unit.getAttachment(Attachments.MODULE);
+            return new ProtoStreamByteBufferMarshaller(new SerializationContextBuilder(new ModuleClassLoaderMarshaller(module.getModuleLoader())).load(module.getClassLoader()).build());
         }
     },
     ;