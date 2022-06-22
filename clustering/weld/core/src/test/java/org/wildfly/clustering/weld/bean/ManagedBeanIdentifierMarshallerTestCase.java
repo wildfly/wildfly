c2a65440a0e6fc8194b4be6b40a378a345e20c7f
Paul Ferraro <paul.ferraro@redhat.com>
2022-06-22 10:28:15 -0400

    WFLY-16524 Add missing static container initialization causing test to fail when run individually.

--- clustering/weld/core/src/test/java/org/wildfly/clustering/weld/bean/ManagedBeanIdentifierMarshallerTestCase.java
+++ clustering/weld/core/src/test/java/org/wildfly/clustering/weld/bean/ManagedBeanIdentifierMarshallerTestCase.java
@@ -28,6 +28,7 @@ import org.jboss.weld.bean.ManagedBeanIdentifier;
 import org.junit.Test;
 import org.wildfly.clustering.marshalling.Tester;
 import org.wildfly.clustering.marshalling.protostream.ProtoStreamTesterFactory;
+import org.wildfly.clustering.weld.BeanManagerProvider;
 
 /**
  * Validates marshalling of {@link ManagedBeanIdentifier}.
@@ -37,6 +38,7 @@ public class ManagedBeanIdentifierMarshallerTestCase {
 
     @Test
     public void test() throws IOException {
+        BeanManagerProvider.INSTANCE.apply("foo", "bar");
         Tester<ManagedBeanIdentifier> tester = ProtoStreamTesterFactory.INSTANCE.createTester();
         tester.test(new ManagedBeanIdentifier(AnnotatedTypeIdentifier.forBackedAnnotatedType("foo", String.class, String.class, "bar")));
         tester.test(new ManagedBeanIdentifier(AnnotatedTypeIdentifier.forBackedAnnotatedType("foo", String.class, String.class, "bar", "blah")));
