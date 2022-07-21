e06f5c06c1f3545821831c1bbc7a197b84678c66
Paul Ferraro <paul.ferraro@redhat.com>
2022-07-21 13:35:33 -0400

    Drop obsolete grouper configuration logic.

--- clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/CacheConfigurationServiceConfigurator.java
+++ clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/CacheConfigurationServiceConfigurator.java
@@ -25,20 +25,16 @@ package org.jboss.as.clustering.infinispan.subsystem;
 import static org.jboss.as.clustering.infinispan.subsystem.CacheResourceDefinition.Attribute.STATISTICS_ENABLED;
 import static org.jboss.as.clustering.infinispan.subsystem.CacheResourceDefinition.Capability.CONFIGURATION;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 import org.infinispan.commons.CacheException;
 import org.infinispan.commons.dataconversion.MediaType;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.ExpirationConfiguration;
-import org.infinispan.configuration.cache.GroupsConfigurationBuilder;
 import org.infinispan.configuration.cache.LockingConfiguration;
 import org.infinispan.configuration.cache.MemoryConfiguration;
 import org.infinispan.configuration.cache.PersistenceConfiguration;
-import org.infinispan.configuration.cache.StorageType;
 import org.infinispan.configuration.cache.TransactionConfiguration;
-import org.infinispan.distribution.group.Grouper;
 import org.infinispan.transaction.tm.EmbeddedTransactionManager;
 import org.jboss.as.clustering.controller.CapabilityServiceNameProvider;
 import org.jboss.as.clustering.controller.ResourceServiceConfigurator;
@@ -46,7 +42,6 @@ import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.PathAddress;
 import org.jboss.dmr.ModelNode;
-import org.jboss.modules.Module;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceTarget;
 import org.wildfly.clustering.infinispan.service.ConfigurationServiceConfigurator;
@@ -68,7 +63,6 @@ public class CacheConfigurationServiceConfigurator extends CapabilityServiceName
     private final SupplierDependency<LockingConfiguration> locking;
     private final SupplierDependency<PersistenceConfiguration> persistence;
     private final SupplierDependency<TransactionConfiguration> transaction;
-    private final SupplierDependency<List<Module>> modules;
 
     private volatile boolean statisticsEnabled;
 
@@ -79,20 +73,10 @@ public class CacheConfigurationServiceConfigurator extends CapabilityServiceName
         this.locking = new ServiceSupplierDependency<>(CacheComponent.LOCKING.getServiceName(address));
         this.persistence = new ServiceSupplierDependency<>(CacheComponent.PERSISTENCE.getServiceName(address));
         this.transaction = new ServiceSupplierDependency<>(CacheComponent.TRANSACTION.getServiceName(address));
-        this.modules = new ServiceSupplierDependency<>(CacheComponent.MODULES.getServiceName(address));
 
         String containerName = address.getParent().getLastElement().getValue();
         String cacheName = address.getLastElement().getValue();
-        this.configurator = new ConfigurationServiceConfigurator(this.getServiceName(), containerName, cacheName, this.andThen(builder -> {
-            if (builder.memory().storage() == StorageType.HEAP) {
-                GroupsConfigurationBuilder groupsBuilder = builder.clustering().hash().groups().enabled();
-                for (Module module : this.modules.get()) {
-                    for (Grouper<?> grouper : module.loadService(Grouper.class)) {
-                        groupsBuilder.addGrouper(grouper);
-                    }
-                }
-            }
-        })).require(this);
+        this.configurator = new ConfigurationServiceConfigurator(this.getServiceName(), containerName, cacheName, this).require(this);
     }
 
     @Override
@@ -102,7 +86,7 @@ public class CacheConfigurationServiceConfigurator extends CapabilityServiceName
 
     @Override
     public <T> ServiceBuilder<T> register(ServiceBuilder<T> builder) {
-        return new CompositeDependency(this.memory, this.expiration, this.locking, this.persistence, this.transaction, this.modules).register(builder);
+        return new CompositeDependency(this.memory, this.expiration, this.locking, this.persistence, this.transaction).register(builder);
     }
 
     @Override
