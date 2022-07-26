147830077686f6c057b5f32a4c84df7ecefc35d4
Paul Ferraro <paul.ferraro@redhat.com>
2022-07-26 13:04:50 -0400

    Don't use the same media type as Infinispan's ProtoStreamMarshaller.

--- clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/remote/HotRodStore.java
+++ clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/remote/HotRodStore.java
@@ -32,6 +32,7 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import org.infinispan.client.hotrod.DataFormat;
 import org.infinispan.client.hotrod.DefaultTemplate;
 import org.infinispan.client.hotrod.Flag;
 import org.infinispan.client.hotrod.RemoteCache;
@@ -43,6 +44,7 @@ import org.infinispan.client.hotrod.exceptions.HotRodClientException;
 import org.infinispan.client.hotrod.impl.operations.OperationsFactory;
 import org.infinispan.client.hotrod.impl.operations.PingResponse;
 import org.infinispan.commons.configuration.ConfiguredBy;
+import org.infinispan.commons.dataconversion.MediaType;
 import org.infinispan.commons.io.ByteBuffer;
 import org.infinispan.commons.util.IntSet;
 import org.infinispan.marshall.persistence.PersistenceMarshaller;
@@ -116,12 +118,14 @@ public class HotRodStore<K, V> implements NonBlockingStore<K, V> {
     }
 
     synchronized void setRemoteCache(RemoteCacheContainer container, String cacheName) {
-        this.cache = container.getCache(cacheName);
+        RemoteCache<ByteBuffer, ByteBuffer> cache = container.getCache(cacheName);
 
-        if (this.cache == null) {
+        if (cache == null) {
             // Administration support was introduced in protocol version 2.7
             throw InfinispanLogger.ROOT_LOGGER.remoteCacheMustBeDefined(container.getConfiguration().version().toString(), cacheName);
         }
+
+        this.cache = cache.withDataFormat(DataFormat.builder().keyType(MediaType.APPLICATION_OBJECT).keyMarshaller(container.getMarshaller()).valueType(MediaType.APPLICATION_OBJECT).valueMarshaller(container.getMarshaller()).build());
 
         RemoteCacheManager manager = this.cache.getRemoteCacheManager();
         this.operationsFactory = new OperationsFactory(manager.getChannelFactory(), manager.getCodec(), null, manager.getConfiguration());
--- clustering/infinispan/marshalling/src/main/java/org/wildfly/clustering/infinispan/marshalling/protostream/ProtoStreamMarshaller.java
+++ clustering/infinispan/marshalling/src/main/java/org/wildfly/clustering/infinispan/marshalling/protostream/ProtoStreamMarshaller.java
@@ -35,12 +35,14 @@ import org.wildfly.clustering.marshalling.protostream.SerializationContextBuilde
  * @author Paul Ferraro
  */
 public class ProtoStreamMarshaller extends UserMarshaller {
+    public static final String MEDIA_TYPE_NAME = MediaType.APPLICATION_OCTET_STREAM_TYPE;
+    public static final MediaType MEDIA_TYPE = MediaType.APPLICATION_OCTET_STREAM;
 
     public ProtoStreamMarshaller(ClassLoaderMarshaller loaderMarshaller, UnaryOperator<SerializationContextBuilder> builder) {
         this(builder.apply(new SerializationContextBuilder(loaderMarshaller).register(new IOSerializationContextInitializer())).build());
     }
 
     public ProtoStreamMarshaller(ImmutableSerializationContext context) {
-        super(MediaType.APPLICATION_PROTOSTREAM, new ProtoStreamByteBufferMarshaller(context));
+        super(MEDIA_TYPE, new ProtoStreamByteBufferMarshaller(context));
     }
 }
