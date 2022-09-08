edfd610061586264aab1756985e21f7f9053c79e
Richard Opálka <opalka.richard@gmail.com>
2022-09-08 10:43:43 +0200

    [WFLY-16972] Rewriting DiscoveryService to use new MSC API

--- ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/DiscoveryRegistrationProcessor.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/deployment/processors/DiscoveryRegistrationProcessor.java
@@ -32,19 +32,19 @@ import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
 import org.jboss.as.server.deployment.DeploymentUnitProcessor;
 import org.jboss.ejb.protocol.remote.RemoteEJBDiscoveryConfigurator;
-import org.jboss.msc.inject.InjectionException;
-import org.jboss.msc.inject.Injector;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceName;
 import org.wildfly.discovery.Discovery;
-import org.wildfly.discovery.impl.StaticDiscoveryProvider;
-import org.wildfly.discovery.spi.DiscoveryProvider;
 import org.wildfly.httpclient.ejb.HttpDiscoveryConfigurator;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Processor responsible for ensuring that the discovery service for each deployment unit exists.
  *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
 public final class DiscoveryRegistrationProcessor implements DeploymentUnitProcessor {
     private final boolean appClient;
@@ -59,47 +59,20 @@ public final class DiscoveryRegistrationProcessor implements DeploymentUnitProce
         if (deploymentUnit.getParent() != null) {
             return;
         }
+
         final ServiceName profileServiceName = deploymentUnit.getAttachment(EjbDeploymentAttachmentKeys.EJB_REMOTING_PROFILE_SERVICE_NAME);
-
-        final DiscoveryService discoveryService = new DiscoveryService();
         final ServiceName discoveryServiceName = DiscoveryService.BASE_NAME.append(deploymentUnit.getName());
-        final ServiceBuilder<Discovery> builder = phaseContext.getServiceTarget().addService(discoveryServiceName, discoveryService);
-        Injector<DiscoveryProvider> providerInjector = discoveryService.getDiscoveryProviderInjector();
-        new RemoteEJBDiscoveryConfigurator().configure(providerInjector::inject, registryProvider -> {});
-        if (profileServiceName != null) builder.addDependency(profileServiceName, RemotingProfileService.class, new Injector<RemotingProfileService>() {
-            Injector<DiscoveryProvider> providerInjector = discoveryService.getDiscoveryProviderInjector();
-
-            public void inject(final RemotingProfileService value) throws InjectionException {
-                providerInjector.inject(new StaticDiscoveryProvider(value.getServiceUrls()));
-            }
-
-            public void uninject() {
-                providerInjector.uninject();
-            }
-        });
-
-        providerInjector = discoveryService.getDiscoveryProviderInjector();
-        new HttpDiscoveryConfigurator().configure(providerInjector::inject, registryProvider -> {});
-
+        final ServiceBuilder<?> builder = phaseContext.getServiceTarget().addService(discoveryServiceName);
+        final Consumer<Discovery> discoveryConsumer = builder.provides(discoveryServiceName);
+        final Supplier<RemotingProfileService> remotingProfileServiceSupplier = profileServiceName != null ? builder.requires(profileServiceName) : null;
         // only add association service dependency if the context is configured to use the local EJB receiver & we are not app client
-
-        final EJBClientDescriptorMetaData ejbClientDescriptorMetaData = deploymentUnit
-                .getAttachment(Attachments.EJB_CLIENT_METADATA);
-
+        final EJBClientDescriptorMetaData ejbClientDescriptorMetaData = deploymentUnit.getAttachment(Attachments.EJB_CLIENT_METADATA);
         final boolean useLocalReceiver = ejbClientDescriptorMetaData == null || ejbClientDescriptorMetaData.isLocalReceiverExcluded() != Boolean.TRUE;
-        if (useLocalReceiver && ! appClient) {
-            builder.addDependency(AssociationService.SERVICE_NAME, AssociationService.class, new Injector<AssociationService>() {
-                Injector<DiscoveryProvider> providerInjector = discoveryService.getDiscoveryProviderInjector();
-
-                public void inject(final AssociationService value) throws InjectionException {
-                    providerInjector.inject(value.getLocalDiscoveryProvider());
-                }
-
-                public void uninject() {
-                    providerInjector.uninject();
-                }
-            });
-        }
+        final Supplier<AssociationService> associationServiceSupplier = (useLocalReceiver && !appClient) ? builder.requires(AssociationService.SERVICE_NAME) : null;
+        final DiscoveryService discoveryService = new DiscoveryService(discoveryConsumer, remotingProfileServiceSupplier, associationServiceSupplier);
+        new RemoteEJBDiscoveryConfigurator().configure(discoveryService.getDiscoveryProviderConsumer(), registryProvider -> {});
+        new HttpDiscoveryConfigurator().configure(discoveryService.getDiscoveryProviderConsumer(), registryProvider -> {});
+        builder.setInstance(discoveryService);
         builder.install();
     }
 }
