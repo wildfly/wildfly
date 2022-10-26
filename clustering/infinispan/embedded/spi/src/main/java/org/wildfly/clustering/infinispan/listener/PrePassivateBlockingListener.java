4d4afcce4671b46061224b55da1c11ad85b1ed5d
Paul Ferraro <paul.ferraro@redhat.com>
2022-10-26 09:09:17 -0400

    WFLY-17203 Create separate blocking vs non-blocking pre-passivate listener implementations. Use non-blocking pre-passivate listener where possible.

--- clustering/infinispan/embedded/spi/src/main/java/org/wildfly/clustering/infinispan/listener/PrePassivateBlockingListener.java
+++ clustering/infinispan/embedded/spi/src/main/java/org/wildfly/clustering/infinispan/listener/PrePassivateBlockingListener.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2022, Red Hat, Inc., and individual contributors
+ * Copyright 2021, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -22,29 +22,17 @@
 
 package org.wildfly.clustering.infinispan.listener;
 
-import java.util.concurrent.CompletionStage;
-import java.util.function.Function;
+import java.util.function.BiConsumer;
 
-import org.infinispan.notifications.Listener;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryPassivated;
-import org.infinispan.notifications.cachelistener.event.CacheEntryEvent;
-import org.infinispan.notifications.cachelistener.event.CacheEntryPassivatedEvent;
+import org.infinispan.Cache;
 
 /**
- * Generic non-blocking pre-passivation listener that delegates to a generic cache event listener.
+ * Generic non-blocking pre-passivation listener that delegates to a blocking consumer.
  * @author Paul Ferraro
  */
-@Listener(observation = Listener.Observation.PRE)
-public class PrePassivateListener<K, V> {
+public class PrePassivateBlockingListener<K, V> extends CacheEventListenerRegistrar<K, V> {
 
-    private final Function<CacheEntryEvent<K, V>, CompletionStage<Void>> listener;
-
-    public PrePassivateListener(Function<CacheEntryEvent<K, V>, CompletionStage<Void>> listener) {
-        this.listener = listener;
-    }
-
-    @CacheEntryPassivated
-    public CompletionStage<Void> prePassivate(CacheEntryPassivatedEvent<K, V> event) {
-        return this.listener.apply(event);
+    public PrePassivateBlockingListener(Cache<K, V> cache, BiConsumer<K, V> consumer) {
+        super(cache, new PrePassivateListener<>(new BlockingCacheEventListener<>(cache, consumer)));
     }
 }
