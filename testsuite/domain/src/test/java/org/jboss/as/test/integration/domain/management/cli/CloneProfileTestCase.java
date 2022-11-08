jbosstm/jboss-as

[WFLY-13385](AMS CI fix) increasing CLI timeout for CLI wrapper operations

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/CloneProfileTestCase.java
@@ -29,6 +29,7 @@ import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.logging.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -63,7 +64,7 @@ public class CloneProfileTestCase extends AbstractCliTestBase {
     @BeforeClass
     public static void before() throws Exception {
         DomainTestSupport domainSupport = CLITestSuite.createSupport(CloneProfileTestCase.class.getSimpleName());
-        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress, TimeoutUtil.adjust(20 * 1000));
 
         File masterDir = new File(domainSupport.getDomainMasterConfiguration().getDomainDirectory());
         domainCfg = new File(masterDir, "configuration"

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/DataSourceTestCase.java
@@ -32,6 +32,7 @@ import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
 import org.jboss.as.test.integration.domain.suites.CLITestSuite;
 import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
 import org.jboss.as.test.integration.management.util.CLIOpResult;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.impl.base.exporter.zip.ZipExporterImpl;
@@ -57,7 +58,7 @@ public class DataSourceTestCase extends AbstractCliTestBase {
     public static void before() throws Exception {
 
         CLITestSuite.createSupport(DataSourceTestCase.class.getSimpleName());
-        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
     @AfterClass

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/DeployAllServerGroupsTestCase.java
@@ -28,6 +28,7 @@ import java.io.File;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.junit.Assert;
 
 import org.jboss.as.test.integration.common.HttpRequest;
@@ -64,7 +65,7 @@ public class DeployAllServerGroupsTestCase extends AbstractCliTestBase {
         warFile = new File(tempDir, "SimpleServlet.war");
         new ZipExporterImpl(war).exportTo(warFile, true);
 
-        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
     @AfterClass

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/DomainDeployWithRuntimeNameTestCase.java
@@ -36,6 +36,7 @@ import org.jboss.as.test.integration.common.HttpRequest;
 import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
 import org.jboss.as.test.integration.domain.suites.CLITestSuite;
 import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -66,7 +67,7 @@ public class DomainDeployWithRuntimeNameTestCase extends AbstractCliTestBase {
         List<String> groups = new ArrayList<>(CLITestSuite.serverGroups.keySet());
         Collections.sort(groups);
         serverGroups = groups.toArray(new String[groups.size()]);
-        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
    private File createWarFile(String content) throws IOException {

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/JmsTestCase.java
@@ -27,6 +27,7 @@ import static org.junit.Assert.assertTrue;
 import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
 import org.jboss.as.test.integration.domain.suites.CLITestSuite;
 import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -44,7 +45,7 @@ public class JmsTestCase extends AbstractCliTestBase {
     public static void before() throws Exception {
 
         CLITestSuite.createSupport(JmsTestCase.class.getSimpleName());
-        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
     @AfterClass

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/RolloutPlanTestCase.java
@@ -38,6 +38,7 @@ import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
 import org.jboss.as.test.integration.management.util.CLIOpResult;
 import org.jboss.as.test.shared.RetryTaskExecutor;
 import org.jboss.as.test.shared.TestSuiteEnvironment;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.impl.base.exporter.zip.ZipExporterImpl;
@@ -74,7 +75,7 @@ public class RolloutPlanTestCase extends AbstractCliTestBase {
         warFile = new File(tempDir + File.separator + "RolloutPlanTestCase.war");
         new ZipExporterImpl(war).exportTo(warFile, true);
 
-        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.masterAddress, TimeoutUtil.adjust(20 * 1000));
 
         // add another server group to default profile
         cli.sendLine("/server-group=test-server-group:add(profile=default,socket-binding-group=standard-sockets)");

wildfly/wildfly

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/CloneProfileTestCase.java
@@ -29,6 +29,7 @@ import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.logging.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -63,7 +64,7 @@ public class CloneProfileTestCase extends AbstractCliTestBase {
     @BeforeClass
     public static void before() throws Exception {
         DomainTestSupport domainSupport = CLITestSuite.createSupport(CloneProfileTestCase.class.getSimpleName());
-        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress, TimeoutUtil.adjust(20 * 1000));
 
         File primaryDir = new File(domainSupport.getDomainPrimaryConfiguration().getDomainDirectory());
         domainCfg = new File(primaryDir, "configuration"

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/DataSourceTestCase.java
@@ -32,6 +32,7 @@ import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
 import org.jboss.as.test.integration.domain.suites.CLITestSuite;
 import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
 import org.jboss.as.test.integration.management.util.CLIOpResult;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.impl.base.exporter.zip.ZipExporterImpl;
@@ -57,7 +58,7 @@ public class DataSourceTestCase extends AbstractCliTestBase {
     public static void before() throws Exception {
 
         CLITestSuite.createSupport(DataSourceTestCase.class.getSimpleName());
-        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
     @AfterClass

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/DeployAllServerGroupsTestCase.java
@@ -29,6 +29,9 @@ import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
+import org.jboss.as.test.shared.TimeoutUtil;
+
 import org.jboss.as.test.integration.common.HttpRequest;
 import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
 import org.jboss.as.test.integration.domain.suites.CLITestSuite;
@@ -64,7 +67,7 @@ public class DeployAllServerGroupsTestCase extends AbstractCliTestBase {
 
         new ZipExporterImpl(war).exportTo(warFile, true);
 
-        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
     @AfterClass

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/DomainDeployWithRuntimeNameTestCase.java
@@ -68,7 +68,7 @@ public class DomainDeployWithRuntimeNameTestCase extends AbstractCliTestBase {
         List<String> groups = new ArrayList<>(CLITestSuite.serverGroups.keySet());
         Collections.sort(groups);
         serverGroups = groups.toArray(new String[groups.size()]);
-        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
    private File createWarFile(String content) throws IOException {

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/JmsTestCase.java
@@ -27,6 +27,7 @@ import static org.junit.Assert.assertTrue;
 import org.jboss.as.test.integration.domain.management.util.DomainTestSupport;
 import org.jboss.as.test.integration.domain.suites.CLITestSuite;
 import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -44,7 +45,7 @@ public class JmsTestCase extends AbstractCliTestBase {
     public static void before() throws Exception {
 
         CLITestSuite.createSupport(JmsTestCase.class.getSimpleName());
-        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress, TimeoutUtil.adjust(20 * 1000));
     }
 
     @AfterClass

testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/management/cli/RolloutPlanTestCase.java
@@ -40,6 +40,7 @@ import org.jboss.as.test.integration.management.base.AbstractCliTestBase;
 import org.jboss.as.test.integration.management.util.CLIOpResult;
 import org.jboss.as.test.shared.RetryTaskExecutor;
 import org.jboss.as.test.shared.TestSuiteEnvironment;
+import org.jboss.as.test.shared.TimeoutUtil;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.impl.base.exporter.zip.ZipExporterImpl;
@@ -74,7 +75,7 @@ public class RolloutPlanTestCase extends AbstractCliTestBase {
         warFile = new File(tempDir + File.separator + "RolloutPlanTestCase.war");
         new ZipExporterImpl(war).exportTo(warFile, true);
 
-        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress);
+        AbstractCliTestBase.initCLI(DomainTestSupport.primaryAddress, TimeoutUtil.adjust(20 * 1000));
 
         // add another server group to default profile
         cli.sendLine("/server-group=test-server-group:add(profile=default,socket-binding-group=standard-sockets)");