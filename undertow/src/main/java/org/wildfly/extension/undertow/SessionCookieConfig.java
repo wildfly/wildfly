937848b2951cbdbe1f9fcae2f859d2ccd75d2298
Paul Ferraro <paul.ferraro@redhat.com>
2022-12-08 09:29:32 -0500

    Drop obsolete usage of cookie comment.

--- undertow/src/main/java/org/wildfly/extension/undertow/SessionCookieConfig.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/SessionCookieConfig.java
@@ -32,15 +32,13 @@ public class SessionCookieConfig {
 
     private final String name;
     private final String domain;
-    private final String comment;
     private final Boolean httpOnly;
     private final Boolean secure;
     private final Integer maxAge;
 
-    public SessionCookieConfig(final String name, final String domain, final String comment, final Boolean httpOnly, final Boolean secure, final Integer maxAge) {
+    public SessionCookieConfig(final String name, final String domain, final Boolean httpOnly, final Boolean secure, final Integer maxAge) {
         this.name = name;
         this.domain = domain;
-        this.comment = comment;
         this.httpOnly = httpOnly;
         this.secure = secure;
         this.maxAge = maxAge;
@@ -52,10 +50,6 @@ public class SessionCookieConfig {
 
     public String getDomain() {
         return domain;
-    }
-
-    public String getComment() {
-        return comment;
     }
 
     public Boolean getHttpOnly() {
