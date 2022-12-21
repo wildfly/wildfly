b07f3db02e07ea3497b1fe9c3a6ebf9b4f41f057
Paul Ferraro <paul.ferraro@redhat.com>
2022-12-21 09:53:19 -0500

    Consolidate affinity resource registration and only require restart of the parent service.

--- undertow/src/main/java/org/wildfly/extension/undertow/filters/AffinityResourceDefinition.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/filters/AffinityResourceDefinition.java
@@ -22,7 +22,11 @@
 
 package org.wildfly.extension.undertow.filters;
 
+import java.util.function.UnaryOperator;
+
 import org.jboss.as.clustering.controller.ChildResourceDefinition;
+import org.jboss.as.clustering.controller.ResourceDescriptor;
+import org.jboss.as.clustering.controller.RestartParentResourceRegistrar;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.wildfly.extension.undertow.Constants;
@@ -40,7 +44,20 @@ public abstract class AffinityResourceDefinition extends ChildResourceDefinition
     }
     static final PathElement WILDCARD_PATH = pathElement(PathElement.WILDCARD_VALUE);
 
-    AffinityResourceDefinition(PathElement path) {
+    private final UnaryOperator<ResourceDescriptor> configurator;
+
+    AffinityResourceDefinition(PathElement path, UnaryOperator<ResourceDescriptor> configurator) {
         super(path, UndertowExtension.getResolver(Constants.FILTER, ModClusterDefinition.PATH_ELEMENT.getKey(), path.getKey(), path.getValue()));
+        this.configurator = configurator;
+    }
+
+    @Override
+    public ManagementResourceRegistration register(ManagementResourceRegistration parent) {
+        ManagementResourceRegistration registration = parent.registerSubModel(this);
+
+        ResourceDescriptor descriptor = this.configurator.apply(new ResourceDescriptor(this.getResourceDescriptionResolver()));
+        new RestartParentResourceRegistrar(ModClusterServiceConfigurator::new, descriptor).register(registration);
+
+        return registration;
     }
 }
