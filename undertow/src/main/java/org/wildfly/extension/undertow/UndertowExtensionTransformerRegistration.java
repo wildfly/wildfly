c6d9ba95b5b21891c5f47278702a823331333a5a
Paul Ferraro <paul.ferraro@redhat.com>
2023-01-12 17:34:26 -0500

    Add model transformation for EAP 7.4.

---
+++ undertow/src/main/java/org/wildfly/extension/undertow/UndertowExtensionTransformerRegistration.java
@@ -0,0 +1,66 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.wildfly.extension.undertow;
+
+import java.util.EnumSet;
+
+import org.jboss.as.controller.ModelVersion;
+import org.jboss.as.controller.transform.ExtensionTransformerRegistration;
+import org.jboss.as.controller.transform.SubsystemTransformerRegistration;
+import org.jboss.as.controller.transform.description.DiscardAttributeChecker;
+import org.jboss.as.controller.transform.description.RejectAttributeChecker;
+import org.jboss.as.controller.transform.description.ResourceTransformationDescriptionBuilder;
+import org.jboss.as.controller.transform.description.TransformationDescription;
+import org.jboss.as.controller.transform.description.TransformationDescriptionBuilder;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * Registers transformers for the Undertow subsystem.
+ * @author Paul Ferraro
+ */
+@MetaInfServices
+public class UndertowExtensionTransformerRegistration implements ExtensionTransformerRegistration {
+
+    @Override
+    public String getSubsystemName() {
+        return UndertowExtension.SUBSYSTEM_NAME;
+    }
+
+    @Override
+    public void registerTransformers(SubsystemTransformerRegistration registration) {
+        for (UndertowModel model : EnumSet.complementOf(EnumSet.of(UndertowModel.CURRENT))) {
+            ModelVersion version = model.getVersion();
+            ResourceTransformationDescriptionBuilder subsystem = TransformationDescriptionBuilder.Factory.createSubsystemInstance();
+
+            ResourceTransformationDescriptionBuilder servletContainer = subsystem.addChildResource(ServletContainerDefinition.PATH_ELEMENT);
+            if (UndertowModel.VERSION_13_0_0.requiresTransformation(version)) {
+                servletContainer.getAttributeBuilder()
+                    .setDiscard(DiscardAttributeChecker.UNDEFINED, ServletContainerDefinition.ORPHAN_SESSION_ALLOWED)
+                    .addRejectCheck(RejectAttributeChecker.DEFINED, ServletContainerDefinition.ORPHAN_SESSION_ALLOWED)
+                    .end();
+            }
+
+            TransformationDescription.Tools.register(subsystem.build(), registration, version);
+        }
+    }
+}
