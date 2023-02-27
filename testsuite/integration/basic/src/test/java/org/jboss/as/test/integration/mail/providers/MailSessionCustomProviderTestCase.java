023113a1fa2aff49d388e894f10ae339fa085e5c
Yeray Borges <yborgess@redhat.com>
2023-02-27 11:14:34 +0000

    [WFLY-17470] Fix Mail Session cannot be injected when using CDI method producer Jira issue: https://issues.redhat.com/browse/WFLY-17470

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/mail/providers/MailSessionCustomProviderTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/mail/providers/MailSessionCustomProviderTestCase.java
@@ -22,9 +22,8 @@
 
 package org.jboss.as.test.integration.mail.providers;
 
-import jakarta.annotation.Resource;
-import jakarta.mail.Provider;
-import jakarta.mail.Session;
+import java.util.List;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -34,35 +33,54 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import jakarta.annotation.Resource;
+import jakarta.inject.Inject;
+import jakarta.mail.Provider;
+import jakarta.mail.Session;
+
 @RunWith(Arquillian.class)
 public class MailSessionCustomProviderTestCase {
 
     @Resource(lookup = "java:jboss/mail/Default")
-    private Session session;
+    private Session sessionOne;
+
+    @Inject
+    private Session sessionTwo;
+
+    @Inject
+    @MethodInjectQualifier
+    private Session sessionThree;
 
     @Deployment
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class)
+                .addClasses(MailSessionProducer.class, MethodInjectQualifier.class)
                 .addAsResource(MailSessionCustomProviderTestCase.class.getPackage(),"javamail.providers", "META-INF/javamail.providers")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @Test
     public void testCustomProvidersInApplication() {
-        Assert.assertNotNull(session);
+        Assert.assertNotNull(sessionOne);
+        Assert.assertNotNull(sessionTwo);
+        Assert.assertNotNull(sessionThree);
 
-        Provider[] providers = session.getProviders();
+        List<Session> sessions = List.of(sessionOne, sessionTwo, sessionThree);
 
-        int found = 0;
-        for (Provider p : providers) {
-            if (p.toString().equals("jakarta.mail.Provider[TRANSPORT,CustomTransport,org.jboss.qa.management.mail.custom.CustomTransport,JBoss QE]")) {
-                found++;
+        for(Session session : sessions) {
+            Provider[] providers = session.getProviders();
+
+            int found = 0;
+            for (Provider p : providers) {
+                if (p.toString().equals("jakarta.mail.Provider[TRANSPORT,CustomTransport,org.jboss.qa.management.mail.custom.CustomTransport,JBoss QE]")) {
+                    found++;
+                }
+                if (p.toString().equals("jakarta.mail.Provider[STORE,CustomStore,org.jboss.qa.management.mail.custom.CustomStore,JBoss QE]")) {
+                    found++;
+                }
             }
-            if (p.toString().equals("jakarta.mail.Provider[STORE,CustomStore,org.jboss.qa.management.mail.custom.CustomStore,JBoss QE]")) {
-                found++;
-            }
+
+            Assert.assertTrue("The expected custom providers cannot be found on the default Mail Session", found == 2);
         }
-
-        Assert.assertTrue("The expected custom providers cannot be found on the default Mail Session", found == 2);
     }
 }
