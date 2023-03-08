3686dfc4eef9dd41e52498bfdb280b657b21f332
Paul Ferraro <paul.ferraro@redhat.com>
2023-03-08 15:59:41 -0500

    Implement SubsystemModel in OpenTelemetry subsystem.

--- observability/opentelemetry/src/main/java/org/wildfly/extension/opentelemetry/OpenTelemetrySubsystemModel.java
+++ observability/opentelemetry/src/main/java/org/wildfly/extension/opentelemetry/OpenTelemetrySubsystemModel.java
@@ -1,18 +1,20 @@
 package org.wildfly.extension.opentelemetry;
 
 import org.jboss.as.controller.ModelVersion;
+import org.jboss.as.controller.SubsystemModel;
 
-public enum OpenTelemetryModel {
+public enum OpenTelemetrySubsystemModel implements SubsystemModel {
     VERSION_1_0_0(1, 0, 0);
 
-    public static final OpenTelemetryModel CURRENT = VERSION_1_0_0;
+    public static final OpenTelemetrySubsystemModel CURRENT = VERSION_1_0_0;
 
     private final ModelVersion version;
 
-    OpenTelemetryModel(int major, int minor, int micro) {
+    OpenTelemetrySubsystemModel(int major, int minor, int micro) {
         this.version = ModelVersion.create(major, minor, micro);
     }
 
+    @Override
     public ModelVersion getVersion() {
         return version;
     }
