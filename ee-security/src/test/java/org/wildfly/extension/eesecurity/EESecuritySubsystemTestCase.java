e9152befade47e31fd0f555d8f82f6096e4934b1
Paul Ferraro <paul.ferraro@redhat.com>
2023-03-08 17:41:36 -0500

    WFLY-17708 Migrate ee-security subsystem to PersistentSubsystemSchema.

--- ee-security/src/test/java/org/wildfly/extension/eesecurity/EESecuritySubsystemTestCase.java
+++ ee-security/src/test/java/org/wildfly/extension/eesecurity/EESecuritySubsystemTestCase.java
@@ -22,26 +22,39 @@
 package org.wildfly.extension.eesecurity;
 
 import java.io.IOException;
+import java.util.EnumSet;
+import java.util.Locale;
 
 import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
  */
+@RunWith(Parameterized.class)
 public class EESecuritySubsystemTestCase extends AbstractSubsystemBaseTest {
+    @Parameters
+    public static Iterable<EESecuritySubsystemSchema> parameters() {
+        return EnumSet.allOf(EESecuritySubsystemSchema.class);
+    }
 
-    public EESecuritySubsystemTestCase() {
+    private final EESecuritySubsystemSchema schema;
+
+    public EESecuritySubsystemTestCase(EESecuritySubsystemSchema schema) {
         super(EESecurityExtension.SUBSYSTEM_NAME, new EESecurityExtension());
+        this.schema = schema;
     }
 
     @Override
     protected String getSubsystemXml() throws IOException {
-        return "<subsystem xmlns=\"urn:jboss:domain:ee-security:1.0\"/>";
+        return String.format(Locale.ROOT, "<subsystem xmlns=\"%s\"/>", this.schema.getNamespace());
     }
 
     @Override
     protected String getSubsystemXsdPath() throws Exception {
-        return "schema/wildfly-ee-security_1_0.xsd";
+        return String.format(Locale.ROOT, "schema/wildfly-ee-security_%d_%d.xsd", this.schema.getVersion().major(), this.schema.getVersion().minor());
     }
 
     //no point in testing 1.0.0 (current) --> 1.0.0 (all previous) for transformers
