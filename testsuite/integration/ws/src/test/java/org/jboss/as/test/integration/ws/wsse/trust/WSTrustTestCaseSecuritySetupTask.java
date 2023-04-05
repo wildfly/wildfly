a9c6dfc392ec1f4f737d71d9a2baddfc62415c6f
R Searls <rsearls@redhat.com>
2023-04-05 09:45:10 -0400

    [WFLY-17844] remove code that configures JBossWS-trust-sts in obsolete subsystem security

--- testsuite/integration/ws/src/test/java/org/jboss/as/test/integration/ws/wsse/trust/WSTrustTestCaseSecuritySetupTask.java
+++ testsuite/integration/ws/src/test/java/org/jboss/as/test/integration/ws/wsse/trust/WSTrustTestCaseSecuritySetupTask.java
@@ -11,27 +11,19 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SSL
 import static org.jboss.as.test.integration.management.util.ModelUtil.createOpNode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.jboss.as.arquillian.api.ServerSetupTask;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.as.controller.operations.common.Util;
-import org.jboss.as.test.integration.security.common.AbstractSecurityDomainsServerSetupTask;
 import org.jboss.as.test.integration.security.common.CoreUtils;
-import org.jboss.as.test.integration.security.common.config.SecurityDomain;
-import org.jboss.as.test.integration.security.common.config.SecurityModule;
 import org.jboss.as.test.shared.ServerReload;
 import org.jboss.dmr.ModelNode;
 
 
 public class WSTrustTestCaseSecuritySetupTask implements ServerSetupTask {
 
-    public static final String SECURITY_DOMAIN_NAME = "JBossWS-trust-sts";
     public static final String HTTPS_LISTENER_NAME = "jbws-test-https-listener";
-
-    private WSTrustSecurityDomainSetupTask securityDomainsSubtask = new WSTrustSecurityDomainSetupTask();
 
     @Override
     public void setup(ManagementClient managementClient, String containerId) throws Exception {
@@ -41,12 +33,10 @@ public class WSTrustTestCaseSecuritySetupTask implements ServerSetupTask {
         ModelNode updateOp = Util.createCompositeOperation(operations);
         updateOp.get(OPERATION_HEADERS, ALLOW_RESOURCE_SERVICE_RESTART).set(true);
         CoreUtils.applyUpdate(updateOp, managementClient.getControllerClient());
-        securityDomainsSubtask.setup(managementClient, containerId);
     }
 
     @Override
     public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
-        securityDomainsSubtask.tearDown(managementClient, containerId);
         List<ModelNode> operations = new ArrayList<>();
         removeHttpsListener(operations);
         removeSSLContext(operations);
@@ -120,31 +110,6 @@ public class WSTrustTestCaseSecuritySetupTask implements ServerSetupTask {
         operations.add(removeOp);
         removeOp = createOpNode("subsystem=undertow/server=default-server/https-listener=" + HTTPS_LISTENER_NAME, REMOVE);
         operations.add(removeOp);
-    }
-
-    class WSTrustSecurityDomainSetupTask extends AbstractSecurityDomainsServerSetupTask {
-
-        @Override
-        protected SecurityDomain[] getSecurityDomains() throws Exception {
-
-            Map<String, String> loginOptions = new HashMap<>();
-            loginOptions.put("usersProperties", WSTrustTestCaseSecuritySetupTask.class.getResource("WEB-INF/jbossws-users.properties").getPath());
-            loginOptions.put("rolesProperties", WSTrustTestCaseSecuritySetupTask.class.getResource("WEB-INF/jbossws-roles.properties").getPath());
-            loginOptions.put("unauthenticatedIdentity", "anonymous");
-
-            SecurityModule loginModule = new SecurityModule.Builder()
-                    .name("UsersRoles")
-                    .flag("required")
-                    .options(loginOptions)
-                    .build();
-
-            SecurityDomain securityDomain = new SecurityDomain.Builder()
-                    .name(SECURITY_DOMAIN_NAME)
-                    .cacheType("default")
-                    .loginModules(loginModule)
-                    .build();
-            return new SecurityDomain[]{securityDomain};
-        }
     }
 
 }
