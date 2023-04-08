54aa170755ffd4a1f9dccefcd8d04e996ed31315
Richard Opálka <opalka.richard@gmail.com>
2023-04-08 13:44:20 +0200

    [WFLY-17931] Eliminating deprecated javax.api module usages

--- testsuite/layers/src/test/java/org/jboss/as/test/layers/LayersTestCase.java
+++ testsuite/layers/src/test/java/org/jboss/as/test/layers/LayersTestCase.java
@@ -40,6 +40,7 @@ public class LayersTestCase {
     private static final String[] NOT_USED_COMMON = {
             // not used
             "ibm.jdk",
+            "javax.api",
             "javax.xml.stream.api",
             // test-all-layers installation is non-ha and does not include layers that provide jgroups
             "org.jboss.as.clustering.jgroups",
