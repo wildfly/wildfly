b0a864f4deb39c459cdbb422cb05ef2b7f0cdd83
Scott Marlow <smarlow@redhat.com>
2023-04-14 09:43:51 -0400

    WFLY-17830 enable org.jboss.as.test.integration.jpa.hibernate.sessionfactorytest.SessionFactoryTestCase testInjectPUIntoHibernateSessionFactory testw
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hibernate/sessionfactorytest/SessionFactoryTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/hibernate/sessionfactorytest/SessionFactoryTestCase.java
@@ -42,7 +42,6 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -125,7 +124,6 @@ public class SessionFactoryTestCase {
 
     // Test that a Persistence unit can be injected into a Hibernate Session factory
     @Test
-    @Ignore // WFLY-17830: renable when upgrading to Hibernate ORM 6.2.1.Final which should have the https://github.com/hibernate/hibernate-orm/pull/6350 change included
     public void testInjectPUIntoHibernateSessionFactory() throws Exception {
 
         SFSBHibernateSessionFactory sfsbHibernateSessionFactory =
