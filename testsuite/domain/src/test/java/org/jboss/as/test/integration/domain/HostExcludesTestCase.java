7254c6e43ca1d5a7303f8e7ac62a0d33f1333e37
Yeray Borges <yborgess@redhat.com>
2023-04-20 11:11:23 +0100

    [WFLY-17920] Move HostExcludesTestCase test case to the next iteration release Jira issue: https://issues.redhat.com/browse/WFLY-17920

--- testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/HostExcludesTestCase.java
+++ testsuite/domain/src/test/java/org/jboss/as/test/integration/domain/HostExcludesTestCase.java
@@ -90,7 +90,7 @@ public class HostExcludesTestCase extends BuildConfigurationTestBase {
     private final boolean isFullDistribution = AssumeTestGroupUtil.isFullDistribution();
     private final boolean isPreviewGalleonPack = AssumeTestGroupUtil.isWildFlyPreview();
 
-    private static final String MAJOR = "28.";
+    private static final String MAJOR = "29.";
 
     /**
      * Maintains the list of expected extensions for each host-exclude name for previous releases.
@@ -204,16 +204,17 @@ public class HostExcludesTestCase extends BuildConfigurationTestBase {
                 "org.wildfly.extension.clustering.ejb",
                 "org.wildfly.extension.datasources-agroal"
         ), true),
+        WILDFLY_28_0("WildFly28.0", WILDFLY_27_0, Arrays.asList(
+                "org.wildfly.extension.micrometer",
+                "org.wildfly.extension.microprofile.lra-coordinator",
+                "org.wildfly.extension.microprofile.lra-participant",
+                "org.wildfly.extension.microprofile.telemetry"
+        ),true),
         // If an extension is added to this enum, also check if it is supplied only by wildfly-galleon-pack. If so, add it also
         // to the internal mpExtensions Set defined on this class.
         // Don't add here extensions supplied only by the wildfly-preview-feature-pack because we are not tracking different releases
         // of wildfly preview. In such a case, add them to previewExtensions set defined below.
-        CURRENT(MAJOR, WILDFLY_27_0, Arrays.asList(
-            "org.wildfly.extension.micrometer",
-            "org.wildfly.extension.microprofile.lra-coordinator",
-            "org.wildfly.extension.microprofile.lra-participant",
-            "org.wildfly.extension.microprofile.telemetry"
-        ), getCurrentRemovedExtensions(), true);
+        CURRENT(MAJOR, WILDFLY_28_0, null, getCurrentRemovedExtensions(), true);
 
         private static List<String> getCurrentRemovedExtensions() {
             // TODO If we decide to remove these modules from WFP, uncomment this.
