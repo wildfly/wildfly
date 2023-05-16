1943b8d1efe3127af2a5f85d303079a649dd3b00
Ivo Studensky <istudens@redhat.com>
2023-05-16 15:38:26 +0200

    [WFLY-17349] cloned org.jboss.as.test.integration.jpa.hibernate.entity entities into seperate entities for webtxem tests

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/webtxem/entity/WebTxEmCompany.java
@@ -0,0 +1,81 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.jpa.webtxem.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Company entity class
+ *
+ * @author Zbyněk Roubalík
+ */
+@Entity
+public class WebTxEmCompany {
+
+    private Long id;
+    private String name;
+    private Set<WebTxEmFlight> flights = new HashSet<WebTxEmFlight>();
+
+    public WebTxEmCompany() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comp_id")
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long newId) {
+        id = newId;
+    }
+
+    public void setName(String string) {
+        name = string;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "company")
+    @Column(name = "flight_id")
+    public Set<WebTxEmFlight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(Set<WebTxEmFlight> flights) {
+        this.flights = flights;
+    }
+
+}
