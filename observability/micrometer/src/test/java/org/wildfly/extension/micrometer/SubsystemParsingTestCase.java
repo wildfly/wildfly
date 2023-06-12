ea110640f2d64b9f23152f2c8069f95b20b5b836
Paul Ferraro <paul.ferraro@redhat.com>
2023-06-12 09:40:09 -0400

    WFLY-18107 Migrate Micrometer subsystem test to use AbstractSubsystemSchemaTest from wildfly-core

--- observability/micrometer/src/test/java/org/wildfly/extension/micrometer/SubsystemParsingTestCase.java
+++ observability/micrometer/src/test/java/org/wildfly/extension/micrometer/SubsystemParsingTestCase.java
@@ -1,10 +1,8 @@
 package org.wildfly.extension.micrometer;
 
-import java.io.IOException;
 import java.util.EnumSet;
-import java.util.Locale;
 
-import org.jboss.as.subsystem.test.AbstractSubsystemBaseTest;
+import org.jboss.as.subsystem.test.AbstractSubsystemSchemaTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -14,39 +12,18 @@ import org.junit.runners.Parameterized.Parameters;
  * @author <a href="jasondlee@redhat.com">Jason Lee</a>
  */
 @RunWith(Parameterized.class)
-public class SubsystemParsingTestCase extends AbstractSubsystemBaseTest {
+public class SubsystemParsingTestCase extends AbstractSubsystemSchemaTest<MicrometerSubsystemSchema> {
     @Parameters
     public static Iterable<MicrometerSubsystemSchema> parameters() {
         return EnumSet.allOf(MicrometerSubsystemSchema.class);
     }
 
-    private final MicrometerSubsystemSchema schema;
-
     public SubsystemParsingTestCase(MicrometerSubsystemSchema schema) {
-        super(MicrometerExtension.SUBSYSTEM_NAME, new MicrometerExtension());
-        this.schema = schema;
+        super(MicrometerExtension.SUBSYSTEM_NAME, new MicrometerExtension(), schema, MicrometerSubsystemSchema.CURRENT);
     }
 
     @Override
-    protected String getSubsystemXml() throws IOException {
-        return readResource(
-            String.format(Locale.ROOT, "micrometer_%d_%d.xml",
-                    this.schema.getVersion().major(),
-                    this.schema.getVersion().minor())
-        );
-    }
-
-    @Override
-    protected String getSubsystemXsdPath() {
-        return String.format(Locale.ROOT, "schema/wildfly-micrometer_%d_%d.xsd",
-                this.schema.getVersion().major(),
-                this.schema.getVersion().minor());
-    }
-
-    @Override
-    protected void compareXml(String configId, final String original, final String marshalled) throws Exception {
-        if (this.schema == MicrometerSubsystemSchema.CURRENT) {
-            super.compareXml(configId, original, marshalled);
-        }
+    protected String getSubsystemXmlPathPattern() {
+        return "%s_%d_%d.xml";
     }
 }
