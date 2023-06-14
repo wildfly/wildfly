72b0954bab56f09e9a32c5bb650157e074b8fe56
Richard Opálka <opalka.richard@gmail.com>
2023-06-14 21:23:53 +0200

    [WFLY-18158] Oracle JDBC Driver provided as deployment needs jdk.security.jgss module dependency to be configured

--- connector/src/main/java/org/jboss/as/connector/deployers/ds/DsDeploymentActivator.java
+++ connector/src/main/java/org/jboss/as/connector/deployers/ds/DsDeploymentActivator.java
@@ -26,6 +26,7 @@ import org.jboss.as.connector.deployers.datasource.DataSourceDefinitionAnnotatio
 import org.jboss.as.connector.deployers.datasource.DataSourceDefinitionDescriptorProcessor;
 import org.jboss.as.connector.deployers.ds.processors.DsXmlDeploymentInstallProcessor;
 import org.jboss.as.connector.deployers.ds.processors.DsXmlDeploymentParsingProcessor;
+import org.jboss.as.connector.deployers.ds.processors.JdbcDriverDeploymentProcessor;
 import org.jboss.as.connector.subsystems.datasources.DataSourcesExtension;
 import org.jboss.as.server.DeploymentProcessorTarget;
 import org.jboss.as.server.deployment.Phase;
@@ -41,7 +42,10 @@ public class DsDeploymentActivator {
     public void activateProcessors(final DeploymentProcessorTarget updateContext) {
         updateContext.addDeploymentProcessor(DataSourcesExtension.SUBSYSTEM_NAME, Phase.PARSE, Phase.PARSE_DSXML_DEPLOYMENT, new DsXmlDeploymentParsingProcessor());
         updateContext.addDeploymentProcessor(DataSourcesExtension.SUBSYSTEM_NAME, Phase.PARSE, Phase.PARSE_RESOURCE_DEF_ANNOTATION_DATA_SOURCE, new DataSourceDefinitionAnnotationProcessor());
+        // TODO: Replace below 'DEPENDENCIES_RAR_CONFIG + 1' with 'DEPENDENCIES_JDBC_DRIVER' once WFCORE-6408 fix is available via WildFly Core upgrade
+        updateContext.addDeploymentProcessor(DataSourcesExtension.SUBSYSTEM_NAME, Phase.DEPENDENCIES, Phase.DEPENDENCIES_RAR_CONFIG + 1, new JdbcDriverDeploymentProcessor());
         updateContext.addDeploymentProcessor(DataSourcesExtension.SUBSYSTEM_NAME, Phase.POST_MODULE, Phase.POST_MODULE_RESOURCE_DEF_XML_DATA_SOURCE, new DataSourceDefinitionDescriptorProcessor());
         updateContext.addDeploymentProcessor(DataSourcesExtension.SUBSYSTEM_NAME, Phase.FIRST_MODULE_USE, Phase.FIRST_MODULE_USE_DSXML_DEPLOYMENT, new DsXmlDeploymentInstallProcessor());
     }
+
 }
