465c784a728230f20666dd172efd0cdd3aeaf46f
Paul Ferraro <paul.ferraro@redhat.com>
2023-09-09 15:48:16 -0400

    Add missing javadoc.

--- clustering/ejb/spi/src/main/java/org/wildfly/clustering/ejb/timer/ImmutableTimerMetaData.java
+++ clustering/ejb/spi/src/main/java/org/wildfly/clustering/ejb/timer/ImmutableTimerMetaData.java
@@ -15,12 +15,47 @@ import java.util.function.Predicate;
  */
 public interface ImmutableTimerMetaData {
 
+    /**
+     * Returns the type of this timer
+     * @return the timer type
+     */
     TimerType getType();
+
+    /**
+     * Returns the context with which this timer was created.
+     * @return the timer context
+     */
     Object getContext();
+
+    /**
+     * Returns the timeout matcher, or null if the timer has no associated timeout matcher
+     * @return an optional timeout matcher
+     */
     Predicate<Method> getTimeoutMatcher();
+
+    /**
+     * Indicates whether or not this timer is persistent.
+     * @return true, if this is a persistent timer, false otherwise
+     */
     boolean isPersistent();
+
+    /**
+     * The configuration of this timer
+     * @param <C> the timer configuration type
+     * @param configurationClass the configuration class
+     * @return the timer configuration
+     */
     <C extends TimerConfiguration> C getConfiguration(Class<C> configurationClass);
 
+    /**
+     * Returns the time of the next timeout event, or null if there are no future timeout events.
+     * @return the optional time of the next timeout event
+     */
     Instant getNextTimeout();
+
+    /**
+     * Returns the time of the most recent timeout event of this timer, or null if there are no previous timeout events
+     * @return the optional time of the last timeout event
+     */
     Instant getLastTimout();
 }
