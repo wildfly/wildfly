5d99db0005291a69659bcf6ecd8f9c15c6b59fcb
Paul Ferraro <paul.ferraro@redhat.com>
2023-09-29 16:15:03 -0400

    Drop deprecated functions and reindex marshallers.

--- clustering/ee/cache/src/main/java/org/wildfly/clustering/ee/cache/function/FunctionSerializationContextInitializer.java
+++ clustering/ee/cache/src/main/java/org/wildfly/clustering/ee/cache/function/FunctionSerializationContextInitializer.java
@@ -5,16 +5,10 @@
 
 package org.wildfly.clustering.ee.cache.function;
 
-import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.Map;
-
 import org.infinispan.protostream.SerializationContext;
 import org.infinispan.protostream.SerializationContextInitializer;
 import org.kohsuke.MetaInfServices;
 import org.wildfly.clustering.marshalling.protostream.AbstractSerializationContextInitializer;
-import org.wildfly.clustering.marshalling.protostream.FunctionalMarshaller;
-import org.wildfly.clustering.marshalling.protostream.FunctionalScalarMarshaller;
-import org.wildfly.clustering.marshalling.protostream.Scalar;
 
 /**
  * @author Paul Ferraro
@@ -22,20 +16,10 @@ import org.wildfly.clustering.marshalling.protostream.Scalar;
 @MetaInfServices(SerializationContextInitializer.class)
 public class FunctionSerializationContextInitializer extends AbstractSerializationContextInitializer {
 
-    @SuppressWarnings("unchecked")
     @Override
     public void registerMarshallers(SerializationContext context) {
         context.registerMarshaller(new MapComputeFunctionMarshaller());
         context.registerMarshaller(new CollectionFunctionMarshaller<>(SetAddFunction.class, SetAddFunction::new));
         context.registerMarshaller(new CollectionFunctionMarshaller<>(SetRemoveFunction.class, SetRemoveFunction::new));
-        // Deprecated functions, to be removed
-        context.registerMarshaller(new FunctionalMarshaller<>(ConcurrentMapPutFunction.class, (Class<Map.Entry<Object, Object>>) (Class<?>) SimpleImmutableEntry.class, ConcurrentMapPutFunction<Object, Object>::getEntry, ConcurrentMapPutFunction<Object, Object>::new));
-        context.registerMarshaller(new FunctionalScalarMarshaller<>(ConcurrentMapRemoveFunction.class, Scalar.ANY, ConcurrentMapRemoveFunction::getKey, ConcurrentMapRemoveFunction::new));
-        context.registerMarshaller(new FunctionalScalarMarshaller<>(ConcurrentSetAddFunction.class, Scalar.ANY, ConcurrentSetAddFunction::getValue, ConcurrentSetAddFunction::new));
-        context.registerMarshaller(new FunctionalScalarMarshaller<>(ConcurrentSetRemoveFunction.class, Scalar.ANY, ConcurrentSetRemoveFunction::getValue, ConcurrentSetRemoveFunction::new));
-        context.registerMarshaller(new FunctionalMarshaller<>(CopyOnWriteMapPutFunction.class, (Class<Map.Entry<Object, Object>>) (Class<?>) SimpleImmutableEntry.class, CopyOnWriteMapPutFunction<Object, Object>::getEntry, CopyOnWriteMapPutFunction<Object, Object>::new));
-        context.registerMarshaller(new FunctionalScalarMarshaller<>(CopyOnWriteMapRemoveFunction.class, Scalar.ANY, CopyOnWriteMapRemoveFunction::getKey, CopyOnWriteMapRemoveFunction::new));
-        context.registerMarshaller(new FunctionalScalarMarshaller<>(CopyOnWriteSetAddFunction.class, Scalar.ANY, CopyOnWriteSetAddFunction::getValue, CopyOnWriteSetAddFunction::new));
-        context.registerMarshaller(new FunctionalScalarMarshaller<>(CopyOnWriteSetRemoveFunction.class, Scalar.ANY, CopyOnWriteSetRemoveFunction::getValue, CopyOnWriteSetRemoveFunction::new));
     }
 }
--- clustering/server/infinispan/src/main/java/org/wildfly/clustering/server/infinispan/provider/ServiceProviderRegistrySerializationContextInitializer.java
+++ clustering/server/infinispan/src/main/java/org/wildfly/clustering/server/infinispan/provider/ServiceProviderRegistrySerializationContextInitializer.java
@@ -6,12 +6,7 @@
 package org.wildfly.clustering.server.infinispan.provider;
 
 import org.infinispan.protostream.SerializationContext;
-import org.infinispan.remoting.transport.Address;
 import org.wildfly.clustering.marshalling.protostream.AbstractSerializationContextInitializer;
-import org.wildfly.clustering.marshalling.protostream.FunctionalMarshaller;
-import org.wildfly.clustering.marshalling.protostream.ProtoStreamMarshaller;
-import org.wildfly.clustering.marshalling.protostream.SimpleFieldSetMarshaller;
-import org.wildfly.clustering.server.infinispan.group.InfinispanAddressMarshaller;
 
 /**
  * {@link org.infinispan.protostream.SerializationContextInitializer} for this package.
@@ -23,11 +18,5 @@ public class ServiceProviderRegistrySerializationContextInitializer extends Abst
     public void registerMarshallers(SerializationContext context) {
         context.registerMarshaller(new AddressSetFunctionMarshaller<>(AddressSetAddFunction.class, AddressSetAddFunction::new));
         context.registerMarshaller(new AddressSetFunctionMarshaller<>(AddressSetRemoveFunction.class, AddressSetRemoveFunction::new));
-        // Deprecated functions to be removed
-        ProtoStreamMarshaller<Address> addressMarshaller = new SimpleFieldSetMarshaller<>(Address.class, InfinispanAddressMarshaller.INSTANCE);
-        context.registerMarshaller(new FunctionalMarshaller<>(ConcurrentAddressSetAddFunction.class, addressMarshaller, ConcurrentAddressSetAddFunction::getValue, ConcurrentAddressSetAddFunction::new));
-        context.registerMarshaller(new FunctionalMarshaller<>(ConcurrentAddressSetRemoveFunction.class, addressMarshaller, ConcurrentAddressSetRemoveFunction::getValue, ConcurrentAddressSetRemoveFunction::new));
-        context.registerMarshaller(new FunctionalMarshaller<>(CopyOnWriteAddressSetAddFunction.class, addressMarshaller, CopyOnWriteAddressSetAddFunction::getValue, CopyOnWriteAddressSetAddFunction::new));
-        context.registerMarshaller(new FunctionalMarshaller<>(CopyOnWriteAddressSetRemoveFunction.class, addressMarshaller, CopyOnWriteAddressSetRemoveFunction::getValue, CopyOnWriteAddressSetRemoveFunction::new));
     }
 }
