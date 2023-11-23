77d44e7ba6b60182ac76e57d2e53e5fe74d59bc2
amehenda <amehenda@amehenda.pnq.csb>
2023-11-24 00:09:55 +0530

    [WFLY-15746] WildFly Application Development Guide

---
+++ docs/src/main/asciidoc/_app-dev-guide/assemblies/modules/fragments/sources/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package org.jboss.as.quickstarts.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/HelloWorld")                      //<1>
+public class HelloWorldServlet extends HttpServlet {
+
+    static String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
+
+    static String PAGE_FOOTER = "</body></html>";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println(PAGE_HEADER);
+        writer.println("<h1> Hello World! </h1>");
+        writer.println(PAGE_FOOTER);
+        writer.close();
+    }
+}
