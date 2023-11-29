1ecbc2cbaeb728a7dff83ace97f8aafdb2981738
Farah Juma <fjuma@redhat.com>
2023-11-29 16:15:45 -0500

    [WFLY-17784] Add tests for identity propagation with MP-JWT

---
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/microprofile/jwt/propagation/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package to contain a test case and required classes to test MP-JWT identity
+ * propagation using a virtual-security-domain.
+ */
+package org.wildfly.test.integration.microprofile.jwt.propagation;
