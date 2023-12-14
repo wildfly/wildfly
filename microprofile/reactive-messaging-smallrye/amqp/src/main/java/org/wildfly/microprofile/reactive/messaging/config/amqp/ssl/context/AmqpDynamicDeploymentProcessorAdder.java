2bb00dd357cab3d476ece5f674b75585d81d24c7
Kabir Khan <kkhan@redhat.com>
2023-12-14 11:09:28 +0000

    [WFLY-15405] Force property to turn off Google DNS as a fallback
    Google DNS causes problems looking up services in Kubernetes

--- microprofile/reactive-messaging-smallrye/amqp/src/main/java/org/wildfly/microprofile/reactive/messaging/config/amqp/ssl/context/AmqpDynamicDeploymentProcessorAdder.java
+++ microprofile/reactive-messaging-smallrye/amqp/src/main/java/org/wildfly/microprofile/reactive/messaging/config/amqp/ssl/context/AmqpDynamicDeploymentProcessorAdder.java
@@ -13,10 +13,18 @@ import org.wildfly.microprofile.reactive.messaging.common.DynamicDeploymentProce
  * @author <a href="mailto:kabir.khan@jboss.com">Kabir Khan</a>
  */
 public class AmqpDynamicDeploymentProcessorAdder implements DynamicDeploymentProcessorAdder {
+
+    private static final String VERTX_DISABLE_DNS_RESOLVER = "vertx.disableDnsResolver";
+
     @Override
     public void addDeploymentProcessor(DeploymentProcessorTarget target, String subsystemName) {
 
         final int POST_MODULE_MICROPROFILE_REACTIVE_MESSAGING = 0x3828;
+
+        // We need to disable vertx's DNS resolver as it causes failures under k8s
+        if (System.getProperty(VERTX_DISABLE_DNS_RESOLVER) == null) {
+            System.setProperty(VERTX_DISABLE_DNS_RESOLVER, "true");
+        }
 
         target.addDeploymentProcessor(subsystemName,
                 Phase.POST_MODULE,
