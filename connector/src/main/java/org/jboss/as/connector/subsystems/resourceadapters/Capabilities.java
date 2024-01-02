4362a3e20044762694ffeadbbf206d09e33c8e20
Tomasz Adamski <tadamski@redhat.com>
2024-01-03 00:11:45 +0100

    [WFLY-18882] Add resource adapter subsystem dependencies based on capability

---
+++ connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/Capabilities.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.as.connector.subsystems.resourceadapters;
+
+public interface Capabilities {
+    public static final String RESOURCE_ADAPTERS_SUBSYSTEM_CAPABILITY_NAME = "org.wildfly.resource-adapters";
+}
--- connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/ResourceAdaptersRootResourceDefinition.java
+++ connector/src/main/java/org/jboss/as/connector/subsystems/resourceadapters/ResourceAdaptersRootResourceDefinition.java
@@ -8,6 +8,7 @@ import static org.jboss.as.connector.subsystems.resourceadapters.ResourceAdapter
 
 import org.jboss.as.controller.ReloadRequiredRemoveStepHandler;
 import org.jboss.as.controller.SimpleResourceDefinition;
+import org.jboss.as.controller.capability.RuntimeCapability;
 import org.jboss.as.controller.operations.common.GenericSubsystemDescribeHandler;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.as.controller.registry.RuntimePackageDependency;
@@ -18,6 +19,8 @@ import org.jboss.as.controller.registry.RuntimePackageDependency;
  */
 public class ResourceAdaptersRootResourceDefinition extends SimpleResourceDefinition {
 
+    static final RuntimeCapability<Void> RESOURCE_ADAPTERS_SUBSYSTEM = RuntimeCapability.Builder.of(Capabilities.RESOURCE_ADAPTERS_SUBSYSTEM_CAPABILITY_NAME).build();
+
     private final boolean runtimeOnlyRegistrationValid;
 
     @Override
@@ -26,7 +29,11 @@ public class ResourceAdaptersRootResourceDefinition extends SimpleResourceDefini
     }
 
     public ResourceAdaptersRootResourceDefinition(boolean runtimeOnlyRegistrationValid) {
-        super(ResourceAdaptersExtension.SUBSYSTEM_PATH, ResourceAdaptersExtension.getResourceDescriptionResolver(SUBSYSTEM_NAME), ResourceAdaptersSubsystemAdd.INSTANCE, ReloadRequiredRemoveStepHandler.INSTANCE);
+        super(new Parameters(ResourceAdaptersExtension.SUBSYSTEM_PATH,
+                ResourceAdaptersExtension.getResourceDescriptionResolver(SUBSYSTEM_NAME))
+                .setAddHandler(ResourceAdaptersSubsystemAdd.INSTANCE)
+                .setRemoveHandler(ReloadRequiredRemoveStepHandler.INSTANCE)
+                .setCapabilities(RESOURCE_ADAPTERS_SUBSYSTEM));
         this.runtimeOnlyRegistrationValid = runtimeOnlyRegistrationValid;
     }
 
