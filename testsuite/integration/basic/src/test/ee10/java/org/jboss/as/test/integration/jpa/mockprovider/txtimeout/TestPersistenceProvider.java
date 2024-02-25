f822e1fcce2b1aea6717f832f421b1cb9e2316d1
Brian Stansberry <brian.stansberry@redhat.com>
2024-02-25 11:18:28 -0600

    [WFLY-19064] Update the EE 11 preview to use current EE 11 releases

---
+++ testsuite/integration/basic/src/test/ee10/java/org/jboss/as/test/integration/jpa/mockprovider/txtimeout/TestPersistenceProvider.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.jboss.as.test.integration.jpa.mockprovider.txtimeout;
+
+/**
+ * EE 10 variant of AbstractTestPersistenceProvider.
+ *c
+ * @author Brian Stansberry
+ */
+public class TestPersistenceProvider extends AbstractTestPersistenceProvider {
+}
