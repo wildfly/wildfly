e2516302bc522d2bf122ae9cd0ed2e95a08bc8d4
Brian Stansberry <brian.stansberry@redhat.com>
2024-03-29 19:42:25 -0500

    [WFLY-19184] Move @ManagedBean smoke tests to the ee10-only src folder

--- testsuite/integration/smoke/src/test/ee10/java/org/jboss/as/test/smoke/managedbean/CDIBinding.java
+++ testsuite/integration/smoke/src/test/ee10/java/org/jboss/as/test/smoke/managedbean/CDIBinding.java
@@ -8,8 +8,10 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import jakarta.interceptor.InterceptorBinding;
@@ -17,8 +19,8 @@ import jakarta.interceptor.InterceptorBinding;
 /**
  * @author Jozef Hartinger
  */
-@Target({ TYPE, METHOD })
-@Retention(RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @InterceptorBinding
 public @interface CDIBinding {
