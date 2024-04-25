1ce90e9dc97e8cfecbfbd9d4a14b8ac0409ef5a8
James R. Perkins <jperkins@redhat.com>
2024-04-25 14:57:14 -0700

    [WFLY-19257] Use different module names for tests that create modules. This is required until WFLY-19284 is resolved.
    https://issues.redhat.com/browse/WFLY-19257
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/singleton/dependson/session/SetupModuleServerSetupTask.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/singleton/dependson/session/SetupModuleServerSetupTask.java
@@ -8,21 +8,27 @@ package org.jboss.as.test.integration.ejb.singleton.dependson.session;
 import org.jboss.as.arquillian.api.ServerSetupTask;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.as.test.integration.ejb.singleton.dependson.mdb.CallCounterInterface;
-import org.jboss.as.test.module.util.TestModule;
+import org.jboss.as.test.module.util.ModuleBuilder;
 import org.jboss.as.test.shared.ModuleUtils;
 
 /**
  * @author baranowb
  */
-public class SetupModuleServerSetupTask implements ServerSetupTask {
+public abstract class SetupModuleServerSetupTask implements ServerSetupTask {
 
-    private volatile TestModule testModule;
+    private final String moduleName;
+    private volatile Runnable testModule;
+
+    protected SetupModuleServerSetupTask(final String moduleName) {
+        this.moduleName = moduleName;
+    }
 
     @Override
     public void setup(ManagementClient arg0, String arg1) throws Exception {
-        testModule = ModuleUtils.createTestModuleWithEEDependencies(SessionConstants.TEST_MODULE_NAME);
-        testModule.addResource("module.jar").addClasses(CallCounterInterface.class, Trigger.class);
-        testModule.create();
+        testModule = ModuleBuilder.of(moduleName)
+                .addClasses(CallCounterInterface.class, Trigger.class)
+                .addDependencies(ModuleUtils.EE_DEPENDENCIES)
+                .build();
 
     }
 
@@ -30,7 +36,7 @@ public class SetupModuleServerSetupTask implements ServerSetupTask {
     @Override
     public void tearDown(ManagementClient arg0, String arg1) throws Exception {
         if (testModule != null) {
-            testModule.remove();
+            testModule.run();
         }
     }
 }
--- testsuite/integration/secman/src/test/java/org/jboss/as/testsuite/integration/secman/custompermissions/GrantCustomPermissionModuleTestCase.java
+++ testsuite/integration/secman/src/test/java/org/jboss/as/testsuite/integration/secman/custompermissions/GrantCustomPermissionModuleTestCase.java
@@ -5,12 +5,15 @@
 
 package org.jboss.as.testsuite.integration.secman.custompermissions;
 
+import java.util.Set;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.runner.RunWith;
+import org.wildfly.testing.tools.deployments.DeploymentDescriptors;
 
 /**
  * Test case which checks if permissions.xml which contains CustomPermission
@@ -22,8 +25,13 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 @ServerSetup(GrantCustomPermissionModuleTestCase.CustomPermissionModuleServerSetupTask.class)
 public class GrantCustomPermissionModuleTestCase extends AbstractGrantCustomPermissionTestCase {
+    private static final String MODULE_NAME = "org.jboss.test.deployment.permission";
 
     private static final String DEP_PERMISSIONS_XML_NAME = "DEP_PERMISSIONS_XML.war";
+
+    public GrantCustomPermissionModuleTestCase() {
+        super(MODULE_NAME);
+    }
 
     /**
      * Creates test web archive.
@@ -35,10 +43,13 @@ public class GrantCustomPermissionModuleTestCase extends AbstractGrantCustomPerm
         return ShrinkWrap
                 .create(WebArchive.class, DEP_PERMISSIONS_XML_NAME)
                 .addClasses(AbstractGrantCustomPermissionTestCase.class, AbstractCustomPermissionServerSetup.class)
-                .addAsManifestResource(GrantCustomPermissionModuleTestCase.class.getResource("permissions.xml"), "permissions.xml")
-                .addAsManifestResource(Utils.getJBossDeploymentStructure("org.jboss.test"), "jboss-deployment-structure.xml");
+                .addAsManifestResource(DeploymentDescriptors.createPermissionsXmlAsset(new CustomPermission(MODULE_NAME)), "permissions.xml")
+                .addAsManifestResource(DeploymentDescriptors.createJBossDeploymentStructureAsset(Set.of(MODULE_NAME), Set.of()), "jboss-deployment-structure.xml");
     }
 
     static class CustomPermissionModuleServerSetupTask extends AbstractCustomPermissionServerSetup {
+        CustomPermissionModuleServerSetupTask() {
+            super(MODULE_NAME);
+        }
     }
 }
