395acec8c4383eb0fcf565bddb66dc62de148780
Paul Ferraro <paul.ferraro@redhat.com>
2024-04-26 12:31:33 -0400

    Deprecate Requirement based methods in favor of ServiceDescriptor.

--- clustering/common/src/test/java/org/jboss/as/clustering/subsystem/AdditionalInitialization.java
+++ clustering/common/src/test/java/org/jboss/as/clustering/subsystem/AdditionalInitialization.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.jboss.as.controller.RunningMode;
+import org.jboss.as.controller.capability.RuntimeCapability;
 import org.jboss.as.controller.capability.registry.RuntimeCapabilityRegistry;
 import org.jboss.as.controller.extension.ExtensionRegistry;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
@@ -18,6 +19,9 @@ import org.jboss.as.controller.registry.Resource;
 import org.wildfly.clustering.service.BinaryRequirement;
 import org.wildfly.clustering.service.Requirement;
 import org.wildfly.clustering.service.UnaryRequirement;
+import org.wildfly.service.descriptor.BinaryServiceDescriptor;
+import org.wildfly.service.descriptor.NullaryServiceDescriptor;
+import org.wildfly.service.descriptor.UnaryServiceDescriptor;
 
 /**
  * {@link AdditionalInitialization} extension that simplifies setup of required capabilities.
@@ -52,18 +56,36 @@ public class AdditionalInitialization extends org.jboss.as.subsystem.test.Additi
         return this;
     }
 
+    @Deprecated(forRemoval = true)
     public AdditionalInitialization require(Requirement requirement) {
         this.requirements.add(requirement.getName());
         return this;
     }
 
+    @Deprecated(forRemoval = true)
     public AdditionalInitialization require(UnaryRequirement requirement, String... names) {
         Stream.of(names).forEach(name -> this.requirements.add(requirement.resolve(name)));
         return this;
     }
 
+    @Deprecated(forRemoval = true)
     public AdditionalInitialization require(BinaryRequirement requirement, String parent, String child) {
         this.requirements.add(requirement.resolve(parent, child));
+        return this;
+    }
+
+    public AdditionalInitialization require(NullaryServiceDescriptor<?> requirement) {
+        this.requirements.add(requirement.getName());
+        return this;
+    }
+
+    public AdditionalInitialization require(UnaryServiceDescriptor<?> requirement, String name) {
+        this.requirements.add(RuntimeCapability.resolveCapabilityName(requirement, name));
+        return this;
+    }
+
+    public AdditionalInitialization require(BinaryServiceDescriptor<?> requirement, String parent, String child) {
+        this.requirements.add(RuntimeCapability.resolveCapabilityName(requirement, parent, child));
         return this;
     }
 }
