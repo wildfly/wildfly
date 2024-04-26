8af55ef150a772b55a9348dd32ca7d24aa26fbeb
Jason Lee <jason@steeplesoft.com>
2024-04-25 21:53:30 -0500

    [WFLY-19283] Migrate Micrometer extension to new wildfly-subsystem approach
    Migrate classes
    Modify module deps

--- observability/micrometer/src/main/java/org/wildfly/extension/micrometer/MicrometerExtension.java
+++ observability/micrometer/src/main/java/org/wildfly/extension/micrometer/MicrometerExtension.java
@@ -4,51 +4,15 @@
  */
 package org.wildfly.extension.micrometer;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import org.wildfly.subsystem.SubsystemConfiguration;
+import org.wildfly.subsystem.SubsystemExtension;
+import org.wildfly.subsystem.SubsystemPersistence;
 
-import java.util.EnumSet;
-import java.util.List;
-
-import org.jboss.as.controller.Extension;
-import org.jboss.as.controller.ExtensionContext;
-import org.jboss.as.controller.PathElement;
-import org.jboss.as.controller.PersistentResourceXMLDescription;
-import org.jboss.as.controller.PersistentResourceXMLDescriptionReader;
-import org.jboss.as.controller.PersistentResourceXMLDescriptionWriter;
-import org.jboss.as.controller.SubsystemRegistration;
-import org.jboss.as.controller.descriptions.ParentResourceDescriptionResolver;
-import org.jboss.as.controller.descriptions.SubsystemResourceDescriptionResolver;
-import org.jboss.as.controller.operations.common.GenericSubsystemDescribeHandler;
-import org.jboss.as.controller.parsing.ExtensionParsingContext;
-import org.jboss.as.controller.registry.ManagementResourceRegistration;
-import org.jboss.dmr.ModelNode;
-import org.jboss.staxmapper.XMLElementReader;
-
-public class MicrometerExtension implements Extension {
-    public static final String WELD_CAPABILITY_NAME = "org.wildfly.weld";
-    public static final String SUBSYSTEM_NAME = "micrometer";
-    public static final PathElement SUBSYSTEM_PATH = PathElement.pathElement(SUBSYSTEM, SUBSYSTEM_NAME);
-
-    static final ParentResourceDescriptionResolver SUBSYSTEM_RESOLVER = new SubsystemResourceDescriptionResolver(SUBSYSTEM_NAME,
-            MicrometerExtension.class);
-
-    private final PersistentResourceXMLDescription currentDescription = MicrometerSubsystemSchema.CURRENT.getXMLDescription();
-
-    @Override
-    public void initialize(ExtensionContext context) {
-        final SubsystemRegistration subsystem = context.registerSubsystem(SUBSYSTEM_NAME, MicrometerSubsystemModel.CURRENT.getVersion());
-        subsystem.registerXMLElementWriter(new PersistentResourceXMLDescriptionWriter(this.currentDescription));
-
-        final ManagementResourceRegistration registration = subsystem.registerSubsystemModel(new MicrometerSubsystemDefinition());
-        registration.registerOperationHandler(GenericSubsystemDescribeHandler.DEFINITION, GenericSubsystemDescribeHandler.INSTANCE);
-    }
-
-    @Override
-    public void initializeParsers(ExtensionParsingContext context) {
-        for (MicrometerSubsystemSchema schema : EnumSet.allOf(MicrometerSubsystemSchema.class)) {
-            XMLElementReader<List<ModelNode>> reader = (schema == MicrometerSubsystemSchema.CURRENT) ?
-                    new PersistentResourceXMLDescriptionReader(this.currentDescription) : schema;
-            context.setSubsystemXmlMapping(SUBSYSTEM_NAME, schema.getNamespace().getUri(), reader);
-        }
+public class MicrometerExtension extends SubsystemExtension<MicrometerSubsystemSchema> {
+    public MicrometerExtension() {
+        super(SubsystemConfiguration.of(MicrometerConfigurationConstants.NAME,
+                        MicrometerSubsystemModel.CURRENT,
+                        MicrometerSubsystemRegistrar::new),
+                SubsystemPersistence.of(MicrometerSubsystemSchema.CURRENT));
     }
 }
