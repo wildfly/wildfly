2bc4e35ff6e5fa2428b5c952d16f4f4d7725e001
Paul Ferraro <paul.ferraro@redhat.com>
2024-05-07 12:40:21 -0400

    WFLY-19317 Remove references to AbstractControllerService.EXECUTOR_CAPABILITY from metrics subsystem.

--- metrics/src/main/java/org/wildfly/extension/metrics/deployment/DeploymentMetricService.java
+++ metrics/src/main/java/org/wildfly/extension/metrics/deployment/DeploymentMetricService.java
@@ -11,25 +11,24 @@ import static org.wildfly.extension.metrics.MetricsSubsystemDefinition.METRICS_R
 import static org.wildfly.extension.metrics.MetricsSubsystemDefinition.WILDFLY_COLLECTOR;
 
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.RejectedExecutionException;
 import java.util.function.Supplier;
 
 import org.jboss.as.controller.PathAddress;
+import org.jboss.as.controller.RequirementServiceTarget;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.as.controller.registry.Resource;
-import org.jboss.as.server.ServerService;
 import org.jboss.as.server.deployment.Attachments;
 import org.jboss.as.server.deployment.DeploymentCompleteServiceProcessor;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.msc.Service;
 import org.jboss.msc.service.ServiceBuilder;
-import org.jboss.msc.service.ServiceTarget;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StopContext;
 import org.wildfly.extension.metrics.MetricCollector;
 import org.wildfly.extension.metrics.MetricRegistration;
 import org.wildfly.extension.metrics.MetricRegistry;
+import org.wildfly.service.AsyncServiceBuilder.Async;
+import org.wildfly.subsystem.service.AsyncServiceBuilder;
 
 public class DeploymentMetricService implements Service {
 
@@ -39,39 +38,36 @@ public class DeploymentMetricService implements Service {
     private PathAddress deploymentAddress;
     private final Supplier<MetricCollector> metricCollector;
     private Supplier<MetricRegistry> metricRegistry;
-    private Supplier<Executor> managementExecutor;
     private final boolean exposeAnySubsystem;
     private final List<String> exposedSubsystems;
     private final String prefix;
     private MetricRegistration registration;
 
-    public static void install(ServiceTarget serviceTarget, DeploymentUnit deploymentUnit, Resource rootResource, ManagementResourceRegistration managementResourceRegistration, boolean exposeAnySubsystem, List<String> exposedSubsystems, String prefix) {
+    public static void install(RequirementServiceTarget serviceTarget, DeploymentUnit deploymentUnit, Resource rootResource, ManagementResourceRegistration managementResourceRegistration, boolean exposeAnySubsystem, List<String> exposedSubsystems, String prefix) {
         PathAddress deploymentAddress = createDeploymentAddressPrefix(deploymentUnit);
 
-        ServiceBuilder<?> sb = serviceTarget.addService(deploymentUnit.getServiceName().append("metrics"));
+        ServiceBuilder<?> sb = new AsyncServiceBuilder<>(serviceTarget.addService(), Async.START_ONLY);
         Supplier<MetricCollector> metricCollector = sb.requires(WILDFLY_COLLECTOR);
         Supplier<MetricRegistry> metricRegistry = sb.requires(METRICS_REGISTRY_RUNTIME_CAPABILITY.getCapabilityServiceName());
-        Supplier<Executor> managementExecutor = sb.requires(ServerService.EXECUTOR_CAPABILITY.getCapabilityServiceName());
 
         /*
          * The deployment metric service depends on the deployment complete service name to ensure that the metrics from
          * the deployment are collected and registered once the deployment services have all been properly installed.
          */
         sb.requires(DeploymentCompleteServiceProcessor.serviceName(deploymentUnit.getServiceName()));
-        sb.setInstance(new DeploymentMetricService(rootResource, managementResourceRegistration, deploymentAddress, metricCollector, metricRegistry, managementExecutor,
+        sb.setInstance(new DeploymentMetricService(rootResource, managementResourceRegistration, deploymentAddress, metricCollector, metricRegistry,
                 exposeAnySubsystem, exposedSubsystems, prefix))
                 .install();
     }
 
     private DeploymentMetricService(Resource rootResource, ManagementResourceRegistration managementResourceRegistration, PathAddress deploymentAddress,
                                     Supplier<MetricCollector> metricCollector, Supplier<MetricRegistry> metricRegistry,
-                                    Supplier<Executor> managementExecutor, boolean exposeAnySubsystem, List<String> exposedSubsystems, String prefix) {
+                                    boolean exposeAnySubsystem, List<String> exposedSubsystems, String prefix) {
         this.rootResource = rootResource;
         this.managementResourceRegistration = managementResourceRegistration;
         this.deploymentAddress = deploymentAddress;
         this.metricCollector = metricCollector;
         this.metricRegistry = metricRegistry;
-        this.managementExecutor = managementExecutor;
         this.exposeAnySubsystem = exposeAnySubsystem;
         this.exposedSubsystems = exposedSubsystems;
         this.prefix = prefix;
@@ -79,26 +75,13 @@ public class DeploymentMetricService implements Service {
 
     @Override
     public void start(StartContext startContext) {
-        final Runnable task = new Runnable() {
-            @Override
-            public void run() {
-                registration = new MetricRegistration(metricRegistry.get());
-                metricCollector.get().collectResourceMetrics(rootResource,
-                        managementResourceRegistration,
-                        // prepend the deployment address to the subsystem resource address
-                        address -> deploymentAddress.append(address),
-                        exposeAnySubsystem, exposedSubsystems, prefix,
-                        registration);
-                startContext.complete();
-            }
-        };
-        try {
-            managementExecutor.get().execute(task);
-        } catch (RejectedExecutionException e) {
-            task.run();
-        } finally {
-            startContext.asynchronous();
-        }
+        registration = new MetricRegistration(metricRegistry.get());
+        metricCollector.get().collectResourceMetrics(rootResource,
+                managementResourceRegistration,
+                // prepend the deployment address to the subsystem resource address
+                address -> deploymentAddress.append(address),
+                exposeAnySubsystem, exposedSubsystems, prefix,
+                registration);
     }
 
     @Override
