6ca2671d8dd90ea15b8f1e791df5c33af02878be
Jason Lee <jason@steeplesoft.com>
2024-05-22 13:50:47 -0500

    [WFLY-18582-new] Add prometheus endpoint to micrometer extension
    Add prometheus support
    Set stability level to PREVIEW
    Add tests for Prometheus context
    Modify test to account for stability level
    Update documentation

--- observability/micrometer/src/main/java/org/wildfly/extension/micrometer/registry/WildFlyCompositeRegistry.java
+++ observability/micrometer/src/main/java/org/wildfly/extension/micrometer/registry/WildFlyCompositeRegistry.java
@@ -8,14 +8,11 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 
 public class WildFlyCompositeRegistry extends CompositeMeterRegistry implements WildFlyRegistry {
-//    private final List<WildFlyRegistry> registries = new ArrayList<>();
-
     public WildFlyCompositeRegistry() {
         super();
     }
 
     public void addRegistry(WildFlyRegistry registry) {
         this.add((MeterRegistry) registry);
-//        this.registries.add(registry);
     }
 }
