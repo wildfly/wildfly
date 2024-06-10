62275b1cca63864b5b05b720b52a16923b5fb7ae
Paul Ferraro <paul.ferraro@redhat.com>
2024-06-10 14:10:53 -0400

    Validate both legacy and current RegistryFactory API usage.

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/registry/bean/legacy/LegacyRegistryRetrieverBean.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/registry/bean/legacy/LegacyRegistryRetrieverBean.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.jboss.as.test.clustering.cluster.registry.bean;
+package org.jboss.as.test.clustering.cluster.registry.bean.legacy;
 
 import java.util.Collection;
 import java.util.TreeSet;
@@ -12,14 +12,14 @@ import jakarta.ejb.EJB;
 import jakarta.ejb.Remote;
 import jakarta.ejb.Stateless;
 
-import org.wildfly.clustering.server.GroupMember;
-import org.wildfly.clustering.server.registry.Registry;
+import org.jboss.as.test.clustering.cluster.registry.bean.RegistryRetriever;
+import org.wildfly.clustering.registry.Registry;
 
 @Stateless
 @Remote(RegistryRetriever.class)
-public class RegistryRetrieverBean implements RegistryRetriever {
+public class LegacyRegistryRetrieverBean implements RegistryRetriever {
     @EJB
-    private Registry<GroupMember, String, String> registry;
+    private Registry<String, String> registry;
 
     @Override
     public Collection<String> getNodes() {
