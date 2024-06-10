b095b2d14c66d2b4e74f1981d6b7f9b4c1f89a10
Paul Ferraro <paul.ferraro@redhat.com>
2024-06-10 12:59:31 -0400

    Validate both legacy and current ServiceProviderRegistrar API usage.

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/provider/ServiceProviderRegistrationTestCase.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/provider/ServiceProviderRegistrationTestCase.java
@@ -15,6 +15,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.test.clustering.cluster.AbstractClusteringTestCase;
 import org.jboss.as.test.clustering.cluster.provider.bean.ServiceProviderRetriever;
 import org.jboss.as.test.clustering.cluster.provider.bean.ServiceProviderRetrieverBean;
+import org.jboss.as.test.clustering.cluster.provider.bean.legacy.LegacyServiceProviderRetrieverBean;
 import org.jboss.as.test.clustering.ejb.EJBDirectory;
 import org.jboss.as.test.clustering.ejb.RemoteEJBDirectory;
 import org.jboss.as.test.shared.PermissionUtils;
@@ -42,7 +43,8 @@ public class ServiceProviderRegistrationTestCase extends AbstractClusteringTestC
 
     private static Archive<?> createDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class, MODULE_NAME + ".war");
-        war.addPackage(ServiceProviderRetriever.class.getPackage());
+        war.addPackage(ServiceProviderRetrieverBean.class.getPackage());
+        war.addPackage(LegacyServiceProviderRetrieverBean.class.getPackage());
         war.setWebXML(ServiceProviderRegistrationTestCase.class.getPackage(), "web.xml");
         war.addAsManifestResource(PermissionUtils.createPermissionsXmlAsset(new RuntimePermission("getClassLoader")), "permissions.xml");
         return war;
@@ -50,8 +52,17 @@ public class ServiceProviderRegistrationTestCase extends AbstractClusteringTestC
 
     @Test
     public void test() throws Exception {
+        this.test(ServiceProviderRetrieverBean.class);
+    }
+
+    @Test
+    public void legacy() throws Exception {
+        this.test(LegacyServiceProviderRetrieverBean.class);
+    }
+
+    public void test(Class<? extends ServiceProviderRetriever> beanClass) throws Exception {
         try (EJBDirectory directory = new RemoteEJBDirectory(MODULE_NAME)) {
-            ServiceProviderRetriever bean = directory.lookupStateless(ServiceProviderRetrieverBean.class, ServiceProviderRetriever.class);
+            ServiceProviderRetriever bean = directory.lookupStateless(beanClass, ServiceProviderRetriever.class);
 
             Collection<String> names = bean.getProviders();
             assertEquals(2, names.size());
