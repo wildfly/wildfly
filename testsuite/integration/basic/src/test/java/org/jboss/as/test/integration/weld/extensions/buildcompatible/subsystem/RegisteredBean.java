111d2e4ff9d5fefa5a6bcb4ec82335275ebddeae
Brian Stansberry <brian.stansberry@redhat.com>
2024-08-19 15:33:40 -0400

    WFLY-19573 Adding automated test. Move the test module setup into ServerSetupTask. Add mgmt ops to add/remove the test extension and subsystem. Add necessary dependencies to the test.build-compatible-extension module. Fixes for test execution in bootable jar.

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/extensions/buildcompatible/subsystem/RegisteredBean.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.jboss.as.test.integration.weld.extensions.buildcompatible.subsystem;
+
+// no bean defining annotation, processed via RegisteredExtension
+public class RegisteredBean {
+}
