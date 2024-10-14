2e3ead0f4c2f80d20755f8528b510bd5019f2d38
Kabir Khan <kkhan@redhat.com>
2024-10-14 12:56:48 +0100

    Attempt to add transformers

---
+++ microprofile/reactive-messaging-smallrye/extension/src/main/java/org/wildfly/extension/microprofile/reactive/messaging/MicroProfileReactiveMessagingTransformers.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.wildfly.extension.microprofile.reactive.messaging;
+
+import org.jboss.as.controller.ModelVersion;
+import org.jboss.as.controller.transform.ExtensionTransformerRegistration;
+import org.jboss.as.controller.transform.SubsystemTransformerRegistration;
+import org.jboss.as.controller.transform.description.ChainedTransformationDescriptionBuilder;
+import org.jboss.as.controller.transform.description.DiscardAttributeChecker;
+import org.jboss.as.controller.transform.description.RejectAttributeChecker;
+import org.jboss.as.controller.transform.description.ResourceTransformationDescriptionBuilder;
+import org.jboss.as.controller.transform.description.TransformationDescriptionBuilder;
+import org.jboss.dmr.ModelNode;
+
+public class MicroProfileReactiveMessagingTransformers implements ExtensionTransformerRegistration {
+
+    @Override
+    public String getSubsystemName() {
+        return MicroProfileReactiveMessagingExtension.SUBSYSTEM_NAME;
+    }
+
+    @Override
+    public void registerTransformers(SubsystemTransformerRegistration subsystem) {
+
+        ChainedTransformationDescriptionBuilder chainedBuilder = TransformationDescriptionBuilder.Factory
+                .createChainedSubystemInstance(subsystem.getCurrentSubsystemVersion());
+
+        // Differences between the current version (2.0.0) and 1.0.0
+        ResourceTransformationDescriptionBuilder builder_1_0_0 = chainedBuilder.createBuilder(subsystem.getCurrentSubsystemVersion(), MicroProfileReactiveMessagingExtension.VERSION_1_0_0);
+        builder_1_0_0.rejectChildResource(ConnectorOpenTelemetryTracingResourceDefinition.PATH);
+
+        chainedBuilder.buildAndRegister(subsystem, new ModelVersion[]{MicroProfileReactiveMessagingExtension.VERSION_1_0_0});
+    }
+}
