de2f0fe4c9bf76e302d71d19542ffc1ee6b83789
Paul Ferraro <paul.ferraro@redhat.com>
2024-10-19 17:01:50 +0100

    WFLY-19653 Migrate infinispan subsystem to wildfly-subsystem

--- clustering/ejb/infinispan/src/main/java/org/wildfly/clustering/ejb/infinispan/remote/InfinispanClientMappingsRegistryProvider.java
+++ clustering/ejb/infinispan/src/main/java/org/wildfly/clustering/ejb/infinispan/remote/InfinispanClientMappingsRegistryProvider.java
@@ -8,18 +8,17 @@ package org.wildfly.clustering.ejb.infinispan.remote;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.jboss.as.network.ClientMapping;
 import org.wildfly.clustering.ejb.infinispan.network.ClientMappingsRegistryEntryServiceInstallerFactory;
 import org.wildfly.clustering.ejb.remote.ClientMappingsRegistryProvider;
-import org.wildfly.clustering.infinispan.service.CacheServiceInstallerFactory;
-import org.wildfly.clustering.infinispan.service.TemplateConfigurationServiceInstallerFactory;
+import org.wildfly.clustering.infinispan.service.CacheConfigurationServiceInstaller;
+import org.wildfly.clustering.infinispan.service.CacheServiceInstaller;
 import org.wildfly.clustering.server.service.BinaryServiceConfiguration;
 import org.wildfly.clustering.server.service.ClusteringServiceDescriptor;
 import org.wildfly.clustering.server.service.FilteredBinaryServiceInstallerProvider;
-import org.wildfly.common.function.Functions;
 import org.wildfly.subsystem.service.ServiceDependency;
 import org.wildfly.subsystem.service.ServiceInstaller;
 
@@ -32,17 +31,17 @@ import org.wildfly.subsystem.service.ServiceInstaller;
 public class InfinispanClientMappingsRegistryProvider implements ClientMappingsRegistryProvider {
 
     private final BinaryServiceConfiguration configuration;
-    private final Consumer<ConfigurationBuilder> configurator;
+    private final UnaryOperator<ConfigurationBuilder> configurator;
 
     /**
      * Creates an instance of the Infinispan-based client mappings registry provider, for local or distribute use, based on a cache-service abstraction.
      * @param configuration a cache configuration
      */
     public InfinispanClientMappingsRegistryProvider(BinaryServiceConfiguration configuration) {
-        this(configuration, Functions.discardingConsumer());
+        this(configuration, UnaryOperator.identity());
     }
 
-    InfinispanClientMappingsRegistryProvider(BinaryServiceConfiguration configuration, Consumer<ConfigurationBuilder> configurator) {
+    InfinispanClientMappingsRegistryProvider(BinaryServiceConfiguration configuration, UnaryOperator<ConfigurationBuilder> configurator) {
         this.configuration = configuration;
         this.configurator = configurator;
     }
@@ -51,8 +50,8 @@ public class InfinispanClientMappingsRegistryProvider implements ClientMappingsR
     public Iterable<ServiceInstaller> getServiceInstallers(String connectorName, ServiceDependency<List<ClientMapping>> clientMappings) {
         BinaryServiceConfiguration configuration = this.configuration.withChildName(connectorName);
         List<ServiceInstaller> installers = new LinkedList<>();
-        installers.add(new TemplateConfigurationServiceInstallerFactory(this.configurator).apply(this.configuration, configuration));
-        installers.add(CacheServiceInstallerFactory.INSTANCE.apply(configuration));
+        installers.add(new CacheConfigurationServiceInstaller(configuration, CacheConfigurationServiceInstaller.fromTemplate(this.configuration).map(this.configurator)));
+        installers.add(new CacheServiceInstaller(configuration));
         installers.add(new ClientMappingsRegistryEntryServiceInstallerFactory(clientMappings).apply(configuration));
         new FilteredBinaryServiceInstallerProvider(Set.of(ClusteringServiceDescriptor.REGISTRY, ClusteringServiceDescriptor.REGISTRY_FACTORY)).apply(configuration).forEach(installers::add);
         return installers;
