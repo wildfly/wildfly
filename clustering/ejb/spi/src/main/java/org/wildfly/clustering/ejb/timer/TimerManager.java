057560760c77e248dfb995bfe8a613c4ecbdad3b
Paul Ferraro <paul.ferraro@redhat.com>
2024-12-03 13:15:40 +0000

    Reading fixed timer metadata should not require exclusive access. Fixes potential deadlocks when accessing timer meta data via TimerService.getTimers().

--- clustering/ejb/spi/src/main/java/org/wildfly/clustering/ejb/timer/TimerManager.java
+++ clustering/ejb/spi/src/main/java/org/wildfly/clustering/ejb/timer/TimerManager.java
@@ -23,7 +23,22 @@ public interface TimerManager<I> extends Manager<I>, AutoCloseable {
 
     Timer<I> createTimer(I id, ScheduleTimerConfiguration config, Object context, Method method, int index);
 
+    /**
+     * Returns exclusive access to the timer with the specified identifier.
+     * @param id a timer identifier
+     * @return the timer with the specified identifier, or null if no such timer exists
+     */
     Timer<I> getTimer(I id);
+
+    /**
+     * Returns non-exclusive access to the timer with the specified identifier.
+     * This should only be used to read fixed (i.e. non-dynamic) timer meta data, as this would otherwise result in a dirty read.
+     * @param id a timer identifier
+     * @return the timer with the specified identifier, or null if no such timer exists
+     */
+    default Timer<I> readTimer(I id) {
+        return this.getTimer(id);
+    }
 
     Stream<I> getActiveTimers();
 
