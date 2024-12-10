3f259dbedddd7b79d43b58d61cc49b9036c801bc
Jason Lee <jason@steeplesoft.com>
2024-12-09 18:06:24 -0600

    WFLY-20075 - MP Telemetry Metrics (OpenTelemetry) without any rest/servlet call
    * Force eager initialization of OpenTelemetry instance so that metrics
      publication begins on application deployment/start
    * Add test to verify

--- observability/opentelemetry-api/src/main/java/org/wildfly/extension/opentelemetry/api/OpenTelemetryCdiExtension.java
+++ observability/opentelemetry-api/src/main/java/org/wildfly/extension/opentelemetry/api/OpenTelemetryCdiExtension.java
@@ -6,17 +6,19 @@
 package org.wildfly.extension.opentelemetry.api;
 
 import java.util.Map;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.smallrye.opentelemetry.api.OpenTelemetryConfig;
+import io.smallrye.opentelemetry.implementation.rest.OpenTelemetryClientFilter;
+import io.smallrye.opentelemetry.implementation.rest.OpenTelemetryServerFilter;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.Default;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
+import jakarta.enterprise.inject.spi.AfterDeploymentValidation;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
 import jakarta.enterprise.inject.spi.Extension;
 import jakarta.inject.Singleton;
-
-import io.smallrye.opentelemetry.api.OpenTelemetryConfig;
-import io.smallrye.opentelemetry.implementation.rest.OpenTelemetryClientFilter;
-import io.smallrye.opentelemetry.implementation.rest.OpenTelemetryServerFilter;
 
 public final class OpenTelemetryCdiExtension implements Extension {
     private final boolean useServerConfig;
@@ -36,6 +38,14 @@ public final class OpenTelemetryCdiExtension implements Extension {
                 OpenTelemetryServerFilter.class.getName());
         beforeBeanDiscovery.addAnnotatedType(beanManager.createAnnotatedType(OpenTelemetryClientFilter.class),
                 OpenTelemetryClientFilter.class.getName());
+    }
+
+    // For the publication of metrics to begin, the OpenTelemetry instance must be created. Unless something like
+    // this is done, that will not happen until the first request to the application (via the OpenTelemetryServerFilter).
+    // Forcing the creation of that instance here causes that publication to begin as soon as the application is
+    // deployed/started. See https://issues.redhat.com/browse/WFLY-20075
+    void forceEagerInstantiationOfOpenTelemetry(@Observes AfterDeploymentValidation event, BeanManager beanManager) {
+        beanManager.createInstance().select(OpenTelemetry.class).get();
     }
 
     public void registerOpenTelemetryBeans(@Observes AfterBeanDiscovery abd) {
