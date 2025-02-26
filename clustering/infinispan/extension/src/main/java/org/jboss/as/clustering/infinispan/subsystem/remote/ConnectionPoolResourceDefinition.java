0267f0b6c68b2310f657f99c26c7d21b13d40c04
Paul Ferraro <paul.ferraro@redhat.com>
2025-02-26 17:53:50 +0000

    Drop obsolete runtime handling for ConnectionPoolResourceDefinition.

--- clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/remote/ConnectionPoolResourceDefinition.java
+++ clustering/infinispan/extension/src/main/java/org/jboss/as/clustering/infinispan/subsystem/remote/ConnectionPoolResourceDefinition.java
@@ -5,35 +5,25 @@
 
 package org.jboss.as.clustering.infinispan.subsystem.remote;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.client.hotrod.configuration.ConnectionPoolConfiguration;
 import org.infinispan.client.hotrod.configuration.ExhaustedAction;
+import org.jboss.as.clustering.controller.ChildResourceDefinition;
 import org.jboss.as.clustering.controller.ManagementResourceRegistration;
 import org.jboss.as.clustering.controller.ResourceDescriptor;
 import org.jboss.as.clustering.controller.ResourceServiceHandler;
 import org.jboss.as.clustering.controller.SimpleResourceRegistrar;
+import org.jboss.as.clustering.infinispan.subsystem.InfinispanExtension;
 import org.jboss.as.clustering.infinispan.subsystem.InfinispanSubsystemModel;
 import org.jboss.as.controller.AttributeDefinition;
-import org.jboss.as.controller.OperationContext;
-import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
-import org.jboss.as.controller.capability.RuntimeCapability;
-import org.jboss.as.controller.capability.UnaryCapabilityNameResolver;
 import org.jboss.as.controller.client.helpers.MeasurementUnit;
 import org.jboss.as.controller.operations.validation.EnumValidator;
 import org.jboss.as.controller.registry.AttributeAccess;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.ModelType;
-import org.wildfly.service.descriptor.UnaryServiceDescriptor;
-import org.wildfly.subsystem.resource.operation.ResourceOperationRuntimeHandler;
-import org.wildfly.subsystem.service.ResourceServiceInstaller;
-import org.wildfly.subsystem.service.capability.CapabilityServiceInstaller;
 
 /**
  * /subsystem=infinispan/remote-cache-container=X/component=connection-pool
@@ -41,12 +31,9 @@ import org.wildfly.subsystem.service.capability.CapabilityServiceInstaller;
  * @author Radoslav Husar
  */
 @Deprecated(forRemoval = true)
-public class ConnectionPoolResourceDefinition extends ComponentResourceDefinition {
+public class ConnectionPoolResourceDefinition extends ChildResourceDefinition<ManagementResourceRegistration> {
 
-    public static final PathElement PATH = pathElement("connection-pool");
-
-    static final UnaryServiceDescriptor<ConnectionPoolConfiguration> SERVICE_DESCRIPTOR = serviceDescriptor(PATH, ConnectionPoolConfiguration.class);
-    private static final RuntimeCapability<Void> CAPABILITY = RuntimeCapability.Builder.of(SERVICE_DESCRIPTOR).setDynamicNameMapper(UnaryCapabilityNameResolver.PARENT).build();
+    public static final PathElement PATH = ComponentResourceDefinition.pathElement("connection-pool");
 
     public enum Attribute implements org.jboss.as.clustering.controller.Attribute, UnaryOperator<SimpleAttributeDefinitionBuilder> {
         EXHAUSTED_ACTION("exhausted-action", ModelType.STRING, new ModelNode(ExhaustedAction.WAIT.name())) {
@@ -85,7 +72,7 @@ public class ConnectionPoolResourceDefinition extends ComponentResourceDefinitio
     }
 
     ConnectionPoolResourceDefinition() {
-        super(PATH);
+        super(PATH, InfinispanExtension.SUBSYSTEM_RESOLVER.createChildResolver(PATH));
         // No longer used as of Infinispan 15.1
         this.setDeprecated(InfinispanSubsystemModel.VERSION_20_0_0.getVersion());
     }
@@ -96,34 +83,9 @@ public class ConnectionPoolResourceDefinition extends ComponentResourceDefinitio
 
         ResourceDescriptor descriptor = new ResourceDescriptor(this.getResourceDescriptionResolver())
                 .addAttributes(Attribute.class)
-                .addCapabilities(List.of(CAPABILITY))
                 ;
-        ResourceOperationRuntimeHandler handler = ResourceOperationRuntimeHandler.configureService(this);
-        new SimpleResourceRegistrar(descriptor, ResourceServiceHandler.of(handler)).register(registration);
+        new SimpleResourceRegistrar(descriptor, ResourceServiceHandler.of()).register(registration);
 
         return registration;
-    }
-
-    @Override
-    public ResourceServiceInstaller configure(OperationContext context, ModelNode model) throws OperationFailedException {
-        ExhaustedAction exhaustedAction = ExhaustedAction.valueOf(ConnectionPoolResourceDefinition.Attribute.EXHAUSTED_ACTION.resolveModelAttribute(context, model).asString());
-        int maxActive = ConnectionPoolResourceDefinition.Attribute.MAX_ACTIVE.resolveModelAttribute(context, model).asInt(-1);
-        long maxWait = ConnectionPoolResourceDefinition.Attribute.MAX_WAIT.resolveModelAttribute(context, model).asLong(-1L);
-        long minEvictableIdleTime = ConnectionPoolResourceDefinition.Attribute.MIN_EVICTABLE_IDLE_TIME.resolveModelAttribute(context, model).asLong();
-        int minIdle = ConnectionPoolResourceDefinition.Attribute.MIN_IDLE.resolveModelAttribute(context, model).asInt();
-
-        Supplier<ConnectionPoolConfiguration> configurationFactory = new Supplier<>() {
-            @Override
-            public ConnectionPoolConfiguration get() {
-                return new ConfigurationBuilder().connectionPool()
-                        .exhaustedAction(exhaustedAction)
-                        .maxActive(maxActive)
-                        .maxWait(maxWait)
-                        .minEvictableIdleTime(minEvictableIdleTime)
-                        .minIdle(minIdle)
-                        .create();
-            }
-        };
-        return CapabilityServiceInstaller.builder(CAPABILITY, configurationFactory).build();
     }
 }
