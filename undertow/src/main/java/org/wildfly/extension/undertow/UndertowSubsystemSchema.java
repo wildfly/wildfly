b4668d758cb28ebc9455a21bf0c2d3a8d7301150
Paul Ferraro <paul.ferraro@redhat.com>
2025-03-02 13:31:09 +0000

    WFLY-20439 Restrict attributes added via WFLY-15452 & WFLY-14255 to the appropriate schema version and stability.

--- undertow/src/main/java/org/wildfly/extension/undertow/UndertowSubsystemSchema.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/UndertowSubsystemSchema.java
@@ -64,8 +64,8 @@ public enum UndertowSubsystemSchema implements PersistentSubsystemSchema<Underto
     VERSION_11_0(11),   // WildFly 20-22    N.B. There were no parser changes between 10.0 and 11.0 !!
     VERSION_12_0(12),   // WildFly 23-26.1, EAP 7.4
     VERSION_13_0(13),   // WildFly 27       N.B. There were no schema changes between 12.0 and 13.0!
-    VERSION_14_0(14),   // WildFly 28
-    VERSION_14_0_PREVIEW(14, 0, Stability.PREVIEW)   // WildFly 32-present
+    VERSION_14_0(14),   // WildFly 28-32
+    VERSION_14_0_PREVIEW(14, 0, Stability.PREVIEW)   // WildFly 33-present
     ;
 
     static final Map<Stability, UndertowSubsystemSchema> CURRENT = Feature.map(EnumSet.of(VERSION_14_0, VERSION_14_0_PREVIEW));
@@ -145,6 +145,9 @@ public enum UndertowSubsystemSchema implements PersistentSubsystemSchema<Underto
     private PersistentResourceXMLDescription ajpListener() {
         PersistentResourceXMLDescription.Builder builder = this.factory.builder(AjpListenerResourceDefinition.PATH_ELEMENT);
         Stream<AttributeDefinition> attributes = AjpListenerResourceDefinition.ATTRIBUTES.stream();
+        if (!this.since(VERSION_14_0_PREVIEW)) {
+            attributes = attributes.filter(Predicate.isEqual(AjpListenerResourceDefinition.ALLOWED_REQUEST_ATTRIBUTES_PATTERN).negate());
+        }
         Stream.concat(this.listenerAttributes(), attributes).forEach(builder::addAttribute);
         return builder.build();
     }
@@ -270,6 +273,9 @@ public enum UndertowSubsystemSchema implements PersistentSubsystemSchema<Underto
         builder.addChild(this.factory.builder(FileHandlerDefinition.PATH_ELEMENT).addAttributes(FileHandlerDefinition.ATTRIBUTES.stream()).build());
 
         Stream<AttributeDefinition> reverseProxyHandlerAttributes = ReverseProxyHandlerDefinition.ATTRIBUTES.stream();
+        if (!this.since(VERSION_14_0_PREVIEW)) {
+            reverseProxyHandlerAttributes = reverseProxyHandlerAttributes.filter(Predicate.not(Set.of(ReverseProxyHandlerDefinition.REUSE_X_FORWARDED_HEADER, ReverseProxyHandlerDefinition.REWRITE_HOST_HEADER)::contains));
+        }
         if (!this.since(UndertowSubsystemSchema.VERSION_4_0)) {
             reverseProxyHandlerAttributes = reverseProxyHandlerAttributes.filter(Predicate.isEqual(ReverseProxyHandlerDefinition.MAX_RETRIES).negate());
         }
