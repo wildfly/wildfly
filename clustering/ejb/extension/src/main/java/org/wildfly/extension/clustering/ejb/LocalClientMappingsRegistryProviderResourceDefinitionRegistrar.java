741cfd735e0b366c9097d3c7e92a688f9381d2af
Paul Ferraro <paul.ferraro@redhat.com>
2025-03-18 15:51:35 +0000

    WFLY-19485 Migrate distributable-ejb subsystem to wildfly-subsystem.

---
+++ clustering/ejb/extension/src/main/java/org/wildfly/extension/clustering/ejb/LocalClientMappingsRegistryProviderResourceDefinitionRegistrar.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.wildfly.extension.clustering.ejb;
+
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.dmr.ModelNode;
+import org.wildfly.clustering.ejb.remote.ClientMappingsRegistryProvider;
+
+/**
+ * Definition of the /subsystem=distributable-ejb/client-mappings-registry=local resource.
+ *
+ * @author Paul Ferraro
+ * @author Richard Achmatowicz
+ */
+public class LocalClientMappingsRegistryProviderResourceDefinitionRegistrar extends ClientMappingsRegistryProviderResourceDefinitionRegistrar {
+
+    LocalClientMappingsRegistryProviderResourceDefinitionRegistrar() {
+        super(ClientMappingsRegistryProviderResourceRegistration.LOCAL);
+    }
+
+    @Override
+    public ClientMappingsRegistryProvider resolve(OperationContext context, ModelNode model) throws OperationFailedException {
+        return LocalClientMappingsRegistryProvider.INSTANCE;
+    }
+}
