d3b318b3eb97edc3aa83976a57440d62a4288009
Paul Ferraro <paul.ferraro@redhat.com>
2025-04-14 19:33:20 +0100

    HttpInvokerHostService should use affinity cookie, if configured.

--- undertow/src/main/java/org/wildfly/extension/undertow/HttpInvokerHostService.java
+++ undertow/src/main/java/org/wildfly/extension/undertow/HttpInvokerHostService.java
@@ -12,18 +12,23 @@ import java.util.stream.Collectors;
 import io.undertow.security.handlers.AuthenticationCallHandler;
 import io.undertow.security.handlers.AuthenticationConstraintHandler;
 import io.undertow.server.HttpHandler;
-import io.undertow.server.handlers.Cookie;
-import io.undertow.server.handlers.CookieImpl;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.server.session.SecureRandomSessionIdGenerator;
+import io.undertow.server.session.SessionConfig;
+import io.undertow.server.session.SessionCookieConfig;
+import io.undertow.servlet.api.SessionConfigWrapper;
 import io.undertow.util.StatusCodes;
-import org.jboss.as.web.session.SimpleRoutingSupport;
-import org.jboss.as.web.session.SimpleSessionIdentifierCodec;
+
 import org.jboss.msc.Service;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StopContext;
 import org.wildfly.elytron.web.undertow.server.ElytronContextAssociationHandler;
 import org.wildfly.elytron.web.undertow.server.ElytronHttpExchange;
+import org.wildfly.extension.undertow.session.AffinitySessionConfigWrapper;
+import org.wildfly.extension.undertow.session.AffinitySessionIdentifierCodec;
+import org.wildfly.extension.undertow.session.CodecSessionConfigWrapper;
+import org.wildfly.extension.undertow.session.NonDistributableSessionAffinityProvider;
+import org.wildfly.extension.undertow.session.SessionAffinityProvider;
 import org.wildfly.httpclient.common.ElytronIdentityHandler;
 import org.wildfly.security.auth.server.HttpAuthenticationFactory;
 import org.wildfly.security.auth.server.SecurityIdentity;
@@ -34,8 +39,6 @@ import org.wildfly.security.http.HttpServerAuthenticationMechanism;
  * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
 final class HttpInvokerHostService implements Service {
-
-    private static final String JSESSIONID = "JSESSIONID";
 
     private final Supplier<Host> host;
     private final Supplier<HttpAuthenticationFactory> httpAuthenticationFactory;
@@ -59,7 +62,31 @@ final class HttpInvokerHostService implements Service {
         if (httpAuthenticationFactory != null) {
             handler = secureAccess(handler, httpAuthenticationFactory.get());
         }
-        handler = setupRoutes(handler);
+
+        SessionCookieConfig sessionConfig = new SessionCookieConfig();
+        sessionConfig.setPath(this.path);
+        Server server = this.host.get().getServer();
+        ServletContainerService container = server.getServletContainer();
+        CookieConfig affinityCookeConfig = container.getAffinityCookieConfig();
+        CookieConfig sessionCookieConfig = container.getSessionCookieConfig();
+        if (sessionCookieConfig != null) {
+            if (sessionCookieConfig.getDomain() != null) {
+                sessionConfig.setDomain(sessionCookieConfig.getDomain());
+            }
+            if (sessionCookieConfig.getMaxAge() != null) {
+                sessionConfig.setMaxAge(sessionCookieConfig.getMaxAge());
+            }
+            if (sessionCookieConfig.getSecure() != null) {
+                sessionConfig.setSecure(sessionCookieConfig.getSecure());
+            }
+            if (sessionCookieConfig.getHttpOnly() != null) {
+                sessionConfig.setHttpOnly(sessionCookieConfig.getHttpOnly());
+            }
+        }
+        SessionAffinityProvider affinityProvider = new NonDistributableSessionAffinityProvider(server);
+        SessionConfigWrapper wrapper = (affinityCookeConfig != null) ? new AffinitySessionConfigWrapper(affinityCookeConfig, affinityProvider) : new CodecSessionConfigWrapper(new AffinitySessionIdentifierCodec(affinityProvider));
+
+        handler = setupRoutes(handler, wrapper.wrap(sessionConfig, null));
         host.get().registerHandler(path, handler);
         host.get().registerLocation(path);
     }
@@ -70,19 +97,16 @@ final class HttpInvokerHostService implements Service {
         host.get().unregisterLocation(path);
     }
 
-    private HttpHandler setupRoutes(HttpHandler handler) {
-        final SimpleSessionIdentifierCodec codec = new SimpleSessionIdentifierCodec(new SimpleRoutingSupport(), this.host.get().getServer().getRoute());
+    private HttpHandler setupRoutes(HttpHandler handler, SessionConfig sessionConfig) {
         final SecureRandomSessionIdGenerator generator = new SecureRandomSessionIdGenerator();
         return exchange -> {
             exchange.addResponseCommitListener(ex -> {
-                Cookie cookie = ex.getResponseCookies().get(JSESSIONID);
-                if (cookie != null ) {
-                    cookie.setValue(codec.encode(cookie.getValue()).toString());
+                String id = sessionConfig.findSessionId(exchange);
+                if (id != null) {
+                    sessionConfig.setSessionId(exchange, id);
                 } else if (ex.getStatusCode() == StatusCodes.UNAUTHORIZED) {
                     // add a session cookie in order to avoid sticky session issue after 401 Unauthorized response
-                    cookie = new CookieImpl("JSESSIONID", codec.encode(generator.createSessionId()).toString());
-                    cookie.setPath(ex.getResolvedPath());
-                    exchange.getResponseCookies().put("JSESSIONID", cookie);
+                    sessionConfig.setSessionId(exchange, generator.createSessionId());
                 }
             });
             handler.handleRequest(exchange);
