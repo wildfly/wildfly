bb9f377bf065cbc9f3c4b2fe05ff90f36957b0a0
Richard Opálka <opalka.richard@gmail.com>
2025-05-16 23:20:26 +0200

    [WFLY-18182] Refactoring UserTransactionRegistryService to use new JBoss MSC values API

--- transactions/src/main/java/org/jboss/as/txn/service/UserTransactionRegistryService.java
+++ transactions/src/main/java/org/jboss/as/txn/service/UserTransactionRegistryService.java
@@ -5,33 +5,43 @@
 
 package org.jboss.as.txn.service;
 
-import org.jboss.msc.service.Service;
+import org.jboss.msc.Service;
+import org.jboss.msc.service.ServiceBuilder;
+import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceTarget;
 import org.jboss.msc.service.StartContext;
 import org.jboss.msc.service.StartException;
 import org.jboss.msc.service.StopContext;
 import org.jboss.tm.usertx.UserTransactionRegistry;
 
+import java.util.function.Consumer;
+
 /**
  * Service responsible for exposing a {@link UserTransactionRegistry} instance.
  *
  * @author John Bailey
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
-public class UserTransactionRegistryService implements Service<UserTransactionRegistry> {
-
+public class UserTransactionRegistryService implements Service {
     public static final ServiceName SERVICE_NAME = TxnServices.JBOSS_TXN_USER_TRANSACTION_REGISTRY;
+    private final Consumer<UserTransactionRegistry> userTxnRegistryConsumer;
 
-    private UserTransactionRegistry userTransactionRegistry;
-
-    public synchronized void start(StartContext context) throws StartException {
-        userTransactionRegistry = new UserTransactionRegistry();
+    private UserTransactionRegistryService(final Consumer<UserTransactionRegistry> userTxnRegistryConsumer) {
+        this.userTxnRegistryConsumer = userTxnRegistryConsumer;
     }
 
-    public synchronized void stop(StopContext context) {
-        userTransactionRegistry = null;
+    public void start(final StartContext context) throws StartException {
+        userTxnRegistryConsumer.accept(new UserTransactionRegistry());
     }
 
-    public synchronized UserTransactionRegistry getValue() throws IllegalStateException, IllegalArgumentException {
-        return userTransactionRegistry;
+    public void stop(final StopContext context) {
+        userTxnRegistryConsumer.accept(null);
+    }
+
+    public static ServiceController<?> addService(final ServiceTarget target) {
+        final ServiceBuilder<?> userTxnServiceSB = target.addService();
+        final Consumer<UserTransactionRegistry> userTxnRegistryConsumer = userTxnServiceSB.provides(TxnServices.JBOSS_TXN_USER_TRANSACTION_REGISTRY);
+        return userTxnServiceSB.setInstance(new UserTransactionRegistryService(userTxnRegistryConsumer)).install();
     }
 }
