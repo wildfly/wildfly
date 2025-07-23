c747d9184c1daecce3b7db5c5bae0f70618f0cb6
Scott Marlow <smarlow@redhat.com>
2025-07-23 13:34:11 -0400

    [WFLY-19554] Update test bean from usertransaction to instead mark test method with @jakarta.transaction.Transactional.
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/cdi/RequestScopedTestBean.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/jpa/cdi/RequestScopedTestBean.java
@@ -5,7 +5,6 @@
 
 package org.jboss.as.test.integration.jpa.cdi;
 
-import jakarta.annotation.Resource;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.Cache;
@@ -15,7 +14,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.metamodel.Metamodel;
 import jakarta.persistence.PersistenceUnitUtil;
-import jakarta.transaction.UserTransaction;
+import jakarta.transaction.Transactional;
 
 /**
  * test bean
@@ -25,8 +24,6 @@ import jakarta.transaction.UserTransaction;
 
 @RequestScoped
 public class RequestScopedTestBean {
-    @Resource
-    private UserTransaction transaction;
 
     @Inject
     @Pu1Qualifier
@@ -53,31 +50,14 @@ public class RequestScopedTestBean {
     Metamodel metamodel;
 
 
+    @Transactional
     public Employee getEmployeeExpectNullResult(int id) {
-
-        try {
-            transaction.begin();
-            return em.find(Employee.class, id);
-        } catch( Throwable throwable) {
-            throw new RuntimeException(throwable);
-        } finally {
-            try {
-                transaction.rollback();
-            } catch (Throwable ignore) {}
-        }
+        return em.find(Employee.class, id);
     }
 
+    @Transactional
     public EntityManagerFactory entityManagerFactoryOfEntityManager() {
-        try {
-            transaction.begin();
-            return em.getEntityManagerFactory();
-        } catch( Throwable throwable) {
-            throw new RuntimeException(throwable);
-        } finally {
-            try {
-                transaction.rollback();
-            } catch (Throwable ignore) {}
-        }
+        return em.getEntityManagerFactory();
     }
 
     public EntityManagerFactory injectedEntityManagerFactory() {
