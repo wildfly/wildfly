2634600113f38532ab6106f07baeeb95b4cf1967
Radoslav Husar <radosoft@gmail.com>
2025-08-11 11:48:29 +0200

    WFLY-20837 EJB3 subsystem doesn't have any transformer/reject tests - add tests for all supported EAP versions
    Signed-off-by: Radoslav Husar <radosoft@gmail.com>

--- ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3Model.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3Model.java
@@ -18,14 +18,14 @@ public enum EJB3Model implements SubsystemModel {
     VERSION_1_2_0(1, 2, 0),
     VERSION_1_2_1(1, 2, 1), // EAP 6.4.0
     VERSION_1_3_0(1, 3, 0), // EAP 6.4.7
-    VERSION_3_0_0(3, 0, 0), //
+    VERSION_3_0_0(3, 0, 0),
     VERSION_4_0_0(4, 0, 0), // EAP 7.0.0
-    VERSION_5_0_0(5, 0, 0), // EAP 7.2.0, EAP 7.1.0
+    VERSION_5_0_0(5, 0, 0), // EAP 7.1 - 7.2
     VERSION_6_0_0(6, 0, 0),
     VERSION_7_0_0(7, 0, 0),
     VERSION_8_0_0(8, 0, 0),
-    VERSION_9_0_0(9, 0, 0),
-    VERSION_10_0_0(10, 0, 0),
+    VERSION_9_0_0(9, 0, 0), // EAP 7.3 - 7.4
+    VERSION_10_0_0(10, 0, 0), // EAP 8.0 - 8.1
     ;
 
     static final EJB3Model CURRENT = VERSION_10_0_0;
