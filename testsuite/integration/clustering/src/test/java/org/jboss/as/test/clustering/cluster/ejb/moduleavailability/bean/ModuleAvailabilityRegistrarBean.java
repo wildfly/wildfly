88b5d6c7f442f93cb616e2c690288d8d02e38ce3
Richard Achmatowicz <rachmato@redhat.com>
2025-08-27 11:53:21 -0400

    Debugging commit

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/ejb/moduleavailability/bean/ModuleAvailabilityRegistrarBean.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/ejb/moduleavailability/bean/ModuleAvailabilityRegistrarBean.java
@@ -4,18 +4,18 @@
  */
 package org.jboss.as.test.clustering.cluster.ejb.moduleavailability.bean;
 
-import java.util.Set;
-
 import jakarta.annotation.Resource;
 import jakarta.ejb.Local;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
-
+import org.jboss.logging.Logger;
 import org.wildfly.clustering.server.Group;
 import org.wildfly.clustering.server.GroupMember;
+import org.wildfly.clustering.server.provider.ServiceProviderListener;
 import org.wildfly.clustering.server.provider.ServiceProviderRegistrar;
 import org.wildfly.clustering.server.provider.ServiceProviderRegistration;
-import org.wildfly.clustering.server.provider.ServiceProviderListener;
+
+import java.util.Set;
 
 /**
  * An EJB wrapper for the ServiceProviderRegistrar instance used by the ModuleAvailabilityListenerService which manages
@@ -26,6 +26,8 @@ import org.wildfly.clustering.server.provider.ServiceProviderListener;
 @Local(ServiceProviderRegistrar.class)
 public class ModuleAvailabilityRegistrarBean implements ServiceProviderRegistrar<Object, GroupMember>, ServiceProviderListener<GroupMember> {
 
+    protected static final Logger log = Logger.getLogger(ModuleAvailabilityRegistrarBean.class.getSimpleName());
+
     // inject the ServiceProviderRegistrar instance used by ModuleAvailabilityRegistrarService
     @Resource(lookup="java:jboss/clustering/server/service-provider-registrar/ejb/client-services")
     ServiceProviderRegistrar<Object, GroupMember> registrar;
@@ -33,33 +35,43 @@ public class ModuleAvailabilityRegistrarBean implements ServiceProviderRegistrar
     // ServiceProviderRegistrar interface
     @Override
     public Group<GroupMember> getGroup() {
+        log.info("Calling getGroup()");
         return registrar.getGroup();
     }
 
     @Override
     public ServiceProviderRegistration<Object, GroupMember> register(Object service) {
+        log.infof("Calling register() with identifier %s\n", service);
         return registrar.register(service);
     }
 
     @Override
     public ServiceProviderRegistration<Object, GroupMember> register(Object service, ServiceProviderListener<GroupMember> listener) {
+        log.infof("Calling register() with identifier %s\n", service);
         return registrar.register(service, listener);
     }
 
     @Override
     public Set<GroupMember> getProviders(Object service) {
-        return registrar.getProviders(service);
+        log.infof("Calling getProviders() with identifier %s\n", service);
+        Set<GroupMember> result = registrar.getProviders(service);
+        log.infof("Called getProviders() with identifier %s: result = %s\n", service, result);
+        return result;
+//        return registrar.getProviders(service);
     }
 
     @Override
     public Set<Object> getServices() {
-        return registrar.getServices();
+        log.info("Calling getServices()");
+        Set<Object> result = registrar.getServices();
+        log.infof("Called getServices(): result = %s", result);
+        return result;
+//        return registrar.getServices();
     }
 
     // ServiceProviderRegistrarListener interface
     @Override
     public void providersChanged(Set<GroupMember> providers) {
-        System.out.println("Providers changed: " + providers);
+        log.infof("Calling providers changed: provider set %s", providers);
     }
-
 }
