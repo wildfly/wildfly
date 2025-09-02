2f041fb8b80b91386efffcad460cc5da23e3fd7f
Scott Marlow <smarlow@redhat.com>
2025-09-02 09:35:21 -0400

    [WFLY-19554] remove unneeded logger use
    Signed-off-by: Scott Marlow <smarlow@redhat.com>

--- jpa/persistencecdiextension/src/main/java/org/jboss/as/jpa/beanmanager/IntegratePersistenceAfterBeanDiscovery.java
+++ jpa/persistencecdiextension/src/main/java/org/jboss/as/jpa/beanmanager/IntegratePersistenceAfterBeanDiscovery.java
@@ -199,13 +199,6 @@ public class IntegratePersistenceAfterBeanDiscovery implements PersistenceCdiExt
         BeanConfigurator<EntityManagerFactory> beanConfigurator = afterBeanDiscovery.addBean();
         beanConfigurator.addTransitiveTypeClosure(EntityManagerFactory.class);
 
-
-        /**
-         String beanName = persistenceUnitMetadata.getScopedPersistenceUnitName().replace("#",".");
-         JpaLogger.ROOT_LOGGER.infof("Creating EntityManagerFactory CDI bean named %s for accessing persistence unit %s",
-         beanName, persistenceUnitMetadata.getPersistenceUnitName());
-         beanConfigurator.name(beanName);
-         **/
         // Ensure that we do not get "WELD-001414: Bean name is ambiguous" deployment failure with duplicate persistence units in a deployment.
         if (!persistenceUnitMetadata.isDuplicate()) {
             beanConfigurator.name(persistenceUnitMetadata.getPersistenceUnitName());
