aaf176da8d39973e83c33434e0daad30e10fdf92
Brian Stansberry <brian.stansberry@redhat.com>
2025-09-19 16:22:23 -0500

    Ignore intermittent container start failures tracked by WFLY-20945

--- testsuite/integration/expansion/src/test/java/org/wildfly/test/integration/microprofile/reactive/RunArtemisAmqpSetupTask.java
+++ testsuite/integration/expansion/src/test/java/org/wildfly/test/integration/microprofile/reactive/RunArtemisAmqpSetupTask.java
@@ -20,7 +20,9 @@ import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.model.test.ModelTestUtils;
+import org.jboss.as.test.shared.IntermittentFailure;
 import org.jboss.dmr.ModelNode;
+import org.junit.AssumptionViolatedException;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
@@ -84,7 +86,15 @@ public class RunArtemisAmqpSetupTask implements ServerSetupTask {
 
 
             }
-            container.start();
+
+            try {
+                container.start();
+            } catch (Exception e) {
+                // Either throw AssumptionViolatedException because we are ignoring intermittent failures,
+                // or propagate the exception and fail
+                IntermittentFailure.thisTestIsFailingIntermittently("https://issues.redhat.com/browse/WFLY-20945");
+                throw e;
+            }
 
             // Set the calculated port as a property in the model
             int amqpPort = container.getMappedPort(AMQP_PORT);
@@ -96,6 +106,9 @@ public class RunArtemisAmqpSetupTask implements ServerSetupTask {
                 tearDown(managementClient, containerId);
             } catch (Exception ex) {
                 e.printStackTrace();
+            }
+            if (e instanceof AssumptionViolatedException ave) {
+                throw ave;
             }
             throw new RuntimeException(e);
         }
