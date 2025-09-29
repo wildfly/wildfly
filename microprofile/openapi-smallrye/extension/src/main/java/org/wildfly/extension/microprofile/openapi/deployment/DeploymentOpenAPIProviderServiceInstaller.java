9174f0f7c277422cd6261b121c1be35291e0f79f
Paul Ferraro <paul.ferraro@redhat.com>
2025-09-29 21:15:10 +0100

    Prepend context path to OpenAPI service path, so that service paths from different applications sharing the same virtual host do not collide.

--- microprofile/openapi-smallrye/extension/src/main/java/org/wildfly/extension/microprofile/openapi/deployment/DeploymentOpenAPIProviderServiceInstaller.java
+++ microprofile/openapi-smallrye/extension/src/main/java/org/wildfly/extension/microprofile/openapi/deployment/DeploymentOpenAPIProviderServiceInstaller.java
@@ -14,13 +14,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import io.smallrye.openapi.api.OpenApiConfig;
 import io.smallrye.openapi.api.SmallRyeOpenAPI;
+import io.smallrye.openapi.runtime.OpenApiProcessor;
 import io.smallrye.openapi.runtime.scanner.spi.AnnotationScanner;
 import io.undertow.servlet.api.DeploymentInfo;
 
@@ -28,6 +32,8 @@ import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.openapi.OASFactory;
 import org.eclipse.microprofile.openapi.OASFilter;
 import org.eclipse.microprofile.openapi.models.OpenAPI;
+import org.eclipse.microprofile.openapi.models.PathItem;
+import org.eclipse.microprofile.openapi.models.Paths;
 import org.eclipse.microprofile.openapi.models.info.Info;
 import org.eclipse.microprofile.openapi.models.servers.Server;
 import org.jboss.as.network.ClientMapping;
@@ -89,6 +95,7 @@ public class DeploymentOpenAPIProviderServiceInstaller implements DeploymentServ
         String serverName = this.configuration.getServerName();
         String hostName = this.configuration.getHostName();
         String modelName = this.configuration.getModelName();
+        OpenApiConfig openAPIConfig = this.configuration.getOpenApiConfig();
 
         ServiceDependency<Host> host = ServiceDependency.on(Host.SERVICE_DESCRIPTOR, serverName, hostName);
         ServiceDependency<DeploymentInfo> deploymentInfo = ServiceDependency.on(UndertowService.deploymentServiceName(unit.getServiceName()).append(UndertowDeploymentInfoService.SERVICE_NAME));
@@ -96,81 +103,108 @@ public class DeploymentOpenAPIProviderServiceInstaller implements DeploymentServ
         Supplier<OpenAPI> factory = new Supplier<>() {
             @Override
             public OpenAPI get() {
-                return SmallRyeOpenAPI.builder()
+                SmallRyeOpenAPI.Builder builder = SmallRyeOpenAPI.builder()
+                        // Disable "standard filter" logic so that our filter can run last
+                        .enableStandardFilter(false)
                         .withApplicationClassLoader(module.getClassLoader())
                         .withConfig(config)
                         .withIndex(index)
                         .withResourceLocator(resourceResolver)
                         .withScannerClassLoader(WildFlySecurityManager.getClassLoaderPrivileged(AnnotationScanner.class))
-                        .addFilter(new OASFilter() {
-                            @Override
-                            public void filterOpenAPI(OpenAPI model) {
-                                // Generate default title and description based on web metadata
-                                DescriptionGroupMetaData descriptionMetaData = metaData.getDescriptionGroup();
-                                String displayName = (descriptionMetaData != null) ? descriptionMetaData.getDisplayName() : null;
-                                String title = (displayName != null) ? displayName : deploymentName;
-                                String description = (descriptionMetaData != null) ? descriptionMetaData.getDescription() : null;
+                        ;
+                // SmallRyeOpenAPI.Builder.build() woud otherwise apply the standard filter last
+                @SuppressWarnings("deprecation")
+                OASFilter filter = OpenApiProcessor.getFilter(openAPIConfig, module.getClassLoader(), index);
+                if (filter != null) {
+                    builder.addFilter(filter);
+                }
+                builder.addFilter(new OASFilter() {
+                    @Override
+                    public void filterOpenAPI(OpenAPI model) {
+                        // Generate default title and description based on web metadata
+                        DescriptionGroupMetaData descriptionMetaData = metaData.getDescriptionGroup();
+                        String displayName = (descriptionMetaData != null) ? descriptionMetaData.getDisplayName() : null;
+                        String title = (displayName != null) ? displayName : deploymentName;
+                        String description = (descriptionMetaData != null) ? descriptionMetaData.getDescription() : null;
 
-                                Info info = model.getInfo();
-                                if (info == null) {
-                                    info = OASFactory.createInfo();
-                                    model.setInfo(info);
+                        Info info = model.getInfo();
+                        if (info == null) {
+                            info = OASFactory.createInfo();
+                            model.setInfo(info);
+                        }
+                        if (info.getTitle() == null) {
+                            info.setTitle(title);
+                        }
+                        if (info.getDescription() == null) {
+                            info.setDescription(description);
+                        }
+
+                        Collection<UndertowListener> listeners = host.get().getServer().getListeners();
+
+                        if (listeners.stream().map(UndertowListener::getProtocol).noneMatch(REQUISITE_LISTENERS::contains)) {
+                            LOGGER.requiredListenersNotFound(host.get().getServer().getName(), REQUISITE_LISTENERS);
+                        }
+
+                        // Generate Server entries if none exist
+                        if ((model.getServers() == null) && !useRelativeServerURLs) {
+                            int aliases = host.get().getAllAliases().size();
+                            int size = 0;
+                            for (UndertowListener listener : listeners) {
+                                size += aliases + listener.getSocketBinding().getClientMappings().size();
+                            }
+                            List<Server> servers = new ArrayList<>(size);
+                            for (UndertowListener listener : listeners) {
+                                SocketBinding binding = listener.getSocketBinding();
+                                Set<String> virtualHosts = new TreeSet<>(host.get().getAllAliases());
+                                // The name of the host is not a real virtual host (e.g. default-host)
+                                virtualHosts.remove(host.get().getName());
+
+                                InetAddress address = binding.getAddress();
+                                // Omit wildcard addresses
+                                if (!address.isAnyLocalAddress()) {
+                                    virtualHosts.add(address.getCanonicalHostName());
                                 }
-                                if (info.getTitle() == null) {
-                                    info.setTitle(title);
+
+                                for (String virtualHost : virtualHosts) {
+                                    Server server = createServer(listener.getProtocol(), virtualHost, binding.getPort());
+                                    if (server != null) {
+                                        servers.add(server);
+                                    }
                                 }
-                                if (info.getDescription() == null) {
-                                    info.setDescription(description);
-                                }
-
-                                Collection<UndertowListener> listeners = host.get().getServer().getListeners();
-
-                                if (listeners.stream().map(UndertowListener::getProtocol).noneMatch(REQUISITE_LISTENERS::contains)) {
-                                    LOGGER.requiredListenersNotFound(host.get().getServer().getName(), REQUISITE_LISTENERS);
-                                }
-
-                                if (model.getServers() == null) {
-                                    // Generate Server entries if none exist
-                                    String contextPath = deploymentInfo.get().getContextPath();
-                                    if (useRelativeServerURLs) {
-                                        model.setServers(List.of(OASFactory.createServer().url(contextPath)));
-                                    } else {
-                                        int aliases = host.get().getAllAliases().size();
-                                        int size = 0;
-                                        for (UndertowListener listener : listeners) {
-                                            size += aliases + listener.getSocketBinding().getClientMappings().size();
-                                        }
-                                        List<Server> servers = new ArrayList<>(size);
-                                        for (UndertowListener listener : listeners) {
-                                            SocketBinding binding = listener.getSocketBinding();
-                                            Set<String> virtualHosts = new TreeSet<>(host.get().getAllAliases());
-                                            // The name of the host is not a real virtual host (e.g. default-host)
-                                            virtualHosts.remove(host.get().getName());
-
-                                            InetAddress address = binding.getAddress();
-                                            // Omit wildcard addresses
-                                            if (!address.isAnyLocalAddress()) {
-                                                virtualHosts.add(address.getCanonicalHostName());
-                                            }
-
-                                            for (String virtualHost : virtualHosts) {
-                                                Server server = createServer(listener.getProtocol(), virtualHost, binding.getPort(), contextPath);
-                                                if (server != null) {
-                                                    servers.add(server);
-                                                }
-                                            }
-                                            for (ClientMapping mapping : binding.getClientMappings()) {
-                                                Server server = createServer(listener.getProtocol(), mapping.getDestinationAddress(), mapping.getDestinationPort(), contextPath);
-                                                if (server != null) {
-                                                    servers.add(server);
-                                                }
-                                            }
-                                        }
-                                        model.setServers(servers);
+                                for (ClientMapping mapping : binding.getClientMappings()) {
+                                    Server server = createServer(listener.getProtocol(), mapping.getDestinationAddress(), mapping.getDestinationPort());
+                                    if (server != null) {
+                                        servers.add(server);
                                     }
                                 }
                             }
-                        }).build().model();
+                            model.setServers(servers);
+                        }
+                    }
+                });
+                String contextPath = deploymentInfo.get().getContextPath();
+                // If this application is not deployed to the root context, we need to append the context path to all service paths.
+                // This ensures that multiple applications sharing the same virtual host can document services with the same path without colliding.
+                if (contextPath.length() > 1) {
+                    // N.B. The MicroProfile OpenAPI specification API inexplicably lacks a common path API.
+                    builder.addFilter(new OASFilter() {
+                        @Override
+                        public void filterOpenAPI(OpenAPI model) {
+                            Paths paths = model.getPaths();
+                            if (paths != null) {
+                                Map<String, PathItem> items = Optional.ofNullable(paths.getPathItems()).orElse(Map.of());
+                                for (Map.Entry<String, PathItem> entry : items.entrySet()) {
+                                    String path = entry.getKey();
+                                    PathItem item = entry.getValue();
+
+                                    paths.removePathItem(path);
+                                    paths.addPathItem(contextPath + path, item);
+                                }
+                            }
+                        }
+                    });
+                }
+                return builder.build().model();
             }
         };
         ServiceInstaller.builder(OpenAPIProvider::of, factory)
@@ -181,11 +215,11 @@ public class DeploymentOpenAPIProviderServiceInstaller implements DeploymentServ
                 .install(context);
     }
 
-    private static Server createServer(String protocol, String host, int port, String path) {
+    private static Server createServer(String protocol, String host, int port) {
         try {
-            URL url = new URL(protocol, host, port, path);
+            URL url = new URL(protocol, host, port, "");
             if (port == url.getDefaultPort()) {
-                url = new URL(protocol, host, path);
+                url = new URL(protocol, host, "");
             }
             return OASFactory.createServer().url(url.toString());
         } catch (MalformedURLException e) {
