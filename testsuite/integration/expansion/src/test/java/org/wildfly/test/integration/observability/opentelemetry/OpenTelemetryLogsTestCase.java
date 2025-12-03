afe1eb2ae944415ec322e41f8920514e97ae977e
Jason Lee <jason@steeplesoft.com>
2025-12-02 22:23:18 -0600

    WFLY-21232 - Update SmallRye OpenTelemetry to 2.11.1
    Bump version number
    Add test to verify incoming bug fix

--- testsuite/integration/expansion/src/test/java/org/wildfly/test/integration/observability/opentelemetry/OpenTelemetryLogsTestCase.java
+++ testsuite/integration/expansion/src/test/java/org/wildfly/test/integration/observability/opentelemetry/OpenTelemetryLogsTestCase.java
@@ -7,36 +7,65 @@ package org.wildfly.test.integration.observability.opentelemetry;
 import java.net.URL;
 
 import jakarta.ws.rs.core.Response;
+import org.jboss.arquillian.container.test.api.Deployer;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.arquillian.api.ServerSetup;
 import org.jboss.as.test.shared.observability.setuptasks.OpenTelemetryWithCollectorSetupTask;
-import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
+import org.wildfly.test.integration.observability.opentelemetry.application.OtelService2;
 
 @RunAsClient
 @ServerSetup({OpenTelemetryWithCollectorSetupTask.class})
 public class OpenTelemetryLogsTestCase extends BaseOpenTelemetryTest {
     private static final String DEPLOYMENT_NAME = "otel-logs-test";
 
-    @ArquillianResource
-    private URL url;
+    private static final String DEPLOYMENT_SERVICE1 = "service1";
+    private static final String DEPLOYMENT_SERVICE2 = "service2";
 
-    @Deployment(testable = false)
-    public static Archive<?> getDeployment() {
-        return buildBaseArchive(DEPLOYMENT_NAME);
+    private static final String EXPECTED_LOG_ENTRY = "This is a test message: hello";
+
+    @ArquillianResource
+    private Deployer deployer;
+
+    @Deployment(name = DEPLOYMENT_SERVICE1, managed = false, testable = false)
+    public static WebArchive getDeployment1() {
+        return buildBaseArchive(DEPLOYMENT_SERVICE1);
+    }
+
+    @Deployment(name = DEPLOYMENT_SERVICE2, managed = false, testable = false)
+    public static WebArchive getDeployment2() {
+        return buildBaseArchive(DEPLOYMENT_SERVICE2).addClass(OtelService2.class);
     }
 
     @Test
-    public void testLogs() throws Exception {
-        makeRequests(new URL(url, "logging/hello"), 1, Response.noContent().build().getStatus());
+    @InSequence(1)
+    public void deploy() {
+        deployer.deploy(DEPLOYMENT_SERVICE1);
+        deployer.deploy(DEPLOYMENT_SERVICE2);
+    }
 
-        String expectedLogEntry = "This is a test message: hello";
+    @Test
+    @InSequence(2)
+    public void testFormattedLogMessage() throws Exception {
+        makeRequests(new URL(getDeploymentUrl(DEPLOYMENT_SERVICE1) + "logging/hello"), 1, Response.noContent().build().getStatus());
+
         otelCollector.assertOpenTelemetryLogs(logEntries ->
-                Assert.assertTrue("Missing log entry: '" + expectedLogEntry + "'",
+                Assert.assertTrue("Missing log entry: '" + EXPECTED_LOG_ENTRY + "'",
                         logEntries.stream().anyMatch(entry ->
-                                entry.body().contains(expectedLogEntry))));
+                                entry.body().contains(EXPECTED_LOG_ENTRY))));
+    }
+
+    @Test
+    @InSequence(3)
+    public void testDuplicateLogs() {
+        var logMessages = otelCollector.getOpenTelemetryLogs().stream()
+                .filter(log -> log.body().contains(EXPECTED_LOG_ENTRY)).toList();
+
+        Assert.assertEquals("Duplicated log entry found", 1, logMessages.size());
     }
 }
