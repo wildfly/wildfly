86b1483e4c685789f0c5d71544cf22374b1ccc6b
Tomasz Adamski <tomasz.adamski@ibm.com>
2026-01-23 16:42:27 +0100

    [WFLY-19816] EJB: support regex in ejb-name property of container interceptor mapping

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/container/interceptor/regex/TestInterceptorWrongRegex.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.as.test.integration.ejb.container.interceptor.regex;
+
+import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.InvocationContext;
+
+public class TestInterceptorWrongRegex {
+    public static boolean invoked = false;
+
+    @AroundInvoke
+    public Object invoke(final InvocationContext invocationContext) throws Exception {
+        invoked = true;
+        return invocationContext.proceed();
+    }
+
+    boolean wasInvoked() {
+        return invoked;
+    }
+}
