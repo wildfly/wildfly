4bdc17e7d0cf84d98762bb0a98c2b7c471788759
Brian Stansberry <bstansbe@ibm.com>
2026-02-23 16:25:54 -0600

    [WFLY-21515] Comment out unused JipiLogger methods

--- jpa/spi/src/main/java/org/jipijapa/JipiLogger.java
+++ jpa/spi/src/main/java/org/jipijapa/JipiLogger.java
@@ -5,15 +5,11 @@
 
 package org.jipijapa;
 
-import static org.jboss.logging.Logger.Level.WARN;
 
 import java.lang.invoke.MethodHandles;
 
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
-import org.jboss.logging.annotations.Cause;
-import org.jboss.logging.annotations.LogMessage;
-import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 
 /**
@@ -30,84 +26,84 @@ public interface JipiLogger extends BasicLogger {
     JipiLogger JPA_LOGGER = Logger.getMessageLogger(MethodHandles.lookup(), JipiLogger.class, "org.jipijapa");
 
 
-    /**
-     * warn that the entity class could not be loaded with the
-     * {@link jakarta.persistence.spi.PersistenceUnitInfo#getClassLoader()}.
-     *
-     * @param cause     the cause of the error.
-     * @param className the entity class name.
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 20200, value = "Could not load entity class '%s', ignoring this error and continuing with application deployment")
-    void cannotLoadEntityClass(@Cause Throwable cause, String className);
+//    /**
+//     * warn that the entity class could not be loaded with the
+//     * {@link jakarta.persistence.spi.PersistenceUnitInfo#getClassLoader()}.
+//     *
+//     * @param cause     the cause of the error.
+//     * @param className the entity class name.
+//     */
+//    @LogMessage(level = WARN)
+//    @Message(id = 20200, value = "Could not load entity class '%s', ignoring this error and continuing with application deployment")
+//    void cannotLoadEntityClass(@Cause Throwable cause, String className);
 
-    /**
-     * Creates an exception indicating the input stream reference cannot be changed.
-     *
-     * @return an {@link IllegalArgumentException} for the error.
-     */
-    @Message(id = 20201, value = "Cannot change input stream reference.")
-    IllegalArgumentException cannotChangeInputStream();
+//    /**
+//     * Creates an exception indicating the input stream reference cannot be changed.
+//     *
+//     * @return an {@link IllegalArgumentException} for the error.
+//     */
+//    @Message(id = 20201, value = "Cannot change input stream reference.")
+//    IllegalArgumentException cannotChangeInputStream();
 
-    /**
-     * Creates an exception indicating the parameter, likely a collection, is empty.
-     *
-     * @param parameterName the parameter name.
-     * @return an {@link IllegalArgumentException} for the error.
-     */
-    @Message(id = 20202, value = "Parameter %s is empty")
-    IllegalArgumentException emptyParameter(String parameterName);
+//    /**
+//     * Creates an exception indicating the parameter, likely a collection, is empty.
+//     *
+//     * @param parameterName the parameter name.
+//     * @return an {@link IllegalArgumentException} for the error.
+//     */
+//    @Message(id = 20202, value = "Parameter %s is empty")
+//    IllegalArgumentException emptyParameter(String parameterName);
 
-    /**
-     * Creates an exception indicating the persistence unit metadata likely because thread local was not set.
-     *
-     * @return a {@link RuntimeException} for the error.
-     */
-    @Message(id = 20203, value = "Missing PersistenceUnitMetadata (thread local wasn't set)")
-    RuntimeException missingPersistenceUnitMetadata();
+//    /**
+//     * Creates an exception indicating the persistence unit metadata likely because thread local was not set.
+//     *
+//     * @return a {@link RuntimeException} for the error.
+//     */
+//    @Message(id = 20203, value = "Missing PersistenceUnitMetadata (thread local wasn't set)")
+//    RuntimeException missingPersistenceUnitMetadata();
 
-    /**
-     * Creates an exception indicating the method is not yet implemented.
-     *
-     * @return a {@link RuntimeException} for the error.
-     */
-    @Message(id = 20204, value = "Not yet implemented")
-    RuntimeException notYetImplemented();
+//    /**
+//     * Creates an exception indicating the method is not yet implemented.
+//     *
+//     * @return a {@link RuntimeException} for the error.
+//     */
+//    @Message(id = 20204, value = "Not yet implemented")
+//    RuntimeException notYetImplemented();
 
-    /**
-     * Creates an exception indicating the variable is {@code null}.
-     *
-     * @param varName the variable name.
-     * @return an {@link IllegalArgumentException} for the error.
-     */
-    @Message(id = 20205, value = "Parameter %s is null")
-    IllegalArgumentException nullVar(String varName);
+//    /**
+//     * Creates an exception indicating the variable is {@code null}.
+//     *
+//     * @param varName the variable name.
+//     * @return an {@link IllegalArgumentException} for the error.
+//     */
+//    @Message(id = 20205, value = "Parameter %s is null")
+//    IllegalArgumentException nullVar(String varName);
 
-    /**
-     * Could not open VFS stream
-     *
-     * @param cause the cause of the error.
-     * @param name of VFS file
-     * @return a {@link RuntimeException} for the error.
-     */
-    @Message(id = 20250, value = "Unable to open VirtualFile-based InputStream %s")
-    RuntimeException cannotOpenVFSStream(@Cause Throwable cause, String name);
+//    /**
+//     * Could not open VFS stream
+//     *
+//     * @param cause the cause of the error.
+//     * @param name of VFS file
+//     * @return a {@link RuntimeException} for the error.
+//     */
+//    @Message(id = 20250, value = "Unable to open VirtualFile-based InputStream %s")
+//    RuntimeException cannotOpenVFSStream(@Cause Throwable cause, String name);
 
-    /**
-     * URI format is incorrect, which results in a syntax error
-     *
-     * @return an {@link IllegalArgumentException} for the error.
-     */
-    @Message(id = 20251, value = "URI syntax error")
-    IllegalArgumentException uriSyntaxException(@Cause Throwable cause);
+//    /**
+//     * URI format is incorrect, which results in a syntax error
+//     *
+//     * @return an {@link IllegalArgumentException} for the error.
+//     */
+//    @Message(id = 20251, value = "URI syntax error")
+//    IllegalArgumentException uriSyntaxException(@Cause Throwable cause);
 
-    /**
-     * warn that the 2nd is not integrated
-     *
-     * @param scopedPuName identifies the app specific persistence unit name
-     */
-    @LogMessage(level = WARN)
-    @Message(id = 20252, value = "second level cache not integrated - %s")
-    void cannotUseSecondLevelCache(String scopedPuName);
+//    /**
+//     * warn that the 2nd is not integrated
+//     *
+//     * @param scopedPuName identifies the app specific persistence unit name
+//     */
+//    @LogMessage(level = WARN)
+//    @Message(id = 20252, value = "second level cache not integrated - %s")
+//    void cannotUseSecondLevelCache(String scopedPuName);
 
 }
