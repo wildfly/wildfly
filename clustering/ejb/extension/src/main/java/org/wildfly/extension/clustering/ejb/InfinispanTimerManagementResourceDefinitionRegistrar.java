3acb95ebcab9b8fa6d987e87aa106e51a89bdfcf
Radoslav Husar <radosoft@gmail.com>
2026-02-27 21:29:28 +0100

    WFLY-21526 Promote "WFLY-20918 Add idle time-based eviction configuration for distributable HttpSessions, SFSBs, Timers" to DEFAULT stability
    Signed-off-by: Radoslav Husar <radosoft@gmail.com>

--- clustering/ejb/extension/src/main/java/org/wildfly/extension/clustering/ejb/InfinispanTimerManagementResourceDefinitionRegistrar.java
+++ clustering/ejb/extension/src/main/java/org/wildfly/extension/clustering/ejb/InfinispanTimerManagementResourceDefinitionRegistrar.java
@@ -23,7 +23,6 @@ import org.jboss.as.controller.descriptions.ResourceDescriptionResolver;
 import org.jboss.as.controller.operations.validation.IntRangeValidator;
 import org.jboss.as.controller.registry.AttributeAccess.Flag;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
-import org.jboss.as.version.Stability;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.ModelType;
 import org.jboss.modules.Module;
@@ -68,7 +67,6 @@ public class InfinispanTimerManagementResourceDefinitionRegistrar implements Chi
 
     static final DurationAttributeDefinition IDLE_THRESHOLD = DurationAttributeDefinition.builder("idle-threshold")
             .setRequired(false)
-            .setStability(Stability.COMMUNITY)
             .build();
 
     @Override
