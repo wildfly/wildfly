32c1a3a0d99b0a3a97c9d97915a17b678282a135
Paul Ferraro <paul.ferraro@redhat.com>
2026-03-30 10:41:17 +0100

    Adjust mixed-domain test configuration manipulation.

--- testsuite/mixed-domain/src/test/java/org/jboss/as/test/integration/domain/mixed/wfly31/DomainAdjusterWFLY31.java
+++ testsuite/mixed-domain/src/test/java/org/jboss/as/test/integration/domain/mixed/wfly31/DomainAdjusterWFLY31.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 import org.jboss.as.controller.PathAddress;
 import org.jboss.as.controller.client.helpers.domain.DomainClient;
-import org.jboss.as.controller.operations.common.Util;
 import org.jboss.as.test.integration.domain.mixed.DomainAdjuster;
 import org.jboss.dmr.ModelNode;
 
@@ -36,11 +35,7 @@ public class DomainAdjusterWFLY31 extends DomainAdjuster {
     private static void adjustJGroups(List<ModelNode> operations, PathAddress subsystemAddress) {
         // Remove protocols that do not exist in WFLY31, but don't bother replacing
         for (String stack : Arrays.asList("tcp", "udp")) {
-            for (String protocol : Arrays.asList("NAKACK4", "UNICAST4")) {
-                operations.add(Util.createRemoveOperation(subsystemAddress.append("stack", stack).append("protocol", protocol)));
-            }
-            // Remote GMS too - this requires reliable unicast/multicast
-            operations.add(Util.createRemoveOperation(subsystemAddress.append("stack", stack).append("protocol", "pbcast.GMS")));
+            // Reverted stack changes
         }
     }
 }
