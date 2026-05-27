b48d80a5f5f889cc3db5e56abdaf4b7b08185c5d
Radoslav Husar <radosoft@gmail.com>
2026-05-27 22:31:11 +0200

    WFLY-21803 InfinispanModulesTestSuite not executing
    Signed-off-by: Radoslav Husar <rhusar@ibm.com>

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/single/infinispan/InfinispanModulesTestSuite.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/single/infinispan/InfinispanModulesTestSuite.java
@@ -5,6 +5,7 @@
 
 package org.jboss.as.test.clustering.single.infinispan;
 
+import org.infinispan.server.test.core.TestServer;
 import org.infinispan.server.test.junit5.InfinispanServerExtension;
 import org.jboss.as.test.clustering.InfinispanServerUtil;
 import org.jboss.as.test.clustering.single.infinispan.cdi.embedded.GreetingCacheManagerTestCase;
@@ -14,24 +15,22 @@ import org.jboss.as.test.clustering.single.infinispan.query.ContainerManagedHotR
 import org.jboss.as.test.clustering.single.infinispan.query.ContainerRemoteQueryTestCase;
 import org.jboss.as.test.clustering.single.infinispan.query.HotRodClientTestCase;
 import org.jboss.as.test.clustering.single.infinispan.query.RemoteQueryTestCase;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import org.junit.platform.suite.api.AfterSuite;
+import org.junit.platform.suite.api.BeforeSuite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
 /**
- * JUnit suite class to support testable deployments - tests running in-container - typically to verify integration with Infinispan Modules.
- * This avoids the problem of having the Infinispan Server test driver JUnit TestRule be run e.g. in-container.
- * Using suite in conjunction with the @ClassRule starts the Infinispan server prior to running the tests in-container,
- * as a side effect makes the execution pretty fast.
- * NOTE: This doesn't work with -Dtest=... ask the @RunWith(Suite.class) is not in effect thus the @ClassRule does not invoke!
- * For a workaround, to run a single test, comment out the undesired classes in this file.
+ * JUnit suite class to support testable deployments - tests running in-container - typically to verify integration with
+ * Infinispan Modules. This avoids the problem of having the Infinispan Server test driver JUnit extension be run e.g.
+ * in-container. Using suite starts the Infinispan server prior to running the tests in-container, as a side effect makes
+ * the execution pretty fast.
  *
  * @author Radoslav Husar
  * @since 27
  */
-@RunWith(Suite.class)
-@SuiteClasses({
+@Suite
+@SelectClasses({
         HotRodClientTestCase.class,
         RemoteQueryTestCase.class,
         ContainerRemoteQueryTestCase.class,
@@ -42,7 +41,25 @@ import org.junit.runners.Suite.SuiteClasses;
 })
 public class InfinispanModulesTestSuite {
 
-    @RegisterExtension
-    public static final InfinispanServerExtension SERVER = InfinispanServerUtil.infinispanServerExtension();
+    private static final String TEST_NAME = InfinispanModulesTestSuite.class.getName();
+    private static final InfinispanServerExtension SERVER = InfinispanServerUtil.infinispanServerExtension();
 
+    @BeforeSuite
+    static void startInfinispanServer() {
+        TestServer testServer = SERVER.getTestServer();
+        testServer.initServerDriver();
+        testServer.getDriver().prepare(TEST_NAME);
+        testServer.beforeListeners();
+        testServer.enhanceConfiguration();
+        testServer.getDriver().start(TEST_NAME);
+    }
+
+    @AfterSuite
+    static void stopInfinispanServer() {
+        TestServer testServer = SERVER.getTestServer();
+        if (testServer.isDriverInitialized()) {
+            testServer.stopServerDriver(TEST_NAME);
+            testServer.afterListeners();
+        }
+    }
 }
