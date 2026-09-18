2781ba73d8025fe02086d8e33535ef370c7d5770
Jason Lee <jason@steeplesoft.com>
2026-09-18 13:54:43 -0500

    WFLY-22046 - Bump Micrometer and OpenTelemetry model and schema versions
    Micrometer: model 2.1.0 and community schema 2.1.
    OpenTelemetry: model 1.2.0 and community schema 1.2.

--- observability/opentelemetry/src/main/java/org/wildfly/extension/opentelemetry/OpenTelemetryExtensionTransformerRegistration.java
+++ observability/opentelemetry/src/main/java/org/wildfly/extension/opentelemetry/OpenTelemetryExtensionTransformerRegistration.java
@@ -1,6 +1,11 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.wildfly.extension.opentelemetry;
 
 import static org.wildfly.extension.opentelemetry.OpenTelemetrySubsystemModel.VERSION_1_0_0;
+import static org.wildfly.extension.opentelemetry.OpenTelemetrySubsystemModel.VERSION_1_1_0;
 
 import org.jboss.as.controller.transform.ExtensionTransformerRegistration;
 import org.jboss.as.controller.transform.SubsystemTransformerRegistration;
@@ -19,9 +24,14 @@ public class OpenTelemetryExtensionTransformerRegistration implements ExtensionT
 
     @Override
     public void registerTransformers(SubsystemTransformerRegistration registration) {
-        ChainedTransformationDescriptionBuilder builder = TransformationDescriptionBuilder.Factory.createChainedSubystemInstance(registration.getCurrentSubsystemVersion());
+        ChainedTransformationDescriptionBuilder builder =
+                TransformationDescriptionBuilder.Factory.createChainedSubystemInstance(registration.getCurrentSubsystemVersion());
 
-        registerV_1_1_Transformers(builder.createBuilder(OpenTelemetrySubsystemModel.VERSION_1_1_0.getVersion(), VERSION_1_0_0.getVersion()));
+        builder.createBuilder(OpenTelemetrySubsystemModel.VERSION_1_2_0.getVersion(), VERSION_1_1_0.getVersion());
+        registerV_1_1_Transformers(builder.createBuilder(VERSION_1_1_0.getVersion(), VERSION_1_0_0.getVersion()));
+
+        builder.buildAndRegister(registration, new org.jboss.as.controller.ModelVersion[]{
+                VERSION_1_1_0.getVersion(), VERSION_1_0_0.getVersion()});
     }
 
     private void registerV_1_1_Transformers(ResourceTransformationDescriptionBuilder builder) {
