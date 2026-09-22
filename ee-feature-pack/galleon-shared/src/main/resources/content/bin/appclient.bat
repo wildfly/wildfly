23f6ee238a1b99ee2dfa9b7eb1b00a7975a475e0
Yeray Borges <yborgess@redhat.com>
2026-09-22 09:23:17 +0100

    [WFLY-22250] Incorrect usage of 'if errorlevel' on Windows batch scripts
    Jira issue: https://redhat.atlassian.net/browse/WFLY-22250
    
    Signed-off-by: Yeray Borges <yborgess@redhat.com>

--- ee-feature-pack/galleon-shared/src/main/resources/content/bin/appclient.bat
+++ ee-feature-pack/galleon-shared/src/main/resources/content/bin/appclient.bat
@@ -22,7 +22,7 @@ setlocal EnableDelayedExpansion
 call "!DIRNAME!common.bat" :commonConf
 rem check for the security manager system property
 echo(!SERVER_OPTS! | findstr /r /c:"-Djava.security.manager" > nul
-if not errorlevel == 1 (
+if not errorlevel 1 (
     echo ERROR: The use of -Djava.security.manager has been removed. Please use the -secmgr command line argument or SECMGR=true environment variable.
     GOTO :EOF
 )
@@ -90,7 +90,7 @@ setlocal DisableDelayedExpansion
 
 rem Add -server to the JVM options, if supported
 "%JAVA%" -server -version 2>&1 | findstr /I hotspot > nul
-if not errorlevel == 1 (
+if not errorlevel 1 (
   set "JAVA_OPTS=%JAVA_OPTS% -server"
 )
 
