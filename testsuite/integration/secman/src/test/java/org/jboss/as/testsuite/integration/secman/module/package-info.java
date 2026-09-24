8cec0ad001a3ea69f3a1700065cd078286ab116b
Yeray Borges <yborgess@redhat.com>
2026-09-24 19:10:21 +0100

    [WFLY-20213] Make Security Manager as a model-only subsystem including documentation changes and galleon layers removal
    Jira issue: https://redhat.atlassian.net/browse/WFLY-22243
    
    Signed-off-by: Yeray Borges <yborgess@redhat.com>

--- testsuite/integration/secman/src/test/java/org/jboss/as/testsuite/integration/secman/module/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright The WildFly Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * This package contains tests for permissions assigned to custom AS modules (<i>i.e. Permissions defined in module.xml</i>).
- */
-package org.jboss.as.testsuite.integration.secman.module;
--- testsuite/integration/secman/src/test/java/org/jboss/as/testsuite/integration/secman/servlets/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright The WildFly Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * Package in which servlets checking JSM permissions are located.
- */
-package org.jboss.as.testsuite.integration.secman.servlets;
